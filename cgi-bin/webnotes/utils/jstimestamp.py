2362bc800a533f118d42f2668c6e49c1bf9449c6
Pratik Vyas <pdvyas@erpnext.com>
2011-06-09 18:39:29 +0530

    Changed classname of jstimestamp and added disallowed list

--- cgi-bin/webnotes/utils/jstimestamp.py
+++ cgi-bin/webnotes/utils/jstimestamp.py
@@ -1,15 +1,29 @@
-class generate_timestamp:
+class generateTimestamp:
 	def list_js_files(self,jsdir,ext='js'):
 		import os
 		all_files= []
+		nono = ['./tiny_mce','./jquery']
 		oldcwd = os.getcwd()
 		os.chdir(jsdir)
 		for root, subfolders, files in os.walk('.'):
-			for filename in files:
-				if filename.endswith(ext):
-					all_files.append(os.path.join(root,filename))
+			if self.is_allowed(nono,root):
+				for filename in files:
+					if filename.endswith(ext):
+						all_files.append(os.path.join(root,filename))
+
 		os.chdir(oldcwd)
+		for i in nono:
+			for j in all_files:
+				if j.startswith(i):
+					all_files.remove(j)
 		return all_files
+	
+	def is_allowed(self,disallowed,item):
+		for i in disallowed:
+			if item.startswith(i):
+				return False
+		return True
+
 	
 	def get_timestamp_dict(self,jsdir,filelist):
 		tsdict={}
