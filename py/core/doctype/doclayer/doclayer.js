1ed4f2feb9dde7104453f3445180ddb6ec7510fb
Anand Doshi <anand@iwebnotes.com>
2011-11-23 13:56:35 +0530

    DocLayer ready for deployment

--- py/core/doctype/doclayer/doclayer.js
+++ py/core/doctype/doclayer/doclayer.js
@@ -17,9 +17,13 @@ cur_frm.cscript.onload = function(doc, dt, dn) {
 
 cur_frm.cscript.refresh = function(doc, dt, dn) {
 	//console.log(p)
-	cur_frm.frm_head.timestamp_area.hidden = 1;
-	cur_frm.frm_head.page_head.buttons.Save.hidden=1;
-	cur_frm.page_layout.footer.hidden = 1;
+	cur_frm.frm_head.timestamp_area.hidden = true;
+	cur_frm.frm_head.page_head.buttons.Save.hidden = true;
+	cur_frm.page_layout.footer.hidden = true;
+	cur_frm.tip_wrapper.id = 'tip_wrapper';
+	if(doc.doc_type) { $('#tip_wrapper').slideUp('slow'); }
+
+	
 	cur_frm.add_custom_button('Update', function() {
 		if(cur_frm.fields_dict['doc_type'].value) {
 			$c_obj(make_doclist(dt, dn), 'post', '', function(r, rt) {
@@ -37,7 +41,7 @@ cur_frm.cscript.refresh = function(doc, dt, dn) {
 	},1);
 	cur_frm.frm_head.page_head.buttons.Update.className = "cupid-green";
 	
-	cur_frm.add_custom_button('Reload', function() {
+	cur_frm.add_custom_button('Refresh Form', function() {
 		cur_frm.cscript.doc_type(doc, dt, dn);
 	}, 1);
 	
@@ -45,6 +49,15 @@ cur_frm.cscript.refresh = function(doc, dt, dn) {
 		cur_frm.confirm('This will <b>remove the customizations</b> defined for this form.<br /><br />' 
 		+ 'Are you sure you want to <i>reset to defaults</i>?', doc, dt, dn);
 	}, 1);
+
+	if(!doc.doc_type) {
+		cur_frm.set_tip('You can start by selecting a Type from the drop down menu')
+		$('#tip_wrapper').fadeIn();
+		var page_head = cur_frm.frm_head.page_head;
+		page_head.buttons['Update'].disabled = true;
+		page_head.buttons['Refresh Form'].disabled = true;
+		page_head.buttons['Reset to defaults'].disabled = true;
+	}
 }
 
 cur_frm.confirm = function(msg, doc, dt, dn) {
