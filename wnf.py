f5d4218d3de7802dbe3d24ee889997ed430d8f21
Rushabh Mehta <rmehta@gmail.com>
2012-02-03 12:56:19 +0530

    updated feed for comments and login

--- wnf.py
+++ wnf.py
@@ -30,6 +30,41 @@ def replace_code(start, txt1, txt2, extn):
 				
 					print 'updated in %s' % fpath
 
+def setup_options():
+	from optparse import OptionParser
+	parser = OptionParser()
+	parser.add_option("-b", "--build", default=False, action="store_true",
+						help="minify + concat js files")
+	parser.add_option("-c", "--clear", default=False, action="store_true",
+						help="increment version")
+	parser.add_option("--replace", nargs=3, default=False, 
+						metavar = "search replace_by extension",
+						help="file search-replace")
+	parser.add_option("--status", default=False, action="store_true",
+						help="git status")
+	parser.add_option("--pull", nargs=2, default=False,
+						metavar = "remote branch",
+						help="git pull (both repos)")
+	parser.add_option("--push", nargs=3, default=False, 
+						metavar = "remote branch comment",
+						help="git commit + push (both repos) [remote] [branch] [comment]")
+	parser.add_option("-l", "--latest",
+						action="store_true", dest="run_latest", default=False,
+						help="Apply the latest patches")
+	parser.add_option("-p", "--patch", nargs=1, dest="patch_list", metavar='patch_module',
+						action="append",
+						help="Apply patch")
+	parser.add_option("-f", "--force",
+						action="store_true", dest="force", default=False,
+						help="Force Apply all patches specified using option -p or --patch")
+	parser.add_option("-d", "--db",
+						dest="db_name",
+						help="Apply the patches on given db")
+	parser.add_option('-r', '--reload_doc', nargs=3, metavar = "module doctype docname",
+						help="reload doc")
+	
+	return parser.parse_args()
+	
 def run():
 	sys.path.append('.')
 	sys.path.append('lib/py')
@@ -37,57 +72,59 @@ def run():
 	import webnotes.defs
 	sys.path.append(webnotes.defs.modules_path)
 
-	if len(sys.argv)<2:
-		print_help()
-		return
+	(options, args) = setup_options()
 
-	cmd = sys.argv[1]			
 
-	if cmd=='build':
+	from webnotes.db import Database
+	import webnotes.modules.patch_handler
+
+	# build
+	if options.build:
 		import build.project
-		build.project.build()
-	
-	elif cmd=='clear':
+		build.project.build()		
+
+	elif options.clear:
 		from build.project import increment_version
 		print "Version:" + str(increment_version())
-		
-	# replace code
-	elif cmd=='replace':
-		replace_code('.', sys.argv[2], sys.argv[3], sys.argv[4])
-		
-	elif cmd=='patch':
-		from optparse import OptionParser
-		parser = OptionParser()
-		parser.add_option("-l", "--latest",
-						  action="store_true", dest="run_latest", default=False,
-						  help="Apply the latest patches")
-		parser.add_option("-p", "--patch", dest="patch_list", metavar='PATCH_MODULE.PATCH_FILE',
-						  action="append",
-						  help="Apply patch PATCH_MODULE.PATCH_FILE")
-		parser.add_option("-f", "--force",
-						  action="store_true", dest="force", default=False,
-						  help="Force Apply all patches specified using option -p or --patch")
-		parser.add_option("-d", "--db",
-						  dest="db_name",
-						  help="Apply the patches on given db")
-		parser.add_option('-r', '--reload_doc',help="reload doc, requires module, dt, dn", nargs=3)
-		(options, args) = parser.parse_args()
+	
+	# code replace
+	elif options.replace:
+		replace_code('.', options.replace[0], options.replace[1], options.replace[2])
+	
+	# git
+	elif options.status:
+		os.system('git status')
+		os.chdir('lib')
+		os.system('git status')
+	
+	elif options.pull:
+		os.system('git pull %s %s' % (options.pull[0], options.pull[1]))
+		os.chdir('lib')
+		os.system('git pull %s %s' % (options.pull[0], options.pull[1]))
 
-		from webnotes.db import Database
-		import webnotes.modules.patch_handler
-
+	elif options.push:
+		os.system('git commit -a -m "%s"' % options.push[2])
+		os.system('git push %s %s' % (options.push[0], options.push[1]))
+		os.chdir('lib')
+		os.system('git commit -a -m "%s"' % options.push[2])
+		os.system('git push %s %s' % (options.push[0], options.push[1]))
+	
+	
+	
+	# patch
+	elif options.patch:
 		# connect to db
 		if options.db_name is not None:
 			webnotes.connect(options.db_name)
 		else:
 			webnotes.connect()
-		
+	
 		# run individual patches
 		if options.patch_list:
 			for patch in options.patch_list:
 				webnotes.modules.patch_handler.run_single(\
 					patchmodule = patch, force = options.force)
-		
+	
 		# reload
 		elif options.reload_doc:
 			webnotes.modules.patch_handler.reload_doc(\
@@ -96,8 +133,7 @@ def run():
 		# run all pending
 		elif options.run_latest:
 			webnotes.modules.patch_handler.run_all()
-			
-		print '\n'.join(webnotes.modules.patch_handler.log_list)
+		
 
 if __name__=='__main__':
 	run()
