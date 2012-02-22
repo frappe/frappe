e8e30f0812b756ae7a1ada73ced1db6f6c311476
Rushabh Mehta <rmehta@gmail.com>
2012-02-22 17:30:12 +0530

    updated diff tool

--- py/webnotes/modules/diff.py
+++ py/webnotes/modules/diff.py
@@ -1,6 +1,8 @@
 """get diff bettween txt files and database records"""
 
 import webnotes
+import os, webnotes.defs
+from webnotes.model.utils import peval_doclist
 
 dt_map = {
 	'DocType': {
@@ -15,14 +17,13 @@ ignore_fields = ('modified', 'creation', 'owner', 'modified_by',
 missing = property_diff = 0
 property_count = {}
 
-def diff():
-	"""get diff"""
+
+def diff_ref_file():
+	"""get diff using .txt files as reference"""
 	global missing, property_diff, property_count
 	missing = property_diff = 0
 	property_count = {}
 
-	import os, webnotes.defs
-	from webnotes.model.utils import peval_doclist
 	
 	for wt in os.walk(webnotes.defs.modules_path):
 		for fname in wt[2]:
@@ -30,27 +31,72 @@ def diff():
 				path = os.path.join(wt[0], fname)
 				with open(path, 'r') as txtfile:
 					doclist_diff(peval_doclist(txtfile.read()))
+
+	print_stats()
 	
-	print
-	print "Missing records: " + str(missing)
-	print "Property mismatch: " + str(property_diff)
-	for key in property_count:
-		print "- " + key + ": " + str(property_count[key] or 0)
-	print
+def diff_ref_db():
+	"""get diff using database as reference"""
+	from webnotes.modules import scrub
+	for dt in dt_map:
+		# get all main docs
+		for doc in webnotes.conn.sql("""select * from `tab%s`""" % dt, as_dict=1):
+			# get file for this doc
+			doc['doctype'] = dt
+			path = os.path.join(webnotes.defs.modules_path, scrub(doc['module']), \
+				scrub(dt), scrub(doc['name']), scrub(doc['name']) + '.txt')
+				
+			if os.path.exists(path):
+				with open(path, 'r') as txtfile:
+					target = peval_doclist(txtfile.read())					
+			else:
+				target = [None,]
+			
+			doc_diff(doc, target[0])
+			
+			# do diff for child records
+			if target[0] and dt_map[dt].keys():
+				for child_dt in dt_map[dt]:					
+
+					# for each child type, we need to create
+					# a key (e.g. fieldname, label) based mapping of child records in 
+					# txt files
+					child_key_map = {}
+					keys = dt_map[dt][child_dt]
+					for target_d in target:
+						if target_d['doctype'] == child_dt:
+							for key in keys:
+								if target_d.get(key):
+									child_key_map[target_d.get(key)] = target_d
+									break
+
+					for d in webnotes.conn.sql("""select * from `tab%s` where 
+						parent=%s and docstatus<2""" % (child_dt, '%s'), doc['name'], as_dict=1):
+
+						source_key = None
+						d['doctype'] = child_dt
+						for key in keys:
+							if d.get(key):
+								source_key = d.get(key)
+								break
+						
+						# only if a key is found
+						if source_key:
+							doc_diff(d, child_key_map.get(source_key), source_key)
+				
+
+	print_stats()
+
 
 def doclist_diff(doclist):
 	# main doc
-	global missing
-
 	doc = doclist[0]
 	if doc['doctype'] in dt_map.keys():
 		# do for main
-		db_doc = webnotes.conn.sql("""select * from `tab%s` 
+		target = webnotes.conn.sql("""select * from `tab%s` 
 			where name=%s""" % (doc['doctype'], '%s'), doc['name'], as_dict=1)
-		doc_diff(doc, db_doc)
+		doc_diff(doc, target and target[0] or None)
 		
-		if not db_doc:
-			missing += 1
+		if not target:
 			# no parent, no children!
 			return
 			
@@ -70,36 +116,34 @@ def doclist_diff(doclist):
 				# incoming child record has a uniquely
 				# identifiable key
 				if child_key:
-					db_doc = webnotes.conn.sql("""select * from `tab%s`
+					target = webnotes.conn.sql("""select * from `tab%s`
 						where `%s`=%s and parent=%s""" % (d['doctype'], child_key, '%s', '%s'),
 						(child_key_value, d['parent']), as_dict=1)
 					
-					doc_diff(d, db_doc, child_key_value)
+					doc_diff(d, target and target[0] or None, child_key_value)
 	
 
-def doc_diff(doc, db_doc, child_key_value=None):
+def doc_diff(source, target, child_key_value=None):
 	from termcolor import colored
-	global property_diff, property_count
+	global property_diff, property_count, missing
 
-	if doc.get('parent'):
-		docname = doc.get('parent') + '.' + child_key_value
+	docname = source.get('parent') and (source.get('parent') + '.' + child_key_value) \
+		or source['name']
+
+	if not target:
+		missing += 1
+		print(colored((source['doctype'] + ' -> ' + docname), 'red') + ' missing')
+		
 	else:
-		docname = doc['name']
-	
-	if not db_doc:
-		print(colored((doc['doctype'] + ' -> ' + docname), 'red') + ' missing')
-	
-	else:
-		db_doc = db_doc[0]
-		db_doc['doctype'] = doc['doctype']
-		for key in doc:
+		target['doctype'] = source['doctype']
+		for key in source:
 			if (key not in ignore_fields) \
-				and (db_doc.get(key) != doc[key]) \
-				and (not (db_doc.get(key)==None and doc[key]==0)):
+				and (target.get(key) != source[key]) \
+				and (not (target.get(key)==None and source[key]==0)):
 				
-				prefix = doc['doctype']+' -> ' + docname + ' -> '+key+' = '
-				in_db = colored(str(db_doc.get(key)), 'red')
-				in_file = colored(str(doc[key]), 'green')
+				prefix = source['doctype']+' -> ' + docname + ' -> '+key+' = '
+				in_db = colored(str(target.get(key)), 'red')
+				in_file = colored(str(source[key]), 'green')
 				
 				print(prefix + in_db + ' | ' + in_file)
 				
@@ -107,3 +151,11 @@ def doc_diff(doc, db_doc, child_key_value=None):
 				if not key in property_count:
 					property_count[key] = 0
 				property_count[key] += 1
+				
+def print_stats():
+	print
+	print "Missing records: " + str(missing)
+	print "Property mismatch: " + str(property_diff)
+	for key in property_count:
+		print "- " + key + ": " + str(property_count[key] or 0)
+	print
