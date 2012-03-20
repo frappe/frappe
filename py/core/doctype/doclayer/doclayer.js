eac5aba594ea180e1582c109cac7c6c807c60006
Anand Doshi <anand@iwebnotes.com>
2012-03-20 17:49:46 +0530

    hide allow attach if not valid

--- py/core/doctype/doclayer/doclayer.js
+++ py/core/doctype/doclayer/doclayer.js
@@ -70,8 +70,29 @@ cur_frm.cscript.refresh = function(doc, dt, dn) {
 		page_head.buttons['Refresh Form'].disabled = true;
 		page_head.buttons['Reset to defaults'].disabled = true;
 	}
+
+	cur_frm.cscript.hide_allow_attach(doc, dt, dn);
 }
 
+cur_frm.cscript.hide_allow_attach = function(doc, dt, dn) {
+	var allow_attach_list = ['Website Settings', 'Web Page', 'Timesheet', 'Ticket',
+		'Support Ticket', 'Supplier', 'Style Settings', 'Stock Reconciliation',
+		'Stock Entry', 'Serial No', 'Sales Order', 'Receivable Voucher',
+		'Quotation', 'Question', 'Purchase Receipt', 'Purchase Order',
+		'Project', 'Profile', 'Production Order', 'Product', 'Print Format',
+		'Price List', 'Payable Voucher', 'Page', 'Module Def',
+		'Maintenance Visit', 'Maintenance Schedule', 'Letter Head',
+		'Leave Application', 'Lead', 'Journal Voucher', 'Item', 'Indent',
+		'Expense Voucher', 'Enquiry', 'Employee', 'Delivery Note',
+		'Customer Issue', 'Customer', 'Contact Us Settings', 'Company',
+		'Bulk Rename Tool', 'Blog', 'Bill Of Materials', 'About Us Settings'];
+	
+	if(inList(allow_attach_list, doc.doc_type)) {
+		unhide_field('allow_attach');
+	} else {
+		hide_field('allow_attach');
+	}
+}
 
 cur_frm.confirm = function(msg, doc, dt, dn) {
 	var d = new wn.widgets.Dialog({
