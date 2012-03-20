4d433ccce70e4ccd3a49fd1df68f12743ecbef96
Rushabh Mehta <rmehta@gmail.com>
2012-03-20 14:44:53 +0530

    cleanup of form, rename, route, added fonts

--- js/legacy/widgets/form/form.js
+++ js/legacy/widgets/form/form.js
@@ -274,6 +274,7 @@ _f.Frm.prototype.set_heading = function() {
 _f.Frm.prototype.setup_meta = function() {
 	this.meta = get_local('DocType',this.doctype);
 	this.perm = get_perm(this.doctype); // for create
+	if(this.meta.istable) { this.meta.in_dialog = 1 }
 	this.setup_print();
 }
 
