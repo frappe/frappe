294b47b36612e48e2d20e586b0e28007314c631e
Anand Doshi <anand@iwebnotes.com>
2012-04-13 15:19:10 +0530

    doctype.js cleanup

--- py/core/doctype/doctype/doctype.js
+++ py/core/doctype/doctype/doctype.js
@@ -23,39 +23,6 @@
 // -------------
 // Menu Display
 // -------------
-
-cur_frm.cscript.issingle = function(doc, cdt, cdn) {
-	if(doc.issingle)
-		unhide_field('show_in_menu');
-	else {
-		doc.show_in_menu = 0;
-		hide_field('show_in_menu');
-		hide_field('parent_node');
-		hide_field('Parent HTML');
-		hide_field('Menu HTML');
-		hide_field('menu_index');
-		hide_field('smallicon');
-	}
-}
-
-cur_frm.cscript.show_in_menu = function(doc, cdt, cdn) {
-	if(doc.show_in_menu) {
-		unhide_field('parent_node');
-		unhide_field('Parent HTML');
-		unhide_field('Menu HTML');
-		unhide_field('menu_index');
-		unhide_field('smallicon');
-	} else {
-		hide_field('parent_node');
-		hide_field('Parent HTML');
-		hide_field('Menu HTML');
-		hide_field('menu_index');
-		hide_field('smallicon');
-	} 
-}
-
-// Attachment
-
 cur_frm.cscript.allow_attach = function(doc, cdt, cdn) {
 	if(doc.allow_attach) {
 		unhide_field('max_attachments');
@@ -65,9 +32,7 @@ cur_frm.cscript.allow_attach = function(doc, cdt, cdn) {
 }
 
 cur_frm.cscript.onload = function(doc, cdt, cdn) {
-	this.issingle(doc, cdt, cdn);
 	this.allow_attach(doc, cdt, cdn);
-	this.show_in_menu(doc, cdt, cdn);
 }
 
 cur_frm.cscript.refresh = function(doc, cdt, cdn) {
