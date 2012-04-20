17b32d4b60b9f5b7d52b292baaa9064f86c8e034
Anand Doshi <anand@iwebnotes.com>
2012-04-20 11:22:25 +0530

    fixes in currency format, breadcrumbs

--- js/wn/views/breadcrumbs.js
+++ js/wn/views/breadcrumbs.js
@@ -3,9 +3,9 @@ wn.views.breadcrumbs = function(parent, module, doctype, name) {
 	$(parent).empty();
 	var $bspan = $(repl('<span class="breadcrumbs">\
 		<a href="#%(home_page)s">Home</a></span>', {home_page: wn.boot.home_page}));
-	if(module) {
+	if(module && wn.modules[module]) {
 		$bspan.append(repl(' / <a href="#!%(module_page)s">%(module)s Home</a>',
-			{module: module, module_page: erpnext.modules[module] }))
+			{module: module, module_page: wn.modules[module] }))
 	}
 	if(doctype && (locals.DocType[doctype] && !locals.DocType[doctype].issingle)) {
 		$bspan.append(repl(' / <a href="#!List/%(doctype)s">%(doctype)s</a>',
