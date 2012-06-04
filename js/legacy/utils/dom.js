0631708cd04df3163b6d22385d35125e0cd8e748
Anand Doshi <anand@iwebnotes.com>
2012-06-04 13:06:31 +0530

    fix in get_base_url of dom.js

--- js/legacy/utils/dom.js
+++ js/legacy/utils/dom.js
@@ -374,7 +374,7 @@ wn.urllib = {
 	
 	// returns the base url with http + domain + path (-index.cgi or # or ?)
 	get_base_url: function() {
-		var url= window.location.href.split('#')[0].split('?')[0].split('index.cgi')[0];
+		var url= window.location.href.split('#')[0].split('?')[0].split('app.html')[0];
 		if(url.substr(url.length-1, 1)=='/') url = url.substr(0, url.length-1)
 		return url
 	},
