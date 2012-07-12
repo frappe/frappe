5de5b6efc5683713db7f5b9734d9aaf5872facee
Rushabh Mehta <rmehta@gmail.com>
2012-07-12 18:00:37 +0530

    sales bom fixes and mandatory in table fields

--- js/legacy/widgets/form/form.js
+++ js/legacy/widgets/form/form.js
@@ -848,19 +848,8 @@ _f.Frm.prototype.save = function(save_action, call_back) {
  	
 	
 	var ret_fn = function(r) {
-		me.savingflag = false;
-		if(user=='Guest' && !r.exc) {
-			// if user is guest, show a message after succesful saving
-			$dh(me.page_layout.wrapper);
-			$ds(me.saved_wrapper);
-			me.saved_wrapper.innerHTML = 
-				'<div style="padding: 150px 16px; text-align: center; font-size: 14px;">' 
-				+ (cur_frm.message_after_save ? cur_frm.message_after_save : 'Your information has been sent. Thank you!') 
-				+ '</div>';
-			return; // no refresh
-		}
-		
-		if(!me.meta.istable) {
+		me.savingflag = false;		
+		if(!me.meta.istable && r) {
 			me.refresh(r.docname);
 		}
 
