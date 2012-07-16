aac53b37ec44154e2a9e97f213bf1479d6f4ae12
Rushabh Mehta <rmehta@gmail.com>
2012-07-16 16:27:42 +0530

    report breadcrumbs issue

--- js/wn/views/reportview.js
+++ js/wn/views/reportview.js
@@ -101,8 +101,7 @@ wn.views.ReportViewPage = Class.extend({
 	},
 	make_report_view: function() {
 		// add breadcrumbs
-		wn.views.breadcrumbs($('<span>').appendTo(this.page.appframe.$titlebar), 
-			locals.DocType[this.doctype].module);
+		this.page.appframe.add_breadcrumb(locals.DocType[this.doctype].module);
 			
 		this.reportview = new wn.views.ReportView(this.doctype, this.docname, this.page)
 	}
