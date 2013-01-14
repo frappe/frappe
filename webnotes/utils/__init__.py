a5cd4727c481f59db6be53ef83bb44ec9c29e484
Anand Doshi <anand@erpnext.com>
2013-01-14 11:17:19 +0530

    get base path of the folder where erpnext is installed

--- webnotes/utils/__init__.py
+++ webnotes/utils/__init__.py
@@ -760,3 +760,8 @@ def comma_sep(some_list, sep):
 			return ", ".join(some_list[:-1]) + sep + some_list[-1]
 	else:
 		return some_list
+		
+def get_base_path():
+	import conf
+	import os
+	return os.path.dirname(os.path.abspath(conf.__file__))
