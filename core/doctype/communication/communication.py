ffcbfebfd6c079376ab08ae67d755b34b5182fca
Rushabh Mehta <rmehta@gmail.com>
2013-01-16 12:19:27 +0530

    added automatic lead creation for sales email id

--- core/doctype/communication/communication.py
+++ core/doctype/communication/communication.py
@@ -75,6 +75,9 @@ def send_comm_email(d, name, sent_via=None, print_html=None, attachments='[]', s
 		if hasattr(sent_via, "get_content"):
 			d.content = sent_via.get_content(d)
 
+	if not d.sender:
+		d.sender = webnotes.session.user
+
 	from webnotes.utils.email_lib.smtp import get_email
 	mail = get_email(d.recipients, sender=d.sender, subject=d.subject, 
 		msg=d.content)
