44c013c30ad8b92ab37ba9bf821ca502c6b858ed
Rushabh Mehta <rmehta@gmail.com>
2013-04-22 11:22:11 +0530

    [slickgrid] [upgrade] added copy-paste to excel thanks to http://labs.nereo.com/SlickGrid/plugins/slick.cellexternalcopymanager.js

--- public/js/lib/slickgrid/slick.editors.js
+++ public/js/lib/slickgrid/slick.editors.js
@@ -302,7 +302,7 @@
     };
 
     this.loadValue = function (item) {
-      defaultValue = item[args.column.field];
+      defaultValue = !!item[args.column.field];
       if (defaultValue) {
         $select.attr("checked", "checked");
       } else {
@@ -311,7 +311,7 @@
     };
 
     this.serializeValue = function () {
-      return $select.attr("checked");
+      return !!$select.attr("checked");
     };
 
     this.applyValue = function (item, state) {
@@ -319,7 +319,7 @@
     };
 
     this.isValueChanged = function () {
-      return ($select.attr("checked") != defaultValue);
+      return (this.serializeValue() !== defaultValue);
     };
 
     this.validate = function () {
