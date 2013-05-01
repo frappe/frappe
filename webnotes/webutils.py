5844c3dc4c0457c28f7a588c6ac73e8bb34f49a8
Anand Doshi <anand@erpnext.com>
2013-05-01 12:33:50 +0530

    [website] [fix] no cache pages

--- webnotes/webutils.py
+++ webnotes/webutils.py
@@ -28,7 +28,7 @@ def get_html(page_name):
 	
 	# load from cache, if auto cache clear is falsy
 	if not (hasattr(conf, 'auto_cache_clear') and conf.auto_cache_clear or 0):
-		if not get_page_settings().get("page_name", {}).get("no_cache", None):
+		if not get_page_settings().get(page_name, {}).get("no_cache"):
 			html = webnotes.cache().get_value("page:" + page_name)
 			from_cache = True
 
