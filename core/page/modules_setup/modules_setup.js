8fcb4819e78088a19a08354bf964afc117d9b319
Anand Doshi <anand@erpnext.com>
2013-05-09 23:16:36 +0530

    [modules setup] [fix] check modules when not in hidden modules or if hidden modules list does not exist

--- core/page/modules_setup/modules_setup.js
+++ core/page/modules_setup/modules_setup.js
@@ -24,7 +24,7 @@ wn.modules_setup = {
 			if(m!="Setup") {
 				var $chk = $("<input type='checkbox' data-module='"+m+"' style='margin-top: -2px'>")
 					.prependTo($('<p><span> '+m+'</span></p>').appendTo("#modules-list"));
-				if(wn.boot.hidden_modules.indexOf(m)==-1) {
+				if(!wn.boot.hidden_modules || wn.boot.hidden_modules.indexOf(m)==-1) {
 					$chk.attr("checked", true);
 				}
 			}
