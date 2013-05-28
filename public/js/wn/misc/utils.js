25fedea7da3da50cb29f946e36441e4517da435c
Anand Doshi <anand@erpnext.com>
2013-05-28 17:22:56 +0530

    [wn.utils.sum] [fix]

--- public/js/wn/misc/utils.js
+++ public/js/wn/misc/utils.js
@@ -170,6 +170,6 @@ wn.utils = {
 	},
 	
 	sum: function(list) {
-		return list.reduce(function(previous_value, current_value) { return flt(previous_value) + flt(current_value); });
+		return list.reduce(function(previous_value, current_value) { return flt(previous_value) + flt(current_value); }, 0.0);
 	},
 };
