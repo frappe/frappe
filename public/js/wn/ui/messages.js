1256b07c5e301c4b2b8d868d29d3a9e104c4d7cd
Anand Doshi <anand@erpnext.com>
2013-06-13 16:11:03 +0530

    [progress bar] [fix]

--- public/js/wn/ui/messages.js
+++ public/js/wn/ui/messages.js
@@ -5,7 +5,7 @@ wn.messages.waiting = function(parent, msg, bar_percent) {
 	return $(repl('<div class="well" style="width: 63%; margin: 30px auto;">\
 		<p style="text-align: center;">%(msg)s</p>\
 		<div class="progress progress-striped active">\
-			<div class="bar" style="width: %(bar_percent)s%"></div></div>', {
+			<div class="progress-bar progress-bar-info" style="width: %(bar_percent)s%"></div></div>', {
 				bar_percent: bar_percent,
 				msg: msg
 			}))
