01ea6d4a6fac5661e5749753f2aaf96e8ed6659e
Maxwell Morais <max.morais.dmm@gmail.com>
2013-06-13 09:31:09 -0300

    Addition of Tradution for newest doctypes NCM and Join Financial are the newest doctypes

---
+++ public/web.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# ERPNext - web based ERP (http://erpnext.com)
+# Copyright (C) 2012 Web Notes Technologies Pvt Ltd
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+	return a dynamic page from website templates
+
+	all html pages related to website are generated here
+"""
+from __future__ import unicode_literals
+import cgi, cgitb, os, sys
+cgitb.enable()
+
+# import libs
+
+sys.path.append('..')
+sys.path.append('../lib')
+sys.path.append('../app')
+
+import conf
+
+def init():
+	import webnotes.handler
+	webnotes.handler.get_cgi_fields()
+
+def respond():
+	import webnotes
+	import webnotes.webutils
+	return webnotes.webutils.render(webnotes.form_dict.get('page'))
+
+if __name__=="__main__":
+	init()
+	respond()
