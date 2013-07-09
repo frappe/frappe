dca4e59e776e43c87da789fff3989cb8ff596be4
Akhilesh Darjee <akhileshdarjee@gmail.com>
2013-07-09 11:55:25 +0530

    [Report]Added permission to reports for showing data according to the user properties

--- webnotes/widgets/reportview.py
+++ webnotes/widgets/reportview.py
@@ -123,7 +123,7 @@ def load_doctypes():
 	import webnotes.model.doctype
 
 	roles = webnotes.get_roles()
-		
+	
 	for t in tables:
 		if t.startswith('`'):
 			doctype = t[4:-1]
@@ -190,8 +190,9 @@ def build_filter_conditions(filters, conditions):
 				conditions.append('ifnull(' + tname + '.' + f[1] + ",0) " + f[2] \
 					+ " " + cstr(f[3]))
 					
-def build_match_conditions(doctype, fields=None):
+def build_match_conditions(doctype, fields=None, as_condition=True, match_filters={}):
 	"""add match conditions if applicable"""
+	global roles
 	match_conditions = []
 	match = True
 	
@@ -199,7 +200,10 @@ def build_match_conditions(doctype, fields=None):
 		global tables
 		tables = get_tables(doctype, fields)
 		load_doctypes()
-	
+
+	if not roles:
+		roles = webnotes.get_roles()
+
 	for d in doctypes[doctype]:
 		if d.doctype == 'DocPerm' and d.parent == doctype:
 			if d.role in roles:
@@ -208,20 +212,30 @@ def build_match_conditions(doctype, fields=None):
 						document_key, default_key = d.match.split(":")
 					else:
 						default_key = document_key = d.match
-				
-					for v in webnotes.defaults.get_user_default_as_list(default_key) or ["** No Match **"]:
-						match_conditions.append('`tab%s`.%s="%s"' % (doctype,
-							document_key, v))
+					for v in webnotes.defaults.get_user_default_as_list(default_key, \
+						webnotes.session.user) or ["** No Match **"]:
+						if as_condition:
+							match_conditions.append('`tab%s`.%s="%s"' % (doctype,
+								document_key, v))
+						else:
+							if v:
+								match_filters.setdefault(document_key, [])
+								if v not in match_filters[document_key]:
+									match_filters[document_key].append(v)
 							
 				elif d.read == 1 and d.permlevel == 0:
 					# don't restrict if another read permission at level 0 
 					# exists without a match restriction
 					match = False
 		
-	if match_conditions and match:
-		return '('+ ' or '.join(match_conditions) +')'
+	if as_condition:
+		if match_conditions and match:
+			return '('+ ' or '.join(match_conditions) +')'
+		else:
+			return ""
 	else:
-		return ""
+		return match_filters
+
 
 def get_tables(doctype, fields):
 	"""extract tables from fields"""
