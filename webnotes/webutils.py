60fa2b3f3bed48d69833b3679047c1ae71b3ad25
Rushabh Mehta <rmehta@gmail.com>
2013-09-10 14:39:37 +0530

    [minor] webutils moved to lib

--- webnotes/webutils.py
+++ webnotes/webutils.py
@@ -94,7 +94,7 @@ def build_page(page_name):
 		if module:
 			context.update(module.get_context())
 	
-	context = update_context(context)
+	context.update(get_website_settings())
 	jenv = Environment(loader = FileSystemLoader(basepath))
 	context["base_template"] = jenv.get_template(webnotes.get_config().get("base_template"))
 	
@@ -131,15 +131,6 @@ def get_home_page():
 		page_name = 'login'
 
 	return page_name
-
-def update_context(context):
-	try:
-		from startup.webutils import update_template_args
-		context = update_template_args(page_name, context)
-	except ImportError:
-		pass
-
-	return context
 		
 def build_website_sitemap_config():
 	import os, json
@@ -174,6 +165,54 @@ def build_website_sitemap_config():
 					config["generators"][fname] = options
 		
 	return config
+
+def get_website_settings():
+	from webnotes.utils import get_request_site_address
+	from urllib import quote
+		
+	all_top_items = webnotes.conn.sql("""\
+		select * from `tabTop Bar Item`
+		where parent='Website Settings' and parentfield='top_bar_items'
+		order by idx asc""", as_dict=1)
+	
+	top_items = [d for d in all_top_items if not d['parent_label']]
+	
+	# attach child items to top bar
+	for d in all_top_items:
+		if d['parent_label']:
+			for t in top_items:
+				if t['label']==d['parent_label']:
+					if not 'child_items' in t:
+						t['child_items'] = []
+					t['child_items'].append(d)
+					break
+			
+	context = webnotes._dict({
+		'top_bar_items': top_items,
+		'footer_items': webnotes.conn.sql("""\
+			select * from `tabTop Bar Item`
+			where parent='Website Settings' and parentfield='footer_items'
+			order by idx asc""", as_dict=1),
+		"webnotes": webnotes,
+		"utils": webnotes.utils
+	})
+		
+	settings = webnotes.doc("Website Settings", "Website Settings")
+	for k in ["banner_html", "brand_html", "copyright", "address", "twitter_share_via",
+		"favicon", "facebook_share", "google_plus_one", "twitter_share", "linked_in_share",
+		"disable_signup"]:
+		if k in settings.fields:
+			context[k] = settings.fields.get(k)
+
+	for k in ["facebook_share", "google_plus_one", "twitter_share", "linked_in_share",
+		"disable_signup"]:
+		context[k] = cint(context.get(k) or 0)
+	
+	context.url = quote(str(get_request_site_address(full_address=True)), str(""))
+	context.encoded_title = quote(encode(context.title or ""), str(""))
+	context.shopping_cart_enabled = cint(webnotes.conn.get_default("shopping_cart_enabled"))
+	
+	return context
 
 
 
