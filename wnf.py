72e44747a1e3e01a4a72d74a22d799ff6ff39873
Pratik Vyas <m@pd.io>
2013-09-18 00:38:26 +0530

    append to sys.path on the front

--- wnf.py
+++ wnf.py
@@ -296,9 +296,9 @@ def setup_options():
 	return parser.parse_args()
 	
 def run():
-	sys.path.append('.')
-	sys.path.append('lib')
-	sys.path.append('app')
+	sys.path.insert(0, '.')
+	sys.path.insert(0, 'lib')
+	sys.path.insert(0, 'app')
 
 	(options, args) = setup_options()
 	
