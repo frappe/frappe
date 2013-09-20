fabf6798d09631687b626989c23350c6f0a790bb
Anand Doshi <anand@erpnext.com>
2013-09-20 11:44:20 +0530

    [fix] [minor] [file manager] pass utf-8 encoded strings to os.remove and os.path.exists

--- webnotes/utils/file_manager.py
+++ webnotes/utils/file_manager.py
@@ -73,7 +73,7 @@ def save_file(fname, content, dt, dn):
 	file_size = check_max_file_size(content)
 	temp_fname = write_file(content, files_path)
 	fname = scrub_file_name(fname)
-	fpath = os.path.join(files_path, fname)
+	fpath = os.path.join(files_path, fname).encode("utf-8")
 
 	fname_parts = fname.split(".", -1)
 	main = ".".join(fname_parts[:-1])
@@ -95,14 +95,14 @@ def save_file(fname, content, dt, dn):
 			fname = get_new_fname_based_on_version(files_path, main, extn, versions)
 			
 			# rename
-			os.rename(temp_fname, os.path.join(files_path, fname))
+			os.rename(temp_fname, fpath)
 	else:
 		# rename new file
-		os.rename(temp_fname, os.path.join(files_path, fname))
+		os.rename(temp_fname, fpath)
 
 	f = webnotes.bean({
 		"doctype": "File Data",
-		"file_name": os.path.relpath(os.path.join(files_path, fname), get_path("public")),
+		"file_name": os.path.relpath(fpath, get_path("public")),
 		"attached_to_doctype": dt,
 		"attached_to_name": dn,
 		"file_size": file_size
@@ -123,7 +123,7 @@ def get_new_fname_based_on_version(files_path, main, extn, versions):
 		version = 1
 	
 	new_fname = main + "-" + str(version) + "." + extn
-	while os.path.exists(os.path.join(files_path, new_fname)):
+	while os.path.exists(os.path.join(files_path, new_fname).encode("utf-8")):
 		version += 1
 		new_fname = main + "-" + str(version) + "." + extn
 		if version > 100:
