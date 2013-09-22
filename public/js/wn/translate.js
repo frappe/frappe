bc2c85b4008ee1d35410587efbcfca848a3f2bbf
Dennis Ploeger <github.dennis@dieploegers.de>
2013-09-22 08:28:39 +0200

    Enabling use of dict-translation %(key)s

--- public/js/wn/translate.js
+++ public/js/wn/translate.js
@@ -8,7 +8,8 @@ wn._ = function(txt, replace) {
 	if(typeof(txt) != "string") return txt;
 	preformat = wn._messages[txt.replace(/\n/g, "")] || txt;
     if(!replace) return preformat;
-    return vsprintf(preformat, replace);
+    if(replace instanceof Array) return vsprintf(preformat, replace);
+    return repl(preformat, replace);
 };
 wn.translate = function(obj, keys) {
 	$.each(keys, function(i, key) {
