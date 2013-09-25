4b65c9d6d6d249fa32411ab3d63c46177b50b7c3
Pratik Vyas <m@pd.io>
2013-09-25 12:50:02 +0530

    fix conf to override correctly

--- webnotes/__init__.py
+++ webnotes/__init__.py
@@ -536,15 +536,15 @@ def get_conf(site):
 	# TODO Should be heavily cached!
 	import conf
 	from webnotes.utils import get_site_base_path
-	conf = _dict(conf.__dict__)
+	site_config = _dict({})
+	conf = site_config.update(conf.__dict__)
 	if conf.sites_dir and site:
 		conf_path = os.path.join(get_site_base_path(sites_dir=conf.sites_dir, hostname=site), 'site_config.json')
 		if os.path.exists(conf_path):
 			with open(conf_path, 'r') as f:
-				site_config = json.load(f)
-			site_config.update(conf)
+				site_config.update(json.load(f))
 			site_config['site'] = site
-			return _dict(site_config)
+			return site_config
 
 		else:
 			raise NotFound()
