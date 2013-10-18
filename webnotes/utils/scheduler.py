5b6e37c8b32d95ebac7348849a36380d24f4b528
Anand Doshi <anand@erpnext.com>
2013-10-18 17:45:38 +0530

    [minor] [fix] Control Panel scheduler events

--- webnotes/utils/scheduler.py
+++ webnotes/utils/scheduler.py
@@ -63,6 +63,7 @@ def execute(site=None):
 	
 def trigger(method):
 	"""trigger method in startup.schedule_handler"""
+	traceback = ""
 	try:
 		import startup.schedule_handlers
 		
@@ -70,16 +71,16 @@ def trigger(method):
 			webnotes.conn.begin()
 			getattr(startup.schedule_handlers, method)()
 			webnotes.conn.commit()
-			return 'ok'
-		
 	except Exception:
-		return log(method)
+		traceback += log(method)
 		
 	try:
 		cp = webnotes.bean("Control Panel", "Control Panel")
 		cp.run_method(method)
 	except Exception:
-		return log(method)
+		traceback += log("Control Panel: "+method)
+		
+	return traceback or 'ok'
 
 def log(method):
 	"""log error in patch_log"""
