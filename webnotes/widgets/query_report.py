4499a0c33402cb5c1d98f0916003c7477e35b4d2
Syed Qadeer <syed@businessclouds.biz>
2013-10-23 10:37:56 +0300

    [Fix] [Minor] Updated to return response

--- webnotes/widgets/query_report.py
+++ webnotes/widgets/query_report.py
@@ -21,14 +21,15 @@ def get_script(report_name):
 		"report", scrub(report.name), scrub(report.name) + ".js") 
 	locale_path = os.path.join(get_module_path(webnotes.conn.get_value(doctype)),
 		"report", scrub(report.name))
+	# load translations
+	if webnotes.lang != "en":
+		from webnotes.translate import get_lang_data
+		messages = get_lang_data(locale_path, 
+			webnotes.lang, 'js')
+		webnotes.response["__messages"] = messages
+		
 	if os.path.exists(script_path):
 		with open(script_path, "r") as script:
-			# load translations
-			if webnotes.lang != "en":
-				from webnotes.translate import get_lang_data
-				messages = get_lang_data(locale_path, 
-					webnotes.lang, 'js')
-				webnotes.response["__messages"] = messages
 			return script.read()
 	elif report.javascript:
 		return report.javascript
