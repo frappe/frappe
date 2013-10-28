b83386822f7f0e97c46b8922e4ad6fcabe53244a
Anand Doshi <anand@erpnext.com>
2013-10-28 19:55:43 +0530

    [minor] fix in test cases

--- webnotes/tests/test_db.py
+++ webnotes/tests/test_db.py
@@ -8,18 +8,24 @@ from webnotes.test_runner import make_test_records
 
 class TestDB(unittest.TestCase):
 	def test_get_value(self):
+		from webnotes.utils import now_datetime
+		import time
 		webnotes.conn.sql("""delete from `tabProfile` where name not in ('Administrator', 'Guest')""")
-		del webnotes.test_objects["Profile"]
-		make_test_records("Profile")
+		
+		now = now_datetime()
 		
 		self.assertEquals(webnotes.conn.get_value("Profile", {"name": ["=", "Administrator"]}), "Administrator")
 		self.assertEquals(webnotes.conn.get_value("Profile", {"name": ["like", "Admin%"]}), "Administrator")
 		self.assertEquals(webnotes.conn.get_value("Profile", {"name": ["!=", "Guest"]}), "Administrator")
+		self.assertEquals(webnotes.conn.get_value("Profile", {"modified": ["<", now]}), "Administrator")
+		self.assertEquals(webnotes.conn.get_value("Profile", {"modified": ["<=", now]}), "Administrator")
+
+		time.sleep(2)
+		if "Profile" in webnotes.test_objects:
+			del webnotes.test_objects["Profile"]
+		make_test_records("Profile")
 		
-		from webnotes.utils import nowdate
-		self.assertEquals(webnotes.conn.get_value("Profile", {"modified": ["<", nowdate()]}), "Administrator")
-		self.assertEquals(webnotes.conn.get_value("Profile", {"modified": ["<=", nowdate()]}), "Administrator")
-		self.assertEquals(webnotes.conn.get_value("Profile", {"modified": [">", nowdate()]}), "test1@example.com")
-		self.assertEquals(webnotes.conn.get_value("Profile", {"modified": [">=", nowdate()]}), "test1@example.com")
+		self.assertEquals("test1@example.com", webnotes.conn.get_value("Profile", {"modified": [">", now]}))
+		self.assertEquals("test1@example.com", webnotes.conn.get_value("Profile", {"modified": [">=", now]}))
 		
 		
