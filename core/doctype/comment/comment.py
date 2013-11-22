cee4cb88a3ecb1f3f4409c597f56e6ba01846d3a
Rushabh Mehta <rmehta@gmail.com>
2013-11-22 12:22:41 +0530

    [comment] [minor] added patch to update

--- core/doctype/comment/comment.py
+++ core/doctype/comment/comment.py
@@ -23,21 +23,29 @@ class DocType:
 			pass
 			
 	def update_comment_in_doc(self):
-		try:
-			_comments = self.get_comments_from_parent()
-			_comments.append({
-				"comment": self.doc.comment, 
-				"by": self.doc.comment_by or self.doc.owner, 
-				"name":self.doc.name
-			})
-			self.update_comments_in_parent(_comments)
-		except Exception, e:
-			if e.args[0]==1054:
-				from webnotes.model.db_schema import add_column
-				add_column(self.doc.comment_doctype, "_comments", "Text")
-				self.update_comment_in_doc()
-			else:
-				raise
+		if self.doc.comment_doctype and self.doc.comment_docname and self.doc.comment:
+			try:
+				_comments = self.get_comments_from_parent()
+				updated = False
+				for c in _comments:
+					if c.get("name")==self.doc.name:
+						c["comment"] = self.doc.comment
+						updated = True
+						
+				if not updated:
+					_comments.append({
+						"comment": self.doc.comment, 
+						"by": self.doc.comment_by or self.doc.owner, 
+						"name":self.doc.name
+					})
+				self.update_comments_in_parent(_comments)
+			except Exception, e:
+				if e.args[0]==1054:
+					from webnotes.model.db_schema import add_column
+					add_column(self.doc.comment_doctype, "_comments", "Text")
+					self.update_comment_in_doc()
+				else:
+					raise
 	
 	def get_comments_from_parent(self):
 		_comments = webnotes.conn.get_value(self.doc.comment_doctype, 
