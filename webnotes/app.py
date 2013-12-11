183865950cce697c8f610a5d2d6352e3a17fe6d3
Rushabh Mehta <rmehta@gmail.com>
2013-12-11 11:32:43 +0530

    added setup.py

--- webnotes/app.py
+++ webnotes/app.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt 
+
 import sys, os
 import json
 
---
+++ webnotes/app_manager.py
@@ -0,0 +1,2 @@
+# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt	
