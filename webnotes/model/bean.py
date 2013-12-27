19d375a20b27cbcabf1046a255e50f3bc90d8445
Nabin Hait <nabinhait@gmail.com>
2013-12-27 17:38:16 +0530

    Call validate form update_after_submit

--- webnotes/model/bean.py
+++ webnotes/model/bean.py
@@ -344,6 +344,7 @@ class Bean:
 		if self.ignore_permissions or webnotes.has_permission(self.doc.doctype, "write", self.doc):
 			self.to_docstatus = 1
 			self.prepare_for_save("update_after_submit")
+			self.run_method('validate')
 			self.run_method('before_update_after_submit')
 			self.save_main()
 			self.save_children()
