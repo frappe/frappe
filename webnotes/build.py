31012e964a0bb011a2ef8097adf595c376a53982
Anand Doshi <anand@erpnext.com>
2014-01-28 12:51:30 +0530

    Don't walk in files and backups folder for translate and pyc removal

--- webnotes/build.py
+++ webnotes/build.py
@@ -55,6 +55,9 @@ def clear_pyc_files():
 	from webnotes.utils import get_base_path
 	for path, folders, files in os.walk(get_base_path()):
 		if 'locale' in folders: folders.remove('locale')
+		if 'files' in folders: folders.remove('files')
+		if 'backups' in folders: folders.remove('backups')
+		
 		for f in files:
 			if f.decode("utf-8").endswith(".pyc"):
 				os.remove(os.path.join(path, f))
