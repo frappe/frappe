09ef9a5788f4ad3a171514cbb7f4fd679566885b
Rushabh Mehta <rmehta@gmail.com>
2014-02-19 15:21:04 +0530

    cleanup of login page

--- frappe/website/page/sitemap_browser/sitemap_browser.py
+++ frappe/website/page/sitemap_browser/sitemap_browser.py
@@ -16,7 +16,7 @@ def get_children(parent=None):
 	return frappe.conn.sql("""select name as value, 1 as expandable, ref_doctype, docname 
 		from `tabWebsite Sitemap` where 
 		ifnull(parent_website_sitemap, '')=%s 
-			and idx is not null order by -idx desc""", parent, as_dict=True)
+			order by ifnull(idx,0), name asc""", parent, as_dict=True)
 		
 @frappe.whitelist()
 def move(name, up_or_down):
