f923a77524d8ec1ce91a3416acd2eb39ad5031ad
Pratik Vyas <m@pd.io>
2014-02-19 23:18:13 +0530

    add locking to scheduler

--- frappe/tasks.py
+++ frappe/tasks.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from frappe.celery import app
 import frappe
 from frappe.cli import get_sites
+from frappe.utils.file_lock import delete_lock
 
 @app.task()
 def scheduler_task(site, event, handler):
@@ -11,10 +12,12 @@ def scheduler_task(site, event, handler):
 		frappe.init(site=site)
 		frappe.get_attr(handler)()
 		frappe.conn.commit()
+		delete_lock(handler)
 	except Exception:
 		traceback += log("Method: {event}, Handler: {handler}".format(event=event, handler=handler))
 		traceback += log(frappe.get_traceback())
 		frappe.conn.rollback()
+	frappe.destroy()
 	# TODO: use logger
 	print 'ran {handler} for {site} for event: {event}'.format(handler=handler, site=site, event=event)
 
