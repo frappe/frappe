3cb694dc0bd509665920d04726e607a3c5234556
Rushabh Mehta <rmehta@gmail.com>
2014-02-20 18:18:06 +0530

    fixed patch

--- frappe/patches/4_0/set_website_sitemap_idx.py
+++ frappe/patches/4_0/set_website_sitemap_idx.py
@@ -1,6 +1,7 @@
 import frappe
 
 def execute():
+	frappe.reload_doc("website", "doctype", "blog_post")
 	frappe.conn.sql("""update `tabWebsite Sitemap` set idx=null""")
 	for doctype in ["Blog Category", "Blog Post", "Web Page", "Website Group"]:
 		frappe.conn.sql("""update `tab{}` set idx=null""".format(doctype))
