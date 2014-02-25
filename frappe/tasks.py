950a621cabf7a9ee73388ca38d9777fb4585e5af
Pratik Vyas <m@pd.io>
2014-02-25 17:44:44 +0530

    celery

--- frappe/tasks.py
+++ frappe/tasks.py
@@ -1,8 +1,18 @@
 from __future__ import unicode_literals
-from frappe.celery import app
+from frappe.celery_app import app
 import frappe
 from frappe.cli import get_sites
 from frappe.utils.file_lock import delete_lock
+
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
+
+@app.task()
+def sync_queues():
+	sites = get_sites()
+	for site in sites:
+		app.control.broadcast('add_consumer', arguments={'queue': site}, reply=True)
 
 @app.task()
 def scheduler_task(site, event, handler):
@@ -10,20 +20,22 @@ def scheduler_task(site, event, handler):
 	traceback = ""
 	try:
 		frappe.init(site=site)
+		frappe.connect()
 		frappe.get_attr(handler)()
 		frappe.conn.commit()
 		delete_lock(handler)
 	except Exception:
 		traceback += log("Method: {event}, Handler: {handler}".format(event=event, handler=handler))
 		traceback += log(frappe.get_traceback())
+		logger.warn(traceback)
 		frappe.conn.rollback()
 	frappe.destroy()
-	# TODO: use logger
-	print 'ran {handler} for {site} for event: {event}'.format(handler=handler, site=site, event=event)
+	logger.info('ran {handler} for {site} for event: {event}'.format(handler=handler, site=site, event=event))
 
 @app.task()
 def enqueue_scheduler_events():
 	import frappe
 	from frappe.utils.scheduler import execute
+	logger.info('ran {handler} for {site} for event: {event}')
 	for site in get_sites():
 		execute(site)
