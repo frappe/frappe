4e548b61060aff952045e93374d024c97440978e
Pratik Vyas <m@pd.io>
2014-03-04 00:27:07 +0530

    add filemanager patch

---
+++ frappe/patches/4_0/file_manager_hooks.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+
+import frappe
+import os
+from frappe.utils import get_files_path
+
+def execute():
+	for name, file_name, file_url in frappe.db.sql(
+			"""select name, file_name, file_url from `tabFile Data`
+			where file_name is not null"""):
+		b = frappe.bean('File Data', name)
+		old_file_name = b.doc.file_name
+		b.doc.file_name = os.path.basename(old_file_name)
+		if old_file_name.startswith('files/') or old_file_name.startswith('/files/'):
+			b.doc.file_url = os.path.normpath('/' + old_file_name)
+		else:
+			b.doc.file_url = os.path.normpath('/files/' + old_file_name)
+		b.save()
+
