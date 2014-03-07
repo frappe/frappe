275b40118b505249471ef78f313890ec90b18a69
Anand Doshi <anand@erpnext.com>
2014-03-07 17:38:31 +0530

    Reload report doctype before patch

--- frappe/patches/4_0/set_module_in_report.py
+++ frappe/patches/4_0/set_module_in_report.py
@@ -5,5 +5,6 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
+	frappe.reload_doc("core", "doctype", "report")
 	frappe.db.sql("""update `tabReport` r set r.module=(select d.module from `tabDocType` d
 		where d.name=r.ref_doctype) where ifnull(r.module, '')=''""")
