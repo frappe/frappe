a3ae8723dedb8bdbbac045c37b055051bc0ad17b
Rushabh Mehta <rmehta@gmail.com>
2014-04-08 16:40:29 +0530

    added ability to add index in statics

--- frappe/website/sitemap.py
+++ frappe/website/sitemap.py
@@ -17,63 +17,67 @@ def get_sitemap_options(path):
 		sitemap_options = build_sitemap_options(path)
 		if can_cache(sitemap_options.no_cache):
 			frappe.cache().set_value(cache_key, sitemap_options)
-	
+
 	return frappe._dict(sitemap_options)
-	
+
 def build_sitemap_options(path):
 	sitemap_options = frappe._dict(frappe.get_doc("Website Route", path).as_dict())
 	home_page = get_home_page()
-		
-	sitemap_config = frappe.get_doc("Website Template", 
+
+	sitemap_config = frappe.get_doc("Website Template",
 		sitemap_options.get("website_template")).as_dict()
-			
+
 	# get sitemap config fields too
-	for fieldname in ("base_template_path", "template_path", "controller", 
+	for fieldname in ("base_template_path", "template_path", "controller",
 		"no_cache", "no_sitemap", "page_name_field", "condition_field"):
 		sitemap_options[fieldname] = sitemap_config.get(fieldname)
-	
+
 	sitemap_options.doctype = sitemap_options.ref_doctype
 	sitemap_options.title = sitemap_options.page_title
 	sitemap_options.pathname = sitemap_options.name
-	
+
 	# establish hierarchy
 	sitemap_options.parents = frappe.db.sql("""select name, page_title from `tabWebsite Route`
 		where lft < %s and rgt > %s order by lft asc""", (sitemap_options.lft, sitemap_options.rgt), as_dict=True)
 
 	if not sitemap_options.no_sidebar:
-		set_sidebar_items(sitemap_options, sitemap_options.pathname, home_page)
+		sitemap_options.children = get_route_children(sitemap_options.pathname, home_page)
 		if not sitemap_options.children:
-			set_sidebar_items(sitemap_options, sitemap_options.parent_website_route, home_page)
+			sitemap_options.children = get_route_children(sitemap_options.parent_website_route, home_page)
 
 	# determine templates to be used
 	if not sitemap_options.base_template_path:
 		app_base = frappe.get_hooks("base_template")
 		sitemap_options.base_template_path = app_base[0] if app_base else "templates/base.html"
-				
+
 	return sitemap_options
 
-def set_sidebar_items(sitemap_options, pathname, home_page):
+def get_route_children(pathname, home_page=None):
+	if not home_page:
+		home_page = get_home_page()
+
 	if pathname==home_page or not pathname:
-		sitemap_options.children = frappe.db.sql("""select url as name, label as page_title,
+		children = frappe.db.sql("""select url as name, label as page_title,
 			1 as public_read from `tabTop Bar Item` where parentfield='sidebar_items' order by idx""",
 			as_dict=True)
-	else:		
-		sitemap_options.children = frappe.db.sql("""select * from `tabWebsite Route`
-			where ifnull(parent_website_route,'')=%s 
-			and public_read=1 
+	else:
+		children = frappe.db.sql("""select * from `tabWebsite Route`
+			where ifnull(parent_website_route,'')=%s
+			and public_read=1
 			order by idx, page_title asc""", pathname, as_dict=True)
-			
-		if sitemap_options.children:
+
+		if children:
 			# if children are from generator and sort order is specified, then get that condition
-			website_template = frappe.get_doc("Website Template", sitemap_options.children[0].website_template)
+			website_template = frappe.get_doc("Website Template", children[0].website_template)
 			if website_template.sort_by!="name":
-				sitemap_options.children = frappe.db.sql("""select t1.* from 
+				children = frappe.db.sql("""select t1.* from
 					`tabWebsite Route` t1, `tab{ref_doctype}` t2
-					where ifnull(t1.parent_website_route,'')=%s 
-					and t1.public_read=1 
+					where ifnull(t1.parent_website_route,'')=%s
+					and t1.public_read=1
 					and t1.docname = t2.name
-					order by t2.{sort_by} {sort_order}""".format(**website_template.as_dict()), 
+					order by t2.{sort_by} {sort_order}""".format(**website_template.as_dict()),
 						pathname, as_dict=True)
-						
-			sitemap_options.children = [frappe.get_doc("Website Route", pathname)] + sitemap_options.children
-			
+
+			children = [frappe.get_doc("Website Route", pathname)] + children
+
+		return children
--- frappe/website/template.py
+++ frappe/website/template.py
@@ -14,44 +14,44 @@ def render_blocks(context):
 	"""returns a dict of block name and its rendered content"""
 
 	out = {}
-	
+
 	env = frappe.get_jenv()
-		
+
 	def _render_blocks(template_path):
 		source = frappe.local.jloader.get_source(frappe.local.jenv, template_path)[0]
 		for referenced_template_path in meta.find_referenced_templates(env.parse(source)):
 			if referenced_template_path:
 				_render_blocks(referenced_template_path)
-				
+
 		template = frappe.get_template(template_path)
 		for block, render in template.blocks.items():
 			out[block] = scrub_relative_urls(concat(render(template.new_context(context))))
-	
+
 	_render_blocks(context["template_path"])
 
 	# default blocks if not found
 	if "title" not in out and out.get("header"):
 		out["title"] = out["header"]
-	
+
 	if "title" not in out:
 		out["title"] = context.get("title")
-	
+
 	if "header" not in out and out.get("title"):
 		out["header"] = out["title"]
 
 	if not out["header"].startswith("<h"):
 		out["header"] = "<h2>" + out["header"] + "</h2>"
-		
+
 	if "breadcrumbs" not in out:
 		out["breadcrumbs"] = scrub_relative_urls(
 			frappe.get_template("templates/includes/breadcrumbs.html").render(context))
-		
-	if "sidebar" not in out:
+
+	if "sidebar" not in out and not context.get("no_sidebar"):
 		out["sidebar"] = scrub_relative_urls(
 			frappe.get_template("templates/includes/sidebar.html").render(context))
-			
+
 	out["title"] = strip_html(out.get("title") or "")
-	
+
 	# remove style and script tags from blocks
 	out["style"] = re.sub("</?style[^<>]*>", "", out.get("style") or "")
 	out["script"] = re.sub("</?script[^<>]*>", "", out.get("script") or "")
