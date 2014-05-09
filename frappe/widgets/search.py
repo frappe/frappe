b34afadd8de0ce3a4269c82a17bd003a291886c7
Anand Doshi <anand@erpnext.com>
2014-05-09 12:31:13 +0530

    Further improvements to autocomplete

--- frappe/widgets/search.py
+++ frappe/widgets/search.py
@@ -88,7 +88,7 @@ def get_std_fields_list(meta, key):
 def build_for_autosuggest(res):
 	results = []
 	for r in res:
-		out = {"value": r[0], "description": ", ".join([cstr(d) for d in r[1:]])}
+		out = {"value": r[0], "description": ", ".join(list(set(cstr(d) for d in r[1:])))}
 		results.append(out)
 	return results
 
