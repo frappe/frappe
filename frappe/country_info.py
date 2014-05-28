c454efaa706cdbc5bb50fe55c99d015e5aa442c7
Nabin Hait <nabinhait@gmail.com>
2014-05-28 17:11:38 +0530

    removed print statement

--- frappe/country_info.py
+++ frappe/country_info.py
@@ -11,7 +11,7 @@ def get_country_info(country=None):
 	data = frappe._dict(data.get(country, {}))
 	if not 'date_format' in data:
 		data.date_format = "dd-mm-yyyy"
-			
+
 	return data
 
 def get_all():
@@ -19,7 +19,7 @@ def get_all():
 		all_data = json.loads(local_info.read())
 	return all_data
 
-@frappe.whitelist()	
+@frappe.whitelist()
 def get_country_timezone_info():
 	import pytz
 	return {
@@ -30,15 +30,13 @@ def get_country_timezone_info():
 def update():
 	with open(os.path.join(os.path.dirname(__file__), "currency_info.json"), "r") as nformats:
 		nformats = json.loads(nformats.read())
-		
+
 	all_data = get_all()
-	
+
 	for country in all_data:
 		data = all_data[country]
-		data["number_format"] = nformats.get(data.get("currency", "default"), 
+		data["number_format"] = nformats.get(data.get("currency", "default"),
 			nformats.get("default"))["display"]
-	
-	print all_data
-	
+
 	with open(os.path.join(os.path.dirname(__file__), "country_info.json"), "w") as local_info:
 		local_info.write(json.dumps(all_data, indent=1))
