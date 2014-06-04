644660b6c41f029f271a0b8866387f358f8fdf54
Anand Doshi <anand@erpnext.com>
2014-06-04 22:58:27 +0530

    Fix in enable scheduler patch

--- frappe/patches/v4_0/enable_scheduler_in_system_settings.py
+++ frappe/patches/v4_0/enable_scheduler_in_system_settings.py
@@ -4,10 +4,11 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.utils.scheduler import disable_scheduler, enable_scheduler
+from frappe.utils import cint
 
 def execute():
 	frappe.reload_doc("core", "doctype", "system_settings")
-	if frappe.db.get_global("disable_scheduler"):
+	if cint(frappe.db.get_global("disable_scheduler")):
 		disable_scheduler()
 	else:
 		enable_scheduler()
