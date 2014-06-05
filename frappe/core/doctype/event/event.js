60b6a3840049e75ee4ee6fb6feffd7fc9e581b6f
Rushabh Mehta <rmehta@gmail.com>
2014-06-05 18:11:44 +0530

    added reference in link, fixes frappe/erpnext#811

--- frappe/core/doctype/event/event.js
+++ frappe/core/doctype/event/event.js
@@ -1,6 +1,14 @@
 // Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
 // MIT License. See license.txt
 
+frappe.ui.form.on("Event", "refresh", function(frm) {
+	if(frm.doc.ref_type && frm.doc.ref_name) {
+		frm.set_intro('Reference: <a href="#Form/'+frm.doc.ref_type+'/'+frm.doc.ref_name+'">'
+			+ frm.doc.ref_name + '</a>');
+	}
+});
+
+
 cur_frm.cscript.repeat_on = function(doc, cdt, cdn) {
 	if(doc.repeat_on==="Every Day") {
 		$.each(["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"], function(i,v) {
