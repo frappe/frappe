75797666776bbae9f3d59a85ca3da3eb634ec042
Anand Doshi <anand@erpnext.com>
2014-06-30 15:20:12 +0530

    ToDo List: Default filter, avatar and Assigned To in list view

--- frappe/core/doctype/todo/todo_list.js
+++ frappe/core/doctype/todo/todo_list.js
@@ -1,18 +1,20 @@
 frappe.listview_settings['ToDo'] = {
 	onload: function(me) {
-		// $(frappe.container.page)
-		// 	.find(".layout-main-section")
-		// 	.css({
-		// 		"background-color":"cornsilk",
-		// 		"min-height": "400px"
-		// 	})
-		if(user_roles.indexOf("System Manager")!==-1) {
-			frappe.route_options = {
-				"owner": user
-			}
-		}
+		frappe.route_options = {
+			"status": "Open",
+			"owner": user
+		};
 	},
 	set_title_left: function() {
 		frappe.set_route();
+	},
+
+	add_columns: [
+		{"content": "Assigned To", width:"15%", label: "Assigned To"}
+	],
+
+	prepare_data: function(data) {
+		data["user_for_avatar"] = data.owner;
+		data["Assigned To"] = data.owner===user ? null : (frappe.boot.user_info[data.owner] || {}).fullname;
 	}
 }
