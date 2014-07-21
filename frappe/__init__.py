99beb0e7f69d8b46427fe43c26ef782e5580caa9
Rushabh Mehta <rmehta@gmail.com>
2014-07-21 12:01:56 +0530

    separated macros

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -607,6 +607,8 @@ def get_jenv():
 		jenv.globals.update({
 			"frappe": sys.modules[__name__],
 			"frappe.utils": frappe.utils,
+			"get_visible_columns": \
+				frappe.get_attr("frappe.templates.pages.print.get_visible_columns"),
 			"_": _
 		})
 
@@ -672,3 +674,7 @@ def get_test_records(doctype):
 			return json.loads(f.read())
 	else:
 		return []
+
+def format_value(value, df, doc=None):
+	import frappe.utils.formatters
+	return frappe.utils.formatters.format_value(value, df, doc)
