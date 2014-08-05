24ede53b50aa9dd12b8ef134574623b6d8ea5342
Anand Doshi <anand@erpnext.com>
2014-08-05 17:55:58 +0530

    [fix] Hide Currency Symbol

--- frappe/utils/data.py
+++ frappe/utils/data.py
@@ -294,10 +294,9 @@ def fmt_money(amount, precision=None, currency=None):
 	amount = comma_str.join(parts) + ((precision and decimal_str) and (decimal_str + decimals) or "")
 	amount = minus + amount
 
-	if currency:
-		symbol = frappe.db.get_value("Currency", currency, "symbol")
-		if symbol:
-			amount = symbol + " " + amount
+	if currency and frappe.defaults.get_global_default("hide_currency_symbol") != "Yes":
+		symbol = frappe.db.get_value("Currency", currency, "symbol") or currency
+		amount = symbol + " " + amount
 
 	return amount
 
