042419564d1db3b9d1231398e9760cbdc845757e
Rushabh Mehta <rmehta@gmail.com>
2014-08-07 16:36:38 +0530

    button for assignment complete

--- frappe/core/doctype/todo/todo.js
+++ frappe/core/doctype/todo/todo.js
@@ -1,6 +1,12 @@
 // bind events
 
 frappe.ui.form.on("ToDo", "refresh", function(frm) {
+	if(frm.doc.reference_type && frm.doc.reference_name) {
+		frm.add_custom_button(__(frm.doc.reference_name), function() {
+			frappe.set_route("Form", frm.doc.reference_type, frm.doc.reference_name);
+		}, frappe.boot.doctype_icons[frm.doc.reference_type]);
+	}
+
 	if(frm.doc.status=="Open") {
 		frm.add_custom_button(__("Close"), function() {
 			frm.set_value("status", "Closed");
@@ -11,12 +17,5 @@ frappe.ui.form.on("ToDo", "refresh", function(frm) {
 			frm.set_value("status", "Open");
 			frm.save();
 		}, null, "btn-default");
-	}
-
-	if(frm.doc.reference_type && frm.doc.reference_name) {
-		frm.set_intro('Reference: <a href="#Form/'+frm.doc.reference_type+'/'+frm.doc.reference_name+'">'
-			+ frm.doc.reference_name + '</a>');
-	} else {
-		frm.set_intro();
 	}
 });
