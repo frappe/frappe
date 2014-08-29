5b99d4f819780925554c0d1559d3bf0eda4ff0d7
Rushabh Mehta <rmehta@gmail.com>
2014-08-29 11:40:00 +0530

    [fix] #800, #802

--- frappe/public/js/frappe/form/layout.js
+++ frappe/public/js/frappe/form/layout.js
@@ -62,10 +62,10 @@ frappe.ui.form.Layout = Class.extend({
 			};
 			fieldobj.refresh && fieldobj.refresh();
 		});
-		if(this.frm)
+		if(this.frm && this.frm.wrapper)
 			$(this.frm.wrapper).trigger("refresh-fields");
 		setTimeout(function() {
-			this.wrapper.find(".empty-form-alert").remove();
+			me.wrapper.find(".empty-form-alert").remove();
 			if(!(me.wrapper.find(".frappe-control:visible").length)) {
 				$('<div class="alert alert-info empty-form-alert">'+__("This form does not have any input")+'</div>')
 				.appendTo(me.wrapper)
