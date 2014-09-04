69f40f80800f88ae689fff1d74ee1d3e0fbcbeec
Anand Doshi <anand@erpnext.com>
2014-09-04 15:55:24 +0530

    [hotfix] assign patch

--- frappe/patches/v4_2/set_assign_in_doc.py
+++ frappe/patches/v4_2/set_assign_in_doc.py
@@ -3,4 +3,8 @@ import frappe
 def execute():
 	for name in frappe.db.sql_list("""select name from `tabToDo`
 		where ifnull(reference_type, '')!='' and ifnull(reference_name, '')!=''"""):
-		frappe.get_doc("ToDo", name).on_update()
+		try:
+			frappe.get_doc("ToDo", name).on_update()
+		except Exception, e:
+			if e.args[0]!=1146:
+				raise
