d92caeb68741d81fde6ba41b6b4fdb58f23a91ee
Rushabh Mehta <rmehta@gmail.com>
2014-09-09 17:19:06 +0530

    [refactor] fixes

--- frappe/core/doctype/comment/comment.py
+++ frappe/core/doctype/comment/comment.py
@@ -10,7 +10,8 @@ from frappe.model.document import Document
 
 class Comment(Document):
 	def get_feed(self):
-		return "<i>{0}</i>".format(self.comment)
+		return """<a href="#Form/{0}/{1}">{0} {1}</a>: <i>{2}</i>""".format(self.comment_doctype, self.comment_docname,
+			self.comment)
 
 	def validate(self):
 		if frappe.db.sql("""select count(*) from tabComment where comment_doctype=%s
