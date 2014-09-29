31043c453be40c8392a4bf655c51ed838c10e915
Rushabh Mehta <rmehta@gmail.com>
2014-09-29 12:20:31 +0530

    [cleanup] currency

--- frappe/geo/doctype/currency/currency.py
+++ frappe/geo/doctype/currency/currency.py
@@ -10,12 +10,3 @@ from frappe.model.document import Document
 class Currency(Document):
 	def validate(self):
 		frappe.clear_cache()
-
-def validate_conversion_rate(currency, conversion_rate, conversion_rate_label, company):
-	"""common validation for currency and price list currency"""
-
-	company_currency = frappe.db.get_value("Company", company, "default_currency")
-
-	if not conversion_rate:
-		throw(_("{0} is mandatory. Maybe Currency Exchange record is not created for {1} to {2}.").format(
-			conversion_rate_label, currency, company_currency))
