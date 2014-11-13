a07e1b94c5c3c1129ad73f7e1fb65902af5394aa
Rushabh Mehta <rmehta@gmail.com>
2014-11-13 14:22:25 +0530

    moved templates/statics to www

--- frappe/utils/boilerplate.py
+++ frappe/utils/boilerplate.py
@@ -27,8 +27,7 @@ def make_boilerplate(dest):
 	frappe.create_folder(os.path.join(dest, hooks.app_name, hooks.app_name, frappe.scrub(hooks.app_title)),
 		with_init=True)
 	frappe.create_folder(os.path.join(dest, hooks.app_name, hooks.app_name, "templates"), with_init=True)
-	frappe.create_folder(os.path.join(dest, hooks.app_name, hooks.app_name, "templates",
-		"statics"))
+	frappe.create_folder(os.path.join(dest, hooks.app_name, hooks.app_name, "www"))
 	frappe.create_folder(os.path.join(dest, hooks.app_name, hooks.app_name, "templates",
 		"pages"), with_init=True)
 	frappe.create_folder(os.path.join(dest, hooks.app_name, hooks.app_name, "templates",
