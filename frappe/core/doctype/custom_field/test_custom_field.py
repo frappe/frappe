9051e56d94375ec1a488e0200debbac09ba15729
Pratik Vyas <m@pd.io>
2014-11-19 14:26:40 +0530

    remove test for custom field

--- frappe/core/doctype/custom_field/test_custom_field.py
+++ frappe/core/doctype/custom_field/test_custom_field.py
@@ -13,19 +13,4 @@ test_records = frappe.get_test_records('Custom Field')
 from frappe.model.db_schema import InvalidColumnName
 
 class TestCustomField(unittest.TestCase):
-	def test_non_ascii(self):
-		cf = frappe.get_doc({
-			"doctype":"Custom Field",
-			"dt": "ToDo",
-			"fieldtype": "Data",
-			"fieldname": "δου"
-		})
-
-		self.assertRaises(InvalidColumnName, cf.insert)
-
-		# todo = frappe.get_doc({
-		# 	"doctype": "ToDo",
-		# 	"description": "test",
-		# 	"δου": "greek"
-		# })
-		# todo.insert()
+	pass
