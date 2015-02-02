f23bdfe2d2776b14050a8ece9f064a807fb27ad5
Anand Doshi <anand@erpnext.com>
2015-02-02 14:11:25 +0530

    [design] desktop padding and fixed listing new

--- frappe/public/js/frappe/ui/listing.js
+++ frappe/public/js/frappe/ui/listing.js
@@ -116,6 +116,7 @@ frappe.ui.Listing = Class.extend({
 	},
 
 	set_primary_action: function() {
+		var me = this;
 		if(this.new_doctype) {
 			var make_new_doc = function() { (me.custom_new_doc || me.make_new_doc).apply(me, [me.new_doctype]); };
 			this.page.set_primary_action(__("New"), function() { make_new_doc(); }, "octicon octicon-plus");
