1d47d2d084ba3081c3577da920214c8f873cdb82
Anand Doshi <anand@erpnext.com>
2015-03-06 10:54:11 +0530

    Fixes frappe/erpnext#2923

--- frappe/print/doctype/letter_head/letter_head.py
+++ frappe/print/doctype/letter_head/letter_head.py
@@ -8,8 +8,12 @@ import frappe
 from frappe.model.document import Document
 
 class LetterHead(Document):
-
 	def validate(self):
+		if not self.is_default:
+			if not frappe.db.sql("""select count(*) from `tabLetter Head` where ifnull(is_default,0)=1"""):
+				self.is_default = 1
+
+	def on_update(self):
 		self.set_as_default()
 
 		# clear the cache so that the new letter head is uploaded
@@ -17,12 +21,10 @@ class LetterHead(Document):
 
 	def set_as_default(self):
 		from frappe.utils import set_default
-		if not self.is_default:
-			if not frappe.db.sql("""select count(*) from `tabLetter Head` where ifnull(is_default,0)=1"""):
-				self.is_default = 1
 		if self.is_default:
 			frappe.db.sql("update `tabLetter Head` set is_default=0 where name != %s",
 				self.name)
+
 			set_default('letter_head', self.name)
 
 			# update control panel - so it loads new letter directly
