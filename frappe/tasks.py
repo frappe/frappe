9cfa9d733fcc51625f3a6d3aecb51a8c66203347
Pratik Vyas <m@pd.io>
2015-03-27 13:00:11 +0530

    Fix get_sites in tasks

--- frappe/tasks.py
+++ frappe/tasks.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.utils.scheduler import enqueue_events
 from frappe.celery_app import get_celery, celery_task, task_logger, LONGJOBS_PREFIX
-from frappe.cli import get_sites
+from frappe.utils import get_sites
 from frappe.utils.file_lock import create_lock, delete_lock
 
 @celery_task()
