d924fd1715fd31e052d4a24f311edf802cf32bd7
Rushabh Mehta <rmehta@gmail.com>
2015-05-05 17:41:23 +0530

    [fix] [minor]

--- frappe/client.py
+++ frappe/client.py
@@ -12,7 +12,7 @@ import json, os
 def get_list(doctype, fields=None, filters=None, order_by=None,
 	limit_start=None, limit_page_length=20):
 	return frappe.get_list(doctype, fields=fields, filters=filters, order_by=order_by,
-		limit_start=limit_start, limit_page_length=limit_page_length, ignore_permissions=True)
+		limit_start=limit_start, limit_page_length=limit_page_length, ignore_permissions=False)
 
 @frappe.whitelist()
 def get(doctype, name=None, filters=None):
