69965302675d658931930927a06e112d7964a92e
Rushabh Mehta <rmehta@gmail.com>
2015-05-11 12:37:01 +0530

    [fix] rename, fixes frappe/erpnext#3233

--- frappe/public/js/frappe/model/model.js
+++ frappe/public/js/frappe/model/model.js
@@ -424,7 +424,7 @@ $.extend(frappe.model, {
 		var d = new frappe.ui.Dialog({
 			title: __("Rename {0}", [__(docname)]),
 			fields: [
-				{label:__("New Name"), fieldtype:"Data", reqd:1},
+				{label:__("New Name"), fieldname: "new_name", fieldtype:"Data", reqd:1},
 				{label:__("Merge with existing"), fieldtype:"Check", fieldname:"merge"},
 			]
 		});
