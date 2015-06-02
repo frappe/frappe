3a2ffd49762e1bbb71f250fee00e759e8f3c22fb
Rushabh Mehta <rmehta@gmail.com>
2015-06-02 11:07:37 +0530

    [datetime] locale/arabic fixes

--- frappe/public/js/frappe/misc/datetime.js
+++ frappe/public/js/frappe/misc/datetime.js
@@ -39,7 +39,7 @@ $.extend(frappe.datetime, {
 	},
 
 	obj_to_str: function(d) {
-		return moment(d).format();
+		return moment(d).locale("en").format();
 	},
 
 	obj_to_user: function(d) {
@@ -106,7 +106,8 @@ $.extend(frappe.datetime, {
 		}
 
 		// user_fmt.replace("YYYY", "YY")? user might only input 2 digits of the year, which should also be parsed
-		return moment(val, [user_fmt.replace("YYYY", "YY"), user_fmt]).format(system_fmt);
+		return moment(val, [user_fmt.replace("YYYY", "YY"),
+			user_fmt]).locale("en").format(system_fmt);
 	},
 
 	user_to_obj: function(d) {
