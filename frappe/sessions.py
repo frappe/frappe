1200d19618d00af15d2edb877f091c348e1ccaa7
Anand Doshi <anand@erpnext.com>
2015-07-13 14:54:20 +0530

    [fix] Use user's language in website if logged in. Fixes print preview in user's language.

--- frappe/sessions.py
+++ frappe/sessions.py
@@ -196,7 +196,8 @@ class Session:
 			self.start_as_guest()
 
 		if self.sid != "Guest":
-			frappe.local.lang = frappe.translate.get_user_lang(self.data.user)
+			frappe.local.user_lang = frappe.translate.get_user_lang(self.data.user)
+			frappe.local.lang = frappe.local.user_lang
 
 	def get_session_record(self):
 		"""get session record, or return the standard Guest Record"""
