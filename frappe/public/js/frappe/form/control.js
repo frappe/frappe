342fa00f3c8992df855f2c4a7cb708191bec79bf
Rushabh Mehta <rmehta@gmail.com>
2015-07-13 12:46:23 +0530

    [fix] [link] bind df.control

--- frappe/public/js/frappe/form/control.js
+++ frappe/public/js/frappe/form/control.js
@@ -964,6 +964,11 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 		var me = this;
 		this.setup_buttons();
 		this.setup_autocomplete();
+		if(this.df.change) {
+			this.$input.on("change", function() {
+				me.df.change.apply(this);
+			});
+		}
 	},
 	get_options: function() {
 		return this.df.options;
