4ca530aa31503f3387e77f09e9ad14e17ca2e033
MaxMorais <max.morais.dmm@gmail.com>
2015-07-24 20:01:43 +0000

    This small fix, ensure that the default value to the Select field, is a string.
    We can pass options hardcoded has {'key': 'the_key', 'label': 'The Label'}, but the code crash.
    This fix ensure that the code dont will broken in these cases.
    
    When it is utils?
    
     - Small customizations requires personalize in a specific context the value of select fields, like "Person Type" in "Customer", whithout crash
     the standard behavior of the field, and add more options.

--- frappe/public/js/frappe/model/create_new.js
+++ frappe/public/js/frappe/model/create_new.js
@@ -65,7 +65,7 @@ $.extend(frappe.model, {
 
 					doc[f.fieldname] = v;
 					updated.push(f.fieldname);
-				} else if(f.fieldtype == "Select" && f.options
+				} else if(f.fieldtype == "Select" && f.options && typeof f.options === 'string'
 					&& !in_list(["[Select]", "Loading..."], f.options)) {
 						doc[f.fieldname] = f.options.split("\n")[0];
 				}
