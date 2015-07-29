dfe9d1296292e7517376fa43acd40975794ab6af
Nabin Hait <nabinhait@gmail.com>
2015-07-29 17:14:42 +0600

    bumped to version 5.1.5

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -26,7 +26,7 @@ to ERPNext.
 """
 
 app_icon = "octicon octicon-circuit-board"
-app_version = "5.1.4"
+app_version = "5.1.5"
 app_color = "orange"
 github_link = "https://github.com/frappe/frappe"
 
