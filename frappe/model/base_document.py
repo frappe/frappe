815252153f26d89c848489c4c0357cd5516f987b
Anand Doshi <anand@erpnext.com>
2015-07-30 16:03:38 +0530

    [hotfix] set docstatus=0 in append

--- frappe/model/base_document.py
+++ frappe/model/base_document.py
@@ -132,6 +132,7 @@ class BaseDocument(object):
 
 			# reference parent document
 			value.parent_doc = self
+			value.docstatus = 0
 			return value
 		else:
 			raise ValueError, "Document attached to child table must be a dict or BaseDocument, not " + str(type(value))[1:-1]
