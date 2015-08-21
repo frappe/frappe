8c41204877369c585c81fe30a658feaa700c350b
Rushabh Mehta <rmehta@gmail.com>
2015-08-21 16:29:31 +0530

    [socket] fail if unable to connect

--- frappe/public/js/frappe/socket.js
+++ frappe/public/js/frappe/socket.js
@@ -7,6 +7,12 @@ frappe.socket = {
 		}
 
 		frappe.socket.socket = io.connect(frappe.socket.get_host());
+
+		if (!frappe.socket.socket) {
+			console.log("Unable to connect to " + frappe.socket.get_host());
+			return;
+		}
+
 		frappe.socket.socket.on('msgprint', function(message) {
 			frappe.msgprint(message);
 		});
