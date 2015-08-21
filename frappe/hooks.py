bcec3eae6fecff028a9e52fe46f3bfb7728f43e1
Anand Doshi <anand@erpnext.com>
2015-08-21 15:26:46 +0600

    bumped to version 6.0.5

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -26,7 +26,7 @@ to ERPNext.
 """
 
 app_icon = "octicon octicon-circuit-board"
-app_version = "6.0.4"
+app_version = "6.0.5"
 app_color = "orange"
 github_link = "https://github.com/frappe/frappe"
 
