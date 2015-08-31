5a5a6181d8dd1d61e7417e235e799f3e8f79a234
Rushabh Mehta <rmehta@gmail.com>
2015-08-31 12:41:10 +0530

    [enhancement] fixes to style and commonified conditions for calendar and style

--- frappe/public/js/frappe/views/calendar.js
+++ frappe/public/js/frappe/views/calendar.js
@@ -124,6 +124,9 @@ frappe.views.Calendar = frappe.views.CalendarBase.extend({
 		"important": {
 			"color": "#FFDCDC"
 		},
+		"danger": {
+			"color": "#FFDCDC"
+		},
 		"warning": {
 			"color": "#FFE6BF",
 		},
@@ -135,6 +138,9 @@ frappe.views.Calendar = frappe.views.CalendarBase.extend({
 		},
 		"inverse": {
 			"color": "#D9F6FF"
+		},
+		"": {
+			"color": "#F0F4F7"
 		}
 	},
 	get_system_datetime: function(date) {
@@ -249,14 +255,12 @@ frappe.views.Calendar = frappe.views.CalendarBase.extend({
 
 			me.fix_end_date_for_event_render(d);
 
-			if(d.status) {
-				if(me.style_map) {
-					$.extend(d, me.styles[me.style_map[d.status]] || {});
-				} else {
-					$.extend(d, me.styles[frappe.utils.guess_style(d.status, "standard")]);
-				}
+			if(me.get_css_class) {
+				$.extend(d, me.styles[me.get_css_class(d)] || {});
+			} else if(me.style_map) {
+				$.extend(d, me.styles[me.style_map[d.status]] || {});
 			} else {
-				$.extend(d, me.styles["standard"]);
+				$.extend(d, me.styles[frappe.utils.guess_style(d.status, "standard")]);
 			}
 			d["textColor"] = "#36414C";
 		})
