7e6a2ca25621ac04366dddf9ee90d16f142954a8
Rushabh Mehta <rmehta@gmail.com>
2015-09-09 11:17:38 +0530

    [fixes] [style] related to website and web-form

--- frappe/patches/v6_2/ignore_user_permissions_if_missing.py
+++ frappe/patches/v6_2/ignore_user_permissions_if_missing.py
@@ -5,4 +5,5 @@ def execute():
 	frappe.reload_doctype("System Settings")
 	system_settings = frappe.get_doc("System Settings")
 	system_settings.ignore_user_permissions_if_missing = 1
+	system_settings.flags.ignore_mandatory = 1
 	system_settings.save()
