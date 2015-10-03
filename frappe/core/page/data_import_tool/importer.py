ea9c06d9e7104cfec5e496ee88559e4322cbd661
Rushabh Mehta <rmehta@gmail.com>
2015-10-03 15:05:29 +0530

    [minor] [data-import] don't check limit if via console

--- frappe/core/page/data_import_tool/importer.py
+++ frappe/core/page/data_import_tool/importer.py
@@ -41,8 +41,8 @@ def upload(rows = None, submit_after_import=None, ignore_encoding_errors=False, 
 		max_rows = 5000
 		if not data:
 			frappe.throw(_("No data found"))
-		elif len(data) > max_rows:
-			frappe.throw(_("Only allowed {0} rows in one import").format(max_rows))
+		elif not via_console and len(data) > max_rows:
+				frappe.throw(_("Only allowed {0} rows in one import").format(max_rows))
 
 	def get_start_row():
 		for i, row in enumerate(rows):
