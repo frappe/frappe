0ae9f2dd3c0542ef8de26b9cae905fb700da4d43
Nabin Hait <nabinhait@gmail.com>
2015-10-28 18:23:15 +0530

    minor fix

--- frappe/model/base_document.py
+++ frappe/model/base_document.py
@@ -233,7 +233,7 @@ class BaseDocument(object):
 				if k in default_fields:
 					del doc[k]
 
-		for key in ("_user_tags", "__islocal", "__onload", "_starred_by", "__unsaved", "__run_link_triggers"):
+		for key in ("_user_tags", "__islocal", "__onload", "_starred_by", "__run_link_triggers"):
 			if self.get(key):
 				doc[key] = self.get(key)
 
