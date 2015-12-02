8d0452a4768a63e1c78e446aab0836d382b1ac2e
Rushabh Mehta <rmehta@gmail.com>
2015-12-02 12:23:18 +0530

    [fix] [minor] sort by default in search_link, fixes frappe/erpnext#4404

--- frappe/desk/search.py
+++ frappe/desk/search.py
@@ -87,7 +87,7 @@ def search_widget(doctype, txt, query=None, searchfield=None, start=0,
 				filters=filters, fields=fields,
 				or_filters = or_filters, limit_start = start,
 				limit_page_length=page_len,
-				order_by="if(_relevance, _relevance, 99999), name asc".format(doctype),
+				order_by="if(_relevance, _relevance, 99999), modified desc".format(doctype),
 				ignore_permissions = True if doctype == "DocType" else False, # for dynamic links
 				as_list=True)
 
