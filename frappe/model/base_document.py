bbc66be54c717fbd97fea78408de148fdbbad8d9
Anand Doshi <anand@erpnext.com>
2015-12-10 17:16:43 +0530

    [fix] update modified in db_set

--- frappe/model/base_document.py
+++ frappe/model/base_document.py
@@ -331,8 +331,10 @@ class BaseDocument(object):
 
 	def db_set(self, fieldname, value, update_modified=True):
 		self.set(fieldname, value)
-		self.set("modified", now())
-		self.set("modified_by", frappe.session.user)
+		if update_modified:
+			self.set("modified", now())
+			self.set("modified_by", frappe.session.user)
+
 		frappe.db.set_value(self.doctype, self.name, fieldname, value,
 			self.modified, self.modified_by, update_modified=update_modified)
 
