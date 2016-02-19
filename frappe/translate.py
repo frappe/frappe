382d664f6871262550b4eb9dbb63d29fad5aae5a
saurabh <saurabh6790@gmail.com>
2016-02-19 11:21:43 +0530

    added comments

--- frappe/translate.py
+++ frappe/translate.py
@@ -179,6 +179,7 @@ def get_full_dict(lang):
 			# cache lang
 			frappe.cache().hset("lang_full_dict", lang, frappe.local.lang_full_dict)
 
+		# get user specific transaltion data
 		user_translations = get_user_translations(lang)
 		if user_translations:
 			frappe.local.lang_full_dict.update(user_translations)
