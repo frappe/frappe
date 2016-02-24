37c48ce030e0fa7a299c713f34cc202d0697b628
Rushabh Mehta <rmehta@gmail.com>
2016-02-24 15:29:36 +0530

    [wip] modules setup

--- frappe/core/page/modules_setup/modules_setup.js
+++ frappe/core/page/modules_setup/modules_setup.js
@@ -1,61 +1,70 @@
-
 frappe.pages['modules_setup'].on_page_load = function(wrapper) {
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
 		title: __('Show or Hide Modules'),
 		single_column: true
 	});
-
-	frappe.breadcrumbs.add("Setup");
-
-	wrapper.page.set_primary_action(__("Update"), function() {
-		frappe.modules_setup.update(this);
-	});
-
-	page.main.css({"padding":"15px"});
-
-	$('<p>'
-		+__("Select modules to be shown (based on permission). If hidden, they will be hidden for all users.")+'</p>').appendTo($(wrapper).find(".layout-main"));
-	$('<div id="modules-list">').appendTo(page.main);
-
-	frappe.modules_setup.refresh_page();
+	frappe.modules_setup = page;
+	page.body.html(frappe.templates.modules_setup);
 }
-
-frappe.modules_setup = {
-	refresh_page: function() {
-		$('#modules-list').empty();
-
-		var wrapper = $('<div class="list-group"></div>').appendTo("#modules-list");
-
-		$.each(keys(frappe.modules).sort(), function(i, m) {
-			if(m!="Setup") {
-				var row = $('<div class="list-group-item">\
-					<span class="check-area" style="margin-right: 10px;"></span> '
-				+ " <span> " + __(m) +'</span></div>').appendTo("#modules-list");
-				var $chk = $("<input type='checkbox' data-module='"+m+"' style='margin-top: -2px'>")
-					.appendTo(row.find(".check-area"));
-				if(!frappe.boot.hidden_modules || frappe.boot.hidden_modules.indexOf(m)==-1) {
-					$chk.prop("checked", true);
-				}
-			}
-		});
-	},
-	update: function(btn) {
-		var ml = [];
-		$('#modules-list [data-module]:checkbox:not(:checked)').each(function() {
-			ml.push($(this).attr('data-module'));
-		});
-
-		return frappe.call({
-			method: 'frappe.core.page.modules_setup.modules_setup.update',
-			args: {
-				ml: ml
-			},
-			callback: function(r) {
-				if(r.exc) msgprint(__("There were errors"))
-			},
-			btn: btn
-		});
-	}
-
-}
+// frappe.pages['modules_setup'].on_page_load = function(wrapper) {
+// 	var page = frappe.ui.make_app_page({
+// 		parent: wrapper,
+// 		title: __('Show or Hide Modules'),
+// 		single_column: true
+// 	});
+//
+// 	frappe.breadcrumbs.add("Setup");
+//
+// 	wrapper.page.set_primary_action(__("Update"), function() {
+// 		frappe.modules_setup.update(this);
+// 	});
+//
+// 	page.main.css({"padding":"15px"});
+//
+// 	$('<p>'
+// 		+__("Select modules to be shown (based on permission). If hidden, they will be hidden for all users.")+'</p>').appendTo($(wrapper).find(".layout-main"));
+// 	$('<div id="modules-list">').appendTo(page.main);
+//
+// 	frappe.modules_setup.page = page;
+// 	frappe.modules_setup.refresh_page();
+// }
+//
+// frappe.modules_setup = {
+// 	refresh_page: function() {
+// 		$('#modules-list').empty();
+//
+// 		var wrapper = $('<div class="list-group"></div>').appendTo("#modules-list");
+//
+// 		$.each(keys(frappe.modules).sort(), function(i, m) {
+// 			if(m!="Setup") {
+// 				var row = $('<div class="list-group-item">\
+// 					<span class="check-area" style="margin-right: 10px;"></span> '
+// 				+ " <span> " + __(m) +'</span></div>').appendTo("#modules-list");
+// 				var $chk = $("<input type='checkbox' data-module='"+m+"' style='margin-top: -2px'>")
+// 					.appendTo(row.find(".check-area"));
+// 				if(!frappe.boot.hidden_modules || frappe.boot.hidden_modules.indexOf(m)==-1) {
+// 					$chk.prop("checked", true);
+// 				}
+// 			}
+// 		});
+// 	},
+// 	update: function(btn) {
+// 		var ml = [];
+// 		$('#modules-list [data-module]:checkbox:not(:checked)').each(function() {
+// 			ml.push($(this).attr('data-module'));
+// 		});
+//
+// 		return frappe.call({
+// 			method: 'frappe.core.page.modules_setup.modules_setup.update',
+// 			args: {
+// 				ml: ml
+// 			},
+// 			callback: function(r) {
+// 				if(r.exc) msgprint(__("There were errors"))
+// 			},
+// 			btn: btn
+// 		});
+// 	}
+//
+// }
--- frappe/core/page/modules_setup/modules_setup.py
+++ frappe/core/page/modules_setup/modules_setup.py
@@ -10,3 +10,10 @@ def update(ml):
 	frappe.db.set_global('hidden_modules', ml)
 	frappe.msgprint(frappe._('Updated'))
 	frappe.clear_cache()
+
+def get_context(context):
+	context.icons = frappe.db.get_all('Desktop Icon',
+			fields='*', filters={'standard': 1}, order_by='idx')
+	context.users = frappe.db.get_all('User', filters={'user_type': 'System User', 'enabled': 1},
+		fields = ['name', 'first_name', 'last_name'])
+	print context.icons
