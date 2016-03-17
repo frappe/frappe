d046507a4afda785652e166fe320d995cb60244d
Rushabh Mehta <rmehta@gmail.com>
2016-03-17 17:54:39 +0530

    [minor] autocomplete=off in input, for IE

--- frappe/public/js/frappe/form/control.js
+++ frappe/public/js/frappe/form/control.js
@@ -366,6 +366,7 @@ frappe.ui.form.ControlData = frappe.ui.form.ControlInput.extend({
 	make_input: function() {
 		this.$input = $("<"+ this.html_element +">")
 			.attr("type", this.input_type)
+			.attr("autocomplete", "off")
 			.addClass("input-with-feedback form-control")
 			.prependTo(this.input_area)
 
@@ -1032,7 +1033,7 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 	make_input: function() {
 		var me = this;
 		$('<div class="link-field ui-front" style="position: relative;">\
-			<input type="text" class="input-with-feedback form-control">\
+			<input type="text" class="input-with-feedback form-control" autocomplete="off">\
 			<span class="link-btn">\
 				<a class="btn-open no-decoration" title="' + __("Open Link") + '">\
 					<i class="icon-link"></i></a>\
