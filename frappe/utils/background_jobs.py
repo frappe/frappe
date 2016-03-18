4cbe752359e70d54e58af05533f67d43283d8789
Valmik Jangla <mail@valmik.in>
2016-03-18 03:19:23 -0700

    Various fixes and improvements

--- frappe/utils/background_jobs.py
+++ frappe/utils/background_jobs.py
@@ -1,28 +1,39 @@
 from __future__ import unicode_literals
-from redis import Redis
+from redis import Redis, from_url
 from rq import Connection, Queue, Worker
-import frappe
 from collections import defaultdict
 from frappe.utils import cstr
+from urlparse import urlparse
+import os
+import frappe
+
 
 logger = frappe.get_logger(__name__)
+SITES_PATH = os.environ.get('SITES_PATH', '.')
 
 def enqueue(method, queue, timeout, **kwargs):
-	"""queue should be either low, high or default
+	"""queue should be either long, high or short
 	timeout should be set accoridngly"""
-	q = Queue(queue, connection=Redis())
-	q.enqueue(rq_task, args=(frappe.local.site, method, kwargs))
+	conf = get_site_config()
+	try:
+		url = urlparse(conf.redis_queue).hostname
+		port = urlparse(conf.redis_queue).port
+	except:
+		raise Exception('Redis configuration missing for Site: {0}'.format(frappe.local.site))
+
+	q = Queue(queue, connection=Redis(url, port))
+	q.enqueue_call(execute_job, timeout=timeout, args=(frappe.local.site, method, kwargs))
 
 def get_jobs():
-	job_dict = defaultdict(list)
-	queue_list = ['low', 'default', 'high']
+	jobs_per_site = defaultdict(list)
+	queue_list = ['long', 'default', 'short']
 	for queue in queue_list:
 		q = Queue(queue, connection=Redis())
 		for job in q.jobs:
-			job_dict[job.args[0]].append(job.args[1])
-	return job_dict
+			jobs_per_site[job.args[0]].append(job.args[1])
+	return jobs_per_site
 
-def rq_task(site, method, kwargs):
+def execute_job(site, method, kwargs):
 	from frappe.utils.scheduler import log
 	frappe.connect(site)
 
@@ -44,6 +55,8 @@ def rq_task(site, method, kwargs):
 
 def start_worker():
 	with Connection():
-		qs = ['high' , 'default', 'low']
+		qs = ['short' , 'default', 'long']
 		Worker(qs).work()
 
+def get_site_config():
+	return frappe.get_site_config(sites_path=SITES_PATH)
