2a008b0f988b1c7d52a489d65c639dea7142a9ab
Rushabh Mehta <rmehta@gmail.com>
2016-03-25 17:28:34 +0530

    [fix] track_seen is optional

--- frappe/model/db_query.py
+++ frappe/model/db_query.py
@@ -207,7 +207,7 @@ class DatabaseQuery(object):
 				self.filters.remove(each)
 
 		# add _seen if track_seen is set
-		if getattr(frappe.get_meta(self.doctype), 'track_seen'):
+		if getattr(frappe.get_meta(self.doctype), 'track_seen', False):
 			self.fields.append('_seen')
 
 	def build_conditions(self):
