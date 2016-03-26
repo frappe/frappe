25276d857fa19abef3ea001e79dbe1e6e0536765
Valeriy Osipov <valeriy.osipov@semilimes.com>
2016-03-26 14:35:33 +0000

    Files become upload

--- frappe/core/doctype/file/file_list.js
+++ frappe/core/doctype/file/file_list.js
@@ -83,20 +83,13 @@ frappe.listview_settings['File'] = {
 		});
 
 		doclist.page.add_menu_item(__("Import Zip-Archive"), function() {
-			// Сюда вставить вызов формы загрузки файла
-			// get_upload_dialog (Frappe/frappe/public/js/frappe/form/footer/attachments.js)
-			var me = this;
-			//if (this.dialog) {
-			//	// remove upload dialog
-			//	this.dialog.$wrapper.remove();
-			//}
-
 			// make upload dialog
 			frappe.ui.get_upload_dialog({
-				//"args": me.get_args(),
-				//"callback": function(attachment, r) { me.attachment_uploaded(attachment, r) },
-				//"max_width": me.frm.cscript ? me.frm.cscript.attachment_max_width : null,
-				//"max_height": me.frm.cscript ? me.frm.cscript.attachment_max_height : null
+				"args": {
+					"folder": doclist.current_folder,
+					"from_form": 1
+				},
+				"callback": function(attachment, r) { doclist.refresh(); },
 			});
 		});
 	},
