5abb55db0ac4fdcad8e76857758120740e584da2
Saurabh <saurabh6790@gmail.com>
2016-04-02 11:53:51 +0530

    [fixes] reload doc before patch execution

--- frappe/patches/v7_0/set_user_fullname.py
+++ frappe/patches/v7_0/set_user_fullname.py
@@ -1,6 +1,8 @@
 import frappe
 
 def execute():
+	frappe.reload_doc("Core", "DocType", "User")
+	
 	for user in frappe.db.get_all('User'):
 		user = frappe.get_doc('User', user.name)
 		user.set_full_name()
