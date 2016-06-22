2d7a9d2fe6a779df644dd4303145561cdc0b8336
Rushabh Mehta <rmehta@gmail.com>
2016-06-22 11:21:29 +0530

    [minor] get chart date

--- frappe/public/js/frappe/desk.js
+++ frappe/public/js/frappe/desk.js
@@ -171,6 +171,7 @@ frappe.Application = Class.extend({
 		user_roles = frappe.boot.user.roles;
 		user_email = frappe.boot.user.email;
 		sys_defaults = frappe.boot.sysdefaults;
+		frappe.ui.py_date_format = frappe.boot.sysdefaults.date_format.replace('dd', '%d').replace('mm', '%m').replace('yyyy', '%Y');
 	},
 	sync_pages: function() {
 		// clear cached pages if timestamp is not found
--- frappe/public/js/frappe/ui/charts.js
+++ frappe/public/js/frappe/ui/charts.js
@@ -6,15 +6,15 @@ frappe.ui.Chart = Class.extend({
 		$.extend(this.opts, opts);
 		this.show_chart(false);
 
-		if(this.opts.data && ((this.opts.data.columns && this.opts.data.columns.length > 1) 
+		if(this.opts.data && ((this.opts.data.columns && this.opts.data.columns.length > 1)
 			|| (this.opts.data.rows && this.opts.data.rows.length > 1))) {
 				this.chart = this.render_chart();
 				this.show_chart(true);
 		}
-		
+
 		return this.chart;
 	},
-	
+
 	render_chart: function() {
 		var chart_dict = {
 			bindto: this.opts.bind_to,
@@ -40,21 +40,21 @@ frappe.ui.Chart = Class.extend({
 				"width": 10
 			}
 		};
-	
+
 		$.extend(chart_dict, this.opts);
-		
+
 		chart_dict["data"]["type"] = this.opts.chart_type || "line";
-			
+
 		return c3.generate(chart_dict);
 	},
-	
+
 	show_chart: function(show) {
 		this.opts.wrapper.find(this.opts.bind_to).toggle(show);
 	},
-	
+
 	set_chart_size: function(width, height) {
 		this.chart.resize({
-			width: width, 
+			width: width,
 			height: height
 		});
 	}
