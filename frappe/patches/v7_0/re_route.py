50546a55297faa2c2590c5cc911564c384239bea
Anand Doshi <anand@erpnext.com>
2016-06-27 12:01:38 +0530

    [fix] [patch] re-route

--- frappe/patches/v7_0/re_route.py
+++ frappe/patches/v7_0/re_route.py
@@ -6,6 +6,7 @@ def execute():
 def update_routes(doctypes):
 	"""Patch old routing system"""
 	for d in doctypes:
+		frappe.reload_doctype(d)
 		try:
 			frappe.db.sql("""update `tab{0}` set route = concat(ifnull(parent_website_route, ""),
 				if(ifnull(parent_website_route, "")="", "", "/"), page_name)""".format(d))
