3b9a088daf9b9c447039816c7f6790437e9e1f6e
Kanchan Chauhan <angel143.dr@gmail.com>
2016-06-27 16:18:04 +0530

    Fixed

--- frappe/public/js/frappe/views/reports/reportview.js
+++ frappe/public/js/frappe/views/reports/reportview.js
@@ -109,7 +109,6 @@ frappe.views.ReportView = frappe.ui.Listing.extend({
 		this.make_export();
 		this.set_init_columns();
 		this.make_save();
-		this.make_save_as();
 		this.make_user_permissions();
 		this.set_tag_and_status_filter();
 
@@ -559,72 +558,56 @@ frappe.views.ReportView = frappe.ui.Listing.extend({
 		}, true);
 	},
 
+
 	// save
 	make_save: function() {
 		var me = this;
 		if(frappe.user.is_report_manager()) {
-			this.page.add_menu_item(__('Save'), function() {
-				// name
-				if(me.docname) {
-					var name = me.docname
-					}
-				else {
-					var name = prompt(__('Select Report Name'));
-					if(!name) {
+			this.page.add_menu_item(__('Save'), function() { me.save_report('save') }, true);
+			this.page.add_menu_item(__('Save As'), function() { me.save_report('save_as') }, true);
+		}
+	},
+		
+	save_report: function(save_type) {
+		var me = this;
+
+		var _save_report = function(name) {
+			// callback
+			return frappe.call({
+				method: 'frappe.desk.reportview.save_report',
+				args: {
+					name: name,
+					doctype: me.doctype,
+					json: JSON.stringify({
+						filters: me.filter_list.get_filters(),
+						columns: me.columns,
+						sort_by: me.sort_by_select.val(),
+						sort_order: me.sort_order_select.val(),
+						sort_by_next: me.sort_by_next_select.val(),
+						sort_order_next: me.sort_order_next_select.val()
+					})
+				},
+				callback: function(r) {
+					if(r.exc) {
+						msgprint(__("Report was not saved (there were errors)"));
 						return;
-						}
 					}
-
-				// callback
-				return me.save_report(name)
-				}, true);
-		}
-	},
-	
-	// save as
-	make_save_as: function() {
-		var me = this;
-		if(frappe.user.is_report_manager()) {
-			this.page.add_menu_item(__('Save as'), function() {
-				var name = prompt(__('Select Report Name'));
-				if(!name) {
-					return;
-					}
-
-				// callback
-				return me.save_report(name)
-				}, true);
-		}
-	},
-	
-	save_report: function(name) {
-		var me = this;
-		console.log(me.columns)
-		frappe.call({
-			method: 'frappe.desk.reportview.save_report',
-			args: {
-				name: name,
-				doctype: me.doctype,
-				json: JSON.stringify({
-					filters: me.filter_list.get_filters(),
-					columns: me.columns,
-					sort_by: me.sort_by_select.val(),
-					sort_order: me.sort_order_select.val(),
-					sort_by_next: me.sort_by_next_select.val(),
-					sort_order_next: me.sort_order_next_select.val()
-				})
-			},
-			callback: function(r) {
-				if(r.exc) {
-					msgprint(__("Report was not saved (there were errors)"));
-					return;
+					if(r.message != me.docname)
+						frappe.set_route('Report', me.doctype, r.message);
 				}
-				if(r.message != me.docname)
-					frappe.set_route('Report', me.doctype, r.message);
-			}
-		});
+			});
+			
+		}
+
+		if(me.docname && save_type == "save") {
+			_save_report(me.docname);
+		} else {
+			frappe.prompt({fieldname: 'name', label: __('New Report name'), reqd: 1, fieldtype: 'Data'}, function(data) {
+				_save_report(data.name);
+			}, __('Save As'));
+		}
+
 	},
-	
 	
 	make_delete: function() {
 		var me = this;
