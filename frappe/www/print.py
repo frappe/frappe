cf4e65e8f6cffaaf4ddd311ff4bfa9296ccab7d2
Rohit Waghchaure <rohitw1991@gmail.com>
2016-06-30 16:29:44 +0530

    Added doc in download_pdf method

--- frappe/www/print.py
+++ frappe/www/print.py
@@ -194,8 +194,8 @@ def download_multi_pdf(doctype, name, format=None):
 	frappe.local.response.type = "download"
 
 @frappe.whitelist()
-def download_pdf(doctype, name, format=None):
-	html = frappe.get_print(doctype, name, format)
+def download_pdf(doctype, name, format=None, doc=None):
+	html = frappe.get_print(doctype, name, format, doc=doc)
 	frappe.local.response.filename = "{name}.pdf".format(name=name.replace(" ", "-").replace("/", "-"))
 	frappe.local.response.filecontent = get_pdf(html)
 	frappe.local.response.type = "download"
