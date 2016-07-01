c05e139366b036de698062da714fe717f1dfe293
Faris Ansari <netchamp.faris@gmail.com>
2016-07-01 16:12:17 +0530

    dropdown in portal fixed

--- frappe/templates/includes/list/list.js
+++ frappe/templates/includes/list/list.js
@@ -38,7 +38,7 @@ frappe.ready(function() {
 		}
 	};
 	
-	if($('.web-sidebar:visible').length === 0)
+	if($('.navbar-header .navbar-toggle:visible').length === 1)
 	{
 		$('.page-head h1').addClass('list-head').click(function(){
 			window.history.back();
