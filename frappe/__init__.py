552330d2af0e119c7a083d959dadd77a4caa0c70
Anand Doshi <anand@erpnext.com>
2016-07-04 19:32:44 +0600

    bumped to version 6.27.23

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -14,7 +14,7 @@ import os, importlib, inspect, logging, json
 from .exceptions import *
 from .utils.jinja import get_jenv, get_template, render_template
 
-__version__ = "6.27.22"
+__version__ = "6.27.23"
 
 local = Local()
 
