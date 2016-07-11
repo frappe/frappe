bd6001eb3830ef038823d3666d8a8738c0e31342
Faris Ansari <netchamp.faris@gmail.com>
2016-07-11 16:21:20 +0530

    added search in mobile navbar

--- frappe/public/js/frappe/ui/toolbar/awesome_bar.js
+++ frappe/public/js/frappe/ui/toolbar/awesome_bar.js
@@ -79,7 +79,7 @@ frappe.search = {
 			.autocomplete(opts).data('ui-autocomplete')._renderItem =
 				frappe.search.render_item;
 
-		$("#sidebar-search")
+		$("#modal-search")
 			.on("focus", open_recent)
 			.autocomplete(opts).data('ui-autocomplete')._renderItem =
 				frappe.search.render_item;
