f46a80f41155a9c09252cba412a276d1eedb49cd
ERPNext <erpnext@erpnext-vm.erpnext-vm>
2016-07-21 14:14:38 +0530

    report print fix

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -168,7 +168,7 @@ frappe.views.QueryReport = Class.extend({
 		if(this.html_format) {
 			var content = frappe.render(this.html_format,
 				{data: frappe.slickgrid_tools.get_filtered_items(this.dataView), filters:this.get_values(), report:this});
-
+			
 			frappe.render_grid({content:content, title:__(this.report_name)});
 		} else {
 			frappe.render_grid({grid:this.grid, report: this, title:__(this.report_name)});
