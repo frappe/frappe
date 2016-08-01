96a003797801b81a6d1f00c04a4f95852f0200e5
Faris Ansari <netchamp.faris@gmail.com>
2016-08-01 16:31:46 +0530

    [feature] help search modal

---
+++ frappe/public/js/frappe/misc/help_links.js
@@ -0,0 +1,20 @@
+frappe.provide('frappe.help.help_links');
+
+frappe.help.help_links['data-import-tool'] = [
+	{ label: 'Importing and Exporting Data', url: 'https://frappe.github.io/erpnext/user/manual/en/setting-up/data/data-import-tool.html' },
+]
+
+frappe.help.help_links['Form/Rename Tool'] = [
+	{ label: 'Bulk Rename', url: 'https://frappe.github.io/erpnext/user/manual/en/setting-up/data/bulk-rename.html' },
+]
+
+frappe.help.help_links['List/Customer'] = [
+	{ label: 'Customer', url: 'https://frappe.github.io/erpnext/user/manual/en/CRM/customer.html' },
+]
+
+frappe.help.help_links['modules/Accounts'] = [
+	{ label: 'Introduction to Accounts', url: 'http://frappe.github.io/erpnext/user/manual/en/accounts/' },
+	{ label: 'Chart of Accounts', url: 'https://frappe.github.io/erpnext/user/manual/en/accounts/chart-of-accounts.html' },
+	{ label: 'How to make payments', url: 'https://frappe.github.io/erpnext/user/manual/en/accounts/making-payments.html' },
+]
+
--- frappe/public/js/frappe/ui/toolbar/toolbar.js
+++ frappe/public/js/frappe/ui/toolbar/toolbar.js
@@ -40,6 +40,8 @@ frappe.ui.toolbar.Toolbar = Class.extend({
 
 		$('.dropdown-toggle').dropdown();
 
+		this.setup_help();
+
 		$(document).trigger('toolbar_setup');
 
 		// clear all custom menus on page change
@@ -49,6 +51,96 @@ frappe.ui.toolbar.Toolbar = Class.extend({
 
 		frappe.search.setup();
 	},
+
+	setup_help: function () {
+
+		$(".dropdown-help .dropdown-menu").on("click", "input, button", function (e) {
+			e.stopPropagation();
+		});
+
+		$("#input-help").on("keydown", function (e) {
+			if(e.which == 13) {
+				var keywords = $(this).val();
+				show_help_results(keywords);
+				$(this).val("");
+			}
+		});
+
+		$("#input-help + span").on("click", function () {
+			var keywords = $(this).val();
+			show_help_results(keywords);
+			$(this).val("");
+		});
+
+		$(document).on("page-change", function () {
+			var $help_links = $(".dropdown-help #help-links");
+			$help_links.html("");
+			var links = frappe.help.help_links[frappe.get_route_str()] || [];
+
+			if(links.length === 0) {
+				$help_links.prev().hide();
+			}
+			else {
+				$help_links.prev().show();
+			}
+
+			for (var i = 0; i < links.length; i++) {
+				var link = links[i];
+				$("<a>", { href: link.url, text: link.label, target: "_blank" }).appendTo($help_links);
+			}
+		});
+
+		function show_help_results(keywords) {
+
+			frappe.call({
+				method: "frappe.utils.help.get_help",
+				args: {
+					text: keywords
+				},
+				callback: function (r) {
+
+					var results = r.message || [];
+					var converter = new Showdown.converter();
+
+					var result_html = "<h4 style='padding: 0 12px;'>Showing results for '" + keywords + "' </h4>";
+
+					for (var i = 0, l = results.length; i < l; i++) {
+
+						var path = results[i][0];
+						var content = results[i][1];
+						var content_html = converter.makeHtml(content);
+
+						var title = (function (path) {
+							path = path.split('/').pop();
+							path = path.substr(0, path.lastIndexOf('.')) || path;
+							return path.replace(/-?\w*/g, function(txt) {
+								if(txt.charAt(0) === '-')
+									txt = txt.slice(1);
+								return ' ' + txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+							});
+						})(path);
+
+						result_html +=	"<div class='search-result'>" +
+											"<a href='#' class='h4'>" + title + "</a>" +
+											"<div style='display: none'>" + content_html + "</div>" +
+										"</div>";
+					}
+
+					if(results.length === 0) {
+						result_html += "<p class='padding'>No results found</p>";
+					}
+
+					var $help_modal = frappe.get_modal("Help", result_html).modal("show");
+					$help_modal.find(".modal-dialog").css("width", "768px");
+
+					$(".search-result").on("click", function (e) {
+						e.preventDefault();
+						$(this).find("div").fadeToggle("300");
+					});
+				}
+			});
+		}
+	}
 
 });
 
