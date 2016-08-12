7029e55923e9f412625638082569c0e4d8222d32
Rushabh Mehta <rmehta@gmail.com>
2016-08-12 20:28:55 +0600

    bumped to version 7.0.21

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -13,7 +13,7 @@ import os, sys, importlib, inspect, json
 from .exceptions import *
 from .utils.jinja import get_jenv, get_template, render_template
 
-__version__ = "7.0.20"
+__version__ = "7.0.21"
 
 local = Local()
 
