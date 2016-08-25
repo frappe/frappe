2a7c2f82e14ec3d1589fc017ea4f9762dca87f54
Rushabh Mehta <rmehta@gmail.com>
2016-08-25 15:02:59 +0530

    [languages] fix code for zh, zh-TW

--- frappe/patches/v7_1/rename_chinese_language_codes.py
+++ frappe/patches/v7_1/rename_chinese_language_codes.py
@@ -2,7 +2,8 @@ import frappe
 
 def execute():
 	frappe.reload_doc('core', 'doctype', 'language')
-	frappe.rename_doc('Language', 'zh-cn', 'zh', force=True, merge=True)
+	frappe.rename_doc('Language', 'zh-cn', 'zh', force=True,
+		merge=True if frappe.db.exists('Language', 'zh') else False)
 	frappe.rename_doc('Language', 'zh-tw', 'zh-TW', force=True)
 	frappe.db.set_value('Language', 'zh', 'language_code', 'zh')
 	frappe.db.set_value('Language', 'zh-TW', 'language_code', 'zh-TW')
