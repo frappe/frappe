cd4a5addd7292472525e0125a50515790a5790ad
Chude Osiegbu <chude.osiegbu@manqala.com>
2016-08-28 23:25:58 +0100

    Patch rename_chinese_language_codes.py assumes that zh-tw exists in tabLanguage and attempts to rename it to zh-TW. It seems that in at least some cases, the entry in tabLanguage is already zh-TW thereby leading to an error when the rename is attempted. This correction, performs a test before attempting the rename.

--- frappe/patches/v7_1/rename_chinese_language_codes.py
+++ frappe/patches/v7_1/rename_chinese_language_codes.py
@@ -3,6 +3,7 @@ import frappe
 def execute():
 	frappe.rename_doc('Language', 'zh-cn', 'zh', force=True,
 		merge=True if frappe.db.exists('Language', 'zh') else False)
-	frappe.rename_doc('Language', 'zh-tw', 'zh-TW', force=True)
+    if not frappe.db.exists('Language', 'zh-TW') and frappe.db.exists('Language', 'zh-tw'):
+        frappe.rename_doc('Language', 'zh-tw', 'zh-TW', force=True)
 	frappe.db.set_value('Language', 'zh', 'language_code', 'zh')
 	frappe.db.set_value('Language', 'zh-TW', 'language_code', 'zh-TW')
