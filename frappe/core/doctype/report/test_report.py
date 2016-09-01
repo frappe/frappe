34965b75b50cccd200c2d1375e4e651b1fbc35d8
Rushabh Mehta <rmehta@gmail.com>
2016-09-01 21:06:45 +0530

    [fix] test_report.py

--- frappe/core/doctype/report/test_report.py
+++ frappe/core/doctype/report/test_report.py
@@ -1,15 +1,19 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # See license.txt
 
-import frappe, json
+from __future__ import unicode_literals
+import frappe, json, os
 import unittest
 
 test_records = frappe.get_test_records('Report')
 
 class TestReport(unittest.TestCase):
 	def test_report_builder(self):
-		if not frappe.db.exists('Report', 'User Activity Report'):
-			frappe.get_doc(json.loads(user_activity_report)).insert()
+		if frappe.db.exists('Report', 'User Activity Report'):
+			frappe.delete_doc('Report', 'User Activity Report')
+
+		with open(os.path.join(os.path.dirname(__file__), 'user_activity_report.json'), 'r') as f:
+			frappe.get_doc(json.loads(f.read())).insert()
 
 		report = frappe.get_doc('Report', 'User Activity Report')
 		data = report.get_data()
@@ -24,21 +28,3 @@ class TestReport(unittest.TestCase):
 		self.assertEquals(data[0][1], 'Module')
 		self.assertTrue('User' in [d[0] for d in data])
 
-# test standard report with child table
-user_activity_report = '''{
-  "add_total_row": 0,
-  "apply_user_permissions": 1,
-  "disabled": 0,
-  "docstatus": 0,
-  "doctype": "Report",
-  "is_standard": "No",
-  "javascript": null,
-  "json": "{\"filters\":[],\"columns\":[[\"name\",\"User\"],[\"user_type\",\"User\"],[\"first_name\",\"User\"],[\"last_name\",\"User\"],[\"last_active\",\"User\"],[\"role\",\"UserRole\"]],\"sort_by\":\"User.modified\",\"sort_order\":\"desc\",\"sort_by_next\":null,\"sort_order_next\":\"desc\"}",
-  "modified": "2016-09-01 02:59:07.728890",
-  "module": "Core",
-  "name": "User Activity Report",
-  "query": null,
-  "ref_doctype": "User",
-  "report_name": "User Activity Report",
-  "report_type": "Report Builder"
- }'''
