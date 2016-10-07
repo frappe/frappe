c3f2071301e1b279c5fdaeef9d261160a83f839c
Nabin Hait <nabinhait@gmail.com>
2016-10-07 18:18:28 +0600

    bumped to version 7.0.44

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -13,7 +13,7 @@ import os, sys, importlib, inspect, json
 from .exceptions import *
 from .utils.jinja import get_jenv, get_template, render_template
 
-__version__ = "7.0.43"
+__version__ = "7.0.44"
 
 local = Local()
 
