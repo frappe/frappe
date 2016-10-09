da5b402abc65114354dd085926a3162b7148f0c3
Rushabh Mehta <rmehta@gmail.com>
2016-10-09 15:37:12 +0530

    [fix] patch

--- frappe/patches/v7_1/rename_scheduler_log_to_error_log.py
+++ frappe/patches/v7_1/rename_scheduler_log_to_error_log.py
@@ -4,4 +4,7 @@ def execute():
 	if not 'tabError Log' in frappe.db.get_tables():
 		frappe.rename_doc('DocType', 'Scheduler Log', 'Error Log')
 		frappe.db.commit()
+		frappe.db.sql("""delete from `tabError Log` where datediff(curdate(), creation) > 30""")
+		frappe.db.sql('alter table `tabError Log` change column name name varchar(140)')
+		frappe.db.sql('alter table `tabError Log` change column parent parent varchar(140)')
 		frappe.db.sql('alter table `tabError Log` engine=MyISAM')
