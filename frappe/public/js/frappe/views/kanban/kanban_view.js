acdbce9e22be8ad6e87b9fcf9af543e0e54c1da9
Faris Ansari <netchamp.faris@gmail.com>
2016-10-31 16:32:50 +0530

    [kanban] minor

--- frappe/public/js/frappe/views/kanban/kanban_view.js
+++ frappe/public/js/frappe/views/kanban/kanban_view.js
@@ -11,11 +11,26 @@ frappe.views.KanbanView = Class.extend({
 	},
 	render: function() {
 		var me = this;
-		me.get_kanban_board_doc(function (board) {
+		me.get_kanban_board_doc(function() {
 			me.prepare();
 			me.bootstrap();
 			me.make_sortable();
 			me.bind();
+			console.log(me)
+		});
+	},
+	prepare: function() {
+		var me = this;
+		me.board = frappe.get_doc('Kanban Board', me.board_name);
+		var board_field_name = me.board.field_name;
+
+		//map rows as cards into columns
+		me.columns = me.board.columns.map(function (column) {
+			var column_name = column.value;
+			column.cards = me.values.filter(function (row) {
+				return row[board_field_name] === column_name;
+			});
+			return column;
 		});
 	},
 	bootstrap: function() {
@@ -23,32 +38,18 @@ frappe.views.KanbanView = Class.extend({
 			.appendTo(this.wrapper);
 		this.$kanban_cards = this.$el.find('.kanban-cards');
 	},
-	prepare: function() {
+	make_sortable: function() {
 		var me = this;
-		me.board = frappe.get_doc('Kanban Board', me.board_name);
-
-		//map rows as cards into columns
-		me.columns = me.board.columns.map(function (column) {
-			var status = column.value;
-			column.cards = me.values.filter(function (value) {
-				return value.status === status;
-			});
-			return column;
+		this.$kanban_cards.each(function(i, el){
+			me.setup_sortable(el);
 		});
 	},
 	get_kanban_board_doc: function(callback) {
 		var me = this;
 		frappe.model.with_doctype('Kanban Board', function() {
 			frappe.model.with_doc('Kanban Board', me.board_name, function() {
-				var doc = frappe.get_doc('Kanban Board', me.board_name);
-				callback(doc);
+				callback();
 			});
-		});
-	},
-	make_sortable: function() {
-		var me = this;
-		this.$kanban_cards.each(function(i, el){
-			me.setup_sortable(el);
 		});
 	},
 	setup_sortable: function(list) {
@@ -58,21 +59,22 @@ frappe.views.KanbanView = Class.extend({
 			ghostClass: "ghost-card",
 			chosenClass: "chosen-card",
 			onStart: function (evt) {
-				me.$el.find('.kanban-card.compose-card').fadeOut(300);
-				me.$el.find('.kanban-cards').height('20px');
+				me.$el.find('.kanban-card.compose-card').fadeOut(200, function() {
+					me.$el.find('.kanban-cards').height('200px');
+				});
 			},
 			onEnd: function (evt) {
-				me.$el.find('.kanban-card.compose-card').fadeIn(300);
+				me.$el.find('.kanban-card.compose-card').fadeIn(100);
 				me.$el.find('.kanban-cards').height('auto');
 			},
 			onAdd: function (evt) {
 				var column_value = $(evt.to).parent().data().columnValue;
 				var card_name = $(evt.item).data().name;
 				if (!column_value) return;
-				// console.log(evt);
-				// me.update_field(card_name, field_name, column_value, function (r) {
-				// 	console.log(r);
-				// });
+				console.log(evt);
+				me.update_field(card_name, me.board.field_name, column_value, function (r) {
+					console.log(r);
+				});
 			}
 		});
 	},
@@ -81,6 +83,7 @@ frappe.views.KanbanView = Class.extend({
 		this.bind_add_card();
 	},
 	bind_click: function() {
+		var me = this;
 		this.$kanban_cards.on('click', '.kanban-card-wrapper', function (e) {
 			var name = $(this).data().name;
 			frappe.set_route('Form', me.doctype, name);
@@ -92,9 +95,21 @@ frappe.views.KanbanView = Class.extend({
 			$(this).hide();
 			$(this).next('form').removeClass('hide').find('textarea').focus();
 		});
-		this.$el.find('.glyphicon-remove').click(function () {
+		this.$el.find('.octicon-x').click(function () {
 			$(this).closest('form').addClass('hide');
 			me.$el.find('.compose-card').show();
+		});
+	},
+	update_field: function (name, fieldname, value, callback) {
+		frappe.call({
+			method: "frappe.client.set_value",
+			args: {
+				doctype: this.doctype,
+				name: name,
+				fieldname: fieldname,
+				value: value
+			},
+			callback: callback
 		});
 	}
 });
