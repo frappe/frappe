e8ed4f0d4e7df8b04602aee6181130def6daad1e
robert schouten <robert_schouten@live.com>
2016-11-17 16:24:21 +0800

    ch move newsletter from setup to tools

--- frappe/config/desk.py
+++ frappe/config/desk.py
@@ -14,6 +14,11 @@ def get_data():
 				},
 				{
 					"type": "doctype",
+					"name": "Newsletter",
+					"description": _("Newsletters to contacts, leads."),
+				},
+				{
+					"type": "doctype",
 					"name": "ToDo",
 					"label": _("To Do"),
 					"description": _("Documents assigned to you and by you."),
