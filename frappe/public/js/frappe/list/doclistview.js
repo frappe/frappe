9edfb00a6bf4ea8353f92b2663cfcfe4f5ee9ae2
robert schouten <robert_schouten@live.com>
2016-12-02 10:28:12 +0800

    c assign to reorganise into class for timesheet approval

--- frappe/public/js/frappe/list/doclistview.js
+++ frappe/public/js/frappe/list/doclistview.js
@@ -649,7 +649,7 @@ frappe.views.DocListView = frappe.ui.Listing.extend({
 			})
 
 			if(docname.length >= 1){
-				me.dialog = frappe.ui.to_do_dialog({
+				me.dialog = new frappe.ui.to_do_dialog({
 					obj: me,
 					method: 'frappe.desk.form.assign_to.add_multiple',
 					doctype: me.doctype,
