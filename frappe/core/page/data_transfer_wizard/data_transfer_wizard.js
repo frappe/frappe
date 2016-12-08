76a8217da23f256078e1c38573eff31ea85e0bf6
Manas Solanki <manas@erpnext.com>
2016-12-08 12:24:56 +0530

    Added Import function for file processing

--- frappe/core/page/data_transfer_wizard/data_transfer_wizard.js
+++ frappe/core/page/data_transfer_wizard/data_transfer_wizard.js
@@ -22,10 +22,8 @@ frappe.DataTransferWizard = Class.extend({
 		frappe.boot.user.can_import = frappe.boot.user.can_import.sort();
 		$(frappe.render_template("data_transfer_wizard", this)).appendTo(this.page.main);
 		this.select = this.page.main.find("select.doctype");
-		this.select_column = this.page.main.find(".doctype-column")
 		this.select.on("change", function() {
 			me.doctype = $(this).val();
-			console.log(me.doctype);
 			me.page.main.find(".select-file").removeClass("hide");
 			frappe.model.with_doctype(me.doctype, function() {
 				if(me.doctype) {
@@ -35,30 +33,40 @@ frappe.DataTransferWizard = Class.extend({
 		});			
 		
 	},
+
 	make_upload: function() {
 		var me = this;
+		this.show_data = true;
+		console.log(this.show_data);
 		frappe.upload.make({
 			parent: this.page.main.find(".upload-area"),
-			btn: this.page.main.find(".btn-import"),
+			btn: this.page.main.find(".btn-upload"),
 			args: {
-				method: 'frappe.core.page.data_transfer_wizard.import_tool.upload',
+				method: 'frappe.core.page.data_transfer_wizard.import_tool.upload_data',				
 			},
-		
 			callback: function(r) {
-				
-				// r.messages = ["<h5 style='color:green'>" + __("Import Successful!") + "</h5>"].
-				// 	concat(r.message.messages)
-				// me.write_data(r);
 				var selected_doctype = frappe.get_doc('DocType', me.doctype);
 				me.page.main.find(".imported-data").removeClass("hide");
 				me.imported_data = me.page.main.find(".imported-data-row");
-				// for (var i = 0; i < r.length; i++) {
-				// 	for (var j = 0; j < r[i].length; j++) {
-				// 		console.log(r[i][j]);
-				// 	}
-				// }
 				$(frappe.render_template("try_html", {imported_data: r, fields:selected_doctype.fields}))
 										.appendTo(me.imported_data.empty());
+				
+				//for (var col = 0; col < r[0].length; col++) {
+				// $( "select.column-map" ).each(function(index) {
+				// 	//console.log("index:" + index + $(this).val());
+				// 	$(this).change(function(index){
+				// 		console.log($(this).val());
+				// 	});
+				// });
+				me.make_import();
+
+				// me.select_column = me.page.main.find("select.imp-col");
+				// me.select_column.on("change", function() {
+				// console.log("in column")
+				// me.column = $(this).val();
+				// console.log(me.column);
+				// });	
+				
 	
 			},
 			is_private: true
@@ -72,8 +80,38 @@ frappe.DataTransferWizard = Class.extend({
 					data.progress[1]);
 			}
 		})
-
-	}
+	},
+	
+	make_import: function() {
+		var me = this;
+		me.btn_import = me.page.main.find(".btn-import");
+		me.btn_import.click(function(){
+			var column_map = [];
+			var row_no;
+			me.show_data = false;
+			me.select_row_no = me.page.main.find("select.row-no");
+			row_no = me.select_row_no.val();
+			console.log(row_no); 
+			$("select.column-map" ).each(function(index) {
+				column_map.push($(this).val());
+			});  
+			console.log("clicked on the button");
+			console.log(column_map);	
+			frappe.call({
+                method: "frappe.core.page.data_transfer_wizard.import_tool.upload_data",
+                args: {
+                    "column_map": column_map,
+                    "row_no": row_no,
+                    "show_data": me.show_data
+                    //"date": frm.doc.date
+                },
+                callback: function(r) {
+                    console.log("in callback");
+                }
+            });
+		});	
+	},
+	
 	// write_data: function(data) {
 	// 	this.page.main.find(".imported-data").removeClass("hide");
 	// 	var parent = this.page.main.find(".imported-data-row").empty();
