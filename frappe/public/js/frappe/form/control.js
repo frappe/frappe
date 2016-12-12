474f38afdb680ff8efa54e6460aefab480d8b6cd
Faris Ansari <netchamp.faris@gmail.com>
2016-12-12 21:46:43 +0530

    ControlLink migrated to awesomeplete

--- frappe/public/js/frappe/form/control.js
+++ frappe/public/js/frappe/form/control.js
@@ -1178,7 +1178,6 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 				}
 
 				if(!me.$input.val()) {
-					// me.$input.autocomplete("search", "");
 					me.$input.val("").trigger("input");
 				}
 			}, 500);
@@ -1195,7 +1194,6 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 		this.translate_values = true;
 		var me = this;
 		this.setup_buttons();
-		// this.setup_autocomplete();
 		this.setup_awesomeplete();
 		if(this.df.change) {
 			this.$input.on("change", function() {
@@ -1270,9 +1268,7 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 			}
 		});
 
-
 		this.$input.cache = {};
-
 
 		this.awesomplete = new Awesomplete(me.input, {
 			minChars: 0,
@@ -1291,10 +1287,8 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 				};
 			},
 			item: function (item, input) {
-				// console.log(item)
 				var parts = item.split("%%%"),
 				d = { value: parts[0], description: parts[1] };
-
 				var _value = d.value;
 
 				if(me.translate_values) {
@@ -1318,19 +1312,6 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 					return a.value.length - b.value.length;
 				}
 				return a < b ? -1 : 1;
-			},
-			replace: function(text) {
-				var value = text.value;
-				// handlers for link_option button
-				if(value.indexOf("__link_option") !== -1) {
-					if(value.indexOf("create_new") !== -1) {
-						me.new_doc();
-					} else if(value.indexOf("advanced_search") !== -1) {
-						me.open_advanced_search();
-					}
-				} else {
-					this.input.value = value;
-				}
 			}
 		});
 
@@ -1369,192 +1350,70 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 						if(frappe.model.can_create(doctype)
 							&& me.df.fieldtype !== "Dynamic Link") {
 							// new item
-							// r.results.push({
-								// value: "<span class='text-primary link-option'>"
-								// 	+ "<i class='fa fa-plus' style='margin-right: 5px;'></i> "
-								// 	+ __("Create a new {0}", [__(me.df.options)])
-								// 	+ "</span>",
-							// 	action: me.new_doc
-							// });
 							r.results.push({
 								label: "<span class='text-primary link-option'>"
 									+ "<i class='fa fa-plus' style='margin-right: 5px;'></i> "
 									+ __("Create a new {0}", [__(me.df.options)])
 									+ "</span>",
-								value: "create_new__link_option",
-								action: me.new_doc
+								value: "create_new__link_option"
 							})
 						};
 						// advanced search
-						// r.results.push({
-							// value: "<span class='text-primary link-option'>"
-							// 	+ "<i class='fa fa-search' style='margin-right: 5px;'></i> "
-							// 	+ __("Advanced Search")
-							// 	+ "</span>",
-						// 	action: me.open_advanced_search
-						// });
 						r.results.push({
 							label: "<span class='text-primary link-option'>"
 								+ "<i class='fa fa-search' style='margin-right: 5px;'></i> "
 								+ __("Advanced Search")
 								+ "</span>",
-							value: "advanced_search__link_option",
-							action: me.open_advanced_search
+							value: "advanced_search__link_option"
 						})
 					}
 					me.$input.cache[doctype][term] = r.results;
 					me.awesomplete.list = me.$input.cache[doctype][term];
-				},
+				}
 			});
 		});
-	},
-	setup_autocomplete: function() {
-		var me = this;
-		this.$input.on("blur", function() {
-			if(me.selected) {
-				me.selected = false;
+
+		// button: action map object
+		var actions = {
+			"create_new": me.new_doc,
+			"advanced_search": me.open_advanced_search
+		}
+
+		this.input.addEventListener("awesomplete-select", function(e) {
+			var value = e.text.value;
+
+			// handlers for action buttons
+			if(value.indexOf("__link_option") !== -1) {
+				e.preventDefault();
+				me.awesomplete.close();
+				var action = value.split("__link_option")[0];
+				var fn = actions[action];
+				fn.apply(me);
 				return;
 			}
-			var value = me.get_value();
-			if(me.doctype && me.docname) {
-				if(value!==me.last_value) {
-					me.parse_validate_and_set_in_model(value);
-				}
+
+			me.autocomplete_open = false;
+
+			// if remember_last_selected is checked in the doctype against the field,
+			// then add this value
+			// to defaults so you do not need to set it again
+			// unless it is changed.
+			if(me.df.remember_last_selected_value) {
+				frappe.boot.user.last_selected_values[me.df.options] = ui.item.value;
+			}
+
+			if(me.frm && me.frm.doc) {
+				me.selected = true;
+				me.parse_validate_and_set_in_model(value);
+				setTimeout(function() {
+					me.selected = false;
+				}, 100);
 			} else {
+				me.$input.val(value);
+				me.$input.trigger("change");
 				me.set_mandatory(value);
 			}
 		});
-
-		this.$input.cache = {};
-		this.$input.autocomplete({
-			minLength: 0,
-			autoFocus: true,
-			source: function(request, response) {
-				var doctype = me.get_options();
-				if(!doctype) return;
-				if (!me.$input.cache[doctype]) {
-					me.$input.cache[doctype] = {};
-				}
-
-				if (me.$input.cache[doctype][request.term]!=null) {
-					// immediately show from cache
-					response(me.$input.cache[doctype][request.term]);
-				}
-
-				var args = {
-					'txt': request.term,
-					'doctype': doctype,
-				};
-
-				me.set_custom_query(args);
-
-				return frappe.call({
-					type: "GET",
-					method:'frappe.desk.search.search_link',
-					no_spinner: true,
-					args: args,
-					callback: function(r) {
-						if(!me.$input.is(":focus")) {
-							return;
-						}
-
-						if(!me.df.only_select) {
-							if(frappe.model.can_create(doctype)
-								&& me.df.fieldtype !== "Dynamic Link") {
-								// new item
-								r.results.push({
-									value: "<span class='text-primary link-option'>"
-										+ "<i class='fa fa-plus' style='margin-right: 5px;'></i> "
-										+ __("Create a new {0}", [__(me.df.options)])
-										+ "</span>",
-									action: me.new_doc
-								});
-							};
-							// advanced search
-							r.results.push({
-								value: "<span class='text-primary link-option'>"
-									+ "<i class='fa fa-search' style='margin-right: 5px;'></i> "
-									+ __("Advanced Search")
-									+ "</span>",
-								action: me.open_advanced_search
-							});
-						}
-
-						me.$input.cache[doctype][request.term] = r.results;
-						response(r.results);
-					},
-				});
-			},
-			open: function(event, ui) {
-				me.$wrapper.css({"z-index": 101});
-				me.autocomplete_open = true;
-			},
-			close: function(event, ui) {
-				me.$wrapper.css({"z-index": 1});
-				me.autocomplete_open = false;
-			},
-			focus: function( event, ui ) {
-				event.preventDefault();
-				if(ui.item.action) {
-					return false;
-				}
-			},
-			select: function(event, ui) {
-				me.autocomplete_open = false;
-
-				// prevent selection on tab
-				var TABKEY = 9;
-				if(event.keyCode === TABKEY) {
-					event.preventDefault();
-					me.$input.autocomplete("close");
-					return false;
-				}
-
-				if(ui.item.action) {
-					ui.item.value = "";
-					ui.item.action.apply(me);
-				}
-
-				// if remember_last_selected is checked in the doctype against the field,
-				// then add this value
-				// to defaults so you do not need to set it again
-				// unless it is changed.
-				if(me.df.remember_last_selected_value) {
-					frappe.boot.user.last_selected_values[me.df.options] = ui.item.value;
-				}
-
-				if(me.frm && me.frm.doc) {
-					me.selected = true;
-					me.parse_validate_and_set_in_model(ui.item.value);
-					setTimeout(function() {
-						me.selected = false;
-					}, 100);
-				} else {
-					me.$input.val(ui.item.value);
-					me.$input.trigger("change");
-					me.set_mandatory(ui.item.value);
-				}
-			}
-		})
-		.on("blur", function() {
-			$(this).autocomplete("close");
-		})
-		.data('ui-autocomplete')._renderItem = function(ul, d) {
-			var _value = d.value;
-			if(me.translate_values) {
-				_value = __(d.value)
-			}
-			var html = "<strong>" + _value + "</strong>";
-			if(d.description && d.value!==d.description) {
-				html += '<br><span class="small">' + __(d.description) + '</span>';
-			}
-			return $('<li></li>')
-				.data('item.autocomplete', d)
-				.html('<a><p>' + html + '</p></a>')
-				.appendTo(ul);
-		};
-		// remove accessibility span (for now)
-		this.$wrapper.find(".ui-helper-hidden-accessible").remove();
 	},
 	set_custom_query: function(args) {
 		var set_nulls = function(obj) {
