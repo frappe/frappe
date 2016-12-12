58b63f9d3a2f67649203ec1a2d9f0587d660c2cd
Manas Solanki <manas@erpnext.com>
2016-12-12 16:38:19 +0530

    doctype added

--- frappe/core/doctype/data_import/data_import.js
+++ frappe/core/doctype/data_import/data_import.js
@@ -3,6 +3,8 @@
 
 frappe.ui.form.on('Data Import', {
 	refresh: function(frm) {
-
+		//frm.get_field('preview_html').wrapper
+		var preview_html = ["<h1>Manas Solanki</h1>"]
+		set_field_options("file_preview", preview_html);
 	}
 });
--- frappe/core/doctype/data_import/data_import.py
+++ frappe/core/doctype/data_import/data_import.py
@@ -5,6 +5,21 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+import os
+
+from frappe.utils import get_site_name, get_site_path, get_site_base_path, get_path
+from openpyxl import load_workbook
+
 
 class DataImport(Document):
-	pass
+	def on_update(self):
+		print "===============>>>>>> TEST"
+		file_path = os.getcwd()+get_site_path()[1:].encode('utf8')+self.import_file
+
+		wb = load_workbook(filename=file_path, read_only=True)
+		ws = wb.active
+
+		excel_file_as_list = []
+		for row in ws.iter_rows(max_row=20):
+			excel_file_as_list.append([cell.value for cell in row])
+		self.file_preview = excel_file_as_list
