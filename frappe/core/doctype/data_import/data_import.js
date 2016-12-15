d2bc90c9ef78b9250b457699222c6fe5f9dafe36
Manas Solanki <manas@erpnext.com>
2016-12-15 12:50:17 +0530

    File Import with all mapped docfields

--- frappe/core/doctype/data_import/data_import.js
+++ frappe/core/doctype/data_import/data_import.js
@@ -2,9 +2,113 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Data Import', {
+
+	onload: function(frm) {
+		var doctype_options = "";
+		for (var i=0; i < frappe.boot.user.can_import.sort().length; i++) {
+            doctype_options = doctype_options + "\n" + frappe.boot.user.can_import[i];
+        }
+		frm.get_field('reference_doctype').df.options = doctype_options;
+
+	},
+
 	refresh: function(frm) {
-		//frm.get_field('preview_html').wrapper
-		var preview_html = ["<h1>Manas Solanki</h1>"]
-		set_field_options("file_preview", preview_html);
+		if(frm.doc.preview_data) {
+			//frm.events.render_html(frm);
+		}
+		//$(frm.fields_dict.file_preview.wrapper).find("select.column-map" ).each(function(index) {
+		 //  	column_map.push($(this).val());
+
+	},
+	
+	reference_doctype: function(frm) {
+		var me = this;
+		frappe.model.with_doctype(frm.doc.reference_doctype, function() {
+				if(frm.doc.reference_doctype) {
+					frm.selected_doctype = frappe.get_meta(frm.doc.reference_doctype).fields;
+					//frm.doc.reference_doctype = frm.selected_doctype;
+				}
+		});
+	},
+
+	check: function(frm) {
+		var column_map = [];
+		$(frm.fields_dict.file_preview.wrapper).find("select.column-map" ).each(function(index) {
+		   	column_map.push($(this).val());
+		});
+		frm.doc.selected_columns = column_map;
+		console.log(frm.doc.selected_columns);
+		frm.add_custom_button(__("Import into Database"), function() {
+				//frm.events.import_file(frm)
+			}).addClass("btn-primary");
+		
+	},
+
+	render_html: function(frm) {
+		console.log("i am in render template"); 
+		var me = this;
+		
+
+		$(frm.fields_dict.file_preview.wrapper).empty();
+
+		var preview_html = ['<hr><h5>{{ __("Select the docfield to map with the column") }}</h5>\
+							<div class="table-responsive">\
+  							<table class="table table-bordered table-hover">\
+  		 					<caption>{%= __("Imported File Data") %}</caption>\
+  							{% for (var i = -1; i < imported_data.length; i++) { %}\
+  								<tr>\
+								{% if (i == -1) { %}\
+  									<td>{%= __("Row No.") %}</td>\
+	  								{% for (var j = 0; j < imported_data[0].length; j++) { %}\
+									<td>\
+									<select class="form-control column-map">\
+			        				<option value="" disabled selected>\
+			        					{%= __("Select Fieldtype") %}</option>\
+									{% for (var k = 0; k < fields.length; k++) { %}\
+									<option value="{%= fields[k].fieldname %}">\
+										{{ __(fields[k].label) }}</option>\
+			        				{% } %}\
+									</select>\
+									</td>\
+									{% } %}\
+  								{% } %}\
+  								{% if (i != -1) { %}\
+  									<td>{{i}}</td>\
+	  								{% for (var j = 0; j < imported_data[i].length; j++) { \
+										var cell_data = imported_data[i][j] %}\
+										<td>{{ cell_data }}</td>\
+									{% } %}\
+  								{% } %}\
+						    	</tr>\
+					   		{% } %}\
+					  		</table>\
+    						</div>'].join("\n");
+    	me.preview_data = JSON.parse(frm.doc.preview_data);
+    	console.log(frappe.render(preview_html, {imported_data: me.preview_data,fields: frm.doc.reference_doctype}));
+		$(frappe.render(preview_html, {imported_data: me.preview_data, 
+ 			fields: frm.selected_doctype})).appendTo(frm.fields_dict.file_preview.wrapper);
+	},
+
+	// import: function(frm) {
+	// 	var column_map = [];
+	// 	$(frm.fields_dict.file_preview.wrapper).find("select.column-map" ).each(function(index) {
+	// 	   	column_map.push($(this).val());
+	// 	});
+	// 	frm.doc.selected_columns = column_map;
+	// 	console.log(frm.doc.selected_columns);
+		
+	// },
+
+	import_file: function(frm) {
+		var me = this;
+		frappe.call({
+                method: "frappe.core.doctype.data_import.data_import.process_file",
+                args: {
+                },
+                callback: function(r) {
+                }
+            });
+		console.log("import_file");
 	}
+
 });
--- frappe/core/doctype/data_import/data_import.py
+++ frappe/core/doctype/data_import/data_import.py
@@ -5,21 +5,30 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-import os
+import os,json
 
 from frappe.utils import get_site_name, get_site_path, get_site_base_path, get_path
 from openpyxl import load_workbook
 
 
 class DataImport(Document):
-	def on_update(self):
-		print "===============>>>>>> TEST"
-		file_path = os.getcwd()+get_site_path()[1:].encode('utf8')+self.import_file
+	def validate(self):
+		pass
+		# print "===============>>>>>> TEST"
+		# if self.import_file:
+		# 	file_path = os.getcwd()+get_site_path()[1:].encode('utf8')+self.import_file
 
-		wb = load_workbook(filename=file_path, read_only=True)
-		ws = wb.active
+		# 	wb = load_workbook(filename=file_path, read_only=True)
+		# 	ws = wb.active
 
-		excel_file_as_list = []
-		for row in ws.iter_rows(max_row=20):
-			excel_file_as_list.append([cell.value for cell in row])
-		self.file_preview = excel_file_as_list
+		# 	excel_file = []
+		# 	for row in ws.iter_rows(max_row=10):
+		# 		tmp_list = []
+		# 		for cell in row:
+		# 			tmp_list.append(cell.value)
+		# 		excel_file.append(tmp_list)
+		# 	self.preview_data = json.dumps(excel_file)
+
+		# # print excel_file
+		# print self
+		# frappe.throw("just stop")
