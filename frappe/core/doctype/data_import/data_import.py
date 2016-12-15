74741b1cfe934eb51db414f422f7f31b32990f9f
Manas Solanki <manas@erpnext.com>
2016-12-15 18:50:13 +0530

    Basic functionality working

--- frappe/core/doctype/data_import/data_import.py
+++ frappe/core/doctype/data_import/data_import.py
@@ -12,23 +12,62 @@ from openpyxl import load_workbook
 
 
 class DataImport(Document):
+
+	# flag_preview_data = False
+
 	def validate(self):
-		pass
-		# print "===============>>>>>> TEST"
-		# if self.import_file:
-		# 	file_path = os.getcwd()+get_site_path()[1:].encode('utf8')+self.import_file
+		print "===============>>>>>> TEST"
+		if self.import_file:# and not flag_preview_data:
+			self.set_preview_data()
 
-		# 	wb = load_workbook(filename=file_path, read_only=True)
-		# 	ws = wb.active
-
-		# 	excel_file = []
-		# 	for row in ws.iter_rows(max_row=10):
-		# 		tmp_list = []
-		# 		for cell in row:
-		# 			tmp_list.append(cell.value)
-		# 		excel_file.append(tmp_list)
-		# 	self.preview_data = json.dumps(excel_file)
-
-		# # print excel_file
-		# print self
+	def on_update(self):
+		print self.import_file, self.reference_doctype, self.selected_row
+		if self.preview_data and self.selected_columns and self.selected_row:
+			self.insert_into_db()
+			self.docstatus = 1
 		# frappe.throw("just stop")
+
+	def insert_into_db(self):
+		error = False
+		selected_columns = json.loads(self.selected_columns)
+		doc = frappe.new_doc(self.reference_doctype)
+
+		for field in doc.meta.fields:
+			if field.reqd == 1 and field.fieldname not in selected_columns:
+				frappe.throw("Please select all required fields to insert")
+
+		file_path = os.getcwd()+get_site_path()[1:].encode('utf8')+self.import_file
+		wb = load_workbook(filename=file_path, read_only=True)
+		ws = wb.active
+
+		start =  int(self.selected_row)
+		for row in ws.iter_rows(min_row=start+1):
+			try:
+				doc = frappe.new_doc(self.reference_doctype)
+				i = 0
+				for cell in row:
+					if selected_columns[i]:
+						setattr(doc, selected_columns[i], cell.value)
+					i = i + 1
+				doc.insert()
+				doc.save()
+			except Exception, e:
+				error = True
+			if error:
+				frappe.db.rollback()
+			else:
+				frappe.db.commit()
+
+	def set_preview_data(self):
+		file_path = os.getcwd()+get_site_path()[1:].encode('utf8')+self.import_file
+		wb = load_workbook(filename=file_path, read_only=True)
+		ws = wb.active
+
+		excel_file = []
+		for row in ws.iter_rows(max_row=15):
+			tmp_list = []
+			for cell in row:
+				tmp_list.append(cell.value)
+			excel_file.append(tmp_list)
+		self.preview_data = json.dumps(excel_file)
+		# flag_preview_data = True
