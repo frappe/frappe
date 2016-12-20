ff906ca546d34940d6b826a7aca8fbe744295d04
Faris Ansari <netchampfaris@users.noreply.github.com>
2016-12-20 11:44:20 +0530

    Removed jquery-ui, new datepicker and autocomplete (#2460)
    * Replaced old datepicker with flatpickr
    
    * Replaced jquery ui-autocomplete with awesomplete
    
    * airdatepicker, bcoz it's cooler
    
    * air datepicker change event fixed
    
    * ControlLink migrated to awesomeplete
    
    * jQuery ui-autocomplete completely replaced with awesomplete
    
    * removed jquery-ui styling
    
    * awesomebar code cleanup
    
    * @mention now uses awesomplete
    
    * Fix datepicker, added css to complement erpnext, minor fixes
    
    * taggle library for tags
    
    * Communication email input uses awesomplete
    
    * filter dashboard uses awesomplete
    
    * grid report uses awesomplete
    
    * Removed jquery-ui lib, updated build.json

--- frappe/public/js/frappe/views/communication.js
+++ frappe/public/js/frappe/views/communication.js
@@ -89,7 +89,7 @@ frappe.views.CommunicationComposer = Class.extend({
 		this.setup_print();
 		this.setup_attach();
 		this.setup_email();
-		this.setup_autosuggest();
+		this.setup_awesomplete();
 		this.setup_last_edited_communication();
 		this.setup_standard_reply();
 		$(this.dialog.fields_dict.recipients.input).val(this.recipients || "").change();
@@ -503,54 +503,54 @@ frappe.views.CommunicationComposer = Class.extend({
 			fields.content.set_input(reply);
 		}
 	},
-	setup_autosuggest: function() {
+	setup_awesomplete: function() {
 		var me = this;
-
-		function split( val ) {
+		[this.dialog.fields_dict.recipients.input,
+		 this.dialog.fields_dict.cc.input]
+			.map(function(input) {
+				me.setup_awesomplete_for_input(input);
+			});
+	},
+	setup_awesomplete_for_input: function(input) {
+		function split(val) {
 			return val.split( /,\s*/ );
 		}
-		function extractLast( term ) {
+		function extractLast(term) {
 			return split(term).pop();
 		}
 
-		$(this.dialog.fields_dict.recipients.input).add(this.dialog.fields_dict.cc.input)
-			.bind( "keydown", function(event) {
-		        if (event.keyCode === $.ui.keyCode.TAB &&
-		            $(this).autocomplete("instance").menu.active) {
-					event.preventDefault();
-				}
-			})
-			.autocomplete({
-				source: function(request, response) {
-					return frappe.call({
-						method:'frappe.email.get_contact_list',
-						args: {
-							'fieldname': "email_id",
-							'doctype': "Contact",
-							'txt': extractLast(request.term).value || '%'
-						},
-						quiet: true,
-						callback: function(r) {
-							response($.ui.autocomplete.filter(
-								r.message || [], extractLast(request.term)));
-						}
-					});
+		var awesomplete = new Awesomplete(input, {
+			minChars: 0,
+			maxItems: 99,
+			autoFirst: true,
+			list: [],
+			item: function(item, input) {
+				return $('<li>').text(item.value).get(0);
+			},
+			filter: function(text, input) {
+				return Awesomplete.FILTER_CONTAINS(text, input.match(/[^,]*$/)[0]);
+			},
+			replace: function(text) {
+				var before = this.input.value.match(/^.+,\s*|/)[0];
+				this.input.value = before + text + ", ";
+			}
+		});
+
+		var $input = $(input);
+		$input.on("input", function(e) {
+			var term = e.target.value;
+			frappe.call({
+				method:'frappe.email.get_contact_list',
+				args: {
+					'fieldname': "email_id",
+					'doctype': "Contact",
+					'txt': extractLast(term) || '%'
 				},
-				appendTo: this.dialog.$wrapper,
-				focus: function() {
-					return false;
-				},
-				select: function( event, ui ) {
-					var terms = split( this.value );
-					// remove the current input
-					terms.pop();
-					// add the selected item
-					terms.push( ui.item.value );
-					// add placeholder to get the comma-and-space at the end
-					terms.push( "" );
-					this.value = terms.join( ", " );
-					return false;
+				quiet: true,
+				callback: function(r) {
+					awesomplete.list = r.message;
 				}
 			});
+		});
 	}
 });
