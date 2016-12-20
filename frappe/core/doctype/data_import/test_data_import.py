7b9be930a46701625078e82d7a3ec9a332041b4b
Manas Solanki <manas@erpnext.com>
2016-12-20 11:37:10 +0530

    Test case added

--- frappe/core/doctype/data_import/test_data_import.py
+++ frappe/core/doctype/data_import/test_data_import.py
@@ -6,7 +6,22 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
+# from frappe.core.doctype.data_import.data_import import get_data_list
+
 # test_records = frappe.get_test_records('Data Import')
 
 class TestDataImport(unittest.TestCase):
-	pass
+	def test_matching(self):
+
+		doc = frappe.new_doc("Data Import")
+
+		doc.reference_doctype = "Student Applicant"
+		doc.import_file = "/private/files/test (copy).xlsx"
+
+		doc.save()
+
+		# new_doc = frappe.get_all("Student Applicant")
+
+		# print doc
+
+
