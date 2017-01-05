1548f4d47312727269f62a7a3d823366325383a1
Rushabh Mehta <rmehta@gmail.com>
2017-01-05 18:20:35 +0530

    Revert "Fix Custom Field In Standard Filter"

--- frappe/patches/v7_2/custom_field_in_standard_filter_fix.py
+++
@@ -1,8 +0,0 @@
-import frappe
-
-def execute():
-	frappe.reload_doc('custom', 'doctype', 'custom_field', force=True)
-	try:
-		frappe.db.sql('update `tabCustom Field` set in_standard_filter = in_filter_dash`')
-	except Exception, e:
-		if e.args[0]!=1054: raise e
