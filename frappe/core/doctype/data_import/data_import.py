9b91eb0776d56543762ad2fd5fdfb23c933737fe
Manas Solanki <manas@erpnext.com>
2017-01-16 18:05:00 +0530

    Single whitlisted function for both templated and raw file

--- frappe/core/doctype/data_import/data_import.py
+++ frappe/core/doctype/data_import/data_import.py
@@ -4,10 +4,11 @@
 
 from __future__ import unicode_literals
 import os, difflib
-import frappe, json
+import frappe, json, csv
 import frappe.async
 import frappe.permissions
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
 
 from frappe import _
 
@@ -18,57 +19,6 @@ from frappe.utils.csvutils import read_csv_content_from_attached_file, getlink
 
 from openpyxl import load_workbook
 
-class DataImport(Document):
-
-	def on_update(self):
-		print "===============>>>>>>> TESTING" 	
-		if self.import_file and not self.preview_data:
-			file_path = os.getcwd()+get_site_path()[1:].encode('utf8')+self.import_file
-			filename, file_extension = os.path.splitext(file_path)
-			if file_extension == '.xlsx':
-				self.file_format = file_extension	
-				self.set_preview_data(file_path)
-			elif file_extension == '.csv':
-				self.file_format = file_extension
-			else:
-				frappe.throw("Unsupported file format")
-		if self.freeze_doctype == 1:
-			self.docstatus = 1
-
-
-	def set_preview_data(self, file_path):
-		file_data = []
-		wb = load_workbook(filename=file_path)
-		ws = wb.active
-		for row in ws.iter_rows(max_row=11):
-			tmp_list = []
-			for cell in row:
-				tmp_list.append(cell.value)
-			file_data.append(tmp_list)
-		self.preview_data = json.dumps(file_data)
-		column_map = []
-		if file_data:
-			for row in file_data:
-				for cell in row:
-					column_map.append(self.get_matched_column(cell))
-			self.selected_columns = json.dumps(column_map)
-
-
-	def get_matched_column(self, column_name=None):
-		new_doc = frappe.new_doc(self.reference_doctype)
-		doc_field = [field.fieldname for field in new_doc.meta.fields if field.fieldtype not in 
-					['Section Break','Column Break','HTML','Table','Button','Image','Fold','Heading']]
-		max_match = 0
-		matched_field = ''
-		for field in doc_field:
-			seq=difflib.SequenceMatcher(None, str(field), str(column_name))
-			d=seq.ratio()*100
-			if d > max_match:
-				max_match = d
-				matched_field = field
-		return matched_field
-
-
 def get_data_keys_definition():
 		return frappe._dict({
 			"data_separator": _('Start entering data below this line'),
@@ -78,11 +28,173 @@ def get_data_keys_definition():
 			"doctype": _("DocType") + ":"
 		})
 
+class DataImport(Document):
+
+	def validate(self):
+		'''
+		Decide whether the file is templated
+		Template data field can be ['no file', 'raw', 'template']
+		If the template field is 'raw' then set preview data and selected columns
+		'''
+		print "===============>>>>>>>TESTING" 
+
+		if not self.import_file:
+			print "in not import file update"
+			self.preview_data = None
+			self.file_preview = None
+			# self.selected_columns = None Considering user will upload same file in a particular doctype
+			self.template = "no file"
+
+		if self.import_file and self.template == "no file":
+			print "in template deciding"
+			file_path = os.getcwd()+get_site_path()[1:].encode('utf8')+self.import_file
+			filename, file_extension = os.path.splitext(file_path)
+			if file_extension == '.xlsx':	
+				self.set_preview_data(file_path)
+			elif file_extension == '.csv':
+				self.template = "template"
+				self.preview_data = ""
+				frappe.msgprint(_("You have uploaded a template file"))
+			else:
+				frappe.throw("Unsupported file format")
+
+	# def on_update(self):
+	# 	print "in on_update"
+	# 	if self.freeze_doctype == 1 and self.log_details:
+	# 		self.flags.ignore_permissions = True
+	# 		self.submit()
+
+	def set_preview_data(self, file_path):
+		print "in set preview data"
+		wb = load_workbook(filename=file_path)
+		ws = wb.active
+
+		if ws.cell(row=20, column=1).value == get_data_keys_definition().data_separator:
+			self.template = "template"
+			self.preview_data = ""
+			frappe.msgprint(_("You have uploaded a template file"))
+
+		else:
+			frappe.msgprint(_("Map the columns of your file using the dropdown"))
+			
+			self.template = "raw"
+			file_data = []
+			max_randered_row = 25 if ws.max_row>25 else ws.max_row
+			for row in ws.iter_rows(max_row=max_randered_row):
+				tmp_list = []
+				for cell in row:
+					print cell.value
+					tmp_list.append(cell.value)
+				if [x for x in tmp_list if x != None]:
+					file_data.append(tmp_list)
+			print file_data
+			self.preview_data = json.dumps(file_data)
+
+			# Considering user will upload same file in a particular doctype
+			column_map = []
+			if file_data and not self.selected_columns:  
+				for cell in file_data[0]:
+					column_map.append(self.get_matched_column(cell))
+				self.selected_columns = json.dumps(column_map)
+
+
+	def get_matched_column(self, column_name=None):
+		new_doc = frappe.new_doc(self.reference_doctype)
+		doc_field = [frappe.unscrub(field.fieldname) for field in new_doc.meta.fields if field.fieldtype not in 
+					['Section Break','Column Break','HTML','Table','Button','Image','Fold','Heading']]
+		max_match = 0
+		matched_field = ''
+		for field in doc_field:
+			seq=difflib.SequenceMatcher(None, str(field), str(column_name))
+			d=seq.ratio()*100
+			if d > max_match:
+				max_match = d
+				matched_field = field
+		print column_name,max_match,matched_field
+		if max_match > 70:
+			return frappe.scrub(matched_field)
+		else:
+			return ""
+
+
 @frappe.whitelist()
-def insert_into_db(reference_doctype, selected_columns, selected_row, import_file=None,
-		file_path=None):
+def route_to_export_template(source_name):
+	print source_name
+	export_template = get_mapped_doc("Data Import", source_name,
+		{"Data Import": {
+			"doctype": "Export Template",
+			"field_map": {
+				# "reference_doctype": "reference_doctype"
+			}
+		}}, ignore_permissions=True)
+	# export_template.save()
+	return export_template
+
+
+@frappe.whitelist()
+def insert_into_db(reference_doctype, doc_name, only_new_records, only_update, submit_after_import, ignore_encoding_errors,
+	no_email, selected_columns, selected_row, template, import_file=None, file_path=None, rows=None):
+	
+	print "in the insert into db"
 	if not file_path:
 		file_path = os.getcwd()+get_site_path()[1:].encode('utf8')+import_file
+	filename, file_extension = os.path.splitext(file_path)
+
+	if template == "raw" and file_extension == '.xlsx':
+		print "in xlsx file"
+		log_message = upload_raw(reference_doctype=reference_doctype, file_path=file_path, only_new_records=1,
+			only_update=0, submit_after_import=submit_after_import, no_email=no_email,
+			selected_columns=selected_columns, selected_row=selected_row)
+		return log_message
+
+
+	elif template == "template" and file_extension == '.xlsx':
+		print "in xlsx template"
+		wb = load_workbook(filename=file_path, read_only=True)
+		ws = wb.active
+
+		rows = []
+		for row in ws.iter_rows():
+			tmp_list = []
+			for cell in row:
+				tmp_list.append(cell.value)
+			rows.append(tmp_list)
+
+		log_message = upload_template(doc_name=doc_name, overwrite=only_new_records, update_only=only_update,
+			submit_after_import=submit_after_import, ignore_encoding_errors=ignore_encoding_errors,
+			no_email=no_email, rows=rows)
+		return log_message
+
+	elif template == "template" and file_extension == '.csv':
+		print "in csv template"
+		log_message = upload_template(doc_name=doc_name, overwrite=only_new_records, update_only=only_update,
+			submit_after_import=submit_after_import, ignore_encoding_errors=ignore_encoding_errors,
+			no_email=no_email, rows=rows)
+		print log_message
+		return log_message
+
+
+def upload_raw(reference_doctype, file_path, only_new_records, only_update, submit_after_import,
+			no_email, selected_columns, selected_row):
+
+	print "in upload raw"
+
+	frappe.flags.mute_emails = no_email
+
+	if submit_after_import and not cint(frappe.db.get_value("DocType",
+			reference_doctype, "is_submittable")):
+		submit_after_import = 0
+
+	# check permissions
+	if not frappe.permissions.can_import(reference_doctype):
+		frappe.flags.mute_emails = False
+		return {"messages": [_("Not allowed to Import") + ": " + _(reference_doctype)], "error": True}
+
+
+	ret = []
+	def log(msg):
+		ret.append(msg)
+
 	error = False
 	selected_columns = json.loads(selected_columns)
 	doc = frappe.new_doc(reference_doctype)
@@ -93,30 +205,45 @@ def insert_into_db(reference_doctype, selected_columns, selected_row, import_fil
 	ws = wb.active
 
 	start =  int(selected_row)
-	for row in ws.iter_rows(min_row=start+1):
-		try:
-			new_doc = frappe.new_doc(reference_doctype)
-			i = 0
-			for cell in row:
-				if selected_columns[i]:
-					setattr(new_doc, selected_columns[i], cell.value)
-				i = i + 1
-			new_doc.insert()
-			new_doc.save()
-		except Exception, e:
-			error = True
-		if error:
-			frappe.db.rollback()
-		else:
-			frappe.db.commit()
-	return True
+	for i,row in enumerate(ws.iter_rows(min_row=start)):
+		if [cell for cell in row if cell.value != None]:
+			
+			try:
+				new_doc = frappe.new_doc(reference_doctype)
+				i = 0
+				for cell in row:
+					if selected_columns[i] and selected_columns[i] != "do_not_map":
+						setattr(new_doc, selected_columns[i], cell.value)
+					i = i + 1
+				print new_doc
+				new_doc.insert()
+				new_doc.save()
+				log([i,row[1].value,(getlink(reference_doctype,new_doc.name)),"Row Inserted"])
+
+			except Exception, e:
+				error = True
+				if doc:
+					frappe.errprint(doc if isinstance(doc, dict) else doc.as_dict())
+				err_msg = frappe.local.message_log and "\n\n".join(frappe.local.message_log) or cstr(e)
+				log('Error for insertion')
+				frappe.errprint(frappe.get_traceback())
+			
+			finally:
+				frappe.local.message_log = []
+
+			
+			if error:
+				frappe.db.rollback()
+			else:
+				frappe.db.commit()
+
+	return {"messages": ret, "error": error}
 
 
 
-@frappe.whitelist()
 def upload_template(doc_name, overwrite, update_only, submit_after_import, ignore_encoding_errors,
 		no_email, rows = None, ignore_links=False, pre_process=None, via_console=False):
-	"""upload data"""
+	"""upload the templated data"""
 
 	print doc_name,overwrite,update_only,submit_after_import,ignore_encoding_errors,no_email
 
@@ -128,7 +255,7 @@ def upload_template(doc_name, overwrite, update_only, submit_after_import, ignor
 		frappe.throw(_("Please do not change the rows above {0}").format(get_data_keys_definition().data_separator))
 
 	def get_start_row():
-		for i, row in enumerate(rows):
+		for i, row in enumerate(rows[:50]):
 			if row and row[0]==get_data_keys_definition().data_separator:
 				return i+1
 		bad_template()
@@ -270,6 +397,10 @@ def upload_template(doc_name, overwrite, update_only, submit_after_import, ignor
 
 
 	make_column_map()
+
+	# print doctypes
+	# frappe.throw("stop")
+
 
 	# delete child rows (if parenttype)
 	parentfield = None
