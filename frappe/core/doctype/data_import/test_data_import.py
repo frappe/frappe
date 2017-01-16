155d1b2b8071a728e479d8ac4a8835aff7226947
Manas Solanki <manas@erpnext.com>
2017-01-16 18:05:00 +0530

    Single whitlisted function for both templated and raw file

--- frappe/core/doctype/data_import/test_data_import.py
+++ frappe/core/doctype/data_import/test_data_import.py
@@ -5,7 +5,8 @@ from __future__ import unicode_literals
 
 import frappe
 import unittest
-import os, json
+import os, json, csv
+from frappe.utils import encode
 
 from openpyxl import load_workbook
 
@@ -32,32 +33,70 @@ class TestDataImport(unittest.TestCase):
 		# new_doc = frappe.get_all("Student Applicant")
 		# print doc
 
-	def test_insertion(self):
+	def test_raw_file(self):
 
-		file_path = os.path.join(os.path.dirname(__file__), "test_data", "test_file.xlsx")
+		print "in test raw files"
+
+		file_path = os.path.join(os.path.dirname(__file__), "test_data", "test_xlsx_file.xlsx")
 
 		doc = frappe.new_doc("Data Import")
 		doc.reference_doctype = "Student Applicant"
-		doc.selected_columns = json.dumps(["first_name", "last_name", "gender", 
+		doc.selected_columns = json.dumps(["first_name", "last_name", "do_not_map", 
 			"student_mobile_number", "student_email_id", "program"])
-		doc.selected_row = 1
+		doc.selected_row = 2
 
-		status = insert_into_db("Student Applicant", doc.selected_columns, 1, file_path=file_path)
 
-		wb = load_workbook(filename=file_path, read_only=True)
-		ws = wb.active
+		log_messages = insert_into_db(reference_doctype=doc.reference_doctype, doc_name=doc, only_new_records=1, only_update=0,
+			submit_after_import=0, ignore_encoding_errors=0,no_email=1, selected_columns=doc.selected_columns, selected_row=doc.selected_row,
+			template="raw", file_path=file_path)
+		
+		print log_messages
+		# status = insert_into_db("Student Applicant", doc.selected_columns, 1, file_path=file_path)
 
-		for row in ws.iter_rows(min_row=doc.selected_row+1):
-			tmp_list = []
-			for cell in row:
-				tmp_list.append(cell.value)
-			self.assertTrue(frappe.get_all(
-				doc.reference_doctype, filters={
-					"first_name": tmp_list[0],
-					"last_name": tmp_list[1],
-					"gender": tmp_list[2],
-					"student_mobile_number": tmp_list[3],
-					"student_email_id": tmp_list[4],
-					"program": tmp_list[5]
-					}
-				))
+		# wb = load_workbook(filename=file_path, read_only=True)
+		# ws = wb.active
+
+		# for row in ws.iter_rows(min_row=doc.selected_row+1):
+		# 	tmp_list = []
+		# 	for cell in row:
+		# 		tmp_list.append(cell.value)
+		# 	self.assertTrue(frappe.get_all(
+		# 		doc.reference_doctype, filters={
+		# 			"first_name": tmp_list[0],
+		# 			"last_name": tmp_list[1],
+		# 			"gender": tmp_list[2],
+		# 			"student_mobile_number": tmp_list[3],
+		# 			"student_email_id": tmp_list[4],
+		# 			"program": tmp_list[5]
+		# 			}
+		# 		))
+
+	def test_csv_template(self):
+		file_path = os.path.join(os.path.dirname(__file__), "test_data", "test_user_csv_template.csv")
+
+		with open(encode(file_path), 'r') as f:
+			fcontent = f.read()
+		fcontent = fcontent.encode("utf-8").splitlines(True)
+
+		rows = []
+		for row in csv.reader(fcontent):
+			r = []
+			for val in row:
+				val = unicode(val, "utf-8").strip()
+				if val=="":
+					r.append(None)
+				else:
+					r.append(val)
+			rows.append(r)
+
+		log_messages = insert_into_db(reference_doctype="User", file_path=file_path, doc_name="Data Import", only_new_records=0, only_update=0,
+			submit_after_import=0, ignore_encoding_errors=0, no_email=1, selected_columns=None, selected_row=None, template=1, rows=rows)
+		print log_messages
+
+
+	def test_xlsx_template(self):
+		file_path = os.path.join(os.path.dirname(__file__), "test_data", "test_xlsx_template.xlsx")
+
+		log_messages = insert_into_db(reference_doctype="Customer", file_path=file_path, doc_name="Data Import", only_new_records=0, only_update=0,
+			submit_after_import=0, ignore_encoding_errors=0, no_email=1, selected_columns=None, selected_row=None, template="template")
+		print log_messages
