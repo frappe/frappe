a0cf92ef5fda700297c276a0d118645268670715
robert schouten <robert_schouten@live.com>
2017-02-03 16:49:20 +0800

    [hotfix] stop frappe sending automatic responses from non user generated emails

--- frappe/email/doctype/email_account/email_account.py
+++ frappe/email/doctype/email_account/email_account.py
@@ -361,6 +361,12 @@ class EmailAccount(Document):
 			communication.reference_doctype = parent.doctype
 			communication.reference_name = parent.name
 
+		# check if message is notification and disable notifications for this message
+		isnotification = email.mail.get("isnotification")
+		if isnotification:
+			if "notification" in isnotification:
+				communication.unread_notification_sent = 1
+
 	def set_sender_field_and_subject_field(self):
 		'''Identify the sender and subject fields from the `append_to` DocType'''
 		# set subject_field and sender_field
