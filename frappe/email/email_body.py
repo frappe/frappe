268e7ad644da00daeb0cbebdc0e0b2e92673b0a3
robert schouten <robert_schouten@live.com>
2017-02-03 16:59:21 +0800

    [hotfix] stop frappe sending automatic responses from non user generated emails (#2674)

--- frappe/email/email_body.py
+++ frappe/email/email_body.py
@@ -188,6 +188,7 @@ class EMail:
 			self.msg_root["Message-Id"] = '<' + message_id + '>'
 		else:
 			self.msg_root["Message-Id"] = get_message_id()
+			self.msg_root["isnotification"] = '<notification>'
 
 	def set_in_reply_to(self, in_reply_to):
 		"""Used to send the Message-Id of a received email back as In-Reply-To"""
