fc6edabbbc2248cac23c4254460dfeba7b5a13f6
Nabin Hait <nabinhait@gmail.com>
2017-02-20 15:14:19 +0600

    bumped to version 7.2.18

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -13,7 +13,7 @@ import os, sys, importlib, inspect, json
 from .exceptions import *
 from .utils.jinja import get_jenv, get_template, render_template
 
-__version__ = '7.2.17'
+__version__ = '7.2.18'
 __title__ = "Frappe Framework"
 
 local = Local()
