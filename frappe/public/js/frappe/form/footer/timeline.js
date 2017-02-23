f00f2f6b525b63d677c3896e348ace0aae3d98e9
Nabin Hait <nabinhait@gmail.com>
2017-02-23 11:29:30 +0530

    minor fix in timeline

--- frappe/public/js/frappe/form/footer/timeline.js
+++ frappe/public/js/frappe/form/footer/timeline.js
@@ -403,7 +403,8 @@ frappe.ui.form.Timeline = Class.extend({
 					} else {
 						
 						var df = frappe.meta.get_docfield(me.frm.doctype, p[0], me.frm.docname);
-						if(df and !df.hidden) {
+
+						if(df && !df.hidden) {
 							var field_display_status = frappe.perm.get_field_display_status(df, null,
 								me.frm.perm);
 							if(field_display_status === 'Read' || field_display_status === 'Write') {
@@ -430,7 +431,7 @@ frappe.ui.form.Timeline = Class.extend({
 						var df = frappe.meta.get_docfield(me.frm.fields_dict[row[0]].grid.doctype,
 							p[0], me.frm.docname);
 
-						if(!df.hidden) {
+						if(df && !df.hidden) {
 							field_display_status = frappe.perm.get_field_display_status(df,
 								null, me.frm.perm);
 
