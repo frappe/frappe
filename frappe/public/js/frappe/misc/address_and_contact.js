d86c5e9f6fc935560671a7395238f734a7ed17f7
Makarand Bauskar <mbauskar@gmail.com>
2017-02-24 11:26:54 +0530

    minor fixes for address and contact list rendering (#2759)
    * [minor] check if contact_list & addr_list is available before rendering contact and address list
    
    * [minor] hide new button if create permission is not available

--- frappe/public/js/frappe/misc/address_and_contact.js
+++ frappe/public/js/frappe/misc/address_and_contact.js
@@ -8,15 +8,17 @@ $.extend(frappe.geo, {
 
 	render_address_and_contact: function(frm) {
 		// render address
-		$(frm.fields_dict['address_html'].wrapper)
-			.html(frappe.render_template("address_list",
-				cur_frm.doc.__onload))
-			.find(".btn-address").on("click", function() {
-				frappe.new_doc("Address");
-			});
+		if(frm.fields_dict['address_html'] && "addr_list" in frm.doc.__onload) {
+			$(frm.fields_dict['address_html'].wrapper)
+				.html(frappe.render_template("address_list",
+					cur_frm.doc.__onload))
+				.find(".btn-address").on("click", function() {
+					frappe.new_doc("Address");
+				});
+		}
 
 		// render contact
-		if(frm.fields_dict['contact_html']) {
+		if(frm.fields_dict['contact_html'] && "contact_list" in frm.doc.__onload) {
 			$(frm.fields_dict['contact_html'].wrapper)
 				.html(frappe.render_template("contact_list",
 					cur_frm.doc.__onload))
