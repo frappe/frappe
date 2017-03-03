5c311b165ac5c79d9981abc8fcbcd5eb4ead2d7a
Faris Ansari <netchamp.faris@gmail.com>
2017-03-03 16:23:22 +0530

    patch for UserSettings table

---
+++ frappe/patches/v8_0/rename_listsettings_to_usersettings.py
@@ -0,0 +1,20 @@
+import frappe, json
+
+def execute():
+	for us in frappe.db.sql('''select user, doctype, data from __ListSettings''', as_dict=True):
+		data = json.loads(us.data)
+		try:
+			data['page_length'] = data['limit']
+			del data['limit']
+		except:
+			pass
+		new_data = dict(List=data)
+		new_data = json.dumps(new_data)
+
+		frappe.db.sql('''update __ListSettings
+			set data=%(new_data)s
+			where user=%(user)s
+			and doctype=%(doctype)s''',
+			{'new_data': new_data, 'user': us.user, 'doctype': us.doctype})
+
+	frappe.db.sql("RENAME TABLE __ListSettings to __UserSettings")
