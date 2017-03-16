98279b39cba75f63a0e3a7eb8a5a8e7fa0d4e0fe
Ricardo Johann <johannricardo@yahoo.com.br>
2017-03-16 02:39:23 -0300

    Added collapse/expand button to charts

--- frappe/public/js/frappe/ui/charts.js
+++ frappe/public/js/frappe/ui/charts.js
@@ -6,7 +6,17 @@ frappe.ui.Chart = Class.extend({
 		$.extend(this.opts, opts);
 		this.show_chart(false);
 
-		this.opts.bind_to = frappe.dom.set_unique_id(this.opts.wrapper);
+		$(this.opts.wrapper).html('<button class="btn btn-default btn-xs show-hide-chart" ' +
+			'type="button" style="margin: 10px;">' +
+			'<span class="chart-btn-text">Show chart </span>' +
+			'<span class="chart-chevron octicon collapse-indicator octicon-chevron-down"' +
+				'style="font-size: inherit;"></span></button>' +
+			'<div class="chart_area_result" style="padding-bottom: 10px">' +
+			'</div>');
+		
+		this.setup_events();
+		
+		this.opts.bind_to = frappe.dom.set_unique_id(this.opts.wrapper.find(".chart_area_result"));
 
 		if(this.opts.data && ((this.opts.data.columns && this.opts.data.columns.length >= 1)
 			|| (this.opts.data.rows && this.opts.data.rows.length >= 1))) {
@@ -79,6 +89,25 @@ frappe.ui.Chart = Class.extend({
 		this.chart.resize({
 			width: width,
 			height: height
+		});
+	},
+	
+	setup_events: function(){
+		var me = this;
+		var chart_result = me.opts.wrapper.find(".chart_area_result");
+		
+		chart_result.toggle(false);
+		me.opts.wrapper.find(".show-hide-chart").toggle(true).on("click", function(){
+			if ($(this).find(".chart-btn-text").text()==__("Show chart ")) {
+				chart_result.toggle(true);
+				$(this).find(".chart-btn-text").text(__("Hide "));
+				$(this).find(".chart-chevron").removeClass("octicon-chevron-down").addClass("octicon-chevron-up");
+			}
+			else {
+				chart_result.toggle(false);
+				$(this).find(".chart-btn-text").text(__("Show chart "));
+				$(this).find(".chart-chevron").removeClass("octicon-chevron-up").addClass("octicon-chevron-down");
+			}
 		});
 	}
 });
