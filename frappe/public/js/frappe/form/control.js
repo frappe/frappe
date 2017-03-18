6543533c543c0ff7c779d3a93ca93fb0098c4b2a
Nathan Do <nathan.dole@gmail.com>
2017-03-18 22:37:41 +0800

    Update layout, remove edit mode/button, make save button compact. Refactor and use change event to trigger save.

--- frappe/public/js/frappe/form/control.js
+++ frappe/public/js/frappe/form/control.js
@@ -1709,8 +1709,8 @@ frappe.ui.form.ControlTable = frappe.ui.form.Control.extend({
 });
 
 frappe.ui.form.ControlSignature = frappe.ui.form.ControlData.extend({
-	editing: true,
-	empty_img: "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8Xw8AAoMBgDTD2qgAAAAASUVORK5CYII=",
+	saving: false,
+	loading: false,
 	make: function() {
 		var me = this;
 		this._super();
@@ -1718,50 +1718,34 @@ frappe.ui.form.ControlSignature = frappe.ui.form.ControlData.extend({
 		// make jSignature field
 		this.$pad = $('<div class="signature-field"></div>')
 		    .appendTo(me.wrapper)
-		    .jSignature({height:300, width: "100%",lineWidth:0.8});
+		    .jSignature({height:300, width: "100%", "lineWidth": 0.8})
+				.on('change', this.on_save_sign.bind(this));
 
-		this.img_wrapper = $('<div style="margin: 7px 0px;">\
+		this.img_wrapper = $('<div class="signature-display">\
 			<div class="missing-image attach-missing-image"><i class="octicon octicon-circle-slash"></i></div></div>')
 			.appendTo(this.wrapper);
 		this.img = $("<img class='img-responsive attach-image-display'>")
 			.appendTo(this.img_wrapper).toggle(false);
 
-		// handle save button
-		this.$btnEditWrapper = $('<div class="btn-group btn-group-justified"><a href="#" type="button" class="signature-edit btn btn-primary btn-block">Edit</a></div>')
-		    .appendTo(this.img_wrapper)
-		    .on("click", '.signature-edit', function() {
-				this.editing = true;
-				me.toggle_edit(true);
-				return false;
-			});
 
-		this.$btnWrapper = $('<div class="btn-group btn-group-justified">' +
-		                    '<a href="#" type="button" class="signature-reset btn btn-default btn-block">Reset</a>' +
-                            '<a href="#" type="button" class="signature-save btn btn-success btn-block">Save</a></div>')
-		    .appendTo(this.$pad)
-			.on("click", '.signature-save', function() {
-				me.on_save_sign();
-				return false;
-			})
+		this.$btnWrapper = $('<div class="signature-btn-row">' +
+												'<a href="#" type="button" class="signature-reset btn btn-default"><i class="glyphicon glyphicon-repeat"></i></a>')
+			.appendTo(this.$pad)
 			.on("click", '.signature-reset', function() {
 				me.on_reset_sign();
 				return false;
 			});
-
-		this.editing = me.get_status()=="Write";
-        me.toggle_edit(this.editing);
-        this.load_pad();
-		me.$wrapper.find(".control-input").toggle(false);
-
-		this.$wrapper.on("refresh", function() {
-			me.editing = me.get_status()=="Write";
-			me.toggle_edit(me.editing);
-            me.load_pad();
-			me.$wrapper.find(".control-input").toggle(false);
-			if(me.get_status()=="Read") {
-				$(me.disp_area).toggle(false);
-			}
-		});
+		// handle refresh by reloading the pad
+		this.$wrapper.on("refresh", this.on_refresh.bind(this));
+	},
+	on_refresh: function(e) {
+		// prevent to load the second time
+		this.$wrapper.find(".control-input").toggle(false);
+		this.set_editable(this.get_status()=="Write");
+		this.load_pad();
+		if(this.get_status()=="Read") {
+				$(this.disp_area).toggle(false);
+		}
 	},
 	set_image: function(value) {
 		if(value) {
@@ -1773,49 +1757,63 @@ frappe.ui.form.ControlSignature = frappe.ui.form.ControlData.extend({
 		}
 	},
 	load_pad: function() {
-        var value = this.get_value();
-        if (this.$pad) {
-            this.$pad.jSignature('reset');
-            this.set_image(value ? value: this.empty_img);
-            if (value) {
-                this.$pad.jSignature('setData', value);
-            }
-        }
-    },
-    toggle_edit: function(editing) {
-        this.$pad.toggle(editing);
-        this.img_wrapper.toggle(!editing);
-        this.$btnEditWrapper.toggle(!editing && this.get_status()=="Write");
-        this.$btnWrapper.toggle(editing);
-        if (editing) {
-            this.$btnWrapper.addClass('editing');
-            this.editing = true;
-        }
-        else {
-            this.$btnWrapper.removeClass('editing');
-            this.editing = false;
-        }
-    },
-    set_value: function(value) {
-        this._super(value);
-        this.value = value;
-    },
-    get_value: function() {
+		// make sure not triggered during saving
+		if (this.saving) return;
+		// get value
+		var value = this.get_value();
+		// import data for pad
+		if (this.$pad) {
+			this.loading = true;
+			// reset in all cases
+			this.$pad.jSignature('reset');
+			if (value) {
+				// load the image to find out the size, because scaling will affect
+				// stroke width
+				try {
+					this.$pad.jSignature('setData', value);
+					this.set_image(value);
+				}
+				catch (e){
+					console.log("Cannot set data for signature", value, e);
+				}
+			}
+
+			this.loading = false;
+		}
+	},
+	set_editable: function(editable) {
+			this.$pad.toggle(editable);
+			this.img_wrapper.toggle(!editable);
+			this.$btnWrapper.toggle(editable);
+			if (editable) {
+					this.$btnWrapper.addClass('editing');
+			}
+			else {
+					this.$btnWrapper.removeClass('editing');
+			}
+	},
+	set_my_value: function(value) {
+		if (this.saving || this.loading) return;
+		this.saving = true;
+		this.set_value(value);
+		this.value = value;
+		this.saving = false;
+	},
+	get_value: function() {
 		return this.value? this.value: this.get_model_value();
 	},
-    // reset signature canvas
-    on_reset_sign: function() {
-        this.$pad.jSignature("reset");
-        this.set_value("");
-    },
-    // save signature value to model and display
-    on_save_sign: function() {
-        var base64_img = this.$pad.jSignature("getData");
-        this.set_value(base64_img);
-        this.set_image(this.get_value());
-		this.editing=false;
-        this.toggle_edit(false);
-    }
+	// reset signature canvas
+	on_reset_sign: function() {
+		this.$pad.jSignature("reset");
+		this.set_my_value("");
+	},
+	// save signature value to model and display
+	on_save_sign: function() {
+		if (this.saving || this.loading) return;
+		var base64_img = this.$pad.jSignature("getData");
+		this.set_my_value(base64_img);
+		this.set_image(this.get_value());
+	}
 });
 
 frappe.ui.form.fieldtype_icons = {
