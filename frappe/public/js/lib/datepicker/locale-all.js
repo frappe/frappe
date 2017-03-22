da465e754ecab601a15674569e449379875ea412
joezsweet <joezsweet@tiscali.it>
2017-03-22 11:00:30 +0100

    added en-GB localization

--- frappe/public/js/lib/datepicker/locale-all.js
+++ frappe/public/js/lib/datepicker/locale-all.js
@@ -50,6 +50,20 @@
     firstDay: 0
 }; })(jQuery);
 
+;(function ($) { $.fn.datepicker.language['en-GB'] = {
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    months: ['January','February','March','April','May','June', 'July','August','September','October','November','December'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    today: 'Today',
+    clear: 'Clear',
+    dateFormat: 'dd/mm/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 1
+}; })(jQuery);
+
+
 ;(function ($) { $.fn.datepicker.language['es'] = {
     days: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
     daysShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
