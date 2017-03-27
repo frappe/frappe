8cf65957d7a620cbe42d3c382dfc833cb3ba2f63
Ayush AS <ayush.as.shukla@gmail.com>
2017-03-27 14:12:58 +0530

    Latest commit

--- frappe/utils/kickapp/query.py
+++ frappe/utils/kickapp/query.py
@@ -1,54 +1,54 @@
 from __future__ import unicode_literals
-import frappe
+import frappe, re
 from frappe.utils.kickapp.helper import Helper
 
-
 class Query(object):
+
 	def __init__(self, bot_name):
 		self.bot_name = bot_name.lower()
 		self.actions = Helper().get_doctype_actions_from_bot_name(bot_name)
 	
+	def has(self, query, words):
+		for word in words:
+			sub_string = query if len(query) < len(word) else word
+			string = query if len(query) > len(word) else word
+			if sub_string in string:
+				return True
+
 	def get_action_from_text(self, text):
-		count_array = self.get_count_array(text)
-		index = count_array.index(max(count_array))
-		if index > -1:
-			return self.actions[index]
-		return 'error'
-	
-	def get_count_array(self, text):
-		count_array = []
 		for x in self.actions:
-			count_array.append(sum(i == j for i, j in zip(text.lower(), x)))
-		return count_array
-	# def create_keywords(self, doctype):
-	# 	return ['create', 
-	# 			'{0} {1}'.format('create new', doctype), 
-	# 			'{0} {1}'.format('create a', doctype),
-	# 			'{0} {1}'.format('create my', doctype),
-	# 			'{0} {1}'.format('create', doctype),
-	# 			'{0} {1}'.format('hey create new', doctype),
-	# 			'{0} {1}'.format('hey create', doctype),
-	# 			'{0} {1}'.format('make new', doctype),
-	# 			'{0} {1}'.format('make a', doctype),
-	# 			'{0} {1}'.format('make', doctype)]
+			all_keywords = self.generate_keyword(x)
+			if len(all_keywords) > 0 and self.has(text, all_keywords):
+				return x
+		return 'error'
 
-	# def update_keywords(self, doctype):
-	# 	return ['update', 
-	# 			'{0} {1}'.format('update', doctype),
-	# 			'{0} {1}'.format('update a', doctype),
-	# 			'{0} {1}'.format('update my', doctype)]
+	def generate_keyword(self, action):
+		if action == 'create':
+			return self.create_keywords()
+		elif action == 'get':
+			return self.get_keywords() 
+		else:
+			return []
 
-	# def delete_keywords(self, doctype):
-	# 	return ['delete', '{0} {1}'.format('delete', doctype),
-	# 			'{0} {1}'.format('delete a', doctype),
-	# 			'{0} {1}'.format('delete my', doctype)]
+	def create_keywords(self):
+		doctype = self.bot_name
+		return ['create', 
+				'{0} {1}'.format('create new', doctype), 
+				'{0} {1}'.format('create a', doctype),
+				'{0} {1}'.format('create my', doctype),
+				'{0} {1}'.format('create', doctype),
+				'{0} {1}'.format('hey create new', doctype),
+				'{0} {1}'.format('hey create', doctype),
+				'{0} {1}'.format('make new', doctype),
+				'{0} {1}'.format('make a', doctype),
+				'{0} {1}'.format('make', doctype)]
 
-	# def get_keywords(self, doctype):
-	# 	return ['get', 
-	# 			'list', '{0} {1}'.format('list', doctype), 
-	# 			'{0} {1}'.format('get', doctype),
-	# 			'{0} {1}'.format('get all', doctype),
-	# 			'{0} {1}'.format('get my', doctype),
-	# 			'{0} {1} {2}'.format('get', doctype, 's'),
-	# 			'{0} {1} {2}'.format('get all', doctype, 's'),
-	# 			'{0} {1} {2}'.format('get my', doctype, 's')]
+	def get_keywords(self):
+		doctype = self.bot_name
+		return ['get', 'list', '{0} {1}'.format('list', doctype), 
+				'{0} {1}'.format('get', doctype),
+				'{0} {1}'.format('get all', doctype),
+				'{0} {1}'.format('get my', doctype),
+				'{0} {1} {2}'.format('get', doctype, 's'),
+				'{0} {1} {2}'.format('get all', doctype, 's'),
+				'{0} {1} {2}'.format('get my', doctype, 's')]
