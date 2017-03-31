c8a592ede37e7a7143dba1550650d701f1a49c97
frappe <frappe@forked-erpnext.station>
2017-03-31 15:39:27 +0200

    fix Datepicker - Calendar localization compatibility

--- frappe/public/js/lib/datepicker/locale-all.js
+++ frappe/public/js/lib/datepicker/locale-all.js
@@ -47,6 +47,19 @@
     clear: 'Clear',
     dateFormat: 'dd/mm/yyyy',
     timeFormat: 'hh:ii aa',
+    firstDay: 0
+}; })(jQuery);
+
+;(function ($) { $.fn.datepicker.language['en-GB'] = {
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    months: ['January','February','March','April','May','June', 'July','August','September','October','November','December'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    today: 'Today',
+    clear: 'Clear',
+    dateFormat: 'dd/mm/yyyy',
+    timeFormat: 'hh:ii aa',
     firstDay: 1
 }; })(jQuery);
 
