c52781952d48cc5b0b0984bf5edb684512a2098a
Rushabh Mehta <rmehta@gmail.com>
2017-03-31 16:41:57 +0530

    [fix] no ready events in desk

--- frappe/public/js/frappe/desk.js
+++ frappe/public/js/frappe/desk.js
@@ -52,10 +52,6 @@ frappe.Application = Class.extend({
 
 		$(document).trigger('app_ready');
 
-		frappe.ready_events.forEach(function(fn) {
-			fn();
-		});
-
 		if (frappe.boot.messages) {
 			frappe.msgprint(frappe.boot.messages);
 		}
