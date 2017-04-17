ff2e37135dc49cea518ad3368d81df52a6db5e59
Javier Wong <bohlian@gmail.com>
2017-04-17 17:13:59 +0800

    [Enhancement] Allow Notes (announcements) to be notified every time a user logins (#3094)
    * [Enhancement] Allow Notes (announcements) to be notified every time a user logins.
    
    * [Fix] Add Notify on Login flag in frappe.boot.notes
    
    * Patch Doctype Notes to notify once for all existing notes
    
    * Added newline to end of patch_notes_notify_once file
    
    * [fix] Changed Notes Notify Once flag to Notify On Every Login
    
    * Update note.json
    
    * [fix] Removed frappe/patches/v7_2/patch_notes_notify_on_every_login.py

--- frappe/public/js/frappe/desk.js
+++ frappe/public/js/frappe/desk.js
@@ -434,17 +434,22 @@ frappe.Application = Class.extend({
 		var me = this;
 		if(frappe.boot.notes.length) {
 			frappe.boot.notes.forEach(function(note) {
-				if(!note.seen) {
+				if(!note.seen || note.notify_on_every_login) {
 					var d = frappe.msgprint({message:note.content, title:note.title});
 					d.keep_open = true;
 					d.custom_onhide = function() {
 						note.seen = true;
-						frappe.call({
-							method: "frappe.desk.doctype.note.note.mark_as_seen",
-							args: {
-								note: note.name
-							}
-						});
+
+						// Mark note as read if the Notify On Every Login flag is not set
+						if (!note.notify_on_every_login) {
+							frappe.call({
+								method: "frappe.desk.doctype.note.note.mark_as_seen",
+								args: {
+									note: note.name
+								}
+							});
+						}
+						
 						// next note
 						me.show_notes();
 
