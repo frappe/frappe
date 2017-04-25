33dc251722b9d2a45bbc06691e81b02915a4cfae
Faris Ansari <netchamp.faris@gmail.com>
2017-04-25 13:00:28 +0530

    Update build system requirements patch

--- frappe/patches/v8_0/install_new_build_system_requirements.py
+++ frappe/patches/v8_0/install_new_build_system_requirements.py
@@ -1,9 +1,18 @@
-import subprocess
+from subprocess import Popen, call, PIPE
 
 def execute():
+    # update nodejs version if brew exists
+    p = Popen(['which', 'brew'], stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate()
+    if output:
+        subprocess.call(['brew', 'upgrade', 'node'])
+    else:
+        print 'Please update your NodeJS version'
+
     subprocess.call([
         'npm', 'install',
         'babel-core',
+        'less',
         'chokidar',
         'babel-preset-es2015',
         'babel-preset-es2016',
