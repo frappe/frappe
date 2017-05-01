99844756b16d54e4a6bf41d973cc1f8ae36f57f8
Faris Ansari <netchamp.faris@gmail.com>
2017-05-01 11:55:26 +0530

    Fix global `route` variables

--- frappe/public/js/frappe/views/breadcrumbs.js
+++ frappe/public/js/frappe/views/breadcrumbs.js
@@ -78,6 +78,7 @@ frappe.breadcrumbs = {
 				|| frappe.get_doc('DocType', breadcrumbs.doctype).issingle) {
 				// no user listview for non-system managers and single doctypes
 			} else {
+				var route;
 				if(frappe.boot.treeviews.indexOf(breadcrumbs.doctype) !== -1) {
 					route = 'Tree/' + breadcrumbs.doctype;
 				} else {
