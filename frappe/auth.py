25a7fe8eb5b46bab6033e0d565134990e18ad10a
Gaurav Naik <gauravnaik6288@gmail.com>
2017-05-19 15:48:13 +0530

    Phone based login (#3340)
    * Mobile no based login.
    
    Added Mobile No field to User doctype, unique.
    Added check for searching user by mobile no in authenticate method in auth.py
    Changed placeholder Email address to Email address or Mobile number in login.html
    
    * Mobile no login option in System Settings
    
    * Modified By to Administrator
    
    * Update login.html

--- frappe/auth.py
+++ frappe/auth.py
@@ -185,6 +185,9 @@ class LoginManager:
 		if not (user and pwd):
 			self.fail('Incomplete login details', user=user)
 
+		if cint(frappe.db.get_value("System Settings", "System Settings", "allow_login_using_mobile_number")):
+			user = frappe.db.get_value("User", filters={"mobile_no": user}, fieldname="name") or user
+
 		self.check_if_enabled(user)
 		self.user = self.check_password(user, pwd)
 
