ff4c86089c39378c2279b7369aee554c687b0061
tundebabzy <tundebabzy@gmail.com>
2017-06-13 07:39:35 +0100

    Make "Amount in words" spell three decimals (#3358)
    * fixes frappe/erpnext#8895
    
    * checks for non-numeric type
    
    * makes words translatable

--- frappe/tests/test_utils.py
+++ frappe/tests/test_utils.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 import unittest
 
-from frappe.utils import evaluate_filters
+from frappe.utils import evaluate_filters, money_in_words
 
 class TestFilters(unittest.TestCase):
 	def test_simple_dict(self):
@@ -34,3 +34,27 @@ class TestFilters(unittest.TestCase):
 			{'status': 'Open', 'age': ('>', 10)}))
 		self.assertFalse(evaluate_filters({'doctype': 'User', 'status': 'Open', 'age': 20},
 			{'status': 'Open', 'age': ('>', 30)}))
+
+class TestMoney(unittest.TestCase):
+	def test_money_in_words(self):
+		nums_bhd = [
+			(5000, "BHD Five Thousand only."), (5000.0, "BHD Five Thousand only."),
+			(0.1, "One Hundred Fils only."), (0, "BHD Zero only."), ("Fail", "")
+		]
+
+		nums_ngn = [
+			(5000, "NGN Five Thousand only."), (5000.0, "NGN Five Thousand only."),
+			(0.1, "Ten Kobo only."), (0, "NGN Zero only."), ("Fail", "")
+		]
+
+		for num in nums_bhd:
+			self.assertEqual(
+				money_in_words(num[0], "BHD"), num[1], "{0} is not the same as {1}".
+					format(money_in_words(num[0], "BHD"), num[1])
+			)
+
+		for num in nums_ngn:
+			self.assertEqual(
+				money_in_words(num[0], "NGN"), num[1], "{0} is not the same as {1}".
+					format(money_in_words(num[0], "NGN"), num[1])
+			)
