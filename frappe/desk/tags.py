f9d461e28af42ba480376386daf26085e8259712
tunde <tundebabzy@gmail.com>
2017-06-15 00:32:20 +0100

    removes culprit print statement

--- frappe/desk/tags.py
+++ frappe/desk/tags.py
@@ -84,7 +84,6 @@ class DocTags:
 	def remove(self, dn, tag):
 		"""remove a user tag"""
 		tl = self.get_tags(dn).split(',')
-		print(tag, filter(lambda x:x!=tag, tl))
 		self.update(dn, filter(lambda x:x.lower()!=tag.lower(), tl))
 
 	def remove_all(self, dn):
