75a44a6f20098ab66fd0aa96bdb79759df1115b9
Faris Ansari <netchamp.faris@gmail.com>
2017-06-16 16:24:54 +0530

    add nightwatch to travis
    typo
    
    nightwatch fix
    
    kill node
    
    launch_url
    
    run tests on local selenium
    
    manually run sc
    
    sc in bg with sleep
    
    minor
    
    minor
    
    run test on chromedriver
    
    simplify travis
    
    minor
    
    add chrome package
    
    implicit chromedriver
    
    nightwatch.global.js
    
    path fix
    
    console statement
    
    enable screenshot
    
    send screenshot to telegram
    
    path
    
    minor
    
    remove chromium
    
    add binary
    
    screenshot
    
    url
    
    cleanup
    
    increase timout
    
    minor
    
    minor
    
    minor

--- frappe/nightwatch.js
+++ frappe/nightwatch.js
@@ -1,17 +1,22 @@
-// require('env2')('.env'); // optionally store your environment variables in .env
-// const PKG = require('./package.json'); // so we can get the version of the project
-const BINPATH = './node_modules/nightwatch/bin/'; // change if required.
-// const SCREENSHOT_PATH = "./node_modules/nightwatch/screenshots/" + PKG.version + "/";
-const {
-	SAUCE_USERNAME,
-	SAUCE_ACCESS_KEY,
-	TRAVIS_JOB_NUMBER='travis-job-number'
-} = process.env;
-const app_name = get_cli_arg('app');
+const fs = require('fs');
 
-let local_launch_url = 'http://localhost:8000';
-if(get_cli_arg('site')) {
-	local_launch_url = 'http://' + get_cli_arg('site') + ':' + get_cli_arg('port');
+const ci_mode = get_cli_arg('env') === 'ci_server';
+const site_name = get_cli_arg('site');
+
+const app_name = get_cli_arg('app');
+if(!app_name) {
+	console.log('Please specify app to run tests');
+	return;
+}
+
+if(!ci_mode && !site_name) {
+	console.log('Please specify site to run tests');
+	return;
+}
+
+let site_url;
+if(site_name) {
+	site_url = 'http://' + site_name + ':' + get_port();
 }
 
 
@@ -19,160 +24,50 @@ const config = {
 	"src_folders": [
 		`apps/${app_name}/${app_name}/tests/ui`
 	],
-	"output_folder": "./node_modules/nightwatch/reports", // reports (test outcome) output by Nightwatch
+	"globals_path" : 'apps/frappe/frappe/nightwatch.global.js',
 	"selenium": {
-		"start_process": true,
-		"server_path": BINPATH + "selenium.jar", // downloaded by selenium-download module (see below)
-		"log_path": "",
-		"host": "127.0.0.1",
-		"port": 4444,
-		"cli_args": {
-			"webdriver.chrome.driver": BINPATH + "chromedriver" // also downloaded by selenium-download
-		}
+		"start_process": false
 	},
-	// "test_workers": { "enabled": true, "workers": "auto" }, // perform tests in parallel where possible
 	"test_settings": {
-		"sauce": {
-			"launch_url": "http://localhost:8000", // we're testing a Public or "staging" site on Saucelabs
-			"selenium_port": 80,
-			"selenium_host": "ondemand.saucelabs.com",
+		"default": {
+			"launch_url": site_url || 'http://localhost:8000',
+			"selenium_port": 9515,
+			"selenium_host": "127.0.0.1",
+			"default_path_prefix": "",
 			"silent": true,
 			// "screenshots": {
-			// 	"enabled": true, // save screenshots to this directory (excluded by .gitignore)
+			// 	"enabled": true,
 			// 	"path": SCREENSHOT_PATH
 			// },
-			"username": `${SAUCE_USERNAME}`,
-			"access_key": `${SAUCE_ACCESS_KEY}`,
 			"globals": {
-				"waitForConditionTimeout": 10000    // wait for content on the page before continuing
-			},
-			"desiredCapabilities": {
-				build: `build-${TRAVIS_JOB_NUMBER}`,
-				'tunnel-identifier': TRAVIS_JOB_NUMBER,
-			}
-		},
-		"local": {
-			"launch_url": local_launch_url,
-			"selenium_port": 4444,
-			"selenium_host": "127.0.0.1",
-			// "silent": true,
-			// "screenshots": {
-			// 	"enabled": true, // save screenshots taken here
-			// 	"path": SCREENSHOT_PATH
-			// }, // this allows us to control the
-			"globals": {
-				"waitForConditionTimeout": 15000 // on localhost sometimes internet is slow so wait...
+				"waitForConditionTimeout": 15000
 			},
 			"desiredCapabilities": {
 				"browserName": "chrome",
 				"chromeOptions": {
-					"args": [
-						`Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46
-						(KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3`,
-						"--window-size=640,1136" // iphone 5
-					]
+					"args": ["--no-sandbox", "--start-maximized"]
 				},
 				"javascriptEnabled": true,
 				"acceptSslCerts": true
 			}
 		},
-		"chrome": { // your local Chrome browser (chromedriver)
-			"desiredCapabilities": {
-				"browserName": "chrome",
-				"javascriptEnabled": true,
-				"acceptSslCerts": true
-			}
-		},
-		"chromemac": { // browsers used on saucelabs:
-			"desiredCapabilities": {
-				"browserName": "chrome",
-				"platform": "OS X 10.11",
-				"version": "48"
-			}
-		},
-		"ie11": {
-			"desiredCapabilities": {
-				"browserName": "internet explorer",
-				"platform": "Windows 10",
-				"version": "11.0"
-			}
-		},
-		"firefox": {
-			"desiredCapabilities": {
-				"platform": "XP",
-				"browserName": "firefox",
-				"version": "33"
-			}
-		},
-		"internet_explorer_10": {
-			"desiredCapabilities": {
-				"platform": "Windows 7",
-				"browserName": "internet explorer",
-				"version": "10"
-			}
-		},
-		"android_s4_emulator": {
-			"desiredCapabilities": {
-				"browserName": "android",
-				"deviceOrientation": "portrait",
-				"deviceName": "Samsung Galaxy S4 Emulator",
-				"version": "4.4"
-			}
-		},
-		"iphone_6_simulator": {
-			"desiredCapabilities": {
-				"browserName": "iPhone",
-				"deviceOrientation": "portrait",
-				"deviceName": "iPhone 6",
-				"platform": "OSX 10.10",
-				"version": "8.4"
-			}
+		"ci_server": {
+			"launch_url": 'http://localhost:8000'
 		}
 	}
 }
 module.exports = config;
 
-/**
- * selenium-download does exactly what it's name suggests;
- * downloads (or updates) the version of Selenium (& chromedriver)
- * on your localhost where it will be used by Nightwatch.
- */
-require('fs').stat(BINPATH + 'selenium.jar', function (err, stat) {
-	if (err || !stat || stat.size < 1) {
-		require('selenium-download').ensure(BINPATH, function (error) {
-			if (error) throw new Error(error); // no point continuing so exit!
-			console.log('✔ Selenium & Chromedriver downloaded to:', BINPATH);
-		});
-	}
-});
-
 function get_cli_arg(key) {
 	var args = process.argv;
 	var i = args.indexOf('--' + key);
-	return args[i+1];
+	if(i === -1) {
+		return null;
+	}
+	return args[i + 1];
 }
 
-// function padLeft(count) { // theregister.co.uk/2016/03/23/npm_left_pad_chaos/
-// 	return count < 10 ? '0' + count : count.toString();
-// }
-
-// var FILECOUNT = 0; // "global" screenshot file count
-// /**
-//  * The default is to save screenshots to the root of your project even though
-//  * there is a screenshots path in the config object above! ... so we need a
-//  * function that returns the correct path for storing our screenshots.
-//  * While we're at it, we are adding some meta-data to the filename, specifically
-//  * the Platform/Browser where the test was run and the test (file) name.
-//  */
-// function imgpath(browser) {
-// 	var a = browser.options.desiredCapabilities;
-// 	var meta = [a.platform];
-// 	meta.push(a.browserName ? a.browserName : 'any');
-// 	meta.push(a.version ? a.version : 'any');
-// 	meta.push(a.name); // this is the test filename so always exists.
-// 	var metadata = meta.join('~').toLowerCase().replace(/ /g, '');
-// 	return SCREENSHOT_PATH + metadata + '_' + padLeft(FILECOUNT++) + '_';
-// }
-
-// module.exports.imgpath = imgpath;
-// module.exports.SCREENSHOT_PATH = SCREENSHOT_PATH;
+function get_port() {
+	var bench_config = JSON.parse(fs.readFileSync('sites/common_site_config.json'));
+	return bench_config.webserver_port;
+}
--- frappe/tests/ui/todo.js
+++ frappe/tests/ui/todo.js
@@ -1,13 +1,21 @@
 module.exports = {
 	beforeEach: browser => {
+		console.log(browser.launch_url);
 		browser
 			.url(browser.launch_url + '/login')
-			.waitForElementVisible('body');
+			.waitForElementVisible('body', 5000)
 	},
-	'Smoke test': browser => {
+	'Login': browser => {
 		browser
 			.assert.title('Login')
 			.assert.visible('#login_email', 'Check if login box is visible')
+			.setValue("#login_email", "Administrator")
+			.setValue("#login_password", "admin")
+			.click(".btn-login")
+			.waitForElementVisible("#body_div", 15000)
+			.assert.title('Frappe Desk')
 	},
-	after: browser => browser.end(),
+	after: browser => {
+		browser.end();
+	},
 };
