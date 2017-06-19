19736308b67d83d67b5b013ef0aa0a8a5aa49c43
Rushabh Mehta <rmehta@gmail.com>
2017-06-19 12:24:27 +0530

    [minor] fix meta.py

--- frappe/model/meta.py
+++ frappe/model/meta.py
@@ -492,7 +492,7 @@ def trim_tables(doctype=None):
 def clear_cache(doctype=None):
 	cache = frappe.cache()
 
-	if doctype in frappe.local.meta_cache:
+	if getattr(frappe.local, 'meta_cache') and (doctype in frappe.local.meta_cache):
 		del frappe.local.meta_cache[doctype]
 
 	for key in ('is_table', 'doctype_modules'):
