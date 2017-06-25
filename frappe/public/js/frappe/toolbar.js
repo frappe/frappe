d96b20be0ab844695803498750f6398a6c6828a9
Alex Ford <alexf@semmle.com>
2017-06-25 13:26:43 +0100

    Remove some variable declarations for which the assigned value is never read

--- frappe/public/js/frappe/toolbar.js
+++ frappe/public/js/frappe/toolbar.js
@@ -3,8 +3,6 @@
 
 $(document).on("toolbar_setup", function() {
 	var help_links = [];
-	var support_link = "#upgrade";
-	var chat_link = "#upgrade";
 	var limits = frappe.boot.limits;
 
 	if(frappe.boot.expiry_message) {
@@ -24,7 +22,7 @@ $(document).on("toolbar_setup", function() {
 	}
 
 	if(limits.support_email) {
-		support_link = 'mailto:'+frappe.boot.limits.support_email;
+		var support_link = 'mailto:'+frappe.boot.limits.support_email;
 		help_links.push('<li><a href="'+support_link+'">' + frappe._('Email Support') + '</a></li>');
 	}
 
