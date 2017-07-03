c32dbae116e0d2a1d36d5f1d799f956350a3e03c
Rushabh Mehta <rmehta@gmail.com>
2017-07-03 15:34:11 +0530

    [view] form dashboard behaves like a form section and is now collapsible

--- frappe/email/doctype/email_account/email_account.js
+++ frappe/email/doctype/email_account/email_account.js
@@ -119,7 +119,7 @@ frappe.ui.form.on("Email Account", {
 	},
 
 	show_gmail_message_for_less_secure_apps: function(frm) {
-		if(frm.doc.service==="Gmail") {
+		if(frm.doc.service==="GMail") {
 			frm.dashboard.set_headline_alert('Gmail will only work if you allow access for less secure \
 				apps in Gmail settings. <a target="_blank" \
 				href="https://support.google.com/accounts/answer/6010255?hl=en">Read this for details</a>');
