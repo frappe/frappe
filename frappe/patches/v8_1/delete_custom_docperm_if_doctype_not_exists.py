939717a1198b1f53ee70b86397e554578e886c1a
Nabin Hait <nabinhait@gmail.com>
2017-07-10 20:42:42 +0530

    Update delete_custom_docperm_if_doctype_not_exists.py

--- frappe/patches/v8_1/delete_custom_docperm_if_doctype_not_exists.py
+++ frappe/patches/v8_1/delete_custom_docperm_if_doctype_not_exists.py
@@ -1,8 +1,7 @@
 import frappe
 
 def execute():
-	frappe.db.sql("""delete from `tabCustom DocPerm` 
+	frappe.db.sql("""delete from `tabCustom DocPerm`
 		where parent not in ( select name from `tabDoctype` )
 			and parent not in ( select name from `tabCustom Doctype` )
-		
 	""")
