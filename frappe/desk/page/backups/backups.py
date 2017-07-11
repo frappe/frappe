f54aa094f2ae9449e3587cf3e697363025f17cc7
Saurabh <saurabh6790@gmail.com>
2017-07-11 12:26:10 +0530

    [fix] enqueue files backup job if job not in queue

--- frappe/desk/page/backups/backups.py
+++ frappe/desk/page/backups/backups.py
@@ -61,19 +61,22 @@ def delete_downloadable_backups():
 
 @frappe.whitelist()
 def schedule_files_backup(user_email):
-	from frappe.utils.background_jobs import enqueue
-	print(user_email)
-	enqueue("frappe.desk.page.backups.backups.backup_fiels_and_notify_user", queue='long', user_email=user_email)
-	frappe.msgprint(_("Queued backup job. It may take a few minutes. The system will update you once backup is ready"))
+	from frappe.utils.background_jobs import enqueue, get_jobs
+	queued_jobs = get_jobs(site=frappe.local.site, queue="long")
 
-def backup_fiels_and_notify_user(user_email=None):
+	if 'frappe.desk.page.backups.backups.backup_files_and_notify_user' not in queued_jobs[frappe.local.site]:
+		enqueue("frappe.desk.page.backups.backups.backup_files_and_notify_user", queue='long', user_email=user_email)
+		frappe.msgprint(_("Queued backup job. It may take a few minutes. The system will update you once files backup is ready"))
+	else:
+		frappe.msgprint(_("Backup job is already queued. The system will update you once files backup is ready"))
+
+def backup_files_and_notify_user(user_email=None):
 	from frappe.utils.backups import backup
 	backup_files = backup(with_files=True)
 	get_downlodable_links(backup_files)
 
 	subject = "File backup is ready"
 	message = frappe.render_template('frappe/templates/emails/file_backup_notification.html', backup_files, is_path=True)
-	print(message)
 	frappe.sendmail(recipients=[user_email], subject=subject, message=message)
 
 def get_downlodable_links(backup_files):
