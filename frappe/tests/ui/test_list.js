445bb3d2de0aa8f3aa6e197a2edf1f92a8864066
Rushabh Mehta <rmehta@gmail.com>
2017-07-13 14:36:37 +0530

    [test] add timeout

--- frappe/tests/ui/test_list.js
+++ frappe/tests/ui/test_list.js
@@ -32,11 +32,9 @@ QUnit.test("Test list values", function(assert) {
 		() => frappe.set_route('List', 'DocType'),
 		() => frappe.timeout(1),
 		() => {
-			{
-				assert.deepEqual(['List', 'DocType', 'List'], frappe.get_route());
-				assert.ok($('.list-item:visible').length > 10);
-				done();
-			}
+			assert.deepEqual(['List', 'DocType', 'List'], frappe.get_route());
+			assert.ok($('.list-item:visible').length > 10);
+			done();
 		}
 	]);
 });
