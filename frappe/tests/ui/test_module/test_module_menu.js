c91a44698e3b8daff7a78de6749f480cf63d9967
Utkarsh <utkyadav800@gmail.com>
2017-07-17 15:11:04 +0530

    minor changes

--- frappe/tests/ui/test_module/test_module_menu.js
+++ frappe/tests/ui/test_module/test_module_menu.js
@@ -4,21 +4,21 @@ QUnit.test("Test sidebar menu [Module view]", function(assert) {
 	assert.expect(2);
 	let done = assert.async();
 	let sidebar_opt = '.module-link:not(".active")';
-	let random;
+	let random_num;
 	let module_name;
 
 	frappe.run_serially([
 		//testing click on module name in side bar
 		() => frappe.set_route(['modules']),
-		() => frappe.timeout(0.5),
+		() => frappe.timeout(1),
 		() => assert.deepEqual(['modules'], frappe.get_route(), "Module view opened successfully."),
 		() => {
 			//randomly choosing one module (not active)
 			var count = $(sidebar_opt).length;
-			random = Math.floor(Math.random() * (count) + 1);
-			module_name = $(sidebar_opt)[random].innerText;
+			random_num = Math.floor(Math.random() * (count) + 1);
+			module_name = $(sidebar_opt)[random_num].innerText;
 		},
-		() => frappe.tests.click_and_wait(sidebar_opt, random),
+		() => frappe.tests.click_and_wait(sidebar_opt, random_num),
 		() => assert.equal($('.title-text:visible')[0].innerText, module_name, "Module opened successfully using sidebar"),
 		() => done()
 	]);
