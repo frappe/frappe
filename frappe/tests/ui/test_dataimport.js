d963224926de2ad7e78ad24e2519a2e52dba74ef
Utkarsh Goswami <goswamiutkarsh0@gmail.com>
2017-07-18 15:24:02 +0530

    Updated test for Query report and Data Export tool

--- frappe/tests/ui/test_dataimport.js
+++ frappe/tests/ui/test_dataimport.js
@@ -2,7 +2,7 @@
 
 QUnit.test("test data-export-tool", function(assert) {
 	assert.expect(1);
-	var f=0;
+	var f = 0;
 	let done = assert.async();
 	frappe.run_serially([
 		() => frappe.set_route('data-import-tool'),
@@ -11,16 +11,16 @@ QUnit.test("test data-export-tool", function(assert) {
 		() => $('.btn-select-mandatory').click(),
 		() => frappe.timeout(2),
 		//To count the number of mandatory fields of a doctype
-		() => { for (var i = 0; i < frappe.get_meta("Task").fields.length; i++) 
-		{	
-			if(frappe.get_meta("Task").fields[i].reqd == 1)
-				f++;
-		}
-	},
+		() => {
+			var fields = frappe.get_meta("Task").fields;
+			f = fields.filter(f => f.reqd).length
+		},
 		() => $('.btn-download-data').click(),
 		() => frappe.timeout(3),
 		//To determine if the number of fields checked are mandatory
-		() => { assert.ok($('input.select-column-check:checked').length == f); },
+		() => {
+			assert.ok($('input.select-column-check:checked').length == f);
+		},
 		() => done()
 	]);
 });
