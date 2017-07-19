7fea37a2d89187bc5f3cd50af5de33f80715130b
Manas Solanki <manassolanki@gmail.com>
2017-07-19 15:49:07 +0530

    add the parsed email id of the email group member

---
+++ frappe/patches/v8_1/delete_email_group_member_with_invalid_emails.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2017, Frappe and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.utils import validate_email_add
+
+def execute():
+	''' update/delete the email group member with the wrong email '''
+
+	email_group_members = frappe.get_all("Email Group Member", fields=["name", "email"])
+	for member in email_group_members:
+		validated_email = validate_email_add(member.email)
+		if (validated_email==member.email):
+			pass
+		else:
+			try:
+				doc = frappe.get_doc("Email Group Member", member.name)
+				doc.email = validated_email
+				doc.save()
+			except Exception, e:
+				frappe.delete_doc(doctype="Email Group Member", name=member.name, force=1, ignore_permissions=True)
