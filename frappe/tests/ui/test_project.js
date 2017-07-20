b1b6fe860100cf1dbeddad5182041807ff2d762f
Utkarsh Goswami <goswamiutkarsh0@gmail.com>
2017-07-20 16:14:25 +0530

    Test for Project module

---
+++ frappe/tests/ui/test_project.js
@@ -0,0 +1,52 @@
+QUnit.test("test project", function(assert) {
+	assert.expect(0);
+	let done = assert.async();
+	frappe.run_serially([
+		() => {
+			return frappe.tests.make('Project', [{
+				project_name: 'Test App'
+			}, {
+				expected_start_date: '2017-07-22'
+			}, {
+				expected_start_date: '2017-09-22'
+			}, {
+				estimated_costing: '10,000.00'
+			}, {
+				cost_center: 'Main-E'
+			}]);
+		},
+		() => {
+			frappe.timeout(2);
+				return frappe.tests.make('Task', [{
+					subject: 'Documentation'
+				}, {
+					project: 'Test App'
+				}, {
+					description: 'To make a proper documentation defining requirements etc'
+				}]);
+		},
+		() => {
+			frappe.timeout(2);
+				return frappe.tests.make('Activity Type', [{
+					activity_type: 'Planning'
+				}, {
+					billing_rate: '0.00'
+				}, {
+					costing_rate: '0.00'
+				}]);
+		},
+		() => {
+			frappe.timeout(2);
+				return frappe.tests.make('Activity Cost', [{
+					activity_type: 'Planning'
+				}, {
+					employee: 'EMP/0001'
+				}]);
+		},
+		() => {
+			//assert.ok(cur_frm.doc.subject=='Test App');
+			//assert.ok(cur_frm.doc.status=='Lead');
+		},
+		() => done()
+	]);
+});
