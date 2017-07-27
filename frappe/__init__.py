ac3e361ecf9ed31d2c96fc04e73ab13552cdc3de
mbauskar <mbauskar@gmail.com>
2017-07-27 12:47:20 +0600

    bumped to version 8.6.1

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -14,7 +14,7 @@ import os, sys, importlib, inspect, json
 from .exceptions import *
 from .utils.jinja import get_jenv, get_template, render_template, get_email_from_template
 
-__version__ = '8.6.0'
+__version__ = '8.6.1'
 __title__ = "Frappe Framework"
 
 local = Local()
