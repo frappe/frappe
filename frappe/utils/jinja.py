bab692ff599fcb3ab7bf8388f616b94f4f991652
Aditya Hase <aditya@adityahase.com>
2017-07-31 10:50:26 +0530

    Replaced mixed usage of tabs and spaces for indents with tabs only (#3813)
    Python 3 (PEP 8) strictly disallows mixed use of tabs and spaces
    for indentation
    frappe innstalltion only throws error on frappe/utils/jinja.py as
    of now but the rest are almost equally likely to fail, so changed
    all of them in one commit

--- frappe/utils/jinja.py
+++ frappe/utils/jinja.py
@@ -47,7 +47,7 @@ def validate_template(html):
 	try:
 		jenv.from_string(html)
 	except TemplateSyntaxError as e:
- 		frappe.msgprint('Line {}: {}'.format(e.lineno, e.message))
+		frappe.msgprint('Line {}: {}'.format(e.lineno, e.message))
 		frappe.throw(frappe._("Syntax error in template"))
 
 def render_template(template, context, is_path=None):
