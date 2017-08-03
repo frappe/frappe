4a014052aa556f8cd0a85941d750de1e45eda4a8
Faris Ansari <netchamp.faris@gmail.com>
2017-08-03 17:48:50 +0530

    Add test for event

--- frappe/desk/doctype/event/test_event.js
+++ frappe/desk/doctype/event/test_event.js
@@ -1,20 +1,34 @@
-// rename this file from _test_[name] to test_[name] to activate
-// and remove above this line
 
 QUnit.test("test: Event", function (assert) {
 	let done = assert.async();
 
 	// number of asserts
-	assert.expect(1);
+	assert.expect(4);
 
-	frappe.run_serially('Event', [
+	const subject = '_Test Event 1';
+	const datetime = frappe.datetime.now_datetime();
+	const hex = '#6be273';
+	const rgb = 'rgb(107, 226, 115)';
+
+	frappe.run_serially([
 		// insert a new Event
-		() => frappe.tests.make([
+		() => frappe.tests.make('Event', [
 			// values to be set
-			{key: 'value'}
+			{subject: subject},
+			{starts_on: datetime},
+			{color: hex}
 		]),
 		() => {
-			assert.equal(cur_frm.doc.key, 'value');
+			assert.equal(cur_frm.doc.subject, subject, 'Subject correctly set');
+			assert.equal(cur_frm.doc.starts_on, datetime, 'Date correctly set');
+			assert.equal(cur_frm.doc.color, hex, 'Color correctly set');
+		},
+		() => frappe.set_route('List', 'Event', 'Calendar'),
+		() => frappe.timeout(2),
+		() => {
+			const bg_color = $(`.result-list:visible .fc-day-grid-event:contains("${subject}")`)
+				.css('background-color');
+			assert.equal(bg_color, rgb, 'Event background color is set correctly');
 		},
 		() => done()
 	]);
