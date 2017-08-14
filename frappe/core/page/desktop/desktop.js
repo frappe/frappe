67668684ab8e32f0f8fd9de209437f66c127ab0e
Robert Kirschner <robertkirschner@gmail.com>
2017-08-14 13:12:30 +0200

    [minor] fix of translatable terms

--- frappe/core/page/desktop/desktop.js
+++ frappe/core/page/desktop/desktop.js
@@ -27,7 +27,7 @@ $.extend(frappe.desktop, {
 
 	render: function() {
 		var me = this;
-		frappe.utils.set_title("Desktop");
+		frappe.utils.set_title(__("Desktop"));
 
 		var template = frappe.list_desktop ? "desktop_list_view" : "desktop_icon_grid";
 
--- frappe/public/js/frappe/desk.js
+++ frappe/public/js/frappe/desk.js
@@ -92,9 +92,9 @@ frappe.Application = Class.extend({
 			var dialog = frappe.msgprint({
 				message:__("The application has been updated to a new version, please refresh this page"),
 				indicator: 'green',
-				title: 'Version Updated'
+				title: __('Version Updated')
 			});
-			dialog.set_primary_action("Refresh", function() {
+			dialog.set_primary_action(__("Refresh"), function() {
 				location.reload(true);
 			});
 			dialog.get_close_btn().toggle(false);
