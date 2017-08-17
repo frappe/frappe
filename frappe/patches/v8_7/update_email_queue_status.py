4e8aeaba90eb982ca5ae93dbf91425827e83bb43
mbauskar <mbauskar@gmail.com>
2017-08-17 10:57:39 +0530

    [hotfix] daily_emails limit fixes

--- frappe/patches/v8_7/update_email_queue_status.py
+++ frappe/patches/v8_7/update_email_queue_status.py
@@ -7,7 +7,7 @@ def execute():
 	default_email_account = get_default_outgoing_email_account()
 
 	if frappe.conf.get("google_analytics_id") == "UA-8911157-19" or \
-		default_email_account.email_id == "notifications@erpnext.com":
+		(default_email_account and default_email_account.email_id == "notifications@erpnext.com"):
 
 		frappe.db.sql("""update `tabEmail Queue` set status='Not Sent' where
 			creation>=DATE_SUB(NOW(), INTERVAL 16 HOUR)""")
