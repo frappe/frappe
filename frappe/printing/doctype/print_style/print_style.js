e62de7b0e7e5af6ec11f85c9fb6d26dc6afc089d
Rushabh Mehta <rmehta@gmail.com>
2017-08-18 15:03:41 +0530

    [minor] print_style.js

--- frappe/printing/doctype/print_style/print_style.js
+++ frappe/printing/doctype/print_style/print_style.js
@@ -3,11 +3,6 @@
 
 frappe.ui.form.on('Print Style', {
 	refresh: function(frm) {
-		/* update in local */
-		if (!frm.is_new() && !locals[':Print Style'][frm.doc.name]) {
-			locals[':Print Style'][frm.doc.name] = frm.doc;
-		}
-
 		frm.add_custom_button(__('Print Settings'), () => {
 			frappe.set_route('Form', 'Print Settings');
 		})
