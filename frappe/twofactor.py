db7ead143501369b7b1a049f6d325d8d263c66d8
Mohammed Eldadah <mrdadah@gmail.com>
2017-09-03 19:40:27 +0300

    Update twofactor.py
    check twofactors in system settings

--- frappe/twofactor.py
+++ frappe/twofactor.py
@@ -352,6 +352,8 @@ def delete_qrimage(user, check_expiry=False):
 
 def delete_all_barcodes_for_users():
 	'''Task to delete all barcodes for user.'''
+	if not two_factor_is_enabled():
+		return
 	users = frappe.get_all('User', {'enabled':1})
 	for user in users:
 		delete_qrimage(user.name, check_expiry=True)
