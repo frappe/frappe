82d91ba21e1b5fe2ce3517614082fea510527c60
Faris Ansari <netchamp.faris@gmail.com>
2017-09-12 16:27:43 +0530

    move run() from Mapping to Run

--- frappe/data_migration/doctype/data_migration_mapping/data_migration_mapping.py
+++ frappe/data_migration/doctype/data_migration_mapping/data_migration_mapping.py
@@ -8,58 +8,9 @@ from frappe.model.document import Document
 
 class DataMigrationMapping(Document):
 
-	def run_mapping(self, run, condition=None):
-		connection = run.get_connection()
-		start = run.current_mapping_start
-
-		if self.mapping_type == 'Push':
-			done = self.push(connection, start, condition)
-		elif self.mapping_type == 'Pull':
-			done = self.pull(connection, start, condition)
-
-		if done:
-			run.enqueue_next_mapping()
-		else:
-			run.enqueue_next_page(start = start + self.page_length)
-
-	def push(self, connection, start, condition):
-		data = self.get_data(start, condition)
-
-		failed_items = []
-		for d in data:
-			migration_id_value = d.get(self.migration_id_field)
-			response = connection.push(self.remote_objectname, self.get_mapped_record(d), migration_id_value)
-			if response.ok:
-				if not migration_id_value:
-					frappe.db.set_value(self.local_doctype, d.name, self.migration_id_field, response.doc['name'], update_modified=False)
-					frappe.db.commit()
-			else:
-				failed_items.append(response.doc)
-
-		if len(data) < self.page_length:
-			return frappe._dict(dict(
-				done=True,
-				failed_items=failed_items
-			))
-
-		return frappe._dict(dict(
-			done=False,
-			failed_items=failed_items
-		))
-
-	def get_data(self, start=0, condition=None, count=False):
-		filters = {}
+	def get_filters(self):
 		if self.condition:
-			filters = frappe.safe_eval(self.condition, dict(frappe=frappe))
-
-		if condition:
-			filters.update(condition)
-
-		if count:
-			return frappe.get_all(self.local_doctype, ['count(name) as total'], filters=filters)[0].total
-		else:
-			return frappe.get_all(self.local_doctype, fields=self.get_fields(), filters=filters, start=start,
-				page_length=self.page_length, debug=True)
+			return frappe.safe_eval(self.condition, dict(frappe=frappe))
 
 	def get_fields(self):
 		fields = []
@@ -87,12 +38,6 @@ class DataMigrationMapping(Document):
 			mapped[f.remote_fieldname] = value
 
 		return mapped
-
-	# def get_deleted_documents(self):
-	# 	frappe.get_all('Deleted Document', dict(
-	# 		deleted_doctype=self.local_doctype,
-	# 		modified=('>', )
-	# 	))
 
 	def pull(self, connection, start, page_length):
 		data = connection.get_objects(self.remote_objectname, self.condition, "*", start=start, page_length=page_length)
--- frappe/data_migration/doctype/data_migration_run/data_migration_run.py
+++ frappe/data_migration/doctype/data_migration_run/data_migration_run.py
@@ -8,12 +8,6 @@ from frappe.model.document import Document
 from frappe import _
 
 class DataMigrationRun(Document):
-	def get_connection(self):
-		if not hasattr(self, 'connection'):
-			self.connection = frappe.get_doc('Data Migration Connector',
-				self.data_migration_connector).get_connection()
-
-		return self.connection
 
 	def validate(self):
 		exists = frappe.db.exists('Data Migration Run', dict(
@@ -23,43 +17,45 @@ class DataMigrationRun(Document):
 			frappe.throw(_('There are failed runs with the same Data Migration Plan'))
 
 	def run(self):
-		no_of_mappings = len(frappe.get_doc('Data Migration Plan',
-			self.data_migration_plan).mappings)
-		self.db_set(dict(
-			status = 'Started',
-			current_mapping = None,
-			current_mapping_start = 0,
-			current_mapping_pages = 0,
-			no_of_mappings = no_of_mappings
-		), notify=True, commit=True)
+		print('--------------start----------------')
+		self.begin()
 		self.enqueue_next_mapping()
 
 	def enqueue_next_mapping(self):
+		print('--------------start next_mapping----------------')
 		next_mapping_name, percent_mappings_complete = self.get_next_mapping_and_percent_mappings_complete()
 		if next_mapping_name:
-			next_mapping = frappe.get_doc('Data Migration Mapping', next_mapping_name)
+			next_mapping = self.get_mapping(next_mapping_name)
 			self.db_set(dict(
 				current_mapping = next_mapping.name,
-				current_mapping_start = 0,
-				current_mapping_pages = float(next_mapping.get_data(count=True,
-					condition=self.get_last_modified_condition())) / next_mapping.page_length,
-				percent_complete = percent_mappings_complete
+				current_mapping_start = 0
 			), notify=True, commit=True)
 			frappe.enqueue_doc(self.doctype, self.name, 'run_current_mapping')
 		else:
 			self.complete()
 
 	def enqueue_next_page(self, start):
+		print('--------------start next_page----------------')
 		self.db_set(dict(
 			current_mapping_start = start,
-			percent_complete = self.percent_complete + (100.0 / self.no_of_mappings / self.current_mapping_pages)
+			percent_complete = self.percent_complete + (100.0 / self.total_pages)
 		), notify=True, commit=True)
 		frappe.enqueue_doc(self.doctype, self.name, 'run_current_mapping')
 
 	def run_current_mapping(self):
 		try:
-			mapping = frappe.get_doc('Data Migration Mapping', self.current_mapping)
-			mapping.run_mapping(self, condition=self.get_last_modified_condition())
+			mapping = self.get_mapping(self.current_mapping)
+
+			if mapping.mapping_type == 'Push':
+				done = self.push()
+			elif mapping.mapping_type == 'Pull':
+				done = self.pull()
+
+			if done:
+				self.enqueue_next_mapping()
+			else:
+				self.enqueue_next_page(start = self.current_mapping_start + mapping.page_length)
+
 		except Exception as e:
 			self.db_set('status', 'Error', notify=True, commit=True)
 			raise e
@@ -75,14 +71,41 @@ class DataMigrationRun(Document):
 			condition = None
 		return condition
 
+	def begin(self):
+		self.mappings = [frappe.get_doc(
+			'Data Migration Mapping', m.mapping) for m in self.get_plan().mappings]
+		total_pages = 0
+		for m in self.mappings:
+			page_count = float(self.get_count(m)) / m.page_length
+			total_pages += page_count
+		self.db_set(dict(
+			status = 'Started',
+			current_mapping = None,
+			current_mapping_start = 0,
+			current_mapping_pages = 0,
+			total_pages = total_pages
+		), notify=True, commit=True)
+
 	def complete(self):
 		self.db_set(dict(
 			status='Success',
 			percent_complete=100
 		), notify=True, commit=True)
 
+	def get_plan(self):
+		if not hasattr(self, 'plan'):
+			self.plan = frappe.get_doc('Data Migration Plan', self.data_migration_plan)
+		return self.plan
+
+	def get_mapping(self, mapping_name):
+		if hasattr(self, 'mappings'):
+			for m in self.mappings:
+				if m.name == mapping_name:
+					return m
+		return frappe.get_doc('Data Migration Mapping', mapping_name)
+
 	def get_next_mapping_and_percent_mappings_complete(self):
-		plan = frappe.get_doc('Data Migration Plan', self.data_migration_plan)
+		plan = self.get_plan()
 		if not self.current_mapping:
 			# first
 			return plan.mappings[0].mapping, 0
@@ -94,3 +117,62 @@ class DataMigrationRun(Document):
 				return plan.mappings[i+1].mapping, (i + 1) * 100 / len(plan.mappings)
 
 		raise frappe.ValidationError('Mapping Broken')
+
+	def get_data(self, mapping, start=0, condition=None):
+		filters = mapping.get_filters() or {}
+		if condition:
+			filters.update(condition)
+
+		return frappe.get_all(mapping.local_doctype, fields=mapping.get_fields(), filters=filters, start=start,
+			page_length=mapping.page_length, debug=True)
+
+	def get_count(self, mapping):
+		filters = mapping.get_filters() or {}
+		return frappe.get_all(mapping.local_doctype, ['count(name) as total'], filters=filters)[0].total
+
+	def get_connection(self):
+		if not hasattr(self, 'connection'):
+			self.connection = frappe.get_doc('Data Migration Connector',
+				self.get_plan().connector).get_connection()
+
+		return self.connection
+
+	def push(self):
+		mapping = self.get_mapping(self.current_mapping)
+		start = self.current_mapping_start
+		condition = self.get_last_modified_condition()
+		connection = self.get_connection()
+
+		data = self.get_data(mapping, start, condition)
+
+		failed_items = json.loads(self.db_get('failed_log')) or []
+		for d in data:
+			migration_id_value = d.get(mapping.migration_id_field)
+			response = connection.push(mapping.remote_objectname, mapping.get_mapped_record(d), migration_id_value)
+			if response.ok:
+				if not migration_id_value:
+					frappe.db.set_value(mapping.local_doctype, d.name,
+						mapping.migration_id_field, response.doc['name'],
+						update_modified=False)
+					frappe.db.commit()
+			else:
+				failed_items.append(response.doc)
+
+		self.db_set('failed_log', json.dumps(failed_items))
+
+		if len(data) < mapping.page_length:
+			return True
+			# return frappe._dict(dict(
+			# 	done=True,
+			# 	failed_items=failed_items
+			# ))
+
+		return False
+
+		# return frappe._dict(dict(
+		# 	done=False,
+		# 	failed_items=failed_items
+		# ))
+
+	def pull(self):
+		pass
