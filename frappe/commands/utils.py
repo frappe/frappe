9416df270dc9f9920b8c9689be9537c482c9146c
Achilles Rasquinha <achillesrasquinha@gmail.com>
2017-09-26 15:44:04 +0530

    [BUG] fixed, changed to --restore, reset is a flag for test_runner

--- frappe/commands/utils.py
+++ frappe/commands/utils.py
@@ -7,14 +7,14 @@ from frappe.commands import pass_context, get_site
 
 @click.command('build')
 @click.option('--make-copy', is_flag=True, default=False, help='Copy the files instead of symlinking')
-@click.option('--reset', is_flag=True, default=False, help='Copy the files instead of symlinking with force')
+@click.option('--restore', is_flag=True, default=False, help='Copy the files instead of symlinking with force')
 @click.option('--verbose', is_flag=True, default=False, help='Verbose')
-def build(make_copy=False, reset = False, verbose=False):
+def build(make_copy=False, restore = False, verbose=False):
 	"Minify + concatenate JS and CSS files, build translations"
 	import frappe.build
 	import frappe
 	frappe.init('')
-	frappe.build.bundle(False, make_copy=make_copy, reset = reset, verbose=verbose)
+	frappe.build.bundle(False, make_copy=make_copy, restore = restore, verbose=verbose)
 
 @click.command('watch')
 def watch():
