bf33cf435335ba4ff22fe480d329091265dd37f4
Ben Cornwell-Mott <bcornwellmott@velometro.com>
2017-10-01 14:24:26 -0700

    Get rid of whitespace

--- frappe/public/js/frappe/upload.js
+++ frappe/public/js/frappe/upload.js
@@ -14,7 +14,7 @@ frappe.upload = {
 		opts.show_private = !("is_private" in opts);
 		
 		// make private by default
-		if (!("options" in opts) || ("options" in opts && 
+		if (!("options" in opts) || ("options" in opts &&
 			(!opts.options.toLowerCase()=="public" && !opts.options.toLowerCase()=="image"))) {
 			opts.is_private = 1;
 		}
