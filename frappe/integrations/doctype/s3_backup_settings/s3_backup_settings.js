98b036438e36d395d22cf4cca7f27d27f7ce186d
Rishabh Nambiar <rishabhn@protonmail.com>
2017-10-07 02:09:02 +0530

    Fixes for codacy PR review

--- frappe/integrations/doctype/s3_backup_settings/s3_backup_settings.js
+++ frappe/integrations/doctype/s3_backup_settings/s3_backup_settings.js
@@ -10,17 +10,17 @@ frappe.ui.form.on('S3 Backup Settings', {
 	take_backup: function(frm) {
 		if (frm.doc.access_key_id && frm.doc.secret_access_key) {
 			frm.add_custom_button(__("Take Backup Now"), function(){
-				frm.dashboard.set_headline_alert("S3 Backup Started!")
+				frm.dashboard.set_headline_alert("S3 Backup Started!");
 				frappe.call({
 					method: "frappe.integrations.doctype.s3_backup_settings.s3_backup_settings.take_backups_s3",
 					callback: function(r) {
 						if(!r.exc) {
 							frappe.msgprint(__("S3 Backup complete!"));
-							frm.dashboard.clear_headline()
+							frm.dashboard.clear_headline();
 						}
 					}
-				})
-			}).addClass("btn-primary")
+				});
+			}).addClass("btn-primary");
 		}
 	}
 });
--- frappe/integrations/doctype/s3_backup_settings/s3_backup_settings.py
+++ frappe/integrations/doctype/s3_backup_settings/s3_backup_settings.py
@@ -4,7 +4,6 @@
 
 from __future__ import unicode_literals
 import os
-import re
 import os.path
 import frappe
 from frappe import _
