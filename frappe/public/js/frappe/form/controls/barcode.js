d904311a1632367b68a9d10ce5f72f0565297b60
pratu16x7 <pratu16x7@gmail.com>
2017-10-08 21:33:59 +0530

    [wip]

--- frappe/public/js/frappe/form/controls/barcode.js
+++ frappe/public/js/frappe/form/controls/barcode.js
@@ -1,23 +1,21 @@
 frappe.ui.form.ControlBarcode = frappe.ui.form.ControlData.extend({
 	make_input() {
 		this._super();
-		this.setup_barcode();
 	},
 
 	make_wrapper() {
 		this._super();
 		this.$wrapper.find('.control-input').css({'display': 'flex'});
-		this.cam_buttom = $(`<button class="cam-button btn btn-sm btn-default text-muted" style="
-			margin-left: 5px;
-		"><i class="octicon octicon-device-camera"></i></button>`).appendTo(this.$wrapper.find('.control-input'));
+		// this.cam_buttom = $(`<button class="cam-button btn btn-sm btn-default text-muted" style="
+		// 	margin-left: 5px;
+		// "><i class="octicon octicon-device-camera"></i></button>`).appendTo(this.$wrapper.find('.control-input'));
 		this.barcode_area = $(`<div class="barcode-wrapper like-disabled-input"
 		"><svg id="barcode"></svg></div>`).appendTo(this.$wrapper.find('.control-input-wrapper'));
 	},
 
-	setup_barcode() {
-		this.$input.on('blur', () => {
-			this.render_barcode(this.$input.val(), 2);
-		});
+	set_input(value) {
+		this._super();
+		this.render_barcode(value, 2);
 	},
 
 	render_barcode(value, width) {
@@ -27,22 +25,23 @@ frappe.ui.form.ControlBarcode = frappe.ui.form.ControlData.extend({
 			width: width,
 			height: 40
 		});
+		this.barcode_svg = this.barcode_area.html();
 	},
 
-	setup_cam(){
-		this.setup_cam_dialog();
-		this.cam_buttom.on('click', () => {
-			this.cam_dialog.show();
-		});
-	},
+	// setup_cam(){
+	// 	this.setup_cam_dialog();
+	// 	this.cam_buttom.on('click', () => {
+	// 		this.cam_dialog.show();
+	// 	});
+	// },
 
-	setup_cam_dialog() {
-		this.cam_dialog = new frappe.ui.Dialog({
-			title: __(this.df.label || __("Capture"))
-		});
-	},
+	// setup_cam_dialog() {
+	// 	this.cam_dialog = new frappe.ui.Dialog({
+	// 		title: __(this.df.label || __("Capture"))
+	// 	});
+	// },
 
-	get_cam_data() {
-		//
-	}
+	// get_cam_data() {
+	// 	//
+	// }
 });
