04ca68f5c5866a934bba22df54f3152018ae5fde
pratu16x7 <pratu16x7@gmail.com>
2017-10-09 12:43:28 +0530

    [barcode] use set_value

--- frappe/public/js/frappe/form/controls/barcode.js
+++ frappe/public/js/frappe/form/controls/barcode.js
@@ -1,8 +1,4 @@
 frappe.ui.form.ControlBarcode = frappe.ui.form.ControlData.extend({
-	make_input() {
-		this._super();
-	},
-
 	make_wrapper() {
 		this._super();
 		this.$wrapper.find('.control-input').css({'display': 'flex'});
@@ -13,35 +9,21 @@ frappe.ui.form.ControlBarcode = frappe.ui.form.ControlData.extend({
 		"><svg id="barcode"></svg></div>`).appendTo(this.$wrapper.find('.control-input-wrapper'));
 	},
 
-	set_input(value) {
-		this._super();
-		this.render_barcode(value, 2);
+	validate(value) {
+		return this.render_barcode(value, 2);
 	},
 
 	render_barcode(value, width) {
+		this.numeric_val = value;
 		JsBarcode("#barcode", value, {
 			format: "CODE128",
 			background:'#f5f7fa',
 			width: width,
 			height: 40
 		});
-		this.barcode_svg = this.barcode_area.html();
-	},
+		let html = this.barcode_area.html();
+		this.set_value(html);
+		return html;
+	}
 
-	// setup_cam(){
-	// 	this.setup_cam_dialog();
-	// 	this.cam_buttom.on('click', () => {
-	// 		this.cam_dialog.show();
-	// 	});
-	// },
-
-	// setup_cam_dialog() {
-	// 	this.cam_dialog = new frappe.ui.Dialog({
-	// 		title: __(this.df.label || __("Capture"))
-	// 	});
-	// },
-
-	// get_cam_data() {
-	// 	//
-	// }
 });
