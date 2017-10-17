ce7c6a7b77ece48a09b3bf45cd287517edc3ab91
Revant Nandgaonkar <revant.one@gmail.com>
2017-10-17 15:58:07 +0530

    organized map control code

--- frappe/public/js/frappe/form/controls/map.js
+++ frappe/public/js/frappe/form/controls/map.js
@@ -1,101 +1,108 @@
 frappe.ui.form.ControlMap = frappe.ui.form.ControlData.extend({
+	loading: false,
 	make_wrapper() {
 		// Create the elements for barcode area
 		this._super();
 
 		let $input_wrapper = this.$wrapper.find('.control-input-wrapper');
 		this.map_area = $(
-			`<div class="map-wrapper border">
-				<div id="map-control" style="min-height: 400px; z-index: 1"></div>
-			</div>`
-		);
-		frappe.require([
-				"assets/frappe/js/lib/leaflet/leaflet.css",
-				"assets/frappe/js/lib/leaflet/leaflet.js",
-				"assets/frappe/js/lib/leaflet/leaflet.draw.css",
-				"assets/frappe/js/lib/leaflet/leaflet.draw.js",
-				"assets/frappe/js/lib/leaflet/L.Control.Locate.css",
-				"assets/frappe/js/lib/leaflet/L.Control.Locate.js"
-		], () => {
-			L.Icon.Default.imagePath = 'assets/frappe/images/leaflet';
-			this.map_area.prependTo($input_wrapper);
-			map = L.map('map-control').setView([19.0800, 72.8961], 13);
+				`<div class="map-wrapper border">
+					<div id="map-control" style="min-height: 400px; z-index: 1"></div>
+				</div>`
+			);
+		this.map_area.prependTo($input_wrapper);
+		L.Icon.Default.imagePath = 'assets/frappe/images/leaflet';
+		this.map = L.map('map-control').setView([19.0800, 72.8961], 13);
 
-			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-			}).addTo(map);
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(this.map);
 
-			var editableLayers = new L.FeatureGroup();
+		this.$wrapper.find('.control-input').addClass("hidden");
 
-			if((this.value != undefined) || (this.value != null)){
-				editableLayers = L.geoJson(JSON.parse(this.value)).addTo(map);
-			}
-
-			map.addLayer(editableLayers);
-			var options = {
-				position: 'topleft',
-				draw: {
-					polyline: {
-						shapeOptions: {
-							color: '#f357a1',
-							weight: 10
-						}
-					},
-					polygon: {
-						allowIntersection: false, // Restricts shapes to simple polygons
-						drawError: {
-							color: '#e1e100', // Color the shape will turn when intersects
-							message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
-						},
-						shapeOptions: {
-							color: '#f357a1'
-						}
-					},
-					circle: true,
-					rectangle: {
-						shapeOptions: {
-							clickable: false
-						}
+		if (this.editableLayers === undefined){
+			this.set_editable_layers();
+		}
+		var options = {
+			position: 'topleft',
+			draw: {
+				polyline: {
+					shapeOptions: {
+						color: '#f357a1',
+						weight: 10
 					}
 				},
-				edit: {
-					featureGroup: editableLayers, //REQUIRED!!
-					remove: true
+				polygon: {
+					allowIntersection: false, // Restricts shapes to simple polygons
+					drawError: {
+						color: '#e1e100', // Color the shape will turn when intersects
+						message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
+					},
+					shapeOptions: {
+						color: '#f357a1'
+					}
+				},
+				circle: true,
+				rectangle: {
+					shapeOptions: {
+						clickable: false
+					}
 				}
-			};
-			var drawControl = new L.Control.Draw(options);
-			map.addControl(drawControl);
+			},
+			edit: {
+				featureGroup: this.editableLayers, //REQUIRED!!
+				remove: true
+			}
+		};
+		var drawControl = new L.Control.Draw(options);
+		this.map.addControl(drawControl);
+		// create control and add to map
+		this.bind_draw_create();
+		this.bind_draw_delete();
+		this.bind_draw_edit();
+		var lc = L.control.locate().addTo(this.map);
 
-			map.on('draw:created', (e) => {
-				var type = e.layerType,
-				layer = e.layer;
-				if (type === 'marker') {
-					layer.bindPopup('A popup!');
-				}
-				editableLayers.addLayer(layer);
-				this.set_value(JSON.stringify(editableLayers.toGeoJSON()));
-			});
+		// request location update and set location, sets current geolocation on load
+		// lc.start();
+	},
 
-			map.on('draw:deleted', (e) => {
-				var type = e.layerType,
-				layer = e.layer;
-				editableLayers.removeLayer(layer);
-				this.set_value(JSON.stringify(editableLayers.toGeoJSON()));
-			});
-
-			map.on('draw:edited', (e) => {
-				var type = e.layerType,
-				layer = e.layer;
-				editableLayers.removeLayer(layer);
-				this.set_value(JSON.stringify(editableLayers.toGeoJSON()));
-			});
-
-			// create control and add to map
-			var lc = L.control.locate().addTo(map);
-
-			// request location update and set location
-			lc.start();
-			this.$wrapper.find('.control-input').addClass("hidden");
+	bind_draw_create(){
+		this.map.on('draw:created', (e) => {
+			var type = e.layerType,
+			layer = e.layer;
+			if (type === 'marker') {
+				layer.bindPopup('Marker');
+			}
+			this.editableLayers.addLayer(layer);
+			this.set_value(JSON.stringify(this.editableLayers.toGeoJSON()));
 		});
+	},
+
+	bind_draw_delete(){
+		this.map.on('draw:deleted', (e) => {
+			var type = e.layerType,
+			layer = e.layer;
+			this.editableLayers.removeLayer(layer);
+			this.set_value(JSON.stringify(this.editableLayers.toGeoJSON()));
+		});
+	},
+
+	bind_draw_edit(){
+		this.map.on('draw:edited', (e) => {
+			var type = e.layerType,
+			layer = e.layer;
+			this.editableLayers.removeLayer(layer);
+			this.set_value(JSON.stringify(this.editableLayers.toGeoJSON()));
+		});
+	},
+
+	format_for_input(value) {
+		if(value){
+			this.editableLayers = L.geoJson(JSON.parse(value)).addTo(this.map);
+		}
+	},
+
+	set_editable_layers(){
+		this.editableLayers = new L.FeatureGroup();
 	}
 });
