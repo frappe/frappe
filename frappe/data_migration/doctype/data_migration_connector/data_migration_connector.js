7a29af8161b952a335ef15a0593ed429dc25636b
Faris Ansari <netchamp.faris@gmail.com>
2017-10-20 14:18:12 +0530

    Data Migration Connector
    - Create new connection with boilerplate
    - Add Run button in Data Migration Plan

--- frappe/data_migration/doctype/data_migration_connector/data_migration_connector.js
+++ frappe/data_migration/doctype/data_migration_connector/data_migration_connector.js
@@ -4,5 +4,43 @@
 frappe.ui.form.on('Data Migration Connector', {
 	refresh: function() {
 
+	},
+	new_connection(frm) {
+		const d = new frappe.ui.Dialog({
+			title: __('New Connection'),
+			fields: [
+				{ label: __('Module'), fieldtype: 'Link', options: 'Module Def', reqd: 1 },
+				{ label: __('Connection Name'), fieldtype: 'Data', description: 'For e.g: Shopify Connection', reqd: 1 },
+			],
+			primary_action_label: __('Create'),
+			primary_action: (values) => {
+				let { module, connection_name } = values;
+
+				frm.events.create_new_connection(frm, module, connection_name)
+					.then(r => {
+						if (r.message) {
+							const connector_name = connection_name
+								.replace('connection', 'Connector')
+								.replace('Connection', 'Connector')
+								.trim();
+
+							frm.set_value('connector_name', connector_name);
+							frm.set_value('is_custom', 1)
+							frm.set_value('python_module', r.message);
+							frm.save();
+							frappe.show_alert(__(`New module created ${r.message}`));
+							d.hide();
+						}
+					});
+			}
+		});
+
+		d.show();
+
+	},
+	create_new_connection(frm, module, connection_name) {
+		return frm.call('create_new_connection', {
+			module, connection_name
+		});
 	}
 });
--- frappe/data_migration/doctype/data_migration_connector/data_migration_connector.py
+++ frappe/data_migration/doctype/data_migration_connector/data_migration_connector.py
@@ -3,9 +3,10 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, os
 from frappe.model.document import Document
 from frappe import _
+from frappe.modules.export_file import create_init_py
 from .connectors.base import BaseConnection
 from .connectors.postgres import PostGresConnection
 from .connectors.frappe_connection import FrappeConnection
@@ -33,6 +34,33 @@ class DataMigrationConnector(Document):
 
 		return self.connection
 
+	def create_new_connection(self, module, connection_name):
+		if not frappe.conf.get('developer_mode'):
+			frappe.msgprint(_('Please enable developer mode to create new connection'))
+			return
+		# create folder
+		module_path = frappe.get_module_path(module)
+		connectors_folder = os.path.join(module_path, 'connectors')
+		frappe.create_folder(connectors_folder)
+
+		# create init py
+		create_init_py(module_path, 'connectors', '')
+
+		connection_class = connection_name.replace(' ', '')
+		file_name = frappe.scrub(connection_name) + '.py'
+		file_path = os.path.join(module_path, 'connectors', file_name)
+
+		# create boilerplate file
+		with open(file_path, 'w') as f:
+			f.write(connection_boilerplate.format(connection_class=connection_class))
+
+		# get python module string from file_path
+		app_name = frappe.db.get_value('Module Def', module, 'app_name')
+		python_module = os.path.relpath(
+			file_path, '../apps/{0}'.format(app_name)).replace(os.path.sep, '.')[:-3]
+
+		return python_module
+
 def get_connection_class(python_module):
 	filename = python_module.rsplit('.', 1)[-1]
 	classname = frappe.unscrub(filename).replace(' ', '')
@@ -50,3 +78,27 @@ def get_connection_class(python_module):
 		raise ImportError(filename)
 
 	return _class
+
+connection_boilerplate = """from __future__ import unicode_literals
+from frappe.data_migration.doctype.data_migration_connector.connectors.base import BaseConnection
+
+class {connection_class}(BaseConnection):
+	def __init__(self, connector):
+		# self.connector = connector
+		# self.connection = YourModule(self.connector.username, self.get_password())
+		# self.name_field = 'id'
+		pass
+
+	def get(self, remote_objectname, fields=None, filters=None, start=0, page_length=10):
+		pass
+
+	def insert(self, doctype, doc):
+		pass
+
+	def update(self, doctype, doc, migration_id):
+		pass
+
+	def delete(self, doctype, migration_id):
+		pass
+
+"""
