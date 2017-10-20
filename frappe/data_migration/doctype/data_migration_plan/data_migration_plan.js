3997988d0c91d731596443ff036f64ba190c8fcf
Faris Ansari <netchamp.faris@gmail.com>
2017-10-20 14:18:12 +0530

    Data Migration Connector
    - Create new connection with boilerplate
    - Add Run button in Data Migration Plan

--- frappe/data_migration/doctype/data_migration_plan/data_migration_plan.js
+++ frappe/data_migration/doctype/data_migration_plan/data_migration_plan.js
@@ -2,6 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Data Migration Plan', {
+	onload(frm) {
+		frm.add_custom_button(__('Run'), () => frappe.new_doc('Data Migration Run', {
+			data_migration_plan: frm.doc.name
+		}))
+	},
 	refresh: function() {
 
 	}
