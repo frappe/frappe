24b147282f56260ca3374384425d54928eb755b4
ci2014 <christoph.isik@wpci.de>
2017-10-29 23:59:14 +0100

    Update hooks.py

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -153,7 +153,8 @@ scheduler_events = {
 		"frappe.utils.scheduler.restrict_scheduler_events_if_dormant",
 		"frappe.email.doctype.auto_email_report.auto_email_report.send_daily",
 		"frappe.core.doctype.feedback_request.feedback_request.delete_feedback_request",
-		"frappe.core.doctype.authentication_log.authentication_log.clear_authentication_logs"
+		"frappe.core.doctype.authentication_log.authentication_log.clear_authentication_logs",
+		"frappe.desk.doctype.todo.todo.update_overdue"
 	],
 	"daily_long": [
 		"frappe.integrations.doctype.dropbox_settings.dropbox_settings.take_backups_daily",
