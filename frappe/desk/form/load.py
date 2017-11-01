72f20d1a75ee1a2b3af1641c52bb4cfba68a86db
ci2014 <christoph.isik@wpci.de>
2017-11-01 02:52:44 +0100

    Update load.py
    Put overdue as extra status

--- frappe/desk/form/load.py
+++ frappe/desk/form/load.py
@@ -177,7 +177,7 @@ def get_communication_data(doctype, name, start=0, limit=20, after=None, fields=
 
 def get_assignments(dt, dn):
 	cl = frappe.db.sql("""select name, owner, description from `tabToDo`
-		where reference_type=%(doctype)s and reference_name=%(name)s and status="Open"
+		where reference_type=%(doctype)s and reference_name=%(name)s and status in ("Open","Overdue")
 		order by modified desc limit 5""", {
 			"doctype": dt,
 			"name": dn
