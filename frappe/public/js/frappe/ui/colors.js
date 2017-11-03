14fe247738ba3c0dec472b222ec715576e332885
Faris Ansari <netchampfaris@users.noreply.github.com>
2017-11-03 12:52:19 +0530

    Update colors.js

--- frappe/public/js/frappe/ui/colors.js
+++ frappe/public/js/frappe/ui/colors.js
@@ -16,8 +16,8 @@ frappe.ui.color_map = {
 	blue: ["#d2d2ff", "#a3a3ff", "#7575ff", "#4d4da8"],
 	purple: ["#dac7ff", "#b592ff", "#8e58ff", "#5e3aa8"],
 	pink: ["#f8d4f8", "#f3aaf0", "#ec7dea", "#934f92"],
-	white: ["", "", "#F7FAFC", ""],
-	black: ["", "", "#212a33", ""]
+	white: ["#d1d8dd", "#fafbfc", "#ffffff", ""],
+	black: ["#8D99A6", "#6c7680", "#36414c", "#212a33"]
 };
 
 frappe.ui.color = {
