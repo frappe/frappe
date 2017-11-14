c354c54065b3fffa40e2bf7a68e9c5311162d36b
Rushabh Mehta <rmehta@gmail.com>
2017-11-14 13:39:53 +0530

    [hot] revert domain_settings patch

--- frappe/patches/v9_1/revert_domain_settings.py
+++ frappe/patches/v9_1/revert_domain_settings.py
@@ -5,6 +5,6 @@ def execute():
 	active_domains = [d.domain for d in domain_settings.active_domains]
 
 	for domain_name in ('Education', 'Healthcare', 'Hospitality'):
-		if domain_name not in active_domains:
+		if frappe.db.exists('Domain', domain_name) and domain_name not in active_domains:
 			domain = frappe.get_doc('Domain', domain_name)
 			domain.remove_domain()
