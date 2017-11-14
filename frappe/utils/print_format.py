0db18fdf91ad1901635b8f0495ebff665ba96560
rohitwaghchaure <rohitw1991@gmail.com>
2017-11-14 13:43:33 +0530

    [Fix] Letterhead appearing on PDF even when not selected (#4484)

--- frappe/utils/print_format.py
+++ frappe/utils/print_format.py
@@ -42,8 +42,8 @@ def read_multi_pdf(output):
 	return filedata
 
 @frappe.whitelist()
-def download_pdf(doctype, name, format=None, doc=None):
-	html = frappe.get_print(doctype, name, format, doc=doc)
+def download_pdf(doctype, name, format=None, doc=None, no_letterhead=0):
+	html = frappe.get_print(doctype, name, format, doc=doc, no_letterhead=no_letterhead)
 	frappe.local.response.filename = "{name}.pdf".format(name=name.replace(" ", "-").replace("/", "-"))
 	frappe.local.response.filecontent = get_pdf(html)
 	frappe.local.response.type = "download"
