1e9743d7b8c4f8ada723ccdb7db6ef573b0bff3d
Juan Cruz Gardey <juan.gardey@ubykuo.com>
2017-11-16 20:34:44 -0300

    - modified list view to show 6 columns

--- frappe/public/js/frappe/list/list_renderer.js
+++ frappe/public/js/frappe/list/list_renderer.js
@@ -239,8 +239,8 @@ frappe.views.ListRenderer = Class.extend({
 		// Remove color field
 		this.columns = this.columns.filter(col => col.fieldtype !== 'Color');
 
-		// Limit number of columns to 5
-		this.columns = this.columns.slice(0, 5);
+		// Limit number of columns to 6
+		this.columns = this.columns.slice(0, 6);
 	},
 	add_column: function (df) {
 		// field width
