1851fb4a7bc7be9d6ccc8cb0f3630f6bd205eb9a
Saurabh <saurabh6790@gmail.com>
2017-11-17 19:07:14 +0530

    [fix] patch sequence fixes

--- frappe/core/doctype/doctype/doctype.py
+++ frappe/core/doctype/doctype/doctype.py
@@ -767,7 +767,8 @@ def validate_permissions(doctype, for_remove=False):
 def make_module_and_roles(doc, perm_fieldname="permissions"):
 	"""Make `Module Def` and `Role` records if already not made. Called while installing."""
 	try:
-		if doc.restrict_to_domain and not frappe.db.exists('Domain', doc.restrict_to_domain):
+		if hasattr(doc,'restrict_to_domain') and doc.restrict_to_domain and \
+			not frappe.db.exists('Domain', doc.restrict_to_domain):
 			frappe.get_doc(dict(doctype='Domain', domain=doc.restrict_to_domain)).insert()
 
 		if not frappe.db.exists("Module Def", doc.module):
