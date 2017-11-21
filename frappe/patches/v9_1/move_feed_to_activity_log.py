f10b3ebf865dde1968f3cf22220e6f2c55bfd541
Shreya <shreyashah115@gmail.com>
2017-11-21 16:47:04 +0530

    added try catch block in patch

--- frappe/patches/v9_1/move_feed_to_activity_log.py
+++ frappe/patches/v9_1/move_feed_to_activity_log.py
@@ -8,18 +8,22 @@ def execute():
 	activity_log_fields = frappe.get_meta('Activity Log').fields
 
 	for d in communication_data:
-		communication_doc = frappe.get_doc('Communication', d)
+		try:
+			communication_doc = frappe.get_doc('Communication', d)
 
-		activity_data = {'doctype': 'Activity Log'}
-		for field in activity_log_fields:
-			if communication_doc.get(field.fieldname):
-				activity_data[field.fieldname] = communication_doc.get_value(field.fieldname)
+			activity_data = {'doctype': 'Activity Log'}
+			for field in activity_log_fields:
+				if communication_doc.get(field.fieldname):
+					activity_data[field.fieldname] = communication_doc.get_value(field.fieldname)
 
-		activity_doc = frappe.get_doc(activity_data)
-		activity_doc.insert()
+			activity_doc = frappe.get_doc(activity_data)
+			activity_doc.insert()
 
-		frappe.db.sql("""update `tabActivity Log` set creation = %s,\
-			modified = %s where name = %s""", (communication_doc.creation,communication_doc.modified,activity_doc.name))
+			frappe.db.sql("""update `tabActivity Log` set creation = %s,\
+				modified = %s where name = %s""", (communication_doc.creation,communication_doc.modified,activity_doc.name))
 
-		frappe.db.sql("""delete from `tabCommunication` where name='{0}'""".format(communication_doc.name))
+			frappe.db.sql("""delete from `tabCommunication` where name='{0}'""".format(communication_doc.name))
+
+		except frappe.LinkValidationError:
+			pass
 	frappe.delete_doc("DocType", "Authentication Log")
