9feb67027ca04a29f025b440fdc965a795df5f3a
Charles-Henri Decultot <chdecultot@dokos.io>
2017-12-09 14:23:29 +0000

    Tests moved to ERPNext Sales Invoice and Payment Entry

--- frappe/core/doctype/transactionlog/test_transactionlog.py
+++ frappe/core/doctype/transactionlog/test_transactionlog.py
@@ -12,29 +12,13 @@ import hashlib
 test_dependencies = ["Item"]
 
 class TestTransactionLog(unittest.TestCase):
-	def test_transaction_log_creation(self):
-
-		if cint(frappe.conf.get("log_transactions")) == 1:
-			s_invoice = create_sales_invoice()
-
-			si_transactionlog = frappe.get_all("TransactionLog",filters={'reference_doctype':s_invoice.doctype,'document_name':s_invoice.name})
-			self.assertTrue(si_transactionlog[0])
-
-			p_entry = get_payment_entry("Sales Invoice", s_invoice.name, bank_account="_Test Bank - _TC")
-			p_entry.paid_from = "Debtors - _TC"
-			p_entry.reference_no = "Test"
-			p_entry.reference_date = "2016-01-01"
-			p_entry.insert()
-			p_entry.submit()
-
-			pe_transactionlog = frappe.get_all("TransactionLog",filters={'reference_doctype':p_entry.doctype,'document_name':p_entry.name})
-			self.assertTrue(pe_transactionlog[0])
-
 	def test_chaining_hash(self):
 
 		if cint(frappe.conf.get("log_transactions")) == 1:
 			transactionlogs = frappe.get_all("TransactionLog",fields=["name", "row_index", "transaction_hash", "chaining_hash"])
-			sha = hashlib.sha256()
-			sha.update(str(transactionlogs[-2].transaction_hash) + str(transactionlogs[-1].chaining_hash))
 
-			self.assertTrue(sha.hexdigest()==transactionlogs[-2].chaining_hash)
+			if transactionlogs:
+				sha = hashlib.sha256()
+				sha.update(str(transactionlogs[-2].transaction_hash) + str(transactionlogs[-1].chaining_hash))
+
+				self.assertTrue(sha.hexdigest()==transactionlogs[-2].chaining_hash)
