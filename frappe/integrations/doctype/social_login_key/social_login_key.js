e694ad6174fe1cc076c0a4295078fdb8066faff1
Revant Nandgaonkar <revant.one@gmail.com>
2017-12-12 16:01:58 +0530

    [Fix] Social Login Key Form JS

--- frappe/integrations/doctype/social_login_key/social_login_key.js
+++ frappe/integrations/doctype/social_login_key/social_login_key.js
@@ -7,8 +7,8 @@ const fields = [
 ];
 
 frappe.ui.form.on('Social Login Key', {
-	refresh(frm) {
-		setup_fields(frm);
+	custom_base_url (frm) {
+		frm.trigger("setup_fields");
 	},
 
 	social_login_provider(frm) {
@@ -19,40 +19,47 @@ frappe.ui.form.on('Social Login Key', {
 				"args": {
 					"provider": frm.doc.social_login_provider
 				}
-			}).done((r)=>{
+			}).done((r) => {
 				const provider = r.message;
-				for(var field of Object.keys(frm.fields_dict)){
-					if (fields.includes(field)) {
-						frm.set_value(field, provider[field]);
-						frm.set_df_property(field, "read_only", 1);
+				for(var field of fields){
+					frm.set_value(field, provider[field]);
+					frm.set_df_property(field, "read_only", 1);
+					if (frm.doc.custom_base_url){
+						frm.toggle_enable("base_url", 1);
 					}
 				}
 			});
 		} else {
-			for(var field of Object.keys(frm.fields_dict)){
-				if (fields.includes(field)) {
-					frm.set_value(field, "");
-					frm.set_df_property(field, "read_only", 0);
-					setup_fields(frm);
-				}
-			}
+			frm.trigger("clear_fields");
+			frm.trigger("setup_fields");
 		}
 	},
 
-	custom_base_url(frm) {
-		setup_fields(frm);
-	}
-});
-
-function setup_fields(frm) {
-	for(var f of fields){
+	setup_fields(frm) {
+		// set custom_base_url to read only for "Custom" provider
 		if(frm.doc.social_login_provider == "Custom"){
 			frm.set_value("custom_base_url", 1);
 			frm.set_df_property("custom_base_url", "read_only", 1);
 		}
-		if(frm.doc.social_login_provider != "Custom")
-			frm.set_df_property(f, "read_only", 1);
-		if(frm.doc.custom_base_url)
+
+		// set fields to read only for providers from template
+		for(var f of fields){
+			if(frm.doc.social_login_provider != "Custom"){
+				frm.set_df_property(f, "read_only", 1);
+			}
+		}
+
+		// enable base_url for providers with custom_base_url
+		if(frm.doc.custom_base_url) {
 			frm.set_df_property("base_url", "read_only", 0);
+		}
+	},
+
+	clear_fields(frm) {
+		for(var field of fields){
+			frm.set_value(field, "");
+			frm.set_df_property(field, "read_only", 0);
+		}
 	}
-}
+
+});
