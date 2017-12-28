fee5b035396a8c569e9159ff655e24f404448992
Faris Ansari <netchamp.faris@gmail.com>
2017-12-28 15:53:01 +0530

    Include std_fields always

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -82,15 +82,14 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 		} else {
 			super.set_fields();
 		}
-		const option_fields = this.meta.fields
+		// dynamic fields in df meta
+		const dynamic_fields = this.meta.fields
 			.filter(df => df.default && df.default.includes('{'))
-			.map(df => {
-				const fieldname = this.get_dynamic_field_from_default(df.default);
-				return fieldname;
-			})
+			.map(df => this.get_dynamic_field_from_default(df.default))
 			.filter(fieldname => this.meta.fields.map(df => df.fieldname).includes(fieldname));
+		const std_fields = frappe.model.std_fields_list;
 
-		this._fields = this._fields.concat(option_fields);
+		this._fields = this._fields.concat(dynamic_fields, std_fields);
 	}
 
 	setup_page_head() {
