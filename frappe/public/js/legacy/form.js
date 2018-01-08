a9ad5bd47089ade531fc18158478925855674dcf
ci2014 <christoph.isik@wpci.de>
2018-01-08 02:22:47 +0100

    Update form.js

--- frappe/public/js/legacy/form.js
+++ frappe/public/js/legacy/form.js
@@ -178,6 +178,8 @@ _f.Frm.prototype.print_doc = function() {
 	this.print_preview.refresh_print_options().trigger("change");
 	this.page.set_view("print");
 	this.print_preview.set_user_lang();
+	this.print_preview.set_default_print_language();
+	this.print_preview.preview();
 };
 
 _f.Frm.prototype.hide_print = function() {
