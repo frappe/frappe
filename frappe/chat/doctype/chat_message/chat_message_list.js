c0d9122592b58925270fc4634e86bc34a4c7307d
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-01-18 17:01:39 +0530

    Fixed Codacy

--- frappe/chat/doctype/chat_message/chat_message_list.js
+++ frappe/chat/doctype/chat_message/chat_message_list.js
@@ -1,7 +1,7 @@
 frappe.listview_settings['Chat Message'] = {
 	filters: [
 		['Chat Message', 'user',  '==', frappe.session.user, true],
-        ['Chat Room',    'owner', '==', frappe.session.user, true],
-        ['Chat Room',    frappe.session.user, 'in', 'users', true]
+		['Chat Room',    'owner', '==', frappe.session.user, true],
+		['Chat Room',    frappe.session.user, 'in', 'users', true]
 	]
 };
