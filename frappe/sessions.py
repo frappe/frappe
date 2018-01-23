d5e60746ff4ba9a513a9c073e7a6e11dda8aebc6
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-01-23 14:24:29 +0530

    revert

--- frappe/sessions.py
+++ frappe/sessions.py
@@ -417,13 +417,8 @@ def get_expiry_period(device="desktop"):
 
 def get_geo_from_ip(ip_addr):
 	try:
-		from geolite2 import geolite2
-		reader = geolite2.reader()
-		geo    = reader.get(ip_addr)
-
-		return frappe._dict(geo)
-
-		geolite2.close()
+		from geoip import geolite2
+		return geolite2.lookup(ip_addr)
 	except ImportError:
 		return
 	except ValueError:
