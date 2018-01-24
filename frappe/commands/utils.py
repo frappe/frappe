8d43417a2992150b2b5cc54e7a6444247262bf4a
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-01-24 17:20:10 +0530

    Added mysql auto completion

--- frappe/commands/utils.py
+++ frappe/commands/utils.py
@@ -287,8 +287,13 @@ def mysql(context):
 	"Start Mariadb console for a site"
 	site = get_site(context)
 	frappe.init(site=site)
-	msq = find_executable('mysql')
-	os.execv(msq, [msq, '-u', frappe.conf.db_name, '-p'+frappe.conf.db_password, frappe.conf.db_name, '-h', frappe.conf.db_host or "localhost", "-A"])
+	mysql = find_executable('mycli')
+	args  = [mysql, '-u', frappe.conf.db_name, '-p'+frappe.conf.db_password, frappe.conf.db_name, '-h', frappe.conf.db_host or "localhost"]
+	if not mysql:
+		mysql = find_executable('mysql')
+		args.append("-A")
+
+	os.execv(mysql, args)
 
 @click.command('console')
 @pass_context
