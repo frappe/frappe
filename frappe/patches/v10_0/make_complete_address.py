eb5ee8fcd7c1cc2c293677971bbf53f6f51bd246
Saurabh <bhosalesaurabh8@gmail.com>
2018-01-25 12:21:51 +0530

    Multiple changes: [Mod] Made the complete_address field hidden [New] Updated patches.txt to set this field for already existing_addresses

---
+++ frappe/patches/v10_0/make_complete_address.py
@@ -0,0 +1,10 @@
+import frappe
+
+def execute():
+	# Fetch complete address in newly added field 'complete_address'
+	existing_addresses = frappe.get_all("Address")
+	for address in existing_addresses:
+		address_doc = frappe.get_doc("Address",address)
+		address_doc.make_complete_address()
+		address_doc.save()
+		frappe.db.commit()
