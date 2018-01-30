82e8d1e123e82f18da962788867152cc5d42715a
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-01-30 15:26:00 +0530

    debugging

--- frappe/utils/change_log.py
+++ frappe/utils/change_log.py
@@ -75,8 +75,9 @@ def get_change_log_for_app(app, from_version, to_version):
 
 @frappe.whitelist()
 def update_last_known_versions():
+	print(get_versions())
 	frappe.db.set_value("User", frappe.session.user, "last_known_versions",
-		json.dumps(get_versions(), ensure_ascii=True).encode('utf-8'), update_modified=False)
+		json.dumps(get_versions()), update_modified=False)
 
 @frappe.whitelist()
 def get_versions():
