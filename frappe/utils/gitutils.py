f277e51a10dd2ce84ae6f9452b495622e336e342
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-01-30 16:17:53 +0530

    Fixed branch reference

--- frappe/utils/gitutils.py
+++ frappe/utils/gitutils.py
@@ -5,8 +5,11 @@ import subprocess
 def get_app_branch(app):
 	'''Returns branch of an app'''
 	try:
-		return subprocess.check_output('cd ../apps/{0} && git rev-parse --abbrev-ref HEAD'.format(app),
-			shell=True).strip()
+		branch = subprocess.check_output('cd ../apps/{0} && git rev-parse --abbrev-ref HEAD'.format(app),
+			shell=True)
+		branch = branch.decode('utf-8')
+		branch = branch.strip()
+		return branch
 	except Exception:
 		return ''
 
