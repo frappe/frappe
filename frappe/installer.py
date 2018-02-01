d29d47dae9eb0f4d88dda6497121bac8291e1016
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-02-01 18:53:53 +0530

    Fixed Python 3 compats

--- frappe/installer.py
+++ frappe/installer.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # MIT License. See license.txt
+from __future__ import unicode_literals, print_function
 
 from builtins import input
 
-from __future__ import unicode_literals, print_function
 
 import os, json, sys, subprocess, shutil
 import frappe
