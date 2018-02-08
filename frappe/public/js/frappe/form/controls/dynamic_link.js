5c98f39df95b007b8ee8355239a0dbedfa7c16ff
Faris Ansari <netchamp.faris@gmail.com>
2018-02-08 15:52:49 +0530

    [hotfix] Disallow Single DocType for Dynamic Link

--- frappe/public/js/frappe/form/controls/dynamic_link.js
+++ frappe/public/js/frappe/form/controls/dynamic_link.js
@@ -1,11 +1,12 @@
 frappe.ui.form.ControlDynamicLink = frappe.ui.form.ControlLink.extend({
 	get_options: function() {
+		let options = '';
 		if(this.df.get_options) {
-			return this.df.get_options();
+			options = this.df.get_options();
 		}
 		if (this.docname==null && cur_dialog) {
 			//for dialog box
-			return cur_dialog.get_value(this.df.options);
+			options = cur_dialog.get_value(this.df.options);
 		}
 		if (!cur_frm) {
 			const selector = `input[data-fieldname="${this.df.options}"]`;
@@ -18,14 +19,15 @@ frappe.ui.form.ControlDynamicLink = frappe.ui.form.ControlLink.extend({
 				input = $(cur_page.page).find(selector);
 			}
 			if (input) {
-				return input.val();
+				options = input.val();
 			}
 		}
-		var options = frappe.model.get_value(this.df.parent, this.docname, this.df.options);
-		// if(!options) {
-		// 	frappe.msgprint(__("Please set {0} first",
-		// 		[frappe.meta.get_docfield(this.df.parent, this.df.options, this.docname).label]));
-		// }
+		options = frappe.model.get_value(this.df.parent, this.docname, this.df.options);
+
+		if (frappe.model.is_single(options)) {
+			frappe.throw(__(`${options.bold()} is not a valid DocType for Dynamic Link`));
+		}
+
 		return options;
 	},
 });
