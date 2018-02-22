83937380914ffa6cff47162c0774723cd28d0506
crossxcell99 <crossxcell99@gmail.com>
2018-02-22 15:36:24 +0100

    accomodate header variations

--- frappe/app.py
+++ frappe/app.py
@@ -123,7 +123,7 @@ def init_request(request):
 def make_form_dict(request):
 	import json
 
-	if request.content_type == 'application/json' and request.data:
+	if 'application/json' in (request.content_type or '') and request.data:
 		args = json.loads(request.data)
 	else:
 		args = request.form or request.args
