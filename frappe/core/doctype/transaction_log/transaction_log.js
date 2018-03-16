46eb9ec655c4ab3855a0f378483f875d9adb8226
Charles-Henri Decultot <chdecultot@dokos.io>
2018-03-16 09:16:03 +0000

    Transaction Log Report

--- frappe/core/doctype/transaction_log/transaction_log.js
+++ frappe/core/doctype/transaction_log/transaction_log.js
@@ -2,5 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Transaction Log', {
-
+	"formatter":function (row, cell, value, columnDef, dataContext, default_formatter) {
+        value = default_formatter(row, cell, value, columnDef, dataContext);
+       if (columnDef.id == "Chain Integrity" && dataContext["chain_integrity"] == "True") {
+            value = "<span style='color:green!important;'>" + value + "</span>";
+       }
+       if (columnDef.id == "Chain Integrity" && dataContext["chain_integrity"] == "False") {
+            value = "<span style='color:red!important;'>" + value + "</span>";
+       }
+       return value;
+    }
 });
