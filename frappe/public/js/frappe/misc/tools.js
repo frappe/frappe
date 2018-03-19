f00073f6fbcec9214dbd9a9284172953acede660
Faris Ansari <netchamp.faris@gmail.com>
2018-03-19 12:58:39 +0530

    Handle null values in CSV export

--- frappe/public/js/frappe/misc/tools.js
+++ frappe/public/js/frappe/misc/tools.js
@@ -68,7 +68,8 @@ frappe.tools.to_csv = function(data) {
 	var res = [];
 	$.each(data, function(i, row) {
 		row = $.map(row, function(col) {
-			return typeof(col)==="string" ? ('"' + $('<i>').html(col.replace(/"/g, '""')).text() + '"') : col;
+			if (col === null || col === undefined) col = '';
+			return typeof col === "string" ? ('"' + $('<i>').html(col.replace(/"/g, '""')).text() + '"') : col;
 		});
 		res.push(row.join(","));
 	});
