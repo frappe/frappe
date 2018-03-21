4cafe66a3a9b57b3114f1187967ef5f9e2729c85
Faris Ansari <netchampfaris@users.noreply.github.com>
2018-03-21 12:36:41 +0530

    Kanban Board Fixes (#5236)
    - fix frappe/erpnext#13153
    - Add card title field in list fields
    - Quick entry if atleast 1 mandatory field is Table

--- frappe/public/js/frappe/views/kanban/kanban_view.js
+++ frappe/public/js/frappe/views/kanban/kanban_view.js
@@ -27,6 +27,12 @@ frappe.views.KanbanView = class KanbanView extends frappe.views.ListView {
 		this.view_name = 'Kanban';
 		this.board_name = frappe.get_route()[3];
 		this.page_title = this.board_name;
+		this.card_meta = this.get_card_meta();
+	}
+
+	set_fields() {
+		super.set_fields();
+		this._add_field(this.card_meta.title_field);
 	}
 
 	show() {
@@ -48,11 +54,52 @@ frappe.views.KanbanView = class KanbanView extends frappe.views.ListView {
 			doctype: this.doctype,
 			board_name: board_name,
 			cards: this.data,
+			card_meta: this.card_meta,
 			wrapper: this.$result,
 			cur_list: this,
 			user_settings: this.view_user_settings
 		});
 		this.kanban.$kanban_board.trigger('after-refresh');
+	}
+
+	get_card_meta() {
+		var meta = frappe.get_meta(this.doctype);
+		var doc = frappe.model.get_new_doc(this.doctype);
+		var title_field = null;
+		var quick_entry = false;
+
+		if(this.meta.title_field) {
+			title_field = frappe.meta.get_field(this.doctype, this.meta.title_field);
+		}
+
+		this.meta.fields.forEach(function(df) {
+			const is_valid_field =
+				in_list(['Data', 'Text', 'Small Text', 'Text Editor'], df.fieldtype)
+					&& !df.hidden;
+
+			if (is_valid_field && !title_field) {
+				// can be mapped to textarea
+				title_field = df;
+			}
+		});
+
+		// quick entry
+		var mandatory = meta.fields.filter(function(df) {
+			return df.reqd && !doc[df.fieldname];
+		});
+
+		if(mandatory.some(df => df.fieldtype === 'Table') || mandatory.length > 1) {
+			quick_entry = true;
+		}
+
+		if(!title_field) {
+			title_field = frappe.meta.get_field(this.doctype, 'name');
+		}
+
+		return {
+			quick_entry: quick_entry,
+			title_field: title_field
+		};
 	}
 
 	get required_libs() {
