bc0a2acadff78416c514b59986b7c5abc493dd03
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-03-22 23:02:20 +0530

    fix timedelta conversions for pymysq

--- frappe/database.py
+++ frappe/database.py
@@ -17,6 +17,7 @@ from frappe import _
 from frappe.model.utils.link_count import flush_local_link_count
 from frappe.utils.background_jobs import execute_job, get_queue
 from frappe import as_unicode
+import six
 
 # imports - compatibility imports
 from six import (
