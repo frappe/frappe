6d5bc2f28b82825339591b90bac46141b705c21a
Charles-Henri Decultot <chdecultot@dokos.io>
2018-03-23 15:55:51 +0000

    Add test for account creation

--- frappe/integrations/doctype/gcalendar_account/test_gcalendar_account.py
+++ frappe/integrations/doctype/gcalendar_account/test_gcalendar_account.py
@@ -7,4 +7,13 @@ import frappe
 import unittest
 
 class TestGCalendarAccount(unittest.TestCase):
-	pass
+	def test_create_connector(self):
+		users = frappe.get_all("User")
+		doc = frappe.new_doc("GCalendar Account")
+		doc.enabled = 1
+		doc.user = users[0].name
+		doc.calendar_name = "Frappe Test"
+		self.assertTrue(frappe.db.exists('GCalendar Account', users[0].name))
+
+		connector_name = 'Calendar Connector-' + users[0].name
+		self.assertTrue(frappe.db.exists('Data Migration Connector', connector_name))
