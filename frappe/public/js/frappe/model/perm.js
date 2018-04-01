a8320d4bf04b480b9788040bc09ec286c145f91f
Suraj Shetty <surajshetty3416@gmail.com>
2018-04-01 21:42:58 +0530

    perm js refactor

--- frappe/public/js/frappe/model/perm.js
+++ frappe/public/js/frappe/model/perm.js
@@ -20,37 +20,33 @@ $.extend(frappe.perm, {
 
 	doctype_perm: {},
 
-	has_perm: function(doctype, permlevel, ptype, doc) {
+	has_perm: (doctype, permlevel, ptype, doc) => {
 		if (!permlevel) permlevel = 0;
 		if (!frappe.perm.doctype_perm[doctype]) {
 			frappe.perm.doctype_perm[doctype] = frappe.perm.get_perm(doctype);
 		}
 
-		var perms = frappe.perm.doctype_perm[doctype];
-		if (!perms)
-			return false;
+		let perms = frappe.perm.doctype_perm[doctype];
 
-		if (!perms[permlevel])
-			return false;
+		if (!perms || !perms[permlevel]) return false;
 
-		var perm = !!perms[permlevel][ptype];
+		let perm = !!perms[permlevel][ptype];
 
-		if(permlevel===0 && perm && doc) {
-			var docinfo = frappe.model.get_docinfo(doctype, doc.name);
-			if(docinfo && !docinfo.permissions[ptype])
+		if (permlevel === 0 && perm && doc) {
+			let docinfo = frappe.model.get_docinfo(doctype, doc.name);
+			if (docinfo && !docinfo.permissions[ptype])
 				perm = false;
 		}
 
 		return perm;
 	},
 
-	get_perm: function(doctype, doc) {
-		var perm = [{ read: 0 }];
+	get_perm: (doctype, doc) => {
+		let perm = [{ read: 0 }];
 
-		var meta = frappe.get_doc("DocType", doctype);
-		if (!meta) {
-			return perm;
-		}
+		let meta = frappe.get_doc("DocType", doctype);
+
+		if (!meta) return perm;
 
 		if (frappe.session.user === "Administrator" || frappe.user_roles.includes("Administrator")) {
 			perm[0].read = 1;
@@ -58,23 +54,23 @@ $.extend(frappe.perm, {
 
 		frappe.perm.build_role_permissions(perm, meta);
 
-		if(doc) {
+		if (doc) {
 			// apply user permissions via docinfo (which is processed server-side)
-			var docinfo = frappe.model.get_docinfo(doctype, doc.name);
-			if(docinfo && docinfo.permissions) {
+			let docinfo = frappe.model.get_docinfo(doctype, doc.name);
+			if (docinfo && docinfo.permissions) {
 				Object.keys(docinfo.permissions).forEach((ptype) => {
 					perm[0][ptype] = docinfo.permissions[ptype];
 				});
 			}
 
 			// if owner
-			if(!$.isEmptyObject(perm[0].if_owner)) {
-				if(doc.owner === frappe.session.user) {
+			if (!$.isEmptyObject(perm[0].if_owner)) {
+				if (doc.owner === frappe.session.user) {
 					$.extend(perm[0], perm[0].if_owner);
 				} else {
 					// not owner, remove permissions
 					$.each(perm[0].if_owner, (ptype, value) => {
-						if(perm[0].if_owner[ptype]) {
+						if (perm[0].if_owner[ptype]) {
 							perm[0][ptype] = 0;
 						}
 					});
@@ -82,19 +78,19 @@ $.extend(frappe.perm, {
 			}
 
 			// apply permissions from shared
-			if(docinfo && docinfo.shared) {
-				for(var i=0; i<docinfo.shared.length; i++) {
-					var s = docinfo.shared[i];
-					if(s.user === frappe.session.user) {
+			if (docinfo && docinfo.shared) {
+				for (let i = 0; i < docinfo.shared.length; i++) {
+					let s = docinfo.shared[i];
+					if (s.user === frappe.session.user) {
 						perm[0]["read"] = perm[0]["read"] || s.read;
 						perm[0]["write"] = perm[0]["write"] || s.write;
 						perm[0]["share"] = perm[0]["share"] || s.share;
 
-						if(s.read) {
+						if (s.read) {
 							// also give print, email permissions if read
 							// and these permissions exist at level [0]
-							perm[0].email = frappe.boot.user.can_email.indexOf(doctype)!==-1 ? 1 : 0;
-							perm[0].print = frappe.boot.user.can_print.indexOf(doctype)!==-1 ? 1 : 0;
+							perm[0].email = frappe.boot.user.can_email.indexOf(doctype) !== -1 ? 1 : 0;
+							perm[0].print = frappe.boot.user.can_print.indexOf(doctype) !== -1 ? 1 : 0;
 						}
 					}
 				}
@@ -102,7 +98,7 @@ $.extend(frappe.perm, {
 
 		}
 
-		if(frappe.model.can_read(doctype) && !perm[0].read) {
+		if (frappe.model.can_read(doctype) && !perm[0].read) {
 			// read via sharing
 			perm[0].read = 1;
 		}
@@ -113,50 +109,49 @@ $.extend(frappe.perm, {
 	build_role_permissions: (perm, meta) => {
 		// Returns a `dict` of evaluated Role Permissions
 		// Apply User Permission and its DocTypes are used to display match rules in list view
-
-		$.each(meta.permissions || [], function(i, p) {
+		$.each(meta.permissions || [], (i, p) => {
 			// if user has this role
-			if(frappe.user_roles.includes(p.role)) {
-				var permlevel = cint(p.permlevel);
-				if(!perm[permlevel]) {
+			if (frappe.user_roles.includes(p.role)) {
+				let permlevel = cint(p.permlevel);
+				if (!perm[permlevel]) {
 					perm[permlevel] = {};
 				}
 
-				$.each(frappe.perm.rights, function(i, key) {
+				$.each(frappe.perm.rights, (i, key) => {
 					perm[permlevel][key] = perm[permlevel][key] || (p[key] || 0);
 				});
 			}
 		});
 
 		// remove values with 0
-		$.each(perm[0], function(key, val) {
+		$.each(perm[0], (key, val) => {
 			if (!val) {
 				delete perm[0][key];
 			}
 		});
 
-		$.each(perm, function(i, v) {
-			if(v===undefined) {
+		$.each(perm, (i, v) => {
+			if (v === undefined) {
 				perm[i] = {};
 			}
 		});
 	},
 
-	get_match_rules: function(doctype, ptype) {
-		var me = this;
-		var match_rules = [];
+	get_match_rules: (doctype, ptype) => {
+		let me = this;
+		let match_rules = [];
 
 		if (!ptype) ptype = "read";
 
-		var perm = frappe.perm.get_perm(doctype);
+		let perm = frappe.perm.get_perm(doctype);
 
-		var user_permissions = frappe.defaults.get_user_permissions();
+		let user_permissions = frappe.defaults.get_user_permissions();
 
-		if(user_permissions && !$.isEmptyObject(user_permissions)) {
-			var rules = {};
-			var fields_to_check = frappe.meta.get_fields_to_check_permissions(doctype);
-			$.each(fields_to_check, function(i, df) {
-				if(user_permissions[df.options]) rules[df.label] = user_permissions[df.options];
+		if (user_permissions && !$.isEmptyObject(user_permissions)) {
+			let rules = {};
+			let fields_to_check = frappe.meta.get_fields_to_check_permissions(doctype);
+			$.each(fields_to_check, (i, df) => {
+				if (user_permissions[df.options]) rules[df.label] = user_permissions[df.options];
 			});
 			if (!$.isEmptyObject(rules)) {
 				match_rules.push(rules);
@@ -164,93 +159,93 @@ $.extend(frappe.perm, {
 		}
 
 		if (perm[0].if_owner && perm[0].read) {
-			match_rules.push({"Owner": frappe.session.user});
+			match_rules.push({ "Owner": frappe.session.user });
 		}
 		return match_rules;
 	},
 
-	get_field_display_status: function(df, doc, perm, explain) {
+	get_field_display_status: (df, doc, perm, explain) => {
 		// returns the display status of a particular field
 		// returns one of "Read", "Write" or "None"
-		if(!perm && doc) {
+		if (!perm && doc) {
 			perm = frappe.perm.get_perm(doc.doctype, doc);
 		}
 
-		if(!perm) {
-			return (df && (cint(df.hidden) || cint(df.hidden_due_to_dependency))) ? "None": "Write";
+		if (!perm) {
+			return (df && (cint(df.hidden) || cint(df.hidden_due_to_dependency))) ? "None" : "Write";
 		}
 
-		if(!df.permlevel) df.permlevel = 0;
-		var p = perm[df.permlevel];
-		var status = "None";
+		if (!df.permlevel) df.permlevel = 0;
+		let p = perm[df.permlevel];
+		let status = "None";
 
 		// permission
-		if(p) {
-			if(p.write && !df.disabled) {
+		if (p) {
+			if (p.write && !df.disabled) {
 				status = "Write";
-			} else if(p.read) {
+			} else if (p.read) {
 				status = "Read";
 			}
 		}
-		if(explain) console.log("By Permission:" + status);
+		if (explain) console.log("By Permission:" + status);
 
 		// hidden
-		if(cint(df.hidden)) status = "None";
-		if(explain) console.log("By Hidden:" + status);
+		if (cint(df.hidden)) status = "None";
+		if (explain) console.log("By Hidden:" + status);
 
 		// hidden due to dependency
-		if(cint(df.hidden_due_to_dependency)) status = "None";
-		if(explain) console.log("By Hidden Due To Dependency:" + status);
+		if (cint(df.hidden_due_to_dependency)) status = "None";
+		if (explain) console.log("By Hidden Due To Dependency:" + status);
 
-		if(!doc) {
+		if (!doc) {
 			return status;
 		}
 
 		// submit
-		if(status==="Write" && cint(doc.docstatus) > 0) status = "Read";
-		if(explain) console.log("By Submit:" + status);
+		if (status === "Write" && cint(doc.docstatus) > 0) status = "Read";
+		if (explain) console.log("By Submit:" + status);
 
 		// allow on submit
-		// var allow_on_submit = df.fieldtype==="Table" ? 0 : cint(df.allow_on_submit);
-		var allow_on_submit = cint(df.allow_on_submit);
-		if(status==="Read" && allow_on_submit && cint(doc.docstatus)===1 && p.write) {
+		// let allow_on_submit = df.fieldtype==="Table" ? 0 : cint(df.allow_on_submit);
+		let allow_on_submit = cint(df.allow_on_submit);
+		if (status === "Read" && allow_on_submit && cint(doc.docstatus) === 1 && p.write) {
 			status = "Write";
 		}
-		if(explain) console.log("By Allow on Submit:" + status);
+		if (explain) console.log("By Allow on Submit:" + status);
 
 		// workflow state
-		if(status==="Read" && cur_frm && cur_frm.state_fieldname) {
+		if (status === "Read" && cur_frm && cur_frm.state_fieldname) {
 			// fields updated by workflow must be read-only
-			if(cint(cur_frm.read_only) ||
+			if (cint(cur_frm.read_only) ||
 				in_list(cur_frm.states.update_fields, df.fieldname) ||
-				df.fieldname==cur_frm.state_fieldname) {
+				df.fieldname == cur_frm.state_fieldname) {
 				status = "Read";
 			}
 		}
-		if(explain) console.log("By Workflow:" + status);
+		if (explain) console.log("By Workflow:" + status);
 
 		// read only field is checked
-		if(status==="Write" && cint(df.read_only)) {
+		if (status === "Write" && cint(df.read_only)) {
 			status = "Read";
 		}
-		if(explain) console.log("By Read Only:" + status);
+		if (explain) console.log("By Read Only:" + status);
 
-		if(status==="Write" && df.set_only_once && !doc.__islocal) {
+		if (status === "Write" && df.set_only_once && !doc.__islocal) {
 			status = "Read";
 		}
-		if(explain) console.log("By Set Only Once:" + status);
+		if (explain) console.log("By Set Only Once:" + status);
 
 		return status;
 	},
 
-	is_visible: function(df, doc, perm) {
+	is_visible: (df, doc, perm) => {
 		if (typeof df === 'string') {
 			// df is fieldname
 			df = frappe.meta.get_docfield(doc.doctype, df, doc.parent || doc.name);
 		}
 
-		var status = frappe.perm.get_field_display_status(df, doc, perm);
+		let status = frappe.perm.get_field_display_status(df, doc, perm);
 
-		return status==="None" ? false : true;
+		return status === "None" ? false : true;
 	},
 });
