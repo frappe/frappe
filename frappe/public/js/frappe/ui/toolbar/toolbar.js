3b335388e59c994f1bee531fec616f04b6259c78
Faris Ansari <netchampfaris@users.noreply.github.com>
2018-04-06 16:38:03 +0530

    Fix In-App Documentation (#5398)
    * Fix In-App Documentation
    
    * remove unused var

--- frappe/public/js/frappe/ui/toolbar/toolbar.js
+++ frappe/public/js/frappe/ui/toolbar/toolbar.js
@@ -181,7 +181,6 @@ frappe.ui.toolbar.Toolbar = Class.extend({
 			if(href.indexOf('blob') > 0) {
 				window.open(href, '_blank');
 			}
-			var converter = new Showdown.converter();
 			var path = $(e.target).attr("data-path");
 			if(path) {
 				e.preventDefault();
