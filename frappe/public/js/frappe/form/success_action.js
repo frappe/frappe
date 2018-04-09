d1bf39add74c60b5d33916ce7abbe6bb7f845e3a
Suraj Shetty <surajshetty3416@gmail.com>
2018-04-09 11:10:49 +0530

    ui changes

--- frappe/public/js/frappe/form/success_action.js
+++ frappe/public/js/frappe/form/success_action.js
@@ -19,7 +19,7 @@ frappe.ui.form.SuccessAction = class SuccessAction {
 		}
 
 		const $buttons = this.config.actions.map(action => {
-			const $btn = $(`<button class="next-action-button"><i class="${action.icon}"></i> <span>${action.label}</span></button> `);
+			const $btn = $(`<button class="next-action-button text-muted"><i class="${action.icon}"></i> <span>${action.label}</span></button> `);
 			$btn.click(() => action.action(this.form));
 			return $btn;
 		});
@@ -27,7 +27,8 @@ frappe.ui.form.SuccessAction = class SuccessAction {
 		this.wrapper = $(`
 			<div class="success-action border">
 				<i class="success-icon fa fa-check-circle"></i>
-				<h2>${this.form.doctype}</h2>
+				<h2 class="success-message">Success</h2>
+				<p class="success-description">${this.config.message}</>
 				<div class="actions"></div>
 			</div>
 		`).appendTo(this.container);
@@ -35,12 +36,12 @@ frappe.ui.form.SuccessAction = class SuccessAction {
 		this.wrapper.find('.actions').append($buttons);
 		this.container.css('z-index', nav_z_index+1);
 		this.container.show().css('right', '20px');
-		// this.wrapper.delay(2 * 1000).fadeOut(800);
+		this.wrapper.delay(4 * 1000).fadeOut('slow');
 		// this.wrapper.hover((event) => {
 		// 	if (event.type === 'mouseenter') {
 		// 		this.wrapper.stop().fadeIn(100);
 		// 	} else {
-		// 		this.wrapper.stop().delay(2 * 1000).fadeOut(800);
+		// 		this.wrapper.stop().delay(2 * 1000).fadeOut('slow');
 		// 	}
 
 		// });
