4d1c2ec7b6063db32718ca64f9e2dfeab433ed5f
Tom Price <tom@codedinternet.com>
2018-04-09 12:43:19 +0100

    Fix code quality issue
    Trailing whitespace in patch SQL, was unoticable in editor.

--- frappe/patches/v10_0/migrate_passwords_passlib.py
+++ frappe/patches/v10_0/migrate_passwords_passlib.py
@@ -3,7 +3,7 @@ from frappe.utils.password import LegacyPassword
 
 
 def execute():
-	all_auths = frappe.db.sql("""SELECT `name`, `password`, `salt` FROM `__Auth` 
+	all_auths = frappe.db.sql("""SELECT `name`, `password`, `salt` FROM `__Auth`
 									WHERE doctype='User' AND `fieldname`='password'""",
 							  as_dict=True
 							  )
@@ -11,7 +11,7 @@ def execute():
 	for auth in all_auths:
 		if auth.salt and auth.salt != "":
 			pwd = LegacyPassword.hash(auth.password, salt=auth.salt.encode('UTF-8'))
-			frappe.db.sql("""UPDATE `__Auth` SET `password`=%(pwd)s, `salt`=NULL 
+			frappe.db.sql("""UPDATE `__Auth` SET `password`=%(pwd)s, `salt`=NULL
 								WHERE `doctype`='User' AND `fieldname`='password' AND `name`=%(user)s""",
 						  {'pwd': pwd, 'user': auth.name}
 						  )
