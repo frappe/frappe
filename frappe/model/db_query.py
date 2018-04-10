9e86fda7ca6dcee394056e046b0488f0167e54ce
Ammar Harara <ah1995@students.iugaza.edu.ps>
2018-04-10 15:02:20 +0300

    Revert to original to check

--- frappe/model/db_query.py
+++ frappe/model/db_query.py
@@ -200,17 +200,12 @@ class DatabaseQuery(object):
 
 		for field in self.fields:
 			if regex.match(field):
-				field_lower = field.lower()
-                tab_name = re.search(r'`.*?`', field_lower)
-                if tab_name:
-                    field_lower = field_lower.replace(tab_name.group(0),"")
-                    # remove table name from comparison (quoted name only with ` `)
-                if any(keyword in field_lower for keyword in blacklisted_keywords):
-                    _raise_exception()
+				if any(keyword in field.lower() for keyword in blacklisted_keywords):
+					_raise_exception()
 
-                if any("{0}(".format(keyword) in field_lower \
-                    for keyword in blacklisted_functions):
-                    _raise_exception()
+				if any("{0}(".format(keyword) in field.lower() \
+					for keyword in blacklisted_functions):
+					_raise_exception()
 
 	def extract_tables(self):
 		"""extract tables from fields"""
