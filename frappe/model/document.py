f395e31e3027b4d6db0bc18b4d9d28737dec26c8
Saurabh <saurabh6790@gmail.com>
2018-04-11 15:55:56 +0530

    check track changes attribute with getattr (#5435)

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -910,7 +910,7 @@ class Document(BaseDocument):
 
 		update_global_search(self)
 
-		if self.meta.track_changes and self._doc_before_save and not self.flags.ignore_version:
+		if getattr(self.meta, 'track_changes', False) and self._doc_before_save and not self.flags.ignore_version:
 			self.save_version()
 
 		if (self.doctype, self.name) in frappe.flags.currently_saving:
