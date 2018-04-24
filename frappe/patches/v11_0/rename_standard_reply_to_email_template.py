d3d1c0a483fabd5f003770aa24377348e44f6cbd
Nabin Hait <nabinhait@gmail.com>
2018-04-24 17:50:10 +0530

    Minor fix in reloading email template

--- frappe/patches/v11_0/rename_standard_reply_to_email_template.py
+++ frappe/patches/v11_0/rename_standard_reply_to_email_template.py
@@ -4,4 +4,4 @@ from frappe.model.rename_doc import rename_doc
 def execute():
 	if frappe.db.table_exists("Standard Reply") and not frappe.db.table_exists("Email Template"):
 		rename_doc('DocType', 'Standard Reply', 'Email Template')
-		frappe.reload_doc('setup', 'doctype', 'email_template')
+		frappe.reload_doc('email', 'doctype', 'email_template')
