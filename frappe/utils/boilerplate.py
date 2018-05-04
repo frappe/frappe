5cc28b12576f98a3bfcb88afe7261b120a3b7b68
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-05-04 19:05:48 +0530

    pip fix (#5527)

--- frappe/utils/boilerplate.py
+++ frappe/utils/boilerplate.py
@@ -267,11 +267,10 @@ def get_data():
 
 setup_template = """# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-try: # for pip >= 10
-	from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-	from pip.req import parse_requirements
 import re, ast
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in {app_name}/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -279,8 +278,6 @@ _version_re = re.compile(r'__version__\s+=\s+(.*)')
 with open('{app_name}/__init__.py', 'rb') as f:
 	version = str(ast.literal_eval(_version_re.search(
 		f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='{app_name}',
@@ -291,8 +288,7 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
 """
 
