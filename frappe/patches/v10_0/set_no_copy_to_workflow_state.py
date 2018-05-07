94204a7092c929f752bb7f96f81509bd6763f48e
Saurabh <saurabh6790@gmail.com>
2018-05-07 11:03:27 +0530

    [fix] check if custom field exists before loading doc

--- frappe/patches/v10_0/set_no_copy_to_workflow_state.py
+++ frappe/patches/v10_0/set_no_copy_to_workflow_state.py
@@ -1,10 +1,11 @@
 import frappe
-from frappe.custom.doctype.property_setter.property_setter import make_property_setter
 
 def execute():
 	for dt in frappe.get_all("Workflow", fields=['name', 'document_type', 'workflow_state_field']):
-		custom_field_name = "{0}-{1}".format(dt.document_type, dt.workflow_state_field)
+		fieldname = frappe.db.get_value("Custom Field", filters={'dt': dt.document_type,
+			'fieldname': dt.workflow_state_field})
 
-		custom_field = frappe.get_doc("Custom Field", custom_field_name)
-		custom_field.no_copy = 1
-		custom_field.save()
+		if fieldname:
+			custom_field = frappe.get_doc("Custom Field", fieldname)
+			custom_field.no_copy = 1
+			custom_field.save()
