9ab48384d174e3ec21c5962eae9be6597cf4a45d
Faris Ansari <netchamp.faris@gmail.com>
2018-05-14 12:43:28 +0530

    Quick Kanban Board
    - dont create project implicitly, results in Deadlock most of the time
    - add explicit field to set the project

--- frappe/public/js/frappe/list/list_sidebar.js
+++ frappe/public/js/frappe/list/list_sidebar.js
@@ -171,18 +171,33 @@ frappe.views.ListSidebar = Class.extend({
 				reqd: 1
 			}];
 
+<<<<<<< HEAD
 			if (select_fields.length > 0) {
+=======
+			if(me.doctype === 'Task') {
+				fields.push({
+					fieldtype: 'Link',
+					fieldname: 'project',
+					label: __('Project'),
+					options: 'Project'
+				})
+			}
+
+			if(select_fields.length > 0) {
+>>>>>>> Quick Kanban Board
 				fields = fields.concat([{
 					fieldtype: 'Select',
 					fieldname: 'field_name',
 					label: __('Columns based on'),
 					options: select_fields.map(df => df.label).join('\n'),
-					default: select_fields[0]
+					default: select_fields[0],
+					depends_on: 'eval:doc.custom_column===0'
 				},
 				{
 					fieldtype: 'Check',
 					fieldname: 'custom_column',
 					label: __('Custom Column'),
+<<<<<<< HEAD
 					default: 0,
 					onchange: function() {
 						var checked = d.get_value('custom_column');
@@ -201,6 +216,13 @@ frappe.views.ListSidebar = Class.extend({
 			}
 
 			if (['Note', 'ToDo'].includes(me.doctype)) {
+=======
+					default: 0
+				}]);
+			}
+
+			if(['Note', 'ToDo'].includes(me.doctype)) {
+>>>>>>> Quick Kanban Board
 				fields[0].description = __('This Kanban Board will be private');
 			}
 
@@ -216,15 +238,27 @@ frappe.views.ListSidebar = Class.extend({
 					if (custom_column) {
 						field_name = 'kanban_column';
 					} else {
+<<<<<<< HEAD
 						field_name =
+=======
+						if (!values.field_name) {
+							frappe.throw(__('Please select Columns Based On'))
+						}
+						var field_name =
+>>>>>>> Quick Kanban Board
 							select_fields
 								.find(df => df.label === values.field_name)
 								.fieldname;
 					}
 
 					me.add_custom_column_field(custom_column)
+<<<<<<< HEAD
 						.then(function() {
 							return me.make_kanban_board(values.board_name, field_name);
+=======
+						.then(function(custom_column) {
+							return me.make_kanban_board(values.board_name, field_name, values.project)
+>>>>>>> Quick Kanban Board
 						})
 						.then(function() {
 							d.hide();
@@ -258,14 +292,15 @@ frappe.views.ListSidebar = Class.extend({
 			});
 		});
 	},
-	make_kanban_board: function(board_name, field_name) {
+	make_kanban_board: function(board_name, field_name, project) {
 		var me = this;
 		return frappe.call({
 			method: 'frappe.desk.doctype.kanban_board.kanban_board.quick_kanban_board',
 			args: {
 				doctype: me.doctype,
-				board_name: board_name,
-				field_name: field_name
+				board_name,
+				field_name,
+				project
 			},
 			callback: function(r) {
 				var kb = r.message;
