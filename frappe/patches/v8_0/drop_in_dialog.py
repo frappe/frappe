76045cb7f150149a088609bca53f671c06369c84
Zlash65 <zarrar65@gmail.com>
2018-05-17 19:37:18 +0530

    refresh meta filter for DocType and DocField after column drop

--- frappe/patches/v8_0/drop_in_dialog.py
+++ frappe/patches/v8_0/drop_in_dialog.py
@@ -3,3 +3,4 @@ import frappe
 def execute():
 	if frappe.db.has_column('DocType', 'in_dialog'):
 		frappe.db.sql('alter table tabDocType drop column in_dialog')
+	frappe.clear_cache(doctype="DocType")
