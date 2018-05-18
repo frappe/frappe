ee9ace34a8ea8188109ab3d715fa0a0c24705f8e
Suraj Shetty <surajshetty3416@gmail.com>
2018-05-18 14:40:25 +0530

    Clear old workflow action on docstatus change
    - Add check for user permission before creating a workflow action record
    - Make doctype changes to support the Workflow Action  System

--- frappe/workflow/doctype/workflow_action/workflow_action.py
+++ frappe/workflow/doctype/workflow_action/workflow_action.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from frappe.core.page.permission_manager.permission_manager import get_users_with_role
+from frappe.permissions import has_permission
 
 class WorkflowAction(Document):
 	pass
@@ -15,41 +16,46 @@ def create_workflow_actions(doc, state):
 		filters=[['document_type', '=', doc.get('doctype')], ['is_active', '=', 1]]
 	)
 
+	if not workflow: return
+
 	reference_doctype = doc.doctype
 	reference_name = doc.name
 
-	if workflow:
+	workflow = frappe.get_doc('Workflow', workflow[0].name)
 
-		workflow = frappe.get_doc('Workflow', workflow[0].name)
+	doc_current_state = doc.get(workflow.workflow_state_field)
 
-		doc_current_state = doc.get(workflow.workflow_state_field)
+	immediate_next_possible_transitions = [d for d in workflow.transitions if d.state == doc_current_state]
 
-		immediate_next_possible_transitions = [d for d in workflow.transitions if d.state == doc_current_state]
+	# update status of completed workflow actions
+	frappe.db.sql('''update `tabWorkflow Action` set status='Completed', completed_by=%s
+		where reference_doctype=%s and reference_name=%s and next_workflow_state=%s''',
+		(frappe.session.user, reference_doctype, reference_name, doc_current_state), debug=True)
 
-		for transition in immediate_next_possible_transitions:
-			users = get_users_with_role(transition.allowed)
-			for user in users:
-				newdoc = frappe.get_doc({
-					'doctype': 'Workflow Action',
-					'reference_doctype': reference_doctype,
-					'reference_name': reference_name,
-					'workflow_state': transition.state,
-					'status': 'Open',
-					'user': user
-				})
-				newdoc.save()
+	# delete irrelevant workflow actions
+	frappe.db.sql('''delete from `tabWorkflow Action`
+		where reference_doctype=%s and reference_name=%s and next_workflow_state!=%s''',
+		(reference_doctype, reference_name, doc_current_state), debug=True)
 
-		# update status of old workflow actions
-		frappe.db.sql('''update `tabWorkflow Action` set status='Completed', completed_by=%s
-			where reference_doctype=%s and reference_name=%s and workflow_state=%s''',
-			(frappe.session.user, reference_doctype, reference_name, doc_current_state))
+	for transition in immediate_next_possible_transitions:
+		users = get_users_with_role(transition.allowed)
+		for user in users:
+			if not has_permission(doctype=doc, user=user): continue
+			newdoc = frappe.get_doc({
+				'doctype': 'Workflow Action',
+				'reference_doctype': reference_doctype,
+				'reference_name': reference_name,
+				'next_workflow_state': transition.next_state,
+				'status': 'Open',
+				'action': transition.action,
+				'user': user
+			})
+			newdoc.insert(ignore_permissions=True)
+			print(newdoc)
 
-		frappe.db.commit()
-
-	else: pass
-
-	if(doc.doctype == "Leave Application"):
-		frappe.throw('ERROR')
+	frappe.db.commit()
+	# print('--------------------------doc', doc, state, workflow, reference_doctype, reference_name)
+	# return
 
 def clear_workflow_actions(doc, state):
 	print('---------------clear', doc, state)
