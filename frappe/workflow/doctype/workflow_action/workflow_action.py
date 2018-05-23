894ba9a0b5d195d9d85a6b076a6f0e4b723b5bb5
Suraj Shetty <surajshetty3416@gmail.com>
2018-05-23 15:22:30 +0530

    Fix ui/ux for Workflow Action
    - Fix action url and improve response on action button click
    - Fix workflow action template style

--- frappe/workflow/doctype/workflow_action/workflow_action.py
+++ frappe/workflow/doctype/workflow_action/workflow_action.py
@@ -9,10 +9,11 @@ from frappe.utils.background_jobs import enqueue
 from frappe.utils import get_url
 from frappe.utils.verified_command import get_signed_params, verify_request
 from frappe import _
-from frappe.model.workflow import apply_workflow
+from frappe.model.workflow import apply_workflow, get_workflow_name
 
 class WorkflowAction(Document):
 	pass
+
 
 def get_permission_query_conditions(user):
 	if not user: user = frappe.session.user
@@ -29,18 +30,13 @@ def has_permission(doc, user):
 
 def create_workflow_actions(doc, state):
 
-	from frappe.permissions import has_permission
-
-	workflow = frappe.get_all("Workflow",
-		fields=['name'],
-		filters=[['document_type', '=', doc.get('doctype')], ['is_active', '=', 1]]
-	)
-	if not workflow: return
-
 	reference_doctype = doc.doctype
 	reference_name = doc.name
+	workflow = get_workflow_name(reference_doctype)
 
-	workflow = frappe.get_doc('Workflow', workflow[0].name)
+	if not workflow: return
+
+	workflow = frappe.get_doc('Workflow', workflow)
 
 	doc_current_state = doc.get(workflow.workflow_state_field)
 
@@ -67,11 +63,15 @@ def create_workflow_actions(doc, state):
 		where reference_doctype=%s and reference_name=%s and user!=%s''',
 		(reference_doctype, reference_name, current_user))
 
+
+	from frappe.permissions import has_permission
+
 	email_map = {}
+
 	for transition in immediate_next_possible_transitions:
 		users = get_users_with_role(transition.allowed)
 		for user in users:
-			if not has_permission(doctype=doc, user=user): continue
+			if not has_permission(doctype=doc, ptype='read', user=user): continue
 
 			if not email_map.get(user):
 				email_map[user] = {
@@ -81,7 +81,11 @@ def create_workflow_actions(doc, state):
 
 			email_map[user].get('possible_actions').append({
 				'action_name': transition.action,
-				'action_link': get_workflow_action_url(transition.action, reference_doctype, reference_name)
+				'action_link': get_workflow_action_url(
+					transition.action,
+					reference_doctype,
+					reference_name,
+					doc_current_state)
 			})
 
 	for user in email_map.keys():
@@ -96,10 +100,12 @@ def create_workflow_actions(doc, state):
 		newdoc.insert(ignore_permissions=True)
 
 	frappe.db.commit()
+	if email_map:
+		send_workflow_action_email(email_map, reference_doctype, reference_name)
 
-	send_workflow_action_email(email_map, reference_doctype, reference_name)
 
 def send_workflow_action_email(email_map, doctype, docname):
+	email_message = _('{0}:{1}'.format(doctype, docname))
 	common_args = {
 		'template': 'workflow_action',
 		'attachments': [frappe.attach_print(doctype, docname , file_name=docname)],
@@ -112,32 +118,44 @@ def send_workflow_action_email(email_map, doctype, docname):
 			'recipients': [d.get('email')],
 			'args': {
 				'actions': d.get('possible_actions'),
-				'message': _('New {0} has been created: {1}'.format(doctype, docname))
+				'message': _('{0} : {1}'.format(doctype, docname))
 			},
 		}
 		email_args.update(common_args)
 		enqueue(method=frappe.sendmail, queue='short', **email_args)
 
+
 @frappe.whitelist()
-def apply_action(action, doctype, docname):
+def apply_action(action, doctype, docname, current_state):
 	doc = frappe.get_doc(doctype, docname)
-	try:
-		apply_workflow(doc, action)
-		frappe.respond_as_web_page(_("Success"),
-			_("Done"),
-			indicator_color='green')
-	except Exception as e:
-		frappe.respond_as_web_page(_("Error"), e,
-			indicator_color='red')
+	workflow = get_workflow_name(doctype)
+	doc_workflow_state = doc.get(
+		frappe.get_value('Workflow', workflow, 'workflow_state_field')
+	)
+
+	if doc_workflow_state == current_state:
+		try:
+			apply_workflow(doc, action)
+			frappe.respond_as_web_page(_("Success"),
+				_("Done"),
+				indicator_color='green')
+		except Exception as e:
+			frappe.respond_as_web_page(_("Error"), e,
+				indicator_color='red')
+	else:
+		frappe.respond_as_web_page(_("Link Expired"),
+			_("Document {0} has been updated by {1}".format(docname, doc.get("modified_by"))),
+			indicator_color='blue')
 
 
-def get_workflow_action_url(action, doctype, docname):
+def get_workflow_action_url(action, doctype, docname, current_state):
 	apply_action_method = "/api/method/frappe.workflow.doctype.workflow_action.workflow_action.apply_action"
 
 	params = {
 		"doctype": doctype,
 		"docname": docname,
 		"action": action,
+		"current_state": current_state
 	}
 
 	return get_url(apply_action_method + "?" + get_signed_params(params))
