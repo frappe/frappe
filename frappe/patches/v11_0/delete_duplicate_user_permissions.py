120962e0d40b895e588659b886478cfd6f9a063c
Suraj Shetty <surajshetty3416@gmail.com>
2018-06-15 15:14:35 +0530

    Update code to make it less exotic :stuck_out_tongue:

--- frappe/patches/v11_0/delete_duplicate_user_permissions.py
+++ frappe/patches/v11_0/delete_duplicate_user_permissions.py
@@ -1,8 +1,12 @@
 import frappe
 
 def execute():
-	frappe.db.sql("""DELETE from `tabUser Permission` where name not in
-		(Select * from
-			(SELECT name FROM `tabUser Permission` GROUP BY allow, user, for_value)
-		as retain)
-	""") # https://stackoverflow.com/a/4685232
+	duplicateRecords = frappe.db.sql("""select count(name) as `count`, allow, user, for_value
+		from `tabUser Permission`
+		group by allow, user, for_value
+		having count(*) > 1 """, as_dict=1)
+
+	for record in duplicateRecords:
+		frappe.db.sql("""delete from `tabUser Permission`
+			where allow='{0}' and user='{1}' and for_value='{2}' limit {3}"""
+			.format(record.allow, record.user, record.for_value, record.count - 1))
