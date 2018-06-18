fe625c6bb76cb8dd7877ee6526751d9cc632dacf
shriharishastry <shriharishastrysmg@gmail.com>
2018-06-18 12:03:25 +0530

    changes on doctype naming

--- frappe/core/doctype/background_report_result/test_background_report_result.js
+++ frappe/core/doctype/background_report_result/test_background_report_result.js
@@ -2,15 +2,15 @@
 // rename this file from _test_[name] to test_[name] to activate
 // and remove above this line
 
-QUnit.test("test: Result", function (assert) {
+QUnit.test("test: Background Report Result", function (assert) {
 	let done = assert.async();
 
 	// number of asserts
 	assert.expect(1);
 
 	frappe.run_serially([
-		// insert a new Result
-		() => frappe.tests.make('Result', [
+		// insert a new Background Report Result
+		() => frappe.tests.make('Background Report Result', [
 			// values to be set
 			{key: 'value'}
 		]),
--- frappe/core/doctype/background_report_result/test_background_report_result.py
+++ frappe/core/doctype/background_report_result/test_background_report_result.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
-class TestResult(unittest.TestCase):
+class TestBackgroundReportResult(unittest.TestCase):
 	pass
