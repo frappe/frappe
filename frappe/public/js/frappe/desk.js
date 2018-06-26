9d57661c445fb8d5cc407d76ae28f44b9c4b05e1
sbco <mhbu50@gmail.com>
2018-06-26 21:21:22 +0300

    solved

--- frappe/public/js/frappe/desk.js
+++ frappe/public/js/frappe/desk.js
@@ -454,13 +454,10 @@ frappe.Application = Class.extend({
 		}
 	},
 
-	set_rtl: function() {
-		if (["ar", "he", "fa"].indexOf(frappe.boot.lang) >= 0) {
-			var ls = document.createElement('link');
-			ls.rel="stylesheet";
-			ls.href= "assets/css/frappe-rtl.css";
-			document.getElementsByTagName('head')[0].appendChild(ls);
-			$('body').addClass('frappe-rtl');
+	set_rtl: function () {
+		if (["ar", "he","fa"].indexOf(frappe.boot.lang) >= 0) {
+			$('body').addClass('frappe-rtl')
+
 		}
 	},
 
