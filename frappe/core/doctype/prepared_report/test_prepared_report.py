1800bf0cbe77de482134008096921186d8728ff7
shriharishastry <shrihari.s@zerodha.com>
2018-06-26 13:45:43 +0530

    test cases

--- frappe/core/doctype/prepared_report/test_prepared_report.py
+++ frappe/core/doctype/prepared_report/test_prepared_report.py
@@ -6,9 +6,6 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 import json
-import time
-
-from frappe.core.doctype.prepared_report.prepared_report import run_background
 
 
 class TestPreparedReport(unittest.TestCase):
@@ -35,14 +32,3 @@ class TestPreparedReport(unittest.TestCase):
 	def test_for_creation(self):
 		self.assertTrue('QUEUED' == self.prepared_report_doc.status.upper())
 		self.assertTrue(self.prepared_report_doc.report_start_time)
-
-	def test_for_completion(self):
-		run_background(self.prepared_report_doc)
-		time.sleep(5)
-		self.result = frappe.get_doc("Prepared Report", self.prepared_report_doc.name)
-		self.assertTrue('COMPLETED' == self.result.status.upper())
-		self.assertTrue(self.result.report_end_time)
-		self.assertGreater(
-			len(frappe.desk.form.load.get_attachments(
-				dt="Prepared Report", dn=self.result.name)), 0)
-
