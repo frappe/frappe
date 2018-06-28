d7d0491f5bf1bb907c95495f9131b3b436ddde92
Prateeksha Singh <pratu16x7@gmail.com>
2018-06-28 15:18:39 +0530

    [init] add show report in prepared report

--- frappe/core/doctype/prepared_report/prepared_report.js
+++ frappe/core/doctype/prepared_report/prepared_report.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Prepared Report', {
-	refresh: function() {
+	refresh: function(frm) {
+		frm.add_custom_button(__("Show Report"), function() {
+			return frm.call({
+				method: "frappe.core.doctype.prepared_report.prepared_report.get_report_attachment_data",
+				args: {
+					dn: frm.doc.name
+				},
+				callback: function(r) {
+					if(r.message) {
+						let data = r.message;
+						frappe.flags.prepared_report_data = data;
+
+						frappe.set_route("query-report", frm.doc.report_name);
+					}
+				}
+			});
+		});
 
 	}
 });
