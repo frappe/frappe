75c44c12a3c912d2833b944e05c03ed1df880795
Shridhar Patil <shridharpatil2792@gmail.com>
2018-07-05 20:14:49 +0530

    set frappe.local.form_dict after setting user

--- frappe/api.py
+++ frappe/api.py
@@ -177,6 +177,8 @@ def validate_api_key_secret(api_key, api_secret):
 		filters={"api_key": api_key},
 		fieldname=['name']
 	)
+	form_dict = frappe.local.form_dict
 	user_secret = frappe.utils.password.get_decrypted_password ("User", user, fieldname='api_secret')
 	if api_secret == user_secret:
 		frappe.set_user(user)
+		frappe.local.form_dict = form_dict
