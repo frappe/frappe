6f84e922f8e64627d90565d88b914c311de1fac5
rohitwaghchaure <rohitw1991@gmail.com>
2018-07-11 11:14:05 +0530

    [Fix] Brute force security (#5785)
    * [Fix] Brute force security
    
    * Added patch and change the error message
    
    * Added test case

--- frappe/core/doctype/activity_log/test_activity_log.py
+++ frappe/core/doctype/activity_log/test_activity_log.py
@@ -5,10 +5,11 @@ from __future__ import unicode_literals
 
 import frappe
 import unittest
+import time
+from frappe.auth import LoginManager, CookieManager
 
 class TestActivityLog(unittest.TestCase):
 	def test_activity_log(self):
-		from frappe.auth import LoginManager, CookieManager
 
 		# test user login log
 		frappe.local.form_dict = frappe._dict({
@@ -45,3 +46,43 @@ class TestActivityLog(unittest.TestCase):
 		name = names[0]
 		auth_log = frappe.get_doc('Activity Log', name)
 		return auth_log
+
+	def test_brute_security(self):
+		update_system_settings({
+			'allow_consecutive_login_attempts': 3,
+			'allow_login_after_fail': 5
+		})
+
+		frappe.local.form_dict = frappe._dict({
+			'cmd': 'login',
+			'sid': 'Guest',
+			'pwd': 'admin',
+			'usr': 'Administrator'
+		})
+
+		frappe.local.cookie_manager = CookieManager()
+		frappe.local.login_manager = LoginManager()
+
+		auth_log = self.get_auth_log()
+		self.assertEquals(auth_log.status, 'Success')
+
+		# test user logout log
+		frappe.local.login_manager.logout()
+		auth_log = self.get_auth_log(operation='Logout')
+		self.assertEquals(auth_log.status, 'Success')
+
+		# test invalid login
+		frappe.form_dict.update({ 'pwd': 'password' })
+		self.assertRaises(frappe.AuthenticationError, LoginManager)
+		self.assertRaises(frappe.AuthenticationError, LoginManager)
+		self.assertRaises(frappe.AuthenticationError, LoginManager)
+		self.assertRaises(frappe.SecurityException, LoginManager)
+		time.sleep(5)
+		self.assertRaises(frappe.AuthenticationError, LoginManager)
+
+		frappe.local.form_dict = frappe._dict()
+
+def update_system_settings(args):
+	doc = frappe.get_doc('System Settings')
+	doc.update(args)
+	doc.save()
