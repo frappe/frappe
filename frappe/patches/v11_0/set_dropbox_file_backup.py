6d131243d212eaf196cecf394f92142422ed1e92
Rushabh Mehta <rmehta@gmail.com>
2018-07-16 17:47:24 +0530

    Update set_dropbox_file_backup.py

--- frappe/patches/v11_0/set_dropbox_file_backup.py
+++ frappe/patches/v11_0/set_dropbox_file_backup.py
@@ -3,6 +3,7 @@ from frappe.utils import cint
 import frappe
 
 def execute():
+	frappe.reload_doctype("Dropbox Settings")
 	check_dropbox_enabled = cint(frappe.db.get_value("Dropbox Settings", None, "enabled"))
 	if  check_dropbox_enabled == 1:
 		frappe.db.set_value("Dropbox Settings", None, 'file_backup', 1)
