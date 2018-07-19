0cbe6497621ce2dce61640ceba54758c1a4eacbd
Fisher Yu <szufisher@gmail.com>
2018-07-19 22:01:31 +0800

    Update test_global_search.py
    added 2 test cases

--- frappe/tests/test_global_search.py
+++ frappe/tests/test_global_search.py
@@ -50,8 +50,12 @@ class TestGlobalSearch(unittest.TestCase):
 		self.assertTrue('After Mulder awakens from his coma, he realizes his duty to prevent alien colonization. ' in results[0].content)
 
 		results = global_search.search('extraterrestrial')
-		self.assertTrue('Carter explored themes of extraterrestrial involvement in ancient mass extinctions in this episode, the third in a trilogy.' in results[0].content)
-
+		self.assertTrue('Carter explored themes of extraterrestrial involvement in ancient mass extinctions in this episode, the third in a trilogy.' in results[0].content)                
+		results = global_search.search('awakens & duty & alien')
+		self.assertTrue('After Mulder awakens from his coma, he realizes his duty to prevent alien colonization. ' in results[0].content)
+		results = global_search.search('awakens & duty & otherterm')
+		self.assertTrue(!results[0].content)
+		
 	def test_update_doc(self):
 		self.insert_test_events()
 		test_subject = 'testing global search'
