a5774c1dba92e290faac95b50ad4a8f67450f550
Rushabh Mehta <rmehta@gmail.com>
2018-07-23 10:39:39 +0530

    Update user.js

--- frappe/core/doctype/user/user.js
+++ frappe/core/doctype/user/user.js
@@ -198,7 +198,7 @@ frappe.ui.form.on('User', {
 			},
 			callback: function(r){
 				if(r.message){
-					frappe.msgprint("Save api Secret " + r.message.api_secret);
+					frappe.msgprint(__("Save API Secret: ") + r.message.api_secret);
 				}
 			}
 		})
