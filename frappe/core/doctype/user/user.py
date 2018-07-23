abd3333baea8567152bd2f2efb3050dae1746486
Shridhar Patil <shridharpatil2792@gmail.com>
2018-07-23 11:06:29 +0530

    Token based authentication (#5752)
    * token based authentication
    
    * authentication
    
    Basic base64encode(api_key:api_secret)
    token api_key:api_secret
    
    * test added
    
    Validation: only user with system manager can generate the keys
    
    * codacy issues fixed
    
    * token based authentication
    
    * authentication
    
    Basic base64encode(api_key:api_secret)
    token api_key:api_secret
    
    * test added
    
    Validation: only user with system manager can generate the keys
    
    * codacy issues fixed
    
    * use frappe.safe_encode
    
    * base64 encode use frappe.safe_encode
    
    * set frappe.local.form_dict after setting user
    
    * removed test
    
    * removed unused imports
    
    * test for python 3
    
    * Update user.js
    
    * [user.py] throw correct error
    
    * Update user.py

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -1058,3 +1058,24 @@ def create_contact(user):
 			"phone": user.phone,
 			"mobile_no": user.mobile_no
 		}).insert(ignore_permissions=True)
+
+
+@frappe.whitelist()
+def generate_keys(user):
+	"""
+	generate api key and api secret
+
+	:param user: str
+	"""
+	if "System Manager" in frappe.get_roles():
+		user_details = frappe.get_doc("User", user)
+		api_secret = frappe.generate_hash(length=15)
+		# if api key is not set generate api key
+		if not user_details.api_key:
+			api_key = frappe.generate_hash(length=15)
+			user_details.api_key = api_key
+		user_details.api_secret = api_secret
+		user_details.save()
+
+		return {"api_secret": api_secret}
+	frappe.throw(frappe._("Not Permitted"), frappe.PermissionError)
--- frappe/tests/test_api.py
+++ frappe/tests/test_api.py
@@ -4,6 +4,11 @@ from __future__ import unicode_literals
 
 import unittest, frappe, os
 from frappe.utils import get_url
+from frappe.core.doctype.user.user import generate_keys
+
+import requests
+import base64
+
 
 class TestAPI(unittest.TestCase):
 	def test_insert_many(self):
@@ -25,3 +30,39 @@ class TestAPI(unittest.TestCase):
 		self.assertTrue(frappe.db.get_value('ToDo', {'description': 'Test API 1'}))
 		self.assertTrue(frappe.db.get_value('ToDo', {'description': 'Test API 2'}))
 		self.assertTrue(frappe.db.get_value('ToDo', {'description': 'Test API 3'}))
+
+	def test_auth_via_api_key_secret(self):
+
+		# generate api ke and api secret for administrator
+		keys = generate_keys("Administrator")
+		frappe.db.commit()
+		generated_secret = frappe.utils.password.get_decrypted_password(
+			"User", "Administrator", fieldname='api_secret'
+		)
+
+		api_key = frappe.db.get_value("User", "Administrator", "api_key")
+		header = {"Authorization": "token {}:{}".format(api_key, generated_secret)}
+		res = requests.post(frappe.get_site_config().host_name + "/api/method/frappe.auth.get_logged_user", headers=header)
+
+		self.assertEqual(res.status_code, 200)
+		self.assertEqual("Administrator", res.json()["message"])
+		self.assertEqual(keys['api_secret'], generated_secret)
+
+		header = {"Authorization": "Basic {}".format(base64.b64encode(frappe.safe_encode("{}:{}".format(api_key, generated_secret))).decode())}
+		res = requests.post(frappe.get_site_config().host_name + "/api/method/frappe.auth.get_logged_user", headers=header)
+		self.assertEqual(res.status_code, 200)
+		self.assertEqual("Administrator", res.json()["message"])
+
+		# Valid api key, invalid api secret
+		api_secret = "ksk&93nxoe3os"
+		header = {"Authorization": "token {}:{}".format(api_key, api_secret)}
+		res = requests.post(frappe.get_site_config().host_name + "/api/method/frappe.auth.get_logged_user", headers=header)
+		self.assertEqual(res.status_code, 403)
+
+
+		# random api key and api secret
+		api_key = "@3djdk3kld"
+		api_secret = "ksk&93nxoe3os"
+		header = {"Authorization": "token {}:{}".format(api_key, api_secret)}
+		res = requests.post(frappe.get_site_config().host_name + "/api/method/frappe.auth.get_logged_user", headers=header)
+		self.assertEqual(res.status_code, 401)
