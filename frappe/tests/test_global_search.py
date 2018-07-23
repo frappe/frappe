e0cfa416cb078e2374f69080e3d9d0193d6091fb
Fisher Yu <szufisher@gmail.com>
2018-07-24 00:19:40 +0800

    Update test_global_search.py

--- frappe/tests/test_global_search.py
+++ frappe/tests/test_global_search.py
@@ -50,7 +50,7 @@ class TestGlobalSearch(unittest.TestCase):
 		self.assertTrue('After Mulder awakens from his coma, he realizes his duty to prevent alien colonization. ' in results[0].content)
 
 		results = global_search.search('extraterrestrial')
-		self.assertTrue('Carter explored themes of extraterrestrial involvement in ancient mass extinctions in this episode, the third in a trilogy.' in results[0].content)                
+		self.assertTrue('Carter explored themes of extraterrestrial involvement in ancient mass extinctions in this episode, the third in a trilogy.' in results[0].content)
 		results = global_search.search('awakens & duty & alien')
 		self.assertTrue('After Mulder awakens from his coma, he realizes his duty to prevent alien colonization. ' in results[0].content)
 
