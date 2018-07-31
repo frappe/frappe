5a1451deaeab16660fddd685bf18af1ef38cfda9
pawan <pawan@erpnext.com>
2018-07-31 18:07:32 +0530

    [fix] #5899

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -154,6 +154,7 @@ scheduler_events = {
 		"frappe.desk.page.backups.backups.delete_downloadable_backups",
 		"frappe.limits.update_space_usage",
 		"frappe.desk.doctype.auto_repeat.auto_repeat.make_auto_repeat_entry",
+		"frappe.integrations.doctype.amazon_mws_settings.amazon_mws_settings.schedule_get_order_details"
 	],
 	"daily": [
 		"frappe.email.queue.clear_outbox",
