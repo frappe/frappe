3cd96a4d4d50385862fe26a2c45433c1e7ea21c5
Suraj Shetty <surajshetty3416@gmail.com>
2018-08-06 12:12:39 +0530

    Fix codacy

--- frappe/workflow/doctype/workflow/workflow.js
+++ frappe/workflow/doctype/workflow/workflow.js
@@ -10,10 +10,10 @@ frappe.ui.form.on("Workflow", {
 	document_type: function(frm) {
 		frm.events.update_field_options(frm);
 	},
-	update_field_options: function (frm) {
+	update_field_options: function(frm) {
 		var doc = frm.doc;
 		if (doc.document_type) {
-			const get_field_method = 'frappe.workflow.doctype.workflow.workflow.get_fieldnames_for'
+			const get_field_method = 'frappe.workflow.doctype.workflow.workflow.get_fieldnames_for';
 			frappe.xcall(get_field_method, { doctype: doc.document_type })
 				.then(resp => {
 					frappe.meta.get_docfield("Workflow Document State", "update_field", frm.doc.name).options = [""].concat(resp);
