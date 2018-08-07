e3382f8aec48121ff3e56d38b925e31307f48389
Saurabh <saurabh6790@gmail.com>
2018-08-07 13:12:53 +0600

    bumped to version 10.1.45

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -14,7 +14,7 @@ import os, sys, importlib, inspect, json
 from .exceptions import *
 from .utils.jinja import get_jenv, get_template, render_template, get_email_from_template
 
-__version__ = '10.1.44'
+__version__ = '10.1.45'
 __title__ = "Frappe Framework"
 
 local = Local()
