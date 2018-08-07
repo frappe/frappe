3772f0b457bb0cd2691bd1abbac23be79689f167
Tanay Shah <tanay.shah@elastic.run>
2018-08-07 17:59:58 +0530

    added address standardization feature

--- frappe/contacts/doctype/address/address.js
+++ frappe/contacts/doctype/address/address.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2016, Frappe Technologies and contributors
 // For license information, please see license.txt
 
+var address_fields = ["pincode","city", "county", "state"]
+
 frappe.ui.form.on("Address", {
 	refresh: function(frm) {
+
+		frm.filters_set_using = "state"
 		if(frm.doc.__islocal) {
 			var last_route = frappe.route_history.slice(-2, -1)[0];
 			let docname = last_route[2];
@@ -26,7 +30,21 @@ frappe.ui.form.on("Address", {
 			}
 		});
 		frm.refresh_field("links");
+
+		// add filters to link fields
+		address_fields.forEach(area_type => {
+			if (frm.fields_dict[area_type].df.fieldtype == "Link"){
+				frm.set_query(area_type, function() {
+				 return {
+						 "filters": {
+								 "administrative_area_type": area_type,
+						 }
+				 		};
+		 		});
+	 		}
+		});
 	},
+
 	validate: function(frm) {
 		// clear linked customer / supplier / sales partner on saving...
 		if(frm.doc.links) {
@@ -47,4 +65,55 @@ frappe.ui.form.on("Address", {
 			}
 		]);
 	}
+});
+
+/*
+On click each field modify filters for other fields
+for eg. on changing county first and state later state will not be able to affect filters
+of other fields but revrese is not true
+*/
+address_fields.forEach(type => {
+	frappe.ui.form.on('Address', type, function(frm,cdt,cnt){
+		var previous_filter_set_by = frm.filters_set_using
+		if (address_fields.indexOf(previous_filter_set_by) >= address_fields.indexOf(type)){
+			if (frm.doc[type]){
+				frm.filters_set_using = type
+				frappe.call({
+					method: "frappe.contacts.doctype.address.address.get_administrative_area_details",
+					args: {"administrative_area": frm.doc[type]},
+					callback: function(data){
+						if (data.message.status == 1){
+							address_fields.forEach(address_type => {
+								if (address_type == type){
+									//do nothing
+								}
+								else if (address_fields.indexOf(type) < address_fields.indexOf(address_type)) {
+									frm.set_query(address_type, function() {
+										 return {
+												 "filters": [
+													['Administrative Area','administrative_area_type',"=",address_type],
+													['Administrative Area','rgt',">",data.message.rgt],
+													['Administrative Area','lft',"<",data.message.lft],
+												 ]
+										 }
+								 	});
+								}
+								else {
+									frm.set_query(address_type, function() {
+							 				return {
+							 						"filters": [
+							 						 ['Administrative Area','administrative_area_type',"=",address_type],
+							 						 ['Administrative Area','lft',">",data.message.lft],
+							 						 ['Administrative Area','lft',"<",data.message.rgt],
+							 						]
+							 				}
+							 		});
+								}
+							});
+						}
+					}
+				});
+			}
+		}
+	});
 });
