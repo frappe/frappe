6fa67925a419ffa912f6e07dfe939512f11a0e5f
Suraj Shetty <surajshetty3416@gmail.com>
2018-08-16 15:40:16 +0530

    Remove TestBot

--- frappe/tests/test_bot.py
+++ frappe/tests/test_bot.py
@@ -6,29 +6,5 @@
 from __future__ import unicode_literals
 import unittest
 
-from frappe.utils.bot import BotReply
-
 class TestBot(unittest.TestCase):
-	def test_hello(self):
-		reply = BotReply().get_reply('hello')
-		self.assertEqual(reply, 'Hello Administrator')
-
-	def test_open_notifications(self):
-		reply = BotReply().get_reply('whatsup')
-		self.assertTrue('your attention' in reply)
-
-	def test_find(self):
-		reply = BotReply().get_reply('find user in doctypes')
-		self.assertTrue('[User](#Form/DocType/User)' in reply)
-
-	def test_not_found(self):
-		reply = BotReply().get_reply('find yoyo in doctypes')
-		self.assertTrue('Could not find' in reply)
-
-	def test_list(self):
-		reply = BotReply().get_reply('list users')
-		self.assertTrue('(#Form/User/test@example.com)' in reply)
-
-	def test_how_many(self):
-		reply = BotReply().get_reply('how many users')
-		self.assertTrue(int(reply) > 1)
+	pass
