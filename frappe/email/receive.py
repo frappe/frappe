7e30dded8477320e3889b0691ec8aa6259345491
Benji <ben@zhai.ch>
2018-08-21 19:20:03 +0800

    Update receive.py

--- frappe/email/receive.py
+++ frappe/email/receive.py
@@ -283,7 +283,7 @@ class EmailServer:
 		flags = []
 		for flag in imaplib.ParseFlags(flag_string) or []:
 			pattern = re.compile("\w+")
-			match = re.search(pattern, flag.decode('utf-8'))
+			match = re.search(pattern, frappe.safe_decode(flag))
 			flags.append(match.group(0))
 
 		if "Seen" in flags:
