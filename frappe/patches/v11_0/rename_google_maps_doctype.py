1b3c258eb8b831dbc5b94f0ab392b1c6aaa00657
Valmik Jangla <mail@valmik.in>
2018-08-24 04:08:59 -0700

    fixed patch to use correct check

--- frappe/patches/v11_0/rename_google_maps_doctype.py
+++ frappe/patches/v11_0/rename_google_maps_doctype.py
@@ -2,6 +2,6 @@ import frappe
 from frappe.model.rename_doc import rename_doc
 
 def execute():
-	if frappe.db.table_exists("Google Maps") and not frappe.db.table_exists("Google Maps Settings"):
+	if frappe.db.exists("DocType","Google Maps") and not frappe.db.exists("DocType","Google Maps Settings"):
 		rename_doc('DocType', 'Google Maps', 'Google Maps Settings')
 		frappe.reload_doc('integrations', 'doctype', 'google_maps_settings')
