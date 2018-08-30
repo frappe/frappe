2fdfa1d2af5c5a2b6cb823bd07f67962301481ad
Prateeksha Singh <pratu16x7@gmail.com>
2018-08-30 11:07:28 +0530

    [autoname] add test for format autoname

--- frappe/tests/test_naming.py
+++ frappe/tests/test_naming.py
@@ -4,10 +4,18 @@
 from __future__ import unicode_literals
 import unittest
 import frappe
+from frappe.utils import now_datetime
 
+from frappe.model.naming import getseries
 from frappe.model.naming import append_number_if_name_exists
 
 class TestNaming(unittest.TestCase):
+	def tearDown(self):
+		# Reset ToDo autoname to hash
+		todo_doctype = frappe.get_doc('DocType', 'ToDo')
+		todo_doctype.autoname = 'hash'
+		todo_doctype.save()
+
 	def test_append_number_if_name_exists(self):
 		'''
 		Append number to name based on existing values
@@ -26,3 +34,29 @@ class TestNaming(unittest.TestCase):
 
 		title2 = append_number_if_name_exists('Note', 'Test', 'title', '_')
 		self.assertEqual(title2, 'Test_1')
+
+	def test_format_autoname(self):
+		'''
+		Test if braced params are replaced in format autoname
+		'''
+		doctype = 'ToDo'
+
+		todo_doctype = frappe.get_doc('DocType', doctype)
+		todo_doctype.autoname = 'format:TODO-{MM}-{status}-{##}'
+		todo_doctype.save()
+
+		description = 'Format'
+
+		todo = frappe.new_doc(doctype)
+		todo.description = description
+		todo.insert()
+
+		series = getseries('', 2, doctype)
+
+		series = str(int(series)-1)
+
+		if len(series) < 2:
+			series = '0' + series
+
+		self.assertEqual(todo.name, 'TODO-{month}-{status}-{series}'.format(
+			month=now_datetime().strftime('%m'), status=todo.status, series=series))
