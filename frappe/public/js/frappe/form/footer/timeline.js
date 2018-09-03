f5a4b1328f8e084602e9035b945326691e47509d
Shreya <shreyashah115@gmail.com>
2018-09-03 16:01:59 +0530

    Show names in dropdown, but send mails to email set in user's master

--- frappe/public/js/frappe/form/footer/timeline.js
+++ frappe/public/js/frappe/form/footer/timeline.js
@@ -677,7 +677,7 @@ frappe.ui.form.Timeline = Class.extend({
 		var valid_users = Object.keys(frappe.boot.user_info)
 			.filter(user => !["Administrator", "Guest"].includes(user));
 
-		return valid_users.map(user => frappe.boot.user_info[user].email);
+		return valid_users.map(user => frappe.boot.user_info[user].name);
 	},
 
 	setup_comment_like: function() {
