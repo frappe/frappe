dbb7ecb7fea25b2c3246f1b22f32a7f94438d519
Saurabh <saurabh6790@gmail.com>
2018-09-07 13:23:17 +0530

    [fix] check for developer mode before adding port to URL

--- frappe/utils/data.py
+++ frappe/utils/data.py
@@ -728,7 +728,7 @@ def get_url(uri=None, full_address=False):
 
 	port = frappe.conf.http_port or frappe.conf.webserver_port
 
-	if host_name and not url_contains_port(host_name) and port:
+	if frappe.conf.developer_mode and host_name and not url_contains_port(host_name) and port:
 		host_name = host_name + ':' + str(port)
 
 	url = urljoin(host_name, uri) if uri else host_name
