b97b6308bcd6823f0a2a9a1730815828efd297f4
Himanshu Mishra <himanshu.mishra@elastic.run>
2018-09-14 12:16:49 +0530

    fix: check CSRF Token for PUT and DELETE methods as well

--- frappe/auth.py
+++ frappe/auth.py
@@ -64,7 +64,7 @@ class HTTPRequest:
 		check_session_stopped()
 
 	def validate_csrf_token(self):
-		if frappe.local.request and frappe.local.request.method=="POST":
+		if frappe.local.request and frappe.local.request.method in ("POST", "PUT", "DELETE"):
 			if not frappe.local.session: return
 			if not frappe.local.session.data.csrf_token \
 				or frappe.local.session.data.device=="mobile" \
