4203d03add9c96adf5d633959dc34783d14208d3
Suraj Shetty <surajshetty3416@gmail.com>
2018-10-03 16:17:33 +0530

    Init sidebars

--- frappe/public/js/frappe/social/components/PostSidebar.vue
+++ frappe/public/js/frappe/social/components/PostSidebar.vue
@@ -1,14 +1,40 @@
 <template>
-	<div>
-		<div>
-			List
+	<div class="flex flex-column">
+		<p><label class="label label-warning" title="This feature is brand new and still experimental">Under Development</label></p>
+		<div class="muted-title">
+			Frequently Visited Links
+		</div>
+		<div class="flex flex-column">
+			<a @click.prevent="goto_list(doctype)" v-for="doctype in frequently_visited_list" :key="doctype">{{doctype}}</a>
 		</div>
 	</div>
 </template>
 <script>
 export default {
 	props: ['user'],
-
+	data() {
+		return {
+			frequently_visited_list: [
+				'User Permission',
+				'Issues',
+				'Sales Order',
+				'Purchase Invoice',
+			]
+		}
+	},
+	methods: {
+		goto_list(doctype) {
+			frappe.set_route('List', doctype);
+		}
+	}
 }
 </script>
+<style lang="less" scoped>
+a {
+	margin: 10px;
+}
+.stats {
+	min-height: 150px
+}
+</style>
 
--- frappe/public/js/frappe/social/components/ProfileSidebar.vue
+++ frappe/public/js/frappe/social/components/ProfileSidebar.vue
@@ -1,15 +1,38 @@
 <template>
 	<div class="profile-sidebar flex flex-column">
 		<div v-html="user_avatar"></div>
-		<div>
+		<div class="user-details">
 			<h3>{{ user.fullname }}</h3>
 			<p class="text-muted">{{ user.bio }}</p>
+		</div>
+		<div class="stats">
+			<div class="muted-title">
+				Posts
+			</div>
+			<div class="text-large">
+				{{ post_count }}
+			</div>
 		</div>
 	</div>
 </template>
 <script>
 export default {
 	props: ['user_id'],
+	data() {
+		return {
+			'post_count': 0
+		}
+	},
+	created() {
+		frappe.db.count('Post', {
+			'filters': {
+				'owner': this.user_id
+			}
+		}).then(count => {
+			console.log(count);
+			this.post_count = count
+		})
+	},
 	computed: {
 		user_avatar() {
 			return frappe.avatar(this.user_id, 'avatar-xl')
@@ -23,5 +46,8 @@ export default {
 <style lang="less" scoped>
 .profile-sidebar {
 	padding-top: 10px
+}
+.user-details {
+	min-height: 150px
 }
 </style>
