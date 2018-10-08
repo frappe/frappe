3f4b0bfd6271fa6c05c008ff558bdfa1a75228bb
Suraj Shetty <surajshetty3416@gmail.com>
2018-10-08 07:13:53 +0530

    Capitalize route links

--- frappe/public/js/frappe/social/components/PostSidebar.vue
+++ frappe/public/js/frappe/social/components/PostSidebar.vue
@@ -1,11 +1,22 @@
 <template>
 	<div class="flex flex-column">
-		<p><label class="label label-warning" title="This feature is brand new and still experimental">Under Development</label></p>
+		<p>
+			<label
+				class="label label-warning"
+				title="This feature is brand new and still experimental">
+				Under Development
+			</label>
+		</p>
 		<div class="muted-title">
 			Frequently Visited Links
 		</div>
 		<div class="flex flex-column">
-			<a @click.prevent="goto_list(route_obj.route)" v-for="route_obj in frequently_visited_list" :key="route_obj.route">{{route_obj.route}}</a>
+			<a class="route-link"
+				@click.prevent="goto_list(route_obj.route)"
+				v-for="route_obj in frequently_visited_list"
+				:key="route_obj.route">
+				{{route_obj.route}}
+			</a>
 		</div>
 	</div>
 </template>
@@ -25,16 +36,18 @@ export default {
 			frappe.set_route(route);
 		},
 		set_frequently_visited_list() {
-			frappe.xcall('frappe.social.doctype.post.post.frequently_visited_links').then(data => {
-				this.frequently_visited_list = data;
-			})
+			frappe.xcall('frappe.social.doctype.post.post.frequently_visited_links')
+				.then(data => {
+					this.frequently_visited_list = data;
+				})
 		}
 	}
 }
 </script>
 <style lang="less" scoped>
-a {
+.route-link {
 	margin: 10px;
+	text-transform: capitalize;
 }
 .stats {
 	min-height: 150px
