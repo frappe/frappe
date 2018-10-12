64a2c177acb6f1d0a8855eaa74ec7644c93e1358
Aditya Hase <aditya@adityahase.com>
2018-10-12 17:49:13 +0530

    feat(recorder): Create a harmless view at /recorder

---
+++ frappe/www/recorder.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+def get_context(context):
+	return context
