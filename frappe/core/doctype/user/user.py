0a758fd9d2a496fc576e4fce116b501b151977c5
Rushabh Mehta <rmehta@gmail.com>
2018-10-15 09:11:51 +0530

    fix(send_email):

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -798,9 +798,12 @@ def sign_up(email, full_name, redirect_to):
 			return 2, _("Please ask your administrator to verify your sign-up")
 
 @frappe.whitelist(allow_guest=True)
-def reset_password(user):
+def reset_password(user, send_email=False):
 	if user=="Administrator":
 		return 'not allowed'
+	
+	if send_email and send_email=='false':
+		send_email = false
 
 	try:
 		user = frappe.get_doc("User", user)
@@ -808,7 +811,7 @@ def reset_password(user):
 			return 'disabled'
 
 		user.validate_reset_password()
-		user.reset_password(send_email=True)
+		user.reset_password(send_email=send_email)
 
 		return frappe.msgprint(_("Password reset instructions have been sent to your email"))
 
