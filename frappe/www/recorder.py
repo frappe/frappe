ef3d95237966042341c25e563f70b885a6934c60
Aditya Hase <aditya@adityahase.com>
2018-10-18 12:50:42 +0530

    feat(recorder): Group and lines by frappe.request.path and show on page

--- frappe/www/recorder.py
+++ frappe/www/recorder.py
@@ -8,4 +8,8 @@ import json
 
 def get_context(context):
 	result = frappe.cache().lrange("recorder-sql", 0, -1)
-	return {"result": result}
+	result = [json.loads(line.decode()) for line in result]
+	paths = {}
+	for line in result:
+		paths.setdefault(line["path"], []).append(line)
+	return {"result": paths}
