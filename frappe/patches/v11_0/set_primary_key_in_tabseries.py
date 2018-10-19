a4bcc203859c6c35f683b6198389cb6e56cb2943
Himanshu Mishra <himanshu.mishra@elastic.run>
2018-10-19 16:34:25 +0530

    remove unnecessary file

--- frappe/patches/v11_0/set_primary_key_in_tabseries.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and Contributors
-# MIT License. See license.txt
-
-import frappe
-
-def execute():
-	frappe.db.sql('ALTER table `tabSeries` add PRIMARY KEY (name)')
