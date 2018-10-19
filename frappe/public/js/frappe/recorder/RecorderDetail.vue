729a4287c9dc94ecef8e8aa9089d657b43dbeb5c
Aditya Hase <aditya@adityahase.com>
2018-10-19 23:03:07 +0530

    refactor(recorder): Restructure recorder vue app and its components

---
+++ frappe/public/js/frappe/recorder/RecorderDetail.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<path-list/>
+	</div>
+</template>
+
+<script>
+import PathList from "./PathList.vue"
+export default {
+	name: "RecorderDetail",
+	components: {
+		PathList,
+	}
+};
+</script>
