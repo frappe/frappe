7a6a046e0ce25a8ce9cc1c80f98a08abd59ec928
Aditya Hase <aditya@adityahase.com>
2018-10-19 14:35:10 +0530

    Create Path component

---
+++ frappe/public/js/frappe/recorder/Path.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<ol>
+			<li v-for="request in requests" :key="request.uuid">{{ request.uuid }}</li>
+		</ol>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "recorder-path",
+	data() {
+		return {
+			requests: [],
+		};
+	},
+	mounted() {
+		frappe.call({
+			method: "frappe.www.recorder.get_requests",
+			args: {
+				path: "/recorder"
+			}
+		}).then( r => {
+			this.requests = r.message
+		})
+	}
+};
+</script>
