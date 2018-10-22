a8341a2c3e1655492c20cfb7b706d2137969769c
Aditya Hase <aditya@adityahase.com>
2018-10-22 12:20:38 +0530

    refactor(recorder): Render calls for each request

--- frappe/public/js/frappe/recorder/RequestDetail.vue
+++ frappe/public/js/frappe/recorder/RequestDetail.vue
@@ -1,11 +1,26 @@
 <template>
 	<div>
-		Just rendering a request
+		{{ calls }}
 	</div>
 </template>
 
 <script>
 export default {
 	name: "RequestDetail",
+ 	data() {
+		return {
+			calls: [],
+		};
+	},
+	mounted() {
+		frappe.call({
+			method: "frappe.www.recorder.get_calls",
+			args: {
+				uuid: this.$route.param
+			}
+		}).then( r => {
+			this.calls = r.message
+		})
+	},
 };
 </script>
