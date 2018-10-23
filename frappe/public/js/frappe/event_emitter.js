9436298560e196af3fb0edb6b57806055d989786
Faris Ansari <netchamp.faris@gmail.com>
2018-10-23 15:12:38 +0530

    fix(route): Fix event emitter on route

--- frappe/public/js/frappe/event_emitter.js
+++ frappe/public/js/frappe/event_emitter.js
@@ -2,25 +2,25 @@ frappe.provide('frappe.utils');
 /**
  * Simple EventEmitter which uses jQuery's event system
  */
-class EventEmitter {
+const event_emitter = {
 	init() {
 		this.jq = jQuery(this);
-	}
+	},
 
 	trigger(evt, data) {
 		!this.jq && this.init();
 		this.jq.trigger(evt, data);
-	}
+	},
 
 	once(evt, handler) {
 		!this.jq && this.init();
 		this.jq.one(evt, (e, data) => handler(data));
-	}
+	},
 
 	on(evt, handler) {
 		!this.jq && this.init();
 		this.jq.bind(evt, (e, data) => handler(data));
-	}
+	},
 
 	off(evt, handler) {
 		!this.jq && this.init();
@@ -29,8 +29,8 @@ class EventEmitter {
 }
 
 frappe.utils.make_event_emitter = function(object) {
-	Object.assign(object, EventEmitter.prototype);
+	Object.assign(object, event_emitter);
 	return object;
 };
 
-export default EventEmitter;
+export default event_emitter;
