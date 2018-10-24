3f730263012c3003d89d6b8ee1bf18b2dc3371ec
Navdeep Ghai <30634335+navdeepghai1@users.noreply.github.com>
2018-10-24 20:13:19 +0400

    Update modify_naming_series_table.py

--- frappe/patches/v10_0/modify_naming_series_table.py
+++ frappe/patches/v10_0/modify_naming_series_table.py
@@ -7,10 +7,4 @@
 import frappe
 import re
 def execute():
-        
-        fields = frappe.db.sql("""DESC `tabSeries` """, as_dict=True)
-        for field in fields:
-                if field.get("Field") == "current" and re.sub("[0-9()]", "", field.get("Type"))=="int":
-                        frappe.db.sql(""" ALTER TABLE `tabSeries` MODIFY current BIGINT """)
-
-
+        frappe.db.sql(""" ALTER TABLE `tabSeries` MODIFY current BIGINT """)
