fd2725bb2c695104fc2b2c8e33594726682691aa
Faris Ansari <netchamp.faris@gmail.com>
2018-10-24 17:01:08 +0530

    fix(Quill): Remove image drop module
    Quill 2.0 already has it

--- frappe/public/js/frappe/form/controls/text_editor.js
+++ frappe/public/js/frappe/form/controls/text_editor.js
@@ -1,8 +1,4 @@
 import Quill from 'quill';
-import { ImageDrop } from 'quill-image-drop-module';
-
-
-Quill.register('modules/imageDrop', ImageDrop);
 
 // replace <p> tag with <div>
 const Block = Quill.import('blots/block');
@@ -66,30 +62,6 @@ frappe.ui.form.ControlTextEditor = frappe.ui.form.ControlCode.extend({
 			e.stopPropagation();
 		});
 
-		// paste images
-		$(this.quill.root).on('paste', (e) => {
-			const clipboardData = e.originalEvent.clipboardData;
-			const files = clipboardData.files;
-			if (files.length > 0) {
-
-				Array.from(files).forEach(file => {
-					if (!file.type.match(/^image\/(gif|jpe?g|a?png|svg|webp|bmp|vnd\.microsoft\.icon)/i)) {
-						// file is not an image
-						// Note that some file formats such as psd start with image/* but are not readable
-						return;
-					}
-
-					frappe.dom.file_to_base64(file)
-						.then(data_url => {
-							setTimeout(() => {
-								const index = (this.quill.getSelection() || {}).index || this.quill.getLength();
-								this.quill.insertEmbed(index, 'image', data_url, 'user');
-							});
-						})
-				});
-			}
-		});
-
 		// table commands
 		this.$wrapper.on('click', '.ql-table .ql-picker-item', (e) => {
 			const $target = $(e.currentTarget);
@@ -133,7 +105,6 @@ frappe.ui.form.ControlTextEditor = frappe.ui.form.ControlCode.extend({
 		return {
 			modules: {
 				toolbar: this.get_toolbar_options(),
-				imageDrop: true,
 				table: true
 			},
 			theme: 'snow'
