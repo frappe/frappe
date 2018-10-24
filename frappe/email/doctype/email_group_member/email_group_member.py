772bf5295bb079e4347cf0093487a7dce81ef898
patilsangram <sangram.p@indictranstech.com>
2018-10-24 17:00:43 +0530

    [fix] Email Group Total Subscribers update on deletion of email group member

--- frappe/email/doctype/email_group_member/email_group_member.py
+++ frappe/email/doctype/email_group_member/email_group_member.py
@@ -7,7 +7,12 @@ import frappe
 from frappe.model.document import Document
 
 class EmailGroupMember(Document):
-	pass
+	def on_trash(self):
+		email_group = frappe.get_doc("Email Group",self.email_group)
+		total_subscribers = frappe.db.sql("""select count(*) from `tabEmail Group Member`
+			where email_group=%s and name <> %s""", (self.email_group, self.name))[0][0]
+		email_group.total_subscribers = total_subscribers
+		email_group.db_update()
 
 def after_doctype_insert():
 	frappe.db.add_unique("Email Group Member", ("email_group", "email"))
