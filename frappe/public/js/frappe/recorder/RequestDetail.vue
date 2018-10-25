5a84087ce3eb836e4b8ffb8a7416c649b91a8d82
Aditya Hase <aditya@adityahase.com>
2018-10-25 16:14:59 +0530

    Use bootstrap collapse to hide unnecessary data

--- frappe/public/js/frappe/recorder/RequestDetail.vue
+++ frappe/public/js/frappe/recorder/RequestDetail.vue
@@ -1,7 +1,48 @@
 <template>
 	<div>
-		<div v-for="call in calls" :key="call.function">
-			{{ call }}
+		<div id="accordion-sql">
+			<div v-for="call in calls" :key="call.index" class="card">
+				<div class="card-header" :id="'heading-' + call.index ">
+					<h5 class="mb-0">
+						<button class="btn btn-link" data-toggle="collapse" :data-target="'#collapse-' + call.index ">
+							{{ call.query }}
+							{{ call.time.total }}
+						</button>
+					</h5>
+				</div>
+				<div :id="'collapse-' + call.index " class="collapse" data-parent="#accordion-sql">
+					<div class="card-body">
+						<div><pre>{{ call.stack }}</pre></div>
+						<div><pre>{{ call.query }}</pre></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="accordion-cache">
+			<div v-for="call in cache" :key="call.index" class="card">
+				<div class="card-header" :id="'heading-' + call.index ">
+					<h5 class="mb-0">
+						<button class="btn btn-link" data-toggle="collapse" :data-target="'#collapse-' + call.index ">
+							{{ call.call }}
+							{{ call.time.total }}
+						</button>
+					</h5>
+				</div>
+				<div :id="'collapse-' + call.index " class="collapse" data-parent="#accordion-cache">
+					<div class="card-body">
+						<div><pre>{{ call.stack }}</pre></div>
+						<div><pre>{{ call.call }}</pre></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div>
+			<div v-for="stat in stats" :key="stat.name">
+				{{ stat }}<br/>
+			</div>
 		</div>
 	</div>
 </template>
