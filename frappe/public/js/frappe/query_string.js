2f9856dbe3d534499a7954540a0422adbd473718
Faris Ansari <netchamp.faris@gmail.com>
2018-10-29 14:23:00 +0530

    feat: Share Listview URL

--- frappe/public/js/frappe/query_string.js
+++ frappe/public/js/frappe/query_string.js
@@ -29,7 +29,11 @@ function get_query_params(query_string) {
 		var value = pair[1];
 		if (typeof value === "string") {
 			value = value.replace(/\+/g, "%20");
-			value = decodeURIComponent(value);
+			try {
+				value = decodeURIComponent(value);
+			} catch(e) {
+				// if value contains %, it fails
+			}
 		}
 
 		if (key in query_params) {
