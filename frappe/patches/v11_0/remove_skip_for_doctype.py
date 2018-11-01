3dfd119269b3a06c50efcea94ab086354747c8c7
Suraj Shetty <surajshetty3416@gmail.com>
2018-11-01 20:37:48 +0530

    Add a patch to remove skip_for_doctype

--- frappe/patches/v11_0/remove_skip_for_doctype.py
+++ frappe/patches/v11_0/remove_skip_for_doctype.py
@@ -10,8 +10,13 @@ import frappe
 # one record is created
 
 def execute():
+	# frappe.db.sql('delete from `tabUser Permission`')
+	# frappe.get_doc({'doctype': 'User Permission', u'_liked_by': None, u'modified_by': u'Administrator', u'name': u'76783e783a', u'parent': None, u'_assign': None, u'_user_tags': None, u'for_value': u'User', u'idx': 0, u'parenttype': None, u'allow': u'DocType', u'user': u'suraj@erpnext.com', u'skip_for_doctype': u'', u'applicable_for': None, u'owner': u'Administrator', u'docstatus': 0, u'_comments': None, u'parentfield': None}).insert()
+	# frappe.get_doc({'doctype': 'User Permission', u'_liked_by': None, u'modified_by': u'Administrator', u'name': u'456f1be7c5', u'parent': None, u'_assign': None, u'_user_tags': None, u'for_value': u'DocType', u'idx': 0, u'parenttype': None, u'allow': u'DocType', u'user': u'suraj@erpnext.com', u'skip_for_doctype': u'Activity Log\nAddress\nAuto Repeat\nBulk Update\nCalendar View\nContact\nCustom Field\nCustom Script\nData Migration Mapping\nDesktop Icon\nDocType\nEmail Account\nEmail Domain\nEvent\nFeedback Trigger\nGSuite Templates\nIntegration Request\nList Filter\nNotification\nPayment Gateway\nPrint Format\nProperty Setter\nReport\nSuccess Action\nTag Category\nUser Permission\nVersion\nView log\nWebhook\nWorkflow\nWorkflow Action', u'applicable_for': None, u'owner': u'Administrator', u'docstatus': 0, u'_comments': None, u'parentfield': None}).insert()
+	# frappe.get_doc({'doctype': 'User Permission', u'_liked_by': None, u'modified_by': u'Administrator', u'name': u'87e85fb24f', u'parent': None, u'_assign': None, u'_user_tags': None, u'for_value': u'Review', u'idx': 0, u'parenttype': None, u'allow': u'Workflow Action Master', u'user': u'suraj@erpnext.com', u'skip_for_doctype': u'Workflow Action Master', u'applicable_for': None, u'owner': u'Administrator', u'docstatus': 0, u'_comments': None, u'parentfield': None}).insert()
 	frappe.reload_doctype('User Permission')
 	for user_permission in frappe.get_all('User Permission', fields=['*']):
+		if not user_permission.skip_for_doctype: continue
 		skip_for_doctype = user_permission.skip_for_doctype.split('\n')
 		if skip_for_doctype:
 			# only specific doctypes are selected
@@ -23,6 +28,5 @@ def execute():
 				if doctype:
 					new_user_permission = frappe.new_doc('User Permission')
 					new_user_permission.update(user_permission)
-					print(test)
 					new_user_permission.applicable_for = doctype
 					new_user_permission.db_insert()
