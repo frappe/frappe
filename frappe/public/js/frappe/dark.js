2f6394d53b4ddc257dd3cca2baa9215530dc929c
hrwX <himanshuwarekar@yahoo.com>
2018-11-08 20:45:03 +0530

    Using checkbox as dark mode indicator

--- frappe/public/js/frappe/dark.js
+++ frappe/public/js/frappe/dark.js
@@ -1,10 +1,12 @@
 frappe.set_dark_mode = function() {
     var date = new Date;
     date.setDate(date.getDate() + 180);
+    //  If Dark Mode is On or If Dark Mode is undefined, Set it to Off
     if(frappe.get_cookie("dark_mode") == "True" || frappe.get_cookie("dark_mode") == undefined){
         document.cookie = "dark_mode=False; expires="+ date +"";
         frappe.dark_mode();
     }
+    //  If Dark Mode is Off, Set it to On
     else{
         document.cookie = "dark_mode=True; expires="+ date +"";
         frappe.dark_mode();
@@ -17,6 +19,7 @@ frappe.dark_mode = function(){
         for (var i = 0; i < styleSheets.length; i++) {
             if (styleSheets[i].href.indexOf("dark.css") != -1) {
                 styleSheets[i].disabled = true;
+                $('#dark_mode').prop('checked', false);
                 break;
             }
         }
@@ -25,6 +28,7 @@ frappe.dark_mode = function(){
         var styleSheets = document.styleSheets;
         for (var i = 0; i < styleSheets.length; i++) {
             if (styleSheets[i].href.indexOf("dark.css") != -1) {
+                $('#dark_mode').prop('checked', true);
                 styleSheets[i].disabled = false;
                 break;
             }
