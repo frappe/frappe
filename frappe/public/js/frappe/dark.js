8c53b78100cb55875b62be592108f43f1178ca03
hrwX <himanshuwarekar@yahoo.com>
2018-11-08 15:39:50 +0530

    set cookie expiration and changed cookie name

--- frappe/public/js/frappe/dark.js
+++ frappe/public/js/frappe/dark.js
@@ -1,16 +1,18 @@
 frappe.set_dark_mode = function() {
-    if(frappe.get_cookie("DarkMode") == "True" || frappe.get_cookie("DarkMode") == undefined){
-        document.cookie = "DarkMode=False";
+    var date = new Date;
+    date.setDate(date.getDate() + 180);
+    if(frappe.get_cookie("dark_mode") == "True" || frappe.get_cookie("dark_mode") == undefined){
+        document.cookie = "dark_mode=False; expires="+ date +"";
         frappe.dark_mode();
     }
     else{
-        document.cookie = "DarkMode=True";
+        document.cookie = "dark_mode=True; expires="+ date +"";
         frappe.dark_mode();
     }
 };
 
 frappe.dark_mode = function(){
-	if(frappe.get_cookie("DarkMode") == "False"){
+	if(frappe.get_cookie("dark_mode") == "False"){
 		var styleSheets = document.styleSheets;
         for (var i = 0; i < styleSheets.length; i++) {
             if (styleSheets[i].href.indexOf("dark.css") != -1) {
