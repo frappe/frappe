215853333174531be343591812c3472834ce972f
Suraj Shetty <surajshetty3416@gmail.com>
2018-11-09 20:40:37 +0530

    Add post delete option

--- frappe/public/js/frappe/social/components/Post.vue
+++ frappe/public/js/frappe/social/components/Post.vue
@@ -1,14 +1,17 @@
 <template>
 	<div class="post-card">
 		<div class="post-body" :class="{'pinned ': is_pinned}">
-			<div class="pull-right">
+			<div class="pull-right flex">
 				<div
 					class="pin-option"
-					:class="{'cursor-pointer': can_pin}"
-					@click="toggle_pin"
-					v-if="can_pin || is_pinned">
-					<i class="octicon octicon-pin"></i>
+					v-if="is_pinned">
+					Globally Pinned
 				</div>
+				<post-dropdown-menu
+					class="post-dropdown-menu"
+					v-if="options.length"
+					:options="options"
+				/>
 			</div>
 			<div class="user-avatar" v-html="user_avatar" @click="goto_profile(post.owner)"></div>
 			<a class="user-name" @click="goto_profile(post.owner)">{{ user_name }}</a>
@@ -32,12 +35,14 @@
 <script>
 import PostAction from './PostAction.vue';
 import PostComment from './PostComment.vue';
+import PostDropdownMenu from './PostDropdownMenu.vue';
 
 export default {
 	props: ['post'],
 	components: {
 		PostAction,
-		PostComment
+		PostComment,
+		PostDropdownMenu
 	},
 	data() {
 		return {
@@ -48,7 +53,8 @@ export default {
 			comments: [],
 			show_comments: false,
 			is_globally_pinnable: frappe.user_roles.includes('System Manager') && frappe.social.is_home_page(),
-			is_pinnable: false
+			is_pinnable: false,
+			is_user_post_owner: this.post.owner === frappe.session.user
 		}
 	},
 	computed: {
@@ -58,6 +64,29 @@ export default {
 		is_pinned() {
 			return false && frappe.social.is_profile_page(this.post.owner)
 				|| this.post.is_globally_pinned && frappe.social.is_home_page()
+		},
+		options() {
+			const options = []
+			if (this.is_user_post_owner) {
+				options.push({
+					'label': __('Delete'),
+					'action': this.delete_post
+				})
+			}
+			if (this.can_pin) {
+				if (this.is_pinned) {
+					options.push({
+						'label': __('Unpin'),
+						'action': this.toggle_pin
+					})
+				} else {
+					options.push({
+						'label': __('Pin Globally'),
+						'action': this.toggle_pin
+					})
+				}
+			}
+			return options;
 		}
 	},
 	created() {
@@ -79,6 +108,10 @@ export default {
 			this.comments = [comment].concat(this.comments);
 		})
 		frappe.realtime.on('update_liked_by' + this.post.name, this.update_liked_by)
+
+		frappe.realtime.on('delete_post' + this.post.name, () => {
+			this.$emit('delete-post')
+		})
 
 		this.$root.$on('user_image_updated', () => {
 			this.user_avatar = frappe.avatar(this.post.owner, 'avatar-medium')
@@ -128,6 +161,14 @@ export default {
 			comment.content = content
 			comment.parent = this.post.name;
 			frappe.db.insert(comment);
+		},
+		delete_post() {
+			frappe.confirm(__("Are you sure you want to delete this post?"), () => {
+				frappe.dom.freeze();
+				frappe.xcall('frappe.social.doctype.post.post.delete_post', {
+					'post_name': this.post.name
+				}).then(frappe.dom.unfreeze)
+			})
 		},
 		generate_preview(link_element) {
 			// TODO: move the code to separate component
