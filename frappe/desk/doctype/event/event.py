dafdeebe45ecd0bb58a53b7c4d6f55d23e21801f
Zlash65 <zarrar65@gmail.com>
2018-11-13 05:50:07 +0530

    delete events associated with any reference doctype

--- frappe/desk/doctype/event/event.py
+++ frappe/desk/doctype/event/event.py
@@ -251,3 +251,20 @@ def get_events(start, end, user=None, for_reminder=False, filters=None):
 			del e[w]
 
 	return events
+
+@frappe.whitelist()
+def delete_events(ref_type, ref_name, delete_event=False):
+	participations = frappe.get_all("Event Participants", filters={"reference_doctype": ref_type, "reference_docname": ref_name,
+		"parenttype": "Event"}, fields=["parent", "name"])
+
+	if participations:
+		for participation in participations:
+			if delete_event:
+				frappe.delete_doc("Event", participation.parent, for_reload=True)
+			else:
+				total_participants = frappe.get_all("Event Participants", filters={"parenttype": "Event", "parent": participation.parent})
+
+				if len(total_participants) <= 1:
+					frappe.db.sql("delete from `tabEvent` where name='%s'" % participation.parent)
+
+				frappe.db.sql("delete from `tabEvent Participants` where name='%s'" % participation.name)
