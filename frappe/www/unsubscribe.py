2a2bf4e4ace675d317d040f99ddd562a7f739cff
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-11-14 11:42:37 +0530

    added validation

--- frappe/www/unsubscribe.py
+++ frappe/www/unsubscribe.py
@@ -3,12 +3,15 @@ import frappe
 
 
 def get_context(context):
-		if frappe.form_dict['name']:
+		if '_signature' in frappe.form_dict:
 			user_email = frappe.form_dict["email"]
 			context.email = user_email
 			tittle = frappe.form_dict["name"]
 			context.email_groups = get_email_groups(user_email)
 			context.current_group = get_current_groups(tittle)
+		else:
+			print("============inside====================")
+			context.valid = "not found"
 
 def get_email_groups(user_email):
 	return frappe.get_all("Email Group Member", fields = ["email_group"], filters = {"email": user_email})
--- frappe/www/unsubscribed.py
+++ frappe/www/unsubscribed.py
@@ -3,6 +3,18 @@ import frappe
 
 
 def get_context(context):
-		if frappe.form_dict['user_email']:
-			context.hello="hello"
-			print("++++++++++++++++++++++++++++++++++sucess++++++++++++++++++++++++++++++++++++++++++++++")
+		if "user_email" in frappe.form_dict:
+			email = frappe.form_dict['user_email']
+			email_group = get_email_groups(email)
+			print("---------------------------------GROUP------------------------------------------------")
+			print(email_group)
+			print(frappe.form_dict)
+			for group in email_group:
+					print("hello---------------------------")
+					if group.email_group in frappe.form_dict:
+						print("-----------------inside if-----------------------------")
+						
+
+
+def get_email_groups(user_email):
+	return frappe.get_all("Email Group Member", fields = ["email_group"], filters = {"email": user_email})
