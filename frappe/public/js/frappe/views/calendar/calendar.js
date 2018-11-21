f86daf0e90615b7b9f664ce590b3e28c3b822770
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-11-21 14:15:55 +0530

    intial commit

--- frappe/public/js/frappe/views/calendar/calendar.js
+++ frappe/public/js/frappe/views/calendar/calendar.js
@@ -34,7 +34,7 @@ frappe.views.CalendarView = class CalendarView extends frappe.views.ListView {
 	}
 
 	setup_view() {
-
+		
 	}
 
 	before_render() {
@@ -125,12 +125,13 @@ frappe.views.Calendar = Class.extend({
 		me.page.clear_user_actions();
 		$.each(frappe.boot.calendars, function(i, doctype) {
 			if(frappe.model.can_read(doctype)) {
-				me.page.add_menu_item(__(doctype), function() {
-					frappe.set_route("List", doctype, "Calendar");
-				});
+				me.page.add_sidebar_check(doctype);
+				//me.page.add_menu_item(__(doctype), function() {
+					//frappe.set_route("List", doctype, "Calendar");
+				//});
 			}
 		});
-
+		me.page.add_button("Apply", function(){ debugger; frappe.set_route("List", "Event", "Calendar");});
 		$(this.parent).on("show", function() {
 			me.$cal.fullCalendar("refetchEvents");
 		});
