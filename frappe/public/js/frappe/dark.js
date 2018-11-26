4d187f169c1ddde55d850b0e4f777bd6260e91d8
hrwX <himanshuwarekar@yahoo.com>
2018-11-26 10:42:13 +0530

    code refactor

--- frappe/public/js/frappe/dark.js
+++ frappe/public/js/frappe/dark.js
@@ -2,7 +2,7 @@ frappe.dark_mode = {
 	set_mode: function() {
 		var date = new Date;
 		date.setDate(date.getDate() + 180);
-		if(frappe.get_cookie("dark_mode") == undefined){	//  Dark Mode is undefined, Set it to Off
+		if(frappe.get_cookie("dark_mode") == undefined) {	//  Dark Mode is undefined, Set it to Off
 			document.cookie = "dark_mode=False; expires="+ date +"";
 		}
 		frappe.dark_mode.check_mode();
@@ -11,10 +11,10 @@ frappe.dark_mode = {
 	toggle_mode: function(){
 		var date = new Date;
 		date.setDate(date.getDate() + 180);
-		if(frappe.get_cookie("dark_mode") == "True"){	//  Dark Mode is On, Set it to Off
+		if(frappe.get_cookie("dark_mode") == "True") {	//  Dark Mode is On, Set it to Off
 			document.cookie = "dark_mode=False; expires="+ date +"";
 		}
-		else{	//  Dark Mode is Off, Set it to On
+		else {	//  Dark Mode is Off, Set it to On
 			document.cookie = "dark_mode=True; expires="+ date +"";
 		}
 		frappe.dark_mode.check_mode();
@@ -23,7 +23,7 @@ frappe.dark_mode = {
 	check_mode: function(){
 		var StyleSheets = document.styleSheets;
 		var StyleSheet;
-		if(frappe.get_cookie("dark_mode") == "False"){
+		if(frappe.get_cookie("dark_mode") == "False") {
 			for (StyleSheet in StyleSheets) {
 				if (StyleSheets[StyleSheet].href.indexOf("desk-dark") != -1) {
 					StyleSheets[StyleSheet].disabled = true;
@@ -32,7 +32,7 @@ frappe.dark_mode = {
 				}
 			}
 		}
-		else{
+		else {
 			for (StyleSheet in StyleSheets) {
 				if (StyleSheets[StyleSheet].href.indexOf("desk-dark") != -1) {
 					StyleSheets[StyleSheet].disabled = false;
