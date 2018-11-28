aab5fb144c5ab8c06d83fcec7959385b99bcd202
Anurag Mishra <mishranaman123@gmail.com>
2018-11-28 19:01:24 +0530

    working on full calendar js

--- frappe/core/page/calendar/calendar.js
+++ frappe/core/page/calendar/calendar.js
@@ -3,5 +3,31 @@ frappe.pages['calendar'].on_page_load = function(wrapper) {
 		parent: wrapper,
 		title: 'Master Calendar',
 		single_column: true
+		
 	});
+	frappe.require('assets/frappe/js/lib/fullcalendar/fullcalendar.min.css');
+	frappe.require('assets/frappe/js/lib/fullcalendar/fullcalendar.min.js', function (){
+		this.$cal = $("<div>").appendTo(page.body);
+		this.$cal.fullCalendar( {
+			dayClick: function(date) {
+				alert(date);
+			},
+			events: function(start, end, timezone) {
+				return frappe.call({
+					method: "frappe.desk.doctype.event.event.get_events",
+					type: "GET",
+					args : {
+						start: "2018-10-28 00:00:00",
+						end: "2018-12-09 00:00:00",
+					}
+				}).then(r => {
+					console.log(r.message);
+					var events = r.message ;
+				});
+			}
+		}
+		);
+	});
+	frappe.require('assets/frappe/js/lib/fullcalendar/locale-all.js');
+
 }
