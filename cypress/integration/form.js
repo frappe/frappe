e5823f3597348ea906ff0c4ab41ef1062cb11392
Faris Ansari <netchamp.faris@gmail.com>
2018-11-29 00:32:37 +0530

    test: Remove specific selector

--- cypress/integration/form.js
+++ cypress/integration/form.js
@@ -10,7 +10,7 @@ context('Form', () => {
         cy.get('[data-fieldname="description"] .ql-editor').type('this is a test todo')
         cy.scrollTo(0, 0)
         cy.wait(2000)
-        cy.get('.primary-action:contains("Save")').click()
+        cy.get('.primary-action').click()
         cy.visit('/desk#List/ToDo')
         cy.location('hash').should('eq', '#List/ToDo/List')
         cy.get('.list-row').should('contain', 'this is a test todo')
