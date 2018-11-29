3495d31e773529a9bb7b17a2417eec6c2f4e2756
Faris Ansari <netchamp.faris@gmail.com>
2018-11-29 10:49:12 +0530

    test: Wait for form to get dirty

--- cypress/integration/form.js
+++ cypress/integration/form.js
@@ -7,6 +7,7 @@ context('Form', () => {
     it('create a new form', () => {
         cy.visit('/desk#Form/ToDo/New ToDo 1')
         cy.fill_field('description', 'this is a test todo', 'Text Editor').blur()
+        cy.get('.page-title').should('contain', 'Not Saved')
         cy.get('.primary-action').click()
         cy.visit('/desk#List/ToDo')
         cy.location('hash').should('eq', '#List/ToDo/List')
