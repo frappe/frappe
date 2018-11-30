33b534ac6e57784ca40e79b1423627bdbfa64893
Suraj Shetty <surajshetty3416@gmail.com>
2018-11-30 15:02:07 +0530

    Fix "replicate user permission" patch

--- frappe/patches/v11_0/remove_skip_for_doctype.py
+++ frappe/patches/v11_0/remove_skip_for_doctype.py
@@ -1,5 +1,6 @@
 import frappe
 from frappe.desk.form.linked_with import get_linked_doctypes
+from frappe.patches.v11_0.replicate_old_user_permissions import get_doctypes_to_skip
 
 # `skip_for_doctype` was a un-normalized way of storing for which
 # doctypes the user permission was applicable.
@@ -12,19 +13,31 @@ from frappe.desk.form.linked_with import get_linked_doctypes
 
 def execute():
 	frappe.reload_doctype('User Permission')
-	linked_with_map = {}
+
+	# to check if we need to migrate from skip_for_doctype
+	has_skip_for_doctype = frappe.db.has_column("User Permission", "skip_for_doctype")
+	skip_for_doctype_map = {}
+
 	for user_permission in frappe.get_all('User Permission', fields=['*']):
-		if not user_permission.skip_for_doctype:
-			frappe.db.set_value('User Permission', user_permission.name, 'apply_to_all_doctypes', 1)
-			continue
-		skip_for_doctype = user_permission.skip_for_doctype.split('\n')
+		if has_skip_for_doctype:
+			if not user_permission.skip_for_doctype:
+				frappe.db.set_value('User Permission', user_permission.name, 'apply_to_all_doctypes', 1)
+				continue
+			skip_for_doctype = user_permission.skip_for_doctype.split('\n')
+		else:
+			if skip_for_doctype_map[(user_permission.allow, user_permission.user)] == None:
+				skip_for_doctype = get_doctypes_to_skip(user_permission.allow, user_permission.user)
+				# cache skip for doctype for same user and doctype
+				skip_for_doctype_map[(user_permission.allow, user_permission.user)] = skip_for_doctype
+			else:
+				skip_for_doctype = skip_for_doctype_map[(user_permission.allow, user_permission.user)]
+
 		if skip_for_doctype:
 			# only specific doctypes are selected
 			# split this into multiple records and delete
-			if linked_with_map.get(user_permission.allow) == None:
-				linked_with_map[user_permission.allow] = get_linked_doctypes(user_permission.allow, True).keys()
-			# linked with doctypes that are not in skip_for_doctype
-			applicable_for_doctypes = filter(lambda d: d not in skip_for_doctype, linked_with_map[user_permission.allow])
+			linked_doctypes = get_linked_doctypes(user_permission.allow, True).keys()
+			applicable_for_doctypes = list(set(linked_doctypes) - set(skip_for_doctype))
+
 			frappe.db.sql('DELETE FROM `tabUser Permission` WHERE `name`=%s', user_permission.name)
 			user_permission.name = None
 			user_permission.skip_for_doctype = None
