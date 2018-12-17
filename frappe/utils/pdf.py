dd45d62f6484b1337d134825f2d51b7b0b0d1dbb
Rohit Waghchaure <rohitw1991@gmail.com>
2018-12-17 20:00:17 +0530

    [Fix] PDF view showing multiple pages

--- frappe/utils/pdf.py
+++ frappe/utils/pdf.py
@@ -140,9 +140,9 @@ def prepare_header_footer(soup):
 			options[html_id] = fname
 		else:
 			if html_id == "header-html":
-				options["margin-top"] = "15mm"
+				options["margin-top"] = "5mm"
 			elif html_id == "footer-html":
-				options["margin-bottom"] = "15mm"
+				options["margin-bottom"] = "5mm"
 
 	return options
 
