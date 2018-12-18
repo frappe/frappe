91700dc36027ec3433abb549b8d07d377ddb4c2d
Faris Ansari <netchampfaris@users.noreply.github.com>
2018-12-18 10:33:49 +0530

    chore: Add comment

--- frappe/public/js/frappe/views/reports/report_view.js
+++ frappe/public/js/frappe/views/reports/report_view.js
@@ -1055,7 +1055,8 @@ frappe.views.ReportView = class ReportView extends frappe.views.ListView {
 							for (let cdt in values) {
 								fields = fields.concat(values[cdt].map(f => [f, cdt]));
 							}
-
+							
+							// always keep name (ID) column
 							this.fields = [["name", this.doctype], ...fields];
 
 							this.fields.map(f => this.add_currency_column(f[0], f[1]));
