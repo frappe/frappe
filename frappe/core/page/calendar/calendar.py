d7162af0683ac629bed02bf5e9484e9ccc91b4f6
Anurag Mishra <mishranaman123@gmail.com>
2018-12-18 11:58:39 +0530

    refractor

--- frappe/core/page/calendar/calendar.py
+++ frappe/core/page/calendar/calendar.py
@@ -46,14 +46,14 @@ def get_master_calendar_events(doctypeinfo, start, end):
 					color = event[field_map.color] if event[field_map.color] else "#D2D1FB"
 
 				master_events.append({'start': str(event[field_map.start]),
-										'end': str(event[field_map.end]),
-										"title" : str(event[field_map.title]),
-										"id" : str(event['name']),
-										"description": str(event[field_map.description]),
-										"color": str(color),
-										"doctype" : str(info),
-										"textColor": "#4D4DA8"
-									})
+					'end': str(event[field_map.end]),
+					"title" : str(event[field_map.title]),
+					"id" : str(event['name']),
+					"description": str(event[field_map.description]),
+					"color": str(color),
+					"doctype" : str(info),
+					"textColor": "#4D4DA8"
+				})
 
 	return master_events
 
