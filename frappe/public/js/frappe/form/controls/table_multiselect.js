4e34c5a73781d095188735f2bc4e8e8703271014
Faris Ansari <netchamp.faris@gmail.com>
2018-12-19 01:38:18 +0530

    feat(control): Add Table MultiSelect control

---
+++ frappe/public/js/frappe/form/controls/table_multiselect.js
@@ -0,0 +1,93 @@
+frappe.ui.form.ControlTableMultiSelect = frappe.ui.form.ControlLink.extend({
+	make_input() {
+        this.with_link_btn = true;
+        this._super();
+
+        this.$input_area.addClass('form-control table-multiselect');
+        this.$input.removeClass('form-control');
+
+        this.$input.on("awesomplete-selectcomplete", () => {
+            this.$input.val('');
+        });
+	},
+    get_options() {
+		return (this.get_link_field() || {}).options;
+    },
+    get_link_field() {
+        const meta = frappe.get_meta(this.df.options);
+        return meta.fields.find(df => df.fieldtype === 'Link');
+    },
+    setup_buttons() {
+        this.$input_area.find('.link-btn').remove();
+    },
+    parse(value) {
+        const values = Array.from(this.$input_area
+            .find('.tb-selected-value'))
+            .map((target) => $(target).text());
+
+        values.push(value);
+
+        const link_field = this.get_link_field();
+        return values.map(value => {
+            return {
+                [link_field.fieldname]: value
+            };
+        });
+    },
+    validate(value) {
+		// validate the value just entered
+		if (this.df.ignore_link_validation) {
+			return value;
+        }
+
+        const link_field = this.get_link_field();
+        if (value.length === 0) {
+            return value;
+        }
+
+        const all_rows_except_last = value.slice(0, value.length - 1);
+        const last_row = value[value.length - 1]
+
+        // validate the last value entered
+        const link_value = last_row[link_field.fieldname];
+
+        // falsy value
+        if (!link_value) {
+            return all_rows_except_last;
+        }
+
+        // duplicate value
+        if (all_rows_except_last.map(row => row[link_field.fieldname]).includes(link_value)) {
+            return all_rows_except_last;
+        }
+
+		const validate_promise = this.validate_link_and_fetch(this.df, this.get_options(),
+            this.docname, link_value);
+
+        return validate_promise.then(validated_value => {
+            if (validated_value === link_value) {
+                return value
+            } else {
+                value.pop();
+                return value;
+            }
+        })
+	},
+    set_formatted_input(value) {
+        const docs = value || [];
+        const link_field = this.get_link_field();
+        const values = docs.map(row => row[link_field.fieldname]);
+        this.set_pill_html(values)
+    },
+    set_pill_html(values) {
+        const html = values
+            .map(value => this.get_pill_html(value))
+            .join('');
+
+        this.$input_area.find('.tb-selected-value').remove();
+        this.$input_area.prepend(html);
+    },
+    get_pill_html(value) {
+        return `<span class="tb-selected-value">${__(value)}</span>`
+    }
+});
