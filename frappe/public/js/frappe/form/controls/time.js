631d6c99672a681203c7cc6fb54a060ab84ca70c
Andrew McLeod <amcleod@astro.ex.ac.uk>
2018-12-21 08:04:31 +0000

    feat: Time format. Client-side support for time_format. Time input type refactored to match date, with similar support for re-formatting input time to DB time format (HH:mm:ss). Support for user time_format in misc/datetime.js. Some alterations to datepicker to remove the seconds slider and make the positioning more reliable (but with reversed logic to before).

--- frappe/public/js/frappe/form/controls/time.js
+++ frappe/public/js/frappe/form/controls/time.js
@@ -1,42 +1,87 @@
 frappe.ui.form.ControlTime = frappe.ui.form.ControlData.extend({
 	make_input: function() {
-		var me = this;
 		this._super();
-		this.$input.datepicker({
+		this.set_time_options();
+                this.set_datepicker();
+		this.refresh();
+	},
+	set_time_options: function() {
+		let sysdefaults = frappe.boot.sysdefaults;
+
+		let time_format = sysdefaults && sysdefaults.time_format
+			? sysdefaults.time_format : 'HH:mm:ss';
+
+		this.time_format = frappe.defaultTimeFormat;
+		this.datepicker_options = {
 			language: "en",
 			timepicker: true,
 			onlyTimepicker: true,
-			timeFormat: "hh:ii:ss",
+			timeFormat: time_format.toLowerCase().replace("mm", "ii"),
 			startDate: frappe.datetime.now_time(true),
-			onSelect: function() {
+			onSelect: () => {
 				// ignore micro seconds
-				if (moment(me.get_value(), 'hh:mm:ss').format('HH:mm:ss') != moment(me.value, 'hh:mm:ss').format('HH:mm:ss')) {
-					me.$input.trigger('change');
+				if (moment(this.get_value(), time_format).format('HH:mm:ss') != moment(this.value, time_format).format('HH:mm:ss')) {
+					this.$input.trigger('change');
 				}				
 			},
-			onShow: function() {
+			onShow: () => {
 				$('.datepicker--button:visible').text(__('Now'));
+
+				this.update_datepicker_position();
 			},
 			keyboardNav: false,
 			todayButton: true
-		});
-		this.datepicker = this.$input.data('datepicker');
-		this.datepicker.$datepicker
-			.find('[data-action="today"]')
-			.click(() => {
-				this.datepicker.selectDate(frappe.datetime.now_time(true));
-			});
-		this.refresh();
-	},
+		};
+        },
 	set_input: function(value) {
 		this._super(value);
 		if(value
 			&& ((this.last_value && this.last_value !== this.value)
 				|| (!this.datepicker.selectedDates.length))) {
 
-			var date_obj = frappe.datetime.moment_to_date_obj(moment(value, 'HH:mm:ss'));
+			var date_obj = frappe.datetime.moment_to_date_obj(moment(value, frappe.sys_defaults['time_format']));
 			this.datepicker.selectDate(date_obj);
 		}
+	},
+	set_datepicker: function() {
+		this.$input.datepicker(this.datepicker_options);
+		this.datepicker = this.$input.data('datepicker');
+
+		this.datepicker.$datepicker
+			.find('[data-action="today"]')
+			.click(() => {
+				this.datepicker.selectDate(frappe.datetime.now_time(true));
+			});
+		this.datepicker.opts['offset'] = 0;
+		if (this.datepicker.opts.timeFormat.indexOf('s') == -1) {
+			// No seconds in time format
+			this.noSeconds = true;
+			const $tp = this.datepicker.timepicker;
+			$tp.$seconds.parent().css('display', 'none');
+			$tp.$secondsText.css('display', 'none');
+			$tp.$secondsText.prev().css('display', 'none');
+		} else {
+			this.noSeconds = false;
+                }
+        },
+	update_datepicker_position: function() {
+		if(!this.frm) return;
+		// show datepicker above or below the input
+		// based on scroll position
+		const scroll_limit = $(window).scrollTop() + $(window).height()
+		const picker_bottom = this.$input.offset().top + this.$input.outerHeight() + 12 + this.datepicker.$datepicker.outerHeight()
+
+		var position = 'bottom left';
+		//if (picker_top > scroll_limit + window_scroll) {
+		if (picker_bottom >= scroll_limit) {
+			position = 'top left';
+			// bodge around the picker being 30 pixels shorter than
+			// it should be due to hiding seconds
+			if (this.noSeconds) this.datepicker.opts['offset'] = -28;
+		} else {
+			if (this.noSeconds) this.datepicker.opts['offset'] = 12;
+		}
+		this.datepicker.update('position', position);
 	},
 	set_description: function() {
 		const { description } = this.df;
@@ -49,5 +94,26 @@ frappe.ui.form.ControlTime = frappe.ui.form.ControlData.extend({
 			}
 		}
 		this._super();
+	},
+	parse: function(value) {
+		if(value) {
+			return frappe.datetime.user_to_str(value, true);
+		}
+	},
+	format_for_input: function(value) {
+		if(value) {
+			return frappe.datetime.str_to_user(value, true);
+		}
+		return "";
+	},
+	validate: function(value) {
+		if(value && !frappe.datetime.validate(value)) {
+			let sysdefaults = frappe.sys_defaults;
+			let time_format = sysdefaults && sysdefaults.time_format
+				? sysdefaults.time_format : 'HH:mm:ss';
+			frappe.msgprint(__("Date {0} must be in format: {1}", [value, date_format]));
+			return '';
+		}
+		return value;
 	}
 });
