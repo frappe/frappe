a5b7f1611c7c5b7a4ad490c9255b460774e4d82f
Andrew McLeod <amcleod@astro.ex.ac.uk>
2018-12-21 08:12:32 +0000

    feat: Time format. Remove second slider from datetime picker.

--- frappe/public/js/frappe/form/controls/datetime.js
+++ frappe/public/js/frappe/form/controls/datetime.js
@@ -25,5 +25,37 @@ frappe.ui.form.ControlDatetime = frappe.ui.form.ControlDate.extend({
 			}
 		}
 		this._super();
-	}
+	},
+	set_datepicker: function() {
+		this._super();
+		if (this.datepicker.opts.timeFormat.indexOf('s') == -1) {
+			// No seconds in time format
+			this.noSeconds = true;
+			const $tp = this.datepicker.timepicker;
+			$tp.$seconds.parent().css('display', 'none');
+			$tp.$secondsText.css('display', 'none');
+			$tp.$secondsText.prev().css('display', 'none');
+		} else {
+			this.noSeconds = false;
+		}
+	},
+	update_datepicker_position: function() {
+		if(!this.frm) return;
+		// show datepicker above or below the input
+		// based on scroll position
+		const scroll_limit = $(window).scrollTop() + $(window).height()
+		const picker_bottom = this.$input.offset().top + this.$input.outerHeight() + 12 + this.datepicker.$datepicker.outerHeight()
+
+		var position = 'bottom left';
+		//if (picker_top > scroll_limit + window_scroll) {
+		if (picker_bottom >= scroll_limit) {
+			position = 'top left';
+			// bodge around the picker being 30 pixels shorter than
+			// it should be due to hiding seconds
+			if (this.noSeconds) this.datepicker.opts['offset'] = -28;
+		} else {
+			if (this.noSeconds) this.datepicker.opts['offset'] = 12;
+		}
+		this.datepicker.update('position', position);
+	},
 });
