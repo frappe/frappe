75779df36d6d2e5fad39ff1907256959fe167bdc
Aditya Hase <aditya@adityahase.com>
2018-12-27 22:23:48 +0530

    refactor(recorder): Remove unnecessary components

--- frappe/public/js/frappe/recorder/PathDetail.vue
+++ frappe/public/js/frappe/recorder/PathDetail.vue
@@ -1,15 +1,41 @@
 <template>
-	<div>
-		<request-list/>
-	</div>
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>UUID</th>
+				<th>CMD</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="request in requests" :key="request.uuid" v-bind="request">
+				<td>
+					<a :href="'#Request/' + request.uuid ">{{ request.uuid }}</a>
+				</td>
+				<td>
+					{{ request.cmd }}
+				</td>
+			</tr>
+		</tbody>
+	</table>
 </template>
 
 <script>
-import RequestList from "./RequestList.vue"
 export default {
 	name: "PathDetail",
-	components: {
-		RequestList,
-	}
+	data() {
+		return {
+			requests: [],
+		};
+	},
+	mounted() {
+		frappe.call({
+			method: "frappe.www.recorder.get_requests",
+			args: {
+				path: this.$route.param
+			}
+		}).then( r => {
+			this.requests = r.message
+		})
+	},
 };
 </script>
--- frappe/public/js/frappe/recorder/RecorderDetail.vue
+++ frappe/public/js/frappe/recorder/RecorderDetail.vue
@@ -1,15 +1,36 @@
 <template>
-	<div>
-		<path-list/>
-	</div>
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>Path</th>
+				<th>Count</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="path in paths" :key="path.path" v-bind="path">
+				<td>
+					<a :href="'#Path/' + path.path ">{{ path.path }}</a>
+				</td>
+				<td>
+					{{ path.count }}
+				</td>
+			</tr>
+		</tbody>
+	</table>
 </template>
 
 <script>
-import PathList from "./PathList.vue"
 export default {
 	name: "RecorderDetail",
-	components: {
-		PathList,
-	}
+	data() {
+		return {
+			paths: [],
+		};
+	},
+	mounted() {
+		frappe.call("frappe.www.recorder.get_paths").then( r => {
+			this.paths = r.message
+		})
+	},
 };
 </script>
