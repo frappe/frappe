f4444a587ae9daebb9b3f1fd3318dd8a021a2d89
Rushabh Mehta <rmehta@gmail.com>
2019-01-01 15:05:04 +0530

    Update utils.py

--- frappe/website/utils.py
+++ frappe/website/utils.py
@@ -186,7 +186,7 @@ def abs_url(path):
 		return
 	if path.startswith('http://') or path.startswith('https://'):
 		return path
-	if path.startswith('data:image/'):
+	if path.startswith('data:'):
 		return path
 	if not path.startswith("/"):
 		path = "/" + path
