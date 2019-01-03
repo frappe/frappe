ac5942e7537a9a84b3ff6c4b6c7e586765369c58
Anurag Mishra <mishranaman123@gmail.com>
2019-01-03 15:51:59 +0530

    ui testing

--- cypress/integration/calendar.js
+++ cypress/integration/calendar.js
@@ -1,54 +1,71 @@
-context('Login', () => {
+context('Calendar', () => {
 	beforeEach(() => {
 		cy.login('Administrator', 'qwe');
 		cy.visit('/desk');
 	});
 
-	it('navigates to calendar', () => {
+	it('navigates to calendar from awesomebar', () => {
 		cy.get('#navbar-search')
 			.type('calendar{downarrow}{enter}', { delay: 100 });
 		cy.location('hash').should('eq', '#calendar');
 	});
 
-	it("Click to Event and edit", () => {
+	it('drag n create event', () =>{
+		cy.visit('/desk#calendar');
+
+		cy.get('.fc-bg:first > table > tbody > tr')
+			.trigger('mousedown', {pageX: 691, pageY: 421, which: 1, force: true});
+		cy.get('.fc-bg:first > table > tbody > tr')
+			.trigger('mousemove', {pageX: 802, pageY: 421, which: 1, force: true});
+		cy.get('.fc-bg:first > table > tbody > tr')
+			.trigger('mouseup', {pageX: 802, pageY: 421, which: 1, force: true});
+
+		cy.fill_field('subject', 'JSFoo')
+		cy.fill_field('event_type', 'Public', 'Select')
+		cy.fill_field('description', 'This is JavaScript Event at Pune', 'Text Editor')
+		// cy.get('input[data-fieldname="subject"]')
+		// 	.type('JSFoo');
+		// cy.get('select[data-fieldname="event_type"]')
+		// 	.select('Public');
+		// cy.get('')
+		// 	.type("This is Jsfoo event");
+		cy.get(".btn.btn-primary.btn-sm.primary-action:not(.hide)").click();
+
+		cy.location('hash').should('eq', '#Form/Event/New%20Event%201');
+	});
+
+	it('click to event and edit', () => {
 		cy.visit('/desk#calendar');
 		cy.get('.fc-day-grid-event')
 			.click();
-		cy.get(".btn.btn-default.btn-xs:contains('Edit')").click();
-		cy.location("hash").should("eq", "#Form/Event/EV00001");
+		cy.get('.btn.btn-default.btn-xs:contains("Edit")').click();
+		cy.location('hash').should('eq', '#Form/Event/EV00001');
 	});
 
-	it("Visit from Doctype", () =>{
-		cy.visit("/desk#List/Event/List");
-		cy.get("a.cal").click({ force: true });
-		cy.location("hash").should("eq", '#calendar/Event');
+	it('visit from list', () => {
+		cy.visit('/desk#List/Event/List');
+		cy.get('a.cal').click({ force: true });
+		cy.location('hash').should('eq', '#calendar/Event');
 	});
 
-	it("drag n create event", () =>{
-		cy.visit("/desk#calendar");
-		cy.get(".fc-day[data-date='2019-01-13']")
-			.trigger('mousedown', {pageX: 691, pageY: 421, which: 1, force: true});
-		cy.get(".fc-day[data-date='2019-01-14']")
-			.trigger("mousemove", {pageX: 802, pageY: 421, which: 1, force: true});
-		cy.get(".fc-day[data-date='2019-01-14']")
-			.trigger("mouseup", {pageX: 802, pageY: 421, which: 1, force: true});
-		cy.location("hash").should("eq", "#Form/Event/New%20Event%201");
-	});
-
-	it("drag the event", () =>{
+	it('drag the event', () =>{
 		cy.visit('/desk#calendar');
+		var today
+		cy.window().its('frappe').then(frappe => {
+			today = frappe.datetime.get_today();
+		})
 		cy.get('.fc-day-grid-event')
 			.trigger('mousedown', {pageX: 349, pageY: 341, which: 1, force: true});
-		cy.get(".fc-day[data-date='2019-01-14']")
-			.trigger("mousemove", {pageX: 459, pageY: 459, which: 1, force: true});
-		cy.get(".fc-day[data-date='2019-01-14']")
-			.trigger("mouseup", {pageX: 459, pageY: 459, which: 1, force: true});
+		cy.get('.layout-main-section')
+			.trigger('mousemove', {pageX: 459, pageY: 459, which: 1, force: true});
+		cy.get('.layout-main-section')
+			.trigger('mouseup', {pageX: 459, pageY: 459, which: 1, force: true});
 
 	});
 
-	it("Visit from tools", () =>{
+	it('Visit from tools', () =>{
 		cy.visit('desk#modules/Desk');
-		cy.get("a:contains('Calendar')").click({ force: true });
-		cy.location("hash").should("eq", '#calendar');
+		cy.get('a:contains("Calendar")').click({ force: true });
+		cy.location('hash').should('eq', '#calendar');
 	});
 });
