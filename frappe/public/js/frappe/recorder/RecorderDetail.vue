a2aed90c4ff645e45f8b965a73c8dfdb51391f3f
Aditya Hase <aditya@adityahase.com>
2019-01-07 11:06:44 +0530

    feat(recorder): Allow sorting requests

--- frappe/public/js/frappe/recorder/RecorderDetail.vue
+++ frappe/public/js/frappe/recorder/RecorderDetail.vue
@@ -2,30 +2,20 @@
 	<table class="table table-hover">
 		<thead>
 			<tr>
-				<th>UUID</th>
-				<th>Path</th>
-				<th>CMD</th>
-				<th>Time</th>
-				<th>Method</th>
+				<th>Index<button v-on:click=" key='index'">Sort</button></th>
+				<th>Time<button v-on:click=" key='time'">Sort</button></th>
+				<th>Method<button v-on:click=" key='method'">Sort</button></th>
+				<th>Path<button v-on:click=" key='path'">Sort</button></th>
+				<th>CMD<button v-on:click=" key='cmd'">Sort</button></th>
 			</tr>
 		</thead>
 		<tbody>
-			<router-link style="cursor: pointer" :to="{name: 'request-detail', params: {request_uuid: request.uuid}}" tag="tr"  v-for="request in requests" :key="request.uuid" v-bind="request">
-				<td>
-					{{ request.uuid }}
-				</td>
-				<td>
-					{{ request.path }}
-				</td>
-				<td>
-					{{ request.cmd }}
-				</td>
-				<td>
-					{{ request.time }}
-				</td>
-				<td>
-					{{ request.method }}
-				</td>
+			<router-link style="cursor: pointer" :to="{name: 'request-detail', params: {request_uuid: request.uuid}}" tag="tr"  v-for="request in sortedRequests" :key="request.index" v-bind="request">
+				<td>{{ request.index }}</td>
+				<td>{{ request.time }}</td>
+				<td>{{ request.method }}</td>
+				<td>{{ request.path }}</td>
+				<td>{{ request.cmd }}</td>
 			</router-link>
 		</tbody>
 	</table>
@@ -37,12 +27,18 @@ export default {
 	data() {
 		return {
 			requests: [],
+			key: "index",
 		};
 	},
 	mounted() {
 		frappe.call("frappe.www.recorder.get_requests").then( r => {
 			this.requests = r.message
 		})
+	},
+	computed: {
+		sortedRequests: function() {
+			return this.requests.sort((a,b) => (a[this.key] > b[this.key]) ? 1 : -1)
+		}
 	},
 };
 </script>
