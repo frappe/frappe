8840a179991caafcbd9a70bd853ad1b057dce063
Aditya Hase <aditya@adityahase.com>
2019-01-09 11:19:08 +0530

    feat(dashboard): Add timespan filter

--- frappe/core/page/dashboard/dashboard.js
+++ frappe/core/page/dashboard/dashboard.js
@@ -1,29 +1,50 @@
 frappe.pages['dashboard'].on_page_load = function(wrapper) {
+	var me = this
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
 		title: 'Dashboard',
 		single_column: true
-	});
+	})
 
 	$(`<div class="dashboard page-main-content">
-		<div class="dashboard-graph"></div>
-	</div>`).appendTo(this.page.main);
+		<div id="dashboard-graph"></div>
+	</div>`).appendTo(this.page.main)
 
-	frappe.call({
-		method: "frappe.core.page.dashboard.dashboard.get_data",
-		args: {
-			dashboard_name: "erpnext.accounts.dashboard.get",
-		},
-		callback: function(message) {
-			const data = message.message
-			var chart_args = {
-				data: {
-					datasets: data.datasets,
-					labels: data.labels,
-				},
-				type: 'line',
+	this.timespans = ["Last Week", "Last Month", "Last Quarter", "Last Year"];
+	this.filters = {
+		timespan: "Week",
+	}
+	this.timespan_select = this.page.add_select(__("Timespan"),
+		this.timespans.map(d => {
+			return {"label": __(d), value: d }
+		})
+	)
+
+	this.timespan_select.on("change", function() {
+		me.filters.timespan = this.value
+		me.create_chart()
+	});
+
+	this.create_chart = function() {
+		frappe.call({
+			method: "frappe.core.page.dashboard.dashboard.get_data",
+			args: {
+				dashboard_name: "erpnext.accounts.dashboard.get",
+				filters: this.filters,
+			},
+			callback: function(message) {
+				const data = message.message
+				var chart_args = {
+					data: {
+						datasets: data.datasets,
+						labels: data.labels,
+					},
+					type: 'line',
+				}
+				new Chart('#dashboard-graph', chart_args)
 			}
-			new Chart('.dashboard-graph', chart_args)
-		}
-	})
+		})
+	}
+
+	this.create_chart()
 }
--- frappe/core/page/dashboard/dashboard.py
+++ frappe/core/page/dashboard/dashboard.py
@@ -1,10 +1,12 @@
 # Copyright (c) 2019, Frappe Technologies Pvt. Ltd. and Contributors
 # MIT License. See license.txt
 from __future__ import unicode_literals
+import json
 import frappe
 
 
 @frappe.whitelist()
-def get_data(dashboard_name):
-    results = frappe.get_attr(dashboard_name)()
+def get_data(dashboard_name, filters=None):
+    filters = json.loads(filters)
+    results = frappe.get_attr(dashboard_name)(filters=frappe._dict(filters))
     return results
