da2d6fe61d937ed67efca8269d46ba1876757158
Anurag Mishra <mishranaman123@gmail.com>
2019-01-10 17:31:35 +0530

    ui-fixes

--- frappe/doc_subscription.py
+++ frappe/doc_subscription.py
@@ -4,6 +4,7 @@
 from __future__ import unicode_literals
 import frappe
 import json
+import frappe.utils
 from frappe.utils.background_jobs import enqueue
 from frappe import _
 
@@ -21,38 +22,45 @@ def add_subcription(doctype, doc_name, user_email):
 
 @frappe.whitelist()
 def get_message(doc_name, doctype):
-	version = frappe.get_list("Version", filters = [["docname","=",doc_name],["modified","like","%"+frappe.utils.nowdate()+"%"]],fields=["ref_doctype","data"])
+	version = frappe.get_list("Version", filters = [["docname","=",doc_name],["modified","like","%"+frappe.utils.nowdate()+"%"]],fields=["ref_doctype","data","modified"])
+	html = ""
+	activity = ""
+	changed_fields = ""
+	communication = ""
 	if version:
-		html = ""
-		activity = ""
-		changed_fields = ""
-		for d in version:
-			if isinstance(d.data, frappe.string_types):
-				change = frappe._dict(json.loads(d.data))
+		for d1 in version:
+			if isinstance(d1.data, frappe.string_types):
+				change = frappe._dict(json.loads(d1.data))
+				time = frappe.utils.format_datetime(d1.modified,"hh:mm a")
 				if change.comment:
-					activity += "<p>\n"+ change.comment
-					activity += "</p>\n"
+					activity += "<li> "+ change.comment +"<span style ='color:#8d99a6!important'> ("+ time
+					activity += ") </span></li>"
 				if change.changed:
 					for d in change.changed:
 						d[1] = d[1] if d[1] else " None "
 						d[2] = d[2] if d[2] else " None "
 						d[0] = d[0] if d[0] else "None"
-						changed_fields += "<p>\n"+d[0]+" changed from \n"
-						changed_fields += d[1] + " to \n"
-						changed_fields += d[2] + "\n</p>"
+						changed_fields += "<li>"+d[0]+" changed from "
+						changed_fields += d[1] + " to "
+						changed_fields += d[2] + "<span style ='color:#8d99a6!important'> (" +time +")</span> </li>"
 		if activity != "":
-			html += '\n<h4>Activity</h4>\n' + activity
+			html += '<h4>Activity</h4><ul>' + activity + '</ul>'
 		if changed_fields != "":
-			html += '\n<h4>Changed fields</h4>\n' + changed_fields
+			html += '<h4>Changed fields</h4><ul>' + changed_fields+ '</ul>'
 
-	doc = frappe.get_doc(doctype, doc_name)
-	if doc._comments:
-		html += "<h4>Comments</h4>\n"
-		com = json.loads(doc._comments)
+	comments = frappe.db.get_value(doctype, doc_name, "_comments")
+	if comments:
+		com = json.loads(comments)
 		for comment in com:
 			dictio = frappe._dict(comment)
-			html += dictio.comment + "\n<p> By: "
-			html += dictio.by + "</p>\n"
+			if len(frappe.get_list("Communication", filters = [["name","=",dictio.name],["modified","like","%"+frappe.utils.nowdate()+"%"]])) != 0:
+				communication += "<li>"+ dictio.comment + " By: "
+				communication += dictio.by + "</li>"
+
+		if communication != "":
+			html += "<h4>Comments</h4><ul>" + communication+ '</ul>'
+
+
 	return html
 
 @frappe.whitelist()
@@ -61,7 +69,7 @@ def sent_email_alert(doc_name, doctype, receiver, message):
 		email_args = {
 			"recipients": [receiver],
 			"message": message,
-			"subject": 'Documemt Follow {0}:{1}'.format(doctype,doc_name),
+			"subject": 'Document Follow Notification',
 			"reference_doctype": doctype,
 			"reference_name": doc_name,
 			"delayed": False,
@@ -71,8 +79,20 @@ def sent_email_alert(doc_name, doctype, receiver, message):
 	frappe.db.commit()
 
 def sending_mail():
-	data = frappe.get_list("Document Follow", fields = ["ref_doctype","ref_docname","user"])
-	for d in data:
-		message = get_message(d.ref_docname, d.ref_doctype)
+	users = frappe.get_all("Document Follow", distinct=1, fields=["user"])
+	#data = frappe.get_list("Document Follow", fields = ["user"])
+	for d in users:
+		print("----------------------->>>>>",d.user)
+		data = frappe.get_all("Document Follow", filters={"user" : d.user}, distinct=1, fields=["ref_doctype","ref_docname"])
+		message = ""
+		for d2 in data:
+			content = get_message(d2.ref_docname, d2.ref_doctype)
+			if content != '':
+				message += """<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+				<div style='margin-bottom: 20px; background-color: #fff; border: 1px solid transparent; border-radius: 4px; -webkit-box-shadow: 0 1px 1px rgba(0,0,0,.05); box-shadow: 0 1px 1px rgba(0,0,0,.05); border-color: #ddd;'>
+					<div style='color: #333; background-color: #f5f5f5; border-color: #ddd; padding: 10px 15px; border-bottom: 1px solid transparent; border-top-left-radius: 3px; border-top-right-radius: 3px; color:#8d99a6!important'>{0} : {1} <button>Unsubscribe</button></div>
+					<div style='padding: 15px; color:#36414C!important'> {2} </div>
+				</div>""".format(d2.ref_doctype, d2.ref_docname, content)
+
 		if message != "":
 			sent_email_alert(d.ref_docname, d.ref_doctype, d.user, message)
