4f5abcbcf11b121f317e4f06538065f12da9d54b
Suraj Shetty <surajshetty3416@gmail.com>
2019-01-12 18:38:21 +0530

    Add button to delete user permissions in bulk

---
+++ frappe/core/doctype/user_permission/user_permission_list.js
@@ -0,0 +1,32 @@
+frappe.listview_settings['User Permission'] = {
+	onload: function(listview) {
+		listview.page.add_inner_button(__("Clear User Permissions"), () => {
+			let dialog = new frappe.ui.Dialog({
+				title: 'Clear User Permissions',
+				fields: [
+					{fieldname: 'user', label: __('For User'), fieldtype: 'Link', options: 'User', reqd: 1},
+					{fieldname: 'for_doctype', label: __('For Document Type'), fieldtype: 'Link', options: 'DocType', reqd: 1},
+				],
+				primary_action: (data) => {
+					// mandatory not filled
+					if (!data) return;
+
+					frappe.confirm(__('Are you sure?'), () => {
+						frappe.xcall('frappe.core.doctype.user_permission.user_permission.clear_user_permissions', 	data)
+							.then((data) => {
+								dialog.hide();
+								frappe.show_alert({
+									message: __('{0} records deleted', [data]),
+									indicator: 'green'
+								});
+								listview.refresh();
+							});
+
+					}, () => {});
+
+				},
+				primary_action_label: __('Clear')
+			}).show();
+		});
+	}
+};
