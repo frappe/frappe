82bb567ace99add58c20cc394a720246953fafa0
Aditya Hase <aditya@adityahase.com>
2019-01-15 12:47:05 +0530

    fix(dashboard): Remove global filters

--- frappe/core/page/dashboard/dashboard.js
+++ frappe/core/page/dashboard/dashboard.js
@@ -27,53 +27,6 @@ frappe.pages['dashboard'].on_page_load = function(wrapper) {
 		<div id="dashboard-graph" class="row"></div>
 	</div>`).appendTo(this.page.main)
 
-	this.timespans = ["Last Week", "Last Month", "Last Quarter", "Last Year"];
-	this.timegrains = ["Daily", "Weekly", "Monthly", "Quarterly"];
-	this.filters = {
-		timespan: "Last Week",
-		timegrain: "Daily",
-		account: "Cash - GTPL",
-	}
-
-	this.account_select = this.page.add_field({
-		fieldname: "account",
-		label: __("Account"),
-		fieldtype: "Link",
-		options: "Account",
-		get_query: {
-			doctype: "Account",
-			filters: {
-				company: "Gadget Technologies Pvt. Ltd.",
-			}
-		}
-	}).$wrapper.find("input")
-
-	this.timespan_select = this.page.add_select(__("Time Span"),
-		this.timespans.map(d => {
-			return {"label": __(d), value: d }
-		})
-	)
-
-	this.timegrain_select = this.page.add_select(__("Time Grain"),
-		this.timegrains.map(d => {
-			return {"label": __(d), value: d }
-		})
-	)
-	this.account_select.on("change", function() {
-		me.filters.account = this.value
-		me.create_chart()
-	});
-
-	this.timespan_select.on("change", function() {
-		me.filters.timespan = this.value
-		me.create_chart()
-	});
-
-	this.timegrain_select.on("change", function() {
-		me.filters.timegrain = this.value
-		me.create_chart()
-	});
-
 	this.create_chart = function(wrapper, chart, filters) {
 		frappe.call({
 			method: "frappe.core.page.dashboard.dashboard.get_data",
