aa3a0a89dd8406bfc2add0dfbd4e5a06f4986785
Aditya Hase <aditya@adityahase.com>
2019-01-15 12:35:45 +0530

    feat(dashboard): Translate Show Dashboard button label

--- frappe/core/doctype/dashboard/dashboard.js
+++ frappe/core/doctype/dashboard/dashboard.js
@@ -3,6 +3,6 @@
 
 frappe.ui.form.on('Dashboard', {
 	refresh: function(frm) {
-		frm.add_custom_button("Show Dashboard", () => frappe.set_route('dashboard', frm.doc.name))
+		frm.add_custom_button(__("Show Dashboard"), () => frappe.set_route('dashboard', frm.doc.name))
 	}
 });
