afb9b496bcdbdec7a50f063f23c035609075150d
Anurag Mishra <mishranaman123@gmail.com>
2019-01-15 20:02:36 +0530

    Timeline, email template, Ui fixes, code refractor

--- frappe/doc_subscription.py
+++ frappe/doc_subscription.py
@@ -7,6 +7,7 @@ import json
 import frappe.utils
 from frappe.utils.background_jobs import enqueue
 from frappe import _
+from pprint import pprint
 
 @frappe.whitelist()
 def add_subcription(doctype, doc_name, user_email):
@@ -22,38 +23,103 @@ def add_subcription(doctype, doc_name, user_email):
 
 @frappe.whitelist()
 def get_message(doc_name, doctype):
+	html = get_version(doctype, doc_name) + get_comments(doctype, doc_name)
+	t = sorted(html, key=lambda k: k['time'], reverse=True)
+	return t
+#	for data in t:
+#		mess += data['content']
+#	if html != "":
+#		message += "<ul style='list-style-type: none;'>" + mess+ '</ul>'
+#		message = message.replace("</div>"," ")
+#		message = message.replace("<div>"," ")
+
+#	return message
+
+@frappe.whitelist()
+def sent_email_alert(doc_name, doctype, receiver, docinfo,timeline):
+	if receiver:
+		email_args = {
+			'template' :'doc_subscription',
+			'args' : {
+				"docinfo" : docinfo,
+				"timeline" : timeline,
+			},
+			"recipients": [receiver],
+			"subject": 'Document Follow Notification',
+			"reference_doctype": doctype,
+			"reference_name": doc_name,
+			"delayed": False,
+		}
+	enqueue(method=frappe.sendmail, now=True, queue='short', timeout=300, async=True, **email_args)
+	frappe.db.commit()
+
+def sending_mail():
+	users = frappe.get_all("Document Follow", distinct=1, fields=["user"])
+	message = []
+	for d in users:
+		data = frappe.get_all("Document Follow", filters={"user" : d.user}, distinct=1, fields=["ref_doctype","ref_docname","user"])
+		message = []
+		for d2 in data:
+			content = get_message(d2.ref_docname, d2.ref_doctype)
+			if content != []:
+				message = message + content
+				#message += """<div style='margin-bottom: 20px; background-color: #fff; border: 1px solid transparent; border-radius: 4px; -webkit-box-shadow: 0 1px 1px rgba(0,0,0,.05); box-shadow: 0 1px 1px rgba(0,0,0,.05); border-color: #ddd;'>
+				#	<div style='color: #333; background-color: #f5f5f5; border-color: #ddd; padding: 10px 15px; border-bottom: 1px solid transparent; border-top-left-radius: 3px; border-top-right-radius: 3px; color:#8d99a6!important'>{0}: {1} </div>
+				#	<div style='padding: 15px; color:#36414C!important'> {2} </div>
+				#</div>""".format(d2.ref_doctype, d2.ref_docname, content)
+		#if message != "":
+		if message != []:
+			sent_email_alert(d2.ref_docname, d2.ref_doctype, d.user, data, message)
+
+def get_version(doctype,doc_name):
+	timeline = []
 	version = frappe.get_list("Version", filters = [["docname","=",doc_name],["modified","like","%"+frappe.utils.nowdate()+"%"]],fields=["ref_doctype","data","modified"])
-	html = ""
-	message = ""
 	if version:
 		for d1 in version:
 			if isinstance(d1.data, frappe.string_types):
 				change = frappe._dict(json.loads(d1.data))
-				#print(change)
 				time = frappe.utils.format_datetime(d1.modified,"hh:mm a")
 				if change.comment:
-					html += "<li> "+"<span style ='color:#8d99a6!important'>"+time+ ": </span>"
-					html += change.comment + "</li>"
+					timeline.append({
+							"time": d1.modified,
+							"content" : "<li><span style ='color:#8d99a6!important'>" + time + ": </span>" +change.comment + "</li>",
+							"doctype" : doctype,
+							"doc_name" : doc_name
+						})
 				if change.changed:
 					for d in change.changed:
-						d[1] = d[1] if d[1] else " None "
-						d[2] = d[2] if d[2] else " None "
-						d[0] = d[0] if d[0] else "None"
-						html += "<li><span style ='color:#8d99a6!important'>Field " +time +": </span>"+d[0]+" changed from "
-						html += d[1] + " to "
-						html += d[2] + "</li>"
+						d[1] = d[1] if d[1] else ' '
+						d[2] = d[2] if d[2] else ' '
+						d[0] = d[0] if d[0] else ' '
+						timeline.append({
+							"time": d1.modified,
+							"content" : "<li><span style ='color:#8d99a6!important'>" +time +": </span>Field: <b>"+cleantext(d[0])+'</b> changed from <b>"' + cleantext(d[1]) + '"</b>  to <b>"' +cleantext(d[2]) + '"</b> </li>',
+							"doctype" : doctype,
+							"doc_name" : doc_name
+						})
 				if change.row_changed:
 					for d in change.row_changed:
-						d[2] = d[2] if d[2] else " None "
-						d[0] = d[0] if d[0] else "None"
-						d[3][0][1] = d[3][0][1] if d[3][0][1] else "None"
-						html += "<li><span style ='color:#8d99a6!important'>" +time +": </span> Table Field: "+d[0]+" Row# "
-						html += str(d[1]) + " Field: "
-						html += d[3][0][0] + " Changed from " + d[3][0][1] + " to " + d[3][0][2] + "</li>"
+						d[2] = d[2] if d[2] else ' '
+						d[0] = d[0] if d[0] else ' '
+						d[3][0][1] = d[3][0][1] if d[3][0][1] else ' '
+						timeline.append({
+							"time": d1.modified,
+							"content" : "<li><span style ='color:#8d99a6!important'>" +time +": </span> Table Field: <b>"+d[0]+"</b> Row# " + str(d[1]) + " Field: <b>" +cleantext(d[3][0][0]) + '</b> changed from <b>"' + cleantext(d[3][0][1]) + '"</b>  to <b>"' + cleantext(d[3][0][2]) + '"</b> </li>',
+							"doctype" : doctype,
+							"doc_name" : doc_name
+						})
 				if change.added:
 					for d in change.added:
-						html += "<li><span style ='color:#8d99a6!important'>" +time +": </span>Row Added to Table Field "+d[0]
+						timeline.append({
+							"time": d1.modified,
+							"content" : "<li><span style ='color:#8d99a6!important'>" +time +": </span>Row Added to Table Field <b>"+d[0]+"</b></li>",
+							"doctype" : doctype,
+							"doc_name" : doc_name
+						})
+	return timeline
 
+def get_comments(doctype, doc_name):
+	timeline = []
 	comments = frappe.db.get_value(doctype, doc_name, "_comments")
 	if comments:
 		com = json.loads(comments)
@@ -61,42 +127,18 @@ def get_message(doc_name, doctype):
 			dictio = frappe._dict(comment)
 			if len(frappe.get_list("Communication", filters = [["name","=",dictio.name],["modified","like","%"+frappe.utils.nowdate()+"%"]])) != 0:
 				modified = frappe.db.get_value("Communication", dictio.name, "modified")
-				print(dictio.comment)
 				time = frappe.utils.format_datetime(modified,"hh:mm a")
-				html += "<li><span style ='color:#8d99a6!important'>" +time +": </span>"+ dictio.comment + " By: "
-				html += dictio.by + "</li>"
+				timeline.append({
+					"time": modified,
+					"content": "<li><span style ='color:#8d99a6!important'>" +time +": </span>"+cleantext(dictio.comment)+ "comment By: " + dictio.by + "</li>",
+					"doctype" : doctype,
+					"doc_name" : doc_name
+				})
+	return timeline
 
-		if html != "":
-			message += "<ul style='list-style-type: none;'>" + html+ '</ul>'
-	return message
+def cleantext(s):
+	s = s.replace("<div>"," ")
+	s = s.replace("</div>"," ")
+	return s
 
-@frappe.whitelist()
-def sent_email_alert(doc_name, doctype, receiver, message):
-	if receiver:
-		email_args = {
-			"recipients": [receiver],
-			"message": message,
-			"subject": 'Document Follow Notification',
-			"reference_doctype": doctype,
-			"reference_name": doc_name,
-			"delayed": False,
-		}
-		#frappe.sendmail(**email_args)
-	enqueue(method=frappe.sendmail, now=True, queue='short', timeout=300, async=True, **email_args)
-	frappe.db.commit()
 
-def sending_mail():
-	users = frappe.get_all("Document Follow", distinct=1, fields=["user"])
-	#data = frappe.get_list("Document Follow", fields = ["user"])
-	for d in users:
-		data = frappe.get_all("Document Follow", filters={"user" : d.user}, distinct=1, fields=["ref_doctype","ref_docname"])
-		message = ""
-		for d2 in data:
-			content = get_message(d2.ref_docname, d2.ref_doctype)
-			if content != '':
-				message += """<div style='margin-bottom: 20px; background-color: #fff; border: 1px solid transparent; border-radius: 4px; -webkit-box-shadow: 0 1px 1px rgba(0,0,0,.05); box-shadow: 0 1px 1px rgba(0,0,0,.05); border-color: #ddd;'>
-					<div style='color: #333; background-color: #f5f5f5; border-color: #ddd; padding: 10px 15px; border-bottom: 1px solid transparent; border-top-left-radius: 3px; border-top-right-radius: 3px; color:#8d99a6!important'>{0} : {1} </div>
-					<div style='padding: 15px; color:#36414C!important'> {2} </div>
-				</div>""".format(d2.ref_doctype, d2.ref_docname, content)
-		if message != "":
-			sent_email_alert(d.ref_docname, d.ref_doctype, d.user, message)
