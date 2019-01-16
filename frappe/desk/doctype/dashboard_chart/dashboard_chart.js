241d13743fd4d86da2d5cec2d140a0b7e509b734
Aditya Hase <aditya@adityahase.com>
2019-01-16 13:39:55 +0530

    refactor(dashboard): Move DocTypes to Desk module
    - Rename Chart to Dashboard Chart

--- frappe/desk/doctype/dashboard_chart/dashboard_chart.js
+++ frappe/desk/doctype/dashboard_chart/dashboard_chart.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2019, Frappe Technologies and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Chart', {
+frappe.ui.form.on('Dashboard Chart', {
 	refresh: function(frm) {
 
 	}
--- frappe/desk/doctype/dashboard_chart/test_dashboard_chart.py
+++ frappe/desk/doctype/dashboard_chart/test_dashboard_chart.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
-class TestChart(unittest.TestCase):
+class TestDashboardChart(unittest.TestCase):
 	pass
