874dedc0670301f1407350339362b69f3dba9fbd
Aditya Hase <aditya@adityahase.com>
2019-01-19 23:32:13 +0530

    feat(recorder): Add First and Last button in pagination

--- frappe/public/js/frappe/recorder/RecorderDetail.vue
+++ frappe/public/js/frappe/recorder/RecorderDetail.vue
@@ -12,10 +12,10 @@
 		<table class="table table-hover table-condensed">
 			<thead>
 				<tr>
-					<th style="width:10%"><span style="margin-right:5px">Index</span><i @click="sort('index')" class="glyphicon" :class="glyphicon('index')"></i></th>
-					<th style="width:23%"><span style="margin-right:5px">Time</span><i @click="sort('time')" class="glyphicon" :class="glyphicon('time')"></i></th>
+					<th style="width:8%"><span style="margin-right:5px">Index</span><i @click="sort('index')" class="glyphicon" :class="glyphicon('index')"></i></th>
+					<th style="width:24%"><span style="margin-right:5px">Time</span><i @click="sort('time')" class="glyphicon" :class="glyphicon('time')"></i></th>
 					<th style="width:10%"><span style="margin-right:5px">Duration</span><i @click="sort('duration')" class="glyphicon" :class="glyphicon('duration')"></i></th>
-					<th style="width:7%"><span>Method</span></th>
+					<th style="width:8%"><span>Method</span></th>
 					<th style="width:25%"><span style="margin-right:5px">Path</span><i @click="sort('path')" class="glyphicon" :class="glyphicon('path')"></i></th>
 					<th style="width:25%"><span style="margin-right:5px">CMD</span><i @click="sort('cmd')" class="glyphicon" :class="glyphicon('cmd')"></i></th>
 				</tr>
@@ -87,7 +87,11 @@ export default {
 			const current_page = this.query.pagination.page;
 			const total_pages = this.query.pagination.total;
 			return [{
-				label:"Previous",
+				label: "First",
+				number: 1,
+				status: (current_page == 1) ? "disabled" : "",
+			},{
+				label: "Previous",
 				number: Math.max(current_page - 1, 1),
 				status: (current_page == 1) ? "disabled" : "",
 			}, {
@@ -95,8 +99,12 @@ export default {
 				number: current_page,
 				status: "active",
 			}, {
-				label:"Next",
+				label: "Next",
 				number: Math.min(current_page + 1, total_pages),
+				status: (current_page == total_pages) ? "disabled" : "",
+			}, {
+				label: "Last",
+				number: total_pages,
 				status: (current_page == total_pages) ? "disabled" : "",
 			}];
 		}
