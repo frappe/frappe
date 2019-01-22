ce87a68baa012b3eba90c11cf921947593f3f18a
rohitwaghchaure <rohitw1991@gmail.com>
2019-01-22 22:48:09 +0530

    Fix: auto email report not showing all columns in email (#6806)

--- frappe/core/doctype/report/report.py
+++ frappe/core/doctype/report/report.py
@@ -125,7 +125,9 @@ class Report(Document):
 			# standard report
 			params = json.loads(self.json)
 
-			if params.get('columns'):
+			if params.get('fields'):
+				columns = params.get('fields')
+			elif params.get('columns'):
 				columns = params.get('columns')
 			else:
 				columns = [['name', self.ref_doctype]]
@@ -163,6 +165,7 @@ class Report(Document):
 				user=user)
 
 			_columns = []
+
 			for column in columns:
 				meta = frappe.get_meta(column[1])
 				field = [meta.get_field(column[0]) or frappe._dict(label=meta.get_label(column[0]), fieldname=column[0])]
