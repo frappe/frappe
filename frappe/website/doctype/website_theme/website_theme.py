d66f9ce864bfa2c8d9d31fee2f025171b79c5177
Faris Ansari <netchamp.faris@gmail.com>
2019-01-27 16:49:52 +0530

    fix: Absolute css url

--- frappe/website/doctype/website_theme/website_theme.py
+++ frappe/website/doctype/website_theme/website_theme.py
@@ -64,7 +64,7 @@ class WebsiteTheme(Document):
 		content = content.replace('\n', '\\n')
 		command = ['node', 'generate_bootstrap_theme.js', file_name, content]
 		frappe.commands.popen(command, cwd=frappe.get_app_path('frappe', '..'), shell=False)
-		self.css_file_url = 'assets/frappe/website_theme/' + file_name
+		self.css_file_url = '/assets/frappe/website_theme/' + file_name
 
 	def use_theme(self):
 		use_theme(self.name)
