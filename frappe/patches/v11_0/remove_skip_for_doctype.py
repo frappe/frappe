25487af1f4839ed79009ea1aa9ceeee7ab44e518
Suraj Shetty <surajshetty3416@gmail.com>
2019-01-29 20:08:36 +0530

    Remove unwanted import

--- frappe/patches/v11_0/remove_skip_for_doctype.py
+++ frappe/patches/v11_0/remove_skip_for_doctype.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 import frappe
-from frappe.utils import now
 from frappe.desk.form.linked_with import get_linked_doctypes
 from frappe.patches.v11_0.replicate_old_user_permissions import get_doctypes_to_skip
 
