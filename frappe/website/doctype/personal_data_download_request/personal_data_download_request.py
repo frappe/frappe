da5dad810f9d855686a80eed35d1e111322dbfad
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-01-29 12:39:07 +0530

    get unlinked user data

--- frappe/website/doctype/personal_data_download_request/personal_data_download_request.py
+++ frappe/website/doctype/personal_data_download_request/personal_data_download_request.py
@@ -9,4 +9,18 @@ from frappe.model.document import Document
 from frappe.desk.form.linked_with import get_linked_doctypes
 
 class PersonalDataDownloadRequest(Document):
-	pass
+	def after_insert(self):
+		if self.user in ['Administrator', 'Guest']:
+			frappe.throw(_("This user cannot request to download data"))
+		else:
+			personal_data = get_unlinked_user_data(self.user)
+
+def get_unlinked_user_data(user):
+	""" returns user data not linked to User doctype """
+	hooks = frappe.get_hooks("user_privacy_documents")
+	data = {}
+	for hook in hooks:
+		d = frappe.get_all(hook.get('doctype'), {hook.get('email_field'): user},["*"])
+		if d:
+			data.update({ hook.get('doctype'):d })	
+	return data
