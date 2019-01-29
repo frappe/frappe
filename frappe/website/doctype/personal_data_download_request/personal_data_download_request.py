43fec2b2c02c264f0766e29f0ed93fe64301ffa4
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-01-29 12:41:14 +0530

    generate file and send email

--- frappe/website/doctype/personal_data_download_request/personal_data_download_request.py
+++ frappe/website/doctype/personal_data_download_request/personal_data_download_request.py
@@ -15,6 +15,23 @@ class PersonalDataDownloadRequest(Document):
 		else:
 			personal_data = get_unlinked_user_data(self.user)
 			personal_data.update(get_linked_user_data(self.user))
+			
+			self.generate_file_and_send_mail(personal_data)
+
+	def generate_file_and_send_mail(self, personal_data):
+		"""generate the file link for download"""
+		user_name = self.user_name.replace(' ','-')
+		f = frappe.get_doc({
+			'doctype': 'File',
+			'file_name': 'Personal-Data-'+user_name+'.json',
+			'content': str(personal_data),
+			'is_private': True
+		})
+		f.save()
+		frappe.sendmail(recipients= self.user,
+		subject=_("ERPNext: User Data"),
+		message= "Your data is ready, <a href="+f.file_url+" download>Click here to download your data</a>",
+		header=["ERPNext: User Data", "green"])
 
 def get_unlinked_user_data(user):
 	""" returns user data not linked to User doctype """
