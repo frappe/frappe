ab6e1a312cdc8f0bd4e82db4619ed95f402f70cb
Aditya Hase <aditya@adityahase.com>
2019-02-06 09:37:46 +0530

    feat(dashboard): Show filters in a table

--- frappe/desk/doctype/dashboard_chart/dashboard_chart.js
+++ frappe/desk/doctype/dashboard_chart/dashboard_chart.js
@@ -2,6 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Dashboard Chart', {
+	onload: function(frm) {
+		var wrapper = $(frm.fields_dict["filters_json"].wrapper).empty();
+		let filter_table = $(`<table class="table table-bordered">
+			<thead>
+				<tr>
+					<td>${ __("Filter") }</td>
+					<td>${ __("Value") }</td>
+				</tr>
+			</thead>
+			<tbody></tbody>
+		</table>`);
+
+		const filters = JSON.parse(frm.doc.filters_json);
+
+		Object.keys(filters).forEach(key => {
+			const filter_row = $(`<tr>
+				<td>${frappe.model.unscrub(key)}</td>
+				<td>${filters[key]}</td>
+			</tr>`);
+			filter_table.find('tbody').append(filter_row);
+		});
+
+		wrapper.append(filter_table);
+	},
 	refresh: function(frm) {
 
 	}
