83c0e3154649c31e7ea2ba4725e90046d456b931
Prateeksha Singh <pratu16x7@gmail.com>
2019-02-07 09:52:08 +0530

    feat(module_links): show onboard untouches points

--- frappe/config/__init__.py
+++ frappe/config/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from frappe import _
 import frappe
+from frappe.desk.moduleview import get_data
 from six import iteritems
 
 def get_modules_from_all_apps_for_user(user=None):
@@ -51,9 +52,34 @@ def get_modules_from_app(app):
 				to_add = False
 
 			if to_add:
+				onboard_present = is_onboard_present(m) if show_onboard(m) else False
+				m["onboard_present"] = onboard_present
 				active_modules_list.append(m)
 
 	return active_modules_list
+
+def show_onboard(module):
+	return module.get("type") == "module" and module.get("category") in ["Modules", "Domains"]
+
+def is_onboard_present(module):
+	print(module["module_name"])
+	exists_cache = {}
+	def exists(name, link_type):
+		exists = exists_cache.get(name)
+		if not exists:
+			if link_type == "doctype" and not frappe.db.get_value('DocType', name, 'issingle'):
+				exists = frappe.db.count(name)
+			else:
+				exists = True
+			exists_cache[name] = exists
+		return exists
+
+	sections = get_data(module["module_name"], False)
+	for section in sections:
+		for item in section["items"]:
+			if exists(item.get("name"), item.get("type")):
+				return True
+	return False
 
 def is_domain(module):
 	return module.get("category") == "Domains"
