192cba460ed6faa1760274769ac9f11ad668b500
Sahil Khan <sahil@erpnext.com>
2019-02-08 18:22:01 +0530

    fix(email_group): show correct total_subscribers after inserting or deleting email group members

--- frappe/email/doctype/email_group_member/email_group_member.py
+++ frappe/email/doctype/email_group_member/email_group_member.py
@@ -7,7 +7,15 @@ import frappe
 from frappe.model.document import Document
 
 class EmailGroupMember(Document):
-	pass
+	def after_delete(self):
+		print("*"*50, "delete")
+		email_group = frappe.get_doc('Email Group', self.email_group)
+		email_group.update_total_subscribers()
+
+	def after_insert(self):
+		print("*"*50, "add")
+		email_group = frappe.get_doc('Email Group', self.email_group)
+		email_group.update_total_subscribers()
 
 def after_doctype_insert():
 	frappe.db.add_unique("Email Group Member", ("email_group", "email"))
