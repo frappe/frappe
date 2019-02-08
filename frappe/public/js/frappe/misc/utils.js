53f802c96e9ca6ff3d78e00eb3ebfc3f0cf79413
Saif Ur Rehman <saifi0102@gmail.com>
2019-02-08 22:08:13 +0500

    fix: Update DataTable hook for columnTotal

--- frappe/public/js/frappe/misc/utils.js
+++ frappe/public/js/frappe/misc/utils.js
@@ -658,13 +658,17 @@ Object.assign(frappe.utils, {
 		return route;
 	},
 
-	report_total_accumulator: function(column, values, type) {
-		if (column.fieldtype == "Percent" || type === "mean") {
-			return values.reduce((a, b) => ({content: a.content + flt(b.content)})).content / values.length;
-		} else if (frappe.model.is_numeric_field(column.fieldtype)) {
-			return values.reduce((a, b) => ({content: a.content + flt(b.content)})).content;
+	report_column_total: function(values, column, type) {
+		console.log(column.column.fieldtype);
+		if (column.column.fieldtype == "Percent" || type === "mean") {
+			return values.reduce((a, b) => a + flt(b)) / values.length;
+		} else if (column.column.fieldtype == "Int") {
+			console.log("HI");
+			return values.reduce((a, b) => a + cint(b));
+		} else if (frappe.model.is_numeric_field(column.column.fieldtype)) {
+			return values.reduce((a, b) => a + flt(b));
 		} else {
-			return false;
+			return null;
 		}
 	}
 });
