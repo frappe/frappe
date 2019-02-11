2ae056ad7a2eeca07777fcabfbb429c44eb993fa
Prateeksha Singh <pratu16x7@gmail.com>
2019-02-12 04:37:57 +0530

    fix(modules): One line module descriptions

--- frappe/public/js/frappe/views/components/Desktop.vue
+++ frappe/public/js/frappe/views/components/Desktop.vue
@@ -106,7 +106,7 @@ export default {
 .modules-container {
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-	grid-auto-rows: minmax(87px, 1fr);
+	grid-auto-rows: minmax(72px, 1fr);
 	column-gap: 15px;
 	row-gap: 15px;
 }
