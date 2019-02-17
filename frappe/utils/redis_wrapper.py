94aae12cde16252c0168719d71d430d0454cf58d
Aditya Hase <aditya@adityahase.com>
2019-02-17 21:45:41 +0530

    Revert "feat(redis-wrapper): Add support for HINCRBY"
    This reverts commit 9024370ee31c8674f8245f7ff81dfc5522f90919.

--- frappe/utils/redis_wrapper.py
+++ frappe/utils/redis_wrapper.py
@@ -162,9 +162,6 @@ class RedisWrapper(redis.Redis):
 		return {key: pickle.loads(value) for key, value in
 			iteritems(super(RedisWrapper, self).hgetall(self.make_key(name)))}
 
-	def hincrby(self, name, key, amount=1):
-		return super(redis.Redis, self).hincrby(self.make_key(name), key, amount=amount)
-
 	def hget(self, name, key, generator=None, shared=False):
 		_name = self.make_key(name, shared=shared)
 		if not _name in frappe.local.cache:
