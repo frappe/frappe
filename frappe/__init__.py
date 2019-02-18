0facedda0c5cf7c0775c5164cf0f557916a171d5
Aditya Hase <aditya@adityahase.com>
2019-02-18 17:50:20 +0530

    fix: Escape traceback in errprint

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -274,7 +274,8 @@ def errprint(msg):
 	if not request or (not "cmd" in local.form_dict) or conf.developer_mode:
 		print(msg)
 
-	error_log.append({"exc": msg, "locals": get_frame_locals()})
+	from .utils import escape_html
+	error_log.append({"exc": escape_html(msg), "locals": get_frame_locals()})
 
 def log(msg):
 	"""Add to `debug_log`.
