e22d6d3977f5fc89d32e1fb7b724bf1a6409eb4f
Faris Ansari <netchamp.faris@gmail.com>
2019-02-20 19:05:23 +0530

    fix: Webpage meta patch

--- frappe/patches/v12_0/webpage_migrate_description_to_meta_tag.py
+++ frappe/patches/v12_0/webpage_migrate_description_to_meta_tag.py
@@ -4,8 +4,9 @@ def execute():
 	web_pages = frappe.get_all('Web Page', ['name', 'description'])
 
 	for web_page in web_pages:
-		if web_page.description:
-			doc = frappe.get_doc('Web Page', web_page.name)
+		if web_page.description and web_page.route:
+			doc = frappe.new_doc('Website Route Meta')
+			doc.name = web_page.route
 			doc.append('meta_tags', {
 				'key': 'description',
 				'value': web_page.description
