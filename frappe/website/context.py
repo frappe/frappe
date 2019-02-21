08f51749348ad81a03ddacf061f03c845b1fd0c5
Faris Ansari <netchamp.faris@gmail.com>
2019-02-21 14:01:23 +0530

    fix: Handle falsy homepage route

--- frappe/website/context.py
+++ frappe/website/context.py
@@ -250,14 +250,16 @@ def add_metatags(context):
 
 	# Get meta tags from Website Route meta
 	# they can override the defaults set above
-
 	route = context.route
 	if route == '':
 		# homepage
 		route = frappe.db.get_single_value('Website Settings', 'home_page')
 
-	is_not_file = not route.endswith(('.js', '.css'))
-	if is_not_file and frappe.db.exists('Website Route Meta', route):
+	route_exists = (route
+		and route.endswith(('.js', '.css'))
+		and frappe.db.exists('Website Route Meta', route))
+
+	if route_exists:
 		context.setdefault('metatags', frappe._dict({}))
 		website_route_meta = frappe.get_doc('Website Route Meta', route)
 		for meta_tag in website_route_meta.meta_tags:
