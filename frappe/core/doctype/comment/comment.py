c22fcc61d7ba2e737c076488cfa10195464ce2d7
Rushabh Mehta <rmehta@gmail.com>
2019-02-26 17:04:58 +0530

    fix(typo): comment.py

--- frappe/core/doctype/comment/comment.py
+++ frappe/core/doctype/comment/comment.py
@@ -102,7 +102,7 @@ class Comment(Document):
 
 def on_doctype_update():
 	frappe.db.add_index("Comment", ["reference_doctype", "reference_name"])
-	frappe.db.add_index("Communication", ["link_doctype", "link_name"])
+	frappe.db.add_index("Comment", ["link_doctype", "link_name"])
 
 
 def update_comment_in_doc(doc):
