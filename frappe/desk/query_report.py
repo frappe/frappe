302f781fd83007987ec854fd55911a40ed560944
Rohan <Alchez@users.noreply.github.com>
2019-02-26 16:11:55 +0530

    fix(report): Fix file exports for reports that show totals (#6902)
    * fix(report): Fix file exports for reports that show totals
    
    * fix(report): Add tests for Query Report export

--- frappe/desk/query_report.py
+++ frappe/desk/query_report.py
@@ -242,43 +242,51 @@ def export_query():
 		report_name = data["report_name"]
 	if isinstance(data.get("file_format_type"), string_types):
 		file_format_type = data["file_format_type"]
+
 	if isinstance(data.get("visible_idx"), string_types):
 		visible_idx = json.loads(data.get("visible_idx"))
 	else:
 		visible_idx = None
 
 	if file_format_type == "Excel":
-
 		data = run(report_name, filters)
 		data = frappe._dict(data)
 		columns = get_columns_dict(data.columns)
 
-		result = [[]]
-
-		# add column headings
-		for idx in range(len(data.columns)):
-			result[0].append(columns[idx]["label"])
-
-		# build table from dict
-		if isinstance(data.result[0], dict):
-			for i,row in enumerate(data.result):
-				# only rows which are visible in the report
-				if row and (i in visible_idx):
-					row_list = []
-					for idx in range(len(data.columns)):
-						row_list.append(row.get(columns[idx]["fieldname"], row.get(columns[idx]["label"], "")))
-					result.append(row_list)
-				elif not row:
-					result.append([])
-		else:
-			result = result + [d for i,d in enumerate(data.result) if (i in visible_idx)]
-
 		from frappe.utils.xlsxutils import make_xlsx
-		xlsx_file = make_xlsx(result, "Query Report")
+		xlsx_data = build_xlsx_data(columns, data, visible_idx)
+		xlsx_file = make_xlsx(xlsx_data, "Query Report")
 
 		frappe.response['filename'] = report_name + '.xlsx'
 		frappe.response['filecontent'] = xlsx_file.getvalue()
 		frappe.response['type'] = 'binary'
+
+
+def build_xlsx_data(columns, data, visible_idx):
+	result = [[]]
+
+	# add column headings
+	for idx in range(len(data.columns)):
+		result[0].append(columns[idx]["label"])
+
+	# build table from result
+	for i, row in enumerate(data.result):
+		# only pick up rows that are visible in the report
+		if i in visible_idx:
+			row_data = []
+
+			if isinstance(row, list):
+				row_data = row
+			elif isinstance(row, dict) and row:
+				for idx in range(len(data.columns)):
+					label = columns[idx]["label"]
+					fieldname = columns[idx]["fieldname"]
+
+					row_data.append(row.get(fieldname, row.get(label, "")))
+
+			result.append(row_data)
+
+	return result
 
 
 def get_report_module_dotted_path(module, report_name):
