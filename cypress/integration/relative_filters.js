3d02648259662267aa9f25ad103569628cd14a31
Faris Ansari <netchamp.faris@gmail.com>
2019-03-02 19:30:56 +0530

    tests: Wait for list to update

--- cypress/integration/relative_filters.js
+++ cypress/integration/relative_filters.js
@@ -19,15 +19,15 @@ context('Relative Timeframe', () => {
 		cy.get('select.condition.form-control').select("Previous");
 		cy.get('.filter-field select.input-with-feedback.form-control').select("1 week");
 		cy.server();
-		cy.route({
-			method: 'POST',
-			url: '/api/method/frappe.desk.reportview.get'
-		}).as('applyFilter');
+		cy.route('POST', '/api/method/frappe.desk.reportview.get').as('list_refresh');
 		cy.get('.filter-box .btn:contains("Apply")').click();
-		cy.wait('@applyFilter');
+		cy.wait('@list_refresh');
 		cy.get('.list-row-container').its('length').should('eq', 1);
 		cy.get('.list-row-container').should('contain', 'this is second todo');
+		cy.route('POST', '/api/method/frappe.model.utils.user_settings.save')
+			.as('save_user_settings');
 		cy.get('.remove-filter.btn').click();
+		cy.wait('@save_user_settings');
 	});
 	it('set relative filter for Next and check list', () => {
 		cy.visit('/desk#List/ToDo/List');
@@ -37,14 +37,14 @@ context('Relative Timeframe', () => {
 		cy.get('select.condition.form-control').select("Next");
 		cy.get('.filter-field select.input-with-feedback.form-control').select("1 week");
 		cy.server();
-		cy.route({
-			method: 'POST',
-			url: '/api/method/frappe.desk.reportview.get'
-		}).as('applyFilter');
+		cy.route('POST', '/api/method/frappe.desk.reportview.get').as('list_refresh');
 		cy.get('.filter-box .btn:contains("Apply")').click();
-		cy.wait('@applyFilter');
+		cy.wait('@list_refresh');
 		cy.get('.list-row-container').its('length').should('eq', 1);
 		cy.get('.list-row').should('contain', 'this is first todo');
+		cy.route('POST', '/api/method/frappe.model.utils.user_settings.save')
+			.as('save_user_settings');
 		cy.get('.remove-filter.btn').click();
+		cy.wait('@save_user_settings');
 	});
 });
