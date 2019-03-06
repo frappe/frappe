4cbd887c47aa5d386fec0d5613745c5165bef5ef
Aditya Hase <aditya@adityahase.com>
2019-03-06 19:16:31 +0530

    test: Add Cypress tests for List View Settings

---
+++ cypress/integration/listview_settings.js
@@ -0,0 +1,43 @@
+context('Relative Timeframe', () => {
+	beforeEach(() => {
+		cy.login('Administrator', 'qwe');
+		cy.visit('/desk');
+	});
+	it('Default settings', () => {
+		cy.visit('/desk#List/DocType/List');
+		cy.get('.list-count').should('contain', "20 of");
+		cy.get('.sidebar-stat').should('contain', "No Tags");
+	});
+	it('disable count and verify', () => {
+		cy.visit('/desk#List/DocType/List');
+		cy.get('.list-count').should('contain', "20 of");
+		cy.get('button').contains('Menu').click();
+		cy.get('.dropdown-menu li').filter(':visible').contains('Settings').click();
+		cy.get('.modal-dialog').should('contain', 'Settings');
+		cy.get('input[data-fieldname="disable_count"]').check({force: true});
+		cy.get('button').filter(':visible').contains('Save').click();
+		cy.reload();
+		cy.get('.list-count').should('be.empty');
+		cy.get('button').contains('Menu').click();
+		cy.get('.dropdown-menu li').filter(':visible').contains('Settings').click();
+		cy.get('.modal-dialog').should('contain', 'Settings');
+		cy.get('input[data-fieldname="disable_count"]').uncheck({force: true});
+		cy.get('button').filter(':visible').contains('Save').click();
+	});
+	it('disable sidebar stats and verify', () => {
+		cy.visit('/desk#List/DocType/List');
+		cy.get('.list-count').should('contain', "20 of");
+		cy.get('button').contains('Menu').click();
+		cy.get('.dropdown-menu li').filter(':visible').contains('Settings').click();
+		cy.get('.modal-dialog').should('contain', 'Settings');
+		cy.get('input[data-fieldname="disable_sidebar_stats"]').check({force: true});
+		cy.get('button').filter(':visible').contains('Save').click();
+		cy.reload();
+		cy.get('.list-sidebar .sidebar-stat').should('not.exist');
+		cy.get('button').contains('Menu').click();
+		cy.get('.dropdown-menu li').filter(':visible').contains('Settings').click();
+		cy.get('.modal-dialog').should('contain', 'Settings');
+		cy.get('input[data-fieldname="disable_sidebar_stats"]').uncheck({force: true});
+		cy.get('button').filter(':visible').contains('Save').click();
+	});
+});
