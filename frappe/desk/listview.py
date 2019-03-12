e5eade2fa179d314e20f53d59a9812859269f90b
Prssanna Desai <prssud@gmail.com>
2019-03-13 00:05:41 +0530

    feat(Sidebar): Add "Assigned To" Dropdown

--- frappe/desk/listview.py
+++ frappe/desk/listview.py
@@ -24,3 +24,10 @@ def set_list_settings(doctype, values):
 		frappe.clear_messages()
 	doc.update(json.loads(values))
 	doc.save()
+
+@frappe.whitelist()
+def get_user_assignments_and_count():
+	user_list = frappe.get_list("User", filters={"user_type": "System User"})
+	assignment_data = sorted([{"count":frappe.db.count('ToDo', filters = {'reference_type': 'Issue', 'owner': user['name'], 'status': 'Open'}),
+						"name": user['name']} for user in user_list], key=lambda k: k['count'], reverse = True)
+	return assignment_data
