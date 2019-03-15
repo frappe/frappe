bfe3153535672d4f5c8649e67922bbf4ce60bea9
Aleksas Pielikis <ant.kampo@gmail.com>
2019-03-15 21:01:40 +0200

    Correction according to comments.

--- frappe/email/receive.py
+++ frappe/email/receive.py
@@ -358,12 +358,12 @@ class Email:
 
 		:param content: Raw message."""
 		if six.PY2:
-			self.mail = email.message_from_string( safe_encode(content) )
+			self.mail = email.message_from_string(safe_encode(content))
 		else:
 			if isinstance(content, bytes):
 				self.mail = email.message_from_bytes(content)
 			else:
-				self.mail = email.message_from_string( safe_decode(content) )
+				self.mail = email.message_from_string(content)
 
 		self.text_content = ''
 		self.html_content = ''
