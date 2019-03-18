78deb1aec4b3b959614405aa5e88319a7c97381e
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-03-18 09:27:35 +0530

    fix: Escape sql keywords

--- frappe/patches/v12_0/replace_null_values_in_tables.py
+++ frappe/patches/v12_0/replace_null_values_in_tables.py
@@ -12,8 +12,7 @@ def execute():
 	for field in fields:
 		update_column_table_map.setdefault(field.TABLE_NAME, [])
 
-		if field.TABLE_NAME not in ['tabDocPerm', 'tabCustom Field', 'tabWebsite Route Permission']:
-			update_column_table_map[field.TABLE_NAME].append(str(field.COLUMN_NAME)+"=COALESCE("+str(field.COLUMN_NAME)+", 0)")
+		update_column_table_map[field.TABLE_NAME].append("`"+str(field.COLUMN_NAME)+"`=COALESCE(`"+str(field.COLUMN_NAME)+"`, 0)")
 
 	for table in frappe.db.get_tables():
 		if update_column_table_map.get(table):
