48e2ff59247f0402c64a5544326c7c93f5e6a1a6
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-03-18 15:18:25 +0530

    fix: Code cleanup

--- frappe/patches/v12_0/replace_null_values_in_tables.py
+++ frappe/patches/v12_0/replace_null_values_in_tables.py
@@ -1,7 +1,6 @@
 import frappe
 
 def execute():
-
 	fields = frappe.db.sql("""
 			SELECT COLUMN_NAME , TABLE_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
 			WHERE DATA_TYPE IN ('INT', 'FLOAT', 'DECIMAL') AND IS_NULLABLE = 'YES'
@@ -12,10 +11,10 @@ def execute():
 	for field in fields:
 		update_column_table_map.setdefault(field.TABLE_NAME, [])
 
-		update_column_table_map[field.TABLE_NAME].append("`"+str(field.COLUMN_NAME)+"`=COALESCE(`"+str(field.COLUMN_NAME)+"`, 0)")
+		update_column_table_map[field.TABLE_NAME].append("`{fieldname}`=COALESCE(`{fieldname}`, 0)".format(fieldname=field.COLUMN_NAME))
 
 	for table in frappe.db.get_tables():
 		if update_column_table_map.get(table):
 			frappe.db.sql("""UPDATE `{table}` SET {columns}"""
-				.format(table=table, columns=",".join(update_column_table_map.get(table))))
+				.format(table=table, columns=", ".join(update_column_table_map.get(table))))
 
