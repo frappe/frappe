a43d89db8202649b386a8f04309bbe3383a66b21
Prssanna Desai <prssud@gmail.com>
2019-03-19 15:09:56 +0530

    fix: Improvements to "Assigned To" dropdown

--- frappe/desk/listview.py
+++ frappe/desk/listview.py
@@ -26,8 +26,14 @@ def set_list_settings(doctype, values):
 	doc.save()
 
 @frappe.whitelist()
-def get_user_assignments_and_count():
-	user_list = frappe.get_list("User", filters={"user_type": "System User"})
-	assignment_data = sorted([{"count":frappe.db.count('ToDo', filters = {'reference_type': 'Issue', 'owner': user['name'], 'status': 'Open'}),
+def get_user_assignments_and_count(doctype, current_filters):
+	filtered_list = frappe.get_all(doctype,
+		filters=current_filters)
+	names = list(map(lambda x: x.name, filtered_list))
+	user_list = frappe.get_all("User", filters={"user_type": "System User"})
+	if names:
+		assignment_data = sorted([{"count":frappe.db.count('ToDo', filters = {'owner': user['name'], 'status': 'Open', 'reference_name': ('in', names)}),
 						"name": user['name']} for user in user_list], key=lambda k: k['count'], reverse = True)
-	return assignment_data
+		return assignment_data
+	else: 
+		return []
