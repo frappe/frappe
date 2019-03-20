19c4f6086e381389f6a77c58be62137016dea969
Faris Ansari <netchamp.faris@gmail.com>
2019-03-20 14:54:41 +0530

    fix: Remove cscript and cur_frm from all client scripts
    - DocType
    - Report
    - Property Setter
    - Newsletter
    - Currency
    - Contact
    - Address and Contact

--- frappe/core/doctype/report/report.js
+++ frappe/core/doctype/report/report.js
@@ -1,78 +1,67 @@
-cur_frm.cscript.report_type = function(doc) {
-	cur_frm.set_intro("");
-	switch(doc.report_type) {
-		case "Report Builder":
-			cur_frm.set_intro(__("Report Builder reports are managed directly by the report builder. Nothing to do."));
-			break;
-		case "Query Report":
-			cur_frm.set_intro(__("Write a SELECT query. Note result is not paged (all data is sent in one go).")
-				+ __("To format columns, give column labels in the query.") + "<br>"
-				+ __("[Label]:[Field Type]/[Options]:[Width]") + "<br><br>"
-				+ __("Example:") + "<br>"
-				+ "Employee:Link/Employee:200" + "<br>"
-				+ "Rate:Currency:120" + "<br>")
-			break;
-		case "Script Report":
-			cur_frm.set_intro(__("Write a Python file in the same folder where this is saved and return column and result."))
-			break;
-	}
-}
-
-cur_frm.cscript.refresh = function(doc) {
-	cur_frm.add_custom_button("Show Report", function() {
-		switch(doc.report_type) {
-			case "Report Builder":
-				frappe.set_route('List', doc.ref_doctype, 'Report', doc.name);
-				break;
-			case "Query Report":
-				frappe.set_route("query-report", doc.name);
-				break;
-			case "Script Report":
-				frappe.set_route("query-report", doc.name);
-				break;
-		}
-	}, "fa fa-table");
-
-	if (doc.is_standard === "Yes") {
-		cur_frm.add_custom_button(doc.disabled ? __("Enable Report") : __("Disable Report"), function() {
-			$.ajax({
-				url: "/api/resource/Report/" + encodeURIComponent(doc.name),
-				type: "POST",
-				data: {
-					run_method: "toggle_disable",
-					disable: doc.disabled ? 0 : 1
-				}
-			}).always(function() {
-				cur_frm.reload_doc();
-			});
-		}, doc.disabled ? "fa fa-check" : "fa fa-off");
-	}
-
-	cur_frm.cscript.report_type(doc);
-}
-
-
 frappe.ui.form.on('Report', {
 	refresh: function(frm) {
-		if(!frappe.boot.developer_mode && frappe.session.user != 'Administrator') {
+		if(!frappe.boot.developer_mode && frappe.session.user !== 'Administrator') {
 			// make the document read-only
 			frm.set_read_only();
 		}
+
+		let doc = frm.doc;
+		frm.add_custom_button(__("Show Report"), function() {
+			switch(doc.report_type) {
+				case "Report Builder":
+					frappe.set_route('List', doc.ref_doctype, 'Report', doc.name);
+					break;
+				case "Query Report":
+					frappe.set_route("query-report", doc.name);
+					break;
+				case "Script Report":
+					frappe.set_route("query-report", doc.name);
+					break;
+			}
+		}, "fa fa-table");
+
+		if (doc.is_standard === "Yes") {
+			frm.add_custom_button(doc.disabled ? __("Enable Report") : __("Disable Report"), function() {
+				frm.call('toggle_disable', {
+					disable: doc.disabled ? 0 : 1
+				}).then(() => {
+					frm.reload_doc();
+				});
+			}, doc.disabled ? "fa fa-check" : "fa fa-off");
+		}
+
+		frm.events.report_type(frm);
 	},
 
 	ref_doctype: function(frm) {
 		if(frm.doc.ref_doctype) {
-			frm.trigger("set_doctype_roles")
+			frm.trigger("set_doctype_roles");
+		}
+	},
+
+	report_type: function(frm) {
+		frm.set_intro("");
+		switch(frm.doc.report_type) {
+			case "Report Builder":
+				frm.set_intro(__("Report Builder reports are managed directly by the report builder. Nothing to do."));
+				break;
+			case "Query Report":
+				frm.set_intro(__("Write a SELECT query. Note result is not paged (all data is sent in one go).")
+					+ __("To format columns, give column labels in the query.") + "<br>"
+					+ __("[Label]:[Field Type]/[Options]:[Width]") + "<br><br>"
+					+ __("Example:") + "<br>"
+					+ "Employee:Link/Employee:200" + "<br>"
+					+ "Rate:Currency:120" + "<br>")
+				break;
+			case "Script Report":
+				frm.set_intro(__("Write a Python file in the same folder where this is saved and return column and result."));
+				break;
 		}
 	},
 
 	set_doctype_roles: function(frm) {
-		return frappe.call({
-			method: "set_doctype_roles",
-			doc:frm.doc,
-			callback: function(r) {
-				refresh_field('roles')
-			}
-		})
+		return frm.call('set_doctype_roles').then(() => {
+			frm.refresh_field('roles');
+		});
 	}
 })
--- frappe/geo/doctype/currency/currency.js
+++ frappe/geo/doctype/currency/currency.js
@@ -1,9 +1,11 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: See license.txt
 
-cur_frm.cscript.refresh = function(doc) {
-	cur_frm.set_intro("");
-	if(!cur_frm.doc.enabled) {
-		cur_frm.set_intro(__("This Currency is disabled. Enable to use in transactions"))
+frappe.ui.form.on('Currency', {
+	refresh(frm) {
+		frm.set_intro("");
+		if(!frm.doc.enabled) {
+			frm.set_intro(__("This Currency is disabled. Enable to use in transactions"));
+		}
 	}
-}
+});
