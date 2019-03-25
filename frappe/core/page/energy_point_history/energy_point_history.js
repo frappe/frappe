ae1db4613c1f9c273b6cb7c5c7b6914ec2b4b4fe
Suraj Shetty <surajshetty3416@gmail.com>
2019-03-25 11:04:43 +0530

    feat: Add more data to energy point history page

--- frappe/core/page/energy_point_history/energy_point_history.js
+++ frappe/core/page/energy_point_history/energy_point_history.js
@@ -1,19 +1,16 @@
 frappe.pages['energy-point-history'].on_page_load = function(wrapper) {
 	const page = frappe.ui.make_app_page({
 		parent: wrapper,
-		title: 'Transaction',
+		title: 'Energy Point History',
 		single_column: true,
 		btn_primary: console.log
 
 	});
-	console.log(page);
 	frappe.db.get_list('Energy Point Log', {
-		filters: { user: frappe.session.user },
-		fields: ['point', 'reason']
-	})
-	.then((data) => {
-		console.log(data);
+		filters: { user: frappe.session.user, type: ['not in', 'Review'] },
+		fields: ['points', 'reason', 'rule', 'reference_doctype', 'reference_name', 'type']
+	}).then((data) => {
 		$(frappe.render_template('energy_point_transactions', {data: data}))
 			.appendTo(page.body);
-	})
+	});
 }
