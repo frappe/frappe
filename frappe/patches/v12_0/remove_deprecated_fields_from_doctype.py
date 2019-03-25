3b8474cbed06b52fd1a29b2daa91ab7d783a7b30
Faris Ansari <netchamp.faris@gmail.com>
2019-03-25 19:23:18 +0530

    fix: Move doctype patch to top
    - This fixes the ERPNext patch

--- frappe/patches/v12_0/remove_deprecated_fields_from_doctype.py
+++ frappe/patches/v12_0/remove_deprecated_fields_from_doctype.py
@@ -2,9 +2,6 @@ import frappe
 
 def execute():
 	frappe.reload_doc('core', 'doctype', 'doctype')
-	frappe.db.sql('''
-		ALTER TABLE tabDocType
-		DROP COLUMN IF EXISTS hide_heading,
-		DROP COLUMN IF EXISTS image_view,
-		DROP COLUMN IF EXISTS read_only_onload
-	''')
+	frappe.model.delete_fields({
+		'DocType': ['hide_heading', 'image_view', 'read_only_onload']
+	}, delete=1)
