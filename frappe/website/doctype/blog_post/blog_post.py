855036384e91397e75d7b9439b22d958a4022a6a
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-03-26 11:12:42 +0530

    fix: fetch content_html and content_md

--- frappe/website/doctype/blog_post/blog_post.py
+++ frappe/website/doctype/blog_post/blog_post.py
@@ -163,6 +163,8 @@ def get_blog_list(doctype, txt=None, filters=None, limit_start=0, limit_page_len
 				t1.published_on as creation,
 				t1.content as content,
 				t1.content_type as content_type,
+				t1.content_html as content_html,
+				t1.content_md as content_md,
 				ifnull(t1.blog_intro, t1.content) as intro,
 				t2.full_name, t2.avatar, t1.blogger,
 				(select count(name) from `tabComment`
