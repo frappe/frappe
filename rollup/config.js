1b48f6e6df66d63f3c2d14db07de481de884b49f
Suraj Shetty <surajshetty3416@gmail.com>
2019-03-27 12:32:37 +0530

    revert: fix(build): Disable spread transpilation
    This reverts commit 7fee2c42f3981b56ad22e622dac9bd5478659da0.

--- rollup/config.js
+++ rollup/config.js
@@ -55,9 +55,7 @@ function get_rollup_options_for_js(output_file, input_files) {
 			objectAssign: 'Object.assign',
 			transforms: {
 				dangerousForOf: true,
-				classes: false,
-				// https://github.com/bublejs/buble/issues/81
-				spreadRest: false
+				classes: false
 			},
 			exclude: [path.resolve(bench_path, '**/*.css'), path.resolve(bench_path, '**/*.less')]
 		}),
