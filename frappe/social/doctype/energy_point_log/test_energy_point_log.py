c79ab1279de8552986671a2051babb1a20f0cae9
Suraj Shetty <surajshetty3416@gmail.com>
2019-03-29 09:28:37 +0530

    test: Add test for review

--- frappe/social/doctype/energy_point_log/test_energy_point_log.py
+++ frappe/social/doctype/energy_point_log/test_energy_point_log.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 
 import frappe
 import unittest
-from .energy_point_log import get_energy_points as _get_energy_points
+from .energy_point_log import get_energy_points as _get_energy_points, create_review_points_log, review
 from frappe.utils.testutils import add_custom_field, clear_custom_fields
 
 class TestEnergyPointLog(unittest.TestCase):
@@ -17,19 +17,19 @@ class TestEnergyPointLog(unittest.TestCase):
 	def test_user_energy_point(self):
 		frappe.set_user('test@example.com')
 		todo_point_rule = create_energy_point_rule_for_todo()
-		energy_point_of_user = get_energy_points('test@example.com')
+		energy_point_of_user = get_points('test@example.com')
 
 		created_todo = create_a_todo()
 
 		created_todo.status = 'Closed'
 		created_todo.save()
 
-		points_after_closing_todo = get_energy_points('test@example.com')
+		points_after_closing_todo = get_points('test@example.com')
 
 		self.assertEquals(points_after_closing_todo, energy_point_of_user + todo_point_rule.points)
 
 		created_todo.save()
-		points_after_double_save = get_energy_points('test@example.com')
+		points_after_double_save = get_points('test@example.com')
 
 		# point should not be awarded more than once for same doc
 		self.assertEquals(points_after_double_save, energy_point_of_user + todo_point_rule.points)
@@ -40,15 +40,15 @@ class TestEnergyPointLog(unittest.TestCase):
 		multiplier_value = 2
 
 		todo_point_rule = create_energy_point_rule_for_todo('multiplier')
-		energy_point_of_user = get_energy_points('test@example.com')
+		energy_point_of_user = get_points('test@example.com')
 
 		created_todo = create_a_todo()
-		points_after_closing_todo = get_energy_points('test@example.com')
+		points_after_closing_todo = get_points('test@example.com')
 		created_todo.status = 'Closed'
 		created_todo.multiplier = multiplier_value
 		created_todo.save()
 
-		points_after_closing_todo = get_energy_points('test@example.com')
+		points_after_closing_todo = get_points('test@example.com')
 
 		self.assertEquals(points_after_closing_todo, energy_point_of_user + (todo_point_rule.points * multiplier_value))
 		clear_custom_fields('ToDo')
@@ -60,20 +60,50 @@ class TestEnergyPointLog(unittest.TestCase):
 
 		frappe.set_user('test@example.com')
 		create_energy_point_rule_for_todo()
-		energy_point_of_user = get_energy_points('test@example.com')
+		energy_point_of_user = get_points('test@example.com')
 
 		created_todo = create_a_todo()
 
 		created_todo.status = 'Closed'
 		created_todo.save()
 
-		points_after_closing_todo = get_energy_points('test@example.com')
+		points_after_closing_todo = get_points('test@example.com')
 
 		# no change in points
 		self.assertEquals(points_after_closing_todo, energy_point_of_user)
 
 		settings.enabled = 1
 		settings.save()
+
+	def test_review(self):
+		created_todo = create_a_todo()
+		review_points = 20
+		create_review_points_log('test2@example.com', review_points)
+
+		# reviewer
+		frappe.set_user('test2@example.com')
+
+		review_points_before_review = get_points('test2@example.com', 'review_points')
+		self.assertEquals(review_points_before_review, review_points)
+
+		# for appreciation
+		appreciation_points = 5
+		energy_points_before_review = get_points('test@example.com')
+		review(created_todo, appreciation_points, 'test@example.com', 'good job')
+		energy_points_after_review = get_points('test@example.com')
+		review_points_after_review = get_points('test2@example.com', 'review_points')
+		self.assertEquals(energy_points_after_review, energy_points_before_review + appreciation_points)
+		self.assertEquals(review_points_after_review, review_points_before_review - appreciation_points)
+
+		# for criticism
+		criticism_points = 2
+		energy_points_before_review = energy_points_after_review
+		review_points_before_review = review_points_after_review
+		review(created_todo, criticism_points, 'test@example.com', 'You could have done better.', 'Criticism')
+		energy_points_after_review = get_points('test@example.com')
+		review_points_after_review = get_points('test2@example.com', 'review_points')
+		self.assertEquals(energy_points_after_review, energy_points_before_review - criticism_points)
+		self.assertEquals(review_points_after_review, review_points_before_review - criticism_points)
 
 def create_energy_point_rule_for_todo(multiplier_field=None):
 	name = 'ToDo Closed'
@@ -103,5 +133,5 @@ def create_a_todo():
 	}).insert()
 
 
-def get_energy_points(user):
-	return _get_energy_points(user).energy_points or 0
+def get_points(user, point_type='energy_points'):
+	return _get_energy_points(user).get(point_type) or 0
