6deb7d2c3ad3da3ed60ed76039b78d40a9d1c024
Suraj Shetty <surajshetty3416@gmail.com>
2019-04-04 12:56:42 +0530

    fix: Export parent fields while exporting fixtures
    - To allow Custom DocPerm export which needs parent fields

--- frappe/core/doctype/data_import/data_import.py
+++ frappe/core/doctype/data_import/data_import.py
@@ -71,7 +71,7 @@ def import_file_by_path(path, ignore_links=False, overwrite=False, submit=False,
 
 def export_json(doctype, path, filters=None, or_filters=None, name=None, order_by="creation asc"):
 	def post_process(out):
-		del_keys = ('parent', 'parentfield', 'parenttype', 'modified_by', 'creation', 'owner', 'idx')
+		del_keys = ('modified_by', 'creation', 'owner', 'idx')
 		for doc in out:
 			for key in del_keys:
 				if key in doc:
