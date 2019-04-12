5d14cdc44affcec2ce16243bcd5addc3ca48a9d9
Raffael Meyer <raffael@alyf.de>
2019-04-12 19:56:26 +0200

    put functions in scope

--- frappe/integrations/doctype/gsuite_templates/gsuite_templates.js
+++ frappe/integrations/doctype/gsuite_templates/gsuite_templates.js
@@ -5,48 +5,49 @@ frappe.ui.form.on('GSuite Templates', {
 	refresh: function(frm) {
 		if (frm.is_new()) {
 			// if doc is new, get all options immediately
-			set_available_docs(frm);
-			set_available_folders(frm);
+			frm.trigger('set_available_docs');
+			frm.trigger('set_available_folders');
 		}
 	},
 	template_id: function(frm) {
 		if (!frm.is_new()) {
 			// if doc is NOT new, get options when selecting field
-			set_available_docs(frm);
+			frm.trigger('set_available_docs');
 		}
 	},
 	destination_id: function(frm) {
 		if (!frm.is_new()) {
 			// if doc is NOT new, get options when selecting field
-			set_available_folders(frm);
+			frm.trigger('set_available_folders');
 		}
+	},
+	set_available_docs: function(frm) {
+		frappe.call({
+			// get documents from Google Drive
+			method: 'frappe.integrations.doctype.gsuite_templates.gsuite_templates.get_gdrive_docs',
+			callback: function(res) {
+				// set available documents as options
+				frm.trigger('set_options', 'template_id', res);
+			}
+		});
+	},
+	set_available_folders: function(frm) {
+		frappe.call({
+			// get folders from Google Drive
+			method: 'frappe.integrations.doctype.gsuite_templates.gsuite_templates.get_gdrive_folders',
+			callback: function(res) {
+				// set available folders as options
+				frm.trigger('set_options', 'destination_id', res);
+			}
+		});
+	},
+	set_options: function(frm, field, data) {
+		var options = [];
+		(data.message || []).forEach(function(row){ 
+			options.push({'value': row.id, 'label': row.name});
+		});
+		frm.set_df_property(field, 'options', options);
 	}
 });
 
-const set_available_docs = (frm) => {
-	frappe.call({
-		method: 'frappe.integrations.doctype.gsuite_templates.gsuite_templates.get_gdrive_docs',
-		callback: function(res) {
-			// set available docs as options
-			set_options(frm, 'template_id', res);
-		}
-	});
-};
 
-const set_available_folders = (frm) => {
-	frappe.call({
-		method: 'frappe.integrations.doctype.gsuite_templates.gsuite_templates.get_gdrive_folders',
-		callback: function(res) {
-			// set available folders as options
-			set_options(frm, 'destination_id', res);
-		}
-	});
-};
-
-const set_options = (frm, field, data) => {
-	var options = [];
-	(data.message || []).forEach(function(row){ 
-		options.push({'value': row.id, 'label': row.name});
-	});
-	frm.set_df_property(field, 'options', options);
-};
