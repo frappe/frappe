3848a2cb83532eacc3044adc71ed9314aaeb90e3
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-04-14 15:40:43 +0530

    fix: add functin to delete communication links

--- frappe/model/delete_doc.py
+++ frappe/model/delete_doc.py
@@ -275,9 +275,7 @@ def delete_dynamic_links(doctype, name):
 	delete_references('Document Follow', doctype, name, 'ref_doctype', 'ref_docname')
 
 	# unlink communications
-	clear_references('Communication', doctype, name)
-	#clear_references('Communication', doctype, name, 'link_doctype', 'link_name')
-	#clear_references('Communication', doctype, name, 'timeline_doctype', 'timeline_name')
+	clear_communication_references('Communication', doctype, name)
 
 	clear_references('Activity Log', doctype, name)
 	clear_references('Activity Log', doctype, name, 'timeline_doctype', 'timeline_name')
@@ -298,6 +296,10 @@ def clear_references(doctype, reference_doctype, reference_name,
 			{1}=%s and {2}=%s'''.format(doctype, reference_doctype_field, reference_name_field), # nosec
 		(reference_doctype, reference_name))
 
+def clear_communication_references(doctype, link_doctype, link_name):
+	references = frappe.get_list("Dynamic Link", filters={"parent": doctype, "link_doctype": link_doctype, "link_name": link_name})
+	for reference in references:
+		frappe.delete_doc("Dynamic Link", reference.name)
 
 def insert_feed(doc):
 	from frappe.utils import get_fullname
