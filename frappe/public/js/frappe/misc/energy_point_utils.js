8aaadebf76318935f26acfc0a8d65fe78837ea63
Suraj Shetty <surajshetty3416@gmail.com>
2019-04-16 10:55:35 +0530

    fix: Ignore permission while saving revert doc
    - Since on system manager can revert points

--- frappe/public/js/frappe/misc/energy_point_utils.js
+++ frappe/public/js/frappe/misc/energy_point_utils.js
@@ -32,6 +32,7 @@ Object.assign(frappe.energy_points, {
 	get_history_log_message(log) {
 		const doc_link = frappe.utils.get_form_link(log.reference_doctype, log.reference_name, true);
 		const owner_name = frappe.user.full_name(log.owner).bold();
+		const user = frappe.user.full_name(log.user).bold();
 		if (log.type === 'Appreciation') {
 			return __('{0} appreciated on {1}', [owner_name, doc_link]);
 		}
@@ -39,7 +40,7 @@ Object.assign(frappe.energy_points, {
 			return __('{0} criticized on {1}', [owner_name, doc_link]);
 		}
 		if (log.type === 'Revert') {
-			return __('{0} reverted {1}', [owner_name,
+			return __('{0} reverted {1}', [user,
 				frappe.utils.get_form_link('Energy Point Log', log.revert_of, true)]);
 		}
 		return __('via automatic rule {0} on {1}', [log.rule.bold(), doc_link]);
