43ce786071959700afe66ad41af7fcbfcd876320
Faris Ansari <netchamp.faris@gmail.com>
2019-04-19 15:46:14 +0530

    style: Method could be a function

--- frappe/email/doctype/auto_email_report/test_auto_email_report.py
+++ frappe/email/doctype/auto_email_report/test_auto_email_report.py
@@ -13,7 +13,7 @@ class TestAutoEmailReport(unittest.TestCase):
 	def test_auto_email(self):
 		frappe.delete_doc('Auto Email Report', 'Permitted Documents For User')
 
-		auto_email_report = self.get_auto_email_report()
+		auto_email_report = get_auto_email_report()
 
 		data = auto_email_report.get_report_content()
 
@@ -30,7 +30,7 @@ class TestAutoEmailReport(unittest.TestCase):
 
 
 	def test_dynamic_date_filters(self):
-		auto_email_report = self.get_auto_email_report()
+		auto_email_report = get_auto_email_report()
 
 		auto_email_report.dynamic_date_period = 'Weekly'
 		auto_email_report.from_date_field = 'from_date'
@@ -42,20 +42,20 @@ class TestAutoEmailReport(unittest.TestCase):
 		self.assertEqual(auto_email_report.filters['to_date'], today())
 
 
-	def get_auto_email_report(self):
-		if not frappe.db.exists('Auto Email Report', 'Permitted Documents For User'):
-			auto_email_report = frappe.get_doc(dict(
-				doctype='Auto Email Report',
-				report='Permitted Documents For User',
-				report_type='Script Report',
-				user='Administrator',
-				enabled=1,
-				email_to='test@example.com',
-				format='HTML',
-				frequency='Daily',
-				filters=json.dumps(dict(user='Administrator', doctype='DocType'))
-			)).insert()
-		else:
-			auto_email_report = frappe.get_doc('Auto Email Report', 'Permitted Documents For User')
+def get_auto_email_report():
+	if not frappe.db.exists('Auto Email Report', 'Permitted Documents For User'):
+		auto_email_report = frappe.get_doc(dict(
+			doctype='Auto Email Report',
+			report='Permitted Documents For User',
+			report_type='Script Report',
+			user='Administrator',
+			enabled=1,
+			email_to='test@example.com',
+			format='HTML',
+			frequency='Daily',
+			filters=json.dumps(dict(user='Administrator', doctype='DocType'))
+		)).insert()
+	else:
+		auto_email_report = frappe.get_doc('Auto Email Report', 'Permitted Documents For User')
 
-		return auto_email_report
+	return auto_email_report
