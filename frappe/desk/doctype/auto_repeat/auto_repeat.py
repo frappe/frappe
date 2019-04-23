e7677b37ac90d28a17f345ec5252045da5cad5d0
Ivan Ray Altomera <irayspacii@gmail.com>
2019-04-23 10:43:53 +0800

    refactor: translatables and extended make_auto_repeat function

--- frappe/desk/doctype/auto_repeat/auto_repeat.py
+++ frappe/desk/doctype/auto_repeat/auto_repeat.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 import calendar
+import json
 from frappe import _
 from frappe.desk.form import assign_to
 from frappe.utils.jinja import validate_template
@@ -227,7 +228,7 @@ def update_doc(new_document, reference_doc, args, schedule_date):
 		new_document.set('auto_repeat', args.name)
 
 	for fieldname in ['naming_series', 'ignore_pricing_rule', 'posting_time',
-						'select_print_heading', 'remarks', 'owner']:
+					  'select_print_heading', 'remarks', 'owner']:
 		if new_document.meta.get_field(fieldname):
 			new_document.set(fieldname, reference_doc.get(fieldname))
 
@@ -292,10 +293,10 @@ def send_notification(new_rv, auto_repeat_doc, print_format='Standard'):
 		message = frappe.render_template(auto_repeat_doc.message, {'doc': new_rv})
 
 	attachments = [frappe.attach_print(new_rv.doctype, new_rv.name,
-						file_name=new_rv.name, print_format=print_format)]
+									   file_name=new_rv.name, print_format=print_format)]
 
 	make(doctype=new_rv.doctype, name=new_rv.name, recipients=auto_repeat_doc.recipients,
-					subject=subject, content=message, attachments=attachments, send_email=1)
+		 subject=subject, content=message, attachments=attachments, send_email=1)
 
 def notify_errors(doc, doctype, party, owner, name):
 	recipients = get_system_managers(only_name=True)
@@ -324,13 +325,25 @@ def assign_task_to_owner(name, msg, users):
 		assign_to.add(args)
 
 @frappe.whitelist()
-def make_auto_repeat(doctype, docname):
+def make_auto_repeat(doctype, docname, submit=False, opts=None):
 	doc = frappe.new_doc('Auto Repeat')
-
-	reference_doc = frappe.get_doc(doctype, docname)
 	doc.reference_doctype = doctype
 	doc.reference_document = docname
-	doc.start_date = reference_doc.get('posting_date') or reference_doc.get('transaction_date')
+
+	if opts:
+		opts = json.loads(opts)
+		doc.update({
+			'start_date': opts['start_date'],
+			'end_date': opts['end_date'],
+			'frequency': opts['frequency']
+		})
+	else:
+		reference_doc = frappe.get_doc(doctype, docname)
+		doc.start_date = reference_doc.get('posting_date') or reference_doc.get('transaction_date')
+
+	if submit:
+		doc.submit()
+
 	return doc
 
 @frappe.whitelist()
@@ -339,7 +352,7 @@ def stop_resume_auto_repeat(auto_repeat, status):
 	frappe.msgprint(_("Auto Repeat has been {0}").format(status))
 	if status == 'Resumed':
 		doc.next_schedule_date = get_next_schedule_date(today(),
-			doc.frequency, doc.repeat_on_day)
+														doc.frequency, doc.repeat_on_day)
 
 	doc.update_status(status)
 	doc.save()
@@ -354,13 +367,13 @@ def auto_repeat_doctype_query(doctype, txt, searchfield, start, page_len, filter
 			if(locate(%(_txt)s, parent), locate(%(_txt)s, parent), 99999),
 			parent
 		limit %(start)s, %(page_len)s""".format(**{
-			'key': searchfield,
-		}), {
-			'txt': "%%%s%%" % txt,
-			'_txt': txt.replace("%", ""),
-			'start': start,
-			'page_len': page_len
-		})
+		'key': searchfield,
+	}), {
+							 'txt': "%%%s%%" % txt,
+							 '_txt': txt.replace("%", ""),
+							 'start': start,
+							 'page_len': page_len
+						 })
 
 @frappe.whitelist()
 def get_contacts(reference_doctype, reference_name):
