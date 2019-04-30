44ac05d1fed4abd9c611ee0d09005c21cc6e892d
nexusfidelity <fidelivancdo@gmail.com>
2019-04-30 15:53:09 +0800

    refactor: use utils new_auto_repeat_prompt

--- frappe/desk/doctype/todo/todo.js
+++ frappe/desk/doctype/todo/todo.js
@@ -5,17 +5,15 @@ frappe.ui.form.on("ToDo", {
 		frm.set_query("reference_type", function (txt) {
 			return {
 				"filters": {
-					"issingle": 0,
-				}
+					"issingle": 0
+                }
 			};
 		});
 	},
 
-	validate: function (frm) {
-		if (frm.doc.is_repeating && frm.doc.__islocal) {
-			frm.trigger("new_auto_repeat_prompt");
-		}
-	},
+    validate: function (frm) {
+        frm.trigger("new_auto_repeat_prompt");
+    },
 
 	refresh: function (frm) {
 		if (frm.doc.reference_type && frm.doc.reference_name) {
@@ -45,60 +43,7 @@ frappe.ui.form.on("ToDo", {
 		}
 	},
 
-	new_auto_repeat_prompt: function (frm) {
-		const fields = [
-			{
-				'fieldname': 'start_date',
-				'fieldtype': 'Date',
-				'label': __('Start Date'),
-				'default': frappe.datetime.nowdate()
-			},
-			{
-				'fieldname': 'end_date',
-				'fieldtype': 'Date',
-				'label': __('End Date')
-			},
-			{
-				'fieldname': 'frequency',
-				'fieldtype': 'Select',
-				'label': __('Frequency'),
-				'reqd': 1,
-				'options': [
-					{'label': __('Daily'), 'value': 'Daily'},
-					{'label': __('Weekly'), 'value': 'Weekly'},
-					{'label': __('Monthly'), 'value': 'Monthly'},
-					{'label': __('Quarterly'), 'value': 'Quarterly'},
-					{'label': __('Half-yearly'), 'value': 'Half-yearly'},
-					{'label': __('Yearly'), 'value': 'Yearly'}
-				]
-			}
-		];
-
-		frappe.prompt(fields, function (values) {
-			frappe.call({
-				method: "frappe.desk.doctype.auto_repeat.auto_repeat.make_auto_repeat",
-				args: {
-					'doctype': frm.doc.doctype,
-					'docname': frm.doc.name,
-					'submit': true,
-					'opts': {
-						'start_date': values['start_date'],
-						'end_date': values['end_date'],
-						'frequency': values['frequency']
-					}
-				},
-				callback: function (r) {
-					if (r.message) {
-						frappe.show_alert({
-							'message': __("Successfully created repeating task"),
-							'indicator': 'green'
-						});
-					}
-				}
-			});
-		},
-		__('Auto Repeat'),
-		__('Submit')
-		);
-	}
+    new_auto_repeat_prompt: function (frm) {
+        frappe.utils.new_auto_repeat_prompt(frm);
+    }
 });
