38a2ef8635f3aef95b5c1aa239a492cda2edbea0
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-05-03 20:26:40 +0530

    fix: patch to migrate links to dynamic links

---
+++ frappe/patches/v12_0/communication_links_to_dynamic_links.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+import frappe
+
+def execute():
+	for communication in frappe.get_list("Communication"):
+		comm = frappe.get_doc("Communication", communication.name)
+		if comm.reference_doctype and comm.reference_name:
+			comm.add_link(comm.reference_doctype, comm.reference_name)
+		if comm.timeline_doctype and comm.timeline_name:
+			comm.add_link(comm.timeline_doctype, comm.timeline_name)
+		if comm.link_doctype and comm.link_name:
+			comm.add_link(comm.link_doctype, comm.link_name)
+		comm.save(ignore_permissions=True)
