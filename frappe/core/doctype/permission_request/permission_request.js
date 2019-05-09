e7917ee213641e89165b32c4ea43a1c57b43cd0c
Rohan Bansal <rohan@digithinkit.com>
2019-05-09 18:17:11 +0530

    feat(core): Allow users to request access for unauthorized doctypes

---
+++ frappe/core/doctype/permission_request/permission_request.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2019, Frappe Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Permission Request', {
+	onload: (frm) => {
+		frm.set_value("user", frappe.session.user);
+	},
+
+	refresh: (frm) => {
+		frm.set_query("authorizer", () => {
+			return {
+				filters: {
+					email: ["!=", frappe.session.user],
+					user_type: "System User"
+				}
+			}
+		})
+	}
+});
