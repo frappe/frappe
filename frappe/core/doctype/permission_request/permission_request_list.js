37c876ef1de380a486503a5a0387a17536e4b56a
Rohan Bansal <rohan@digithinkit.com>
2019-05-15 13:29:17 +0530

    fix(core): Fix filters for duplicate requests

--- frappe/core/doctype/permission_request/permission_request_list.js
+++ frappe/core/doctype/permission_request/permission_request_list.js
@@ -1,11 +1,11 @@
 frappe.listview_settings['Permission Request'] = {
 	get_indicator: function (doc) {
-		if (doc.status === "Requested") {
-			return [__("Requested"), "orange", "status,=,Requested"];
-		} else if (doc.status === "Approved") {
-			return [__("Approved"), "green", "status,=,Approved"];
-		} else if (doc.status === "Denied") {
-			return [__("Denied"), "red", "status,=,Denied"];
+		let status_map = {
+			"Requested": "orange",
+			"Approved": "green",
+			"Denied": "red"
 		}
+
+		return [__(doc.status), status_map[doc.status], "status,=," + doc.status];
 	}
 };
