4095768c470e9a68ce6327f4d7460bc171d8cf2c
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-05-15 14:52:49 +0530

    fix: Use filter instead of pushing in list

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -992,15 +992,11 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 								change: () => {
 									let doctype = d.get_value('doctype');
 									frappe.model.with_doctype(doctype, () => {
-										let field_list = []
-										frappe.meta.get_docfields(doctype)
-											.map(df => {
-												if (!in_list(frappe.model.no_value_type, df.fieldtype)) {
-													let fields = { label: df.label, value: df.fieldname }
-													field_list.push(fields);
-												}
-											});
-										d.set_df_property('field', 'options', field_list);
+										let options = frappe.meta.get_docfields(doctype)
+											.filter(frappe.model.is_value_type)
+											.map(df => ({ label: df.label, value: df.fieldname }));
+
+										d.set_df_property('field', 'options', options);
 
 									});
 								}
