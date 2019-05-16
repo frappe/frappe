80cab9abacca10631d7c4c29406c401ddd6054b1
Shivam Mishra <scm.mymail@gmail.com>
2019-05-16 11:13:21 +0530

    feat: Added WebFormListRow class

--- frappe/public/js/frappe/web_form/web_form_class.js
+++ frappe/public/js/frappe/web_form/web_form_class.js
@@ -1,7 +1,5 @@
-import DataTable from 'frappe-datatable';
-
 frappe.provide("frappe.ui");
-frappe.provide('frappe.views');
+frappe.provide("frappe.views");
 
 window.web_form = null;
 window.web_form_list = null;
@@ -22,7 +20,7 @@ frappe.ui.WebForm = class WebForm extends frappe.ui.FieldGroup {
 
 	set_field_values() {
 		if (this.doc_name) this.set_values(this.doc);
-		else return
+		else return;
 	}
 
 	setup_primary_action() {
@@ -114,35 +112,215 @@ frappe.views.WebFormList = class WebFormList {
 	constructor(opts) {
 		Object.assign(this, opts);
 		window.web_form_list = this;
+		this.rows = []
+		this.wrapper = document.getElementById("datatable")
+		this.refresh_list();
+		this.make_actions();
+	}
 
+	refresh_list() {
+		if (this.table) {
+			this.table.remove();
+			delete this.table;
+		}
 		frappe.run_serially([
 			() => this.get_list_view_fields(),
 			() => this.get_data(),
-			() => this.make_datatable()
-		])
+			() => this.make_table()
+		]);
 	}
 
 	get_list_view_fields() {
-		return frappe.call({
-			method: 'frappe.website.doctype.web_form.web_form.get_in_list_view_fields',
-			args: { doctype: this.doctype }
-		}).then((response) => this.fields_list = response.message)
+		return frappe
+			.call({
+				method:
+					"frappe.website.doctype.web_form.web_form.get_in_list_view_fields",
+				args: { doctype: this.doctype }
+			})
+			.then(response => this.fields_list = response.message);
 	}
 
 	get_data() {
+		return frappe
+			.call({
+				method: "frappe.www.list.get_list_data",
+				args: {
+					doctype: this.doctype,
+					fields: this.fields_list.map(df => df.fieldname),
+					web_form_name: this.web_form_name
+				}
+			})
+			.then(response => this.data = response.message);
+	}
+
+	make_table() {
+		this.table = document.createElement("table");
+		this.table.classList.add("table", "table-bordered", "table-hover");
+
+		this.make_table_head();
+		this.make_table_rows();
+
+		this.wrapper.appendChild(this.table);
+	}
+
+	make_table_head() {
+		// Create Heading
+		let thead = this.table.createTHead();
+		let row = thead.insertRow();
+		this.columns = this.fields_list.map(df => {
+			return {
+				label: df.label,
+				fieldname: df.fieldname
+			};
+		});
+
+		let th = document.createElement("th");
+
+		let checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.onchange = event =>
+			this.toggle_select_all(event.target.checked);
+
+		th.appendChild(checkbox);
+		row.appendChild(th);
+
+		add_heading(row, "Sr.");
+		this.columns.forEach(col => {
+			let th = document.createElement("th");
+			let text = document.createTextNode(col.label);
+			th.appendChild(text);
+			row.appendChild(th);
+		});
+		add_heading(row, "Edit")
+
+
+		function add_heading(row, label) {
+			let th = document.createElement("th")
+			th.innerText = label;
+			row.appendChild(th);
+		}
+	}
+
+	make_table_rows() {
+		const tbody = this.table.createTBody();
+		this.data.forEach((data_item, index) => {
+			let row_element = tbody.insertRow();
+			row_element.setAttribute("id", data_item.name);
+
+			let row = new frappe.ui.WebFromListRow({
+				row: row_element,
+				doc: data_item,
+				columns: this.columns,
+				serial_number: index + 1,
+				events: {
+					onEdit: () => this.open_form(data_item.name),
+					onSelect: () => this.toggle_delete()
+				}
+			});
+
+			this.rows.push(row);
+		});
+	}
+
+	toggle_select_all(checked) {
+		this.rows.forEach(row => row.toggle_select(checked));
+		this.toggle_delete();
+	}
+
+	open_form(name) {
+		window.location.href = window.location.pathname + "?name=" + name;
+	}
+
+	get_selected() {
+		return this.rows.filter(row => row.is_selected());
+	}
+
+	make_actions() {
+		const delete_button = document.createElement("button");
+		delete_button.id = "delete-select-row";
+		delete_button.classList.add(
+			"btn",
+			"btn-danger",
+			"button-delete",
+			"btn-sm",
+			"ml-2"
+		);
+		delete_button.innerText = "Delete";
+		delete_button.hidden = true;
+		delete_button.onclick = () => this.delete_rows();
+		document.querySelector(".list-view-actions").appendChild(delete_button);
+
+		const new_button = document.createElement("button");
+		new_button.id = "new-row";
+		new_button.classList.add("btn", "btn-primary", "btn-sm", "ml-2");
+		new_button.innerText = "New";
+		new_button.onclick = () =>
+			(window.location.href = window.location.pathname + "?new=1");
+		document.querySelector(".list-view-actions").appendChild(new_button);
+	}
+
+	toggle_delete() {
+		let btn = document.getElementById("delete-select-row");
+		btn.hidden = !this.get_selected().length;
+	}
+
+	delete_rows() {
 		frappe.call({
-			method: 'frappe.www.list.get_list_data',
+			type:"POST",
+			method: "frappe.website.doctype.web_form.web_form.delete_multiple",
 			args: {
-				doctype: this.doctype,
-				fields: this.fields_list.map(df => df.fieldname),
-				web_form_name: this.web_form_name
+				web_form_name: this.web_form_name,
+				docnames: this.get_selected().map(row => row.doc.name)
 			}
-		}).then((response) => console.log(response))
+		}).then(() => this.refresh_list());
+	}
+};
+
+frappe.ui.WebFromListRow = class WebFromListRow {
+	constructor({ row, doc, columns, serial_number, events }) {
+		Object.assign(this, { row, doc, columns, serial_number, events });
+		this.make_row();
 	}
 
-	make_datatable() {
-		console.log("All good")
+	make_row() {
+		// Add Checkboxes
+		let th = document.createElement("th");
+
+		this.checkbox = document.createElement("input");
+		this.checkbox.type = "checkbox";
+		this.checkbox.onchange = event =>
+			this.toggle_select(event.target.checked);
+
+		th.appendChild(this.checkbox);
+		this.row.appendChild(th);
+
+		// Add Serial Number
+		let serialNo = this.row.insertCell();
+		serialNo.innerText = this.serial_number;
+
+		this.data.forEach(data => {
+			let cell = this.row.insertCell();
+			let text = document.createTextNode(data);
+			cell.appendChild(text);
+		});
+
+		let cell = this.row.insertCell();
+		cell.classList.add("text-center");
+
+		let editButton = document.createElement("button");
+		editButton.classList.add("btn", "btn-default", "btn-xs");
+		editButton.innerHTML = `<i class="octicon octicon-pencil"></i>`;
+		editButton.onclick = event => this.events.onEdit();
+		cell.appendChild(editButton);
+		this.row.appendChild(cell);
 	}
 
+	toggle_select(checked) {
+		this.checkbox.checked = checked;
+		this.events.onSelect(checked);
+	}
 
-}
+	is_selected() {
+		return this.checkbox.checked;
+	}
+};
