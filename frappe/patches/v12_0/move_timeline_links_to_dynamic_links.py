dbd0a79ffc7e3672e42de6fd635ac4951e6470dd
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-05-17 14:26:53 +0530

    patch: move timeline and link to dynamic links

--- frappe/patches/v12_0/move_timeline_links_to_dynamic_links.py
+++ frappe/patches/v12_0/move_timeline_links_to_dynamic_links.py
@@ -8,11 +8,21 @@ def execute():
 			fields=[
 				"name", "creation", "modified", "modified_by",
 				"timeline_doctype", "timeline_name",
+				"link_doctype", "link_name"
 			]):
+		counter = 1
 		if communication.timeline_doctype and communication.timeline_name:
 			comm_lists.append((
-				"1", frappe.generate_hash(length=10), "dynamic_links", "Communication",
+				counter, frappe.generate_hash(length=10), "timeline_links", "Communication",
 				communication.name, communication.timeline_doctype, communication.timeline_name,
+				communication.creation, communication.modified, communication.modified_by
+			))
+			counter += 1
+
+		if communication.link_doctype and communication.link_name:
+			comm_lists.append((
+				counter, frappe.generate_hash(length=10), "timeline_links", "Communication",
+				communication.name, communication.link_doctype, communication.link_name,
 				communication.creation, communication.modified, communication.modified_by
 			))
 
