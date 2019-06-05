39bda1749ce9b532db9ca4e99b18ec2dc595834a
Rushabh Mehta <rmehta@gmail.com>
2019-06-05 12:03:11 +0530

    fix(linting)

--- frappe/social/doctype/energy_point_log/energy_point_list.js
+++ frappe/social/doctype/energy_point_log/energy_point_list.js
@@ -8,7 +8,7 @@ frappe.listview_settings['Energy Point Log'] = {
 			'Auto': 'blue',
 			'Revert': 'orange',
 			'Review': 'grey'
-		}
+		};
 		return [__(doc.type), colors[doc.type], "type,=," + doc.type];
 	},
 
@@ -20,7 +20,7 @@ frappe.listview_settings['Energy Point Log'] = {
 			return __('View Ref');
 		},
 		get_description: function(doc) {
-			return __('Open {0}', [`${doc.reference_doctype} ${doc.reference_name}`])
+			return __('Open {0}', [`${doc.reference_doctype} ${doc.reference_name}`]);
 		},
 		action: function(doc) {
 			frappe.set_route('Form', doc.reference_doctype, doc.reference_name);
