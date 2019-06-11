df40bcbd6e59551e3b402c5f2bea8057dc4d450d
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-06-12 00:24:06 +0530

    feat: add manual sync button

--- frappe/integrations/doctype/google_contacts/google_contacts.js
+++ frappe/integrations/doctype/google_contacts/google_contacts.js
@@ -4,13 +4,39 @@
 frappe.ui.form.on('Google Contacts', {
 	refresh: function(frm) {
 		frm.set_value("user", frappe.session.user);
+
+		if (frm.doc.enable && frm.doc.client_id && frm.doc.client_secret) {
+			frm.add_custom_button(__('Sync Contacts'), function () {
+				frappe.show_alert({
+					indicator: 'green',
+					message: __('Syncing')
+				});
+				frappe.call({
+					method: "frappe.integrations.doctype.google_contacts.google_contacts.sync",
+					args: {
+						"doc": frm.doc.name
+					},
+				}).then(() => {
+					frappe.hide_progress();
+					frappe.msgprint(__("Google Contacts Synced."))
+				});
+			});
+		}
 	},
 	allow_contacts_access: function(frm) {
 		frappe.call({
 			method: "frappe.integrations.doctype.google_contacts.google_contacts.google_callback",
-			args: {
-				"doc": frm.doc.name
-			},
+			callback: function(r) {
+				if(!r.exc) {
+					frm.save();
+					window.open(r.message.url);
+				}
+			}
+		});
+	},
+	sync: function(frm) {
+		frappe.call({
+			method: "frappe.integrations.doctype.google_contacts.google_contacts.google_callback",
 			callback: function(r) {
 				if(!r.exc) {
 					frm.save();
