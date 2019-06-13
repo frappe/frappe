6cc0a7ab488bd9ec51f5fe1c52be5cdcc6c9b8c6
Kenneth Sequeira <33246109+kennethsequeira@users.noreply.github.com>
2019-06-13 19:37:01 +0530

    remove redundant expression

--- frappe/website/js/website.js
+++ frappe/website/js/website.js
@@ -336,7 +336,7 @@ $.extend(frappe, {
 
 window.valid_email = function(id) {
 	// eslint-disable-next-line
-	return (/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(id.toLowerCase())== 'false')? 0 : 1;
+	return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(id.toLowerCase());
 }
 
 window.validate_email = valid_email;
