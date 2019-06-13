78d35a1d0c948cd640cb8a17c8b47734d5053212
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-06-14 01:14:22 +0530

    feat: auto sync contacts

--- frappe/integrations/doctype/google_contacts/google_contacts.py
+++ frappe/integrations/doctype/google_contacts/google_contacts.py
@@ -96,59 +96,61 @@ def google_callback(client_id=None, redirect_uri=None, code=None):
 
 		authenticate_access(google_contact)
 
-# @frappe.whitelist()
-# def sync(g_contact=None):
-# 	filters = {"enable": 1}
+@frappe.whitelist()
+def sync(g_contact=None):
+	filters = {"enable": 1}
 
-# 	if contact:
-# 		filters.update({"name": contact})
+	if g_contact:
+		filters.update({"name": g_contact})
 
-# 	access_token = frappe.get_doc("Google Contacts").get_access_token()
-# 	google_contacts = frappe.get_list("Google Contacts", filters=filters)
+	google_contacts = frappe.get_list("Google Contacts", filters=filters)
 
-# 	for google_contact in google_contacts:
-# 		doc = frappe.get_doc("Google Contacts", google_contact.name)
+	for google_contact in google_contacts:
+		doc = frappe.get_doc("Google Contacts", google_contact.name)
 
-# 		headers = {'Authorization': 'Bearer {}'.format(access_token)}
+		access_token = doc.get_access_token()
 
-# 		try:
-# 			r = requests.get(REQUEST, headers=headers, params=PARAMS)
-# 		except Exception as e:
-# 			frappe.throw(e.message)
+		headers = {'Authorization': 'Bearer {}'.format(access_token)}
 
-# 		try:
-# 			r = r.json()
-# 		except Exception as e:
-# 			# if request doesn't return json show HTML ask permissions or to identify the error on google side
-# 			frappe.throw(e)
+		try:
+			r = requests.get(REQUEST, headers=headers, params=PARAMS)
+		except Exception as e:
+			frappe.throw(e.message)
 
-# 		connections = r.get('connections')
-# 		contacts_updated = 0
+		try:
+			r = r.json()
+		except Exception as e:
+			# if request doesn't return json show HTML ask permissions or to identify the error on google side
+			frappe.throw(e)
 
-# 		if connections:
-# 			for idx, connection in enumerate(connections):
-# 				for name in connection.get('names'):
-# 					if contact:
-# 						show_progress(len(connections), "Google Contacts", idx, name.get('displayName'))
-# 					for email in connection.get('emailAddresses'):
-# 						if not frappe.db.exists("Contact", {"email_id": email.get('value')}):
-# 							contacts_updated += 1
-# 							frappe.get_doc({
-# 								"doctype": "Contact",
-# 								"first_name": name.get('givenName'),
-# 								"last_name": name.get('familyName'),
-# 								"email_id": email.get('value'),
-# 								"source": "Google Contacts"
-# 							}).insert(ignore_permissions=True)
+		connections = r.get('connections')
+		contacts_updated = 0
 
-# 			return "{} Google Contacts synced.".format(contacts_updated) if contacts_updated > 0 else "No new Google Contacts synced."
+		frappe.db.set_value("Google Contacts", doc.name, "last_sync_on", frappe.utils.now_datetime())
+		if connections:
+			for idx, connection in enumerate(connections):
+				for name in connection.get('names'):
+					if g_contact:
+						show_progress(len(connections), "Google Contacts", idx, name.get('displayName'))
+					for email in connection.get('emailAddresses'):
+						if not frappe.db.exists("Contact", {"email_id": email.get('value')}):
+							contacts_updated += 1
+							frappe.get_doc({
+								"doctype": "Contact",
+								"first_name": name.get('givenName'),
+								"last_name": name.get('familyName'),
+								"email_id": email.get('value'),
+								"source": "Google Contacts"
+							}).insert(ignore_permissions=True)
 
-# 		return "No Google Contacts present to sync." # If no Google Contacts to sync
+			return "{} Google Contacts synced.".format(contacts_updated) if contacts_updated > 0 else "No new Google Contacts synced."
 
-# def show_progress(length, message, i, description):
-# 	if length > 5:
-# 		frappe.publish_progress(
-# 			float(i) * 100 / length,
-# 			title = message,
-# 			description = description
-# 		)
+		return "No Google Contacts present to sync." # If no Google Contacts to sync
+
+def show_progress(length, message, i, description):
+	if length > 5:
+		frappe.publish_progress(
+			float(i) * 100 / length,
+			title = message,
+			description = description
+		)
