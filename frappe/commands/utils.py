ce058866d3642dd8130a704375ee7a7064b19bab
unknown <pipech@gmail.com>
2019-06-14 16:30:43 +0700

    add app argument to build function

--- frappe/commands/utils.py
+++ frappe/commands/utils.py
@@ -11,7 +11,7 @@ from frappe.utils import update_progress_bar
 @click.option('--make-copy', is_flag=True, default=False, help='Copy the files instead of symlinking')
 @click.option('--restore', is_flag=True, default=False, help='Copy the files instead of symlinking with force')
 @click.option('--verbose', is_flag=True, default=False, help='Verbose')
-def build(make_copy=False, restore = False, verbose=False):
+def build(app=None, make_copy=False, restore = False, verbose=False):
 	"Minify + concatenate JS and CSS files, build translations"
 	import frappe.build
 	import frappe
