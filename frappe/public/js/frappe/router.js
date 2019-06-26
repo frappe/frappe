b5b80c6889c746963d15c5a7ccec9194a9b3511f
Anurag Mishra <mishranaman123@gmail.com>
2019-06-26 16:52:32 +0530

    chore: Resolve merge conflict

--- frappe/public/js/frappe/router.js
+++ frappe/public/js/frappe/router.js
@@ -43,25 +43,9 @@ frappe.route = function() {
 	}
 
 	frappe.route_history.push(route);
-	if (route[0] === 'calendar') {
-		console.log(route[0]);
-		debugger;
-			if(!frappe.view_factory[route[0]]) {
-				frappe.view_factory[route[0]] = new frappe.views["calendarFactory"]();
-		}
 
 	if(route[0]) {
 		const title_cased_route = frappe.utils.to_title_case(route[0]);
-		frappe.view_factory[route[0]].show();
-
-	}
-	else if(route[0] && route[1] && frappe.views[route[0] + "Factory"]) {
-
-	if(route[0] && route[1] && frappe.views[route[0] + "Factory"]) {
-		// has a view generator, generate!
-		if(!frappe.view_factory[route[0]]) {
-			frappe.view_factory[route[0]] = new frappe.views[route[0] + "Factory"]();
-		}
 
 		if (title_cased_route === 'Desktop') {
 			frappe.views.pageview.show('');
