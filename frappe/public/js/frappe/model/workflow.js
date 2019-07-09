cb77aa4a1c51a07cd8437e58a00d6125d9a35d36
Fisher Yu <12823863+szufisher@users.noreply.github.com>
2019-07-09 22:40:31 +0800

    Update workflow.js
    workflow by document other than doctype

--- frappe/public/js/frappe/model/workflow.js
+++ frappe/public/js/frappe/model/workflow.js
@@ -6,25 +6,15 @@ frappe.provide("frappe.workflow");
 frappe.workflow = {
 	state_fields: {},
 	workflows: {},
-	setup: function(doctype) {
-		var wf = frappe.get_list("Workflow", {document_type: doctype});
-		if(wf.length) {
-			frappe.workflow.workflows[doctype] = wf[0];
-			frappe.workflow.state_fields[doctype] = wf[0].workflow_state_field;
-		} else {
-			frappe.workflow.state_fields[doctype] = null;
-		}
+	get_workflow: function(doc){
+		return frappe.model.get_docinfo(doc.doctype, doc.name)  && frappe.model.get_docinfo(doc.doctype, doc.name)['workflow'] ;
 	},
-	get_state_fieldname: function(doctype) {
-		if(frappe.workflow.state_fields[doctype]===undefined) {
-			frappe.workflow.setup(doctype);
-		}
-		return frappe.workflow.state_fields[doctype];
+	get_state_fieldname: function(doc) {
+		return this.get_workflow(doc)  && this.get_workflow(doc)['workflow_state_field'];
 	},
-	get_default_state: function(doctype, docstatus) {
-		frappe.workflow.setup(doctype);
+	get_default_state: function(doc, docstatus) {		
 		var value = null;
-		$.each(frappe.workflow.workflows[doctype].states, function(i, workflow_state) {
+		$.each(this.get_workflow(doc).states, function(i, workflow_state) {
 			if(cint(workflow_state.doc_status)===cint(docstatus)) {
 				value = workflow_state.state;
 				return false;
@@ -33,56 +23,53 @@ frappe.workflow = {
 		return value;
 	},
 	get_transitions: function(doc) {
-		frappe.workflow.setup(doc.doctype);
 		return frappe.xcall('frappe.model.workflow.get_transitions', {doc: doc});
 	},
-	get_document_state: function(doctype, state) {
-		frappe.workflow.setup(doctype);
-		return frappe.get_children(frappe.workflow.workflows[doctype], "states", {state:state})[0];
+	get_document_state: function(doc, state) {
+		return frappe.get_children(this.get_workflow(doc), "states", {state:state})[0];
 	},
-	is_self_approval_enabled: function(doctype) {
-		return frappe.workflow.workflows[doctype].allow_self_approval;
+	is_self_approval_enabled: function(doc) {
+		return this.get_workflow(doc) && this.get_workflow(doc)['allow_self_approval'];
 	},
-	is_read_only: function(doctype, name) {
-		var state_fieldname = frappe.workflow.get_state_fieldname(doctype);
+	is_read_only: function(doc) {
+		if(!doc)
+			return false;
+		if(doc.__islocal)
+			return false;
+		var state_fieldname = this.get_state_fieldname(doc);
 		if(state_fieldname) {
-			var doc = locals[doctype][name];
-			if(!doc)
-				return false;
-			if(doc.__islocal)
-				return false;
-
-			var state = doc[state_fieldname] ||
-				frappe.workflow.get_default_state(doctype, doc.docstatus);
-
-			var allow_edit = state ? frappe.workflow.get_document_state(doctype, state) && frappe.workflow.get_document_state(doctype, state).allow_edit : null;
-
-			if(!frappe.user_roles.includes(allow_edit)) {
-				return true;
-			}
+			//var doc = locals[doctype][name];
+			var state = doc[state_fieldname] || this.get_default_state(doc, doc.docstatus);
+			var filters={'status':['!=','Completed'],'reference_doctype':doc.doctype,'reference_name':doc.name,'user':frappe.session.user}
+			return frappe.db.get_value('Workflow Action',filters,'user').then((r) => {
+				var allow_edit = state ? this.get_document_state(doc, state) && this.get_document_state(doc, state).allow_edit : null;			
+				if((!frappe.user_roles.includes(allow_edit)) || (!r.message)) {
+					return true;
+				}	
+			})			
 		}
 		return false;
 	},
-	get_update_fields: function(doctype) {
-		var update_fields = $.unique($.map(frappe.workflow.workflows[doctype].states || [],
+	get_update_fields: function(doc) {
+		var update_fields = $.unique($.map(this.get_workflow(doc)['states'] || [],
 			function(d) {
 				return d.update_field;
 			}));
 		return update_fields;
 	},
 	get_state(doc) {
-		const state_field = this.get_state_fieldname(doc.doctype);
+		const state_field = this.get_state_fieldname(doc);
 		let state = doc[state_field];
 		if (!state) {
-			state = this.get_default_state(doc.doctype, doc.docstatus);
+			state = this.get_default_state(doc, doc.docstatus);
 		}
 		return state;
 	},
-	get_all_transitions(doctype) {
-		return frappe.workflow.workflows[doctype].transitions || [];
+	get_all_transitions(doc) {
+		return this.get_workflow(doc).transitions || [];
 	},
-	get_all_transition_actions(doctype) {
-		const transitions = this.get_all_transitions(doctype);
+	get_all_transition_actions(doc) {
+		const transitions = this.get_all_transitions(doc);
 		return transitions.map(transition => {
 			return transition.action;
 		});
