47bd288dedba933ba0fa64f859643b4da43a82f6
Fisher Yu <12823863+szufisher@users.noreply.github.com>
2019-07-10 08:12:47 +0800

    Update workflow.js
    add missing ;

--- frappe/public/js/frappe/form/workflow.js
+++ frappe/public/js/frappe/form/workflow.js
@@ -20,19 +20,21 @@ frappe.ui.form.States = Class.extend({
 
 	setup_help: function() {
 		var me = this;
-		var html_content = undefined
+		var html_content = undefined;
 		var state = me.get_state();
 		if (!this.frm.doc.workflow_def) return;
 		frappe.xcall('frappe.model.workflow.get_user_actions',{doc:cur_frm.doc}).then(r => {
 			if (r && r.length){
 				var action_html = function(actions){ 
 					return $.map(actions.split(';'),
-							function(d) {return d.split(':')[0]}
-						).join(", ") || __("None: End of Workflow");
+						function(d) {return d.split(':')[0]
+							    };
+					).join(", ") || __("None: End of Workflow");
 				}
 				var next_html = function(user_status){
 					return $.map(user_status, function(u){
-						return '<p>' + action_html(u.actions).bold() + ' by user ' + u.user.bold() + '</p>'}).join(' ')
+						return '<p>' + action_html(u.actions).bold() + ' by user ' + u.user.bold() + '</p>'
+					}).join(' ')
 				}
 				html_content = "<p>"+__("Current status")+": " + state.bold() + "</p>"
 				+ "<p>"+__("Document is only editable by users of role")+": "
@@ -40,9 +42,10 @@ frappe.ui.form.States = Class.extend({
 				+ "<p>"+__("Next actions")+": "+ next_html(r) +"</p>"
 				+ (me.frm.doc.__islocal ? ("<div class='alert alert-info'>"
 					+__("Workflow will start after saving.")+"</div>") : "")
-				+ "<p class='help'>"+__("Note: Other permission rules may also apply")+"</p>"				
+				+ "<p class='help'>"+__("Note: Other permission rules may also apply")+"</p>";			
 			}
-			else{ return; }
+			else{return;
+			    }
 		}).then(()=>{	
 			if (html_content && html_content.length){			
 				me.frm.page.add_action_item(__("Help"), function() {				
@@ -77,34 +80,33 @@ frappe.ui.form.States = Class.extend({
 	},
 
 	refresh_field_status: function(state){
-		const frm = this.frm		
-		const doctype = this.frm.doctype;
-		const docname = this.frm.docname
+		const frm = this.frm;
+		const docname = this.frm.docname;
 		frappe.xcall('frappe.model.workflow.get_workflow_field_status',
-			 {workflow_name: frm.doc.workflow_def, state: state}).then((field_status)=>{
-				$.each(field_status, function(i, f) {
-					var parent_field = null;
-					var field=f.field_name
-					if (f.field_name.indexOf('.')>0){
-						parent_field= f.field_name.split('.')[0];
-						field =f.field_name.split('.')[1];
-                                                                                             }
-					if (f.reqd){frm.workflow_mandatory_entry = 1};
-					frm.set_df_property(field, 'read_only', f.read_only, docname, parent_field);
-					frm.set_df_property(field, 'reqd', f.reqd, docname, parent_field);													
-					frm.set_df_property(field, 'hidden', f.hidden, docname, parent_field);
-				})
-				if (frm.workflow_mandatory_entry){frm.perm[0].write=1;}	
+			{workflow_name: frm.doc.workflow_def, state: state}).then((field_status)=>{
+			$.each(field_status, function(i, f) {
+				var parent_field = null;
+				var field=f.field_name;
+				if (f.field_name.indexOf('.')>0){
+					parent_field= f.field_name.split('.')[0];
+					field =f.field_name.split('.')[1];
+				}
+				if (f.reqd){frm.workflow_mandatory_entry = 1
+					   }
+				frm.set_df_property(field, 'read_only', f.read_only, docname, parent_field);
+				frm.set_df_property(field, 'reqd', f.reqd, docname, parent_field);													
+				frm.set_df_property(field, 'hidden', f.hidden, docname, parent_field);
 			})
+			if (frm.workflow_mandatory_entry){frm.perm[0].write=1;};
+		})
 
 	},
 
 	show_actions: function() {
 		var added = false;
 		var me = this;
-		var actions = ''
-		var action_list = []
-		var action_array = []
+		var actions = '';
+		var action_list = [];
 
 		this.frm.page.clear_actions_menu();
 
@@ -126,30 +128,30 @@ frappe.ui.form.States = Class.extend({
 		frappe.xcall('frappe.model.workflow.get_user_actions',
 			{doc:me.frm.doc, user: frappe.session.user}).then(r => {
 			if (r && r.length){
-				actions = r[0].actions
-				action_list = actions.split(';')
+				actions = r[0].actions;
+				action_list = actions.split(';');
 			}
 			else{
 				return;
 			}
 			$.each(action_list, function(i, d) {
-				let action_arr = d.split(':')
-				let action = action_arr[0]
-				let transition = action_arr[1]
-				let allow_self_approval = action_arr[2]
+				let action_arr = d.split(':');
+				let action = action_arr[0];
+				let transition = action_arr[1];
+				let allow_self_approval = action_arr[2];
 				if(has_approval_access(allow_self_approval)) {
 					added = true;
 					me.frm.page.add_action_item(__(action), function() {
 						var apply_workflow =function(comment){
 							frappe.xcall('frappe.model.workflow.apply_workflow',
 								{doc: me.frm.doc, action: action, transition_name: transition,
-								 action_source: r[0].action_source, previous_user: r[0].previous_user,
-								 comment: comment})
+								action_source: r[0].action_source, previous_user: r[0].previous_user,
+								comment: comment})
 								.then((doc) => {
 									frappe.model.sync(doc);
 									me.frm.refresh();
 							});
-						}
+						};
 						const is_reqd = action.toLowerCase() == 'reject'? 1 : 0;
 						var d = new frappe.ui.Dialog({
 							title: __(action),
@@ -173,14 +175,14 @@ frappe.ui.form.States = Class.extend({
 				}
 			});
 			if(added && frappe.workflow.get_state(this.frm.doc) != frappe.workflow.get_default_state(this.frm.doc, this.frm.doc.docstatus)) {
-				var special_actions = ['Forward', 'Add Additional Check']
+				var special_actions = ['Forward', 'Add Additional Check'];
 				$.each(special_actions, function(i, action) {
 					me.frm.page.add_action_item(__(action), function() {
 						var d = new frappe.ui.Dialog({
 							title: __(action),
 							fields:  [{fieldtype:'Link', fieldname:'next_user', options:'User',
 								label: __('Select the Next User'), reqd:1},
-								{fieldtype:'Data', fieldname:'comment',	label: __('Comment to Next User')}],
+							{fieldtype:'Data', fieldname:'comment',	label: __('Comment to Next User')}],
 							primary_action: function() {
 								var data = d.get_values();
 								d.hide();							
