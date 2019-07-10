826287c1689f19726ccb0f5542fa1c29d0a83ce2
Himanshu <himanshuwarekar@yahoo.com>
2019-07-10 11:23:48 +0530

    fix: remove patch

--- frappe/patches/v12_0/delete_google_maps_integration.py
+++
@@ -1,6 +0,0 @@
-import frappe
-
-def execute():
-	key = frappe.db.get_single_value("Google Maps Settings", "client_key")
-	frappe.db.set_value("Google Settings", None, "api_key", key)
-	frappe.db.commit()
