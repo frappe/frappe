3aa50caf723ec6ea85cb2f2f5713d641923cbede
deepeshgarg007 <deepeshgarg6@gmail.com>
2019-07-11 18:23:43 +0530

    feat(quick_entry): Allow submitting doc in quick entry

--- frappe/public/js/frappe/form/quick_entry.js
+++ frappe/public/js/frappe/form/quick_entry.js
@@ -159,17 +159,24 @@ frappe.ui.form.QuickEntryForm = Class.extend({
 					doc: me.dialog.doc
 				},
 				callback: function(r) {
-					me.dialog.hide();
-					// delete the old doc
-					frappe.model.clear_doc(me.dialog.doc.doctype, me.dialog.doc.name);
-					me.dialog.doc = r.message;
-					if(frappe._from_link) {
-						frappe.ui.form.update_calling_link(me.dialog.doc);
+
+					if (frappe.model.is_submittable(me.doctype)) {
+						me.dialog.set_primary_action(__('Submit'), function() {
+							me.submit();
+						});
 					} else {
-						if(me.after_insert) {
-							me.after_insert(me.dialog.doc);
+						me.dialog.hide();
+						// delete the old doc
+						frappe.model.clear_doc(me.dialog.doc.doctype, me.dialog.doc.name);
+						me.dialog.doc = r.message;
+						if(frappe._from_link) {
+							frappe.ui.form.update_calling_link(me.dialog.doc);
 						} else {
-							me.open_form_if_not_list();
+							if(me.after_insert) {
+								me.after_insert(me.dialog.doc);
+							} else {
+								me.open_form_if_not_list();
+							}
 						}
 					}
 				},
@@ -182,6 +189,25 @@ frappe.ui.form.QuickEntryForm = Class.extend({
 				},
 				freeze: true
 			});
+		});
+	},
+
+	submit: function() {
+		var me = this;
+		frappe.call({
+			method: "frappe.client.submit",
+			args : {
+				doc: me.dialog.doc
+			},
+			callback: function(r) {
+				me.dialog.hide();
+				// delete the old doc
+				frappe.model.clear_doc(me.dialog.doc.doctype, me.dialog.doc.name);
+				me.dialog.doc = r.message;
+				if (frappe._from_link) {
+					frappe.ui.form.update_calling_link(me.dialog.doc);
+				}
+			}
 		});
 	},
 
