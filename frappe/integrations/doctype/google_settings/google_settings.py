ad377384e83e0eeeccaf0c84c79b65f8259d105a
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-07-12 06:56:17 +0530

    fix: remove validation from settings

--- frappe/integrations/doctype/google_settings/google_settings.py
+++ frappe/integrations/doctype/google_settings/google_settings.py
@@ -3,12 +3,8 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
-from frappe import _
+# import frappe
 from frappe.model.document import Document
 
 class GoogleSettings(Document):
-
-	def validate(self):
-		if not (self.client_id and self.client_secret):
-			frappe.throw(_("Set Client ID and Client Secret for Google Integrations."))
+	pass
