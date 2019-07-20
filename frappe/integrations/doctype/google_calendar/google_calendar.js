183adf165905a38ecc42bb19c5ee764a110d8f23
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-07-20 22:29:06 +0530

    fix: frappe call for authorization

--- frappe/integrations/doctype/google_calendar/google_calendar.js
+++ frappe/integrations/doctype/google_calendar/google_calendar.js
@@ -2,7 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Google Calendar', {
-	// refresh: function(frm) {
+	authorize_google_calendar_access: function(frm) {
+		let reauthorize = 0;
+		if(frm.doc.authorization_code) {
+			reauthorize = 1;
+		}
 
-	// }
+		frappe.call({
+			method: "frappe.integrations.doctype.google_calendar.google_calendar.authorize_access",
+			args: {
+				"g_contact": frm.doc.name,
+				"reauthorize": reauthorize
+			},
+			callback: function(r) {
+				if(!r.exc) {
+					frm.save();
+					window.open(r.message.url);
+				}
+			}
+		});
+	}
 });
