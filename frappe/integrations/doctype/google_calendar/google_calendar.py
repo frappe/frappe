70f016b1f1c79f543747282610df025c0007014d
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-07-20 22:21:32 +0530

    fix: remove unwanted code

--- frappe/integrations/doctype/google_calendar/google_calendar.py
+++ frappe/integrations/doctype/google_calendar/google_calendar.py
@@ -46,38 +46,6 @@ class GoogleCalendar(Document):
 		return r.get("access_token")
 
 @frappe.whitelist()
-def google_callback(code=None, state=None, account=None):
-	redirect_uri = get_request_site_address(True) + "?cmd=frappe.integrations.doctype.gcalendar_settings.gcalendar_settings.google_callback"
-	if account is not None:
-		frappe.cache().hset("gcalendar_account","GCalendar Account", account)
-	doc = frappe.get_doc("GCalendar Settings")
-	if code is None:
-		return {
-			'url': 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&response_type=code&prompt=consent&client_id={}&include_granted_scopes=true&scope={}&redirect_uri={}'.format(doc.client_id, SCOPES, redirect_uri)
-			}
-	else:
-		try:
-			account = frappe.get_doc("Google Calendar", frappe.cache().hget("gcalendar_account", "GCalendar Account"))
-			data = {'code': code,
-				'client_id': doc.client_id,
-				'client_secret': doc.get_password(fieldname='client_secret',raise_exception=False),
-				'redirect_uri': redirect_uri,
-				'grant_type': 'authorization_code'}
-
-			r = requests.post('https://www.googleapis.com/oauth2/v4/token', data=data).json()
-			frappe.db.set_value("GCalendar Account", account.name, "authorization_code", code)
-			if 'access_token' in r:
-				frappe.db.set_value("GCalendar Account", account.name, "session_token", r['access_token'])
-			if 'refresh_token' in r:
-				frappe.db.set_value("GCalendar Account", account.name, "refresh_token", r['refresh_token'])
-			frappe.db.commit()
-			frappe.local.response["type"] = "redirect"
-			frappe.local.response["location"] = "/integrations/gcalendar-success.html"
-			return
-		except Exception as e:
-			frappe.throw(e.message)
-
-@frappe.whitelist()
 def authorize_access(g_calendar, reauthorize=None):
 	"""
 		If no Authorization code get it from Google and then request for Refresh Token.
