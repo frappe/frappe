a0de7c0dfbb9fcd9b5dbb792e943854d09fca2ea
Suraj Shetty <surajshetty3416@gmail.com>
2019-07-26 10:51:06 +0530

    fix: Patch to create file in right location

---
+++ frappe/patches/v12_0/fix_public_private_files.py
@@ -0,0 +1,33 @@
+import frappe
+import os
+
+def execute():
+	files = frappe.get_all('File',
+		fields=['is_private', 'file_url', 'name'],
+		filters={'is_folder': 0})
+
+	for file in files:
+		file_url = file.file_url
+		if file.is_private:
+			if not file_url.startswith('/private/files/'):
+				generate_file(file.name)
+		else:
+			if file_url.startswith('/private/files/'):
+				generate_file(file.name)
+
+def generate_file(file_name):
+	try:
+		file_doc = frappe.get_doc('File', file_name)
+		# private
+		new_doc = frappe.new_doc('File')
+		new_doc.is_private = file_doc.is_private
+		new_doc.file_name = file_doc.file_name
+		# to create copy of file in right location
+		# if the file doc is private file will be created in /private folder
+		# if the file doc is public file will be created in /files folder
+		new_doc.save_file(content=file_doc.get_content(), ignore_existing_file_check=True)
+
+		file_doc.file_url = new_doc.file_url
+		file_doc.save()
+	except IOError:
+		pass
