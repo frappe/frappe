e405e1625941dd431f5535cb4e50b1fac4e25091
Gavin D'souza <gavin18d@gmail.com>
2019-07-26 18:34:51 +0530

    feat: Access log doctype for logging information about private file downloads, backups downloads, printing and exporting data

---
+++ frappe/core/doctype/access_log/access_log.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Frappe Technologies and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+
+class AccessLog(Document):
+	pass
+
+
+@frappe.whitelist()
+def make_access_log(doctype=None, file_type=None, document=None, filters=None, report_name=None, page=None, backup=False):
+	user = frappe.get_user().load_user()
+	user_name = user.username if user.username else user.first_name
+	
+	if backup:
+		report_name = 'Backup'
+
+	frappe.get_doc({
+		'doctype': 'Access Log',
+		'name': "{}-{}".format(user_name,frappe.generate_hash(length=5)),
+		'user': user.first_name,
+		'export_from': doctype,
+		'reference_document': document,
+		'file_type': file_type,
+		'report_name': report_name,
+		'page': page,
+		'filters': filters
+	}).insert()
+	frappe.db.commit()
