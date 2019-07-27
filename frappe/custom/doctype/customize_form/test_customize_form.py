50c10a2fcf381eeb52a7548bf823518cd1692aba
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-07-27 11:24:15 +0530

    fix: test case for customize form

--- frappe/custom/doctype/customize_form/test_customize_form.py
+++ frappe/custom/doctype/customize_form/test_customize_form.py
@@ -46,7 +46,7 @@ class TestCustomizeForm(unittest.TestCase):
 
 		d = self.get_customize_form("Event")
 		self.assertEquals(d.doc_type, "Event")
-		self.assertEquals(len(d.get("fields")), 28)
+		self.assertEquals(len(d.get("fields")), 29)
 
 		d = self.get_customize_form("Event")
 		self.assertEquals(d.doc_type, "Event")
