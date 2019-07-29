98819bbf4207843e95303ea61543274cb0573b17
Faris Ansari <netchampfaris@users.noreply.github.com>
2019-07-29 10:13:53 +0530

    fix: Dont update modified

--- frappe/patches/v12_0/rename_events_repeat_on.py
+++ frappe/patches/v12_0/rename_events_repeat_on.py
@@ -15,4 +15,4 @@ def execute():
 
 	for weekly_event in weekly_events:
 		# Set WeekDay based on the starts_on so that event can repeat Weekly
-		frappe.db.set_value('Event', weekly_event.name, weekdays[get_datetime(weekly_event.starts_on).weekday()], 1, update_modified=1)
+		frappe.db.set_value('Event', weekly_event.name, weekdays[get_datetime(weekly_event.starts_on).weekday()], 1, update_modified=False)
