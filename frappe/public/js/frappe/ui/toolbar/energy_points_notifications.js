83743b59b38fbb2c91829a512f61e490d11d86ed
Prssanna Desai <prssud@gmail.com>
2019-08-02 12:26:37 +0530

    fix: formatting fixes

--- frappe/public/js/frappe/ui/toolbar/energy_points_notifications.js
+++ frappe/public/js/frappe/ui/toolbar/energy_points_notifications.js
@@ -10,10 +10,10 @@ frappe.ui.EnergyPointsNotifications = class {
 	}
 
 	setup_energy_points_notifications() {
-		this.get_energy_points_list(this.max_length).then(user_points_list=> {
+		this.get_energy_points_list(this.max_length).then(user_points_list => {
 			this.dropdown_items = user_points_list;
 			this.render_energy_points_dropdown();
-			if(this.$dropdown_list.find('.unseen').length) {
+			if (this.$dropdown_list.find('.unseen').length) {
 				this.$notification_indicator.show();
 			}
 		});
@@ -27,21 +27,21 @@ frappe.ui.EnergyPointsNotifications = class {
 			this.update_dropdown();
 		});
 
-		this.$dropdown.on('hide.bs.dropdown',()=> {
+		this.$dropdown.on('hide.bs.dropdown', () => {
 			this.$notification_indicator.hide();
 			this.$dropdown_list.find('.unseen').removeClass('unseen');
 		});
 
-		this.$dropdown.on('show.bs.dropdown',()=> {
+		this.$dropdown.on('show.bs.dropdown', () => {
 			this.check_seen();
 		});
 	}
 
 	update_dropdown() {
-		this.get_energy_points_list(1).then(r=> {
+		this.get_energy_points_list(1).then(r => {
 			let new_item = r[0];
 			this.dropdown_items.unshift(new_item);
-			if(this.dropdown_items.length > this.max_length) {
+			if (this.dropdown_items.length > this.max_length) {
 				this.$dropdown_list.find('.recent-points-item').last().remove();
 				this.dropdown_items.pop();
 			}
@@ -51,19 +51,19 @@ frappe.ui.EnergyPointsNotifications = class {
 	}
 
 	check_seen() {
-		let unseen_items = this.dropdown_items.filter(item=> item.seen === 0);
+		let unseen_items = this.dropdown_items.filter(item => item.seen === 0);
 		frappe.call('frappe.social.doctype.energy_point_log.energy_point_log.set_notification_as_seen', {notifications: unseen_items});
 	}
 
 	get_energy_points_date_range(date) {
 		let current_date = frappe.datetime.now_date();
 		let prev_week = frappe.datetime.add_days(current_date, -7);
-		let prev_month = frappe.datetime.add_months(frappe.datetime.now_date(),-1);
-		if(date >= current_date) {
+		let prev_month = frappe.datetime.add_months(frappe.datetime.now_date(), -1);
+		if (date >= current_date) {
 			return 'Today';
-		} else if(date > prev_week) {
+		} else if (date > prev_week) {
 			return 'Last 7 days';
-		} else if(date > prev_month) {
+		} else if (date > prev_month) {
 			return 'Last 30 days';
 		} else  {
 			return 'Older';
@@ -71,9 +71,16 @@ frappe.ui.EnergyPointsNotifications = class {
 	}
 
 	get_energy_points_list(limit) {
-		return frappe.db.get_list('Energy Point Log', {filters: {user: frappe.session.user, type:['not in',['Review']]},
-			fields:['name','user', 'points', 'reference_doctype', 'reference_name', 'reason', 'type', 'seen', 'rule', 'owner', 'creation'],
-			limit:limit, order_by:'creation desc'}).then((energy_points_list)=> {
+		return frappe.db.get_list('Energy Point Log', {
+			filters: {
+				user: frappe.session.user,
+				type: ['not in',['Review']]
+			},
+			fields:
+				['name','user', 'points', 'reference_doctype', 'reference_name', 'reason', 'type', 'seen', 'rule', 'owner', 'creation'],
+			limit: limit,
+			order_by:'creation desc'
+		}).then((energy_points_list) => {
 			return energy_points_list;
 		});
 	}
@@ -86,17 +93,17 @@ frappe.ui.EnergyPointsNotifications = class {
 			</li>`;
 		let body_html = '';
 
-		if(this.dropdown_items.length) {
+		if (this.dropdown_items.length) {
 			let date_range= this.get_energy_points_date_range(this.dropdown_items[0].creation);
 			body_html += `<li class="points-date-range text-muted">${__(date_range)}</li>`;
-			this.dropdown_items.forEach(field=> {
+			this.dropdown_items.forEach(field => {
 				let current_field_date_range = this.get_energy_points_date_range(field.creation);
-				if(date_range !== current_field_date_range) {
+				if (date_range !== current_field_date_range) {
 					body_html+=`<li class="points-date-range text-muted">${__(current_field_date_range)}</li>`;
 					date_range=current_field_date_range;
 				}
 				let item_html = this.get_dropdown_item_html(field);
-				if(item_html) body_html += item_html;
+				if (item_html) body_html += item_html;
 			});
 		} else {
 			body_html += `<li><a href="#" onclick = "return false" class="text-muted text-center">${__('No activity')}</a></li>`;
@@ -130,15 +137,15 @@ frappe.ui.EnergyPointsNotifications = class {
 		let owner_name = frappe.user.full_name(field.owner).trim();
 		owner_name = this.truncate_value(owner_name);
 		let message_html = '';
-		if(field.type === 'Auto' ) {
+		if (field.type === 'Auto' ) {
 			message_html = `For ${__(field.rule)} <span class="points-reason-name text-muted">${__(field.reference_name)}</span>`;
 		} else {
 			let message;
-			if(field.type === 'Appreciation') {
+			if (field.type === 'Appreciation') {
 				message = `${__(owner_name)} appreciated your work on `;
-			} else if(field.type === 'Criticism') {
+			} else if (field.type === 'Criticism') {
 				message =  `${__(owner_name)} criticized your work on `;
-			} else if(field.type === 'Revert') {
+			} else if (field.type === 'Revert') {
 				message =  `${__(owner_name)} reverted your points on `;
 			}
 			let reason_string = '- "' + this.truncate_value(field.reason) + '"';
