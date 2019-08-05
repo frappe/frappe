0ce662f3f44fd3d7966453736183c66f3e41e768
Fisher Yu <12823863+szufisher@users.noreply.github.com>
2019-08-05 14:00:50 +0800

    Update data_import.js
    prevent multiple times click the start import button to import same data multiple times

--- frappe/core/doctype/data_import/data_import.js
+++ frappe/core/doctype/data_import/data_import.js
@@ -84,6 +84,8 @@ frappe.ui.form.on('Data Import', {
 			frm.doc.docstatus === 0 && (!frm.doc.import_status || frm.doc.import_status == "Failed")) {
 			frm.page.set_primary_action(__("Start Import"), function() {
 				frappe.call({
+					freeze: true,
+					btn: frm.page.btn_primary,
 					method: "frappe.core.doctype.data_import.data_import.import_data",
 					args: {
 						data_import: frm.doc.name
