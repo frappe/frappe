291a14f47c0872e616d674623faef504ac60af6f
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2019-08-06 05:24:01 +0530

    style: Fix indentation

--- frappe/desk/reportview.py
+++ frappe/desk/reportview.py
@@ -138,9 +138,9 @@ def export_query():
 		del form_params["selected_items"]
 
 	make_access_log(doctype=doctype,
-			file_type=file_format_type,
-			report_name=form_params.report_name,
-			filters=form_params.filters)
+		file_type=file_format_type,
+		report_name=form_params.report_name,
+		filters=form_params.filters)
 
 	db_query = DatabaseQuery(doctype)
 	ret = db_query.execute(**form_params)
