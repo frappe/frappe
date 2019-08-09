c9c1baa3b07ddff966110b02a52e97d5c593f811
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-08-09 16:47:05 +0530

    patch: fix patch to escape quotes

--- frappe/patches/v12_0/move_email_and_phone_to_child_table.py
+++ frappe/patches/v12_0/move_email_and_phone_to_child_table.py
@@ -8,26 +8,28 @@ def execute():
 
 	name_counter = 100000000
 	for contact_detail in contact_details:
+		contact_name = contact_detail.name.replace('"', '\\"').replace("'", "\\'")
+
 		if contact_detail.email_id:
 			frappe.db.sql("""
 				INSERT INTO `tabContact Email`
 					(`idx`, `name`, `email_id`, `parentfield`, `parenttype`, `parent`, `is_primary`, `creation`, `modified`, `modified_by`)
-				VALUES ("1", {0}, {1}, "email_ids", "Contact Email", {2}, "1", {3}, {4}, {5})
-			""".format(name_counter, contact_detail.email_id, contact_detail.creation, contact_detail.modified, contact_detail.modified_by))
+				VALUES (1, "{0}", "{1}", "email_ids", "Contact", "{2}", 1, "{3}", "{4}", "{5}")
+			""".format(str(name_counter), contact_detail.email_id, contact_name, contact_detail.creation, contact_detail.modified, contact_detail.modified_by))
 			name_counter += 1
 
 		if contact_detail.phone:
 			frappe.db.sql("""
 				INSERT INTO `tabContact Phone`
 					(`idx`, `name`, `phone`, `parentfield`, `parenttype`, `parent`, `is_primary`, `creation`, `modified`, `modified_by`)
-				VALUES ("1", {0}, {1}, "email_ids", "Contact Email", {2}, "1", {3}, {4}, {5})
-			""".format(name_counter, contact_detail.phone, contact_detail.creation, contact_detail.modified, contact_detail.modified_by))
+				VALUES (1, "{0}", "{1}", "phone_nos", "Contact", "{2}", 1, "{3}", "{4}", "{5}")
+			""".format(str(name_counter), contact_detail.phone, contact_name, contact_detail.creation, contact_detail.modified, contact_detail.modified_by))
 			name_counter += 1
 
 		if contact_detail.mobile_no:
 			frappe.db.sql("""
 				INSERT INTO `tabContact Phone`
 					(`idx`, `name`, `phone`, `parentfield`, `parenttype`, `parent`, `is_primary`, `creation`, `modified`, `modified_by`)
-				VALUES ("1", {0}, {1}, "email_ids", "Contact Email", {2}, "0", {3}, {4}, {5})
-			""".format(name_counter, contact_detail.mobile_no, contact_detail.creation, contact_detail.modified, contact_detail.modified_by))
+				VALUES (2, "{0}", "{1}", "phone_nos", "Contact", "{2}", 0, "{3}", "{4}", "{5}")
+			""".format(str(name_counter), contact_detail.mobile_no, contact_name, contact_detail.creation, contact_detail.modified, contact_detail.modified_by))
 			name_counter += 1
