df81d56de0a4a230eabd96db69df3cae0f744005
frappe <frappe@ubuntu.vm>
2019-08-18 08:13:57 +0000

    fix frontmatter nonsens

--- frappe/website/router.py
+++ frappe/website/router.py
@@ -231,7 +231,7 @@ def get_page_info(path, app, start, basepath=None, app_path=None, fname=None):
 
 def setup_source(page_info):
 	'''Get the HTML source of the template'''
-	from frontmatter import Frontmatter
+	# from frontmatter import Frontmatter
 
 	jenv = frappe.get_jenv()
 	source = jenv.loader.get_source(jenv, page_info.template)[0]
@@ -239,14 +239,14 @@ def setup_source(page_info):
 
 	if page_info.template.endswith('.md'):
 		# extract frontmatter block if exists
-		try:
-			# values will be used to update page_info
-			res = Frontmatter.read(source)
-			if res['attributes']:
-				page_info.update(res['attributes'])
-				source = res['body']
-		except Exception as e:
-			pass
+		#try:
+		#	# values will be used to update page_info
+		#	res = Frontmatter.read(source)
+		#	if res['attributes']:
+		#		page_info.update(res['attributes'])
+		#		source = res['body']
+		#except Exception as e:
+		#	pass
 
 		source = frappe.utils.md_to_html(source)
 
