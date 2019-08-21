8a2d224000c904abd05297d2fb77f2a7b189bef2
Shivam Mishra <scm.mymail@gmail.com>
2019-08-21 18:02:15 +0530

    refactor: filter doc_type to hide core doctypes

--- frappe/custom/doctype/property_setter/property_setter.js
+++ frappe/custom/doctype/property_setter/property_setter.js
@@ -6,5 +6,18 @@ frappe.ui.form.on('Property Setter', {
 		if(frm.doc.property_type=='Check' && !in_list(['0','1'], frm.doc.value)) {
 			frappe.throw(__('Value for a check field can be either 0 or 1'));
 		}
+	},
+	onload: function(frm) {
+		frm.set_query("doc_type", function() {
+			return {
+				translate_values: false,
+				filters: [
+					['DocType', 'issingle', '=', 0],
+					['DocType', 'custom', '=', 0],
+					['DocType', 'name', 'not in', frappe.model.core_doctypes_list],
+					['DocType', 'restrict_to_domain', 'in', frappe.boot.active_domains]
+				]
+			};
+		});
 	}
 });
