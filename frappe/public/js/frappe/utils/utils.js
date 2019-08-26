b05159f3495f4c1ab788367bd9b4951b939851d3
Faris Ansari <netchamp.faris@gmail.com>
2019-08-26 19:54:26 +0530

    fix: Remove event listener before attaching

--- frappe/public/js/frappe/utils/utils.js
+++ frappe/public/js/frappe/utils/utils.js
@@ -727,7 +727,10 @@ Object.assign(frappe.utils, {
 		return ["ar", "he", "fa"].includes(frappe.boot.lang);
 	},
 	bind_actions_with_class($el, class_instance) {
-		$($el).on('click', '[data-action]', e => {
+		// remove previously bound event
+		$($el).off('click.class_actions');
+		// attach new event
+		$($el).on('click.class_actions', '[data-action]', e => {
 			let $target = $(e.currentTarget);
 			let action = $target.data('action');
 			let method = class_instance[action];
