8c44f6b8e795cec7f9d76ec1df6f93f656af5f40
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-09-05 21:02:46 +0530

    fix: insert document at start

--- frappe/desk/doctype/global_search_settings/global_search_settings.py
+++ frappe/desk/doctype/global_search_settings/global_search_settings.py
@@ -19,6 +19,15 @@ class GlobalSearchSettings(Document):
 			core_dts = (", ".join([frappe.bold(dt) for dt in core_dts]))
 			frappe.throw(_("Core Modules {0} cannot be searched in Global Search.").format(core_dts))
 
+	def insert_at_start(self, doctype):
+		for document in self.allowed_in_global_search:
+			document.idx += 1
+
+		self.append("allowed_in_global_search", {
+			"document": doctype,
+			"idx": 1
+		})
+
 def get_doctypes_for_global_search(as_list=False):
 	doctypes = frappe.get_list("Global Search Allow", fields=["document"], order_by="idx ASC")
 	if not doctypes:
