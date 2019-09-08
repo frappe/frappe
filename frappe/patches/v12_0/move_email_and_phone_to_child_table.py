14b4991d6254731b6d84cc471106e9992413fd8d
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-09-08 15:32:02 +0530

    fix: query

--- frappe/patches/v12_0/move_email_and_phone_to_child_table.py
+++ frappe/patches/v12_0/move_email_and_phone_to_child_table.py
@@ -17,50 +17,58 @@ def execute():
 		is_primary = 1
 
 		if contact_detail.email_id:
-			email_values.append("""(1, '{0}', {1}, 'email_ids', 'Contact', {2}, 1, '{3}', '{4}', {5})""".format(
+			email_values.append([
+				1,
 				frappe.generate_hash(contact_detail.email_id, 10),
-				frappe.db.escape(contact_detail.email_id),
-				frappe.db.escape(contact_detail.name),
+				contact_detail.email_id,
+				'email_ids',
+				'Contact',
+				contact_detail.name,
+				1,
 				contact_detail.creation,
 				contact_detail.modified,
-				frappe.db.escape(contact_detail.modified_by)
-			))
+				contact_detail.modified_by
+			])
 
 		if contact_detail.phone:
 			is_primary = 1 if phone_counter == 1 else 0
-			phone_values.append("""({0}, '{1}', {2}, 'phone_nos', 'Contact', {3}, {4}, '{5}', '{6}', {7})""".format(
+			phone_values.append([
 				phone_counter,
 				frappe.generate_hash(contact_detail.email_id, 10),
-				frappe.db.escape(contact_detail.phone),
-				frappe.db.escape(contact_detail.name),
+				contact_detail.phone,
+				'phone_nos',
+				'Contact',
+				contact_detail.name,
 				is_primary,
 				contact_detail.creation,
 				contact_detail.modified,
-				frappe.db.escape(contact_detail.modified_by)
-			))
+				contact_detail.modified_by
+			])
 			phone_counter += 1
 			is_primary += 1
 
 		if contact_detail.mobile_no:
 			is_primary = 1 if phone_counter == 1 else 0
-			phone_values.append("""({0}, '{1}', {2}, 'phone_nos', 'Contact', {3}, {4}, '{5}', '{6}', {7})""".format(
+			phone_values.append([
 				phone_counter,
 				frappe.generate_hash(contact_detail.email_id, 10),
-				frappe.db.escape(contact_detail.phone),
-				frappe.db.escape(contact_detail.name),
+				contact_detail.phone,
+				'phone_nos',
+				'Contact',
+				contact_detail.name,
 				is_primary,
 				contact_detail.creation,
 				contact_detail.modified,
-				frappe.db.escape(contact_detail.modified_by)
-			))
+				contact_detail.modified_by
+			])
 
 		if email_values and (count%10000 == 0 or count == len(contact_details)-1):
 			frappe.db.sql("""
 				INSERT INTO `tabContact Email`
 					(`idx`, `name`, `email_id`, `parentfield`, `parenttype`, `parent`, `is_primary`, `creation`,
 					`modified`, `modified_by`)
-				VALUES {0}
-			""".format(", ".join([d for d in email_values])))
+				VALUES {}
+			""".format(", ".join(['%s'] * len(email_values))), tuple(email_values))
 
 			email_values = []
 
@@ -69,8 +77,8 @@ def execute():
 				INSERT INTO `tabContact Phone`
 					(`idx`, `name`, `phone`, `parentfield`, `parenttype`, `parent`, `is_primary`, `creation`,
 					`modified`, `modified_by`)
-				VALUES {0}
-			""".format(", ".join([d for d in phone_values])))
+				VALUES {}
+			""".format(", ".join(['%s'] * len(phone_values))), tuple(phone_values))
 
 			phone_values = []
 
