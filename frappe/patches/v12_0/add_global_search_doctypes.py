956d9ddfdc73297c97db08026e0cf71c20635ce8
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-09-11 13:52:03 +0530

    feat: query optimization for global search

--- frappe/patches/v12_0/add_global_search_doctypes.py
+++ frappe/patches/v12_0/add_global_search_doctypes.py
@@ -2,4 +2,6 @@ import frappe
 from frappe.desk.page.setup_wizard.install_fixtures import update_global_search_doctypes
 
 def execute():
+	frappe.reload_doc("desk", "doctype", "global_search_allow")
+	frappe.reload_doc("desk", "doctype", "global_search_settings")
 	update_global_search_doctypes()
