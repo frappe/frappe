517e4a1d711716ad8332a5565eae029bacd93154
Himanshu <himanshuwarekar@yahoo.com>
2019-09-16 12:02:06 +0530

    fix: v11 patch breaking as v11 dint have gcontacts (#8426)

--- frappe/contacts/doctype/contact/contact.py
+++ frappe/contacts/doctype/contact/contact.py
@@ -42,7 +42,7 @@ class Contact(Document):
 		if self.email_id and not self.image:
 			self.image = has_gravatar(self.email_id)
 
-		if self.sync_with_google_contacts and not self.google_contacts:
+		if self.get("sync_with_google_contacts") and not self.get("google_contacts"):
 			frappe.throw(_("Select Google Contacts to which contact should be synced."))
 
 		deduplicate_dynamic_links(self)
