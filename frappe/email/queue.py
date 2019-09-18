dfed827b7ba3d3b55b91a140dfc5a66c00d690ea
Gavin D'souza <gavin18d@gmail.com>
2019-09-18 18:33:55 +0530

    fix: fixed email count for current month

--- frappe/email/queue.py
+++ frappe/email/queue.py
@@ -277,8 +277,10 @@ def check_email_limit(recipients):
 				EmailLimitCrossedError)
 
 def get_emails_sent_this_month():
-	return frappe.db.sql("""select count(name) from `tabEmail Queue` where
-		status='Sent' and MONTH(creation)=MONTH(CURDATE())""")[0][0]
+	return frappe.db.sql("""
+		SELECT COUNT(*) FROM `tabEmail Queue`
+		WHERE `status`='Sent' AND EXTRACT(YEAR_MONTH FROM `creation`) = EXTRACT(YEAR_MONTH FROM NOW())
+	""")[0][0]
 
 def get_emails_sent_today():
 	return frappe.db.sql("""select count(name) from `tabEmail Queue` where
