d3b0c1a00f888c4cebe957971fafced489ca49c4
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2019-09-24 17:25:07 +0530

    fix: Remove unwanted patch

--- frappe/patches/v12_0/set_mobile_no.py
+++
@@ -1,7 +0,0 @@
-import frappe
-
-def execute():
-	frappe.reload_doc("contacts", "doctype", "contact_phone")
-	frappe.reload_doc("contacts", "doctype", "contact")
-
-	frappe.db.sql("""UPDATE `tabContact Phone` SET is_primary_mobile_no='1' WHERE idx='2'""")
