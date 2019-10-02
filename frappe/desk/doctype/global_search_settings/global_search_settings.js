3f980dbd79844407d60adaf8e470d389ac337e73
Himanshu <himanshuwarekar@yahoo.com>
2019-10-02 17:13:42 +0530

    fix(Global Search Settings): Show progress while setting global search doctypes (#8515)
    * fix: add frm dashboard
    
    * fix: add search for other domains
    
    * fix: set global search doctypes for all domains
    
    * fix: add doctypes to global search dict in hooks
    
    * fix: Remove unused variable

--- frappe/desk/doctype/global_search_settings/global_search_settings.js
+++ frappe/desk/doctype/global_search_settings/global_search_settings.js
@@ -3,6 +3,16 @@
 
 frappe.ui.form.on('Global Search Settings', {
 	refresh: function(frm) {
+
+		frappe.realtime.on('global_search_settings', (data) => {
+			if (data.progress) {
+				frm.dashboard.show_progress('Setting up Global Search', data.progress / data.total * 100, data.msg);
+				if (data.progress === data.total) {
+					frm.dashboard.hide_progress('Setting up Global Search');
+				}
+			}
+		});
+
 		frm.add_custom_button(__("Reset"), function () {
 			frappe.call({
 				method: "frappe.desk.doctype.global_search_settings.global_search_settings.reset_global_search_settings_doctypes",
