652a0b5329c73877901c4f49920839a03c130f46
Rucha Mahabal <ruchamahabal2@gmail.com>
2019-10-09 08:40:43 +0530

    fix: child table mapping

--- frappe/events_streaming/doctype/document_type_mapping/document_type_mapping.js
+++ frappe/events_streaming/doctype/document_type_mapping/document_type_mapping.js
@@ -3,18 +3,10 @@
 
 frappe.ui.form.on('Document Type Mapping', {
 	refresh: function(frm) {
-		//set fieldname select
-		if (frm.doc.local_doctype) {
-			frappe.model.with_doctype(frm.doc.local_doctype, () => {
-				//get doctype fields
-				let fields = frm.events.get_fields(frm);
-				frappe.meta.get_docfield('Document Type Field Mapping', 'local_fieldname', frm.doc.name).options = [''].concat(fields);
-			});
-		}
 	},
 
 	local_doctype: function(frm) {
-		frappe.model.with_doctype(frm.doc.local_doctype, () => {
+		if (frm.doc.local_doctype) {
 			let fields = frm.events.get_fields(frm);
 			frappe.model.clear_table(frm.doc, 'field_mapping');
 			$.each(fields, function(i, data) {
@@ -22,17 +14,17 @@ frappe.ui.form.on('Document Type Mapping', {
 				row.local_fieldname = data.value;
 			});
 			refresh_field('field_mapping');
-		});
+		}
 	},
 
 	get_fields: function(frm) {
 		let fields = $.map(frappe.get_doc('DocType', frm.doc.local_doctype).fields, (data) => {
 			if (frappe.model.no_value_type.includes(data.fieldtype) && !(frappe.model.table_fields.includes(data.fieldtype))) {
 				return null;
-			} else if(data.fieldname == 'remote_docname' || data.fieldname == 'remote_site_name') {
+			} else if (data.fieldname == 'remote_docname' || data.fieldname == 'remote_site_name') {
 				return null;
 			} else {
-				return { label: data.fieldname, value: data.fieldname };
+				return { value: data.fieldname };
 			}
 		});
 		return fields
--- frappe/events_streaming/doctype/document_type_mapping/document_type_mapping.py
+++ frappe/events_streaming/doctype/document_type_mapping/document_type_mapping.py
@@ -12,8 +12,28 @@ class DocumentTypeMapping(Document):
 		doc = frappe._dict(json.loads(update))
 		for mapping in self.field_mapping:
 			if doc.get(mapping.remote_fieldname):
-				#copy value into local fieldname key and remove remote fieldname key
-				doc[mapping.local_fieldname] = doc[mapping.remote_fieldname]
-				doc.pop(mapping.remote_fieldname, None)
+
+				if mapping.is_child_table:
+					doc[mapping.local_fieldname] = self.get_mapped_child_table_docs(mapping.child_table_mapping, doc[mapping.remote_fieldname])
+					doc.pop(mapping.remote_fieldname, None)
+
+				else:
+					#copy value into local fieldname key and remove remote fieldname key
+					doc[mapping.local_fieldname] = doc[mapping.remote_fieldname]
+					doc.pop(mapping.remote_fieldname, None)
+
 		doc['doctype'] = self.local_doctype
 		return frappe.as_json(doc)
+
+	def get_mapped_child_table_docs(self, child_map, table_entries):
+		child_map = frappe.get_doc('Document Type Mapping', child_map)
+		mapped_entries = []
+		for entry in table_entries:
+			child_doc = entry
+			for mapping in child_map.field_mapping:
+				if child_doc.get(mapping.remote_fieldname):
+					child_doc[mapping.local_fieldname] = child_doc[mapping.remote_fieldname]
+					child_doc.pop(mapping.remote_fieldname, None)
+			child_doc['doctype'] = child_map.local_doctype
+			mapped_entries.append(child_doc)
+		return mapped_entries
