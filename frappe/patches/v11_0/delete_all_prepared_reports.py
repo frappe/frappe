106b7e995c6c2fd51fdec412964ab9dfa4e2e16c
Sahil Khan <sahilkhan28297@gmail.com>
2019-10-11 16:08:16 +0530

    fix(patch): reload prepared report doctype

--- frappe/patches/v11_0/delete_all_prepared_reports.py
+++ frappe/patches/v11_0/delete_all_prepared_reports.py
@@ -3,6 +3,7 @@ import frappe
 
 def execute():
 	if frappe.db.table_exists('Prepared Report'):
+		frappe.reload_doc("core", "doctype", "prepared_doctype")
 		prepared_reports = frappe.get_all("Prepared Report")
 		for report in prepared_reports:
 			frappe.delete_doc("Prepared Report", report.name)
