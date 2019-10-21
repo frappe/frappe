83c10c92634d56f857beed83d4075eb7e6996858
Aditya Hase <aditya@adityahase.com>
2019-10-21 20:16:34 +0530

    perf(database): Regularly clean old unhandled emails
    Co-authored-by: sahil28297 <sahilkhan28297@gmail.com>

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -188,7 +188,8 @@ scheduler_events = {
 		"frappe.social.doctype.energy_point_settings.energy_point_settings.allocate_review_points",
 		"frappe.integrations.doctype.google_contacts.google_contacts.sync",
 		"frappe.automation.doctype.auto_repeat.auto_repeat.make_auto_repeat_entry",
-		"frappe.automation.doctype.auto_repeat.auto_repeat.set_auto_repeat_as_completed"
+		"frappe.automation.doctype.auto_repeat.auto_repeat.set_auto_repeat_as_completed",
+		"frappe.email.doctype.unhandled_email.unhandled_email.remove_old_unhandled_emails"
 	],
 	"daily_long": [
 		"frappe.integrations.doctype.dropbox_settings.dropbox_settings.take_backups_daily",
