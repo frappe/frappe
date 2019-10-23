b50d6c08151a9860971019b70f1caa7dd604c30f
Himanshu <himanshuwarekar@yahoo.com>
2019-10-23 13:11:58 +0530

    feat: Select dropdown arrow (#8549)
    * feat: select arrow
    
    * fix: muted text and change px to em
    
    * fix: Inject select icon
    
    * fix: use wrapper instead of input_wrapper

--- frappe/public/js/frappe/form/controls/select.js
+++ frappe/public/js/frappe/form/controls/select.js
@@ -1,7 +1,10 @@
 frappe.ui.form.ControlSelect = frappe.ui.form.ControlData.extend({
-	html_element: "select",
+	html_element: 'select',
 	make_input: function() {
 		this._super();
+		this.$wrapper.find('.control-input')
+			.addClass('flex align-center')
+			.append('<i class="octicon octicon-chevron-down text-muted"></i>');
 		this.set_options();
 	},
 	set_formatted_input: function(value) {
