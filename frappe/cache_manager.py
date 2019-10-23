59ddc94ed47cb6648593b012d716547aa4dfcff9
Aditya Hase <aditya@adityahase.com>
2019-10-23 14:01:10 +0530

    fix(notifications): Add notification_config to global_cache_keys
    Co-authored-by: prssanna <prssud@gmail.com>

--- frappe/cache_manager.py
+++ frappe/cache_manager.py
@@ -10,7 +10,7 @@ from frappe.desk.notifications import (delete_notification_count_for,
 
 common_default_keys = ["__default", "__global"]
 
-global_cache_keys = ("app_hooks", "installed_apps",
+global_cache_keys = ("app_hooks", "installed_apps", "notification_config",
 		"app_modules", "module_app", "system_settings",
 		'scheduler_events', 'time_zone', 'webhooks', 'active_domains',
 		'active_modules', 'assignment_rule', 'server_script_map')
