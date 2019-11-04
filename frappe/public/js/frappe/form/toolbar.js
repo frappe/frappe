7b05814dc713cd483843d2710bf2de4c3a39629e
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2019-11-04 12:20:33 +0530

    fix: Handle null value

--- frappe/public/js/frappe/form/toolbar.js
+++ frappe/public/js/frappe/form/toolbar.js
@@ -30,7 +30,7 @@ frappe.ui.form.Toolbar = Class.extend({
 	},
 	set_title: function() {
 		if(this.frm.meta.title_field) {
-			let title_field = (this.frm.doc[this.frm.meta.title_field].toString() || "").trim();
+			let title_field = (this.frm.doc[this.frm.meta.title_field] || "").toString().trim();
 			var title = strip_html(title_field || this.frm.docname);
 			if(this.frm.doc.__islocal || title === this.frm.docname || this.frm.meta.autoname==="hash") {
 				this.page.set_title_sub("");
