919ed733966b40622ca9fb64dd62d6beaac1b7fb
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-11-10 10:22:16 +0530

    fix: add index on child table

--- frappe/desk/doctype/global_search_doctype/global_search_doctype.py
+++ frappe/desk/doctype/global_search_doctype/global_search_doctype.py
@@ -3,8 +3,10 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class GlobalSearchDocType(Document):
-	pass
+
+	def on_doctype_update():
+		frappe.db.add_index("Global Search DocType", ["document_type"])
