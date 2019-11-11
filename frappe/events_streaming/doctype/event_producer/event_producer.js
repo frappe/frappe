6ad08fa1445230bb3125c81735e7f0c0b4fd9680
Rucha Mahabal <ruchamahabal2@gmail.com>
2019-11-11 20:21:47 +0530

    feat: added indicators for approval status

--- frappe/events_streaming/doctype/event_producer/event_producer.js
+++ frappe/events_streaming/doctype/event_producer/event_producer.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Event Producer', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		// formatter for subscribed doctype approval status
+		frm.set_indicator_formatter('status',
+			function(doc) {
+				let indicator = 'orange';
+				if (doc.status == 'Approved') {
+					indicator = 'green';
+				} else if (doc.status == 'Rejected') {
+					indicator = 'red';
+				}
+				return indicator;
+			}
+		);
+	}
 });
