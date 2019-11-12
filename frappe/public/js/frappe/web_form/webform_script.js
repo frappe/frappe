b80b4a64dfbdf5f159a3ed85ab7319fc0694f912
Nickesh <nick9822@gmail.com>
2019-11-12 19:08:16 +0530

    Web form allow edit and new fix

--- frappe/public/js/frappe/web_form/webform_script.js
+++ frappe/public/js/frappe/web_form/webform_script.js
@@ -52,9 +52,10 @@ frappe.ready(function() {
 			const data = setup_fields(r.message);
 			let web_form_doc = data.web_form;
 
-			if (web_form_doc.doc_name && web_form_doc.allow_edit === 0) {
-				window.location.replace(window.location.pathname + "?new=1");
-				return;
+			if (web_form_doc.name && web_form_doc.allow_edit === 0) {
+				if(!window.location.href.includes("?new=1")){
+					window.location.replace(window.location.pathname + "?new=1");
+				}
 			}
 			let doc = r.message.doc || build_doc(r.message);
 			web_form.prepare(web_form_doc, doc);
