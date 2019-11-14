9d5baca7de690239b893edc044bc4d83dbf65faa
Rohan Bansal <rohan@digithinkit.com>
2019-11-14 14:54:59 +0530

    fix: change tags on filter change

--- frappe/public/js/frappe/list/list_sidebar.js
+++ frappe/public/js/frappe/list/list_sidebar.js
@@ -285,7 +285,8 @@ frappe.views.ListSidebar = class ListSidebar {
 			args: {
 				stats: me.stats,
 				doctype: me.doctype,
-				filters: me.default_filters || []
+				// wait for list filter area to be generated before getting filters, or fallback to default filters
+				filters: (me.list_view.filter_area ? me.list_filter.get_current_filters() : me.default_filters) || []
 			},
 			callback: function(r) {
 				me.render_stat("_user_tags", (r.message.stats || {})["_user_tags"]);
@@ -386,6 +387,7 @@ frappe.views.ListSidebar = class ListSidebar {
 
 	reload_stats() {
 		this.sidebar.find(".sidebar-stat").remove();
+		this.sidebar.find(".list-tag-preview").remove();
 		this.get_stats();
 	}
 
