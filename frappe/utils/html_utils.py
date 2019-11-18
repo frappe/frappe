5d02a080b9d5a1cb44e8a44f930ac2cf28ff21f3
Rucha Mahabal <ruchamahabal2@gmail.com>
2019-11-18 11:20:34 +0530

    fix(Blog Post): enabled ignore XSS filter for Content(HTML) field

--- frappe/utils/html_utils.py
+++ frappe/utils/html_utils.py
@@ -117,7 +117,7 @@ acceptable_elements = [
 	'p', 'pre', 'progress', 'q', 's', 'samp', 'section', 'select',
 	'small', 'sound', 'source', 'spacer', 'span', 'strike', 'strong',
 	'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'time', 'tfoot',
-	'th', 'thead', 'tr', 'tt', 'u', 'ul', 'var', 'video', 'iframe'
+	'th', 'thead', 'tr', 'tt', 'u', 'ul', 'var', 'video'
 ]
 
 mathml_elements = [
