3a73cf6e7d2ea8997a839b36c0c6d20ed110c741
Nabin Hait <nabinhait@gmail.com>
2019-11-22 15:13:19 +0530

    feat: Handle server and client side primary action in msgprint (#8870)
    * feat: Handle server and client side primary action in msgprint
    
    - Added provision to pass primary action from server side msgprint
    - msgprint on client side handles server side and client side methods as primary actions
    
    * chore: Code cleanup
    
    * fix: Codacy
    
    * fix: patch

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -290,7 +290,7 @@ def log(msg):
 
 	debug_log.append(as_unicode(msg))
 
-def msgprint(msg, title=None, raise_exception=0, as_table=False, indicator=None, alert=False):
+def msgprint(msg, title=None, raise_exception=0, as_table=False, indicator=None, alert=False, primary_action=None):
 	"""Print a message to the user (via HTTP response).
 	Messages are sent in the `__server_messages` property in the
 	response JSON and shown in a pop-up / modal.
@@ -299,6 +299,7 @@ def msgprint(msg, title=None, raise_exception=0, as_table=False, indicator=None,
 	:param title: [optional] Message title.
 	:param raise_exception: [optional] Raise given exception and show message.
 	:param as_table: [optional] If `msg` is a list of lists, render as HTML table.
+	:param primary_action: [optional] Bind a primary server/client side action.
 	"""
 	from frappe.utils import encode
 
@@ -337,6 +338,9 @@ def msgprint(msg, title=None, raise_exception=0, as_table=False, indicator=None,
 
 	if alert:
 		out.alert = 1
+
+	if primary_action:
+		out.primary_action = primary_action
 
 	message_log.append(json.dumps(out))
 
