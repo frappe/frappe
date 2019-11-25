1755e1b0ff9642b0d843d49d6e21159206c2876b
Rucha Mahabal <ruchamahabal2@gmail.com>
2019-11-25 17:02:18 +0530

    feat: fetch mandatory fields from ref_doctype for slide fields child table

--- frappe/desk/doctype/setup_wizard_slide/setup_wizard_slide.js
+++ frappe/desk/doctype/setup_wizard_slide/setup_wizard_slide.js
@@ -2,7 +2,39 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Setup Wizard Slide', {
-	// refresh: {
+	ref_doctype: function(frm) {
+		frm.set_query('ref_doctype', function() {
+			if (frm.doc.slide_type === 'Create') {
+				return {
+					filters: {
+						'issingle': 0,
+						'istable': 0
+					}
+				};
+			} else if (frm.doc.slide_type === 'Settings') {
+				return {
+					filters: {
+						'issingle': 1,
+						'istable': 0
+					}
+				};
+			}
+		});
 
-	// }
+		//fetch mandatory fields automatically
+		if (frm.doc.ref_doctype) {
+			frappe.model.clear_table(frm.doc, 'slide_fields');
+			const fields = frappe.meta.get_docfields(frm.doc.ref_doctype, null, {
+				reqd: 1
+			})
+			$.each(fields, function(_i, data) {
+				let row = frappe.model.add_child(frm.doc, 'Setup Wizard Slide', 'slide_fields');
+				row.label = data.label;
+				row.fieldtype = data.fieldtype;
+				row.fieldname = data.fieldname;
+				row.options = data.options;
+			});
+			refresh_field('slide_fields');
+		}
+	}
 });
