7f82ee873ef5c4e62c033a2b37d0bef99dc47f04
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-11-27 14:08:45 +0530

    fix: dont add index for global search doctype

--- frappe/desk/doctype/global_search_doctype/global_search_doctype.py
+++ frappe/desk/doctype/global_search_doctype/global_search_doctype.py
@@ -3,10 +3,8 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+# import frappe
 from frappe.model.document import Document
 
 class GlobalSearchDocType(Document):
-
-	def on_doctype_update():
-		frappe.db.add_index("Global Search DocType", ["document_type"])
+	pass
