8f57cae0b34f8f2571caafc64da208be5d3c50a5
Suraj Shetty <surajshetty3416@gmail.com>
2019-12-01 11:41:11 +0530

    test: Fix listview test by waiting for clear-cache

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -162,9 +162,15 @@ Cypress.Commands.add('go_to_list', (doctype) => {
 });
 
 Cypress.Commands.add('clear_cache', () => {
+	cy.server();
+	cy.route({
+		method: 'POST',
+		url: 'frappe.sessions.clear'
+	}).as('clear-cache');
 	cy.window().its('frappe').then(frappe => {
 		frappe.ui.toolbar.clear_cache();
 	});
+	cy.wait(['@clear-cache']);
 });
 
 Cypress.Commands.add('dialog', (opts) => {
