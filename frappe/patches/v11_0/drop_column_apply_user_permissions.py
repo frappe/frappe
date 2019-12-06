54e55380657a97540fcdd12041d6621881fc87e0
Rohit Waghchaure <rohitw1991@gmail.com>
2019-12-06 15:18:54 +0530

    fix: patch

--- frappe/patches/v11_0/drop_column_apply_user_permissions.py
+++ frappe/patches/v11_0/drop_column_apply_user_permissions.py
@@ -10,6 +10,7 @@ def execute():
 			if column in frappe.db.get_table_columns(doctype):
 				frappe.db.sql("alter table `tab{0}` drop column {1}".format(doctype, column))
 
+	frappe.reload_doc('core', 'doctype', 'doctype_link', force=True)
 	frappe.reload_doc('core', 'doctype', 'docperm', force=True)
 	frappe.reload_doc('core', 'doctype', 'custom_docperm', force=True)
 
