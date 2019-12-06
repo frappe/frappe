e07f5ffafe32d98ce14bf4b461e1144896961411
Rucha Mahabal <ruchamahabal2@gmail.com>
2019-12-06 12:29:09 +0530

    fix: spacing
    Co-Authored-By: Shivam Mishra <scmmishra@users.noreply.github.com>

--- frappe/model/sync.py
+++ frappe/model/sync.py
@@ -73,7 +73,7 @@ def get_doc_files(files, start_path, force=0, sync_everything = False, verbose=F
 	# load in sequence - warning for devs
 	document_types = ['doctype', 'page', 'report', 'dashboard_chart_source', 'print_format',
 		'website_theme', 'web_form', 'notification', 'print_style',
-		 'data_migration_mapping', 'data_migration_plan', 'onboarding_slide']
+		'data_migration_mapping', 'data_migration_plan', 'onboarding_slide']
 	for doctype in document_types:
 		doctype_path = os.path.join(start_path, doctype)
 		if os.path.exists(doctype_path):
