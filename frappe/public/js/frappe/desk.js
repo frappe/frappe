f149a789f4bd7c08d752621ba5d2699b61579715
Shivam Mishra <scm.mymail@gmail.com>
2019-12-09 10:29:45 +0530

    style: added semi-colon

--- frappe/public/js/frappe/desk.js
+++ frappe/public/js/frappe/desk.js
@@ -481,10 +481,10 @@ frappe.Application = Class.extend({
 		// Iterate over changelog
 		change_log.forEach(log => {
 			log.change_log.forEach(version_info => {
-				version_info[1] = version_info[1].replace(/#/, '##')
+				version_info[1] = version_info[1].replace(/#/, '##');
 				// replace all # with ## for rendering them as <h2>
-			})
-		})
+			});
+		});
 
 		var change_log_dialog = frappe.msgprint({
 			message: frappe.render_template("change_log", {"change_log": change_log}),
