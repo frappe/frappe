81c3fe147d6d44dbd7f00c6b7a15a629b8d8c2ae
vishal <vishaldhayagude09@gmail.com>
2019-12-09 13:42:19 +0530

    fix: Distinct check added for possible links

--- frappe/desk/form/linked_with.py
+++ frappe/desk/form/linked_with.py
@@ -291,8 +291,9 @@ def get_dynamic_linked_fields(doctype, without_ignore_user_permissions_enabled=F
 	for df in links:
 		if is_single(df.doctype): continue
 
-		# removed doctype_fieldname form fields it only check with child with parenttype
+		# optimized to get both link exists and parenttype
 		possible_link = frappe.get_all(df.doctype, filters={df.doctype_fieldname: doctype}, fields=['parenttype'], distinct=True)
+
 		if not possible_link: continue
 
 		for d in possible_link:
