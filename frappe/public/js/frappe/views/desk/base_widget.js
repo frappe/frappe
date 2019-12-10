b140fb33d514fd01865fc6c883c65941ac11ce5a
Shivam Mishra <scm.mymail@gmail.com>
2019-12-10 15:30:41 +0530

    feat: added link widget

--- frappe/public/js/frappe/views/desk/base_widget.js
+++ frappe/public/js/frappe/views/desk/base_widget.js
@@ -1,7 +1,24 @@
 export default class Widget {
 	constructor(opts) {
 		Object.assign(this, opts);
+		this.make();
+	}
+
+	refresh() {
+		//
+	}
+
+	make() {
 		this.make_widget();
+		this.widget.appendTo(this.container);
 		this.setup_events();
+	}
+
+	make_widget() {
+		//
+	}
+
+	setup_events() {
+		//
 	}
 }
--- frappe/public/js/frappe/views/desk/widget.js
+++ frappe/public/js/frappe/views/desk/widget.js
@@ -1,9 +1,10 @@
 import ModuleWidget from './module_widget.js'
+import LinkWidget from './link_widget.js'
 
 
 const widget_factory = {
 	'module': ModuleWidget,
-	'link': ModuleWidget
+	'link': LinkWidget
 }
 
 export function get_widget_class(widget_type) {
