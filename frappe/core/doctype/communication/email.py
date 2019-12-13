d03abbb7a414efaf4d1438be72bd69dc908cfed5
Chinmay D. Pai <chinmaydpai@gmail.com>
2019-12-13 15:28:55 +0530

    fix: use filter for removing administrator from email list
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>
    Co-authored-by: Saurabh Palande <saurabh6790@gmail.com>
    (cherry picked from commit 7347764c87c757247acbfa6b1003a175f9fdb854)

--- frappe/core/doctype/communication/email.py
+++ frappe/core/doctype/communication/email.py
@@ -239,8 +239,9 @@ def get_recipients_cc_and_bcc(doc, recipients, cc, bcc, fetched_from_email_accou
 	return recipients, cc, bcc
 
 def remove_administrator_from_email_list(email_list):
-	if 'Administrator' in email_list:
-		email_list.remove('Administrator')
+	administrator_email = list(filter(lambda emails: "Administrator" in emails, email_list))
+	if administrator_email:
+		email_list.remove(administrator_email[0])
 
 def prepare_to_notify(doc, print_html=None, print_format=None, attachments=None):
 	"""Prepare to make multipart MIME Email
