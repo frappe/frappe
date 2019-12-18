8d99e2303ca9198eb73bf50e4697ca419a076883
prssanna <prssud@gmail.com>
2019-12-18 13:29:41 +0530

    fix: fix tag dropdown search

--- frappe/public/js/frappe/list/list_sidebar.js
+++ frappe/public/js/frappe/list/list_sidebar.js
@@ -253,13 +253,15 @@ frappe.views.ListSidebar = class ListSidebar {
 			let text_filter = $search_input.val().toLowerCase();
 			// Replace trailing and leading spaces
 			text_filter = text_filter.replace(/^\s+|\s+$/g, '');
-			let text;
 			for (var i = 0; i < $elements.length; i++) {
 				let text_element = $elements.eq(i).find(text_class);
 
 				let text = text_element.text().toLowerCase();
 				// Search data-name since label for current user is 'Me'
-				let name = text_element.data('name').toLowerCase();
+				let name = '';
+				if (text_element.data('name')) {
+					name = text_element.data('name').toLowerCase();
+				}
 				if (text.includes(text_filter) || name.includes(text_filter)) {
 					$elements.eq(i).css('display','');
 				} else {
