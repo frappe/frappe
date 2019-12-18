e3dfbf4562bd31e6431d08c8900c197d5537997c
Suraj Shetty <surajshetty3416@gmail.com>
2019-12-18 09:16:35 +0530

    fix: Get guest's config if session.user is None

--- frappe/desk/notifications.py
+++ frappe/desk/notifications.py
@@ -182,8 +182,8 @@ def get_notification_info():
 	return out
 
 def get_notification_config():
-	if not frappe.session.user:
-		return frappe._dict()
+	user = frappe.session.user or 'Guest'
+
 	def _get():
 		subscribed_documents = get_subscribed_documents()
 		config = frappe._dict()
@@ -207,7 +207,7 @@ def get_notification_config():
 						config[key].update(nc.get(key, {}))
 		return config
 
-	return frappe.cache().hget("notification_config", frappe.session.user, _get)
+	return frappe.cache().hget("notification_config", user, _get)
 
 def get_filters_for(doctype):
 	'''get open filters for doctype'''
