a71734f87c440a93feae0000e0a1dcf218103930
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-12-19 11:48:18 +0530

    fix: do not hide sidebar

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -337,7 +337,7 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 					.then(() => this.filter_area.set(this.filters));
 			}
 		}
-		this.show_or_hide_sidebar();
+
 		return Promise.resolve();
 	}
 
