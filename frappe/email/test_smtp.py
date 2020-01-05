37943b5b044a8d6f7dd8f763425643ed7dbd9cb3
Mark Walker <mark@argotrans.com>
2020-01-04 18:48:28 -0600

    removing content

--- frappe/email/test_smtp.py
+++ frappe/email/test_smtp.py
@@ -13,6 +13,5 @@ class TestEmailAccount(unittest.TestCase):
     pass
 
   def test_default_email_account(self):
-    # test to make sure with no append_to that the default email account will be passed, this will
-    # work irrespective of whether or not a default account is actually provided.
+
     self.assetEqual(get_outgoing_email_account(), get_default_outgoing_email_account())
