43ccae0327cf8d9b81ee527778a5d86793592722
Rohan Bansal <rohan@digithinkit.com>
2020-01-13 15:35:15 +0530

    fix: hash secret before assigning to header

--- frappe/integrations/doctype/webhook/webhook.py
+++ frappe/integrations/doctype/webhook/webhook.py
@@ -4,7 +4,10 @@
 
 from __future__ import unicode_literals
 
+import base64
 import datetime
+import hashlib
+import hmac
 import json
 from time import sleep
 
@@ -16,7 +19,7 @@ from frappe import _
 from frappe.model.document import Document
 from frappe.utils.jinja import validate_template
 
-WEBHOOK_SECRET_HEADER = "X-Frappe-Webhook-Key"
+WEBHOOK_SECRET_HEADER = "X-Frappe-Webhook-Signature"
 
 
 class Webhook(Document):
@@ -98,7 +101,15 @@ def get_webhook_headers(doc, webhook):
 	headers = {}
 
 	if webhook.enable_security:
-		headers[WEBHOOK_SECRET_HEADER] = webhook.get_password("webhook_secret")
+		data = get_webhook_data(doc, webhook)
+		signature = base64.b64encode(
+			hmac.new(
+				webhook.get_password("webhook_secret").encode("utf8"),
+				json.dumps(data).encode("utf8"),
+				hashlib.sha256
+			).digest()
+		)
+		headers[WEBHOOK_SECRET_HEADER] = signature
 
 	if webhook.webhook_headers:
 		for h in webhook.webhook_headers:
