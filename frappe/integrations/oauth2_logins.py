524e600973078f6649859cf359d9e31ebaccf60d
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2020-01-14 12:23:28 +0530

    style: Remove trailing whitespace

--- frappe/integrations/oauth2_logins.py
+++ frappe/integrations/oauth2_logins.py
@@ -30,10 +30,10 @@ def login_via_office365(code, state):
 @frappe.whitelist(allow_guest=True)
 def login_via_salesforce(code, state):
 	login_via_oauth2("salesforce", code, state, decoder=decoder_compat)
-	
+
 @frappe.whitelist(allow_guest=True)
 def login_via_fairlogin(code, state):
-	login_via_oauth2("fairlogin", code, state, decoder=decoder_compat)	
+	login_via_oauth2("fairlogin", code, state, decoder=decoder_compat)
 
 @frappe.whitelist(allow_guest=True)
 def custom(code, state):
