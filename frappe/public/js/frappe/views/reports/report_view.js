3b24fe5c45946d5c245d47e363b6f5dff2e85c36
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-01-16 22:14:56 +0530

    fix: add status dependency column

--- frappe/public/js/frappe/views/reports/report_view.js
+++ frappe/public/js/frappe/views/reports/report_view.js
@@ -741,6 +741,16 @@ frappe.views.ReportView = class ReportView extends frappe.views.ListView {
 		}
 	}
 
+	add_status_dependency_column(col, doctype) {
+		// Adds dependent column from which status is derived if required
+		if (!this.fields.find(f => f[0] === col)) {
+			const field = [col, doctype];
+			this.fields.push(field);
+			this.refresh();
+			frappe.show_alert(__('Also adding the status dependency field {0}', [field[0].bold()]));
+		}
+	}
+
 	remove_column_from_datatable(column) {
 		const index = this.fields.findIndex(f => column.field === f[0]);
 		if (index === -1) return;
@@ -1027,11 +1037,17 @@ frappe.views.ReportView = class ReportView extends frappe.views.ListView {
 			}
 			if (col.field === 'docstatus' && !frappe.meta.has_field(this.doctype, 'status')) {
 				// get status from docstatus
-				let status = frappe.get_indicator(d, this.doctype)[0];
+				let status = frappe.get_indicator(d, this.doctype);
+				if (!status[0]) {
+					// get_indicator returns the dependent field's condition as the 3rd parameter
+					let dependent_col = status[2].split(',')[0];
+					// add status dependency column
+					this.add_status_dependency_column(dependent_col, this.doctype);
+				}
 				return {
 					name: d.name,
 					doctype: col.docfield.parent,
-					content: status,
+					content: status[0],
 					editable: false
 				};
 			} else if (col.field in d) {
