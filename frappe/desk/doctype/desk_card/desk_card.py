76829807638c834d4d0750b7a3ba3e9c177dc2aa
Shivam Mishra <scm.mymail@gmail.com>
2020-01-29 17:42:44 +0530

    feat: updated desk page schema

--- frappe/desk/doctype/desk_card/desk_card.py
+++ frappe/desk/doctype/desk_card/desk_card.py
@@ -3,9 +3,8 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-from frappe.modules.export_file import export_to_files
+# import frappe
 from frappe.model.document import Document
 
-class DeskPage(Document):
-	def on_update(self):
-		export_to_files(record_list=[['Desk Page', self.name]], record_module=self.module)
+class DeskCard(Document):
+	pass
