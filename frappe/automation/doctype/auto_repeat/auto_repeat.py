3f0c8ca9583907e183ba035317946a45bd066676
Rucha Mahabal <ruchamahabal2@gmail.com>
2020-02-03 16:12:59 +0530

    fix: Translatable string for error

--- frappe/automation/doctype/auto_repeat/auto_repeat.py
+++ frappe/automation/doctype/auto_repeat/auto_repeat.py
@@ -220,7 +220,13 @@ class AutoRepeat(Document):
 				file_name=new_doc.name, print_format=print_format)]
 
 		except frappe.PermissionError:
-			error_string = _("A recurring {0} {1} has been created for you via Auto Repeat {2}. <br><br><b>Note</b>: Failed to attach new recurring document. To enable attaching document in the auto repeat notification email, enable <b>Allow Print for Draft</b> in Print Settings").format(new_doc.doctype, new_doc.name, self.name)
+			error_string = _("A recurring {0} {1} has been created for you via Auto Repeat {2}.").format(new_doc.doctype, new_doc.name, self.name)
+			error_string += "<br><br>"
+
+			error_string += _("{0}: Failed to attach new recurring document. To enable attaching document in the auto repeat notification email, enable {1} in Print Settings").format(
+				frappe.bold(_('Note')),
+				frappe.bold(_('Allow Print for Draft'))
+			)
 			attachments = '[]'
 
 		if error_string:
