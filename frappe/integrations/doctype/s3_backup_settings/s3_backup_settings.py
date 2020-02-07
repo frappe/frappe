924b535ddac77dc220e61443e35745fc78f67e26
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-02-07 10:34:45 +0530

    chore: remove default sender and notify_email check
    * default sender already gets set in queue.py, so there's no need for a
      check inside __init__.py
    
    * notify_email seems to be a mandatory field, so there's no need to
      check if the field has a value (assuming that it always will).
    
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- frappe/integrations/doctype/s3_backup_settings/s3_backup_settings.py
+++ frappe/integrations/doctype/s3_backup_settings/s3_backup_settings.py
@@ -105,11 +105,9 @@ def send_email(success, service_name, error_status=None):
 
 	if not frappe.db:
 		frappe.connect()
-		
-	notify_email = frappe.db.get_value("S3 Backup Settings", None, "notify_email")
-	if notify_email:
-		recipients = split_emails(notify_email)
-		frappe.sendmail(recipients=recipients, subject=subject, message=message)
+
+	recipients = split_emails(frappe.db.get_value("S3 Backup Settings", None, "notify_email"))
+	frappe.sendmail(recipients=recipients, subject=subject, message=message)
 
 
 def backup_to_s3():
