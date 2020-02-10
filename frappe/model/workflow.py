d290c645ef924608b172f717afe688399248b2f4
marination <maricadsouza221197@gmail.com>
2020-02-10 12:55:39 +0530

    fix: Commit succesfull transactions and rollback the rest

--- frappe/model/workflow.py
+++ frappe/model/workflow.py
@@ -187,6 +187,7 @@ def bulk_workflow_approval(docnames, doctype, action):
 		try:
 			show_progress(docnames, _('Applying: {0}').format(action), i, docname)
 			apply_workflow(frappe.get_doc(doctype, docname), action)
+			frappe.db.commit()
 		except frappe.ValidationError:
 			frappe.log_error(frappe.get_traceback(), "Workflow {0} threw an error for {1} {2}".format(action, doctype, docname))
 			frappe.db.rollback()
