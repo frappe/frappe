7270c743a5cb893f0bdea6ec0b9466901a6ef5f2
Himanshu Warekar <himanshuwarekar@yahoo.com>
2020-02-10 23:56:11 +0530

    fix: ui test fixes

--- cypress/integration/awesome_bar.js
+++ cypress/integration/awesome_bar.js
@@ -25,9 +25,8 @@ context('Awesome Bar', () => {
 
 		cy.get('h1').should('contain', 'To Do');
 
-		cy.get('.toggle-filter')
-			.should('have.length', 1)
-			.should('contain', 'ID like %test%');
+		cy.get('.frappe-control[data-fieldname=description] input')
+			.should('contain', '%test%');
 	});
 
 	it('navigates to new form', () => {
