097be530c36514bf0085eaa5d509f28dd3153c48
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-02-17 14:14:29 +0530

    chore: cleanup code and get from dict
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- frappe/integrations/doctype/slack_webhook_url/slack_webhook_url.py
+++ frappe/integrations/doctype/slack_webhook_url/slack_webhook_url.py
@@ -10,8 +10,19 @@ from frappe import _
 import requests
 import json
 
+
+error_messages = {
+	400: "400: Invalid Payload or User not found",
+	403: "403: Action Prohibited",
+	404: "404: Channel not found",
+	410: "410: The Channel is Archived",
+	500: "500: Rollup Error, Slack seems to be down"
+}
+
+
 class SlackWebhookURL(Document):
 	pass
+
 
 def send_slack_message(webhook_url, message, reference_doctype, reference_name):
 	slack_url = frappe.db.get_value("Slack Webhook URL", webhook_url, "webhook_url")
@@ -21,10 +32,10 @@ def send_slack_message(webhook_url, message, reference_doctype, reference_name):
 			"fallback": _("See the document at {0}").format(doc_url),
 			"actions": [
 				{
-				"type": "button",
-				"text": _("Go to the document"),
-				"url": doc_url,
-				"style": "primary"
+					"type": "button",
+					"text": _("Go to the document"),
+					"url": doc_url,
+					"style": "primary"
 				}
 			]
 		}
@@ -32,24 +43,9 @@ def send_slack_message(webhook_url, message, reference_doctype, reference_name):
 	data = {"text": message, "attachments": attachments}
 	r = requests.post(slack_url, data=json.dumps(data))
 
-
-	if r.ok == True:
-		return 'success'
-
-	elif r.ok == False:
-		status = r.status_code
-		if status == 400:
-			message = _("400 - Invalid payload or user not found")
-		elif status == 403:
-			message = _("403 - Action Prohibited")
-		elif status == 404:
-			message = _("404 - Channel not found")
-		elif status == 410:
-			message = _("410 - The Channel is Archived")
-		elif status == 500:
-			message = _("500 - Rollup error, slack seems down")
-		else:
-			message = r.status_code
-
+	if not r.ok:
+		message = error_messages.get(r.status_code, r.status_code)
 		frappe.log_error(message, _('Slack Webhook Error'))
 		return 'error'
+
+	return 'success'
