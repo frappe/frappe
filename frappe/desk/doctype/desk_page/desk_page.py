22f9425932c20b5b96602ffe16839149688f0389
Shivam Mishra <scm.mymail@gmail.com>
2020-03-02 18:49:13 +0530

    feat: only allow developers to edit

--- frappe/desk/doctype/desk_page/desk_page.py
+++ frappe/desk/doctype/desk_page/desk_page.py
@@ -3,9 +3,16 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import frappe
+from frappe import _
 from frappe.modules.export_file import export_to_files
 from frappe.model.document import Document
 
 class DeskPage(Document):
+	def validate(self):
+		if (not (frappe.flags.in_install or frappe.flags.in_patch or frappe.flags.in_test or frappe.flags.in_fixtures)
+			and not frappe.conf.developer_mode):
+			frappe.throw(_("You need to be in developer mode to edit this document"))
+
 	def on_update(self):
 		export_to_files(record_list=[['Desk Page', self.name]], record_module=self.module)
