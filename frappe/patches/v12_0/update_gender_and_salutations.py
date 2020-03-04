30ac74c0af973d441f3e751ec509b5e0389bb79c
PriyankaGangar <pinka0925@gmail.com>
2020-03-04 14:42:07 +0530

    Simplified Patches

--- frappe/patches/v12_0/update_gender_and_salutations.py
+++ frappe/patches/v12_0/update_gender_and_salutations.py
@@ -2,14 +2,7 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.desk.page.setup_wizard.install_fixtures import update_genders, update_salutations
+from frappe.desk.page.setup_wizard.install_fixtures import update_genders
 
 def execute():
-	frappe.db.set_value("DocType", "Contact", "module", "Contacts")
-	frappe.db.set_value("DocType", "Address", "module", "Contacts")
-	frappe.db.set_value("DocType", "Address Template", "module", "Contacts")
-	frappe.reload_doc('contacts', 'doctype', 'gender')
-	frappe.reload_doc('contacts', 'doctype', 'salutation')
-
-	update_genders() 
-	update_salutations()
+	update_genders()
