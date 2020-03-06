4441e765c98b73ca321f0e126025db4b8a111cde
Shivam Mishra <scm.mymail@gmail.com>
2020-03-06 12:47:49 +0530

    feat: add is standard option

--- frappe/desk/doctype/desk_page/desk_page.js
+++ frappe/desk/doctype/desk_page/desk_page.js
@@ -2,7 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Desk Page', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		frm.get_field("is_standard").toggle(frappe.boot.developer_mode);
 
-	// }
+		if (frm.doc.is_standard && !frappe.boot.developer_mode) {
+			frm.set_read_only();
+			frm.disable_save();
+		}
+	}
 });
