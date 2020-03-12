a8792726b4b8e9e1c3c9807a3ebc0670f29f3247
Suraj Shetty <surajshetty3416@gmail.com>
2020-03-12 09:02:01 +0530

    fix: Fix clear cache code and remove unwanted code

--- frappe/core/doctype/translation/translation.py
+++ frappe/core/doctype/translation/translation.py
@@ -5,7 +5,6 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from frappe.translate import clear_cache
 from frappe.utils import strip_html_tags, is_html
 from frappe.integrations.utils import make_post_request
 import json
@@ -19,46 +18,10 @@ class Translation(Document):
 		self.source_name = strip_html_tags(self.source_name).strip()
 
 	def on_update(self):
-		clear_cache()
+		clear_user_translation_cache(self.language)
 
 	def on_trash(self):
-		clear_cache()
-
-	def onload(self):
-		if self.contributed_translation_doctype_name:
-			data = {"data": json.dumps({
-				"doc_name": self.contributed_translation_doctype_name
-			})}
-			try:
-				response = make_post_request(url=frappe.get_hooks("translation_contribution_status")[0], data=data)
-			except Exception:
-				frappe.msgprint("Something went wrong. Please check error log for more details")
-			if response.get("message").get("message") == "Contributed Translation has been deleted":
-				self.status = "Deleted"
-				self.contributed_translation_doctype_name = ""
-				self.save()
-			else:
-				self.status = response.get("message").get("status")
-				self.save()
-
-@frappe.whitelist()
-def contribute_translation(language, contributor, source_name, target_name, doc_name):
-	data = {"data": json.dumps({
-		"language": language,
-		"contributor": contributor,
-		"source_name": source_name,
-		"target_name": target_name,
-		"posting_date": frappe.utils.nowdate()
-	})}
-	try:
-		response = make_post_request(url=frappe.get_hooks("translation_contribution_url")[0], data=data)
-	except Exception:
-		frappe.msgprint("Something went wrong while contributing translation. Please check error log for more details")
-	if response.get("message").get("message") == "Already exists":
-		frappe.msgprint("Translation already exists")
-	elif response.get("message").get("message") == "Added to contribution list":
-		frappe.set_value("Translation", doc_name, "contributed_translation_doctype_name", response.get("message").get("doc_name"))
-		frappe.msgprint("Translation successfully contributed")
+		clear_user_translation_cache(self.language)
 
 
 @frappe.whitelist()
@@ -96,3 +59,6 @@ def create_translations(translation_map, language):
 
 	translator = FrappeClient(frappe.conf.translator_url)
 	return translator.post_api('translator.api.add_translations', params=params)
+
+def clear_user_translation_cache(lang):
+	frappe.cache().hdel('lang_user_translations', lang)
