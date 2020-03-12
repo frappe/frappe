85c436ac1b0f8ce6c3134f9613510b0b19fcd369
Shivam Mishra <scm.mymail@gmail.com>
2020-03-12 16:27:25 +0530

    feat: updated client script

--- frappe/desk/doctype/desk_page/desk_page.js
+++ frappe/desk/doctype/desk_page/desk_page.js
@@ -4,9 +4,14 @@
 frappe.ui.form.on('Desk Page', {
 	refresh: function(frm) {
 		frm.get_field("is_standard").toggle(frappe.boot.developer_mode);
-
-		if (frm.doc.is_standard && !frappe.boot.developer_mode) {
+		frm.get_field("extends_another_page").toggle(frappe.boot.developer_mode);
+		if (!frappe.boot.developer_mode) {
 			frm.set_read_only();
+			frm.fields
+				.filter(field => field.has_input)
+				.forEach(field => {
+					frm.set_df_property(field.df.fieldname, "read_only", "1")
+				})
 			frm.disable_save();
 		}
 	}
