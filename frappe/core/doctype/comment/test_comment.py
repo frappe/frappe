e3332e4acf7048d219c6cfb44576695373774f43
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-03-17 17:32:59 +0530

    chore: change test to check for raised exception
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- frappe/core/doctype/comment/test_comment.py
+++ frappe/core/doctype/comment/test_comment.py
@@ -45,15 +45,7 @@ class TestComment(unittest.TestCase):
 
 		frappe.db.sql("delete from `tabComment` where reference_doctype = 'Blog Post'")
 
-		add_comment('pleez vizits my site http://mysite.com', 'test@test.com', 'bad commentor',
-			'Blog Post', test_blog.name, test_blog.route)
-
-		self.assertEqual(len(frappe.get_all('Comment', fields = ['*'], filters = dict(
-			reference_doctype = test_blog.doctype,
-			reference_name = test_blog.name
-		))), 0)
+		self.assertRaises(frappe.exceptions.ValidationError, add_comment('pleez vizits my site http://mysite.com', 'test@test.com', 'bad commentor',
+			'Blog Post', test_blog.name, test_blog.route))
 
 		test_blog.delete()
-
-
-
