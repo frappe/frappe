d61a2ad429d1071df1af62123a9688b621a12705
Mangesh-Khairnar <mkhairnar10@gmail.com>
2020-03-20 22:32:39 +0530

    fix: check if website is published

--- frappe/website/doctype/website_settings/google_indexing.py
+++ frappe/website/doctype/website_settings/google_indexing.py
@@ -19,9 +19,7 @@ SCOPES = "https://www.googleapis.com/auth/indexing"
 
 @frappe.whitelist()
 def authorize_access(reauthorize=None):
-	"""
-		If no Authorization code get it from Google and then request for Refresh Token.
-	"""
+	""" If no Authorization code get it from Google and then request for Refresh Token. """
 
 	google_settings = frappe.get_doc("Google Settings")
 	website_settings = frappe.get_doc("Website Settings")
@@ -52,8 +50,9 @@ def authorize_access(reauthorize=None):
 		except Exception as e:
 			frappe.throw(e)
 
+
 def get_authentication_url(client_id, redirect_uri):
-	''' Return authentication url with the client id and redirect uri '''
+	""" Return authentication url with the client id and redirect uri. """
 	return {
 		"url": "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&response_type=code&prompt=consent&client_id={}&include_granted_scopes=true&scope={}&redirect_uri={}".format(client_id, SCOPES, redirect_uri)
 	}
@@ -61,9 +60,7 @@ def get_authentication_url(client_id, redirect_uri):
 
 @frappe.whitelist()
 def google_callback(code=None):
-	"""
-		Authorization code is sent to callback as per the API configuration
-	"""
+	""" Authorization code is sent to callback as per the API configuration. """
 	frappe.db.set_value("Website Settings", None, "indexing_authorization_code", code)
 	frappe.db.commit()
 
@@ -71,9 +68,7 @@ def google_callback(code=None):
 
 
 def get_google_indexing_object():
-	"""
-		Returns an object of Google Indexing object
-	"""
+	""" Returns an object of Google Indexing object. """
 	google_settings = frappe.get_doc("Google Settings")
 	account = frappe.get_doc("Website Settings")
 
@@ -81,8 +76,7 @@ def get_google_indexing_object():
 		"token": account.get_access_token(),
 		"refresh_token": account.get_password(fieldname="indexing_refresh_token", raise_exception=False),
 		"token_uri": get_auth_url(),
-		"client_id": google_settings.client_id,
-		"client_secret": google_settings.get_password(fieldname="client_secret", raise_exception=False),
+		"client_id": google_settings.client_i . "client_secret": google_settings.get_password(fieldname="client_secret", raise_exception=False),
 		"scopes": "https://www.googleapis.com/auth/indexing"
 	}
 
@@ -93,7 +87,7 @@ def get_google_indexing_object():
 
 
 def publish_site(url, operation_type="URL_UPDATED"):
-	''' Send an update/remove url request '''
+	""" Send an update/remove url request. """
 
 	google_indexing = get_google_indexing_object()
 	body = {
--- frappe/website/website_generator.py
+++ frappe/website/website_generator.py
@@ -122,10 +122,10 @@ class WebsiteGenerator(Document):
 		return route
 
 	def send_indexing_request(self, operation_type='URL_UPDATED'):
-		''' Send indexing request on update/trash operation '''
+		''' Send indexing request on update/trash operation. '''
 
-		if frappe.db.get_single_value('Website Settings', 'enable_google_indexing')\
-			and self.meta.is_published_field and self.meta.allow_guest_to_view:
+		if frappe.db.get_single_value('Website Settings', 'enable_google_indexing') \
+			and self.is_website_published() and self.meta.allow_guest_to_view:
 
 			url = frappe.utils.get_url(self.route)
 			frappe.enqueue('frappe.website.doctype.website_settings.google_indexing.publish_site', \
