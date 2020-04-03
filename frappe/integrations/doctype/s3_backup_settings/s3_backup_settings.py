e2014cfe38ca1d9f0888fa492f01b2a36a99db04
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-04-03 11:36:56 +0530

    chore: implement file backup logic for aws s3
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- frappe/integrations/doctype/s3_backup_settings/s3_backup_settings.py
+++ frappe/integrations/doctype/s3_backup_settings/s3_backup_settings.py
@@ -111,11 +111,11 @@ def backup_to_s3():
 		backup = new_backup(ignore_files=False, backup_path_db=None,
 						backup_path_files=None, backup_path_private_files=None, force=True)
 		db_filename = os.path.join(get_backups_path(), os.path.basename(backup.backup_path_db))
+		files_filename = os.path.join(get_backups_path(), os.path.basename(backup.backup_path_files))
+		private_files = os.path.join(get_backups_path(), os.path.basename(backup.backup_path_private_files))
 	else:
-		db_filename = get_latest_backup_file()
+		db_filename, files_filename, private_files = get_latest_backup_file(with_files=True)
 
-	files_filename = os.path.join(get_backups_path(), os.path.basename(backup.backup_path_files))
-	private_files = os.path.join(get_backups_path(), os.path.basename(backup.backup_path_private_files))
 	folder = os.path.basename(db_filename)[:15] + '/'
 	# for adding datetime to folder name
 
