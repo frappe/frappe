2111e563e53f0ef515bd08eb8df5dc6c7d963918
Yaqin Shurrab <yshurrab@gmail.com>
2020-04-11 21:16:14 +0200

    Update __init__.py

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -265,6 +265,16 @@ def cache():
 			or "redis://localhost:11311")
 	return redis_server
 
+
+def cache_apis():
+	 """Returns memcache connection."""
+	 global redis_server
+	 if not redis_server:
+	 from frappe.utils.redis_wrapper import RedisWrapper
+	 redis_server = RedisWrapper.from_url(conf.get('redis_cache_api') or conf.get('redis_cache'))
+	 return redis_server
+
+
 def get_traceback():
 	"""Returns error traceback."""
 	from frappe.utils import get_traceback
