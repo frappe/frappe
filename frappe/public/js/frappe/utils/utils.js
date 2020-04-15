6233e94dab4de5bf73f161aa7565f758662e2100
Shivam Mishra <scm.mymail@gmail.com>
2020-04-16 00:02:27 +0530

    feat: added utility to get browser details

--- frappe/public/js/frappe/utils/utils.js
+++ frappe/public/js/frappe/utils/utils.js
@@ -743,7 +743,25 @@ Object.assign(frappe.utils, {
 		});
 
 		return $el;
-	}
+	},
+
+	get_browser() {
+		var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+		if(/trident/i.test(M[1])){
+			tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
+			return {name:'IE',version:(tem[1]||'')};
+			}
+		if(M[1]==='Chrome'){
+			tem=ua.match(/\bOPR|Edge\/(\d+)/)
+			if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+			}
+		M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+		if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+		return {
+		  name: M[0],
+		  version: M[1]
+		};
+	 }
 });
 
 // Array de duplicate
