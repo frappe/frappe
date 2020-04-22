2425217a4d7d8661918e3602900e17e5b51f4e75
Kenneth Sequeira <seq.kenneth@gmail.com>
2020-04-22 20:08:57 +0530

    updated js function, cleaned preview

--- frappe/website/doctype/blog_post/blog_post.js
+++ frappe/website/doctype/blog_post/blog_post.js
@@ -3,24 +3,47 @@
 
 frappe.ui.form.on('Blog Post', {
 	refresh: function(frm) {
-		frm.call('get_url').then((r)=>{
-			let google_preview = frm.get_field("google_preview")
-			google_preview.html(`
-				<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-				<link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&display=swap" rel="stylesheet">
-				<cite style="color: #006621;
-				font-style: normal;
-				font-size: 14px;
-				display: inline-block;
-				font-family: 'Roboto, sans-serf'; font-size:14px;line-height:1.3; margin: 0 0">${ r.message + '/' + frm.doc.route } </cite>
-				<h3 style="color:#1a0dab; font-family: 'Roboto', sans-serif; font-size:20px; line-height:1.3; hover:underline; margin: 0 0"><a style="color:#1a0dab;">
-				${ frm.doc.title }</a></h3>
-				<span style="color:808080; font-family: 'Roboto, sans-serf'; font-size:14px;line-height:1.3; margin: 0 0""">${ frm.doc.published_on } </span><span style="color: #545454;
-				font-size: small;
-				line-height: 1.4;
-				word-wrap: break-word;
-				font-family: 'Roboto, sans-serf'; font-size:14px;line-height:1.3; margin: 0 0"">${frm.doc.meta_description}
-			`)
-		})
+		generate_google_search_preview(frm)
+	},
+	title: function(frm) {
+		generate_google_search_preview(frm)
+	},
+	meta_description:function(frm){
+		generate_google_search_preview(frm)
+	},
+	description:function(frm){
+		generate_google_search_preview(frm)
 	}
 });
+function generate_google_search_preview(frm){
+	frm.call('get_url').then((r)=>{
+		let seo_title = frm.doc.title
+		seo_title = seo_title.slice(0,60)
+		let seo_description =  frm.doc.meta_description || frm.doc.description
+		seo_description = seo_description.slice(0,160)
+		let google_preview = frm.get_field("google_preview")
+		google_preview.html(`
+			<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&display=swap" rel="stylesheet">			
+			<div style="max-width: 750px;">
+			<div>
+				<cite style="color: #006621;
+					font-style: normal;
+					font-size: 14px;
+					display: inline-block;
+					font-family: 'Open Sans'; font-size:14px;line-height:1.3; margin: 0 0">${ r.message + '/' + frm.doc.route }
+				</cite>
+				<div style="max:width: 580px">
+					<h3 style="color:#1a0dab; font-family: 'Open Sans'; font-size:20px; line-height:1.3; hover:underline; margin: 0 0"><a style="color:#1a0dab;">
+						${ seo_title }</a></h3>
+				</div>
+					<p style="color: #545454;
+					font-size: small;
+					line-height: 1.4;
+					font-family: 'Open Sans'; font-size:14px; line-height:1.3; margin: 0 0">
+					${ frm.doc.published_on }-${seo_description}
+				</p>	
+			</div>
+			</div>	
+		`)
+	});	
+}
