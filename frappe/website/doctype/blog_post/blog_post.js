6e50d8e79255302322716ff911a14dcf63db01fb
Himanshu Warekar <himanshuwarekar@yahoo.com>
2020-04-24 15:15:34 +0530

    fix: add form control html

--- frappe/website/doctype/blog_post/blog_post.js
+++ frappe/website/doctype/blog_post/blog_post.js
@@ -3,47 +3,56 @@
 
 frappe.ui.form.on('Blog Post', {
 	refresh: function(frm) {
-		generate_google_search_preview(frm)
+		generate_google_search_preview(frm);
 	},
 	title: function(frm) {
-		generate_google_search_preview(frm)
+		generate_google_search_preview(frm);
 	},
-	meta_description:function(frm){
-		generate_google_search_preview(frm)
+	meta_description: function(frm) {
+		generate_google_search_preview(frm);
 	},
-	description:function(frm){
-		generate_google_search_preview(frm)
+	title: function(frm) {
+		generate_google_search_preview(frm);
 	}
 });
 function generate_google_search_preview(frm){
 	frm.call('get_url').then((r)=>{
-		let seo_title = frm.doc.title
-		seo_title = seo_title.slice(0,60)
-		let seo_description =  frm.doc.meta_description || frm.doc.description
-		seo_description = seo_description.slice(0,160)
-		let google_preview = frm.get_field("google_preview")
+		let google_preview = frm.get_field("google_preview");
+		let seo_title = (frm.doc.title || "").slice(0, 60);
+		let seo_description =  (frm.doc.meta_description || frm.doc.blog_intro || "").slice(0, 160);
+
 		google_preview.html(`
-			<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&display=swap" rel="stylesheet">			
-			<div style="max-width: 750px;">
-			<div>
-				<cite style="color: #006621;
-					font-style: normal;
-					font-size: 14px;
-					display: inline-block;
-					font-family: 'Open Sans'; font-size:14px;line-height:1.3; margin: 0 0">${ r.message + '/' + frm.doc.route }
-				</cite>
-				<div style="max:width: 580px">
-					<h3 style="color:#1a0dab; font-family: 'Open Sans'; font-size:20px; line-height:1.3; hover:underline; margin: 0 0"><a style="color:#1a0dab;">
-						${ seo_title }</a></h3>
+			<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&display=swap" rel="stylesheet">
+
+			<div class="frappe-control">
+				<div class="form-group">
+					<div class="clearfix">
+						<label class="control-label" style="padding-right: 0px;">Google SEO</label>
+					</div>
+					<div class="control-input-wrapper">
+						<div class="control-input">
+							<div>
+							<cite style="color: #006621; font-style: normal; font-size: 14px; display: inline-block;
+								font-family: 'Open Sans'; font-size: 14px; line-height: 1.3; margin: 0 0">
+								${ r.message + '/' + frm.doc.route }
+							</cite>
+							<h3 style="color: #1a0dab; font-family: 'Open Sans'; font-size: 20px; line-height: 1.3; hover: underline; margin: 0 0">
+								<a style="color:#1a0dab;">
+									${ seo_title }
+								</a>
+							</h3>
+							<p style="color: #545454; font-size: small; line-height: 1.4; font-family: 'Open Sans'; font-size:14px;
+								line-height:1.3; margin: 0 0">
+								${ frm.doc.published_on } - ${ seo_description }
+							</p>
+							</div>
+						</div>
+						<p class="help-box small text-muted hidden-xs">
+							Description for listing page, in plain text, only a couple of lines. (max 140 characters)
+						</p>
+					</div>
 				</div>
-					<p style="color: #545454;
-					font-size: small;
-					line-height: 1.4;
-					font-family: 'Open Sans'; font-size:14px; line-height:1.3; margin: 0 0">
-					${ frm.doc.published_on }-${seo_description}
-				</p>	
 			</div>
-			</div>	
 		`)
-	});	
+	});
 }
