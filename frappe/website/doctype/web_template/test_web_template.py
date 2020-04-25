31b8b53e88e246a9a9b968585ca350c45049c57b
Faris Ansari <netchamp.faris@gmail.com>
2020-04-25 20:50:31 +0530

    fix: Use assertEqual instead of assertEquals
    - assertEquals will be deprecated

--- frappe/website/doctype/web_template/test_web_template.py
+++ frappe/website/doctype/web_template/test_web_template.py
@@ -38,7 +38,7 @@ class TestWebTemplate(unittest.TestCase):
 		set_request(method="GET", path="test-web-template")
 		response = render()
 
-		self.assertEquals(response.status_code, 200)
+		self.assertEqual(response.status_code, 200)
 
 		html = frappe.safe_decode(response.get_data())
 
@@ -59,7 +59,7 @@ class TestWebTemplate(unittest.TestCase):
 
 		set_request(method="GET", path="test-web-template")
 		response = render()
-		self.assertEquals(response.status_code, 200)
+		self.assertEqual(response.status_code, 200)
 		html = frappe.safe_decode(response.get_data())
 
 		soup = BeautifulSoup(html, "html.parser")
@@ -78,7 +78,7 @@ class TestWebTemplate(unittest.TestCase):
 
 		set_request(method="GET", path="test-web-template")
 		response = render()
-		self.assertEquals(response.status_code, 200)
+		self.assertEqual(response.status_code, 200)
 		html = frappe.safe_decode(response.get_data())
 
 		soup = BeautifulSoup(html, "html.parser")
