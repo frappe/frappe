94a950bc98b907100bfae30b68f04dbc57532f34
Shivam Mishra <scm.mymail@gmail.com>
2020-04-29 22:04:36 +0530

    feat: allow configurable field validation

--- frappe/desk/doctype/onboarding_step/onboarding_step.js
+++ frappe/desk/doctype/onboarding_step/onboarding_step.js
@@ -2,7 +2,32 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Onboarding Step', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		if (frm.doc.reference_document && frm.doc.action == "Update Settings") {
+			setup_fields(frm);
+		}
+	},
 
-	// }
+	reference_document: function(frm) {
+		if (frm.doc.reference_document && frm.doc.action == "Update Settings") {
+			setup_fields(frm);
+		}
+	}
 });
+
+function setup_fields(frm) {
+	if (frm.doc.reference_document && frm.doc.action == "Update Settings") {
+		frappe.model.with_doctype(frm.doc.reference_document, () => {
+			let fields = frappe.get_meta(frm.doc.reference_document).fields.filter(df => {
+				return ["Data", "Check", "Int", "Link", "Select"].includes(df.fieldtype);
+			}).map(df => {
+				return {
+						"label": `${__(df.label)} (${df.fieldname})`,
+						"value": df.fieldname
+					}
+			});
+
+			frm.set_df_property('field', 'options', fields)
+		});
+	}
+}
