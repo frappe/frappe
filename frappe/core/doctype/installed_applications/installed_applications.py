8480b3dbfef7ccbb10c2fd5c7981b0746c11bd70
Gavin D'souza <gavin18d@gmail.com>
2020-05-11 20:12:05 +0530

    feat: single doctype to store installed apps data
    updated on migrate
    versions enabled

---
+++ frappe/core/doctype/installed_applications/installed_applications.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Frappe Technologies and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class InstalledApplications(Document):
+	def before_save(self):
+		self.delete_key("installed_applications")
+		for app in frappe.utils.get_installed_apps_info():
+			self.append("installed_applications", {
+				"application_name": app.get("app_name"),
+				"application_version": app.get("version"),
+				"git_branch": app.get("branch")
+			})
