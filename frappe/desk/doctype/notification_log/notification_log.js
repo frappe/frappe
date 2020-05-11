a6a3919ac61cd9217abd32155d5e95e3637c3767
prssanna <prssud@gmail.com>
2020-05-11 19:27:01 +0530

    fix: add button to route to reference doc form

--- frappe/desk/doctype/notification_log/notification_log.js
+++ frappe/desk/doctype/notification_log/notification_log.js
@@ -2,11 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Notification Log', {
-	refresh: function(frm) {
+	open_reference_document: function(frm) {
 		let dt = frm.doc.document_type;
 		let dn = frm.doc.document_name;
-		frm.fields_dict.document_name.$input_wrapper
-			.find('.control-value')
-			.wrapInner(`<a href='#Form/${dt}/${dn}'></a>`);
+		frappe.set_route('Form', dt, dn);
 	}
 });
