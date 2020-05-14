9c412d60df01b5f0c555f102b950092a669ba14e
prssanna <prssud@gmail.com>
2020-05-15 00:40:01 +0530

    fix: fix rebase

--- frappe/public/js/frappe/widgets/chart_widget.js
+++ frappe/public/js/frappe/widgets/chart_widget.js
@@ -523,7 +523,6 @@ export default class ChartWidget extends Widget {
 		}
 	}
 
-
 	get_chart_args() {
 		let colors = this.get_chart_colors();
 
@@ -551,6 +550,21 @@ export default class ChartWidget extends Widget {
 			const heatmap_year = parseInt(this.selected_heatmap_year || this.chart_settings.heatmap_year || this.chart_doc.heatmap_year);
 			chart_args.data.start = new Date(`${heatmap_year}-01-01`);
 			chart_args.data.end = new Date(`${heatmap_year+1}-01-01`);
+		}
+
+		let set_options = (options) => {
+			let custom_options = JSON.parse(options);
+			for (let key in custom_options) {
+				chart_args[key] = custom_options[key];
+			}
+		};
+
+		if (this.custom_options) {
+			set_options(this.custom_options);
+		}
+
+		if (this.chart_doc.custom_options) {
+			set_options(this.chart_doc.custom_options);
 		}
 
 		return chart_args;
@@ -591,59 +605,6 @@ export default class ChartWidget extends Widget {
 				`);
 			this.body.append(this.$heatmap_legend);
 		}
-	}
-
-	get_chart_args() {
-		let colors = this.get_chart_colors();
-
-		const chart_type_map = {
-			Line: "line",
-			Bar: "bar",
-			Percentage: "percentage",
-			Pie: "pie",
-			Donut: "donut"
-		};
-
-		let chart_args = {
-			data: this.data,
-			type: chart_type_map[this.chart_doc.type],
-			colors: colors,
-			height: this.height,
-			axisOptions: {
-				xIsSeries: this.chart_doc.timeseries,
-				shortenYAxisNumbers: 1
-			}
-		};
-
-		let set_options = (options) => {
-			let custom_options = JSON.parse(options);
-			for (let key in custom_options) {
-				chart_args[key] = custom_options[key];
-			}
-		};
-
-		if (this.custom_options) {
-			set_options(this.custom_options);
-		}
-
-		if (this.chart_doc.custom_options) {
-			set_options(this.chart_doc.custom_options);
-		}
-
-		return chart_args;
-	}
-
-	get_chart_colors() {
-		let colors = [];
-		if (this.chart_doc.y_axis.length) {
-			this.chart_doc.y_axis.map(field => {
-				colors.push(field.color);
-			});
-		} else if (["Line", "Bar"].includes(this.chart_doc.type)) {
-			colors = [this.chart_doc.color || "light-blue"];
-		}
-
-		return colors;
 	}
 
 	update_last_synced() {
