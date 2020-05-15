05b880335ae6a618601e46e24b42211a37a18ca9
Faris Ansari <netchamp.faris@gmail.com>
2020-05-15 16:13:16 +0530

    fix: Remove hardcoded url
    (cherry picked from commit 4af13edc87fee4083491fcc14197040300b4575f)

--- frappe/integrations/frappe_providers/__init__.py
+++ frappe/integrations/frappe_providers/__init__.py
@@ -4,7 +4,6 @@ from frappe.integrations.frappe_providers.frappecloud import frappecloud_migrato
 
 def migrate_to(local_site, frappe_provider):
 	if frappe_provider in ("frappe.cloud", "frappecloud.com"):
-		frappe_provider = "staging.frappe.cloud"
 		return frappecloud_migrator(local_site, frappe_provider)
 	else:
 		print("{} is not supported yet".format(frappe_provider))
