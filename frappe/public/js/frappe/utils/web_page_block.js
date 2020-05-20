66df42b28144bd17766ddb4f6fa506396c560244
Shivam Mishra <scm.mymail@gmail.com>
2020-05-20 15:34:13 +0530

    refactor: show sections with values

--- frappe/public/js/frappe/utils/web_page_block.js
+++ frappe/public/js/frappe/utils/web_page_block.js
@@ -1,10 +1,10 @@
-frappe.ui.form.on('Web Page Block', {
+frappe.ui.form.on("Web Page Block", {
 	edit_values(frm, cdt, cdn) {
 		let row = frm.selected_doc;
-		frappe.model.with_doc('Web Template', row.web_template).then(doc => {
+		frappe.model.with_doc("Web Template", row.web_template).then((doc) => {
 			let d = new frappe.ui.Dialog({
-				title: __('Edit Values'),
-				fields: doc.fields.map(df => {
+				title: __("Edit Values"),
+				fields: doc.fields.map((df) => {
 					if (df.fieldtype == "Section Break") {
 						df.collapsible = 1;
 					}
@@ -14,15 +14,25 @@ frappe.ui.form.on('Web Page Block', {
 					frappe.model.set_value(
 						cdt,
 						cdn,
-						'web_template_values',
+						"web_template_values",
 						JSON.stringify(values)
 					);
 					d.hide();
-				}
+				},
 			});
-			let values = JSON.parse(row.web_template_values || '{}');
+			let values = JSON.parse(row.web_template_values || "{}");
 			d.set_values(values);
 			d.show();
+
+			d.sections.forEach((sect) => {
+				let fields_with_value = sect.fields_list.filter(
+					(field) => values[field.df.fieldname]
+				);
+
+				if (fields_with_value.length) {
+					sect.collapse(false);
+				}
+			});
 		});
-	}
+	},
 });
