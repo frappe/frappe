2813a0edc1a92607d4e90fb43b87c59514c6c909
Shivam Mishra <scm.mymail@gmail.com>
2020-05-22 13:31:49 +0530

    feat: added webhook hmac verification

--- frappe/integrations/doctype/razorpay_settings/razorpay_settings.py
+++ frappe/integrations/doctype/razorpay_settings/razorpay_settings.py
@@ -64,6 +64,9 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 import json
+import hmac
+import sys
+import hashlib
 from six.moves.urllib.parse import urlencode
 from frappe.model.document import Document
 from frappe.utils import get_url, call_hook_method, cint, get_timestamp
@@ -316,6 +319,27 @@ class RazorpaySettings(Document):
 					settings.api_secret))
 		except Exception:
 			frappe.log_error(frappe.get_traceback())
+
+	def verify_signature(self, body, signature, key):
+		if sys.version_info[0] == 3:
+			key = bytes(key, 'utf-8')
+			body = bytes(body, 'utf-8')
+
+		dig = hmac.new(key=key,
+					   msg=body,
+					   digestmod=hashlib.sha256)
+
+		generated_signature = dig.hexdigest()
+
+		if sys.version_info[0:3] < (2, 7, 7):
+			result = self.compare_string(generated_signature, signature)
+		else:
+			result = hmac.compare_digest(generated_signature, signature)
+
+		if not result:
+			frappe.throw(_('Razorpay Signature Verification Failed'), exc=frappe.PermissionError)
+
+		return result
 
 def capture_payment(is_sandbox=False, sanbox_response=None):
 	"""
