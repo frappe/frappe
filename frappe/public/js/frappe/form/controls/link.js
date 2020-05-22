ac879b0d577044323d28d381fde8bf105ff06468
Himanshu Warekar <himanshuwarekar@yahoo.com>
2020-05-22 13:47:32 +0530

    fix: filter table multiselect datalist

--- frappe/public/js/frappe/form/controls/link.js
+++ frappe/public/js/frappe/form/controls/link.js
@@ -117,9 +117,6 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 					value: item.value
 				};
 			},
-			filter: function() {
-				return true;
-			},
 			item: function (item) {
 				var d = this.get_item(item.value);
 				if(!d.label) {	d.label = d.value; }
@@ -139,6 +136,8 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 				return 0;
 			}
 		});
+
+		this.filter_awesomplete(this.awesomplete);
 
 		this.$input.on("input", frappe.utils.debounce(function(e) {
 			var doctype = me.get_options();
@@ -461,15 +460,22 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 			});
 		}
 	},
+
 	set_fetch_values: function(df, docname, fetch_values) {
 		var fl = this.frm.fetch_dict[df.fieldname].fields;
 		for(var i=0; i < fl.length; i++) {
 			frappe.model.set_value(df.parent, docname, fl[i], fetch_values[i], df.fieldtype);
 		}
+	},
+
+	filter_awesomplete: function(awesomplete) {
+		awesomplete.filter = function() {
+			return true;
+		}
 	}
 });
 
-if(Awesomplete) {
+if (Awesomplete) {
 	Awesomplete.prototype.get_item = function(value) {
 		return this._list.find(function(item) {
 			return item.value === value;
