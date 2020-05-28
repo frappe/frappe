4f99b5879ec9e5aff46294b58474e76a54f65427
Gavin D'souza <gavin18d@gmail.com>
2020-05-28 20:28:55 +0530

    fix: set frappecloud_url in conf to override specified url

--- frappe/integrations/frappe_providers/frappecloud.py
+++ frappe/integrations/frappe_providers/frappecloud.py
@@ -372,7 +372,7 @@ def frappecloud_migrator(local_site, frappecloud_site):
 	global login_url, upload_url, files_url, options_url, site_exists_url, restore_site_url, account_details_url, all_site_url
 	global session, migrator_actions, remote_site
 
-	remote_site = frappecloud_site
+	remote_site = frappe.conf.frappecloud_url or "frappecloud.com"
 
 	login_url = "https://{}/api/method/login".format(remote_site)
 	upload_url = "https://{}/api/method/press.api.site.new".format(remote_site)
