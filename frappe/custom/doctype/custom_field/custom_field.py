e8a0b5b873cc8c7d5c007d6d37ddb5afd91fd730
theopen-institute <admin@theopen.institute>
2020-05-30 14:34:27 +0545

    Update frappe/custom/doctype/custom_field/custom_field.py
    Co-authored-by: Chinmay Pai <chinmaydpai@gmail.com>

--- frappe/custom/doctype/custom_field/custom_field.py
+++ frappe/custom/doctype/custom_field/custom_field.py
@@ -47,7 +47,7 @@ class CustomField(Document):
 			frappe.throw(_("Fieldname not set for Custom Field"))
 
 		if self.fieldname in fieldnames:
-			frappe.throw(_("A field with the name '{}' already exists in doctype {}.".format(self.fieldname, self.dt)))
+			frappe.throw(_("A field with the name '{}' already exists in doctype {}.").format(self.fieldname, self.dt))
 
 		if self.get('translatable', 0) and not supports_translation(self.fieldtype):
 			self.translatable = 0
