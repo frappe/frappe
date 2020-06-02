0781d048d178aea71ccfdeeeb5d6caac3db42299
Mangesh-Khairnar <mkhairnar10@gmail.com>
2020-06-02 12:33:01 +0530

    fix: add docs link to the settings page dashboard

--- frappe/integrations/doctype/paytm_settings/paytm_settings.js
+++ frappe/integrations/doctype/paytm_settings/paytm_settings.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Paytm Settings', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.dashboard.set_headline(__("For more information, {0}.", [`<a href='https://erpnext.com/docs/user/manual/en/erpnext_integration/paytm-integration'>${__('Click here')}</a>`]));
+	}
 });
