68c0450ac772caa44d845dd4c923f96c9c3b5e75
Shivam Mishra <scmmishra@users.noreply.github.com>
2020-06-11 13:30:39 +0000

    fix: better error title
    Co-authored-by: Chinmay Pai <chinmaydpai@gmail.com>

--- frappe/email/doctype/notification/notification.py
+++ frappe/email/doctype/notification/notification.py
@@ -129,7 +129,7 @@ def get_context(context):
 			if self.channel == 'System Notification' or self.send_system_notification:
 				self.create_system_notification(doc, context)
 		except:
-			frappe.log_error(title='Notificaiton Sending Failed', message=frappe.get_traceback())
+			frappe.log_error(title='Failed to send notification', message=frappe.get_traceback())
 
 		if self.set_property_after_alert:
 			allow_update = True
