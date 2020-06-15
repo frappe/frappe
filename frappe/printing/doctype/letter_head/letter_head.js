a5be91c255528dbf5071ec551af99b956eca2d7d
Shivam Mishra <scm.mymail@gmail.com>
2020-06-15 13:03:38 +0530

    feat (ux): button to add image to footer

--- frappe/printing/doctype/letter_head/letter_head.js
+++ frappe/printing/doctype/letter_head/letter_head.js
@@ -4,5 +4,20 @@
 frappe.ui.form.on('Letter Head', {
 	refresh: function(frm) {
 		frm.flag_public_attachments = true;
+		frm.fields_dict.add_image.onclick = () => {
+			let options = {
+				allow_multiple: false,
+				doctype: frm.doctype,
+				docname: frm.docname,
+				restrictions: {
+					allowed_file_types: ['image/*']
+				},
+				on_success: file => {
+					let footer_html = frm.fields_dict.footer.value || '';
+					frm.set_value('footer', footer_html + `<img src="${file.file_url}" alt="">`);
+				}
+			};
+			let file_uploader = new frappe.ui.FileUploader(options);
+		}
 	}
 });
