443edf209abeaedfa5105308188b7d6704d186f7
Anupam K <anupamvns0099@gmail.com>
2020-06-17 14:02:30 +0530

    calendar view fix

--- frappe/public/js/frappe/views/calendar/calendar.js
+++ frappe/public/js/frappe/views/calendar/calendar.js
@@ -73,14 +73,15 @@ frappe.views.CalendarView = class CalendarView extends frappe.views.ListView {
 			} else {
 				frappe.model.with_doc('Calendar View', calendar_name, () => {
 					const doc = frappe.get_doc('Calendar View', calendar_name);
-					let field_map = {
-						id: "name",
-						start: doc.start_date_field,
-						end: doc.end_date_field,
-						title: doc.subject_field
-					};
-					field_map.allDay = doc.all_day ? 0 : 1;
-					Object.assign(options, { field_map: field_map });
+					Object.assign(options, {
+						field_map: {
+							id: "name",
+							start: doc.start_date_field,
+							end: doc.end_date_field,
+							title: doc.subject_field,
+							allDay = doc.all_day ? 1 : 0
+						}
+					});
 					resolve(options);
 				});
 			}
