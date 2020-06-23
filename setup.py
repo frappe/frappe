70d264c4a07fc087902c1a7db073a24da82ef786
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-06-23 11:47:44 +0530

    build: move to a declarative package setup
    use declarative package configuration using setup.cfg instead of
    cluttering setup.py, making it easier to maintain app configuration
    
    also simplify the cleanup command to use pathlib instead of os and
    shutil and make it run slightly faster
    
    NOTE: requirements.txt should no longer be _required_, but is kept for
    compatibility until we slowly deprecate it. so any changes made to
    requirements.txt should be made to setup.cfg for now.
    
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- setup.py
+++ setup.py
@@ -1,63 +1,33 @@
 from __future__ import unicode_literals
 
 # imports - standard imports
-import os, shutil
 from distutils.command.clean import clean as Clean
+from pathlib import Path
+from setuptools import setup
 
-from setuptools import setup, find_packages
-import re, ast
-
-# get version from __version__ variable in frappe/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-with open('frappe/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
 
 class CleanCommand(Clean):
 	def run(self):
 		Clean.run(self)
 
-		basedir = os.path.abspath(os.path.dirname(__file__))
-
-		for relpath in ['build', '.cache', '.coverage', 'dist', 'frappe.egg-info']:
-			abspath = os.path.join(basedir, relpath)
-			if os.path.exists(abspath):
-				if os.path.isfile(abspath):
-					os.remove(abspath)
+		def rm_tree(path):
+			path = Path(path)
+			for file in path.glob('*'):
+				if file.is_file():
+					file.unlink()
 				else:
-					shutil.rmtree(abspath)
+					rm_tree(file)
+			if path.is_dir():
+				path.rmdir()
 
-		for dirpath, dirnames, filenames in os.walk(basedir):
-			for filename in filenames:
-				_, extension = os.path.splitext(filename)
-				if extension in ['.pyc']:
-					abspath = os.path.join(dirpath, filename)
-					os.remove(abspath)
-			for dirname in dirnames:
-				if dirname in ['__pycache__']:
-					abspath = os.path.join(dirpath,  dirname)
-					shutil.rmtree(abspath)
+		for relpath in ('build', '.cache', '.coverage', 'dist', 'frappe.egg-info'):
+			rm_tree(relpath)
 
-setup(
-	name='frappe',
-	version=version,
-	description='Metadata driven, full-stack web framework',
-	author='Frappe Technologies',
-	author_email='info@frappe.io',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires,
-	dependency_links=[
-		'https://github.com/frappe/python-pdfkit.git#egg=pdfkit'
-	],
-	cmdclass = \
-	{
-		'clean': CleanCommand
-	},
-	python_requires='>=3.6'
-)
+		for path in Path('.').rglob('*.py[co]'):
+			path.unlink()
+
+		for path in Path('.').rglob('__pycache__'):
+			path.rmdir()
+
+
+setup(cmdclass={'clean': CleanCommand})
