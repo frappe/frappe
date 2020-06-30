45d097f6b52670140983455fd3c101e691e68abd
Faris Ansari <netchamp.faris@gmail.com>
2020-06-30 13:13:56 +0530

    fix(patch): Use rename_doc

--- frappe/patches/v13_0/replace_old_data_import.py
+++ frappe/patches/v13_0/replace_old_data_import.py
@@ -13,6 +13,4 @@ def execute():
 	frappe.rename_doc('DocType', 'Data Import', 'Data Import Legacy')
 	frappe.db.commit()
 	frappe.db.sql("DROP TABLE IF EXISTS `tabData Import`")
-	frappe.reload_doc("core", "doctype", "data_import")
-	frappe.get_doc("DocType", "Data Import").on_update()
-	frappe.delete_doc_if_exists("DocType", "Data Import Beta")
+	frappe.rename_doc('DocType', 'Data Import Beta', 'Data Import')
