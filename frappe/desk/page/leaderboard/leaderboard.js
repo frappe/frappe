002dfec30dfd36d76392ebb0d01c90218a44eb98
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2020-07-03 10:59:00 +0530

    refactor: Use current_date variable

--- frappe/desk/page/leaderboard/leaderboard.js
+++ frappe/desk/page/leaderboard/leaderboard.js
@@ -379,16 +379,16 @@ class Leaderboard {
 		let timespan = this.options.selected_timespan.toLowerCase();
 		let current_date = frappe.datetime.now_date();
 		let date_range_map = {
-			"this week": [frappe.datetime.week_start(), frappe.datetime.now_date()],
-			"this month": [frappe.datetime.month_start(), frappe.datetime.now_date()],
-			"this quarter": [frappe.datetime.quarter_start(), frappe.datetime.now_date()],
-			"this year": [frappe.datetime.year_start(), frappe.datetime.now_date()],
-			"last week": [frappe.datetime.add_days(current_date, -7), frappe.datetime.now_date()],
-			"last month": [frappe.datetime.add_months(current_date, -1), frappe.datetime.now_date()],
-			"last quarter": [frappe.datetime.add_months(current_date, -3), frappe.datetime.now_date()],
-			"last year": [frappe.datetime.add_months(current_date, -12), frappe.datetime.now_date()],
+			"this week": [frappe.datetime.week_start(), current_date],
+			"this month": [frappe.datetime.month_start(), current_date],
+			"this quarter": [frappe.datetime.quarter_start(), current_date],
+			"this year": [frappe.datetime.year_start(), current_date],
+			"last week": [frappe.datetime.add_days(current_date, -7), current_date],
+			"last month": [frappe.datetime.add_months(current_date, -1), current_date],
+			"last quarter": [frappe.datetime.add_months(current_date, -3), current_date],
+			"last year": [frappe.datetime.add_months(current_date, -12), current_date],
 			"all time": null,
-			"select date range": this.selected_date_range || [frappe.datetime.month_start(), frappe.datetime.now_date()]
+			"select date range": this.selected_date_range || [frappe.datetime.month_start(), current_date]
 		}
 		return date_range_map[timespan];
 	}
