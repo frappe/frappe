b622f8c75f9f6f8a43e830f926c849fbec56e8fa
Himanshu Mishra <himanshu.mishra@elastic.run>
2020-07-03 23:36:29 +0530

    use default dict methods for smaller stack

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -30,21 +30,15 @@ local = Local()
 
 class _dict(dict):
 	"""dict like object that exposes keys as attributes"""
-	def __getattr__(self, key):
-		ret = self.get(key)
-		if not ret and key.startswith("__"):
-			raise AttributeError()
-		return ret
-	def __setattr__(self, key, value):
-		self[key] = value
-	def __getstate__(self):
-		return self
-	def __setstate__(self, d):
-		self.update(d)
+	__getattr__ = dict.get
+
+	__setattr__ = dict.__setitem__
+
 	def update(self, d):
 		"""update and return self -- the missing dict feature in python"""
 		super(_dict, self).update(d)
 		return self
+
 	def copy(self):
 		return _dict(dict(self).copy())
 
