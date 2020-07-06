127ba4f17c95fafcfb6efa706dc37d094ab3ccf6
Neel Bhanushali <neal.bhanushali@gmail.com>
2020-07-06 12:49:52 +0530

    feat(Deleted Document): file restore from trash functionality

--- frappe/core/doctype/deleted_document/deleted_document.py
+++ frappe/core/doctype/deleted_document/deleted_document.py
@@ -6,6 +6,9 @@ from __future__ import unicode_literals
 import frappe, json
 from frappe.model.document import Document
 from frappe import _
+from frappe.core.doctype.file.file import _get_trash_path
+from os.path import exists
+from shutil import move
 
 class DeletedDocument(Document):
 	pass
@@ -13,13 +16,20 @@ class DeletedDocument(Document):
 @frappe.whitelist()
 def restore(name):
 	deleted = frappe.get_doc('Deleted Document', name)
-	doc = frappe.get_doc(json.loads(deleted.data))
+	data = json.loads(deleted.data)
+	doc = frappe.get_doc(data)
 	try:
 		doc.insert()
 	except frappe.DocstatusTransitionError:
 		frappe.msgprint(_("Cancelled Document restored as Draft"))
 		doc.docstatus = 0
 		doc.insert()
+
+	
+	if data.get('doctype') == "File":
+		trash_path = _get_trash_path(doc.get('file_name'), doc.get('is_private'))
+		if exists(trash_path):
+			move(trash_path, frappe.utils.get_files_path(doc.get('file_name'), is_private=data.get('is_private')))
 
 	doc.add_comment('Edit', _('restored {0} as {1}').format(deleted.deleted_name, doc.name))
 
