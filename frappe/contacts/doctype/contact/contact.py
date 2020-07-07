338151a7e5e067ec3e566025b1c99b04621432e3
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-07-07 11:22:37 +0530

    fix: correctly format sql query
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- frappe/contacts/doctype/contact/contact.py
+++ frappe/contacts/doctype/contact/contact.py
@@ -205,14 +205,14 @@ def contact_query(doctype, txt, searchfield, start, page_len, filters):
 			`tabDynamic Link`.parenttype = 'Contact' and
 			`tabDynamic Link`.link_doctype = %(link_doctype)s and
 			`tabDynamic Link`.link_name = %(link_name)s and
-			`tabContact`.`{key}` like %(txt)s
-			{mcond}
+			`tabContact`.`%(key)s` like %(txt)s
+			%(mcond)s
 		order by
 			if(locate(%(_txt)s, `tabContact`.name), locate(%(_txt)s, `tabContact`.name), 99999),
 			`tabContact`.idx desc, `tabContact`.name
-		limit %(start)s, %(page_len)s """.format(
-			mcond=get_match_cond(doctype),
-			key=searchfield), {
+		limit %(start)s, %(page_len)s """, {
+			'mcond': get_match_cond(doctype),
+			'key': searchfield,
 			'txt': '%' + txt + '%',
 			'_txt': txt.replace("%", ""),
 			'start': start,
