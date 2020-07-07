7471fe368cf3379e4c53b3c0860da30dda6e97ae
prssanna <prssud@gmail.com>
2020-07-07 14:16:52 +0530

    fix: better code

--- frappe/patches/v12_0/set_correct_assign_value_in_docs.py
+++ frappe/patches/v12_0/set_correct_assign_value_in_docs.py
@@ -3,25 +3,29 @@ import frappe
 def execute():
 	frappe.reload_doc('desk', 'doctype', 'todo')
 
-	if frappe.db.db_type == 'mariadb':
-		fields = 'name, reference_type, reference_name, group_concat(distinct owner) as owner'
-	else:
-		fields = 'name, reference_type, reference_name, string_agg(distinct owner, ",") as owner'
-
-	closed_todos = frappe.db.sql('''
+	query = '''
 		SELECT
-			{fields}
+			name, reference_type, reference_name, {} as assignees
 		FROM
-			`tabTodo`
+			`tabToDo`
 		WHERE
-			reference_type != '' and reference_name != '' and
-			status = 'Closed'
+			COALESCE(reference_type, '') != '' and
+			COALESCE(reference_name, '') != ''
 		GROUP BY
 			reference_type, reference_name
-	'''.format(fields=fields), as_dict=True)
+	'''
 
-	for doc in closed_todos:
-		assignments = frappe.db.get_value(doc.reference_type, doc.reference_name, '_assign')
-		assignments = frappe.parse_json(assignments) or []
-		unique_assignments = list(set(doc.owner.split(',') + assignments))
-		frappe.db.set_value(doc.reference_type, doc.reference_name, '_assign', frappe.as_json(unique_assignments))
+	assignments = frappe.db.multisql({
+		'mariadb': query.format('GROUP_CONCAT(DISTINCT `owner`)'),
+		'postgres': query.format('STRING_AGG(DISTINCT "owner", ",")')
+	}, as_dict=True)
+
+	for doc in assignments:
+		assignments = doc.assignees.split(',')
+		frappe.db.set_value(
+			doc.reference_type,
+			doc.reference_name,
+			'_assign',
+			frappe.as_json(assignments),
+			update_modified=False
+		)
