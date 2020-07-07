9c6725ffda231a7e3b8d5a5b6a2fa45afc23e835
prssanna <prssud@gmail.com>
2020-07-07 13:18:10 +0530

    fix: group todos by reference type and name

--- frappe/patches/v12_0/set_correct_assign_value_in_docs.py
+++ frappe/patches/v12_0/set_correct_assign_value_in_docs.py
@@ -3,18 +3,25 @@ import frappe
 def execute():
 	frappe.reload_doc('desk', 'doctype', 'todo')
 
-	closed_todos = frappe.get_all(
-		'ToDo',
-		fields = ['name', 'reference_type', 'reference_name', 'owner'],
-		filters = {
-			'status': 'Closed'
-		}
-	)
+	if frappe.db.db_type == 'mariadb':
+		fields = 'name, reference_type, reference_name, group_concat(distinct owner) as owner'
+	else:
+		fields = 'name, reference_type, reference_name, string_agg(distinct owner, ",") as owner'
+
+	closed_todos = frappe.db.sql('''
+		SELECT
+			{fields}
+		FROM
+			`tabTodo`
+		WHERE
+			reference_type != '' and reference_name != '' and
+			status = 'Closed'
+		GROUP BY
+			reference_type, reference_name
+	'''.format(fields=fields), as_dict=True)
 
 	for doc in closed_todos:
-		if doc.reference_type and doc.reference_name:
-			assignments = frappe.db.get_value(doc.reference_type, doc.reference_name, '_assign')
-			assignments = frappe.parse_json(assignments) or []
-			if isinstance(assignments, list) and doc.owner not in assignments:
-				assignments.append(doc.owner)
-				frappe.db.set_value(doc.reference_type, doc.reference_name, '_assign', frappe.as_json(assignments))
+		assignments = frappe.db.get_value(doc.reference_type, doc.reference_name, '_assign')
+		assignments = frappe.parse_json(assignments) or []
+		unique_assignments = list(set(doc.owner.split(',') + assignments))
+		frappe.db.set_value(doc.reference_type, doc.reference_name, '_assign', frappe.as_json(unique_assignments))
