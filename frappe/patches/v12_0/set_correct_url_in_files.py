d3b508efb989d984547cccfaa4feb53045e9f122
prssanna <prssud@gmail.com>
2020-07-07 19:33:11 +0530

    fix: don't use get doc

--- frappe/patches/v12_0/set_correct_url_in_files.py
+++ frappe/patches/v12_0/set_correct_url_in_files.py
@@ -2,18 +2,25 @@ import frappe
 import os
 
 def execute():
-	files = frappe.get_all('File', filters={'is_folder': 0})
+	files = frappe.get_all(
+		'File',
+		fields = ['name', 'file_name', 'file_url'],
+		filters = {
+			'is_folder': 0,
+			'file_url': ['!=', '']
+		}
+	)
+
 	for file in files:
-		doc = frappe.get_doc('File', file.name)
-		file_name = doc.file_url.split('/')[-1]
+		file_name = file.file_url.split('/')[-1]
 		private_file_path = frappe.get_site_path('private', 'files')
 		public_file_path = frappe.get_site_path('public', 'files')
 
-		file_path = doc.file_url or doc.file_name
+		file_path = file.file_url or file.file_name
 		if '/' not in file_path:
 			file_path = '/files/' + file_path
 
-		full_path = doc.get_full_path()
+		full_path = get_full_path(file_path)
 
 		if not os.path.exists(full_path):
 			if file_path.startswith('/private/files/'):
@@ -21,7 +28,7 @@ def execute():
 				if os.path.exists(public_file_url):
 					frappe.db.set_value(
 						'File',
-						doc.name,
+						file.name,
 						{
 							'file_url': '/files/{0}'.format(file_name),
 							'is_private': 0
@@ -34,7 +41,7 @@ def execute():
 				if os.path.exists(private_file_url):
 					frappe.db.set_value(
 						'File',
-						doc.name,
+						file.name,
 						{
 							'file_url': '/private/files/{0}'.format(file_name),
 							'is_private': 1
@@ -42,3 +49,14 @@ def execute():
 						update_modified=False
 					)
 
+def get_full_path(file_path):
+	if file_path.startswith("/private/files/"):
+		file_path = frappe.utils.get_files_path(*file_path.split("/private/files/", 1)[1].split("/"), is_private=1)
+
+	elif file_path.startswith("/files/"):
+		file_path = frappe.utils.get_files_path(*file_path.split("/files/", 1)[1].split("/"))
+
+	else:
+		pass
+
+	return file_path
