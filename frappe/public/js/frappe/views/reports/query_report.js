6672434b24c127a1bf434d9192b7352e286e8335
Shivam Mishra <scm.mymail@gmail.com>
2020-07-09 15:57:29 +0530

    refactor: override set_default_secondary_action from base list

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -62,6 +62,14 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 		});
 	}
 
+	set_default_secondary_action() {
+		this.refresh_button && this.refresh_button.remove();
+		this.refresh_button = this.page.add_action_icon("refresh", () => {
+			this.setup_progress_bar();
+			this.refresh()
+		});
+	}
+
 	setup_events() {
 		frappe.realtime.on("report_generated", (data) => {
 			if(data.report_name) {
