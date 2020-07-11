545412ff4e2eeed21e42442cdaff6c1be19e34f2
prssanna <prssud@gmail.com>
2020-07-11 15:20:30 +0530

    fix: validate if all docs in dashboard have been set as standard

--- frappe/desk/doctype/dashboard/dashboard.py
+++ frappe/desk/doctype/dashboard/dashboard.py
@@ -22,6 +22,17 @@ class Dashboard(Document):
 	def validate(self):
 		if not frappe.conf.developer_mode and self.is_standard:
 			frappe.throw('Cannot edit Standard Dashboards')
+
+		if self.is_standard:
+			non_standard_docs_map = {
+				'Dashboard Chart': get_non_standard_charts_in_dashboard(self),
+				'Number Card': get_non_standard_cards_in_dashboard(self)
+			}
+
+			if non_standard_docs_map['Dashboard Chart'] or non_standard_docs_map['Number Card']:
+				message = get_non_standard_warning_message(non_standard_docs_map)
+				frappe.throw(message, title=_("Standard Not Set"), is_minimizable=True)
+
 		self.validate_custom_options()
 
 	def validate_custom_options(self):
@@ -54,3 +65,29 @@ def get_permitted_cards(dashboard_name):
 		if frappe.has_permission('Number Card', doc=card.card):
 			permitted_cards.append(card)
 	return permitted_cards
+
+def get_non_standard_charts_in_dashboard(dashboard):
+	non_standard_charts = [doc.name for doc in frappe.get_list('Dashboard Chart', {'is_standard': 0})]
+	return [chart_link.chart for chart_link in dashboard.charts if chart_link.chart in non_standard_charts]
+
+def get_non_standard_cards_in_dashboard(dashboard):
+	non_standard_cards = [doc.name for doc in frappe.get_list('Number Card', {'is_standard': 0})]
+	return [card_link.card for card_link in dashboard.cards if card_link.card in non_standard_cards]
+
+def get_non_standard_warning_message(non_standard_docs_map):
+	message = _('''Please set the following documents in this Dashboard as standard first.''')
+
+	def get_html(docs, doctype):
+		html = '<p>{}</p>'.format(frappe.bold(doctype))
+		for doc in docs:
+			html += '<div><a href="#Form/{doctype}/{doc}">{doc}</a></div>'.format(doctype=doctype, doc=doc)
+		html += '<br>'
+		return html
+
+	html = message + '<br>'
+
+	for doctype in non_standard_docs_map:
+		if non_standard_docs_map[doctype]:
+			html += get_html(non_standard_docs_map[doctype], doctype)
+
+	return html
