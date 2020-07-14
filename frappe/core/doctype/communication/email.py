8a747eec2759a5153e52d17bbfe463548a85734f
Abhishek Balam <abhishekbalam96@gmail.com>
2020-07-14 13:45:44 +0530

    fix: Move checkbox from Print Settings to System Settings (#10905)

--- frappe/core/doctype/communication/email.py
+++ frappe/core/doctype/communication/email.py
@@ -221,7 +221,7 @@ def prepare_to_notify(doc, print_html=None, print_format=None, attachments=None)
 	:param print_html: Send given value as HTML attachment.
 	:param print_format: Attach print format of parent document."""
 
-	view_link = frappe.utils.cint(frappe.db.get_value("Print Settings", "Print Settings", "attach_view_link"))
+	view_link = frappe.utils.cint(frappe.db.get_value("System Settings", "System Settings", "attach_view_link"))
 
 	if print_format and view_link:
 		doc.content += get_attach_link(doc, print_format)
