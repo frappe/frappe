5f87fd119567b6e342d6bcac13f1c1f2656606a0
Suraj Shetty <surajshetty3416@gmail.com>
2020-07-15 10:15:41 +0530

    fix: Remove .ql-editor class from wrapping container

--- frappe/public/js/frappe/form/controls/text_editor.js
+++ frappe/public/js/frappe/form/controls/text_editor.js
@@ -69,7 +69,6 @@ Quill.register(CustomColor, true);
 frappe.ui.form.ControlTextEditor = frappe.ui.form.ControlCode.extend({
 	make_wrapper() {
 		this._super();
-		this.$wrapper.find(".like-disabled-input").addClass('ql-editor');
 	},
 
 	make_input() {
