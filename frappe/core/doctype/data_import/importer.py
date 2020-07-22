97691308a852d5c74678fa89807c12612395298a
Faris Ansari <netchamp.faris@gmail.com>
2020-07-22 17:54:48 +0530

    fix: Validate select values in Column
    (cherry picked from commit a2b2c9c926b1fd2be8192499dc8a21b250019eaa)

--- frappe/core/doctype/data_import/importer.py
+++ frappe/core/doctype/data_import/importer.py
@@ -615,7 +615,7 @@ class Row:
 	def validate_value(self, value, col):
 		df = col.df
 		if df.fieldtype == "Select":
-			select_options = [d for d in (df.options or '').split('\n') if d]
+			select_options = get_select_options(df)
 			if select_options and value not in select_options:
 				options_string = ", ".join([frappe.bold(d) for d in select_options])
 				msg = _("Value must be one of {0}").format(options_string)
@@ -971,12 +971,23 @@ class Column:
 			# guess date format
 			self.date_format = self.guess_date_format_for_column()
 			if not self.date_format:
-				self.date_format = '%Y-%m-%d'
-				self.warnings.append({
-					'col': self.column_number,
-					'message': _("Date format could not determined from the values in this column. Defaulting to yyyy-mm-dd."),
-					'type': 'info'
-				})
+		elif self.df.fieldtype == "Select":
+			options = get_select_options(self.df)
+			if options:
+				values = list(set([cstr(v) for v in self.column_values[1:] if v]))
+				invalid = list(set(values) - set(options))
+				if invalid:
+					valid_values = ", ".join([frappe.bold(o) for o in options])
+					invalid_values = ", ".join([frappe.bold(i) for i in invalid])
+					self.warnings.append(
+						{
+							"col": self.column_number,
+							"message": (
+								"The following values are invalid: {0}. Values must be"
+								" one of {1}".format(invalid_values, valid_values)
+							),
+						}
+					)
 
 	def as_dict(self):
 		d = frappe._dict()
@@ -1170,3 +1181,7 @@ def df_as_json(df):
 		'parent': df.parent,
 		'default': df.default
 	}
+
+
+def get_select_options(df):
+	return [d for d in (df.options or "").split("\n") if d]
