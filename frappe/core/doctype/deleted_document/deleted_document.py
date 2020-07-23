b0b53e03f6a602c5cddcf2c3de5db580dba197af
Gavin D'souza <gavin18d@gmail.com>
2020-07-23 19:01:18 +0530

    feat: Add Bulk restore API

--- frappe/core/doctype/deleted_document/deleted_document.py
+++ frappe/core/doctype/deleted_document/deleted_document.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe, json
+from frappe.desk.doctype.bulk_update.bulk_update import show_progress
 from frappe.model.document import Document
 from frappe import _
 
@@ -11,9 +12,14 @@ class DeletedDocument(Document):
 	pass
 
 @frappe.whitelist()
-def restore(name):
+def restore(name, alert=True):
 	deleted = frappe.get_doc('Deleted Document', name)
+
+	if deleted.restored:
+		frappe.throw(_("Document {0} Already Restored".format(name)), exc=frappe.DocumentAlreadyRestored)
+
 	doc = frappe.get_doc(json.loads(deleted.data))
+
 	try:
 		doc.insert()
 	except frappe.DocstatusTransitionError:
@@ -27,4 +33,28 @@ def restore(name):
 	deleted.restored = 1
 	deleted.db_update()
 
-	frappe.msgprint(_('Document Restored'))
+	if alert:
+		frappe.msgprint(_('Document Restored'))
+
+
+@frappe.whitelist()
+def bulk_restore(docnames):
+	docnames = frappe.parse_json(docnames)
+	doctype = "Deleted Document"
+	restored = []
+	failed = []
+
+	for i, d in enumerate(docnames):
+		try:
+			restore(d, alert=False)
+			message = _('Restoring {0}').format(doctype)
+			frappe.db.commit()
+			show_progress(docnames, message, i+1, d)
+			restored.append(d)
+		except frappe.DocumentAlreadyRestored:
+			failed.append(d)
+		except Exception as e:
+			failed.append(d)
+			frappe.db.rollback()
+
+	return restored
