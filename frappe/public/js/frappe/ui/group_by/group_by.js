31d63bc2526a141cef76cba3bfe78804a6dea3f3
prssanna <prssud@gmail.com>
2020-07-23 16:05:02 +0530

    refactor: card layout for kanban

--- frappe/public/js/frappe/ui/group_by/group_by.js
+++ frappe/public/js/frappe/ui/group_by/group_by.js
@@ -233,7 +233,7 @@ frappe.ui.GroupBy = class {
 	make_group_by_button() {
 		this.page.wrapper.find('.sort-selector').before(
 			$(`<div class="group-by-selector">
-				<button class="btn btn-default btn-xs group-by-button ellipsis">
+				<button class="btn btn-default btn-sm group-by-button ellipsis">
 					<span class="group-by-icon">
 						<svg class="icon icon-sm">
 							<use xlink:href="#icon-group-by"></use>
