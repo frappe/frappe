156ca5baf13097a1906638f7945a234022a56e63
Shivam Mishra <scm.mymail@gmail.com>
2020-07-24 19:03:17 +0530

    fix: update help text

--- frappe/commands/site.py
+++ frappe/commands/site.py
@@ -274,7 +274,7 @@ def disable_user(context, email):
 @click.command('migrate')
 @click.option('--rebuild-website', help="Rebuild webpages after migration")
 @click.option('--skip-failing', is_flag=True, help="Skip patches that fail to run")
-@click.option('--skip-search-index', is_flag=True, help="Skip patches that fail to run")
+@click.option('--skip-search-index', is_flag=True, help="Skip search indexing for web documents")
 @pass_context
 def migrate(context, rebuild_website=False, skip_failing=False, skip_search_index=False):
 	"Run patches, sync schema and rebuild files/translations"
