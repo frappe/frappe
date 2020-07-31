2440e72be24f14e934fdb093f73c825cd277ac9a
Gavin D'souza <gavin18d@gmail.com>
2020-07-31 16:07:51 +0530

    feat(logger): allow max_size and file_count params
    (cherry picked from commit 94116ae93c416958619e98aa47ad979481ec9dd1)

--- frappe/utils/logger.py
+++ frappe/utils/logger.py
@@ -16,14 +16,16 @@ import frappe
 default_log_level = logging.DEBUG
 
 
-def get_logger(module, with_more_info=False, allow_site=True, filter=None):
+def get_logger(module, with_more_info=False, allow_site=True, filter=None, max_size=100_000, file_count=20):
 	"""Application Logger for your given module
 
 	Args:
 		module (str): Name of your logger and consequently your log file.
 		with_more_info (bool, optional): Will log the form dict using the SiteContextFilter. Defaults to False.
 		allow_site ((str, bool), optional): Pass site name to explicitly log under it's logs. If True and unspecified, guesses which site the logs would be saved under. Defaults to True.
-		filter (function, optional): Add a filter function for your logger
+		filter (function, optional): Add a filter function for your logger. Defaults to None.
+		max_size (int, optional): Max file size of each log file in kB. Defaults to 100_000.
+		file_count (int, optional): Max count of log files to be retained via Log Rotation. Defaults to 20.
 
 	Returns:
 		<class 'logging.Logger'>: Returns a Python logger object with Site and Bench level logging capabilities.
@@ -36,10 +38,10 @@ def get_logger(module, with_more_info=False, allow_site=True, filter=None):
 	else:
 		site = False
 
-	LOGGER_NAME = "{}-{}".format(module, site or "all")
+	logger_name = "{0}-{1}".format(module, site or "all")
 
 	try:
-		return frappe.loggers[LOGGER_NAME]
+		return frappe.loggers[logger_name]
 	except KeyError:
 		pass
 
@@ -50,18 +52,18 @@ def get_logger(module, with_more_info=False, allow_site=True, filter=None):
 	logfile = module + ".log"
 	log_filename = os.path.join("..", "logs", logfile)
 
-	logger = logging.getLogger(LOGGER_NAME)
+	logger = logging.getLogger(logger_name)
 	logger.setLevel(frappe.log_level or default_log_level)
 	logger.propagate = False
 
 	formatter = logging.Formatter("%(asctime)s %(levelname)s {0} %(message)s".format(module))
-	handler = RotatingFileHandler(log_filename, maxBytes=100_000, backupCount=20)
+	handler = RotatingFileHandler(log_filename, maxBytes=max_size, backupCount=file_count)
 	handler.setFormatter(formatter)
 	logger.addHandler(handler)
 
 	if site:
-		SITELOG_FILENAME = os.path.join(site, "logs", logfile)
-		site_handler = RotatingFileHandler(SITELOG_FILENAME, maxBytes=100_000, backupCount=20)
+		sitelog_filename = os.path.join(site, "logs", logfile)
+		site_handler = RotatingFileHandler(sitelog_filename, maxBytes=max_size, backupCount=file_count)
 		site_handler.setFormatter(formatter)
 		logger.addHandler(site_handler)
 
@@ -71,7 +73,7 @@ def get_logger(module, with_more_info=False, allow_site=True, filter=None):
 	if filter:
 		logger.addFilter(filter)
 
-	frappe.loggers[LOGGER_NAME] = logger
+	frappe.loggers[logger_name] = logger
 
 	return logger
 
