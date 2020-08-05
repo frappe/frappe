cd1ab8e23c4a1de4bbd5137c34a864af53788237
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2020-08-05 19:41:41 +0530

    feat: Add validate_and_sanitize_search_inputs decorator (#11194)
    * feat: Add validate_and_sanitize_search_inputs decorator
    
    Co-authored-by: Nabin Hait <nabinhait@gmail.com>
    Co-authored-by: Prssanna Desai <prssud@gmail.com>
    Co-authored-by: Shivam Mishra <scmmishra@users.noreply.github.com>
    
    * refactor: Move validate_and_sanitize_search_inputs to init
    
    * refactor: Move validate_and_sanitize_search_inputs to search.py
    
    * test: validate_and_sanitize_search_inputs decorator
    
    * chore: Add wrapt module
    
    * refactor: Use @wrapt to define validate_and_sanitize_search_inputs decorator
    
    * test: Add a case to make sure frappe.call works as well
    
    Co-authored-by: Nabin Hait <nabinhait@gmail.com>
    Co-authored-by: Prssanna Desai <prssud@gmail.com>
    Co-authored-by: Shivam Mishra <scmmishra@users.noreply.github.com>

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -1707,3 +1707,7 @@ def mock(type, size=1, locale='en'):
 
 	from frappe.chat.util import squashify
 	return squashify(results)
+
+def validate_and_sanitize_search_inputs(fn):
+	from frappe.desk.search import validate_and_sanitize_search_inputs as func
+	return func(fn)
