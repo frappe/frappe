80733d4124b5ed5dc0a82d7a77b5f6593dd4b67b
Shridhar Patil <shridharpatil2792@gmail.com>
2020-08-07 21:52:04 +0530

    fix: raise incorrect site path error instead of sys.exit (#11206)

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -231,8 +231,7 @@ def get_site_config(sites_path=None, site_path=None):
 		if os.path.exists(site_config):
 			config.update(get_file_json(site_config))
 		elif local.site and not local.flags.new_site:
-			print("Site {0} does not exist".format(local.site))
-			sys.exit(1)
+			raise IncorrectSitePath("{0} does not exist".format(local.site))
 
 	return _dict(config)
 
