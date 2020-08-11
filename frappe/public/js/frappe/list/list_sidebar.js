8061b37748b1058bc04b643fa9c7ba1a7c7cff5d
Mathieu Brunot <mathieu.brunot@monogramm.io>
2020-08-11 17:10:02 +0200

    :art: Restore empty line

--- frappe/public/js/frappe/list/list_sidebar.js
+++ frappe/public/js/frappe/list/list_sidebar.js
@@ -219,6 +219,7 @@ frappe.views.ListSidebar = class ListSidebar {
 			let email_account = (account.email_id == "All Accounts") ? "All Accounts" : account.email_account;
 			let route = ["List", "Communication", "Inbox", email_account].join('/');
 			let display_name = ["All Accounts", "Sent Mail", "Spam", "Trash"].includes(account.email_id) ? __(account.email_id) : account.email_id;
+
 			if (!divider) {
 				this.get_divider().appendTo($dropdown);
 				divider = true;
