325d0e32c11f4dc31cd8fe6b8541132d56a118bf
AminovE99 <32329685+AminovE99@users.noreply.github.com>
2020-08-11 17:20:03 +0300

    :art: Changes from new requirements (python)

--- frappe/geo/utils.py
+++ frappe/geo/utils.py
@@ -4,17 +4,17 @@
 
 from __future__ import unicode_literals
 
+import json
+
 import frappe
 
 from pymysql import InternalError
 
 
-
-
 @frappe.whitelist()
 def get_coords(doctype, filters, type):
     '''Get list of coordinates in form
-    returns {name, location} with location being a geojson string'''
+    returns {names: ['latitude', 'longitude']} or location type'''
     filters_sql = get_coords_conditions(doctype, filters)[4:]
     out = None
     if type == 'coordinates':
@@ -23,18 +23,34 @@ def get_coords(doctype, filters, type):
         out = return_location(doctype, filters_sql)
     return out
 
+
+def convert_to_geo_json(coords_list):
+    handled_geo_json_dict = []
+    for element in coords_list:
+        handled_geo_json = json.loads(element['location'])
+        for coord in handled_geo_json['features']:
+            coord['properties']['name'] = element['name']
+            handled_geo_json_dict.append(coord.copy())
+            print(handled_geo_json['features'])
+    handled_geo_json = {"type": "FeatureCollection", "features": handled_geo_json_dict}
+    return handled_geo_json
+
+
 def return_location(doctype, filters_sql):
     if filters_sql:
         try:
-            coords = frappe.db.sql("""SELECT * FROM `tab{}`  WHERE {}""".format(doctype, filters_sql), as_dict=True)
+            coords = frappe.db.sql("""SELECT name,location FROM `tab{}`  WHERE {}""".format(doctype, filters_sql), as_dict=True)
         except InternalError:
-            frappe.msgprint(frappe._('This Doctype did not contain location field'))
+            frappe.msgprint(frappe._('This Doctype did not contains latitude and longitude fields'))
             return
     else:
-        coords = frappe.get_all(doctype, fields = ['name', 'location'])
-    return coords
+        coords = frappe.get_all(doctype, fields=['location', 'name'])
+    handled_geo_json = convert_to_geo_json(coords)
+    return handled_geo_json
+
 
 def return_coordinates(doctype, filters_sql):
+    handled_geo_json = {"type": "FeatureCollection", "features": None}
     if filters_sql:
         try:
             coords = frappe.db.sql("""SELECT * FROM `tab{}`  WHERE {}""".format(doctype, filters_sql), as_dict=True)
@@ -43,12 +59,15 @@ def return_coordinates(doctype, filters_sql):
             return
     else:
         coords = frappe.get_all(doctype, fields=['name', 'latitude', 'longitude'])
-    out = frappe._dict()
+    out_list = []
     for i in coords:
-        out[i.name] = out.get(i.docname, [])
-        out[i.name].append(i.latitude)
-        out[i.name].append(i.longitude)
-    return out
+        node = {"type": "Feature", "properties": {}, "geometry": {"type": "Point", "coordinates": None}}
+        node['properties']['name'] = i.name
+        node['geometry']['coordinates'] = [i.latitude, i.longitude]
+        out_list.append(node.copy())
+    handled_geo_json['features'] = out_list
+    print(handled_geo_json)
+    return handled_geo_json
 
 
 def get_coords_conditions(doctype, filters=None):
