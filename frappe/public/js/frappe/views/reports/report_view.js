ca2f8174425d26785a0978f649b5944496a551b0
prssanna <prssud@gmail.com>
2020-08-12 16:45:28 +0530

    style: missing semicolon
    (cherry picked from commit 9717ba3907f8bde956ef5699072d8b48b5a014d9)

--- frappe/public/js/frappe/views/reports/report_view.js
+++ frappe/public/js/frappe/views/reports/report_view.js
@@ -945,7 +945,7 @@ frappe.views.ReportView = class ReportView extends frappe.views.ListView {
 				label: "Index",
 				fieldtype: "Int",
 				parent: doctype,
-			}
+			};
 		}
 
 		if (!docfield) {
