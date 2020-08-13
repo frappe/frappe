a8f02550157edaeba91705386eb3a06d57e513fb
Suraj Shetty <surajshetty3416@gmail.com>
2020-08-13 15:31:19 +0530

    fix: Comment box reference

--- frappe/public/js/frappe/form/footer/footer.js
+++ frappe/public/js/frappe/form/footer/footer.js
@@ -6,26 +6,24 @@ import './new_timeline.js';
 
 frappe.ui.form.Footer = Class.extend({
 	init: function(opts) {
-		var me = this;
 		$.extend(this, opts);
 		this.make();
 		this.make_comment_box();
 		this.make_timeline();
 		// render-complete
-		$(this.frm.wrapper).on("render_complete", function() {
-			me.refresh();
+		$(this.frm.wrapper).on("render_complete", () => {
+			this.refresh();
 		});
 	},
 	make: function() {
-		var me = this;
 		this.wrapper = $(frappe.render_template("form_footer", {}))
 			.appendTo(this.parent);
-		this.wrapper.find(".btn-save").click(function() {
-			me.frm.save('Save', null, this);
+		this.wrapper.find(".btn-save").click(() => {
+			this.frm.save('Save', null, this);
 		});
 	},
 	make_comment_box: function() {
-		this.comment_box = frappe.ui.form.make_control({
+		this.frm.comment_box = frappe.ui.form.make_control({
 			parent: this.wrapper.find(".comment-box"),
 			render_input: true,
 			only_input: true,
@@ -43,7 +41,7 @@ frappe.ui.form.Footer = Class.extend({
 						comment_email: frappe.session.user,
 						comment_by: frappe.session.user_fullname
 					}).then(() => {
-						this.comment_box.set_value('');
+						this.frm.comment_box.set_value('');
 						frappe.utils.play_sound("click");
 						this.frm.timeline.refresh();
 					});
