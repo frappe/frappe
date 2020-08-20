99496d97f3e00284840d2127556bba0e21d1a99e
Gavin D'souza <gavin18d@gmail.com>
2020-08-20 18:20:02 +0530

    test: Add tests for bench execute

--- frappe/tests/test_commands.py
+++ frappe/tests/test_commands.py
@@ -23,3 +23,21 @@ class BaseTestCommands:
 		self.stdout = clean(self._proc.stdout)
 		self.stderr = clean(self._proc.stderr)
 		self.returncode = clean(self._proc.returncode)
+
+
+class TestCommands(BaseTestCommands, unittest.TestCase):
+	def test_execute(self):
+		# execute a command expecting a numeric output
+		self.execute("bench --site {site} execute frappe.db.get_database_size")
+		self.assertEquals(self.returncode, 0)
+		self.assertIsInstance(float(self.stdout), float)
+
+		# execute a command expecting an errored output as local won't exist
+		self.execute("bench --site {site} execute frappe.local.site")
+		self.assertEquals(self.returncode, 1)
+		self.assertIsNotNone(self.stderr)
+
+		# execute a command with kwargs
+		self.execute("""bench --site {site} execute frappe.bold --kwargs '{{"text": "DocType"}}'""")
+		self.assertEquals(self.returncode, 0)
+		self.assertEquals(self.stdout[1:-1], frappe.bold(text='DocType'))
