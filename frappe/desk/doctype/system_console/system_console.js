12be3d26ff26fa798000504547325251e474820a
Rushabh Mehta <rmehta@gmail.com>
2020-08-21 15:03:57 +0530

    fix(minor): primary button

--- frappe/desk/doctype/system_console/system_console.js
+++ frappe/desk/doctype/system_console/system_console.js
@@ -2,7 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('System Console', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.disable_save();
+		frm.page.set_primary_action(__("Execute"), () => {
+			frm.execute_action('Execute');
+		});
+	}
 });
