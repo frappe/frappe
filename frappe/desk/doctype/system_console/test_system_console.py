39e36e01cde80e37ba0e758f60b4492700dbd177
Rushabh Mehta <rmehta@gmail.com>
2020-08-25 15:09:33 +0530

    fix(test): test for system console;

--- frappe/desk/doctype/system_console/test_system_console.py
+++ frappe/desk/doctype/system_console/test_system_console.py
@@ -3,8 +3,18 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
 import unittest
 
 class TestSystemConsole(unittest.TestCase):
-	pass
+	def test_system_console(self):
+		system_console = frappe.get_doc('System Console')
+		system_console.console = 'log("hello")'
+		system_console.run()
+
+		self.assertEqual(system_console.output, 'hello')
+
+		system_console.console = 'log(frappe.db.get_value("DocType", "DocType", "module"))'
+		system_console.run()
+
+		self.assertEqual(system_console.output, 'Core')
