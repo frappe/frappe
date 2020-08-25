938b87ee466e9d3dd81494ee26c31e64fbbb7a90
Rushabh Mehta <rmehta@gmail.com>
2020-08-25 15:44:08 +0530

    fix(minor): rename field, ignore for single doctype

--- frappe/model/utils/rename_field.py
+++ frappe/model/utils/rename_field.py
@@ -19,7 +19,7 @@ def rename_field(doctype, old_fieldname, new_fieldname):
 		print("rename_field: " + (new_fieldname) + " not found in " + doctype)
 		return
 
-	if not frappe.db.has_column(doctype, old_fieldname):
+	if not meta.issingle and not frappe.db.has_column(doctype, old_fieldname):
 		print("rename_field: " + (old_fieldname) + " not found in table for: " + doctype)
 		# never had the field?
 		return
