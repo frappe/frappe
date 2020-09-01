eaf5c5aeb8ab7311f2631889cd0bd4def9e30aed
prssanna <prssud@gmail.com>
2020-09-01 13:03:15 +0530

    fix: kanban indicator gray color

--- frappe/public/js/frappe/views/kanban/kanban_board.js
+++ frappe/public/js/frappe/views/kanban/kanban_board.js
@@ -777,7 +777,7 @@ frappe.provide("frappe.views");
 			});
 			if(!indicators) {
 				//
-				indicators = ['green', 'blue', 'orange', 'grey'];
+				indicators = ['green', 'blue', 'orange', 'gray'];
 			}
 			callback(indicators);
 		});
