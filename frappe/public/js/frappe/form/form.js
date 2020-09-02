25c5741b35c637cc9052d089fe0fbfb33d376d58
prssanna <prssud@gmail.com>
2020-09-02 19:04:21 +0530

    fix: set perm in set_read_only only if object is not empty

--- frappe/public/js/frappe/form/form.js
+++ frappe/public/js/frappe/form/form.js
@@ -1261,13 +1261,16 @@ frappe.ui.form.Form = class FrappeForm {
 		var docperms = frappe.perm.get_perm(this.doc.doctype);
 		for (var i=0, l=docperms.length; i<l; i++) {
 			var p = docperms[i];
-			perm[p.permlevel || 0] = {
-				read: p.read,
-				cancel: p.cancel,
-				share: p.share,
-				print: p.print,
-				email: p.email
-			};
+			if (Object.keys(p).length !== 0) {
+				perm[p.permlevel || 0] = {
+					read: p.read,
+					cancel: p.cancel,
+					share: p.share,
+					print: p.print,
+					email: p.email
+				};
+			}
+
 		}
 		this.perm = perm;
 	}
