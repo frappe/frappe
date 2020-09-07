5248309a362a8b791a1e4dbc47581ddab63e0011
Saurabh <saurabh6790@gmail.com>
2020-09-07 13:47:48 +0530

    Test Cases

--- frappe/tests/test_client.py
+++ frappe/tests/test_client.py
@@ -21,3 +21,28 @@ class TestClient(unittest.TestCase):
 
 		self.assertFalse(frappe.db.exists("ToDo", todo.name))
 		self.assertRaises(frappe.DoesNotExistError, delete, "ToDo", todo.name)
+
+	def test_http_valid_method_access(self):
+		from frappe.client import delete
+		from frappe.handler import execute_cmd
+
+		frappe.local.request = frappe._dict()
+		frappe.local.request.method = 'POST'
+
+		frappe.form_dict = dict(doc=(dict(doctype='ToDo', description='Valid http method')))
+		todo = execute_cmd('frappe.client.save')
+
+		self.assertEqual(todo.get('description'), 'Valid http method')
+
+		delete("ToDo", todo.name)
+
+	def test_http_invalid_method_access(self):
+		from frappe.client import delete
+		from frappe.handler import execute_cmd
+
+		frappe.local.request = frappe._dict()
+		frappe.local.request.method = 'GET'
+
+		frappe.form_dict = dict(doc=(dict(doctype='ToDo', description='Invalid http method')))
+
+		self.assertRaises(frappe.PermissionError, execute_cmd, 'frappe.client.save')
