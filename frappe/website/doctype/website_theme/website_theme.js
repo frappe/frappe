9673a96ebcd2e73cc6cc27c2c647aa66cff81e9f
Faris Ansari <netchamp.faris@gmail.com>
2020-09-22 08:54:48 +0530

    fix: style

--- frappe/website/doctype/website_theme/website_theme.js
+++ frappe/website/doctype/website_theme/website_theme.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // MIT License. See license.txt
 
+frappe.ui.form.on("Website Theme", {
 	onload_post_render(frm) {
 		frm.events.make_app_theme_selector(frm);
 	},
@@ -9,6 +10,7 @@
 		frm.clear_custom_buttons();
 		frm.toggle_display(["module", "custom"], !frappe.boot.developer_mode);
 
+		frm.trigger("set_default_theme_button_and_indicator");
 		frm.trigger("make_app_theme_selector");
 
 		if (!frm.doc.custom && !frappe.boot.developer_mode) {
@@ -20,14 +22,28 @@
 	},
 
 	set_default_theme_button_and_indicator(frm) {
-		frappe.db.get_single_value('Website Settings', 'website_theme')
+		frappe.db
+			.get_single_value("Website Settings", "website_theme")
 			.then(value => {
 				if (value === frm.doc.name) {
-					frm.page.set_indicator(__('Default Theme'), 'green');
+					frm.page.set_indicator(__("Default Theme"), "green");
 				} else {
 					frm.page.clear_indicator();
 					// show set as default button
 					if (!frm.is_new() && !frm.is_dirty()) {
+						frm.add_custom_button(
+							__("Set as Default Theme"),
+							() => {
+								frm.call("set_as_default").then(() =>
+									frm.trigger("refresh")
+								);
+							}
+						);
+					}
+				}
+			});
+	},
+
 	make_app_theme_selector(frm) {
 		if (frm.app_theme_selector) {
 			frm.events.get_installed_apps(frm).then(apps => {
