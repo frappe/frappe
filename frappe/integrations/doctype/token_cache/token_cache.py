89c056998e6a19a104a4a518c47d20cbfeb4e1eb
barredterra <14891507+barredterra@users.noreply.github.com>
2020-09-27 19:49:22 +0200

    fix(Token Cache): use get_password

--- frappe/integrations/doctype/token_cache/token_cache.py
+++ frappe/integrations/doctype/token_cache/token_cache.py
@@ -78,8 +78,8 @@ class TokenCache(Document):
 
 	def get_json(self):
 		return {
-			'access_token': self.access_token,
-			'refresh_token': self.refresh_token,
+			'access_token': self.get_password('access_token'),
+			'refresh_token': self.get_password('refresh_token'),
 			'expires_in': self.get_expires_in(),
 			'token_type': self.token_type
 		}
