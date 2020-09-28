f8167b4364de1613a819b1722c6672f4be2e1fd8
Faris Ansari <netchamp.faris@gmail.com>
2020-09-28 17:26:36 +0530

    fix: Website Sidebar get_items utility method

--- frappe/website/doctype/website_sidebar/website_sidebar.py
+++ frappe/website/doctype/website_sidebar/website_sidebar.py
@@ -6,5 +6,28 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+
 class WebsiteSidebar(Document):
-	pass
+	def get_items(self):
+		items = frappe.get_all(
+			"Website Sidebar Item",
+			filters=dict(parent=self.name),
+			fields=["title", "route", "`group`"],
+			order_by="idx asc",
+		)
+
+		items_by_group = {}
+		items_without_group = []
+		for item in items:
+			if item.group:
+				items_by_group[item.group] = items_by_group.get(item.group, [])
+				items_by_group[item.group].append(item)
+			else:
+				items_without_group.append(item)
+
+		out = []
+		for group, items in items_by_group.items():
+			out.append({"group_title": group, "group_items": items})
+
+		out += items_without_group
+		return out
