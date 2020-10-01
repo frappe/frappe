ce1b6bde25cdde7b03302dfa7aaeb100027c0155
prssanna <prssud@gmail.com>
2020-10-01 18:08:37 +0530

    feat: buttons to fetch assignment days in table

--- frappe/automation/doctype/assignment_rule/assignment_rule.js
+++ frappe/automation/doctype/assignment_rule/assignment_rule.js
@@ -5,41 +5,75 @@ frappe.ui.form.on('Assignment Rule', {
 	onload: (frm) => {
 		frm.trigger('set_due_date_field_options');
 	},
+	setup: function(frm) {
+		frm.trigger('setup_assignment_days_buttons');
+	},
 	refresh: function(frm) {
 		// refresh description
 		frm.events.rule(frm);
+		frm.trigger('set_field_options');
 	},
 
 	document_type: function(frm) {
 		frm.trigger('set_field_options');
+		frm.trigger('set_due_date_field_options');
+	},
+
+	setup_assignment_days_buttons: function(frm) {
+		const labels = ['Weekends', 'Weekdays', 'All Days'];
+		let get_days = (label) => {
+			const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+			const weekends = ['Saturday', 'Sunday'];
+			return {
+				'All Days': weekdays.concat(weekends),
+				'Weekdays': weekdays,
+				'Weekends': weekends,
+			}[label];
+		};
+
+		let get_button_html = (label) => `<button
+			class="btn btn-xs btn-default pull-right"
+			style="margin-bottom: 10px; margin-right: 5px">${__(
+			label
+		)}</button>`;
+
+		const $wrapper = frm.get_field('assignment_days').$wrapper;
+		$(`<div class="fetch-days-buttons">
+				${labels.map(get_button_html).join('')}
+		</div>`).prependTo($wrapper);
+
+		frm.$wrapper.find('.fetch-days-buttons').on('click', '.btn', (e) => {
+			frm.clear_table('assignment_days');
+			const label = $(e.currentTarget).text();
+			get_days(label).forEach((day) =>
+				frm.add_child('assignment_days', { day: day })
+			);
+			frm.refresh_field('assignment_days');
+		});
 	},
 
 	rule: function(frm) {
 		const description_map = {
 			'Round Robin': __('Assign one by one, in sequence'),
 			'Load Balancing': __('Assign to the one who has the least assignments'),
-			'Based on Field': __('Assign to the user set in this field')
-		}
+			'Based on Field': __('Assign to the user set in this field'),
+		};
 		frm.get_field('rule').set_description(description_map[frm.doc.rule]);
 	},
 
 	set_field_options(frm) {
 		let doctype = frm.doc.document_type;
-		let user_link_fields = [{label: 'Owner', value: 'owner'}];
+		let user_link_fields = [{ label: 'Owner', value: 'owner' }];
 		if (doctype) {
 			frappe.model.with_doctype(doctype, () => {
-				// get all date and datetime fields
-				frappe.get_meta(doctype).fields.map(df => {
+				frappe.get_meta(doctype).fields.map((df) => {
 					if (df.fieldtype == 'Link' && df.options == 'User') {
-						user_link_fields.push({label: df.label, value: df.fieldname});
+						user_link_fields.push({ label: df.label, value: df.fieldname });
 					}
 				});
 				frm.set_df_property('field', 'options', user_link_fields);
 			});
 		}
-	},
-	document_type: (frm) => {
-		frm.trigger('set_due_date_field_options');
 	},
 	set_due_date_field_options: (frm) => {
 		let doctype = frm.doc.document_type;
