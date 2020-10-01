51d50ff7afab78891a2071ed77d7570c54eb3c3e
prssanna <prssud@gmail.com>
2020-10-01 16:25:35 +0530

    feat: rule in assignment rule to assign based on field value

--- frappe/automation/doctype/assignment_rule/assignment_rule.js
+++ frappe/automation/doctype/assignment_rule/assignment_rule.js
@@ -9,11 +9,33 @@ frappe.ui.form.on('Assignment Rule', {
 		// refresh description
 		frm.events.rule(frm);
 	},
+
+	document_type: function(frm) {
+		frm.trigger('set_field_options');
+	},
+
 	rule: function(frm) {
-		if (frm.doc.rule === 'Round Robin') {
-			frm.get_field('rule').set_description(__('Assign one by one, in sequence'));
-		} else {
-			frm.get_field('rule').set_description(__('Assign to the one who has the least assignments'));
+		const description_map = {
+			'Round Robin': __('Assign one by one, in sequence'),
+			'Load Balancing': __('Assign to the one who has the least assignments'),
+			'Based on Field': __('Assign to the user set in this field')
+		}
+		frm.get_field('rule').set_description(description_map[frm.doc.rule]);
+	},
+
+	set_field_options(frm) {
+		let doctype = frm.doc.document_type;
+		let user_link_fields = [{label: 'Owner', value: 'owner'}];
+		if (doctype) {
+			frappe.model.with_doctype(doctype, () => {
+				// get all date and datetime fields
+				frappe.get_meta(doctype).fields.map(df => {
+					if (df.fieldtype == 'Link' && df.options == 'User') {
+						user_link_fields.push({label: df.label, value: df.fieldname});
+					}
+				});
+				frm.set_df_property('field', 'options', user_link_fields);
+			});
 		}
 	},
 	document_type: (frm) => {
