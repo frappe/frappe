992f66e044458408a19eab2504ba2f0c262c6122
Gavin D'souza <gavin18d@gmail.com>
2020-10-12 18:05:04 +0530

    test: Use standard distinct

--- frappe/tests/test_db.py
+++ frappe/tests/test_db.py
@@ -134,8 +134,8 @@ class TestDB(unittest.TestCase):
 		self.assertEqual(list(frappe.get_all("ToDo", fields=["{0} as total".format(random_field)], limit=1)[0])[0], "total")
 
 		# Testing read for distinct and sql functions
-		self.assertEqual(list(frappe.get_all("ToDo", fields=["distinct `{0}` as total".format(random_field)], limit=1)[0])[0], "total")
-		self.assertEqual(list(frappe.get_all("ToDo", fields=["distinct `{0}`".format(random_field)], limit=1)[0])[0], random_field)
+		self.assertEqual(list(frappe.get_all("ToDo", fields=["`{0}` as total".format(random_field)], limit=1, distinct=True)[0])[0], "total")
+		self.assertEqual(list(frappe.get_all("ToDo", fields=["`{0}`".format(random_field)], limit=1, distinct=True)[0])[0], random_field)
 		self.assertEqual(list(frappe.get_all("ToDo", fields=["count(`{0}`)".format(random_field)], limit=1)[0])[0], "count(`{0}`)".format(random_field))
 
 		# Testing update
