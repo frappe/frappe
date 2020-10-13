f7e45d1acddd76fac171ad7ee290663f47efc8ef
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2020-10-13 15:28:06 +0200

    fix: show headline instead of save

--- frappe/website/doctype/web_template/web_template.js
+++ frappe/website/doctype/web_template/web_template.js
@@ -16,7 +16,7 @@ frappe.ui.form.on('Web Template', {
 			// the next save. Changes will get overwritten from the backend
 			// on save and should not be possible in the UI.
 			frm.toggle_display('template', false);
-			frm.save();
+			frm.dashboard.set_headline(__('Please save to edit the template.'));
 		}
 	}
 });
