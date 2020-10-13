3bec678aab6a7782711b67f4865c29defd266da4
Shivam Mishra <scm.mymail@gmail.com>
2020-10-13 15:27:16 +0530

    feat: allow custom class in widget group

--- frappe/public/js/frappe/views/workspace/workspace.js
+++ frappe/public/js/frappe/views/workspace/workspace.js
@@ -358,6 +358,7 @@ class DesktopPage {
 				container: this.page,
 				type: "chart",
 				columns: 1,
+				class_name: "widget-charts",
 				hidden: Boolean(this.onboarding_widget),
 				options: {
 					allow_sorting: this.allow_customization,
