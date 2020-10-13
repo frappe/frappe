6f0104b67e4d2734050f7dc1de279dd9824c7ead
Rushabh Mehta <rmehta@gmail.com>
2020-10-13 13:15:14 +0530

    fix(minor): conflict

--- frappe/patches/v13_0/web_template_set_module.py
+++ frappe/patches/v13_0/web_template_set_module.py
@@ -6,14 +6,9 @@ import frappe
 
 def execute():
 	"""Set default module for standard Web Template, if none."""
-<<<<<<< d5ee3032d494ed35a409a36116679a3d3cb96103
 	frappe.reload_doc('website', 'doctype', 'Web Template Field')
 	frappe.reload_doc('website', 'doctype', 'web_template')
 
-=======
-	frappe.reload_doc('website', 'doctype', 'Web Template')
-	frappe.reload_doc('website', 'doctype', 'Web Template Field')
->>>>>>> fix(minor): fix fieldame in apply_property_setters in model/meta.py
 	standard_templates = frappe.get_list('Web Template', {'standard': 1})
 	for template in standard_templates:
 		doc = frappe.get_doc('Web Template', template.name)
