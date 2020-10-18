8a8e898ccb9b3ce889e5b5eb4a350391c6960379
Manuel <57345036+mtraeber@users.noreply.github.com>
2020-10-18 08:25:54 +0200

    fix: Copy incoming_port from Email Domain to Email Account (#11558)
    When changing an Email Domain, the configuration fields are copied
    to all Email Accounts using this domain. This happens in the method
    `on_update`
    
    - Include `incoming_port` when copying fields
    - Add test case for `on_update`
    
    Co-authored-by: mtraeber <mtraeber@linux-ag.com>

--- frappe/email/doctype/email_domain/email_domain.py
+++ frappe/email/doctype/email_domain/email_domain.py
@@ -69,7 +69,7 @@ class EmailDomain(Document):
 		for email_account in frappe.get_all("Email Account", filters={"domain": self.name}):
 			try:
 				email_account = frappe.get_doc("Email Account", email_account.name)
-				for attr in ["email_server", "use_imap", "use_ssl", "use_tls", "attachment_limit", "smtp_server", "smtp_port", "use_ssl_for_outgoing", "append_emails_to_sent_folder"]:
+				for attr in ["email_server", "use_imap", "use_ssl", "use_tls", "attachment_limit", "smtp_server", "smtp_port", "use_ssl_for_outgoing", "append_emails_to_sent_folder", "incoming_port"]:
 					email_account.set(attr, self.get(attr, default=0))
 				email_account.save()
 
