36e11719232bb87f9fda3a4a25213793b18a30fc
sezanzeb <proxima@hip70890b.de>
2020-10-27 18:22:49 +0100

    style: one more format string

--- frappe/utils/data.py
+++ frappe/utils/data.py
@@ -1129,7 +1129,7 @@ def get_link_to_report(name, label=None, report_type=None, doctype=None, filters
 				for value in v:
 					conditions.append(f'{k}=["{value[0]}","{value[1]}"])')
 			else:
-				conditions.append(str(k)+"="+str(v))
+				conditions.append(f'{k}={v}')
 
 		filters = "&".join(conditions)
 
