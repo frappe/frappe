e230ddf4d3408adbe9a31fa1638ac229504bd3d5
prssanna <prssud@gmail.com>
2020-11-12 13:43:03 +0530

    fix: allow any field to be set in based on field

--- frappe/automation/doctype/assignment_rule/assignment_rule.js
+++ frappe/automation/doctype/assignment_rule/assignment_rule.js
@@ -57,7 +57,7 @@ frappe.ui.form.on('Assignment Rule', {
 		frm.set_fields_as_options(
 			'field',
 			doctype,
-			(df) => df.fieldtype == 'Link' && df.options == 'User',
+			() => true,
 			[{ label: 'Owner', value: 'owner' }]
 		);
 		if (doctype) {
