b17f2e6f8174466a7e5b995b3c5ad2b0ca75e52f
Fisher Yu <12823863+szufisher@users.noreply.github.com>
2020-11-13 21:07:20 +0800

    Update user.js

--- frappe/core/doctype/user/user.js
+++ frappe/core/doctype/user/user.js
@@ -267,7 +267,7 @@ frappe.ModuleEditor = Class.extend({
 		this.frm.doc.__onload.all_modules.forEach(function(m) {
 			$(repl('<div class="col-sm-6"><div class="checkbox">\
 				<label><input type="checkbox" class="block-module-check" data-module="%(module)s">\
-				%(module)s</label></div></div>', {module: m})).appendTo(me.wrapper);
+				%(module)s</label></div></div>', {module: __(m)})).appendTo(me.wrapper);
 		});
 		this.bind();
 	},
