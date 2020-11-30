7d4992549d69bc5485dad51cf75fa60f6143fcc5
Abhishek Balam <abhishekbalam96@gmail.com>
2020-11-30 17:19:35 +0530

    feat: added option in system settings to remove exif tags, default 1

--- frappe/core/doctype/file/file.py
+++ frappe/core/doctype/file/file.py
@@ -470,8 +470,9 @@ class File(Document):
 		
 		self.content_type = mimetypes.guess_type(self.file_name)[0]
 		
-		if self.content_type == "image":
-			self.content = strip_exif_data(self.content)			
+		if self.content_type and "image" in self.content_type and \
+			frappe.get_system_settings("remove_exif_tags"):
+			self.content = strip_exif_data(self.content, self.content_type)			
 
 		self.file_size = self.check_max_file_size()
 		self.content_hash = get_content_hash(self.content)
