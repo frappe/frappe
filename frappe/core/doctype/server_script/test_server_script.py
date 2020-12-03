9ac14fb5abf6930234e4da41217dd74718c858da
Faris Ansari <netchamp.faris@gmail.com>
2020-12-03 23:26:34 +0530

    feat: Permission Query script type
    Add dynamic conditions in where clause of get_list query

--- frappe/core/doctype/server_script/test_server_script.py
+++ frappe/core/doctype/server_script/test_server_script.py
@@ -46,6 +46,14 @@ frappe.response['message'] = 'hello'
 		script = '''
 frappe.flags = 'hello'
 '''
+	),
+	dict(
+		name='test_permission_query',
+		script_type = 'Permission Query',
+		reference_doctype = 'ToDo',
+		script = '''
+conditions = '1 = 1'
+'''
 	)
 ]
 class TestServerScript(unittest.TestCase):
@@ -85,3 +93,7 @@ class TestServerScript(unittest.TestCase):
 
 	def test_api_return(self):
 		self.assertEqual(frappe.get_doc('Server Script', 'test_return_value').execute_method(), 'hello')
+
+	def test_permission_query(self):
+		self.assertTrue('where (1 = 1)' in frappe.db.get_list('ToDo', return_query=1))
+		self.assertTrue(isinstance(frappe.db.get_list('ToDo'), list))
