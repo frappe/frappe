f75b2c98bcaa5fe3e3c9359f8c7321e6f5f8a8e5
Suraj Shetty <surajshetty3416@gmail.com>
2020-12-18 17:55:38 +0530

    fix: Bind user profile "show" to page "show"

--- frappe/desk/page/user_profile/user_profile.js
+++ frappe/desk/page/user_profile/user_profile.js
@@ -1,9 +1,12 @@
 frappe.pages['user-profile'].on_page_load = function (wrapper) {
+	frappe.ui.make_app_page({
+		parent: wrapper,
+	});
 	frappe.require('assets/js/user_profile_controller.min.js', () => {
-		frappe.ui.make_app_page({
-			parent: wrapper,
-		});
 		let user_profile = new frappe.ui.UserProfile(wrapper);
 		user_profile.show();
+		$(wrapper).bind('show', () => {
+			user_profile.show();
+		});
 	});
 };
