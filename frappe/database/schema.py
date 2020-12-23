bcbe16236719ca2c7d70c85b052ab0de7c0f6d23
Chinmay Pai <chinmaydpai@gmail.com>
2020-12-24 00:33:50 +0530

    chore: change an instance of virtual_doctype to is_virtual

--- frappe/database/schema.py
+++ frappe/database/schema.py
@@ -30,7 +30,7 @@ class DBTable:
 		self.get_columns_from_docfields()
 
 	def sync(self):
-		if self.meta.get('virtual_doctype'):
+		if self.meta.get('is_virtual'):
 			# no schema to sync for virtual doctypes
 			return
 		if self.is_new():
