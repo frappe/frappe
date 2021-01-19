83822b14c6e6aded305c00fefee679833c4ee2ab
shariquerik <sharique.rik@gmail.com>
2021-01-19 14:07:19 +0530

    fix: Updated if condition

--- frappe/public/js/frappe/form/controls/link.js
+++ frappe/public/js/frappe/form/controls/link.js
@@ -456,7 +456,7 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 				}
 
 				// check if value exist in the filtered dropdown values 
-				if (this.$input.cache[doctype][""] && !this.$input.cache[doctype][""].some(d => d.value === value)) {
+				if (this.$input.cache[doctype] && !this.$input.cache[doctype][""].some(d => d.value === value)) {
 					value = "";
 				}
 
