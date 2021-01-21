65a82bef1a770c37073e8e3a16ad0cb72c1ebb15
Suraj Shetty <surajshetty3416@gmail.com>
2021-01-21 12:15:18 +0530

    fix: Show brand logo if header is set

--- frappe/email/email_body.py
+++ frappe/email/email_body.py
@@ -255,7 +255,7 @@ def get_formatted_html(subject, message, footer=None, print_html=None,
 		email_account = get_outgoing_email_account(False, sender=sender)
 
 	rendered_email = frappe.get_template("templates/emails/standard.html").render({
-		"brand_logo": get_brand_logo(email_account) if with_container else None,
+		"brand_logo": get_brand_logo(email_account) if with_container or header else None,
 		"with_container": with_container,
 		"site_url": get_url(),
 		"header": get_header(header),
