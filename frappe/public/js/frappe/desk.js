78a8cd74d032646d48ae07150fd6257cddef8333
shariquerik <sharique.rik@gmail.com>
2021-03-09 15:22:22 +0530

    fix: sider fix

--- frappe/public/js/frappe/desk.js
+++ frappe/public/js/frappe/desk.js
@@ -202,7 +202,7 @@ frappe.Application = Class.extend({
 	email_password_prompt: function(email_account,user,i) {
 		var me = this;
 		let d = new frappe.ui.Dialog({
-	  		title: __('Password missing in Email Account'),
+			title: __('Password missing in Email Account'),
 			fields: [
 				{
 					'fieldname': 'password',
