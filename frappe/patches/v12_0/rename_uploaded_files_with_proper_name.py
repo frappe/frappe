71708a1154169f5eaae89371cc2ac5fe3d5a237b
Afshan <afshan13k@gmail.com>
2021-03-09 17:46:37 +0530

    fix: fixing patch

--- frappe/patches/v12_0/rename_uploaded_files_with_proper_name.py
+++ frappe/patches/v12_0/rename_uploaded_files_with_proper_name.py
@@ -3,21 +3,6 @@ import frappe
 
 def execute():
 
-	for f in frappe.get_all("File", filters={"is_folder": 0, "file_name": ["like", "%/%"]}, fields=['file_url', 'name', 'file_name']):
-		filename = f.file_name.rsplit('/', 1)[-1]
-
-		set_query = 'file_name = {0}'.format(filename)
-
-		if not f.file_url:
-			fileurl = f.file_name
-			set_query += ', file_url = {0}'.format(fileurl)
-
-		frappe.db.sql('''
-		update `tabFile` set {0} where name = {1}
-		'''.format(set_query, f.name))
-
-	frappe.db.commit()
-
 	for f in frappe.get_all("File", filters={"is_folder": 0, "file_url": ["is", "not set"], "file_name": ["like", "%/%"]}, fields=['name', 'file_name']):
 		fileurl = f.file_name
 		filename = f.file_name.rsplit('/', 1)[-1]
@@ -25,6 +10,15 @@ def execute():
 		frappe.db.sql('''
 		update `tabFile` set file_url = {0}, file_name = {1} where name = {2}
 		'''.format(fileurl, filename, f.name))
+
+	frappe.db.commit()
+
+	for f in frappe.get_all("File", filters={"is_folder": 0, "file_name": ["like", "%/%"]}, fields=['name', 'file_name']):
+		filename = f.file_name.rsplit('/', 1)[-1]
+
+		frappe.db.sql('''
+		update `tabFile` set file_name = {0} where name = {1}
+		'''.format(filename, f.name))
 
 	frappe.db.commit()
 
