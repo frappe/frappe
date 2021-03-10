b4eea9b9aca9d66381def94e7b2475aa83cc9c59
Afshan <afshan13k@gmail.com>
2021-03-10 14:37:58 +0530

    refact: patch refactor

--- frappe/patches/v12_0/rename_uploaded_files_with_proper_name.py
+++ frappe/patches/v12_0/rename_uploaded_files_with_proper_name.py
@@ -1,36 +1,78 @@
 import frappe
+import os
 
 
 def execute():
 
-	for f in frappe.get_all("File", filters={"is_folder": 0, "file_url": ["is", "not set"], "file_name": ["like", "%/%"]}, fields=['name', 'file_name']):
+	public_path = frappe.utils.get_files_path()
+	private_path = frappe.utils.get_files_path(is_private=1)
+
+	file_names_with_url_and_file_url_not_set = frappe.get_all("File", filters={
+		"is_folder": 0,
+		"file_url": ["is", "not set"],
+		"file_name": ["like", "%/%"]
+	}, fields=['name', 'file_name'])
+
+	for f in file_names_with_url_and_file_url_not_set:
 		fileurl = f.file_name
 		filename = f.file_name.rsplit('/', 1)[-1]
 
-		frappe.db.sql('''
-		update `tabFile` set file_url = {0}, file_name = {1} where name = {2}
-		'''.format(fileurl, filename, f.name))
+		# Check if file exists
+		path = os.path.join(public_path, filename)
+		if '/private/' in fileurl:
+			path = os.path.join(private_path, filename)
+
+		if not os.path.exists(path):
+			continue
+
+		frappe.db.set_values('File', f.name, {
+			"file_name": filename,
+			"file_url": fileurl
+		})
 
 	frappe.db.commit()
 
-	for f in frappe.get_all("File", filters={"is_folder": 0, "file_name": ["like", "%/%"]}, fields=['name', 'file_name']):
+	file_names_with_url = frappe.get_all("File", filters={
+		"is_folder": 0,
+		"file_name": ["like", "%/%"]
+	}, fields=['name', 'file_name'])
+
+	for f in file_names_with_url:
 		filename = f.file_name.rsplit('/', 1)[-1]
 
-		frappe.db.sql('''
-		update `tabFile` set file_name = {0} where name = {1}
-		'''.format(filename, f.name))
+		# Check if file exists
+		path = os.path.join(public_path, filename)
+		if '/private/' in f.file_name:
+			path = os.path.join(private_path, filename)
+
+		if not os.path.exists(path):
+			continue
+
+		frappe.db.set_values('File', f.name, {
+			"file_name": filename,
+		})
 
 	frappe.db.commit()
 
-	for f in frappe.get_all("File", filters={"is_folder": 0, "file_name": ["is", "not set"], "file_url": ["is", "set"]}, fields=['file_url', 'name']):
+	file_names_not_set = frappe.get_all("File", filters={
+		"is_folder": 0,
+		"file_name": ["is", "not set"],
+		"file_url": ["is", "set"]
+	}, fields=['file_url', 'name'])
+
+	for f in file_names_empty
 		filename = f.file_url.rsplit('/', 1)[-1]
 
-		frappe.db.sql('''
-		update `tabFile` set file_name = {0} where name = {1}
-		'''.format(filename, f.name))
+		# Check if file exists
+		path = os.path.join(public_path, filename)
+		if '/private/' in f.file_url:
+			path = os.path.join(private_path, filename)
+
+		if not os.path.exists(path):
+			continue
+
+		frappe.db.set_values('File', f.name, {
+			"file_name": filename,
+		})
 
 	frappe.db.commit()
-
-
-
-
