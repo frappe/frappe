3f75825c9917e44a51e363a1893e454a33ae3989
Afshan <afshan13k@gmail.com>
2021-03-10 16:03:36 +0530

    fix: refactoring patch

--- frappe/patches/v12_0/rename_uploaded_files_with_proper_name.py
+++ frappe/patches/v12_0/rename_uploaded_files_with_proper_name.py
@@ -1,78 +1,36 @@
 import frappe
 import os
 
-
 def execute():
-
-	public_path = frappe.utils.get_files_path()
-	private_path = frappe.utils.get_files_path(is_private=1)
-
-	file_names_with_url_and_file_url_not_set = frappe.get_all("File", filters={
-		"is_folder": 0,
-		"file_url": ["is", "not set"],
-		"file_name": ["like", "%/%"]
-	}, fields=['name', 'file_name'])
-
-	for f in file_names_with_url_and_file_url_not_set:
-		fileurl = f.file_name
-		filename = f.file_name.rsplit('/', 1)[-1]
-
-		# Check if file exists
-		path = os.path.join(public_path, filename)
-		if '/private/' in fileurl:
-			path = os.path.join(private_path, filename)
-
-		if not os.path.exists(path):
-			continue
-
-		frappe.db.set_values('File', f.name, {
-			"file_name": filename,
-			"file_url": fileurl
-		})
-
-	frappe.db.commit()
-
 	file_names_with_url = frappe.get_all("File", filters={
 		"is_folder": 0,
 		"file_name": ["like", "%/%"]
-	}, fields=['name', 'file_name'])
+	}, fields=['name', 'file_name', 'file_url'])
 
 	for f in file_names_with_url:
 		filename = f.file_name.rsplit('/', 1)[-1]
 
-		# Check if file exists
-		path = os.path.join(public_path, filename)
-		if '/private/' in f.file_name:
-			path = os.path.join(private_path, filename)
+		if not f.file_url:
+			f.file_url = f.file_name
 
-		if not os.path.exists(path):
+		if not file_exists(f.file_url):
 			continue
 
 		frappe.db.set_values('File', f.name, {
 			"file_name": filename,
+			"file_url": f.file_url
 		})
 
-	frappe.db.commit()
-
-	file_names_not_set = frappe.get_all("File", filters={
+	files_without_filename = frappe.get_all("File", filters={
 		"is_folder": 0,
 		"file_name": ["is", "not set"],
 		"file_url": ["is", "set"]
 	}, fields=['file_url', 'name'])
 
-	for f in file_names_not_set:
+	for f in files_without_filename:
 		filename = f.file_url.rsplit('/', 1)[-1]
+		frappe.db.set_value("File", f.name, "file_name", filename)
 
-		# Check if file exists
-		path = os.path.join(public_path, filename)
-		if '/private/' in f.file_url:
-			path = os.path.join(private_path, filename)
-
-		if not os.path.exists(path):
-			continue
-
-		frappe.db.set_values('File', f.name, {
-			"file_name": filename,
-		})
-
-	frappe.db.commit()
+def file_exists(file_path):
+	file_path = frappe.utils.get_site_path(file_path.lstrip('/'))
+	return os.path.exists(file_path)
