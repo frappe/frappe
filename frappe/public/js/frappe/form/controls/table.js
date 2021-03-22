71751561a96c173aa3ec6b60ad2032afb2c5c497
Suraj Shetty <surajshetty3416@gmail.com>
2021-03-22 14:10:38 +0530

    refactor: Fix variable name & formatting
    (cherry picked from commit 55a8667c5671947aff6a8836d22a100059b0fff0)

--- frappe/public/js/frappe/form/controls/table.js
+++ frappe/public/js/frappe/form/controls/table.js
@@ -12,91 +12,94 @@ frappe.ui.form.ControlTable = frappe.ui.form.Control.extend({
 			parent: this.wrapper,
 			control: this
 		});
+
 		if (this.frm) {
 			this.frm.grids[this.frm.grids.length] = this;
 		}
+
 		this.$wrapper.on('paste', ':text', e => {
-			const cur_table_field = this.df.fieldname;
-			const cur_field = $(e.target).data('fieldname');
-			const cur_grid = this.grid;
-			const grid_pagination = cur_grid.grid_pagination;
-			const cur_grid_rows = cur_grid.grid_rows;
-			const cur_doctype = cur_grid.doctype;
-			const cur_row_docname = $(e.target).closest('div .grid-row').data('name');
-			let row_idx = locals[cur_doctype][cur_row_docname].idx;
+			const table_field = this.df.fieldname;
+			const grid = this.grid;
+			const grid_pagination = grid.grid_pagination;
+			const grid_rows = grid.grid_rows;
+			const doctype = grid.doctype;
+			const row_docname = $(e.target).closest('.grid-row').data('name');
+
 			let clipboard_data = e.clipboardData || window.clipboardData || e.originalEvent.clipboardData;
 			let pasted_data = clipboard_data.getData('Text');
+
 			if (!pasted_data) return;
 
 			let data = frappe.utils.csv_to_array(pasted_data, '\t');
-
-			if (data.length === 1 & data[0].length === 1) return;
-
 			let fieldnames = [];
-			let get_field = field_name => {
-				let fieldname;
-				cur_grid.meta.fields.some(field => {
-					if (frappe.model.no_value_type.includes(field.fieldtype)) {
-						return false;
-					}
-
-					field_name = field_name.toLowerCase();
-					const is_field_matching = (field_name) => {
-						return field.fieldname.toLowerCase() === field_name
-							|| (field.label || "").toLowerCase() === field_name;
-					};
-
-					if (is_field_matching()) {
-						fieldname = field.fieldname;
-						return true;
-					}
-				});
-				return fieldname;
-			};
-
 			// for raw data with column header
-			if (get_field(data[0][0])) {
-				data[0].forEach(column =>{
-					fieldnames.push(get_field(column));
+			if (this.get_field(data[0][0])) {
+				data[0].forEach(column => {
+					fieldnames.push(this.get_field(column));
 				});
 				data.shift();
 			} else {
 				// no column header, map to the existing visible columns
-				let visible_columns = cur_grid_rows[0].get_visible_columns();
+				const visible_columns = grid_rows[0].get_visible_columns();
 				visible_columns.forEach(column => {
-					if (column.fieldname === cur_field) {
+					if (column.fieldname === $(e.target).data('fieldname')) {
 						fieldnames.push(column.fieldname);
 					}
-				})
+				});
 			}
 
+			let row_idx = locals[doctype][row_docname].idx;
 			data.forEach((row, i) => {
 				let blank_row = !row.filter(Boolean).length;
 				if (blank_row) return;
+
 				setTimeout(() => {
-					if (row_idx > this.frm.doc[cur_table_field].length) {
+					if (row_idx > this.frm.doc[table_field].length) {
 						this.grid.add_new_row();
 					}
 					if (row_idx > 1 && (row_idx - 1) % grid_pagination.page_length === 0) {
 						grid_pagination.go_to_page(grid_pagination.page_index + 1);
 					}
-					const cur_row = cur_grid_rows[row_idx - 1];
-					row_idx ++;
-					const row_name = cur_row.doc.name;
-					row.forEach((value, ci) => {
-						if (fieldnames[ci]) {
-							frappe.model.set_value(cur_doctype, row_name, fieldnames[ci], value);
-							let progress = i + 1;
-							frappe.show_progress(__('Processing'), i + 1, data.length);
-							if (progress == data.length) {
-								frappe.hide_progress();
-							}
+
+					const row_name = grid_rows[row_idx - 1].doc.name;
+					row.forEach((value, data_index) => {
+						if (fieldnames[data_index]) {
+							frappe.model.set_value(doctype, row_name, fieldnames[data_index], value);
 						}
 					});
+					row_idx++;
+
+					let progress = i + 1;
+					frappe.show_progress(__('Processing'), progress, data.length);
+					if (progress === data.length) {
+						frappe.hide_progress();
+					}
 				}, 0);
 			});
 			return false; // Prevent the default handler from running.
 		});
+	},
+	get_field(field_name) {
+		let fieldname;
+		this.grid.meta.fields.some(field => {
+			if (frappe.model.no_value_type.includes(field.fieldtype)) {
+				return false;
+			}
+
+			field_name = field_name.toLowerCase();
+			const is_field_matching = field_name => {
+				return (
+					field.fieldname.toLowerCase() === field_name ||
+					(field.label || '').toLowerCase() === field_name
+				);
+			};
+
+			if (is_field_matching()) {
+				fieldname = field.fieldname;
+				return true;
+			}
+		});
+		return fieldname;
 	},
 	refresh_input: function() {
 		this.grid.refresh();
