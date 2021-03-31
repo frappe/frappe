df670c5f61463da4f1bc554e725b92de0c6be82b
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2021-03-31 12:36:09 +0530

    fix: Miscellaneous changes (#12730)
    (cherry picked from commit 5688b3e7d9736beb2067fe7a5042c0e8050aa085)
    
    # Conflicts:
    #	frappe/core/doctype/system_settings/system_settings.json
    #	frappe/public/js/frappe/form/controls/table.js

--- frappe/public/js/frappe/form/controls/table.js
+++ frappe/public/js/frappe/form/controls/table.js
@@ -15,6 +15,7 @@ frappe.ui.form.ControlTable = frappe.ui.form.Control.extend({
 		if(this.frm) {
 			this.frm.grids[this.frm.grids.length] = this;
 		}
+<<<<<<< HEAD
 		this.$wrapper.on('paste',':text', function(e) {
 			var cur_table_field =$(e.target).closest('div [data-fieldtype="Table"]').data('fieldname');
 			var cur_field = $(e.target).data('fieldname');
@@ -44,6 +45,32 @@ frappe.ui.form.ControlTable = frappe.ui.form.Control.extend({
 						  fieldname = field.fieldname;
 						  return false;
 						}
+=======
+
+		this.$wrapper.on('paste', ':text', e => {
+			const table_field = this.df.fieldname;
+			const grid = this.grid;
+			const grid_pagination = grid.grid_pagination;
+			const grid_rows = grid.grid_rows;
+			const doctype = grid.doctype;
+			const row_docname = $(e.target).closest('.grid-row').data('name');
+			const in_grid_form = $(e.target).closest('.form-in-grid').length;
+
+			let clipboard_data = e.clipboardData || window.clipboardData || e.originalEvent.clipboardData;
+			let pasted_data = clipboard_data.getData('Text');
+
+			if (!pasted_data || in_grid_form) return;
+
+			let data = frappe.utils.csv_to_array(pasted_data, '\t');
+
+			if (data.length === 1 && data[0].length === 1) return;
+
+			let fieldnames = [];
+			// for raw data with column header
+			if (this.get_field(data[0][0])) {
+				data[0].forEach(column => {
+					fieldnames.push(this.get_field(column));
+>>>>>>> 5688b3e7d9... fix: Miscellaneous changes (#12730)
 				});
 				return fieldname;
 			}
@@ -51,6 +78,7 @@ frappe.ui.form.ControlTable = frappe.ui.form.Control.extend({
 				$.each(data[0], (ci, column)=>{fieldnames.push(get_field(column));});
 				data.shift();
 			}
+<<<<<<< HEAD
 			else{ // no column header, map to the existing visible columns
 				var visible_columns = cur_grid_rows[0].get_visible_columns();
 				var find;
@@ -79,6 +107,36 @@ frappe.ui.form.ControlTable = frappe.ui.form.Control.extend({
 					});
 					frappe.show_progress(__('Processing'), i, data.length);
 				}
+=======
+
+			let row_idx = locals[doctype][row_docname].idx;
+			let data_length = data.length;
+			data.forEach((row, i) => {
+				setTimeout(() => {
+					let blank_row = !row.filter(Boolean).length;
+					if (!blank_row) {
+						if (row_idx > this.frm.doc[table_field].length) {
+							this.grid.add_new_row();
+						}
+
+						if (row_idx > 1 && (row_idx - 1) % grid_pagination.page_length === 0) {
+							grid_pagination.go_to_page(grid_pagination.page_index + 1);
+						}
+
+						const row_name = grid_rows[row_idx - 1].doc.name;
+						row.forEach((value, data_index) => {
+							if (fieldnames[data_index]) {
+								frappe.model.set_value(doctype, row_name, fieldnames[data_index], value);
+							}
+						});
+						row_idx++;
+						if (data_length >= 10) {
+							let progress = i + 1;
+							frappe.show_progress(__('Processing'), progress, data_length, null, true);
+						}
+					}
+				}, 0);
+>>>>>>> 5688b3e7d9... fix: Miscellaneous changes (#12730)
 			});
 			frappe.hide_progress();
 			return false; // Prevent the default handler from running.
