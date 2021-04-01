056b8c0fc6996367b4def5ee160670c9766e8211
Shridhar <shridhar.p@zerodha.com>
2021-04-01 09:38:21 +0530

    feat: added get value for virtual doctype

--- frappe/core/doctype/test/test.js
+++ frappe/core/doctype/test/test.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, Frappe Technologies and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('test_virtual', {
+frappe.ui.form.on('test', {
 	// refresh: function(frm) {
 
 	// }
--- frappe/test/doctype/test_des/test_des.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, Frappe Technologies and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('test_des', {
-	// refresh: function(frm) {
-
-	// }
-});
