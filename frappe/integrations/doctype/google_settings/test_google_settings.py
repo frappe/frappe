7c78fda472e9d7ea8264ed21bfe5b7948d77d531
barredterra <14891507+barredterra@users.noreply.github.com>
2021-04-02 20:09:37 +0200

    test: google settings

--- frappe/integrations/doctype/google_settings/test_google_settings.py
+++ frappe/integrations/doctype/google_settings/test_google_settings.py
@@ -3,8 +3,49 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
 import unittest
 
+from .google_settings import get_file_picker_settings
+
 class TestGoogleSettings(unittest.TestCase):
-	pass
+
+	def setUp(self):
+		settings = frappe.get_single('Google Settings')
+		settings.client_id = 'test_client_id'
+		settings.app_id = 'test_app_id'
+		settings.api_key = 'test_api_key'
+		settings.save()
+
+	def test_picker_as_guest(self):
+		# Google Drive Picker should be disbled for Guest
+		frappe.set_user('Guest')
+		frappe.db.set_value('Google Settings', None, 'enable', 1)
+		frappe.db.set_value('Google Settings', None, 'google_drive_picker_enabled', 1)
+		settings = get_file_picker_settings()
+
+		self.assertIn('enabled', settings)
+		self.assertEqual(False, settings.get('enabled', True))
+		self.assertEqual(1, len(settings))
+
+		frappe.set_user('Adminstrator')
+
+	def test_picker_disabled(self):
+		# Google Drive Picker should be disabled if it is not enabled in Google Settings
+		frappe.db.set_value('Google Settings', None, 'enable', 1)
+		frappe.db.set_value('Google Settings', None, 'google_drive_picker_enabled', 0)
+		settings = get_file_picker_settings()
+
+		self.assertIn('enabled', settings)
+		self.assertEqual(False, settings.get('enabled', True))
+		self.assertEqual(1, len(settings))
+
+	def test_google_disabled(self):
+		# Google Drive Picker should be disabled if Google integration is not enabled
+		frappe.db.set_value('Google Settings', None, 'enable', 0)
+		frappe.db.set_value('Google Settings', None, 'google_drive_picker_enabled', 1)
+		settings = get_file_picker_settings()
+
+		self.assertIn('enabled', settings)
+		self.assertEqual(False, settings.get('enabled', True))
+		self.assertEqual(1, len(settings))
