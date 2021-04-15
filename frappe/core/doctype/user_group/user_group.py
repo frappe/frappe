f71a6c09f503bac8028f16eccd6fd2016c7d12be
Suraj Shetty <surajshetty3416@gmail.com>
2021-04-15 14:53:01 +0530

    fix: Sync user groups realtime

--- frappe/core/doctype/user_group/user_group.py
+++ frappe/core/doctype/user_group/user_group.py
@@ -5,6 +5,11 @@
 from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
+import frappe
 
 class UserGroup(Document):
-	pass
+	def after_insert(self):
+		frappe.publish_realtime('user_group_added', self.name)
+
+	def on_trash(self):
+		frappe.publish_realtime('user_group_deleted', self.name)
