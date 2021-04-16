0db5bfa9f5665b50bbd7ba9682afdc562cbb1cb1
prssanna <prssud@gmail.com>
2021-04-16 13:03:21 +0530

    fix: update patch

--- frappe/patches/v13_0/add_original_name_docfield_to_submittable_doctypes.py
+++ frappe/patches/v13_0/add_original_name_docfield_to_submittable_doctypes.py
@@ -1,11 +1,9 @@
 import frappe
-from frappe.database.schema import add_column
 
 def execute():
 	for doctype in frappe.db.get_all('DocType'):
 		doctype = frappe.get_doc("DocType", doctype.name)
 		if doctype.is_submittable and frappe.db.table_exists(doctype.name):
 			doctype.make_cancellable()
-			if not frappe.db.has_column(doctype.name, 'original_name'):
-				add_column(doctype.name, 'original_name', 'Text')
+			frappe.reload_doctype(doctype.name)
 			doctype.db_update_all()
