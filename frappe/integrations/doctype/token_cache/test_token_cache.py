edef0a467d2ad993a68bfd78e2ad844a210851ff
barredterra <14891507+barredterra@users.noreply.github.com>
2021-04-17 18:31:43 +0200

    fix: test_token_cache PermissionError

--- frappe/integrations/doctype/token_cache/test_token_cache.py
+++ frappe/integrations/doctype/token_cache/test_token_cache.py
@@ -13,7 +13,7 @@ class TestTokenCache(unittest.TestCase):
 	def setUp(self):
 		self.token_cache = frappe.get_last_doc('Token Cache')
 		self.token_cache.update({'connected_app': frappe.get_last_doc('Connected App').name})
-		self.token_cache.save()
+		self.token_cache.save(ignore_permissions=True)
 
 	def test_get_auth_header(self):
 		self.token_cache.get_auth_header()
