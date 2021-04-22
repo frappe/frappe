87f3038272a33954437d50759caa05ec3916df12
Faris Ansari <netchamp.faris@gmail.com>
2021-04-22 17:31:34 +0530

    fix: remove hardcoding

--- frappe/patches/v13_0/jinja_hook.py
+++ frappe/patches/v13_0/jinja_hook.py
@@ -6,7 +6,7 @@ import frappe
 from click import secho
 
 def execute():
-	if frappe.get_hooks('jenv') or True:
+	if frappe.get_hooks('jenv'):
 		print()
 		secho('WARNING: The hook "jenv" is deprecated. Follow the migration guide to use the new "jinja" hook.', fg='yellow')
 		secho('https://github.com/frappe/frappe/wiki/Migrating-to-Version-13', fg='yellow')
