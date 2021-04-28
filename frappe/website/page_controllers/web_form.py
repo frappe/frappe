3aa990c6d97dbe7a5c4d8dd222059fd4de539f5c
Suraj Shetty <surajshetty3416@gmail.com>
2021-04-28 13:07:17 +0530

    fix: Add validator for WebForm

--- frappe/website/page_controllers/web_form.py
+++ frappe/website/page_controllers/web_form.py
@@ -1,4 +1,6 @@
 from frappe.website.page_controllers.web_page import WebPage
+import frappe
 
 class WebFormPage(WebPage):
-	pass
+	def validate(self):
+		return bool(frappe.get_all("Web Form", filters={'route': self.path}))
