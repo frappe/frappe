450e9ae45fc35e75192edfe164d630e20f76a8fe
hasnain2808 <hasnain2808@gmail.com>
2021-04-29 14:26:58 +0530

    tests: use frappe namespace

--- frappe/commands/utils.py
+++ frappe/commands/utils.py
@@ -520,7 +520,7 @@ def run_tests(context, app=None, module=None, doctype=None, test=(), profile=Fal
 
 		# Generate coverage report only for app that is being tested
 		source_path = os.path.join(get_bench_path(), 'apps', app or 'frappe')
-		cov = Coverage(source=[source_path], omit=[
+		omit=[
 			'*.html',
 			'*.js',
 			'*.xml',
@@ -529,9 +529,13 @@ def run_tests(context, app=None, module=None, doctype=None, test=(), profile=Fal
 			'*.scss',
 			'*.vue',
 			'*/doctype/*/*_dashboard.py',
-			'*/patches/*',
-			'*/commands/*'
-		])
+			'*/patches/*'
+		]
+
+		if not app or app=='frappe':
+			omit.append('frappe/commands/*')
+
+		cov = Coverage(source=[source_path], omit=omit)
 		cov.start()
 
 	ret = frappe.test_runner.main(app, module, doctype, context.verbose, tests=tests,
