00c2d2b4725fb5448a6d3e18f0a767c311c8235c
Ernesto Ruiz <ernesto_ruiz89@hotmail.com>
2021-04-29 16:14:10 -0600

    fix: Make strings translatable
    Make strings translatable

--- frappe/desk/page/backups/backups.js
+++ frappe/desk/page/backups/backups.js
@@ -1,7 +1,7 @@
 frappe.pages['backups'].on_page_load = function(wrapper) {
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
-		title: 'Download Backups',
+		title: __('Download Backups'),
 		single_column: true
 	});
 
