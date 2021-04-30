b7e139a7bf75ac521b5628b89858d0ce88fff905
Suraj Shetty <surajshetty3416@gmail.com>
2021-04-30 11:09:13 +0530

    fix: Typo
    (cherry picked from commit 43a4f1861e30f1a762e4ea1a74921b05412c3e00)

--- frappe/desk/search.py
+++ frappe/desk/search.py
@@ -226,7 +226,7 @@ def validate_and_sanitize_search_inputs(fn, instance, args, kwargs):
 @frappe.whitelist()
 def get_names_for_mentions(search_term):
 	users_for_mentions = frappe.cache().get_value('users_for_mentions', get_users_for_mentions)
-	user_groups = frappe.cache().get_value('users_groups', get_user_groups)
+	user_groups = frappe.cache().get_value('user_groups', get_user_groups)
 
 	filtered_mentions = []
 	for mention_data in users_for_mentions + user_groups:
