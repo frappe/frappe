3c322c1298ebc3625a1ae067851bb5cec1aa0152
shariquerik <sharique.rik@gmail.com>
2021-05-03 19:35:21 +0530

    fix: Patch Update Notification channel git status

---
+++ frappe/patches/v13_0/update_notification_channel_if_empty.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.utils.rename_field import rename_field
+
+def execute():
+
+	frappe.reload_doc("Email", "doctype", "Notification")
+
+	notifications = frappe.get_all('Notification', {'is_standard': 1}, {'name', 'channel'})
+	for notification in notifications:
+		if not notification.channel:
+			frappe.db.set_value("Notification", notification.name, "channel", "Email", update_modified=False)
+			frappe.db.commit()
