964ae97daa6a35335d2f4530ac539bdb562586aa
gavin <gavin18d@gmail.com>
2021-05-08 12:39:27 +0530

    fix: Avoid possible whitespace bug
    * Handles semgrep warning
    * Changed "" to None as a precaution against future whitespace bugs via human error
    
    Co-authored-by: Ankush Menat <ankushmenat@gmail.com>
    (cherry picked from commit c84feb16e9e456cdd040f20a6ac154d3a1d760c0)

--- frappe/core/doctype/doctype/doctype.py
+++ frappe/core/doctype/doctype/doctype.py
@@ -137,7 +137,7 @@ class DocType(Document):
 		}
 
 		for docfield in self.get("fields") or []:
-			conflict_type = ""
+			conflict_type = None
 			field = docfield.fieldname
 			field_label = docfield.label or docfield.fieldname
 
