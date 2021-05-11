9dcce23d6681388c6bc8d658e1164f51dd900bae
Gavin D'souza <gavin18d@gmail.com>
2021-05-11 13:02:45 +0530

    chore: Remove f prefix on plain string
    (cherry picked from commit 134b2dcd5c938ba555004d3a1a8c995f0dc0bf2d)

--- frappe/search/website_search.py
+++ frappe/search/website_search.py
@@ -40,7 +40,7 @@ class WebsiteSearch(FullTextSearch):
 		self._items_to_index = []
 
 		for i, route in enumerate(routes):
-			update_progress_bar(f"Retrieving Routes", i, len(routes))
+			update_progress_bar("Retrieving Routes", i, len(routes))
 			self._items_to_index += [self.get_document_to_index(route)]
 
 		print()
