9b26307fe5e2210a6c36645e220d077c1e997090
Suraj Shetty <surajshetty3416@gmail.com>
2021-05-17 12:23:50 +0530

    test: Use get_response_content to get content

--- frappe/tests/test_recorder.py
+++ frappe/tests/test_recorder.py
@@ -8,7 +8,7 @@ import unittest
 import frappe
 import frappe.recorder
 from frappe.utils import set_request
-from frappe.website.serve import get_response
+from frappe.website.serve import get_response_content
 
 import sqlparse
 
@@ -122,5 +122,5 @@ class TestRecorder(unittest.TestCase):
 			self.assertEqual(call['exact_copies'], query[1])
 
 	def test_error_page_rendering(self):
-		content = get_response("error")
+		content = get_response_content("error")
 		self.assertIn("Error", content)
