64f1ea794067e81ad077916cd6466170fa599d21
shadrak gurupnor <gurupnor.shadrak@gmail.com>
2021-06-09 14:23:09 +0530

    fix: check for 2FA is enabled or not

--- frappe/auth.py
+++ frappe/auth.py
@@ -209,7 +209,15 @@ class LoginManager:
 			self.fail(_('Incomplete login details'), user=user)
 
 		# Ignore password check if tmp_id is set, 2FA takes care of authentication.
-		validate_password = not bool(frappe.form_dict.get('tmp_id'))
+		tmp_id = bool(frappe.form_dict.get('tmp_id'))
+
+		# Check if 2FA is enabled or not
+		enabled = int(frappe.db.get_value('System Settings', None, 'enable_two_factor_auth') or 0)
+
+		if tmp_id and enabled:
+			validate_password = False
+		
+		validate_password = True
 		user = User.find_by_credentials(user, pwd, validate_password=validate_password)
 
 		if not user:
