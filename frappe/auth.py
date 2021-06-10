5d602fa1649b10b326d1c588d703e827542d9936
Shadrak Gurupnor <30501401+shadrak98@users.noreply.github.com>
2021-06-10 12:12:26 +0530

    Update auth.py

--- frappe/auth.py
+++ frappe/auth.py
@@ -210,7 +210,7 @@ class LoginManager:
 
 		# Ignore password check if tmp_id is set, 2FA takes care of authentication.
 		tmp_id = bool(frappe.form_dict.tmp_id)
-		enabled = cint(frappe.db.get_single_value('System Settings', 'enable_two_factor_auth'))
+		enabled = cint(frappe.db.get_single_value('System Settings', 'enable_two_factor_auth', cache=True))
 		# if tmp_id is set and 2FA is enabled, skip validation by password
 		validate_password = not (tmp_id and enabled)
 		user = User.find_by_credentials(user, pwd, validate_password=validate_password)
