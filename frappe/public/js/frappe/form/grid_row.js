722f05f64efacdec56a1e1c0308d401019cbc14d
walstanb <walstanb@gmail.com>
2021-06-15 13:49:52 +0530

    fix: fixed width for template row index

--- frappe/public/js/frappe/form/grid_row.js
+++ frappe/public/js/frappe/form/grid_row.js
@@ -173,8 +173,7 @@ export default class GridRow {
 		// row index
 		if(this.doc) {
 			if(!this.row_index) {
-				this.row_index = $('<div style="float: left; margin-left: 15px; margin-top: 8px; \
-					margin-right: -20px;">'+this.row_check_html+' <span></span></div>').appendTo(this.row);
+				this.row_index = $(`<div class="template-row-index">${this.row_check_html}<span></span></div>`).appendTo(this.row);
 			}
 			this.row_index.find('span').html(this.doc.idx);
 		}
