9207851a50af07b368b7f0c2dbd8d9a9de12b09a
Steffen <steffen@brennscheidt.net>
2021-06-17 09:58:41 +0200

    Allow Chart Widgets in Document Forms

--- frappe/public/js/frappe/form/dashboard.js
+++ frappe/public/js/frappe/form/dashboard.js
@@ -32,6 +32,13 @@ frappe.ui.form.Dashboard = class FormDashboard {
 			collapsible: 1
 		});
 
+		this.chart_widgets_area = new Section(this.parent, {
+			title: __("Charts"),
+			css_class: 'form-chart-widgets',
+			hidden: 1,
+			collapsible: 1
+		});
+
 		this.stats_area_row = $(`<div class="row"></div>`);
 		this.stats_area = new Section(this.parent, {
 			title: __("Stats"),
@@ -191,6 +198,43 @@ frappe.ui.form.Dashboard = class FormDashboard {
 			this.setup_graph();
 			// show = true;
 		}
+
+		if (this.data.chart_widgets){
+			Promise.all(
+			this.data.chart_widgets.map(widget => {
+				let method = widget.method;
+				if (typeof(method) !== "undefined"){
+					let method_args = {
+						...widget.args,
+						"doctype": this.frm.doctype,
+						"docname": this.frm.docname,
+					}
+					return frappe.call(method, method_args);
+				}
+				return undefined;
+			}).filter(widget => typeof(widget) !== "undefined")).then(widgets_responses => {
+				let widgets = widgets_responses.map(r => {let obj = r.message; obj.chart_settings = {filters: JSON.parse(obj.filters_json)}; return obj;});
+				debugger;
+				this.chart_widgets_area.body.empty();
+				this.chart_group = new frappe.widget.WidgetGroup({
+					container: $(this.chart_widgets_area.body),
+					type: "chart",
+					columns: 2,
+					height: 240,
+					options: {
+						allow_sorting: true,
+						allow_create: true,
+						allow_delete: true,
+						allow_hiding: true,
+						allow_resize: true,
+					},
+					widgets: widgets,
+					in_customize_mode: false,
+				});
+				this.chart_widgets_area.show();
+				this.chart_widgets_area.collapse();
+
+			});
 
 		if (show) {
 			this.show();
