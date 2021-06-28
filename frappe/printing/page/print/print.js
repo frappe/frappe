885e81b7030c016b763b80f65e02974e2d002c1a
Mohammad Hasnain Mohsin Rajan <hasnain2808@gmail.com>
2021-06-28 16:44:38 +0530

    fix: linting
    Co-authored-by: Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>

--- frappe/printing/page/print/print.js
+++ frappe/printing/page/print/print.js
@@ -338,7 +338,7 @@ frappe.ui.form.PrintView = class {
 		let doc_letterhead = this.frm.doc.letter_head;
 
 		return frappe.db
-			.get_list('Letter Head', { fields: ['name', 'is_default'], limit: 0 }   )
+			.get_list('Letter Head', { fields: ['name', 'is_default'], limit: 0 })
 			.then((letterheads) => {
 				letterheads.map((letterhead) => {
 					if (letterhead.is_default) default_letterhead = letterhead.name;
