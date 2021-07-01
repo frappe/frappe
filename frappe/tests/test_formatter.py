9d1aae0a8929ee0c04f22079faad714a93450484
hasnain2808 <hasnain2808@gmail.com>
2021-07-01 09:22:12 +0530

    Revert "fix: add test cases"
    This reverts commit 4bafae6e38d898096c49ce2bf5600f0772f05f17.

--- frappe/tests/test_formatter.py
+++ frappe/tests/test_formatter.py
@@ -23,23 +23,3 @@ class TestFormatter(unittest.TestCase):
 		self.assertEqual(format(100, df, doc), "$ 100.00")
 
 		frappe.db.set_default("currency", None)
-
-	def test_custom_currency_formatting(self):
-		df = frappe._dict({
-			'fieldname': 'amount',
-			'fieldtype': 'Currency',
-			'options': 'currency'
-		})
-
-		doc = frappe._dict({
-			'amount': 5
-		})
-		frappe.db.set_default("currency", 'INR')
-
-		# if currency field is not passed then default currency should be used.
-		self.assertEqual(format(100000, df, doc, format="#,###.##"), '₹ 100,000.00')
-
-		doc.currency = 'USD'
-		self.assertEqual(format(100000, df, doc, format="#,###.##"), "$ 100,000.00")
-
-		frappe.db.set_default("currency", None)
