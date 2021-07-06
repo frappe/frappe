experiments001/frappe-sqlite

fix: RTL for print views

frappe/www/printview.py
@@ -7,6 +7,7 @@ from frappe import _
 from frappe.modules import get_doc_path
 from frappe.core.doctype.access_log.access_log import make_access_log
 from frappe.utils import cint, sanitize_html, strip_html
+from frappe.utils.jinja_globals import is_rtl
 
 no_cache = 1
 
@@ -44,7 +45,8 @@ def get_context(context):
 		"css": get_print_style(frappe.form_dict.style, print_format),
 		"comment": frappe.session.user,
 		"title": doc.get(meta.title_field) if meta.title_field else doc.name,
-		"has_rtl": True if frappe.local.lang in ["ar", "he", "fa", "ps"] else False
+		"lang": frappe.local.lang,
+		"layout_direction": "rtl" if is_rtl() else "ltr"
 	}
 
 def get_print_format_doc(print_format_name, meta):

frappe/frappe

frappe/www/printview.py
@@ -10,6 +10,7 @@ from frappe.modules import get_doc_path
 from frappe.core.doctype.access_log.access_log import make_access_log
 from frappe.utils import cint, sanitize_html, strip_html
 from six import string_types
+from frappe.utils.jinja import is_rtl
 
 no_cache = 1
 
@@ -48,7 +49,8 @@ def get_context(context):
 		"css": get_print_style(frappe.form_dict.style, print_format),
 		"comment": frappe.session.user,
 		"title": doc.get(meta.title_field) if meta.title_field else doc.name,
-		"has_rtl": True if frappe.local.lang in ["ar", "he", "fa", "ps"] else False
+		"lang": frappe.local.lang,
+		"layout_direction": "rtl" if is_rtl() else "ltr"
 	}
 
 def get_print_format_doc(print_format_name, meta):