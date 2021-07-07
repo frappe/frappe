4912b3657f52dcaeca7773c69fce23b98cb2a3f5
Suraj Shetty <surajshetty3416@gmail.com>
2021-07-07 22:39:12 +0530

    fix: Enable set_open_count
    perf: move set_open_count to after_refresh to avoid unnecessary calls
    (cherry picked from commit ee75cea4fe22c79b3163c2a593c7be61622f4a9c)
    
    # Conflicts:
    #	frappe/public/js/frappe/form/dashboard.js

--- frappe/public/js/frappe/form/dashboard.js
+++ frappe/public/js/frappe/form/dashboard.js
@@ -5,7 +5,6 @@ frappe.ui.form.Dashboard = class FormDashboard {
 	constructor(opts) {
 		$.extend(this, opts);
 		this.setup_dashboard_sections();
-		this.set_open_count = frappe.utils.throttle(this.set_open_count, 500);
 	}
 
 	setup_dashboard_sections() {
@@ -179,7 +178,10 @@ frappe.ui.form.Dashboard = class FormDashboard {
 				return;
 			}
 			this.render_links();
+<<<<<<< HEAD
 			this.set_open_count();
+=======
+>>>>>>> ee75cea4fe (fix: Enable set_open_count)
 			show = true;
 		}
 
@@ -206,6 +208,7 @@ frappe.ui.form.Dashboard = class FormDashboard {
 				$(this).removeClass('hidden');
 			}
 		});
+		this.set_open_count();
 	}
 
 	init_data() {
