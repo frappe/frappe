586f72b3355e5ca4595b93323dd25a1ba0d558fa
Gavin D'souza <gavin18d@gmail.com>
2021-07-15 01:11:10 +0530

    test: Added test to check sanity of HTTPRequest.set_lang
    (cherry picked from commit 2ccfb547b50a43e39ce4cbe8d53b81d525689fe6)

--- frappe/tests/test_auth.py
+++ frappe/tests/test_auth.py
@@ -6,8 +6,9 @@ import time
 import unittest
 
 import frappe
-from frappe.auth import LoginAttemptTracker
+from frappe.auth import HTTPRequest, LoginAttemptTracker
 from frappe.frappeclient import FrappeClient, AuthError
+from frappe.utils import set_request
 
 class TestAuth(unittest.TestCase):
 	def __init__(self, *args, **kwargs):
@@ -126,3 +127,20 @@ class TestLoginAttemptTracker(unittest.TestCase):
 
 		tracker.add_failure_attempt()
 		self.assertTrue(tracker.is_user_allowed())
+
+class TestFrappeHTTPRequest(unittest.TestCase):
+	# test frappe.auth.HTTPRequest
+	def test_set_language(self):
+		"""Check if language is set on object initialization
+
+		This is a test to ensure that language has changed. To test correctness
+		of frappe.local.lang, check out the tests of frappe.translate.get_language
+		"""
+		lang_before_request = frappe.local.lang
+		random_lang = frappe.get_all("Language", limit=1, pluck="name")[0]
+		set_request(method='POST', path='/')
+		frappe.form_dict._lang = random_lang
+		HTTPRequest()
+		self.assertTrue(hasattr(frappe.local, "lang"))
+		self.assertIsInstance(frappe.local.lang, str)
+		self.assertNotEqual(lang_before_request, frappe.local.lang)
