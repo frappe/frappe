1b68c02e57fd31dc4a78cff5387d0822294a3efd
Sagar Vora <sagar@resilient.tech>
2021-07-14 12:25:41 +0530

    fix: resolve merge conflict

--- frappe/public/js/frappe/form/layout.js
+++ frappe/public/js/frappe/form/layout.js
@@ -263,14 +263,7 @@ frappe.ui.form.Layout = Class.extend({
 				section.addClass("empty-section");
 			}
 		});
-<<<<<<< HEAD
-
-		this.frm && this.frm.dashboard.refresh();
-
 	},
-=======
-	}
->>>>>>> b4ea6c8890 (fix: dont refresh form dashboard in `layout.refresh_sections` (#13650))
 
 	refresh_fields: function (fields) {
 		let fieldnames = fields.map((field) => {
