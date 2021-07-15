33178958e1c7e3a71a20bc79c8c4535d47d67881
Komal-Saraf0609 <komallsaraf0609@gmail.com>
2021-07-15 14:21:50 +0530

    test: Added proper name for test case, added comments and removed redundancy

--- cypress/integration/timeline.js
+++ cypress/integration/timeline.js
@@ -5,7 +5,16 @@ context('Timeline', () => {
 		cy.visit('/app/todo');
 	});
 
+        it('Adding new ToDo', () => {
+        cy.get('.primary-action').contains('Add ToDo').click();
+        cy.get('.modal-footer > .custom-actions > .btn').contains('Edit in full page').click();
+        cy.get('.row > .section-body > .form-column > form > .frappe-control > .form-group > .control-input-wrapper > .control-input > .ql-container > .ql-editor').first().type('Test ToDo');
+        cy.wait(200);
+        cy.get('#page-ToDo > .page-head > .container > .row > .col > .standard-actions > .primary-action').contains('Save').click();
+        });
+
 	it('Adding Comment', () => {
+        cy.visit('/app/todo');
         cy.get('.level-item.ellipsis > .ellipsis').click();
         cy.get('.comment-input-container > .frappe-control > .ql-container > .ql-editor').should('contain','');
         cy.get('.comment-input-container > .frappe-control > .ql-container > .ql-editor').type('Testing Timeline');
@@ -13,23 +22,19 @@ context('Timeline', () => {
 
 	});
 
-    it('Editing, saving and deleting the comment', () => {
+        it('Editing, saving and deleting the comment', () => {
         cy.get('.timeline-content').should('contain','Testing Timeline');
-        cy.get('.timeline-content > .timeline-message-box > .justify-between > .actions > :nth-child(1)').first().click();
+        cy.get('.timeline-content > .timeline-message-box > .justify-between > .actions > .btn').eq(0).first().click();
         cy.get('.timeline-content > .timeline-message-box > .comment-edit-box > .frappe-control > .ql-container > .ql-editor').first().type(' 123');
-        cy.get('.timeline-content > .timeline-message-box > .justify-between > .actions > :nth-child(1)').first().click();
+        cy.get('.timeline-content > .timeline-message-box > .justify-between > .actions > .btn').eq(0).first().click();
         cy.get('.timeline-content').should('contain','Testing Timeline 123');
-        cy.get('.timeline-content > .timeline-message-box > .justify-between > .actions > :nth-child(1)').first().click();
-        cy.get('.actions > [style="display: block;"]').first().click();
+        cy.get('.timeline-content > .timeline-message-box > .justify-between > .actions > .btn').eq(0).first().click();
+        cy.get('.actions > .btn').eq(1).first().click();
         cy.get('.timeline-content').should('contain','Testing Timeline 123');
-        cy.get('.actions > :nth-child(3) > .icon').first().click();
+        cy.get('.actions > .btn > .icon').first().click();
         cy.get('.modal-footer > .standard-actions > .btn-primary').contains('Yes').click();
-
+        cy.get('#page-ToDo > .page-head > .container > .row > .col > .standard-actions > .menu-btn-group > .btn').click();
+        cy.get(':nth-child(11) > .grey-link').click();
+        cy.get('.modal.show > .modal-dialog > .modal-content > .modal-footer > .standard-actions > .btn-primary').contains('Yes').click();
 	});
-
-    it('Email Feature Test', () => {
-        cy.get('.timeline-actions > .btn').click();
-        cy.get('.modal.show > .modal-dialog > .modal-content > .modal-body > :nth-child(1) > .form-layout > .form-page > .to_section > .section-body > .form-column > form > [title="recipients"] > .form-group > .control-input-wrapper > .control-input > .awesomplete > .input-with-feedback').type('test@example.com');
-        cy.get('.modal.show > .modal-dialog > .modal-content > .modal-body > :nth-child(1) > .form-layout > .form-page > :nth-child(3) > .section-body > .form-column > form > [data-fieldtype="Text Editor"] > .form-group > .control-input-wrapper > .control-input > .ql-container > .ql-editor').type('Test Mail');
-    });
 });
