e7e8fcd6221dc662feb7b5421ef40f7eb7b9414d
Kenneth Sequeira <seq.kenneth@gmail.com>
2021-07-20 20:32:24 +0530

    fix: old currency test

--- frappe/tests/test_currency_formatter.py
+++ frappe/tests/test_currency_formatter.py
@@ -17,10 +17,6 @@ class TestFormatter(unittest.TestCase):
 		frappe.db.set_default("currency", 'INR')
 
 		# if currency field is not passed then default currency should be used.
-		print("doc.currency")
-		print(doc.currency)
-		print("frappe.db.get_default('currency')")
-		print(frappe.db.get_default("currency"))
 
 		self.assertEqual(format(100000, df, doc, format="#,###.##"), '₹ 100,000.00')
 
