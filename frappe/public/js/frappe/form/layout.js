f1e881f0093e17c445b2d52c67ba371c60d21923
Deepesh Garg <deepeshgarg6@gmail.com>
2021-07-30 13:37:55 +0530

    fix: Focus reset issue due to field refresh

--- frappe/public/js/frappe/form/layout.js
+++ frappe/public/js/frappe/form/layout.js
@@ -250,7 +250,13 @@ frappe.ui.form.Layout = class Layout {
 			// collapse sections
 			this.refresh_section_collapse();
 		}
-	}
+
+		document.activeElement.focus();
+
+		if (document.activeElement.tagName == 'INPUT') {
+			document.activeElement.select();
+		}
+	},
 
 	refresh_sections() {
 		// hide invisible sections
