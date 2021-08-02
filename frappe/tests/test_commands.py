fbd900aaff40bcd876adb16b65f1a5a96364cbc0
Faris Ansari <netchamp.faris@gmail.com>
2021-08-02 15:28:25 +0530

    test: for set-password and set-admin-password

--- frappe/tests/test_commands.py
+++ frappe/tests/test_commands.py
@@ -436,3 +436,14 @@ class TestCommands(BaseTestCommands):
 
 		self.execute("bench version -f invalid")
 		self.assertEqual(self.returncode, 2)
+
+	def test_set_password(self):
+		from frappe.utils.password import check_password
+
+		self.execute("bench --site {site} set-password Administrator test1")
+		self.assertEqual(self.returncode, 0)
+		self.assertEqual(check_password('Administrator', 'test1'), 'Administrator')
+
+		self.execute("bench --site {site} set-admin-password test2")
+		self.assertEqual(self.returncode, 0)
+		self.assertEqual(check_password('Administrator', 'test2'), 'Administrator')
