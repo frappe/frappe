84e02306ee5a39f518c7bdbdc338a0db7bb9f271
Suraj Shetty <surajshetty3416@gmail.com>
2021-08-05 14:20:33 +0530

    fix: Resolve conflicts
    Move imports from bundle file to build.json
    
    (cherry picked from commit 52435a6b92cad04592ef943a24816736b412e538)

--- frappe/public/js/controls.bundle.js
+++
@@ -1,4 +0,0 @@
-import "air-datepicker/dist/js/datepicker.min.js";
-import "./frappe/form/controls/datepicker_i18n.js";
-import "./frappe/ui/capture.js";
-import "./frappe/form/controls/control.js";
