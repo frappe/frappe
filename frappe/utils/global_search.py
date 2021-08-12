1015283731a446124b1b763886b8caefeb398491
Gavin D'souza <gavin18d@gmail.com>
2021-08-12 16:23:40 +0530

    fix(global_search): Typecast variable for comparision
    Previously start was being type casted with int. That was reverted by refactoring query to use new Query Builder
    This bug was introduced via ee3c84beef82ae37be04414866827510c8d0ebff

--- frappe/utils/global_search.py
+++ frappe/utils/global_search.py
@@ -442,7 +442,7 @@ def search(text, start=0, limit=20, doctype=""):
 		elif allowed_doctypes:
 			query = query.where(global_search.doctype.isin(allowed_doctypes))
 
-		if start > 0:
+		if cint(start) > 0:
 			query = query.offset(start)
 
 		result = frappe.db.sql(query, as_dict=True)
