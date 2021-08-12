06efca0fa659afb6437cc8ad525807baa37c14e2
Komal-Saraf0609 <komallsaraf0609@gmail.com>
2021-08-12 13:48:03 +0530

    test: Fixed sider issues

--- cypress/integration/dashboard_links.js
+++ cypress/integration/dashboard_links.js
@@ -9,7 +9,7 @@ context('Dashboard links', () => {
 		cy.get('.list-row-col > .level-item > .ellipsis').eq(0).click();
 
 		//To check if initially the dashboard contains only the "Contact" link and there is no counter
-		cy.get('[data-doctype="Contact"]').should('contain','Contact');
+		cy.get('[data-doctype="Contact"]').should('contain', 'Contact');
 
 		//Adding a new contact
 		cy.get('.btn[data-doctype="Contact"]').click();
@@ -19,7 +19,7 @@ context('Dashboard links', () => {
 		cy.get('.list-row-col > .level-item > .ellipsis').eq(0).click();
 
 		//To check if the counter for contact doc is "1" after adding the contact
-		cy.get('[data-doctype="Contact"] > .count').should('contain','1');
+		cy.get('[data-doctype="Contact"] > .count').should('contain', '1');
 		cy.get('[data-doctype="Contact"]').contains('Contact').click(); 
 
 		//Deleting the newly created contact
@@ -27,13 +27,13 @@ context('Dashboard links', () => {
 		cy.get('.list-subject > .select-like > .list-row-checkbox').eq(0).click();
 		cy.get('.actions-btn-group > .btn').contains('Actions').click();
 		cy.get('.actions-btn-group > .dropdown-menu > li > .grey-link').eq(5).click();
-		cy.get('.modal-footer > .standard-actions > .btn-primary').contains('Yes').click({delay : 700});
+		cy.get('.modal-footer > .standard-actions > .btn-primary').contains('Yes').click({delay: 700});
 
 
 		//To check if the counter from the "Contact" doc link is removed
 		cy.visit('/app/user');
 		cy.wait(700);
 		cy.get('.list-row-col > .level-item > .ellipsis').eq(0).click();
-		cy.get('[data-doctype="Contact"]').should('contain','Contact');
+		cy.get('[data-doctype="Contact"]').should('contain', 'Contact');
 	});
 });
