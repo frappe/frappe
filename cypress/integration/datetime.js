632b49fb9597aa519baaa84295b69b986bac5590
Kevin Thorne <kevin.thorne@turfbiz.net>
2021-08-13 20:22:18 +0000

    feat(controldate): hotkey integration test added

--- cypress/integration/datetime.js
+++ cypress/integration/datetime.js
@@ -125,4 +125,102 @@ context('Control Date, Time and DateTime', () => {
 			});
 		});
 	});
+
+	describe('Date/Datetime Hotkeys', () => {
+		let keys = [
+			{
+				key: 'y',
+				keycode: 89,
+				from: '2021-08-12',
+				expected: '2021-01-01',
+			},
+			{
+				key: 'r',
+				keycode: 82,
+				from: '2021-08-12',
+				expected: '2021-12-31',
+			},
+			{
+				key: 'm',
+				keycode: 77,
+				from: '2021-08-12',
+				expected: '2021-08-01',
+			},
+			{
+				key: 'h',
+				keycode: 72,
+				from: '2021-08-12',
+				expected: '2021-08-31',
+			},
+			{
+				key: 'w',
+				keycode: 87,
+				from: '2021-08-12',
+				expected: '2021-08-08',
+			},
+			{
+				key: 'k',
+				keycode: 75,
+				from: '2021-08-12',
+				expected: '2021-08-14',
+			},
+			// The following are incompatible with cy..type
+			// {
+			// 	key: '+',
+			// 	keycode: 61,
+			// 	from: '2021-08-12',
+			// 	expected: '2021-08-13',
+			// },
+			// {
+			// 	key: 'numpad +',
+			// 	keycode: 107,
+			// 	from: '2021-08-12',
+			// 	expected: '2021-08-13',
+			// },
+			// {
+			// 	key: '-',
+			// 	keycode: 173,
+			// 	from: '2021-08-12',
+			// 	expected: '2021-08-11',
+			// },
+			// {
+			// 	key: 'numpad -',
+			// 	keycode: 109,
+			// 	from: '2021-08-12',
+			// 	expected: '2021-08-11',
+			// },
+			//TODO 't' key
+		];
+		let control_types = [
+			{ fieldname: 'date', controltype: 'Date' }, { fieldname: 'datetime', controltype: 'DateTime' }
+		];
+		control_types.forEach(type => {
+			keys.forEach((config) => {
+				it(`'${config.key}' ${type.controltype} Hotkey`, () => {
+					cy.set_value('System Settings', 'System Settings', {
+						date_format: 'yyyy-mm-dd',
+						time_format: 'HH:mm'
+					});
+					cy.window()
+						.its('frappe')
+						.then(frappe => {
+							frappe.sys_defaults.date_format = 'yyyy-mm-dd';
+							frappe.sys_defaults.time_format = 'HH:mm';
+						});
+
+					cy.new_form(doctype_name);
+					cy.fill_field(type.fieldname, config.from, type.controltype).blur();
+					cy.get_field(type.fieldname).should('include.value', config.from);
+
+					cy.get_field(type.fieldname).click({ force: true });
+					cy.get_field(type.fieldname).type(config.key);
+
+					cy.get_field(type.fieldname).should('include.value', config.expected);
+				});
+			});
+		});
+
+	});
+
+
 });
