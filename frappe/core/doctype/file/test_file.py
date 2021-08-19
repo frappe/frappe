f80e68302afaad9c5e32dfb7c2d5a5b3b22fc892
Suraj Shetty <surajshetty3416@gmail.com>
2021-08-19 16:02:20 +0530

    test: Fix file_name assertion
    (cherry picked from commit 21e664b23b07797765b79d42fc222985277e8ff7)

--- frappe/core/doctype/file/test_file.py
+++ frappe/core/doctype/file/test_file.py
@@ -4,6 +4,7 @@
 from __future__ import unicode_literals
 
 import base64
+import json
 import frappe
 import os
 import unittest
@@ -416,7 +417,7 @@ class TestFile(unittest.TestCase):
 		test_file.reload()
 		test_file.file_url = frappe.utils.get_url('unknown.jpg')
 		test_file.make_thumbnail(suffix="xs")
-		self.assertEqual(frappe.message_log[0], '{"message": "File \'http://test-site:8000/unknown.jpg\' not found"}')
+		self.assertEqual(json.loads(frappe.message_log[0]), {"message": f"File '{frappe.utils.get_url('unknown.jpg')}' not found"})
 		self.assertEquals(test_file.thumbnail_url, None)
 
 class TestAttachment(unittest.TestCase):
