e555d0bd92e159637ec1338aec3a9dec6ee5f485
ollyboy <mcd.steven@gmail.com>
2021-08-21 22:17:33 +0000

    fix duplicate name columns in list_view when no title field defined

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -367,7 +367,8 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 
 		if (
 			!this.settings.hide_name_column &&
-			this.meta.title_field !== 'name'
+			this.meta.title_field !== 'name' &&
+			this.meta.title_field !== undefined
 		) {
 			this.columns.push({
 				type: "Field",
