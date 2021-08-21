7db68a1878990ad1aba16580e28ea0d407be2def
Faris Ansari <netchamp.faris@gmail.com>
2021-08-21 18:07:40 +0530

    fix: Handle on_page_show

--- frappe/printing/page/print_format_builder_beta/print_format_builder_beta.js
+++ frappe/printing/page/print_format_builder_beta/print_format_builder_beta.js
@@ -5,47 +5,49 @@ frappe.pages["print-format-builder-beta"].on_page_load = function(wrapper) {
 		single_column: true
 	});
 
-	function load_print_format_builder_beta() {
-		let route = frappe.get_route();
-		let $parent = $(wrapper).find(".layout-main-section");
-		$parent.empty();
-
-		if (route.length > 1) {
-			frappe.require("print_format_builder.bundle.js").then(() => {
-				frappe.print_format_builder = new frappe.ui.PrintFormatBuilder({
-					wrapper: $parent,
-					page,
-					print_format: route[1]
-				});
-			});
-		} else {
-			let d = new frappe.ui.Dialog({
-				title: __("Select Print Format to edit"),
-				fields: [
-					{
-						label: __("Print Format"),
-						fieldname: "print_format",
-						fieldtype: "Link",
-						options: "Print Format",
-						filters: {
-							print_format_builder_beta: 1
-						}
-					}
-				],
-				primary_action({ print_format }) {
-					if (!print_format) return;
-					frappe.set_route("print-format-builder-beta", print_format);
-				}
-			});
-			d.show();
-		}
-	}
-
-	load_print_format_builder_beta();
-
 	// hot reload in development
 	if (frappe.boot.developer_mode) {
 		frappe.hot_update = frappe.hot_update || [];
-		frappe.hot_update.push(load_print_format_builder_beta);
+		frappe.hot_update.push(() => load_print_format_builder_beta(wrapper));
 	}
 };
+
+frappe.pages["print-format-builder-beta"].on_page_show = function(wrapper) {
+	load_print_format_builder_beta(wrapper);
+};
+
+function load_print_format_builder_beta(wrapper) {
+	let route = frappe.get_route();
+	let $parent = $(wrapper).find(".layout-main-section");
+	$parent.empty();
+
+	if (route.length > 1) {
+		frappe.require("print_format_builder.bundle.js").then(() => {
+			frappe.print_format_builder = new frappe.ui.PrintFormatBuilder({
+				wrapper: $parent,
+				page: wrapper.page,
+				print_format: route[1]
+			});
+		});
+	} else {
+		let d = new frappe.ui.Dialog({
+			title: __("Select Print Format to edit"),
+			fields: [
+				{
+					label: __("Print Format"),
+					fieldname: "print_format",
+					fieldtype: "Link",
+					options: "Print Format",
+					filters: {
+						print_format_builder_beta: 1
+					}
+				}
+			],
+			primary_action({ print_format }) {
+				if (!print_format) return;
+				frappe.set_route("print-format-builder-beta", print_format);
+			}
+		});
+		d.show();
+	}
+}
