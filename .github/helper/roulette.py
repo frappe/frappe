03dba0ec7930fa0a159447aa8ed7fd58cdd7d3bc
Gavin D'souza <gavin18d@gmail.com>
2021-08-24 14:14:42 +0530

    ci: Roulette for GHA

--- .github/helper/roulette.py
+++ .github/helper/roulette.py
@@ -1,4 +1,3 @@
-# if the script ends with exit code 0, then no tests are run further, else all tests are run
 import os
 import re
 import shlex
@@ -7,50 +6,50 @@ import sys
 
 
 def get_output(command, shell=True):
-    print(command)
-    command = shlex.split(command)
-    return subprocess.check_output(command, shell=shell, encoding="utf8").strip()
+	print(command)
+	command = shlex.split(command)
+	return subprocess.check_output(command, shell=shell, encoding="utf8").strip()
 
 def is_py(file):
-    return file.endswith("py")
+	return file.endswith("py")
+
+def is_ci(file):
+	return file.endswith("yml") or ".github" in file
 
 def is_js(file):
-    return file.endswith("js")
+	return file.endswith("js")
 
 def is_docs(file):
-    regex = re.compile('\.(md|png|jpg|jpeg)$|^.github|LICENSE')
-    return bool(regex.search(file))
+	regex = re.compile(r'\.(md|png|jpg|jpeg)$|^.github|LICENSE')
+	return bool(regex.search(file))
 
 
 if __name__ == "__main__":
-    build_type = os.environ.get("TYPE")
-    before = os.environ.get("BEFORE")
-    after = os.environ.get("AFTER")
-    commit_range = before + '...' + after
-    print("Build Type: {}".format(build_type))
-    print("Commit Range: {}".format(commit_range))
+	files_list = sys.argv[1:]
+	build_type = os.environ.get("TYPE")
 
-    try:
-        files_changed = get_output("git diff --name-only {}".format(commit_range), shell=False)
-    except Exception:
-        sys.exit(2)
+	if not files_list:
+		print("No files' changes detected. Build is shutting")
+		sys.exit(0)
 
-    if "fatal" not in files_changed:
-        files_list = files_changed.split()
-        only_docs_changed = len(list(filter(is_docs, files_list))) == len(files_list)
-        only_js_changed = len(list(filter(is_js, files_list))) == len(files_list)
-        only_py_changed = len(list(filter(is_py, files_list))) == len(files_list)
+	ci_files_changed = any(f for f in files_list if is_ci(f))
+	only_docs_changed = len(list(filter(is_docs, files_list))) == len(files_list)
+	only_js_changed = len(list(filter(is_js, files_list))) == len(files_list)
+	only_py_changed = len(list(filter(is_py, files_list))) == len(files_list)
 
-        if only_docs_changed:
-            print("Only docs were updated, stopping build process.")
-            sys.exit(0)
+	if ci_files_changed:
+		print("CI related files were updated, running all build processes.")
 
-        if only_js_changed and build_type == "server":
-            print("Only JavaScript code was updated; Stopping Python build process.")
-            sys.exit(0)
+	if only_docs_changed:
+		print("Only docs were updated, stopping build process.")
+		sys.exit(0)
 
-        if only_py_changed and build_type == "ui":
-            print("Only Python code was updated, stopping Cypress build process.")
-            sys.exit(0)
+	if only_js_changed and build_type == "server":
+		print("Only JavaScript code was updated; Stopping Python build process.")
+		sys.exit(0)
 
-    sys.exit(2)
+	if only_py_changed and build_type == "ui":
+		print("Only Python code was updated, stopping Cypress build process.")
+		sys.exit(0)
+
+	os.system('echo "::set-output name=build::strawberry"')
