7dd632dbef56bf90d598a8cf20a4185da2cf1179
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-08-26 18:51:54 +0530

    wip: adding support for sql functions in frappe ORM

--- frappe/database/query.py
+++ frappe/database/query.py
@@ -1,6 +1,7 @@
-from typing import Tuple, Union, List, Any
-import frappe
 import operator
+from typing import Any, Dict, List, Tuple, Union
+
+import frappe
 
 
 class Query:
@@ -25,7 +26,7 @@ class Query:
 		self.sql_functions = ["sum", "now", "interval"]
 
 	@staticmethod
-	def like(key: str, value: Any):
+	def like(key: str, value: str) -> frappe.qb:
 		"""Wrapper method for `LIKE`
 
 		Args:
@@ -33,61 +34,127 @@ class Query:
 			value (ANy): criterion
 
 		Returns:
-			[frappe.qb]: frappe.qb object with `LIKE`
+			frappe.qb: `frappe.qb object with `LIKE`
 		"""
 		return frappe.qb.Field(key).like(value)
 
 	@staticmethod
-	def func_in(key, value):
+	def func_in(key: str, value: Union[List, Tuple]) -> frappe.qb:
+		"""Wrapper method for `IN`
+
+		Args:
+			key (str): field
+			value (ANy): criterion
+
+		Returns:
+			frappe.qb: `frappe.qb object with `IN`
+		"""
 		return frappe.qb.Field(key).isin(value)
 
 	@staticmethod
-	def not_like(key, value):
+	def not_like(key: str, value: str) -> frappe.qb:
+		"""Wrapper method for `NOT LIKE`
+
+		Args:
+			key (str): field
+			value (ANy): criterion
+
+		Returns:
+			frappe.qb: `frappe.qb object with `NOT LIKE`
+		"""
 		return frappe.qb.Field(key).not_like(value)
 
 	@staticmethod
-	def func_not_in(key, value):
+	def func_not_in(key: str, value: Union[List, Tuple]):
+		"""Wrapper method for `NOT IN`
+
+		Args:
+			key (str): field
+			value (ANy): criterion
+
+		Returns:
+			frappe.qb: `frappe.qb object with `NOT IN`
+		"""
 		return frappe.qb.Field(key).notin(value)
 
 	@staticmethod
-	def func_regex(key, value):
+	def func_regex(key: str, value: str) -> frappe.qb:
+		"""Wrapper method for `REGEX`
+
+		Args:
+			key (str): field
+			value (ANy): criterion
+
+		Returns:
+			frappe.qb: `frappe.qb object with `REGEX`
+		"""
 		return frappe.qb.Field(key).regex(value)
 
 	@staticmethod
-	def func_between(key, value):
+	def func_between(key: str, value: Union[List, Tuple]) -> frappe.qb:
+		"""Wrapper method for `BETWEEN`
+
+		Args:
+			key (str): field
+			value (ANy): criterion
+
+		Returns:
+			frappe.qb: `frappe.qb object with `BETWEEN`
+		"""
 		return frappe.qb.Field(key)[slice(*value)]
 
 	@staticmethod
-	def get_func_obj(func: str):
+	def get_func_obj(func: str) -> frappe.qb.functions:
+		"""Wrapper method for generating custom functions
+
+		Args:
+			key (str): field
+			value (ANy): criterion
+
+		Returns:
+			frappe.qb.functions
+		"""
 		return frappe.qb.functions(func)
 
-	def build_conditions(self, filters, table):
-		"""
-		filters = {columns: condition}
-		frappe.qb.from_(table).where(conditions)
-		"""
+	def make_function(self, key: Any, value: Union[int, str]):
+		return self.operator_map[value[0]](key, value[1])
 
-		conditions = frappe.qb.from_(table)
+	def dict_query(self, filters: Dict[str, Union[str, int]], table: str):
+			conditions = frappe.qb.from_(table)
+			for key in filters:
+				value = filters.get(key)
+				_operator = self.operator_map["="]
 
-		def _query(key):
-			nonlocal conditions
-			value = filters.get(key)
-			_operator = self.operator_map["="]
-			if isinstance(value, (list, tuple)):
-				if isinstance(value[1], (list, tuple)) or value[0] in list(self.operator_map.keys())[-4:]:
-					_operator = self.operator_map[value[0]]
-					conditions = conditions.where(_operator(key, value[1]))
+				if not isinstance(key, str):
+					conditions = conditions.where(self.make_function(key, value))
+					continue
+				if isinstance(value, (list, tuple)):
+					if isinstance(value[1], (list, tuple)) or value[0] in list(self.operator_map.keys())[-4:]:
+						_operator = self.operator_map[value[0]]
+						conditions = conditions.where(_operator(key, value[1]))
+					else:
+						_operator = self.operator_map[value[0]]
+						conditions = conditions.where(_operator(frappe.qb.Field(key), value[1]))
 				else:
-					_operator = self.operator_map[value[0]]
-					conditions = conditions.where(_operator(frappe.qb.Field(key), value[1]))
-			else:
-				conditions = conditions.where(_operator(frappe.qb.Field(key), value))
+					conditions = conditions.where(_operator(frappe.qb.Field(key), value))
+
+			return conditions
+
+	def build_conditions(self, filters: Union[Dict[str, Union[str, int]], str, int], table: str) -> frappe.qb:
+		"""Build conditions for sql query
+
+		Args:
+			filters (Union[Dict[str, Union[str, int]], str, int]): conditions built from filters provided
+			table (str): DocType
+
+		Returns:
+			frappe.qb: frappe.qb conditions object
+		"""
+		if isinstance(filters, list):
+			pass
 
 		if isinstance(filters, int) or isinstance(filters, str):
 			filters = {"name": str(filters)}
 
-		if filters:
-			for f in filters:
-				_query(f)
-
-		return conditions
+		if filters and isinstance(filters, dict):
+			return self.dict_query(filters, table)
