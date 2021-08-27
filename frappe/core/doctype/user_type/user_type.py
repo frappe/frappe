5669d9c5591fd266bbd7346339b184128b0802cd
Your Name <as7122000@gmail.com>
2021-08-28 01:42:20 +0530

    fix: pass distinct as argument in set_modules

--- frappe/core/doctype/user_type/user_type.py
+++ frappe/core/doctype/user_type/user_type.py
@@ -36,8 +36,11 @@ class UserType(Document):
 		if not self.user_doctypes:
 			return
 
-		modules = frappe.get_all('DocType', fields=['distinct module as module'],
-			filters={'name': ('in', [d.document_type for d in self.user_doctypes])})
+		modules = frappe.get_all('DocType',
+								fields=['module'],
+								filters={'name': ('in', [d.document_type for d in self.user_doctypes])},
+								distinct=True
+							)
 
 		self.set('user_type_modules', [])
 		for row in modules:
