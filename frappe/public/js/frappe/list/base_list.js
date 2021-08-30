72169124b2149d64ed39977d2b9904e72e8393c6
Shariq Ansari <sharique.rik@gmail.com>
2021-08-30 22:12:02 +0530

    fix: Removed unnessary code to get unique data in base_list
    (cherry picked from commit a62578a4b9663a1cb6c6311ee70a6aaa0f773d4d)
    
    # Conflicts:
    #	frappe/public/js/frappe/list/base_list.js

--- frappe/public/js/frappe/list/base_list.js
+++ frappe/public/js/frappe/list/base_list.js
@@ -329,9 +329,9 @@ frappe.views.BaseList = class BaseList {
 	}
 
 	get_group_by() {
-		let name_field = this.fields && this.fields.filter(f => f[0] == 'name');
-		if (name_field.length == 1) {
-			return frappe.model.get_full_column_name(name_field[0][0], name_field[0][1]);
+		let name_field = this.fields && this.fields.find(f => f[0] == 'name');
+		if (name_field) {
+			return frappe.model.get_full_column_name(name_field[0], name_field[1]);
 		}
 		return null;
 	}
@@ -406,8 +406,11 @@ frappe.views.BaseList = class BaseList {
 		} else {
 			this.data = this.data.concat(data);
 		}
+<<<<<<< HEAD
 
 		this.data = this.data.uniqBy(d => d.name);
+=======
+>>>>>>> a62578a4b9 (fix: Removed unnessary code to get unique data in base_list)
 	}
 
 	freeze() {
