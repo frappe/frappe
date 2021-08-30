5f85ba6c304a6dd2e565321dc3136e46065994d5
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2021-08-30 10:43:29 +0530

    fix: Use old super syntax

--- frappe/public/js/frappe/form/controls/datetime.js
+++ frappe/public/js/frappe/form/controls/datetime.js
@@ -37,7 +37,7 @@ frappe.ui.form.ControlDatetime = frappe.ui.form.ControlDate.extend({
 		}
 	},
 	get_model_value() {
-		let value = super.get_model_value();
+		let value = this._super();
 		return frappe.datetime.get_datetime_as_string(value);
 	}
 });
