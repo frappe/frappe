f7d3c16a9a2066569e3b2749bfa6a692857cb9d4
Gavin D'souza <gavin18d@gmail.com>
2021-08-30 18:51:36 +0530

    test: Add tests for frappe.utils.data.cast
    (cherry picked from commit be72397bcad856a1acbf1ac0fa2202966504afae)
    
    # Conflicts:
    #	frappe/tests/test_utils.py

--- frappe/tests/test_utils.py
+++ frappe/tests/test_utils.py
@@ -6,6 +6,16 @@ import unittest
 
 from frappe.utils import evaluate_filters, money_in_words, scrub_urls, get_url
 from frappe.utils import ceil, floor
+<<<<<<< HEAD
+=======
+from frappe.utils.data import cast, validate_python_code
+
+from PIL import Image
+from frappe.utils.image import strip_exif_data, optimize_image
+import io
+from mimetypes import guess_type
+from datetime import datetime, timedelta, date
+>>>>>>> be72397bca (test: Add tests for frappe.utils.data.cast)
 
 class TestFilters(unittest.TestCase):
 	def test_simple_dict(self):
@@ -86,6 +96,34 @@ class TestDataManipulation(unittest.TestCase):
 		self.assertTrue('style="background-image: url(\'{0}/assets/frappe/bg.jpg\') !important"'.format(url) in html)
 		self.assertTrue('<a href="mailto:test@example.com">email</a>' in html)
 
+class TestFieldCasting(unittest.TestCase):
+	def test_float_types(self):
+		FLOAT_TYPES = ("Currency", "Float", "Percent")
+		for fieldtype in FLOAT_TYPES:
+			self.assertIsInstance(cast(fieldtype, value=None), float)
+			self.assertIsInstance(cast(fieldtype, value=1.12), float)
+			self.assertIsInstance(cast(fieldtype, value=112), float)
+
+	def test_int_types(self):
+		INT_TYPES = ("Int", "Check")
+
+		for fieldtype in INT_TYPES:
+			self.assertIsInstance(cast(fieldtype, value=None), int)
+			self.assertIsInstance(cast(fieldtype, value=1.12), int)
+			self.assertIsInstance(cast(fieldtype, value=112), int)
+
+	def test_datetime_types(self):
+		self.assertIsInstance(cast("Datetime", value=None), datetime)
+		self.assertIsInstance(cast("Datetime", value="12-2-22"), datetime)
+
+	def test_date_types(self):
+		self.assertIsInstance(cast("Date", value=None), date)
+		self.assertIsInstance(cast("Date", value="12-12-2021"), date)
+
+	def test_time_types(self):
+		self.assertIsInstance(cast("Time", value=None), timedelta)
+		self.assertIsInstance(cast("Time", value="12:03:34"), timedelta)
+
 class TestMathUtils(unittest.TestCase):
 	def test_floor(self):
 		from decimal import Decimal
@@ -122,3 +160,106 @@ class TestHTMLUtils(unittest.TestCase):
 		clean = clean_email_html(sample)
 		self.assertTrue('<h1>Hello</h1>' in clean)
 		self.assertTrue('<a href="http://test.com">text</a>' in clean)
+<<<<<<< HEAD
+=======
+
+class TestValidationUtils(unittest.TestCase):
+	def test_valid_url(self):
+		# Edge cases
+		self.assertFalse(validate_url(''))
+		self.assertFalse(validate_url(None))
+
+		# Valid URLs
+		self.assertTrue(validate_url('https://google.com'))
+		self.assertTrue(validate_url('http://frappe.io', throw=True))
+
+		# Invalid URLs without throw
+		self.assertFalse(validate_url('google.io'))
+		self.assertFalse(validate_url('google.io'))
+
+		# Invalid URL with throw
+		self.assertRaises(frappe.ValidationError, validate_url, 'frappe', throw=True)
+
+		# Scheme validation
+		self.assertFalse(validate_url('https://google.com', valid_schemes='http'))
+		self.assertTrue(validate_url('ftp://frappe.cloud', valid_schemes=['https', 'ftp']))
+		self.assertFalse(validate_url('bolo://frappe.io', valid_schemes=("http", "https", "ftp", "ftps")))
+		self.assertRaises(
+			frappe.ValidationError,
+			validate_url,
+			'gopher://frappe.io',
+			valid_schemes='https',
+			throw=True
+		)
+
+	def test_valid_email(self):
+		# Edge cases
+		self.assertFalse(validate_email_address(''))
+		self.assertFalse(validate_email_address(None))
+
+		# Valid addresses
+		self.assertTrue(validate_email_address('someone@frappe.com'))
+		self.assertTrue(validate_email_address('someone@frappe.com, anyone@frappe.io'))
+
+		# Invalid address
+		self.assertFalse(validate_email_address('someone'))
+		self.assertFalse(validate_email_address('someone@----.com'))
+
+		# Invalid with throw
+		self.assertRaises(
+			frappe.InvalidEmailAddressError,
+			validate_email_address,
+			'someone.com',
+			throw=True
+		)
+
+class TestImage(unittest.TestCase):
+	def test_strip_exif_data(self):
+		original_image = Image.open("../apps/frappe/frappe/tests/data/exif_sample_image.jpg")
+		original_image_content = io.open("../apps/frappe/frappe/tests/data/exif_sample_image.jpg", mode='rb').read()
+
+		new_image_content = strip_exif_data(original_image_content, "image/jpeg")
+		new_image = Image.open(io.BytesIO(new_image_content))
+
+		self.assertEqual(new_image._getexif(), None)
+		self.assertNotEqual(original_image._getexif(), new_image._getexif())
+
+	def test_optimize_image(self):
+		image_file_path = "../apps/frappe/frappe/tests/data/sample_image_for_optimization.jpg"
+		content_type = guess_type(image_file_path)[0]
+		original_content = io.open(image_file_path, mode='rb').read()
+
+		optimized_content = optimize_image(original_content, content_type, max_width=500, max_height=500)
+		optimized_image = Image.open(io.BytesIO(optimized_content))
+		width, height = optimized_image.size
+
+		self.assertLessEqual(width, 500)
+		self.assertLessEqual(height, 500)
+		self.assertLess(len(optimized_content), len(original_content))
+
+class TestPythonExpressions(unittest.TestCase):
+	def test_validation_for_good_python_expression(self):
+		valid_expressions = [
+			"foo == bar",
+			"foo == 42",
+			"password != 'hunter2'",
+			"complex != comparison and more_complex == condition",
+			"escaped_values == 'str with newline\\n'",
+			"check_box_field",
+		]
+		for expr in valid_expressions:
+			try:
+				validate_python_code(expr)
+			except Exception as e:
+				self.fail(f"Invalid error thrown for valid expression: {expr}: {str(e)}")
+
+	def test_validation_for_bad_python_expression(self):
+		invalid_expressions = [
+			"these_are && js_conditions",
+			"more || js_conditions",
+			"curly_quotes_bad == “const”",
+			"oops = forgot_equals",
+		]
+		for expr in invalid_expressions:
+			self.assertRaises(frappe.ValidationError, validate_python_code, expr)
+>>>>>>> be72397bca (test: Add tests for frappe.utils.data.cast)
