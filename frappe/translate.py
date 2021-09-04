e5369f756eae1f19d6a815b0c2a709e776a6048e
Vama Mehta <vama.mehta@inqubit.in>
2021-09-04 21:36:31 +0530

    (fix) Problems while using translations via Globe Symbol #14118

--- frappe/translate.py
+++ frappe/translate.py
@@ -846,6 +846,9 @@ def update_translations_for_source(source=None, translation_dict=None):
 
 	translation_dict = json.loads(translation_dict)
 
+	if is_html(source):
+		source = strip_html_tags(source)
+
 	# for existing records
 	translation_records = frappe.db.get_values('Translation', {
 		'source_text': source
