8e9a46fff9f230f00c6dfb29bcfda2ceeec2c38a
Suraj Shetty <surajshetty3416@gmail.com>
2021-09-07 19:57:57 +0530

    test: Miscellaneous fixes to avoid flaky tests

--- cypress/integration/datetime_field_form_validation.js
+++ cypress/integration/datetime_field_form_validation.js
@@ -2,18 +2,15 @@ context('Datetime Field Validation', () => {
 	before(() => {
 		cy.login();
 		cy.visit('/app/communication');
-		cy.window().its('frappe').then(frappe => {
-			frappe.call("frappe.tests.ui_test_helpers.create_communication_records");
-		});
 	});
 
-	// validating datetime field value when value is set from backend and get validated on form load.
 	it('datetime field form validation', () => {
-		cy.visit('/app/communication');
-		cy.get('a[title="Test Form Communication 1"]').invoke('attr', 'data-name')
-			.then((name) => {
-				cy.visit(`/app/communication/${name}`);
-				cy.get('.indicator-pill').should('contain', 'Open').should('have.class', 'red');
-			});
+		// validating datetime field value when value is set from backend and get validated on form load.
+		cy.window().its('frappe').then(frappe => {
+			return frappe.xcall("frappe.tests.ui_test_helpers.create_communication_record");
+		}).then(doc => {
+			cy.visit(`/app/communication/${doc.name}`);
+			cy.get('.indicator-pill').should('contain', 'Open').should('have.class', 'red');
+		});
 	});
 });
