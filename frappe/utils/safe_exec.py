a1251aace99bef5166925e79926a146f79bd7cb6
Deepesh Garg <deepeshgarg6@gmail.com>
2021-09-07 18:29:04 +0530

    fix: Syntax Error

--- frappe/utils/safe_exec.py
+++ frappe/utils/safe_exec.py
@@ -110,7 +110,7 @@ def get_safe_globals():
 			socketio_port=frappe.conf.socketio_port,
 			get_hooks=frappe.get_hooks,
 			sanitize_html=frappe.utils.sanitize_html,
-			log_error=frappe.log_error
+			log_error=frappe.log_error,
 			cache=frappe.cache 
 		),
 		FrappeClient=FrappeClient,
