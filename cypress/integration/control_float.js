7355c8e04d1d5402dd85a686e4dfc2853697c645
Mohammad Hasnain <hasnain2808@gmail.com>
2021-09-08 15:02:56 +0530

    test: tests for float control
    (cherry picked from commit 9eff8bee21de05ac23e92457353f005ccb2207d1)

---
+++ cypress/integration/control_float.js
@@ -0,0 +1,28 @@
+context('Control Float', () => {
+	before(() => {
+		cy.login();
+		cy.visit('/app/website');
+		cy.window().its('frappe').then(frappe => {
+			frappe.boot.sysdefaults.number_format = '#.###,##'
+		});
+	});
+
+	function get_dialog_with_float() {
+		return cy.dialog({
+			title: 'Float Check',
+			fields: [{
+				'fieldname': 'float_number',
+				'fieldtype': 'Float',
+				'Label': 'Float',
+			}]
+		});
+	}
+
+	it('check value changes', () => {
+		get_dialog_with_float().as('dialog');
+		cy.get_field('float_number', 'Float').clear()
+		cy.fill_field('float_number', '36487,334', 'Float').blur();
+		cy.get_field('float_number', 'Float').should('have.value', '36.487,334');
+
+	});
+});
