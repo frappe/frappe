21a4331be45ab32a2b6dd12c7d45b5f7d612c60b
Mohammad Hasnain <hasnain2808@gmail.com>
2021-09-09 14:27:23 +0530

    revert: removal of flt on focus event

--- frappe/public/js/frappe/form/controls/int.js
+++ frappe/public/js/frappe/form/controls/int.js
@@ -8,6 +8,17 @@ frappe.ui.form.ControlInt = class ControlInt extends frappe.ui.form.ControlData 
 	make_input () {
 		var me = this;
 		super.make_input();
+		this.$input
+			// .addClass("text-right")
+			.on("focus", function () {
+				setTimeout(function () {
+					if (!document.activeElement) return;
+					document.activeElement.value
+						= me.validate(document.activeElement.value);
+					document.activeElement.select();
+				}, 100);
+				return false;
+			});
 	}
 	validate (value) {
 		return this.parse(value);
