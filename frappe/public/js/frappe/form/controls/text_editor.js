5dd22876d0a01ddb0ae2f7b643e5350323000adb
Vama Mehta <vama.mehta@inqubit.in>
2021-09-12 01:37:46 +0530

    fix: TextEditor - Text editor will now save unordered list #14104

--- frappe/public/js/frappe/form/controls/text_editor.js
+++ frappe/public/js/frappe/form/controls/text_editor.js
@@ -212,7 +212,21 @@ frappe.ui.form.ControlTextEditor = frappe.ui.form.ControlCode.extend({
 		} catch(e) {
 			value = `<div class="ql-editor read-mode">${value}</div>`;
 		}
-
+	
+		// quill keeps ol as a common container for both type of lists
+		// and uses css for appearances, this is not semantic
+		// so we convert ol to ul if it is unordered
+		const $value = $(`<div>${value}</div>`);
+		$value.find('ol li[data-list=bullet]:first-child').each((i, li) => {
+			let $li = $(li);
+			let $parent = $li.parent();
+			let $children = $parent.children();
+			let $ul = $('<ul>').append($children);
+			$parent.replaceWith($ul);
+		});
+	
+		value = $value.html();
+	
 		return value;
 	},
 
