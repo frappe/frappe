49b2c43c889567cfc3bb787b133f361ee25d80a6
Faris Ansari <netchamp.faris@gmail.com>
2021-09-14 18:12:58 +0530

    Revert "feat(minor): empty state for list view"
    This reverts commit 0ee7c94a9f586539941178d2d7d7e06b9d26b95e.

--- frappe/public/js/frappe/list/list_factory.js
+++ frappe/public/js/frappe/list/list_factory.js
@@ -8,48 +8,36 @@ frappe.views.ListFactory = class ListFactory extends frappe.views.Factory {
 	make (route) {
 		var me = this;
 		var doctype = route[1];
-		const meta_loaded = frappe.get_meta(doctype) ? true : false;
-		const page_name = frappe.get_route_str();
-		let view_class = this.get_view_class(route, doctype);
-		this.make_list_view_page(page_name, doctype, view_class);
-
-		if (view_class && view_class.load_last_view && view_class.load_last_view()) {
-			// view can have custom routing logic
-			return;
-		}
 
 		frappe.model.with_doctype(doctype, function () {
-			if (!meta_loaded) {
-				frappe.views.list_view[page_name].show();
-			}
 			if (locals['DocType'][doctype].issingle) {
 				frappe.set_re_route('Form', doctype);
 			} else {
-				frappe.container.change_to(page_name);
+				// List / Gantt / Kanban / etc
+				// File is a special view
+				const view_name = doctype !== 'File' ? frappe.utils.to_title_case(route[2] || 'List') : 'File';
+				let view_class = frappe.views[view_name + 'View'];
+				if (!view_class) view_class = frappe.views.ListView;
+
+				if (view_class && view_class.load_last_view && view_class.load_last_view()) {
+					// view can have custom routing logic
+					return;
+				}
+
+				frappe.provide('frappe.views.list_view.' + doctype);
+				const page_name = frappe.get_route_str();
+
+				if (!frappe.views.list_view[page_name]) {
+					frappe.views.list_view[page_name] = new view_class({
+						doctype: doctype,
+						parent: me.make_page(true, page_name)
+					});
+				} else {
+					frappe.container.change_to(page_name);
+				}
 				me.set_cur_list();
 			}
 		});
-	}
-
-	get_view_class(route, doctype) {
-		// List / Gantt / Kanban / etc
-		// File is a special view
-		const view_name = doctype !== 'File' ? frappe.utils.to_title_case(route[2] || 'List') : 'File';
-		let view_class = frappe.views[view_name + 'View'];
-		if (!view_class) view_class = frappe.views.ListView;
-
-		return view_class;
-	}
-
-	make_list_view_page(page_name, doctype, view_class) {
-		frappe.provide('frappe.views.list_view.' + doctype);
-
-		if (!frappe.views.list_view[page_name]) {
-			frappe.views.list_view[page_name] = new view_class({
-				doctype: doctype,
-				parent: this.make_page(true, page_name)
-			});
-		}
 	}
 
 	show() {
