4ebdabda485e19777bb64613d9eb0fefa3c76b27
Deepesh Garg <deepeshgarg6@gmail.com>
2021-10-04 10:59:47 +0530

    fix: Encapsulate cache methods

--- frappe/core/doctype/server_script/test_server_script.py
+++ frappe/core/doctype/server_script/test_server_script.py
@@ -79,7 +79,7 @@ frappe.db.commit()
 		reference_doctype = 'ToDo',
 		disabled = 1,
 		script = '''
-frappe.cache.set_value('test_key', doc.name)
+frappe.cache().set_value('test_key', doc.name)
 '''
 	)
 ]
