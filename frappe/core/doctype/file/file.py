05cb2c16cd037a9b6110a45295a46bfe2c07295b
MitulDavid <mituldavid@gmail.com>
2021-10-07 16:41:44 +0530

    fix: Optional parameter for setting extracted image as private

--- frappe/core/doctype/file/file.py
+++ frappe/core/doctype/file/file.py
@@ -813,7 +813,7 @@ def extract_images_from_doc(doc, fieldname):
 		doc.set(fieldname, content)
 
 
-def extract_images_from_html(doc, content):
+def extract_images_from_html(doc, content, is_private=False):
 	frappe.flags.has_dataurl = False
 
 	def _save_file(match):
@@ -839,7 +839,6 @@ def extract_images_from_html(doc, content):
 
 		doctype = doc.parenttype if doc.parent else doc.doctype
 		name = doc.parent or doc.name
-		is_private = True if doctype == "Comment" else False
 
 		_file = frappe.get_doc({
 			"doctype": "File",
