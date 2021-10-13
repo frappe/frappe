f9e2a58d22a42409b1d5fecd0e4743549110b1be
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-10-13 12:58:10 +0530

    test: scheduled job log creation

--- frappe/core/doctype/scheduled_job_type/test_scheduled_job_type.py
+++ frappe/core/doctype/scheduled_job_type/test_scheduled_job_type.py
@@ -14,6 +14,9 @@ class TestScheduledJobType(unittest.TestCase):
 		sync_jobs()
 		frappe.db.commit()
 
+	def tearDown(self):
+		frappe.db.rollback()
+
 	def test_sync_jobs(self):
 		all_job = frappe.get_doc('Scheduled Job Type',
 			dict(method='frappe.email.queue.flush'))
@@ -75,3 +78,56 @@ class TestScheduledJobType(unittest.TestCase):
 			cron_job.save()
 
 		self.assertTrue("not a valid cron expression" in str(err_msg.exception).lower())
+
+	def test_log_creation(self):
+		server_script = frappe.get_doc(dict(
+			doctype='Server Script',
+			name='test_log_creation',
+			script_type = 'Scheduler Event',
+			event_frequency='Hourly',
+			script = '''log("Test Log Creation")'''
+		)).insert()
+
+		scheduled_job = frappe.get_doc('Scheduled Job Type', dict(
+			server_script='test_log_creation'
+		))
+		self.assertTrue(scheduled_job)
+
+		# enable logging
+		scheduled_job.create_log = 1
+		scheduled_job.save()
+		scheduled_job.reload()
+		scheduled_job.execute()
+
+		# check if log created
+		log = frappe.get_last_doc('Scheduled Job Log')
+		self.assertEquals(log.scheduled_job_type, scheduled_job.name)
+		self.assertEquals(log.status, 'Complete')
+		self.assertTrue(log.start)
+		self.assertTrue(log.end)
+		self.assertTrue(log.completed_in)
+
+		# enable only failure logging
+		scheduled_job.create_failure_log = 1
+		scheduled_job.save()
+		scheduled_job.reload()
+		scheduled_job.execute()
+
+		# check if no new log is created
+		prev_log_name = log.name
+		log = frappe.get_last_doc('Scheduled Job Log')
+		self.assertEquals(prev_log_name, log.name)
+
+		# edit the script to raise an error
+		server_script.script = "print('Raise Error')"
+		server_script.save()
+		scheduled_job.execute()
+
+		# check if failed log is created
+		log = frappe.get_last_doc('Scheduled Job Log')
+		self.assertEquals(log.scheduled_job_type, scheduled_job.name)
+		self.assertEquals(log.status, 'Failed')
+		self.assertTrue(log.details)
+
+		server_script.reload()
+		server_script.delete()
