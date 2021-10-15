656dca3e3d1e728fd020d2c3f618413a24192ffa
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-10-15 12:36:06 +0530

    patch: scheduler server script frequency & queue

--- frappe/patches/v13_0/update_schedule_job_type_frequency_and_queue.py
+++ frappe/patches/v13_0/update_schedule_job_type_frequency_and_queue.py
@@ -3,11 +3,20 @@ import frappe
 
 def execute():
 	frappe.reload_doc('core', 'doctype', 'scheduled_job_type')
+	frappe.reload_doc('core', 'doctype', 'server_script')
 
 	for job in frappe.get_all('Scheduled Job Type', ['name', 'frequency']):
 		if 'Long' in job.frequency:
 			frequency = job.frequency.split(' ')[0]
 			frappe.db.set_value('Scheduled Job Type', job.name, {
+				'frequency': frequency,
+				'queue': 'Long'
+			}, update_modified=True)
+
+	for script in frappe.get_all('Server Script', {'script_type': 'Scheduler Event'}, ['name', 'frequency']):
+		if 'Long' in script.frequency:
+			frequency = script.frequency.split(' ')[0]
+			frappe.db.set_value('Server Script', script.name, {
 				'frequency': frequency,
 				'queue': 'Long'
 			}, update_modified=True)
