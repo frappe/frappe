a89406a5cf60a09564b58b4c0bc3b2956c575c3e
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-10-15 12:26:19 +0530

    chore: reduce number of queries

--- frappe/patches/v13_0/update_schedule_job_type_frequency_and_queue.py
+++ frappe/patches/v13_0/update_schedule_job_type_frequency_and_queue.py
@@ -7,5 +7,7 @@ def execute():
 	for job in frappe.get_all('Scheduled Job Type', ['name', 'frequency']):
 		if 'Long' in job.frequency:
 			frequency = job.frequency.split(' ')[0]
-			frappe.db.set_value('Scheduled Job Type', job.name, 'frequency', frequency, update_modified=0)
-			frappe.db.set_value('Scheduled Job Type', job.name, 'queue', 'Long', update_modified=0)
+			frappe.db.set_value('Scheduled Job Type', job.name, {
+				'frequency': frequency,
+				'queue': 'Long'
+			}, update_modified=True)
