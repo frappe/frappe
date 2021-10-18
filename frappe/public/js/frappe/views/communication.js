0f5ab57cb9b4b5ee66481f92b1fc28b074dd970b
ci2014 <christoph.isik@wpci.de>
2021-10-18 19:45:37 +0200

    Only run get_email_template if template selected

--- frappe/public/js/frappe/views/communication.js
+++ frappe/public/js/frappe/views/communication.js
@@ -197,17 +197,19 @@ frappe.views.CommunicationComposer = Class.extend({
 				me.reply_added = email_template;
 			}
 
-			frappe.call({
-				method: 'frappe.email.doctype.email_template.email_template.get_email_template',
-				args: {
-					template_name: email_template,
-					doc: me.frm.doc,
-					_lang: me.dialog.get_value("language_sel")
-				},
-				callback: function(r) {
-					prepend_reply(r.message);
-				},
-			});
+			if (email_template && email_template !== '') {
+				frappe.call({
+					method: 'frappe.email.doctype.email_template.email_template.get_email_template',
+					args: {
+						template_name: email_template,
+						doc: me.frm.doc,
+						_lang: me.dialog.get_value("language_sel")
+					},
+					callback: function(r) {
+						prepend_reply(r.message);
+					},
+				});
+			}
 		}
 	},
 
