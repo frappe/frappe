d88bd035d02c7416c3e0c6d4985ae89a778e4acd
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2021-10-22 09:29:15 +0530

    refactor: Use LRU cache instead of redis cache

--- frappe/printing/page/print_format_builder_beta/print_format_builder_beta.py
+++ frappe/printing/page/print_format_builder_beta/print_format_builder_beta.py
@@ -3,13 +3,11 @@
 
 from __future__ import unicode_literals
 import frappe
+import functools
 
 
 @frappe.whitelist()
+@functools.lru_cache()
 def get_google_fonts():
-	return frappe.cache().get_value("google_fonts", generator=_get_google_fonts)
-
-
-def _get_google_fonts():
 	file_path = frappe.get_app_path("frappe", "data", "google_fonts.json")
 	return frappe.parse_json(frappe.read_file(file_path))
