c679c08675214f53cc366d125421a7221083bd51
Faris Ansari <netchamp.faris@gmail.com>
2021-10-22 12:04:27 +0530

    fix: lru_cache decorator usage

--- frappe/printing/page/print_format_builder_beta/print_format_builder_beta.py
+++ frappe/printing/page/print_format_builder_beta/print_format_builder_beta.py
@@ -7,7 +7,11 @@ import functools
 
 
 @frappe.whitelist()
-@functools.lru_cache()
 def get_google_fonts():
+	return _get_google_fonts()
+
+
+@functools.lru_cache()
+def _get_google_fonts():
 	file_path = frappe.get_app_path("frappe", "data", "google_fonts.json")
 	return frappe.parse_json(frappe.read_file(file_path))
