139ce05233f7a60b0197a901003e7c3c21e0a3cf
Suraj Shetty <surajshetty3416@gmail.com>
2021-10-27 13:00:16 +0530

    test: Test child table permission
    (cherry picked from commit 68cf640545d4f59c584fa05d09c136c0a6b3a0e5)

--- frappe/tests/test_permissions.py
+++ frappe/tests/test_permissions.py
@@ -595,3 +595,9 @@ class TestPermissions(unittest.TestCase):
 
 		# reset the user
 		frappe.set_user(current_user)
+
+	def test_child_table_permissions(self):
+		frappe.set_user("test@example.com")
+		self.assertIsInstance(frappe.get_list("Has Role", parent_doctype="User", limit=1), list)
+		self.assertRaisesRegex(frappe.exceptions.ValidationError, "ToDo is not a valid parent doctype Has Role", frappe.get_list, doctype="Has Role", parent_doctype="ToDo")
+		self.assertRaisesRegex(frappe.exceptions.ValidationError, "Please specify a valid parent doctype for Has Role", frappe.get_list, "Has Role")
