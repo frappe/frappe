bb3a6732d76fb4308d44c8550066f13fe26742da
Suraj Shetty <surajshetty3416@gmail.com>
2021-10-28 15:14:53 +0530

    fix: Check permission for address and contact list
    (cherry picked from commit 57df0bad82e8b4a28e17d5897089b1faf0a50022)

--- frappe/contacts/address_and_contact.py
+++ frappe/contacts/address_and_contact.py
@@ -17,7 +17,7 @@ def load_address_and_contact(doc, key=None):
 		["Dynamic Link", "link_name", "=", doc.name],
 		["Dynamic Link", "parenttype", "=", "Address"],
 	]
-	address_list = frappe.get_all("Address", filters=filters, fields=["*"])
+	address_list = frappe.get_list("Address", filters=filters, fields=["*"])
 
 	address_list = [a.update({"display": get_address_display(a)})
 		for a in address_list]
@@ -35,7 +35,7 @@ def load_address_and_contact(doc, key=None):
 		["Dynamic Link", "link_name", "=", doc.name],
 		["Dynamic Link", "parenttype", "=", "Contact"],
 	]
-	contact_list = frappe.get_all("Contact", filters=filters, fields=["*"])
+	contact_list = frappe.get_list("Contact", filters=filters, fields=["*"])
 
 	for contact in contact_list:
 		contact["email_ids"] = frappe.get_all("Contact Email", filters={
