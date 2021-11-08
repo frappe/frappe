61386fda9432eb428deda5fd57274200403b35e8
Jannat Patel <pateljannat2308@gmail.com>
2021-11-08 20:17:07 +0530

    fix: public info api
    (cherry picked from commit 88d8c1e56a7227c18deb12901cd96a4d855c0a82)
    
    # Conflicts:
    #	frappe/patches.txt

--- frappe/model/utils/update_github_endpoints.py
+++ frappe/model/utils/update_github_endpoints.py
@@ -3,7 +3,6 @@ import json
 
 def execute():
 	if frappe.db.exists("Social Login Key", "github"):
-		frappe.db.set_value("Social Login Key", "github", "api_endpoint", "user/emails")
 		frappe.db.set_value("Social Login Key", "github", "auth_url_data",
 			json.dumps({
 				"scope": "user:email"
