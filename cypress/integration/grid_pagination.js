0441059d2c0794d242bbb2a26ece3557f68de096
Shariq Ansari <sharique.rik@gmail.com>
2021-11-10 11:32:53 +0530

    test: Updated grid_pagination UI Test

--- cypress/integration/grid_pagination.js
+++ cypress/integration/grid_pagination.js
@@ -13,7 +13,7 @@ context('Grid Pagination', () => {
 	it('creates pages for child table', () => {
 		cy.visit('/app/contact/Test Contact');
 		cy.get('.frappe-control[data-fieldname="phone_nos"]').as('table');
-		cy.get('@table').find('.current-page-number').should('contain', '1');
+		cy.get('@table').find('.current-page-number').should('have.value', '1');
 		cy.get('@table').find('.total-page-number').should('contain', '20');
 		cy.get('@table').find('.grid-body .grid-row').should('have.length', 50);
 	});
@@ -21,10 +21,10 @@ context('Grid Pagination', () => {
 		cy.visit('/app/contact/Test Contact');
 		cy.get('.frappe-control[data-fieldname="phone_nos"]').as('table');
 		cy.get('@table').find('.next-page').click();
-		cy.get('@table').find('.current-page-number').should('contain', '2');
+		cy.get('@table').find('.current-page-number').should('have.value', '2');
 		cy.get('@table').find('.grid-body .grid-row').first().should('have.attr', 'data-idx', '51');
 		cy.get('@table').find('.prev-page').click();
-		cy.get('@table').find('.current-page-number').should('contain', '1');
+		cy.get('@table').find('.current-page-number').should('have.value', '1');
 		cy.get('@table').find('.grid-body .grid-row').first().should('have.attr', 'data-idx', '1');
 	});
 	it('adds and deletes rows and changes page', () => {
@@ -32,12 +32,12 @@ context('Grid Pagination', () => {
 		cy.get('.frappe-control[data-fieldname="phone_nos"]').as('table');
 		cy.get('@table').findByRole('button', {name: 'Add Row'}).click();
 		cy.get('@table').find('.grid-body .row-index').should('contain', 1001);
-		cy.get('@table').find('.current-page-number').should('contain', '21');
+		cy.get('@table').find('.current-page-number').should('have.value', '21');
 		cy.get('@table').find('.total-page-number').should('contain', '21');
 		cy.get('@table').find('.grid-body .grid-row .grid-row-check').click({ force: true });
 		cy.get('@table').findByRole('button', {name: 'Delete'}).click();
 		cy.get('@table').find('.grid-body .row-index').last().should('contain', 1000);
-		cy.get('@table').find('.current-page-number').should('contain', '20');
+		cy.get('@table').find('.current-page-number').should('have.value', '20');
 		cy.get('@table').find('.total-page-number').should('contain', '20');
 	});
 	// it('deletes all rows', ()=> {
