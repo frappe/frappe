fb7dda01335aae9325a68ede6f2fab82af342d0b
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2021-11-10 11:08:31 +0530

    fix: Resolve conflicts

--- frappe/email/doctype/auto_email_report/auto_email_report.py
+++ frappe/email/doctype/auto_email_report/auto_email_report.py
@@ -253,14 +253,8 @@ def make_links(columns, data):
 				if col.options and row.get(col.options):
 					row[col.fieldname] = get_link_to_form(row[col.options], row[col.fieldname])
 			elif col.fieldtype == "Currency":
-<<<<<<< HEAD
 				row[col.fieldname] = frappe.format_value(row[col.fieldname], col)
 
-=======
-				doc = frappe.get_doc(col.parent, doc_name) if doc_name and col.parent else None
-				# Pass the Document to get the currency based on docfield option
-				row[col.fieldname] = frappe.format_value(row[col.fieldname], col, doc=doc)
->>>>>>> 29785af915 (refactor: minor code cleanup in auto_email_report.py)
 	return columns, data
 
 def update_field_types(columns):
