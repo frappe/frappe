83d8b152b620f7c097015438f9f2ade1b0452dcc
Shariq Ansari <30859809+shariquerik@users.noreply.github.com>
2021-11-15 13:44:58 +0530

    test: minor fix

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -348,7 +348,7 @@ Cypress.Commands.add('click_listview_primary_button', (btn_name) => {
 	cy.get('.primary-action').contains(btn_name).click({force: true});
 });
 
-Cypress.Commands.add('click_timeline_action_btn', (btn_no) => {
+Cypress.Commands.add('click_timeline_action_btn', (btn_name) => {
 	cy.get('.timeline-message-box .actions .action-btn').contains(btn_name).click();
 });
 
