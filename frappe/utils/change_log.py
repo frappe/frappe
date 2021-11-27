ae31a0d6fbca7e947de5f212a26bbdb7e0fcfcbe
hrwx <himanshuwarekar@yahoo.com>
2021-11-27 00:24:21 +0000

    feat: add option to disable notification

--- frappe/utils/change_log.py
+++ frappe/utils/change_log.py
@@ -155,11 +155,11 @@ def check_for_update():
 		for update_type in updates:
 			if github_version.__dict__[update_type] > instance_version.__dict__[update_type]:
 				updates[update_type].append(frappe._dict(
-					current_version   = str(instance_version),
+					current_version	  = str(instance_version),
 					available_version = str(github_version),
-					org_name          = org_name,
-					app_name          = app,
-					title             = apps[app]['title'],
+					org_name		  = org_name,
+					app_name		  = app,
+					title			  = apps[app]['title'],
 				))
 				break
 			if github_version.__dict__[update_type] < instance_version.__dict__[update_type]: break
@@ -242,10 +242,11 @@ def add_message_to_redis(update_json):
 @frappe.whitelist()
 def show_update_popup():
 	cache = frappe.cache()
-	user  = frappe.session.user
+	user = frappe.session.user
+	system_settings = frappe.get_single("System Settings")
 
 	update_info = cache.get_value("update-info")
-	if not update_info:
+	if not update_info or system_settings.disable_system_update_notification:
 		return
 
 	updates = json.loads(update_info)
@@ -259,9 +260,9 @@ def show_update_popup():
 				app = frappe._dict(app)
 				release_links += "<b>{title}</b>: <a href='https://github.com/{org_name}/{app_name}/releases/tag/v{available_version}'>v{available_version}</a><br>".format(
 					available_version = app.available_version,
-					org_name          = app.org_name,
-					app_name          = app.app_name,
-					title             = app.title
+					org_name = app.org_name,
+					app_name = app.app_name,
+					title = app.title
 				)
 			if release_links:
 				message = _("New {} releases for the following apps are available").format(_(update_type))
