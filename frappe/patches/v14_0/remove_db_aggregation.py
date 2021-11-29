43fc713dd7ef881f8f76632b266965d94c98de21
Gavin D'souza <gavin18d@gmail.com>
2021-11-29 13:46:47 +0530

    refactor: Remove Aggregation methods from DB API
    * Make it DRY & make it "better"
    * Add to patches.txt so the patch runs :')
    * Style fixes - tabs > spaces for consistency, removed unnecessary
      blocks

--- frappe/patches/v14_0/remove_db_aggregation.py
+++ frappe/patches/v14_0/remove_db_aggregation.py
@@ -1,38 +1,32 @@
-from frappe.query_builder import DocType
-import frappe
 import re
+
+import frappe
+from frappe.query_builder import DocType
 
 
 def execute():
-    sub_aggregation()
+	"""Replace temporarily available Database Aggregate APIs on frappe (develop)
 
+	APIs changed:
+		* frappe.db.max => frappe.qb.max
+		* frappe.db.min => frappe.qb.min
+		* frappe.db.sum => frappe.qb.sum
+		* frappe.db.avg => frappe.qb.avg
+	"""
+	ServerScript = DocType("Server Script")
+	server_scripts = frappe.qb.from_(ServerScript).where(
+		ServerScript.script.like("%frappe.db.max(%")
+		| ServerScript.script.like("%frappe.db.min(%")
+		| ServerScript.script.like("%frappe.db.sum(%")
+		| ServerScript.script.like("%frappe.db.avg(%")
+	).select(
+		"name", "script"
+	).run(as_dict=True)
 
-def sub_aggregation():
-    server_scripts = DocType("Server Script")
-    scripts = (
-        frappe.qb.from_(server_scripts)
-        .where(
-            server_scripts.script.like("%frappe.db.max%")
-            | server_scripts.script.like("%frappe.db.min%")
-            | server_scripts.script.like("%frappe.db.sum%")
-            | server_scripts.script.like("%frappe.db.avg%")
-        )
-        .select("name", "script")
-        .run(as_dict=True)
-    )
+	for server_script in server_scripts:
+		name, script = server_script["name"], server_script["script"]
 
-    def _sub_aggregation(scripts, function, substitution):
-        for script in scripts:
-            script.update(
-                {"script": re.sub(f"{function}", f"{substitution}", script["script"])}
-            )
+		for agg in ["avg", "max", "min", "sum"]:
+			script = re.sub(f"frappe.db.{agg}(", f"frappe.qb.{agg}(", script)
 
-    _sub_aggregation(scripts, "frappe.db.max", "frappe.qb.max")
-    _sub_aggregation(scripts, "frappe.db.min", "frappe.qb.min")
-    _sub_aggregation(scripts, "frappe.db.sum", "frappe.qb.sum")
-    _sub_aggregation(scripts, "frappe.db.avg", "frappe.qb.avg")
-
-    for script in scripts:
-        frappe.db.update(
-            "Server Script", {"name": script["name"]}, "script", script["script"]
-        )
+		frappe.db.update("Server Script", name, "script", script)
