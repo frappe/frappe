e503d8117303eb787c0099f02d91b89eb23b6ce4
Aradhya <aradhyatripathi51@gmail.com>
2021-11-29 13:07:31 +0530

    perf: reduced no. of db calls

--- frappe/patches/v14_0/remove_db_aggregation.py
+++ frappe/patches/v14_0/remove_db_aggregation.py
@@ -1,24 +1,37 @@
+from frappe.query_builder import DocType
 import frappe
 import re
 
 
 def execute():
-    _sub_aggregation("frappe.db.max", "frappe.qb.max")
-    _sub_aggregation("frappe.db.min", "frappe.qb.min")
-    _sub_aggregation("frappe.db.sum", "frappe.qb.sum")
-    _sub_aggregation("frappe.db.avg", "frappe.qb.avg")
+    sub_aggregation()
 
 
-def _sub_aggregation(function, subtitution):
-    scripts = frappe.get_all(
-        "Server Script",
-        filters={"script": ("like", f"%{function}%")},
-        fields=["name", "script"],
-    )
-    for script in scripts:
-        script.update(
-            {"script": re.sub(f"{function}", f"{subtitution}", script["script"])}
+def sub_aggregation():
+    server_scripts = DocType("Server Script")
+    scripts = (
+        frappe.qb.from_(server_scripts)
+        .where(
+            server_scripts.script.like("%frappe.db.max%")
+            | server_scripts.script.like("%frappe.db.min%")
+            | server_scripts.script.like("%frappe.db.sum%")
+            | server_scripts.script.like("%frappe.db.avg%")
         )
+        .select("name", "script")
+        .run(as_dict=True)
+    )
+
+    def _sub_aggregation(scripts, function, substitution):
+        for script in scripts:
+            script.update(
+                {"script": re.sub(f"{function}", f"{substitution}", script["script"])}
+            )
+
+    _sub_aggregation(scripts, "frappe.db.max", "frappe.qb.max")
+    _sub_aggregation(scripts, "frappe.db.min", "frappe.qb.min")
+    _sub_aggregation(scripts, "frappe.db.sum", "frappe.qb.sum")
+    _sub_aggregation(scripts, "frappe.db.avg", "frappe.qb.avg")
+
     for script in scripts:
         frappe.db.update(
             "Server Script", {"name": script["name"]}, "script", script["script"]
