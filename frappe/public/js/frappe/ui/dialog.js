cdabac2c433e1962cf7e7635488d523edfabb6aa
Bhavesh Maheshwari <bhavesh.freelancer95863@gmail.com>
2021-11-30 06:03:40 -0800

    chore: format issue

--- frappe/public/js/frappe/ui/dialog.js
+++ frappe/public/js/frappe/ui/dialog.js
@@ -198,7 +198,7 @@ frappe.ui.Dialog = class Dialog extends frappe.ui.FieldGroup {
 
 		this.$wrapper.removeClass('modal-minimize');
 
-		if (this.minimizable && this.is_minimized){
+		if (this.minimizable && this.is_minimized) {
 			$(".modal-backdrop").toggle()
 			this.is_minimized = false
 		}
