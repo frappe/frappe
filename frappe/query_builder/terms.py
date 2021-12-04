6120b4b3c1dbf17bc783be16ba41a5c73d5c5df1
saxenabhishek <as7122000@gmail.com>
2021-12-04 20:12:48 +0530

    fix: extend named parameters to frappe.qb.function

--- frappe/query_builder/terms.py
+++ frappe/query_builder/terms.py
@@ -1,5 +1,6 @@
-from typing import Any, List, Optional, Dict
-from pypika.terms import ValueWrapper
+from typing import Any, Dict, Optional
+
+from pypika.terms import Function, ValueWrapper
 from pypika.utils import format_alias_sql
 
 
@@ -23,5 +24,26 @@ class ParameterizedValueWrapper(ValueWrapper):
 			value_sql = self.get_value_sql(quote_char=quote_char, **kwargs) if not isinstance(self.value,int) else self.value
 			param_sql = param_wrapper.get_sql(**kwargs)
 			param_wrapper.update_parameters(param_key=param_sql, param_value=value_sql, **kwargs)
+		return format_alias_sql(param_sql, self.alias, quote_char=quote_char, **kwargs)
 
-			return format_alias_sql(param_sql, self.alias, quote_char=quote_char, **kwargs)
+
+class ParameterizedFunction(Function):
+	def get_sql(self, **kwargs: Any) -> str:
+		with_alias = kwargs.pop("with_alias", False)
+		with_namespace = kwargs.pop("with_namespace", False)
+		quote_char = kwargs.pop("quote_char", None)
+		dialect = kwargs.pop("dialect", None)
+		param_wrapper = kwargs.pop("param_wrapper", None)
+
+		function_sql = self.get_function_sql(with_namespace=with_namespace, quote_char=quote_char, param_wrapper=param_wrapper, dialect=dialect)
+
+		if self.schema is not None:
+			function_sql = "{schema}.{function}".format(
+				schema=self.schema.get_sql(quote_char=quote_char, dialect=dialect, **kwargs),
+				function=function_sql,
+			)
+
+		if with_alias:
+			return format_alias_sql(function_sql, self.alias, quote_char=quote_char, **kwargs)
+
+		return function_sql
