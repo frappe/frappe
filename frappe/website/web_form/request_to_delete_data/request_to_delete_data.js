7585a26d43bc906ad8c4ec7f5bb2e4555b30213e
Jannat Patel <pateljannat2308@gmail.com>
2021-12-08 18:12:09 +0530

    fix: translation for sla description

--- frappe/website/web_form/request_to_delete_data/request_to_delete_data.js
+++ frappe/website/web_form/request_to_delete_data/request_to_delete_data.js
@@ -5,8 +5,9 @@ frappe.ready(function() {
 			callback: (data) => {
 				if (data.message) {
 					const intro_wrapper = $('#introduction .ql-editor.read-mode');
-					const sla_description = `<br><b>Note: Your request for account deletion will be fulfilled within ${data.message} days.</b>`;
-					intro_wrapper.html(intro_wrapper.html() + sla_description);
+					const sla_description = __("Note: Your request for account deletion will be fulfilled within {0} days.", [data.message])
+					const sla_description_wrapper = `<br><b>${sla_description}</b>`;
+					intro_wrapper.html(intro_wrapper.html() + sla_description_wrapper);
 				}
 			}
 		});
