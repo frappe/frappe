492de434f25ad0b468a364f110f99f799cbb4654
Kenneth Sequeira <33246109+kennethsequeira@users.noreply.github.com>
2021-12-09 15:49:56 +0530

    fix: valid parent doctype error (#15233)
    Co-authored by: Ankush Menat <me@ankush.dev>
    
    (cherry picked from commit bac62ea014a01d013f8abf502a3ce0481351f84f)

--- frappe/desk/doctype/global_search_settings/global_search_settings.py
+++ frappe/desk/doctype/global_search_settings/global_search_settings.py
@@ -34,7 +34,7 @@ class GlobalSearchSettings(Document):
 
 def get_doctypes_for_global_search():
 	def get_from_db():
-		doctypes = frappe.get_list("Global Search DocType", fields=["document_type"], order_by="idx ASC")
+		doctypes = frappe.get_all("Global Search DocType", fields=["document_type"], order_by="idx ASC")
 		return [d.document_type for d in doctypes] or []
 
 	return frappe.cache().hget("global_search", "search_priorities", get_from_db)
