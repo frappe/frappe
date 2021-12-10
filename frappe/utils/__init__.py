d2ebb81f4c6c86eeef0b6702541068b8ef32cbb1
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2021-12-10 15:34:02 +0530

    feat: patched EmailQueue.send and frappe.utils.get_formatted_email
    * patched the above two funcitions with get_hook_method

--- frappe/utils/__init__.py
+++ frappe/utils/__init__.py
@@ -56,6 +56,10 @@ def get_email_address(user=None):
 def get_formatted_email(user, mail=None):
 	"""get Email Address of user formatted as: `John Doe <johndoe@example.com>`"""
 	fullname = get_fullname(user)
+	
+	method = get_hook_method('get_sender_details')
+	if method:
+		fullname, mail = method()
 
 	if not mail:
 		mail = get_email_address(user) or validate_email_address(user)
