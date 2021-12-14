1d67a38059d216c974a4e103317efde0416b4e6e
Faris Ansari <netchamp.faris@gmail.com>
2021-12-14 16:32:58 +0530

    fix: add doctype_state in sync and install

--- frappe/utils/install.py
+++ frappe/utils/install.py
@@ -9,6 +9,7 @@ def before_install():
 	frappe.reload_doc("core", "doctype", "docperm")
 	frappe.reload_doc("core", "doctype", "doctype_action")
 	frappe.reload_doc("core", "doctype", "doctype_link")
+	frappe.reload_doc("core", "doctype", "doctype_state")
 	frappe.reload_doc("desk", "doctype", "form_tour_step")
 	frappe.reload_doc("desk", "doctype", "form_tour")
 	frappe.reload_doc("core", "doctype", "doctype")
