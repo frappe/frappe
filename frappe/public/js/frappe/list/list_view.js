f26b0234d669e36daaa67b912a50e5b71257f62c
Shariq Ansari <sharique.rik@gmail.com>
2021-12-14 12:04:20 +0530

    fix: refresh all data

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -300,7 +300,7 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 	}
 
 	refresh(refresh_header=false) {
-		super.refresh().then(() => {
+		return super.refresh().then(() => {
 			this.render_header(refresh_header);
 			this.update_checkbox();
 		});
