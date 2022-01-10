7a36da571bdf80f27618bdb3d72b5aceb6098dee
saxenabhishek <as7122000@gmail.com>
2022-01-11 00:05:38 +0530

    refactor: pythonic NamedParameterWrapper

--- frappe/query_builder/terms.py
+++ frappe/query_builder/terms.py
@@ -5,18 +5,21 @@ from pypika.utils import format_alias_sql
 
 
 class NamedParameterWrapper():
-	def __init__(self, parameters: Dict[str, Any]):
-		self.parameters = parameters
+	def __init__(self) -> None:
+		self.parameters={}
 
-	def update_parameters(self, param_key: Any, param_value: Any, **kwargs):
+	def update_parameters(self, param_key: Any, param_value: Any, **kwargs)->None:
 		self.parameters[param_key[2:-2]] = param_value
 
-	def get_sql(self, **kwargs):
+	def get_sql(self, **kwargs)->str:
 		return f'%(param{len(self.parameters) + 1})s'
+
+	def get_parameters(self)->Dict[str, Any]:
+		return self.parameters
 
 
 class ParameterizedValueWrapper(ValueWrapper):
-	def get_sql(self, quote_char: Optional[str] = None, secondary_quote_char: str = "'", param_wrapper= None, **kwargs: Any) -> str:
+	def get_sql(self, quote_char: Optional[str] = None, secondary_quote_char: str = "'", param_wrapper:NamedParameterWrapper = None, **kwargs: Any) -> str:
 		if param_wrapper is None:
 			sql = self.get_value_sql(quote_char=quote_char, secondary_quote_char=secondary_quote_char, **kwargs)
 			return format_alias_sql(sql, self.alias, quote_char=quote_char, **kwargs)
