66d919355dee479b48245c09cd248cf530e73bd5
saxenabhishek <as7122000@gmail.com>
2022-01-11 00:05:38 +0530

    refactor: pythonic NamedParameterWrapper
    (cherry picked from commit 7a36da571bdf80f27618bdb3d72b5aceb6098dee)

--- frappe/query_builder/utils.py
+++ frappe/query_builder/utils.py
@@ -57,11 +57,11 @@ def patch_query_execute():
 		return frappe.db.sql(query, params, *args, **kwargs) # nosemgrep
 
 	def prepare_query(query):
-		params = {}
-		query = query.get_sql(param_wrapper=NamedParameterWrapper(params))
+		param_collector = NamedParameterWrapper()
+		query = query.get_sql(param_wrapper=param_collector)
 		if frappe.flags.in_safe_exec and not query.lower().strip().startswith("select"):
 			raise frappe.PermissionError('Only SELECT SQL allowed in scripting')
-		return query, params
+		return query, param_collector.get_parameters()
 
 	query_class = get_attr(str(frappe.qb).split("'")[1])
 	builder_class = get_type_hints(query_class._builder).get('return')
