9121014f488aa82a0fd38222ad2f85ea80ba73c9
saxenabhishek <as7122000@gmail.com>
2022-01-11 16:53:42 +0530

    fix: ignore copy of getattr methods

--- frappe/query_builder/__init__.py
+++ frappe/query_builder/__init__.py
@@ -1,5 +1,7 @@
 import pypika.terms
 from pypika import *
+from pypika import Field
+from pypika.utils import ignore_copy
 
 from frappe.query_builder.terms import ParameterizedFunction, ParameterizedValueWrapper
 from frappe.query_builder.utils import (
@@ -14,5 +16,6 @@ pypika.terms.ValueWrapper = ParameterizedValueWrapper
 pypika.terms.Function = ParameterizedFunction
 
 # * Overrides the field() method and replaces it with the a `PseudoColumn` 'field' for consistency
-pypika.queries.Selectable.__getattr__ = lambda table, x: Field(x, table=table)
+pypika.queries.Selectable.__getattr__ = ignore_copy(lambda table, x: Field(x, table=table))
+pypika.queries.Selectable.__getitem__ = ignore_copy(lambda table, x: Field(x, table=table))
 pypika.queries.Selectable.field = pypika.terms.PseudoColumn("field")
