6d355957f612434fc2996956b182cccce2ac0c18
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-01-13 18:38:43 +0530

    fix: invalid import

--- frappe/public/js/controls.bundle.js
+++ frappe/public/js/controls.bundle.js
@@ -2,4 +2,3 @@ import "air-datepicker/dist/js/datepicker.min.js";
 import "./frappe/form/controls/datepicker_i18n.js";
 import "./frappe/ui/capture.js";
 import "./frappe/form/controls/control.js";
-import "./frappe/link_title.js";
