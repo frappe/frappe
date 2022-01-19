9be8aa34878cc097691b0dd4987b9a32d94291f5
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2022-01-19 13:18:51 +0530

    fix: Revert unnecessary code

--- frappe/email/doctype/email_queue/email_queue.py
+++ frappe/email/doctype/email_queue/email_queue.py
@@ -11,7 +11,6 @@ from frappe.utils import now_datetime
 
 
 class EmailQueue(Document):
-	DOCTYPE = "Email Queue"
 	def set_recipients(self, recipients):
 		self.set("recipients", [])
 		for r in recipients:
@@ -30,17 +29,6 @@ class EmailQueue(Document):
 		duplicate = frappe.get_doc(values)
 		duplicate.set_recipients(recipients)
 		return duplicate
-
-	def update_db(self, commit=False, **kwargs):
-		frappe.db.set_value(self.DOCTYPE, self.name, kwargs)
-		if commit:
-			frappe.db.commit()
-
-	def update_status(self, status, commit=False, **kwargs):
-		self.update_db(status = status, commit = commit, **kwargs)
-		if self.communication:
-			communication_doc = frappe.get_doc('Communication', self.communication)
-			communication_doc.set_delivery_status(commit=commit)
 
 @frappe.whitelist()
 def retry_sending(name):
