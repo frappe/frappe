ef089d00e1845dc6c3a32ea5d38289b7c4115499
Shariq Ansari <sharique.rik@gmail.com>
2022-01-25 16:00:31 +0530

    fix: minor fix

--- frappe/utils/__init__.py
+++ frappe/utils/__init__.py
@@ -901,11 +901,11 @@ def dictify(arg):
 def add_user_info(user, user_info):
 	if user not in user_info:
 		info = frappe.db.get_value("User",
-			user, ["full_name", "user_image", "name", 'email', 'timezone'], as_dict=True) or frappe._dict()
+			user, ["full_name", "user_image", "name", 'email', 'time_zone'], as_dict=True) or frappe._dict()
 		user_info[user] = frappe._dict(
 			fullname = info.full_name or user,
 			image = info.user_image,
 			name = user,
 			email = info.email,
-			timezone = info.timezone
+			time_zone = info.time_zone
 		)
