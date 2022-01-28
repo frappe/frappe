8037866dc1d79476e1dd6607a85f3eb79edfbdd0
Gavin D'souza <gavin18d@gmail.com>
2022-01-28 18:33:03 +0530

    fix: Handle parsing and formatting timedeltas
    * Added utils parse_timedelta, format_timedelta
    * Added to json_handler for de-serializing timedelta objects

--- frappe/utils/__init__.py
+++ frappe/utils/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and Contributors
 # License: MIT. See LICENSE
 
 import functools
