7b495e4e3abd9da3d6975af84080008c7aa7d547
Suraj Shetty <surajshetty3416@gmail.com>
2022-01-31 13:31:31 +0530

    fix: Call validate_link only if "value" is passed
    (cherry picked from commit 851e66e5643de3f8431c9e7b2d84064b3f8f5024)

--- frappe/public/js/frappe/form/controls/link.js
+++ frappe/public/js/frappe/form/controls/link.js
@@ -408,13 +408,13 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 					// add "filters" for standard query (search.py)
 					args.filters = filters;
 				}
-			} else if(typeof(get_query)==="string") {
+			} else if (typeof (get_query) === "string") {
 				args.query = get_query;
 			} else {
 				// get_query by function
 				var q = (get_query)(this.frm && this.frm.doc || this.doc, this.doctype, this.docname);
 
-				if (typeof(q)==="string") {
+				if (typeof (q) ==="string") {
 					// returns a string
 					args.query = q;
 				} else if($.isPlainObject(q)) {
@@ -460,7 +460,6 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 	validate_link_and_fetch(df, options, docname, value) {
 		if (!options) return;
 
-		let field_value = "";
 		const fetch_map = this.get_fetch_map();
 		const columns_to_fetch = Object.values(fetch_map);
 
@@ -469,16 +468,10 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 			return value;
 		}
 
-		return frappe.xcall("frappe.client.validate_link", {
-			doctype: options,
-			docname: value,
-			fields: columns_to_fetch,
-		}).then((response) => {
-			if (!docname || !columns_to_fetch.length) return response.name;
-
+		function update_dependant_fields(response) {
+			let field_value = "";
 			for (const [target_field, source_field] of Object.entries(fetch_map)) {
-				if(value) field_value = response[source_field];
-
+				if (value) field_value = response[source_field];
 				frappe.model.set_value(
 					df.parent,
 					docname,
@@ -487,9 +480,23 @@ frappe.ui.form.ControlLink = frappe.ui.form.ControlData.extend({
 					df.fieldtype,
 				);
 			}
+		}
 
-			return response.name;
-		});
+		// to avoid unnecessary request
+		if (value) {
+			return frappe.xcall("frappe.client.validate_link", {
+				doctype: options,
+				docname: value,
+				fields: columns_to_fetch,
+			}).then((response) => {
+				if (!docname || !columns_to_fetch.length) return response.name;
+				update_dependant_fields(response);
+				return response.name;
+			});
+		} else {
+			update_dependant_fields({});
+			return value;
+		}
 	},
 
 	get_fetch_map() {
