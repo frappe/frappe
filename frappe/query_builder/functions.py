e155b2e49fe4c792572141942c42e8dfd119dae4
Gavin D'souza <gavin18d@gmail.com>
2022-02-07 12:36:55 +0530

    fix: Revert ported aggregate qb functions
    Had to undo since Query engine is develop only as of rn
    
    Added via https://github.com/frappe/frappe/pull/15883/commits/243b02227920851be292de671704885bf00daf4e

--- frappe/query_builder/functions.py
+++ frappe/query_builder/functions.py
@@ -2,7 +2,6 @@ from pypika.functions import *
 from pypika.terms import Function
 from frappe.query_builder.utils import ImportMapper, db_type_is
 from frappe.query_builder.custom import GROUP_CONCAT, STRING_AGG, MATCH, TO_TSVECTOR
-from frappe.database.query import Query
 from .utils import Column
 
 
@@ -24,26 +23,3 @@ Match = ImportMapper(
 		db_type_is.POSTGRES: TO_TSVECTOR
 	}
 )
-
-
-def _aggregate(function, dt, fieldname, filters, **kwargs):
-	return (
-		Query()
-		.build_conditions(dt, filters)
-		.select(function(Column(fieldname)))
-		.run(**kwargs)[0][0]
-		or 0
-	)
-
-
-def _max(dt, fieldname, filters=None, **kwargs):
-	return _aggregate(Max, dt, fieldname, filters, **kwargs)
-
-def _min(dt, fieldname, filters=None, **kwargs):
-	return _aggregate(Min, dt, fieldname, filters, **kwargs)
-
-def _avg(dt, fieldname, filters=None, **kwargs):
-	return _aggregate(Avg, dt, fieldname, filters, **kwargs)
-
-def _sum(dt, fieldname, filters=None, **kwargs):
-	return _aggregate(Sum, dt, fieldname, filters, **kwargs)
