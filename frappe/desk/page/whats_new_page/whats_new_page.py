cb17668e08d670bb330ae9b8f1d89381dd42a5e1
Subin Tom <subintom2@gmail.com>
2022-02-11 16:14:00 +0530

    fix: css adjustments, removing comments

--- frappe/desk/page/whats_new_page/whats_new_page.py
+++ frappe/desk/page/whats_new_page/whats_new_page.py
@@ -7,12 +7,11 @@ import requests
 @frappe.whitelist(allow_guest=True)
 def get_whats_new_posts():
 	host = "http://test-st.frappe.cloud"
-	# host = "http://test-erp:8000"
+
 	try:
 		res = requests.get(host + '/api/method/frappe.desk.doctype.whats_new.whats_new.fetch_latest_posts')
 	except:
 		frappe.throw('Error in establishing connection with host')
 	data = res.json()
-	print('DATA ', data)
+
 	return data.get('message') or []
-	# res.raise_for_status()
