8187574fadf20eda551c1266cfa1dd7585e9f253
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-02-11 11:24:11 +0530

    fix: enumerate from 1
    (cherry picked from commit c4060d42305ce43f4bd2eb7f123def7c3a2585df)

--- frappe/core/doctype/doctype/doctype.py
+++ frappe/core/doctype/doctype/doctype.py
@@ -766,25 +766,25 @@ def validate_links_table_fieldnames(meta):
 		return
 
 	fieldnames = tuple(field.fieldname for field in meta.fields)
-	for index, link in enumerate(meta.links):
+	for index, link in enumerate(meta.links, 1):
 		link_meta = frappe.get_meta(link.link_doctype)
 		if not link_meta.get_field(link.link_fieldname):
-			message = _("Document Links Row #{0}: Could not find field {1} in {2} DocType").format(index+1, frappe.bold(link.link_fieldname), frappe.bold(link.link_doctype))
+			message = _("Document Links Row #{0}: Could not find field {1} in {2} DocType").format(index, frappe.bold(link.link_fieldname), frappe.bold(link.link_doctype))
 			frappe.throw(message, InvalidFieldNameError, _("Invalid Fieldname"))
 
 		if not link.is_child_table:
 			continue
 
 		if not link.parent_doctype:
-			message = _("Document Links Row #{0}: Parent DocType is mandatory for internal links").format(index+1)
+			message = _("Document Links Row #{0}: Parent DocType is mandatory for internal links").format(index)
 			frappe.throw(message, frappe.ValidationError, _("Parent Missing"))
 
 		if not link.table_fieldname:
-			message = _("Document Links Row #{0}: Table Fieldname is mandatory for internal links").format(index+1)
+			message = _("Document Links Row #{0}: Table Fieldname is mandatory for internal links").format(index)
 			frappe.throw(message, frappe.ValidationError, _("Table Fieldname Missing"))
 
 		if link.table_fieldname not in fieldnames:
-			message = _("Document Links Row #{0}: Could not find field {1} in {2} DocType").format(index+1, frappe.bold(link.table_fieldname), frappe.bold(meta.name))
+			message = _("Document Links Row #{0}: Could not find field {1} in {2} DocType").format(index, frappe.bold(link.table_fieldname), frappe.bold(meta.name))
 			frappe.throw(message, frappe.ValidationError, _("Invalid Table Fieldname"))
 
 def validate_fields_for_doctype(doctype):
