a95f3ece6a4ab7fbfeb7e9628412e275515b9f0c
Subin Tom <subintom2@gmail.com>
2022-02-15 20:21:56 +0530

    fix: date time validation in page controller

--- frappe/desk/page/whats_new_page/whats_new_page.py
+++ frappe/desk/page/whats_new_page/whats_new_page.py
@@ -3,6 +3,7 @@
 
 import frappe
 import requests
+from frappe import _
 from datetime import datetime
 
 @frappe.whitelist(allow_guest=True)
@@ -24,10 +25,15 @@ def get_whats_new_posts():
 
 @frappe.whitelist(allow_guest=True)
 def add_whats_new_event_to_calendar(date, time, title):
+	print('DATETIEM', date,time)
+	if date == "undefined":
+		frappe.throw(_("Date is not in vaild format"))
+	if time == "undefined":
+		frappe.throw(_("Time is in invalid format"))
 
 	date_time = datetime.strptime((date+' '+time), "%Y-%m-%d %H:%M:%S")
 	if frappe.db.exists("Event", {"subject":title, "starts_on":date_time}):
-		frappe.throw(title=frappe._("Event Already Created"), msg=frappe._("An event for {0} scheduled on {1} already exists").format(frappe.bold(title), frappe.bold(date_time)))
+		frappe.throw(title=_("Event Already Created"), msg=_("An event for {0} scheduled on {1} already exists").format(frappe.bold(title), frappe.bold(date_time)))
 
 	calendar_event = {
 		"doctype": "Event",
