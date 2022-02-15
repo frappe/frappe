1a912aa787c53bb8226f4588afdb148566d776f3
hrwx <himanshuwarekar@yahoo.com>
2022-02-15 14:58:24 +0000

    fix: merge groups for doctype links

--- frappe/model/meta.py
+++ frappe/model/meta.py
@@ -14,16 +14,28 @@ Example:
 
 
 '''
+import json
+import os
 from datetime import datetime
+
 import click
-import frappe, json, os
-from frappe.utils import cstr, cint, cast
-from frappe.model import default_fields, no_value_fields, optional_fields, data_fieldtypes, table_fields, child_table_fields
-from frappe.model.document import Document
-from frappe.model.base_document import BaseDocument
-from frappe.modules import load_doctype_module
-from frappe.model.workflow import get_workflow_name
+
+import frappe
 from frappe import _
+from frappe.model import (
+	child_table_fields,
+	data_fieldtypes,
+	default_fields,
+	no_value_fields,
+	optional_fields,
+	table_fields,
+)
+from frappe.model.base_document import BaseDocument
+from frappe.model.document import Document
+from frappe.model.workflow import get_workflow_name
+from frappe.modules import load_doctype_module
+from frappe.utils import cast, cint, cstr
+
 
 def get_meta(doctype, cached=True):
 	if cached:
@@ -546,7 +558,7 @@ class Meta(Document):
 				# For internal links parent doctype will be the key
 				doctype = link.parent_doctype or link.link_doctype
 				# group found
-				if link.group and group.label == link.group:
+				if link.group and _(group.label) == _(link.group):
 					if doctype not in group.get('items'):
 						group.get('items').append(doctype)
 					link.added = True
