db8240c96819b8305472aad5c9023bcea50873ca
Sagar Vora <sagar@resilient.tech>
2022-02-15 13:06:30 +0530

    perf: reduce loop in `doc.update`
    (cherry picked from commit 1a20f376f3b5259e66e3d0b4ab58664371722e8e)
    
    # Conflicts:
    #	frappe/model/base_document.py

--- frappe/model/base_document.py
+++ frappe/model/base_document.py
@@ -101,6 +101,7 @@ class BaseDocument(object):
 				"balance": 42000
 			})
 		"""
+<<<<<<< HEAD
 		if "doctype" in d:
 			self.set("doctype", d.get("doctype"))
 
@@ -108,6 +109,12 @@ class BaseDocument(object):
 		for key in default_fields:
 			if key in d:
 				self.set(key, d.get(key))
+=======
+
+		# set name first, as it is used a reference in child document
+		if "name" in d:
+			self.name = d["name"]
+>>>>>>> 1a20f376f3 (perf: reduce loop in `doc.update`)
 
 		for key, value in iteritems(d):
 			self.set(key, value)
@@ -180,6 +187,7 @@ class BaseDocument(object):
 		if isinstance(value, (dict, BaseDocument)):
 			if not self.__dict__.get(key):
 				self.__dict__[key] = []
+
 			value = self._init_child(value, key)
 			self.__dict__[key].append(value)
 
@@ -213,6 +221,7 @@ class BaseDocument(object):
 	def _init_child(self, value, key):
 		if not self.doctype:
 			return value
+
 		if not isinstance(value, BaseDocument):
 			value["doctype"] = self.get_table_field_doctype(key)
 			if not value["doctype"]:
