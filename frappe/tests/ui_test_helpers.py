39d65c5211a30c413920ea43c4a607248c7e36b9
Shariq Ansari <sharique.rik@gmail.com>
2022-02-15 12:22:14 +0530

    test: UI test to check if linked field is populated in child table

--- frappe/tests/ui_test_helpers.py
+++ frappe/tests/ui_test_helpers.py
@@ -256,3 +256,17 @@ def update_webform_to_multistep():
 		_doc.route = "update-profile-duplicate"
 		_doc.is_standard = False
 		_doc.save()
+
+@frappe.whitelist()
+def update_child_table(name):
+	doc = frappe.get_doc('DocType', name)
+	if len(doc.fields) == 1:
+		doc.append('fields', {
+			'fieldname': 'doctype_to_link',
+			'fieldtype': 'Link',
+			'in_list_view': 1,
+			'label': 'Doctype to Link',
+			'options': 'Doctype to Link'
+		})
+
+		doc.save()
