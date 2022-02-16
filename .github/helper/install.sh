6f4046daded045c7d464495e0c025bf0c60526c9
Ankush Menat <ankush@frappe.io>
2022-02-16 19:40:32 +0530

    ci: move wkhtml install to background

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -2,11 +2,13 @@
 
 set -e
 
- # install wkhtmltopdf
-wget -O /tmp/wkhtmltox.tar.xz https://github.com/frappe/wkhtmltopdf/raw/master/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
-tar -xf /tmp/wkhtmltox.tar.xz -C /tmp
-sudo mv /tmp/wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
-sudo chmod o+x /usr/local/bin/wkhtmltopdf
+install_wkhtmltopdf() {
+    wget -O /tmp/wkhtmltox.tar.xz https://github.com/frappe/wkhtmltopdf/raw/master/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+    tar -xf /tmp/wkhtmltox.tar.xz -C /tmp
+    sudo mv /tmp/wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
+    sudo chmod o+x /usr/local/bin/wkhtmltopdf
+}
+install_wkhtmltopdf &
 
 sudo apt-get install libcups2-dev redis-server
 
