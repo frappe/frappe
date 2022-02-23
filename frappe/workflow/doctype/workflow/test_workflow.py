f864f3f972f569de83245a5d842d986502f3be3d
Mohammad Hasnain <hasnain2808@gmail.com>
2022-02-23 19:30:54 +0530

    test: clear-cache after DDl

--- frappe/workflow/doctype/workflow/test_workflow.py
+++ frappe/workflow/doctype/workflow/test_workflow.py
@@ -26,7 +26,7 @@ class TestWorkflow(unittest.TestCase):
 					'mariadb': 'ALTER TABLE `tabWorkflow Action` ADD COLUMN user varchar(140)',
 					'postgres': 'ALTER TABLE "tabWorkflow Action" ADD COLUMN "user" varchar(140)'
 				})
-
+				frappe.cache().delete_value('table_columns')
 
 	def tearDown(self):
 		frappe.delete_doc('Workflow', 'Test ToDo')
@@ -40,6 +40,7 @@ class TestWorkflow(unittest.TestCase):
 					'mariadb': 'ALTER TABLE `tabWorkflow Action` DROP COLUMN user',
 					'postgres': 'ALTER TABLE "tabWorkflow Action" DROP COLUMN "user"'
 				})
+				frappe.cache().delete_value('table_columns')
 
 	def test_default_condition(self):
 		'''test default condition is set'''
