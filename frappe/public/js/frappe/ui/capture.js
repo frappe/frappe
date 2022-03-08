952e7d26c406500da2a76368569a9ccc2be95b2e
hrwx <himanshuwarekar@yahoo.com>
2022-03-08 15:36:21 +0000

    feat: toggle cameras for uploading

--- frappe/public/js/frappe/ui/capture.js
+++ frappe/public/js/frappe/ui/capture.js
@@ -45,6 +45,9 @@ frappe.ui.Capture = class {
 	constructor(options = {}) {
 		this.options = frappe.ui.Capture.OPTIONS;
 		this.set_options(options);
+
+		this.facing_mode = "environment";
+		this.images = []
 	}
 
 	set_options(options) {
@@ -54,7 +57,13 @@ frappe.ui.Capture = class {
 	}
 
 	render() {
-		return navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+		let constraints = {
+			video: {
+				facingMode: this.facing_mode
+			}
+		}
+
+		return navigator.mediaDevices.getUserMedia(constraints).then(stream => {
 			this.stream = stream;
 
 			this.dialog = new frappe.ui.Dialog({
@@ -70,33 +79,55 @@ frappe.ui.Capture = class {
 			const set_take_photo_action = () => {
 				this.dialog.set_primary_action(__('Take Photo'), () => {
 					const data_url = frappe._.get_data_uri(video);
-					$e.find('.fc-p').attr('src', data_url);
+					$e.find('.fc-preview').attr('src', data_url);
 
-					$e.find('.fc-s').hide();
-					$e.find('.fc-p').show();
+					this.images.push(data_url);
+
+					$e.find('.fc-stream').hide();
+					$e.find('.fc-preview').show();
 
 					this.dialog.set_secondary_action_label(__('Retake'));
 					this.dialog.get_secondary_btn().show();
+					this.dialog.custom_actions.find(".btn-multiple").show();
 
 					this.dialog.set_primary_action(__('Submit'), () => {
 						this.hide();
-						if (this.callback) this.callback(data_url);
+						if (this.callback) this.callback(this.images);
 					});
+				});
+
+				this.dialog.set_secondary_action_label(__('Switch Camera'));
+				this.dialog.set_secondary_action(() => {
+					this.facing_mode = this.facing_mode == "environment" ? "user" : "environment";
+					frappe.show_alert({
+						message: __("Switching Camera")
+					});
+					this.hide();
+					this.show();
 				});
 			};
 
 			set_take_photo_action();
 
 			this.dialog.set_secondary_action(() => {
-				$e.find('.fc-p').hide();
-				$e.find('.fc-s').show();
+				this.images.pop();
+				$e.find('.fc-preview').hide();
+				$e.find('.fc-stream').show();
 
-				this.dialog.get_secondary_btn().hide();
 				this.dialog.get_primary_btn().off('click');
 				set_take_photo_action();
 			});
 
-			this.dialog.get_secondary_btn().hide();
+			this.dialog.add_custom_action(__("Take Multiple"), () => {
+				$e.find('.fc-preview').hide();
+				$e.find('.fc-stream').show();
+
+				this.dialog.get_primary_btn().off('click');
+				this.dialog.custom_actions.find(".btn-multiple").hide();
+				set_take_photo_action();
+			}, "btn-multiple");
+
+			this.dialog.custom_actions.find(".btn-multiple").hide();
 
 			const $e = $(frappe.ui.Capture.TEMPLATE);
 
@@ -150,8 +181,8 @@ frappe.ui.Capture.TEMPLATE = `
 <div class="frappe-capture">
 	<div class="panel panel-default">
 		<div class="embed-responsive embed-responsive-16by9">
-			<img class="fc-p embed-responsive-item" style="object-fit: contain; display: none;"/>
-			<video class="fc-s embed-responsive-item">${frappe.ui.Capture.ERR_MESSAGE}</video>
+			<img class="fc-preview embed-responsive-item" style="object-fit: contain; display: none;"/>
+			<video class="fc-stream embed-responsive-item">${frappe.ui.Capture.ERR_MESSAGE}</video>
 		</div>
 	</div>
 </div>
