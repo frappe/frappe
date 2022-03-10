54c0fee9a228374359d2dde1f7d83308894f1301
Shariq Ansari <sharique.rik@gmail.com>
2022-03-10 14:35:05 +0530

    fix(linter): Trim Trailing Whitespace

--- frappe/public/js/frappe/form/grid_row.js
+++ frappe/public/js/frappe/form/grid_row.js
@@ -639,7 +639,7 @@ export default class GridRow {
 
 	show_search_row() {
 		// show or remove search columns based on grid rows
-		this.show_search = this.frm && this.frm.doc && 
+		this.show_search = this.frm && this.frm.doc &&
 			this.frm.doc[this.grid.df.fieldname] &&
 			this.frm.doc[this.grid.df.fieldname].length >= 20;
 		!this.show_search && this.wrapper.remove();
