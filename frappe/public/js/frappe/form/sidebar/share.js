ff5a909d2884895206535e4c3bcf8e9797aaa14e
Suraj Shetty <surajshetty3416@gmail.com>
2022-03-13 19:11:04 +0530

    refactor: Update method name
    get_new_document_share_key -> get_document_share_key

--- frappe/public/js/frappe/form/sidebar/share.js
+++ frappe/public/js/frappe/form/sidebar/share.js
@@ -220,7 +220,7 @@ frappe.ui.form.Share = class Share {
 				fieldtype: "Button",
 				label: __("Get Link"),
 				click: () => {
-					this.frm.call("get_new_document_share_key", {
+					this.frm.call("get_document_share_key", {
 						expires_on: share_modal.get_value("link_expiration_date"),
 						no_expiry: share_modal.get_value("no_expiry")
 					}).then(res => {
