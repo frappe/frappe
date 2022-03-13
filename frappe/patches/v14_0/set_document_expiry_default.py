99aa2ec7f63235a61333212779efd31708a6b38e
Suraj Shetty <surajshetty3416@gmail.com>
2022-03-13 19:08:35 +0530

    fix: Add patch to set document expiry

---
+++ frappe/patches/v14_0/set_document_expiry_default.py
@@ -0,0 +1,5 @@
+import frappe
+
+def execute():
+	frappe.db.set_value("System Settings", "System Settings", "document_share_key_expiry", 30)
+	frappe.db.set_value("System Settings", "System Settings", "allow_older_web_view_links", 1)
