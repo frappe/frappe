221d6b5e7fabfa77d74d86dc47950892d95aec09
Ankush Menat <ankush@frappe.io>
2022-03-15 21:42:48 +0530

    feat: MySQL TIMESTAMP functionality for QB
    (cherry picked from commit 1a0fb216451f361279ec6ed95d63994d211582a9)
    
    # Conflicts:
    #	frappe/query_builder/functions.py

--- frappe/query_builder/functions.py
+++ frappe/query_builder/functions.py
@@ -1,5 +1,5 @@
 from pypika.functions import *
-from pypika.terms import Function
+from pypika.terms import Function, CustomFunction, ArithmeticExpression, Arithmetic
 from frappe.query_builder.utils import ImportMapper, db_type_is
 from frappe.query_builder.custom import GROUP_CONCAT, STRING_AGG, MATCH, TO_TSVECTOR
 from .utils import Column
@@ -23,3 +23,47 @@ Match = ImportMapper(
 		db_type_is.POSTGRES: TO_TSVECTOR
 	}
 )
+<<<<<<< HEAD
+=======
+
+class _PostgresTimestamp(ArithmeticExpression):
+	def __init__(self, datepart, timepart, alias=None):
+		if isinstance(datepart, str):
+			datepart = Cast(datepart, "date")
+		if isinstance(timepart, str):
+			timepart = Cast(timepart, "time")
+
+		super().__init__(operator=Arithmetic.add,
+				left=datepart, right=timepart, alias=alias)
+
+
+CombineDatetime = ImportMapper(
+	{
+		db_type_is.MARIADB: CustomFunction("TIMESTAMP", ["date", "time"]),
+		db_type_is.POSTGRES: _PostgresTimestamp,
+	}
+)
+
+
+def _aggregate(function, dt, fieldname, filters, **kwargs):
+	return (
+		Query()
+		.build_conditions(dt, filters)
+		.select(function(Column(fieldname)))
+		.run(**kwargs)[0][0]
+		or 0
+	)
+
+
+def _max(dt, fieldname, filters=None, **kwargs):
+	return _aggregate(Max, dt, fieldname, filters, **kwargs)
+
+def _min(dt, fieldname, filters=None, **kwargs):
+	return _aggregate(Min, dt, fieldname, filters, **kwargs)
+
+def _avg(dt, fieldname, filters=None, **kwargs):
+	return _aggregate(Avg, dt, fieldname, filters, **kwargs)
+
+def _sum(dt, fieldname, filters=None, **kwargs):
+	return _aggregate(Sum, dt, fieldname, filters, **kwargs)
+>>>>>>> 1a0fb21645 (feat: MySQL TIMESTAMP functionality for QB)
