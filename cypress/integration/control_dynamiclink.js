7fbbf577c90cbb877dc34c42b5e925373b2d34a4
Komal-Saraf0609 <komallsaraf0609@gmail.com>
2022-03-21 13:44:33 +0530

    test: fixing sider issues

--- cypress/integration/control_dynamiclink.js
+++ cypress/integration/control_dynamiclink.js
@@ -4,21 +4,21 @@ context('Dynamic Link', () => {
 		cy.visit('/app/doctype');
 		return cy.window().its('frappe').then(frappe => {
 			return frappe.xcall('frappe.tests.ui_test_helpers.create_doctype', {
-			name: 'Test Dynamic Link',
+				name: 'Test Dynamic Link',
 				fields: [
 					{
-					"label": "Document Type",
-					"fieldname": "doc_type",
-					"fieldtype": "Link",
-					"options": "DocType",
-					"in_list_view": 1,
+						"label": "Document Type",
+						"fieldname": "doc_type",
+						"fieldtype": "Link",
+						"options": "DocType",
+						"in_list_view": 1,
 					},
 					{
-					"label": "Document ID",
-					"fieldname": "doc_id",
-					"fieldtype": "Dynamic Link",
-					"options": "doc_type",
-					"in_list_view": 1,
+						"label": "Document ID",
+						"fieldname": "doc_id",
+						"fieldtype": "Dynamic Link",
+						"options": "doc_type",
+						"in_list_view": 1,
 					},
 				]
 			});
