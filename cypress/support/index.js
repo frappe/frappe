587d4877dae649c70ff65e7d95d68f85f6d56847
Komal-Saraf0609 <komallsaraf0609@gmail.com>
2022-03-21 14:33:54 +0530

    test: fixing sider issues

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,12 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 import '@cypress/code-coverage/support';
-import './commands';
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from failing the test
-  return false
-})
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
