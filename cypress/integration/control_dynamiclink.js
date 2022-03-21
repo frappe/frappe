a139d1d369b5a24e8c4c0e2a7cedcfed7dd9fa90
Komal-Saraf0609 <komallsaraf0609@gmail.com>
2022-03-21 13:39:22 +0530

    test: fixing sider issues

--- cypress/integration/control_dynamiclink.js
+++ cypress/integration/control_dynamiclink.js
@@ -2,27 +2,27 @@ context('Dynamic Link', () => {
 	before(() => {
 		cy.login();
 		cy.visit('/app/doctype');
-			return cy.window().its('frappe').then(frappe => {
-				return frappe.xcall('frappe.tests.ui_test_helpers.create_doctype', {
-				name: 'Test Dynamic Link',
-					fields: [
-						{
-						"label": "Document Type",
-						"fieldname": "doc_type",
-						"fieldtype": "Link",
-						"options": "DocType",
-						"in_list_view": 1,
-						},
-						{
-						"label": "Document ID",
-						"fieldname": "doc_id",
-						"fieldtype": "Dynamic Link",
-						"options": "doc_type",
-						"in_list_view": 1,
-						},
-					]
-				});
+		return cy.window().its('frappe').then(frappe => {
+			return frappe.xcall('frappe.tests.ui_test_helpers.create_doctype', {
+			name: 'Test Dynamic Link',
+				fields: [
+					{
+					"label": "Document Type",
+					"fieldname": "doc_type",
+					"fieldtype": "Link",
+					"options": "DocType",
+					"in_list_view": 1,
+					},
+					{
+					"label": "Document ID",
+					"fieldname": "doc_id",
+					"fieldtype": "Dynamic Link",
+					"options": "doc_type",
+					"in_list_view": 1,
+					},
+				]
 			});
+		});
 	});
 	it('Creating a dynamic link and verifying it', () => {
 		cy.new_form('Test Dynamic Link');
