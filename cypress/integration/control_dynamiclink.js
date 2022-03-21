38c6db06ea1d25d9a13775efc3b2145823ce1bdb
Komal-Saraf0609 <komallsaraf0609@gmail.com>
2022-03-21 13:13:19 +0530

    test: Added test script for control type "Dynamic Link"

---
+++ cypress/integration/control_dynamiclink.js
@@ -0,0 +1,34 @@
+context('Dynamic Link', () => {
+    before(() => {
+        cy.login();
+        cy.visit('/app/doctype');
+        return cy.window().its('frappe').then(frappe => {
+            return frappe.xcall('frappe.tests.ui_test_helpers.create_doctype', {
+                name: 'Test Dynamic Link',
+                fields: [
+                    {
+                        "label": "Document Type",
+                        "fieldname": "doc_type",
+                        "fieldtype": "Link",
+                        "options": "DocType",
+                        "in_list_view": 1,
+                    },
+                    {
+                        "label": "Document ID",
+                        "fieldname": "doc_id",
+                        "fieldtype": "Dynamic Link",
+                        "options": "doc_type",
+                        "in_list_view": 1,
+                    },
+                ]
+            });
+        });
+    });
+    it('Creating a dynamic link and verifying it', () => {
+        cy.new_form('Test Dynamic Link');
+		cy.get('form > [data-fieldname="doc_type"]').type('User');
+		cy.get('form > [data-fieldname="doc_id"]').click();
+		cy.get('[id="awesomplete_list_4"]').its('length').should('be.gte', 0);
+
+    });
+});
