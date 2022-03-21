e8ce58d7587e90452f040d765e7f0b2e31f93ba8
Shariq Ansari <sharique.rik@gmail.com>
2022-03-21 17:24:18 +0530

    fix: First set in model then save attachment
    (cherry picked from commit aa0d10fe0ecc4df770f2f77a4e05db7880384ed8)
    
    # Conflicts:
    #	frappe/public/js/frappe/form/controls/attach.js

--- frappe/public/js/frappe/form/controls/attach.js
+++ frappe/public/js/frappe/form/controls/attach.js
@@ -100,9 +100,13 @@ frappe.ui.form.ControlAttach = frappe.ui.form.ControlData.extend({
 		return this.value || null;
 	},
 
+<<<<<<< HEAD
 	on_upload_complete: function(attachment) {
+=======
+	async on_upload_complete(attachment) {
+>>>>>>> aa0d10fe0e (fix: First set in model then save attachment)
 		if(this.frm) {
-			this.parse_validate_and_set_in_model(attachment.file_url);
+			await this.parse_validate_and_set_in_model(attachment.file_url);
 			this.frm.attachments.update_attachment(attachment);
 			this.frm.doc.docstatus == 1 ? this.frm.save('Update') : this.frm.save();
 		}
