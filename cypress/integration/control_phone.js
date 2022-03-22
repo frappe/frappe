70d0aaf52d0580d56a04afbe2f43c5e51ea81e07
Noah Jacob <noahjacobkurian@gmail.com>
2022-03-22 15:52:59 +0530

    test(ui): updated click events to be specific

--- cypress/integration/control_phone.js
+++ cypress/integration/control_phone.js
@@ -16,14 +16,14 @@ context('Control Phone', () => {
 
 	it('should set flag and data', () => {
 		get_dialog_with_phone().as('dialog');
-		cy.get('.selected-phone').click();
+		cy.get('.selected-phone > svg').click();
 		cy.get('.phone-picker .phone-wrapper[id="afghanistan"]').click();
 		cy.get('.phone-picker .phone-wrapper[id="india"]').click();
 		cy.get('.selected-phone .country').should('have.text', '+91');
 		cy.get('.selected-phone > img').should('have.attr', 'src').and('include', '/in.svg');
 
 		let phone_number = '9312672712';
-		cy.get('.selected-phone').click().first();
+		cy.get('.selected-phone > img').click().first();
 		cy.get('.frappe-control[data-fieldname=phone] input')
 			.first()
 			.click();
@@ -42,7 +42,7 @@ context('Control Phone', () => {
 
 	it('case insensitive search for country and clear search', () => {
 		let search_text = 'india';
-		cy.get('.selected-phone').click().first();
+		cy.get('.selected-phone > img').click().first();
 		cy.get('.phone-picker').findByRole('searchbox').click().type(search_text);
 		cy.get('.phone-section .phone-wrapper:not(.hidden)').then(i => {
 			cy.get(`.phone-section .phone-wrapper[id*='${search_text.toLowerCase()}']`).then(countries => {
