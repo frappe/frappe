162a16e74e6fcf2ab107196b4b12527d87b9ea26
Rushabh Mehta <rmehta@gmail.com>
2022-03-23 23:47:22 +0530

    fix(test): web_form.js

--- cypress/integration/web_form.js
+++ cypress/integration/web_form.js
@@ -7,8 +7,8 @@ context('Web Form', () => {
 		cy.visit('/update-profile');
 		cy.get_field('last_name', 'Data').type('_Test User', {force: true}).wait(200);
 		cy.get('.web-form-actions .btn-primary').click();
-		cy.wait(500);
-		cy.get('.modal.show > .modal-dialog').should('be.visible');
+		cy.wait(5000);
+		cy.url().should('include', '/me');
 	});
 
 	it('Navigate and Submit a MultiStep WebForm', () => {
@@ -20,8 +20,8 @@ context('Web Form', () => {
 			cy.get('.btn-previous').should('be.visible');
 			cy.get('.btn-next').should('not.be.visible');
 			cy.get('.web-form-actions .btn-primary').click();
-			cy.wait(500);
-			cy.get('.modal.show > .modal-dialog').should('be.visible');
+			cy.wait(5000);
+			cy.url().should('include', '/me');
 		});
 	});
 });
