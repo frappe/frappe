3208bb605b438d040627059d1b028992072ab7b0
Summayya <sumaiyahas2908@gmail.com>
2022-03-28 12:52:05 +0530

    refactor: make global_filter object and toggle table

--- frappe/desk/doctype/dashboard/dashboard.js
+++ frappe/desk/doctype/dashboard/dashboard.js
@@ -2,7 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Dashboard', {
-	refresh: function(frm) {
+	refresh: function (frm) {
+		frm.trigger('get_global_filters');
 		frm.add_custom_button(__("Show Dashboard"),
 			() => frappe.set_route('dashboard-view', frm.doc.name)
 		);
@@ -11,7 +12,7 @@ frappe.ui.form.on('Dashboard', {
 			frm.disable_form();
 		}
 
-		frm.set_query("chart", "charts", function() {
+		frm.set_query("chart", "charts", function () {
 			return {
 				filters: {
 					is_public: 1,
@@ -19,12 +20,110 @@ frappe.ui.form.on('Dashboard', {
 			};
 		});
 
-		frm.set_query("card", "cards", function() {
+		frm.set_query("card", "cards", function () {
 			return {
 				filters: {
 					is_public: 1,
 				}
 			};
 		});
+	},
+
+	get_global_filters: function (frm) {
+		let charts = frm.doc.charts;
+		frm.global_filters = [];
+		if (charts[0].chart) {
+			charts.map(({ chart }) => {
+				frappe.model.with_doc('Dashboard Chart', chart, function () {
+					let chart_doc = frappe.model.get_doc('Dashboard Chart', chart);
+					frappe.dashboard_utils.get_filters_for_chart_type(chart_doc).then(filters => {
+						if (filters) {
+							if (frm.global_filters.length == 0) {
+								frm.global_filters = filters.map(obj => ({ ...obj }));
+								frm.global_filters.map(filter => { filter.count = 0; filter.charts = [] })
+							}
+
+							filters.map(function (filter) {
+								let exist = frm.global_filters.find(el => el.fieldname == filter.fieldname);
+								let match = false;
+								if (exist) {
+									if (exist.get_data && filter.get_data) {
+										if (exist.get_data == filter.get_data) {
+											match = true;
+										}
+									}
+	
+									if (exist.options && filter.options) {
+										if (exist.options == filter.options || exist.options.length == filter.options.length) {
+											match = true;
+										}
+									}
+								}
+
+								if (exist && match) {
+									if (exist.charts.includes(chart)) return;
+									exist.charts.push(chart)
+									exist.count++
+								}
+								else {
+									filter.count = 1;
+									filter.charts = [chart]
+									frm.global_filters.push({ ...filter })
+								}
+							})
+							console.log(frm.global_filters)
+							frm.trigger("set_global_filters");
+						}
+					})
+				})
+			})
+		}
+	},
+
+	set_global_filters: function (frm) {
+		var df_filter = frappe.meta.get_docfield("Dashboard Global Filters", "filter", cur_frm.doc.name);
+		df_filter.options = [];
+		frm.global_filters.map((filter) => {
+			if (filter.count >= 2) {
+				df_filter.options.push(filter.label)
+			}
+		})
+		frm.toggle_display('global_filters', df_filter.options.length > 0);
+	},
+});
+
+frappe.ui.form.on("Dashboard Chart Link", {
+	chart: function (frm, cdt, cdn) {
+		if (locals[cdt][cdn].chart) frm.trigger("get_global_filters");
+	},
+
+	charts_remove: function (frm, cdt, cdn) {
+		frm.trigger("get_global_filters");
+	}
+});
+
+frappe.ui.form.on("Dashboard Global Filters", {
+
+	filter: function (frm, cdt, cdn) {
+		let doc = locals[cdt][cdn];
+		if (doc.filter) {
+			frm.global_filters.map((f) => {
+				if (f.label == doc.filter) {
+					frappe.model.set_value(cdt, cdn, "fieldtype", f.fieldtype);
+					frappe.model.set_value(cdt, cdn, "chart_filter_name", f.fieldname);
+					frappe.model.set_value(cdt, cdn, "fieldtype", f.fieldtype)
+					if (f.options) frappe.model.set_value(cdt, cdn, "options", JSON.stringify(f.options));
+					if (f.get_data) frappe.model.set_value(cdt, cdn, "get_data", f.get_data);
+
+					frm.set_query("chart", "global_filters", function () {
+						return {
+							filters: {
+								"name": ['in', f.charts]
+							}
+						};
+					});
+				}
+			})
+		}
 	}
 });
