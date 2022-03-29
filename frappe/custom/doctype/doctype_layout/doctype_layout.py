93f7b34aa85a6bc55958154e8dbf6da5fe95f202
Dany Robert <rtdany10@gmail.com>
2022-03-29 11:42:17 +0530

    fix: missing import

--- frappe/custom/doctype/doctype_layout/doctype_layout.py
+++ frappe/custom/doctype/doctype_layout/doctype_layout.py
@@ -2,9 +2,10 @@
 # Copyright (c) 2020, Frappe Technologies and contributors
 # License: MIT. See LICENSE
 
+from frappe import _
 from frappe.model.document import Document
-
 from frappe.desk.utils import slug
+
 
 class DocTypeLayout(Document):
 	def validate(self):
