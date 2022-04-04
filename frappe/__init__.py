d2dfc8c3357682a1ad0366d5bbf85e5ce4c225f1
Sagar Vora <sagar@resilient.tech>
2022-04-04 10:16:09 +0530

    fix: define `__slots__` to avoid `__dict__` creation

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -55,20 +55,19 @@ controllers = {}
 class _dict(dict):
 	"""dict like object that exposes keys as attributes"""
 
+	__slots__ = ()
 	__getattr__ = dict.get
 	__setattr__ = dict.__setitem__
 	__delattr__ = dict.__delitem__
+	__setstate__ = dict.update
 
 	def __getstate__(self):
 		return self
 
-	def __setstate__(self, d):
-		self.update(d)
-
-	def update(self, d):
+	def update(self, *args, **kwargs):
 		"""update and return self -- the missing dict feature in python"""
 
-		super().update(d)
+		super().update(*args, **kwargs)
 		return self
 
 	def copy(self):
