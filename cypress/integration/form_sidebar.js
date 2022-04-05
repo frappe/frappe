62a08e84fd44c730071cf1724c30222b3e956137
Daizy <DaizyModi>
2022-04-05 14:46:22 +0530

    feat: Doctype to save templates for form

--- cypress/integration/form_sidebar.js
+++ cypress/integration/form_sidebar.js
@@ -1,31 +1,52 @@
 context("Form Sidebar", () => {
 	before(() => {
-		cy.visit('http://resilient_test:8000/');
-		cy.login("administrator", "samx6730");
+		cy.login();
 		cy.visit('/app/todo');
 
 	});
-	
-	it("Test for checking Save Templates on doctypes", () => {
-		cy.insert_doc('ToDo', {
-			'__newname': 'TEST#123', 
-			'description': 'Test this', 
-			'ignore_duplicate': true
+
+	function show_saved_templates() {
+		cy.window().its('frappe').then(frappe => {
+			cy.get_list("ToDo")
+				.its('data')
+				.then(data => expect(data.length).to.be.at.least(1));
+
+			cy.get('.form-template > .sidebar-action > a').click();
+			cy.get('[data-name="Test ToDo"]').should('be.visible');
 		});
-		cy.visit('/app/todo/TEST%23123');
-		cy.findByPlaceholderText('Template Name')
-			.type('Test Quotation{enter}')
-			.blur();
-		cy.get('button[data-name="Test Quotation"]').click();
+	}
+
+	function use_saved_templates() {
+		cy.get('button[data-name="Test ToDo"]').click();
 		cy.intercept('POST', '/api/method/frappe.desk.form.save.savedocs').as('save_form');
 		// trigger save
 		cy.get('.primary-action').click();
 		cy.wait('@save_form').its('response.statusCode').should('eq', 200);
+	}
+	
+	it("Test to save templates for doctypes", () => {
+		cy.insert_doc('ToDo', {
+			'__newname': 'TEST#123', 
+			'description': 'This ToDo is for Cypress Testing', 
+			'ignore_duplicate': true
+		});
+		cy.visit('/app/todo/TEST%23123');
+		cy.findByPlaceholderText('Template Name')
+			.type('Test ToDo{enter}')
+			.blur();
 	});
 
 	it("Show saved Templates", () => {
-		cy.get_list("Quotation").as('saved-template')
-		cy.get('.form-template > .sidebar-action > a').click()
-		cy.get('[data-name="Test Quotation"]').should('be.visible');
+		show_saved_templates()
+	})
+
+	it("Use saved template to update existiong doctype", () => {
+		use_saved_templates()
+	})
+
+	it("Use saved templates in new docs", () => {
+		cy.new_form("ToDo");
+		show_saved_templates();
+		use_saved_templates();
 	})
 });
