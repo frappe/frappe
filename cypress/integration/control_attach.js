e9bd8f2a5e13067ac0f6d4cb5253a4ab6a76443c
Komal-Saraf0609 <komallsaraf0609@gmail.com>
2022-04-06 19:35:09 +0530

    test: Corrected selectors

--- cypress/integration/control_attach.js
+++ cypress/integration/control_attach.js
@@ -1,4 +1,4 @@
-context('Date Control', () => {
+context('Attach Control', () => {
 	before(() => {
 		cy.login();
 		cy.visit('/app/doctype');
@@ -21,16 +21,15 @@ context('Date Control', () => {
 		cy.new_form('Test Attach Control');	
 
 		//Clicking on the attach button which is displayed as part of creating a doctype with "Attach" fieldtype
-		cy.get('.control-input > .btn-sm').contains('Attach').click();
+		cy.findByRole('button', {name: 'Attach'}).click();
 
 		//Clicking on "Link" button to attach a file using the "Link" button
-		cy.get('.mt-2').findByRole('button', {name: 'Link'}).click();
-		cy.get('.input-group > .form-control').type('https://wallpaperplay.com/walls/full/8/2/b/72402.jpg');
-
+		cy.findByRole('button', {name: 'Link'}).click();
+		cy.findByPlaceholderText('Attach a web link').type('https://wallpaperplay.com/walls/full/8/2/b/72402.jpg');
 		//Clicking on the Upload button to upload the file
-		cy.get('.modal-footer > .standard-actions > .btn-primary').contains('Upload').click({delay: 500});
+		cy.get('.modal-footer').findByRole("button", {name: "Upload"}).click({delay: 500});
 		cy.wait(800);
-		cy.get('.primary-action').click();
+		cy.findByRole('button', {name: 'Save'}).click();
 
 		//Checking if the URL of the attached image is getting displayed in the field of the newly created doctype
 		cy.get('.attached-file > .ellipsis > .attached-file-link')
@@ -56,16 +55,16 @@ context('Date Control', () => {
 		cy.new_form('Test Attach Control');	
 
 		//Clicking on the attach button which is displayed as part of creating a doctype with "Attach" fieldtype
-		cy.get('.control-input > .btn-sm').contains('Attach').click();
+		cy.findByRole('button', {name: 'Attach'}).click();
 
 		//Clicking on "Library" button to attach a file using the "Library" button
-		cy.get('.mt-2').findByRole('button', {name: 'Library'}).click();
+		cy.findByRole('button', {name: 'Library'}).click();
 		cy.contains('72402.jpg').click();
 
 		//Clicking on the Upload button to upload the file
-		cy.get('.modal-footer > .standard-actions > .btn-primary').contains('Upload').click({delay: 500});
+		cy.get('.modal-footer').findByRole("button", {name: "Upload"}).click({delay: 500});
 		cy.wait(800);
-		cy.get('.primary-action').click();
+		cy.findByRole('button', {name: 'Save'}).click();
 
 		//Checking if the URL of the attached image is getting displayed in the field of the newly created doctype
 		cy.get('.attached-file > .ellipsis > .attached-file-link')
@@ -85,5 +84,4 @@ context('Date Control', () => {
 		cy.get('.actions-btn-group > .dropdown-menu [data-label="Delete"]').click();
 		cy.click_modal_primary_button('Yes');
 	});
-
 });
