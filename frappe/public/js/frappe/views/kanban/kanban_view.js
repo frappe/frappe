557795a3aca851ab4ab9d39c694cfe192aa3798a
barredterra <14891507+barredterra@users.noreply.github.com>
2022-04-08 14:04:14 +0200

    refactor: remove unused method

--- frappe/public/js/frappe/views/kanban/kanban_view.js
+++ frappe/public/js/frappe/views/kanban/kanban_view.js
@@ -28,12 +28,6 @@ frappe.views.KanbanView = class KanbanView extends frappe.views.ListView {
 		return 'Kanban';
 	}
 
-	get_fields_in_list_view() {
-		let fields = JSON.parse(this.board.fields || "[]").map(e => e.fieldname);
-
-		return this.meta.fields.filter((df) => fields.includes(df.fieldname));
-	}
-
 	setup_defaults() {
 		return super.setup_defaults()
 			.then(() => {
