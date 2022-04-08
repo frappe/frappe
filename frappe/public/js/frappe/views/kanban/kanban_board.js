40139f321185de186261e693a0450dcc144f4ad1
barredterra <14891507+barredterra@users.noreply.github.com>
2022-04-08 13:07:31 +0200

    fix: card layout
    - card title links to form
    - whole card can be grabbed
    - increase height of empty columns to serve as target for drag-and-drop

--- frappe/public/js/frappe/views/kanban/kanban_board.js
+++ frappe/public/js/frappe/views/kanban/kanban_board.js
@@ -630,7 +630,6 @@ frappe.provide("frappe.views");
 			if(!card) return;
 			make_dom();
 			render_card_meta();
-			add_task_link();
 		}
 
 		function make_dom() {
@@ -711,12 +710,6 @@ frappe.provide("frappe.views");
 
 			self.$card.find(".kanban-card-meta").empty().append(html)
 				.find('.kanban-assignments').append($assignees_group);
-		}
-
-		function add_task_link() {
-			let task_link = frappe.utils.get_form_link(card.doctype, card.name);
-			self.$card.find('.kanban-card-redirect')
-				.attr('href', task_link);
 		}
 
 		function get_assignees_group() {
