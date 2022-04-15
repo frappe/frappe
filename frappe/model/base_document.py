f748ae85fc8f51b4c4dcf9029f452703aa238b7a
Ankush Menat <ankush@frappe.io>
2022-04-15 11:48:13 +0530

    fix: set docstatus to 0 if None present

--- frappe/model/base_document.py
+++ frappe/model/base_document.py
@@ -348,7 +348,7 @@ class BaseDocument(object):
 
 	@property
 	def docstatus(self):
-		return DocStatus(self.get("docstatus"))
+		return DocStatus(cint(self.get("docstatus")))
 
 	@docstatus.setter
 	def docstatus(self, value):
