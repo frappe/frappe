ac3704950e7795ace6a10d2ac7b129c18cbf0883
lapardnemihk1099 <lapardnemihk@gmail.com>
2022-04-18 13:32:17 +0530

    test: bulk_insert

--- frappe/tests/test_db.py
+++ frappe/tests/test_db.py
@@ -4,6 +4,7 @@
 import datetime
 import inspect
 import unittest
+from math import ceil
 from random import choice
 from unittest.mock import patch
 
@@ -444,6 +445,29 @@ class TestDB(unittest.TestCase):
 		self.assertEqual(filters["doctype"], dt)  # make sure that doctype was not removed from filters
 
 		self.assertEqual(frappe.db.exists(dt, [["name", "=", dn]]), dn)
+
+	def test_bulk_insert(self):
+		current_count = frappe.db.count("ToDo")
+		chunk_size = 10
+
+		for number_of_values in (1, 2, 5, 27):
+			current_transaction_writes = frappe.db.transaction_writes
+
+			frappe.db.bulk_insert(
+				"ToDo",
+				["name", "description"],
+				[[f"ToDo Test Bulk Insert {i}", "test_bulk_insert"] for i in range(number_of_values)],
+				chunk_size=chunk_size,
+			)
+
+			# check that all records were inserted
+			self.assertEqual(number_of_values, frappe.db.count("ToDo") - current_count)
+
+			# check if inserts were done in chunks
+			expected_chuncks = ceil(number_of_values / chunk_size)
+			self.assertEqual(expected_chuncks, frappe.db.transaction_writes - current_transaction_writes)
+
+			frappe.db.rollback()
 
 
 @run_only_if(db_type_is.MARIADB)
