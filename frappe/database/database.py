e939481b83946dcc1f2c5699295057efb07a59cd
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-04-18 17:22:47 +0530

    fix: ignore_duplicates for Postgres

--- frappe/database/database.py
+++ frappe/database/database.py
@@ -20,7 +20,7 @@ import frappe.model.meta
 from frappe import _
 from frappe.model.utils.link_count import flush_local_link_count
 from frappe.query_builder.functions import Count
-from frappe.query_builder.utils import DocType
+from frappe.query_builder.utils import DocType, db_type_is
 from frappe.utils import cast, get_datetime, getdate, now, sbool
 
 from .query import Query
@@ -1241,7 +1241,11 @@ class Database(object):
 		for start_index in range(0, len(values), chunk_size):
 			query = frappe.qb.into(table)
 			if ignore_duplicates:
-				query = query.ignore()
+				# Pypika does not have same api for ignoring duplicates
+				if frappe.conf.db_type == db_type_is.MARIADB.value:
+					query = query.ignore()
+				elif frappe.conf.db_type == db_type_is.POSTGRES.value:
+					query = query.on_conflict().do_nothing()
 
 			values_to_insert = values[start_index : start_index + chunk_size]
 			query.columns(fields).insert(*values_to_insert).run()
