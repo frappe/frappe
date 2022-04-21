d23914f7c522e431749011739a2e5d30214b482a
Ankush Menat <ankush@frappe.io>
2022-04-21 17:14:49 +0530

    test: form tour doc creation

--- frappe/desk/doctype/form_tour/test_form_tour.py
+++ frappe/desk/doctype/form_tour/test_form_tour.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2021, Frappe Technologies and Contributors
 # License: MIT. See LICENSE
 
-# import frappe
 import unittest
+
+import frappe
+from frappe.tests.ui_test_helpers import create_form_tour
 
 
 class TestFormTour(unittest.TestCase):
-	pass
+	def test_creation(self):
+		create_form_tour()
+		self.assertEqual(frappe.db.exists("Form Tour", {"name": "Test Form Tour"}))
