1412802ef919cae8da530a31992a0cab35e87b81
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2022-05-11 14:19:55 +0530

    fix: Resolve conflicts

--- frappe/contacts/address_and_contact.py
+++ frappe/contacts/address_and_contact.py
@@ -19,12 +19,7 @@ def load_address_and_contact(doc, key=None):
 		["Dynamic Link", "link_name", "=", doc.name],
 		["Dynamic Link", "parenttype", "=", "Address"],
 	]
-<<<<<<< HEAD
-	address_list = frappe.get_all("Address", filters=filters, fields=["*"])
-=======
 	address_list = frappe.get_list("Address", filters=filters, fields=["*"], order_by="creation asc")
->>>>>>> 5ac729252f (fix: change ordering to creation (#16863))
-
 	address_list = [a.update({"display": get_address_display(a)}) for a in address_list]
 
 	address_list = sorted(
