3ff34e1d66e705d43e6ade393d6e6375115c221e
Shariq Ansari <30859809+shariquerik@users.noreply.github.com>
2022-05-13 19:04:53 +0530

    chore: used better db API arguments
    Co-authored-by: gavin <gavin18d@gmail.com>

--- frappe/patches/v14_0/reload_workspace_child_tables.py
+++ frappe/patches/v14_0/reload_workspace_child_tables.py
@@ -3,15 +3,11 @@ from frappe.modules import get_doctype_module
 
 
 def execute():
-	child_tables = [
-		r[0]
-		for r in frappe.get_all(
+	child_tables = frappe.get_all(
 			"DocField",
-			fields="options",
+			pluck="options",
 			filters={"fieldtype": ["in", frappe.model.table_fields], "parent": "Workspace"},
-			as_list=1,
 		)
-	]
 
 	for child_table in child_tables:
 		module = get_doctype_module(child_table).lower()
