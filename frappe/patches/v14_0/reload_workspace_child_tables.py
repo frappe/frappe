472ee335ed371e3808a70deb25532b8042ec75b0
Shariq Ansari <sharique.rik@gmail.com>
2022-05-13 19:08:15 +0530

    fix: fixed indentation

--- frappe/patches/v14_0/reload_workspace_child_tables.py
+++ frappe/patches/v14_0/reload_workspace_child_tables.py
@@ -4,10 +4,10 @@ from frappe.modules import get_doctype_module
 
 def execute():
 	child_tables = frappe.get_all(
-			"DocField",
-			pluck="options",
-			filters={"fieldtype": ["in", frappe.model.table_fields], "parent": "Workspace"},
-		)
+		"DocField",
+		pluck="options",
+		filters={"fieldtype": ["in", frappe.model.table_fields], "parent": "Workspace"},
+	)
 
 	for child_table in child_tables:
 		module = get_doctype_module(child_table).lower()
