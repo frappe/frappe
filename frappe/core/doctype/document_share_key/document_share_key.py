393a3cf0bb5b54e147b3aa2c37940393fa82b5e3
Suraj Shetty <surajshetty3416@gmail.com>
2022-05-30 16:11:55 +0530

    style: Fix linter warnings
    (cherry picked from commit 306f223f73afbfb0aee372d214fa838dae7394ac)

--- frappe/core/doctype/document_share_key/document_share_key.py
+++ frappe/core/doctype/document_share_key/document_share_key.py
@@ -4,11 +4,14 @@
 import frappe
 from frappe.model.document import Document
 
+
 class DocumentShareKey(Document):
 	def before_insert(self):
 		self.key = frappe.generate_hash(length=32)
 		if not self.expires_on and not self.flags.no_expiry:
-			self.expires_on = frappe.utils.add_days(None, days=frappe.get_system_settings("document_share_key_expiry") or 90)
+			self.expires_on = frappe.utils.add_days(
+				None, days=frappe.get_system_settings("document_share_key_expiry") or 90
+			)
 
 	def is_expired(self):
 		return self.expires_on and self.expires_on < frappe.utils.getdate()
--- frappe/core/doctype/document_share_key/test_document_share_key.py
+++ frappe/core/doctype/document_share_key/test_document_share_key.py
@@ -4,5 +4,6 @@
 # import frappe
 import unittest
 
+
 class TestDocumentShareKey(unittest.TestCase):
 	pass
