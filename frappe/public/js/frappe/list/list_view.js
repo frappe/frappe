f4e8dee669e7922d27618e3c66fa10836e49326a
Ankush Menat <ankush@frappe.io>
2022-06-05 22:19:20 +0530

    fix: double urlencoding of values
    This was breaking URLs for complex filters

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -1461,9 +1461,8 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 	get_url_with_filters() {
 		const query_params = this.get_filters_for_args()
 			.map((filter) => {
-				filter[3] = encodeURIComponent(filter[3]);
 				if (filter[2] === "=") {
-					return `${filter[1]}=${filter[3]}`;
+					return `${filter[1]}=${encodeURIComponent(filter[3])}`;
 				}
 				return [
 					filter[1],
