59a9be4caf4ac20a715a510d8305975820436ea0
Ankush Menat <ankush@frappe.io>
2022-06-08 11:50:59 +0530

    refactor: simplify timegrain conditions
    All special handling for timegrains is same as general handling, no idea why it's done
    like this.

--- frappe/utils/dateutils.py
+++ frappe/utils/dateutils.py
@@ -109,10 +109,7 @@ def get_dates_from_timegrain(from_date, to_date, timegrain="Daily"):
 	elif "Yearly" == timegrain:
 		months = 1
 
-	if "Weekly" == timegrain:
-		dates = [get_last_day_of_week(from_date)]
-	else:
-		dates = [get_period_ending(from_date, timegrain)]
+	dates = [get_period_ending(from_date, timegrain)]
 
 	while getdate(dates[-1]) < getdate(to_date):
 		if "Weekly" == timegrain:
@@ -165,16 +162,12 @@ def get_period_beginning(date, timegrain, as_str=True):
 
 
 def get_period_ending(date, timegrain):
-	date = getdate(date)
-	if timegrain == "Daily":
-		return date
-	else:
-		return getdate(
-			{
-				"Daily": date,
-				"Weekly": get_last_day_of_week(date),
-				"Monthly": get_last_day(date),
-				"Quarterly": get_quarter_ending(date),
-				"Yearly": get_year_ending(date),
-			}[timegrain]
-		)
+	return getdate(
+		{
+			"Daily": date,
+			"Weekly": get_last_day_of_week(date),
+			"Monthly": get_last_day(date),
+			"Quarterly": get_quarter_ending(date),
+			"Yearly": get_year_ending(date),
+		}[timegrain]
+	)
