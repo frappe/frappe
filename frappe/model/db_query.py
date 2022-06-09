f5724d24225f9ee4fd5a5696c6499036b6c86431
Shariq Ansari <sharique.rik@gmail.com>
2022-06-09 17:41:31 +0530

    fix: only allow aggregate functions

--- frappe/model/db_query.py
+++ frappe/model/db_query.py
@@ -402,6 +402,8 @@ class DatabaseQuery(object):
 			"extract(",
 			"locate(",
 			"strpos(",
+		]
+		aggregate_functions = [
 			"count(",
 			"sum(",
 			"avg(",
@@ -409,10 +411,10 @@ class DatabaseQuery(object):
 		# add tables from fields
 		if self.fields:
 			for field in self.fields:
-				if not ("tab" in field and "." in field):
+				if not ("tab" in field and "." in field) or any(x for x in sql_functions if x in field):
 					continue
 
-				if any(x for x in sql_functions if x in field):
+				if any(x for x in aggregate_functions if x in field):
 					field = field.split("(", 1)[1][:-1]
 
 				table_name = field.split(".")[0]
