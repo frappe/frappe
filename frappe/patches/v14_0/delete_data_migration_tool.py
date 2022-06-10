dad809e236ddf8dbd9cacf734302c55d887f560c
Faris Ansari <netchamp.faris@gmail.com>
2022-06-10 15:11:34 +0530

    refactor: Remove Data Migration Tool

---
+++ frappe/patches/v14_0/delete_data_migration_tool.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+
+import frappe
+
+
+def execute():
+	doctypes = frappe.db.get_all("DocType", {"module": "Data Migration", "custom": 0}, pluck="name")
+	for doctype in doctypes:
+		frappe.delete_doc("DocType", doctype, ignore_missing=True)
+
+	frappe.delete_doc("Module Def", "Data Migration", ignore_missing=True, force=True)
