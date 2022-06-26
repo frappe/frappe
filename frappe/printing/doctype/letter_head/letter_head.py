5502267e33997e93108ae9c788b3cda888ca770a
Himanshu <himanshuwarekar@yahoo.com>
2022-06-26 18:10:06 +0100

    Update letter_head.py

--- frappe/printing/doctype/letter_head/letter_head.py
+++ frappe/printing/doctype/letter_head/letter_head.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import is_image
+from frappe.utils import flt, is_image
 
 
 class LetterHead(Document):
