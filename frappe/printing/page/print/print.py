4beb66f83dfba0d4b117224ddd3da96b74628db6
Gavin D'souza <gavin18d@gmail.com>
2022-06-29 12:53:49 +0530

    fix: Skip invalid field in get_print_settings_to_show
    (cherry picked from commit a15411d1e18477f980838f421477628e60d0e2bc)

--- frappe/printing/page/print/print.py
+++ frappe/printing/page/print/print.py
@@ -14,6 +14,8 @@ def get_print_settings_to_show(doctype, docname):
 	print_settings_fields = []
 	for fieldname in fields:
 		df = print_settings.meta.get_field(fieldname)
+		if not df:
+			continue
 		df.default = print_settings.get(fieldname)
 		print_settings_fields.append(df)
 
