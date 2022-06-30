7edc20dd1788b562f56d89ccb8bfcb3f4c676dd8
Aradhya <aradhyatripathi51@gmail.com>
2022-06-30 15:33:00 +0530

    Revert "Revert "feat: Adding support to Query engine""
    This reverts commit 813dcc1848b6409ac4a734359d54413665c02db8.

--- frappe/desk/doctype/number_card/number_card.py
+++ frappe/desk/doctype/number_card/number_card.py
@@ -204,7 +204,7 @@ def get_cards_for_user(doctype, txt, searchfield, start, page_len, filters):
 	if txt:
 		search_conditions = [numberCard[field].like("%{txt}%".format(txt=txt)) for field in searchfields]
 
-	condition_query = frappe.db.query.build_conditions(doctype, filters)
+	condition_query = frappe.qb.engine.build_conditions(doctype, filters)
 
 	return (
 		condition_query.select(numberCard.name, numberCard.label, numberCard.document_type)
