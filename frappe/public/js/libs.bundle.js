58d79720c6c7c072b38698c7a0bfb2930b8a7496
Suraj Shetty <surajshetty3416@gmail.com>
2022-07-04 08:25:51 +0530

    chore: Remove unused libraries and files

--- frappe/public/js/libs.bundle.js
+++ frappe/public/js/libs.bundle.js
@@ -2,9 +2,6 @@ import "./jquery-bootstrap";
 import Vue from "vue/dist/vue.esm.js";
 import "./lib/moment";
 import Sortable from "./lib/Sortable.min.js";
-// TODO: esbuild
-// Don't think jquery.hotkeys is being used anywhere. Will remove this after being sure.
-// import "./lib/jquery/jquery.hotkeys.js";
 
 window.Vue = Vue;
 window.Sortable = Sortable;
