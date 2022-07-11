49a6a99c3f3c35e7d8bcdcf7e697ba02d42710db
Faris Ansari <netchamp.faris@gmail.com>
2022-07-11 19:01:45 +0530

    fix!: dynamic homepage for all users on login

--- frappe/auth.py
+++ frappe/auth.py
@@ -194,7 +194,7 @@ class LoginManager:
 			frappe.local.cookie_manager.set_cookie("system_user", "yes")
 			if not resume:
 				frappe.local.response["message"] = "Logged In"
-				frappe.local.response["home_page"] = "/app"
+				frappe.local.response["home_page"] = "/" + get_home_page()
 
 		if not resume:
 			frappe.response["full_name"] = self.full_name
