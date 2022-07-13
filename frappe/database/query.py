164f95f0309a8a93c6abae75713f4a1f98f68db2
Ankush Menat <ankush@frappe.io>
2022-07-13 10:31:19 +0530

    refactor!: remove old weekly cleanup for route history
    This is now configurable with log settings.
    
    This also fixes circular import issue that occurs when restoring backup

--- frappe/database/query.py
+++ frappe/database/query.py
@@ -5,7 +5,6 @@ from typing import Any, Callable
 
 import frappe
 from frappe import _
-from frappe.boot import get_additional_filters_from_hooks
 from frappe.model.db_query import get_timespan_date_range
 from frappe.query_builder import Criterion, Field, Order, Table
 
@@ -173,6 +172,8 @@ class Query:
 
 	@cached_property
 	def OPERATOR_MAP(self):
+		from frappe.boot import get_additional_filters_from_hooks
+
 		# default operators
 		all_operators = OPERATOR_MAP.copy()
 
