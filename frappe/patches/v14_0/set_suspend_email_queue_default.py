4b0a9da400d87578a39a8d2b81f0efe893515dac
Sagar Vora <sagar@resilient.tech>
2022-07-18 22:32:12 +0530

    refactor: rename `hold_queue` to `suspend_email_queue`

--- frappe/patches/v14_0/set_suspend_email_queue_default.py
+++ frappe/patches/v14_0/set_suspend_email_queue_default.py
@@ -4,17 +4,10 @@ from frappe.cache_manager import clear_defaults_cache
 
 def execute():
 	frappe.db.set_default(
-		"hold_queue",
+		"suspend_email_queue",
 		frappe.db.get_default("hold_queue", "Administrator") or 0,
 		parent="__default",
 	)
 
-	frappe.db.delete(
-		"DefaultValue",
-		{
-			"defkey": "hold_queue",
-			"parent": ("!=", "__default"),
-		},
-	)
-
+	frappe.db.delete("DefaultValue", {"defkey": "hold_queue"})
 	clear_defaults_cache()
