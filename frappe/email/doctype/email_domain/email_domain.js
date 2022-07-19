037746cddc9b6aa44f4d742620f0125f08ecff55
phot0n <ritwikpuri5678@gmail.com>
2022-07-19 19:47:24 +0530

    feat(minor): fetch attachment_limit from get_max_file_size api

--- frappe/email/doctype/email_domain/email_domain.js
+++ frappe/email/doctype/email_domain/email_domain.js
@@ -4,6 +4,17 @@ frappe.ui.form.on("Email Domain", {
 			frm.dashboard.clear_headline();
 			let msg = __("Changing any setting will reflect on all the email accounts associated with this domain.");
 			frm.dashboard.set_headline_alert(msg);
+		} else {
+			if (!frm.doc.attachment_limit) {
+				frappe.call({
+					method: "frappe.core.api.file.get_max_file_size",
+					callback: function(r) {
+						if (!r.exc) {
+							frm.set_value("attachment_limit", Number(r.message)/(1024*1024));
+						}
+					},
+				});
+			}
 		}
 	}
 })
