8aa624b1c2dd31d11ab82f5a842791f280aca690
barredterra <14891507+barredterra@users.noreply.github.com>
2022-07-21 19:11:27 +0200

    patch: clean up Comments of type "Attachment"

---
+++ frappe/patches/v14_0/update_attachment_comment.py
@@ -0,0 +1,31 @@
+import frappe
+
+
+def execute():
+	# Strip everything except link to attachment and icon from comments of type "Attached"
+	for name, content in frappe.get_all(
+		"Comment", filters={"comment_type": "Attachment"}, fields=["name", "content"], as_list=True
+	):
+		if not content:
+			continue
+
+		start = content.find("<a href")
+		if start != -1:
+			content = content[start:]
+
+		end = content.find("</i>")
+		end = content.find("</a>") if end == -1 else end
+		if end != -1:
+			content = content[: end + 4]
+
+		frappe.db.set_value("Comment", name, "content", content)
+
+	# Strip "Removed " from comments of type "Attachment Removed"
+	for name, content in frappe.get_all(
+		"Comment",
+		filters={"comment_type": "Attachment Removed"},
+		fields=["name", "content"],
+		as_list=True,
+	):
+		if content and content.startswith("Removed "):
+			frappe.db.set_value("Comment", name, "content", content[8:])
