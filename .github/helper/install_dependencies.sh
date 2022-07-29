3e5b0e6b817993def60018a0de0eb20d5d8da3d2
Gavin D'souza <gavin18d@gmail.com>
2022-07-29 13:36:54 +0530

    ci: Install wkhtmltopdf in fg sequentially
    bg execution causes dpkg locks and unnecessary comlexity

--- .github/helper/install_dependencies.sh
+++ .github/helper/install_dependencies.sh
@@ -3,11 +3,8 @@ set -e
 
 echo "Setting Up System Dependencies..."
 
-install_wkhtmltopdf() {
-  wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
-  sudo apt install ./wkhtmltox_0.12.6-1.focal_amd64.deb
-}
-install_wkhtmltopdf &
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
+sudo apt install ./wkhtmltox_0.12.6-1.focal_amd64.deb
 
 curl -LsS -O https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
 sudo bash mariadb_repo_setup --mariadb-server-version=10.6
