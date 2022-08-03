177567e6e8c7b1237262577d2132b12f6dbd8aad
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-03 17:21:28 +0300

    Bulk unassign from

--- frappe/public/js/frappe/form/sidebar/assign_to.js
+++ frappe/public/js/frappe/form/sidebar/assign_to.js
@@ -307,3 +307,89 @@ frappe.ui.form.AssignmentDialog = class {
 		return row;
 	}
 };
+
+frappe.ui.form.UnassignFromDialog = class UnassignFromDialog {
+	constructor(opts) {
+		$.extend(this, opts);
+
+		this.make();
+		this.set_description_from_doc();
+	}
+	make() {
+		let me = this;
+
+		me.dialog = new frappe.ui.Dialog({
+			title: __('Remove from ToDo'),
+			fields: me.get_fields(),
+			primary_action_label: __("Remove"),
+			primary_action: function() {
+				let args = me.dialog.get_values();
+
+				if (args && args.unassign_from) {
+					me.dialog.set_message("Unassigning...");
+
+					frappe.call({
+						method: me.method,
+						args: $.extend(args, {
+							doctype: me.doctype,
+							name: me.docname,
+							unassign_from: args.unassign_from,
+							bulk_assign: me.bulk_assign || false,
+							re_assign: me.re_assign || false
+						}),
+						btn: me.dialog.get_primary_btn(),
+						callback: function(r) {
+							if (!r.exc) {
+								if (me.callback) {
+									me.callback(r);
+								}
+								me.dialog && me.dialog.hide();
+							} else {
+								me.dialog.clear_message();
+							}
+						},
+					});
+				}
+			},
+		});
+	}
+	unassign_from_me() {
+		let me = this;
+		let unassign_from = [];
+
+		if (me.dialog.get_value("unassign_from_me")) {
+			unassign_from.push(frappe.session.user);
+		}
+
+		me.dialog.set_value("unassign_from", unassign_from);
+	}
+	set_description_from_doc() {
+		let me = this;
+
+		if (me.frm && me.frm.meta.title_field) {
+			me.dialog.set_value("description", me.frm.doc[me.frm.meta.title_field]);
+		}
+	}
+	get_fields() {
+		let me = this;
+
+		return [
+			{
+				label: __("Unassign from me"),
+				fieldtype: 'Check',
+				fieldname: 'unassign_from_me',
+				default: 0,
+				onchange: () => me.unassign_from_me()
+			},
+			{
+				fieldtype: 'MultiSelectPills',
+				fieldname: 'unassign_from',
+				label: __("Unassign From"),
+				reqd: true,
+				get_data: function(txt) {
+					return frappe.db.get_link_options("User", txt, {user_type: "System User", enabled: 1});
+				}
+			},
+		];
+	}
+};
