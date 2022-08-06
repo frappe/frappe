1c6f3f4cd9846d0f9f329379d61285c7305b5801
hrwx <himanshuwarekar@yahoo.com>
2022-08-06 20:48:38 +0100

    chore: route to view/home for file tests
    (cherry picked from commit ea19e8a469cc78f8d8b8615778cf50b8019adb76)

--- cypress/integration/folder_navigation.js
+++ cypress/integration/folder_navigation.js
@@ -2,7 +2,7 @@ context("Folder Navigation", () => {
 	before(() => {
 		cy.visit("/login");
 		cy.login();
-		cy.visit("/app/file");
+		cy.visit("/app/file/view/home");
 	});
 
 	it("Adding Folders", () => {
