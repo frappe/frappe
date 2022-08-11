14bcd977b54ea9286a70e13f20482c3124d12bb6
Shariq Ansari <30859809+shariquerik@users.noreply.github.com>
2022-08-11 21:18:51 +0530

    feat: Webform Success Page and some customization options (#17790)

--- cypress/integration/web_form.js
+++ cypress/integration/web_form.js
@@ -142,10 +142,9 @@ context("Web Form", () => {
 	it("Custom Breadcrumbs", () => {
 		cy.visit("/app/web-form/note");
 
-		cy.findByRole("tab", { name: "Form Settings" }).click();
-		cy.get(".form-section .section-head").contains("Customization").click();
+		cy.findByRole("tab", { name: "Customization" }).click();
 		cy.fill_field("breadcrumbs", '[{"label": _("Notes"), "route":"note"}]', "Code");
-		cy.get(".form-section .section-head").contains("Customization").click();
+		cy.get(".form-tabs .nav-item .nav-link").contains("Customization").click();
 		cy.save();
 
 		cy.visit("/note/Note 1");
@@ -188,6 +187,7 @@ context("Web Form", () => {
 
 		cy.fill_field("title", " Edited");
 		cy.get(".web-form-actions button").contains("Save").click();
+		cy.get(".success-page .edit-button").click();
 		cy.get_field("title").should("have.value", "Note 1 Edited");
 	});
 
