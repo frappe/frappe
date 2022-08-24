721b2e47f3dd2e04742b6ff6afac6b9efc8be5b3
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2022-08-24 08:07:41 +0200

    fix: translate import warnings (#17924)
    (cherry picked from commit 63a60c6457cada56f9bdffa9ffd96d02289e2f57)
    
    # Conflicts:
    #	frappe/core/doctype/data_import/importer.py

--- frappe/core/doctype/data_import/importer.py
+++ frappe/core/doctype/data_import/importer.py
@@ -905,12 +905,17 @@ class Column:
 			not_exists = list(set(values) - set(exists))
 			if not_exists:
 				missing_values = ", ".join(not_exists)
+				message = _("The following values do not exist for {0}: {1}")
 				self.warnings.append(
 					{
 						"col": self.column_number,
+<<<<<<< HEAD
 						"message": (
 							"The following values do not exist for {}: {}".format(self.df.options, missing_values)
 						),
+=======
+						"message": message.format(self.df.options, missing_values),
+>>>>>>> 63a60c6457 (fix: translate import warnings (#17924))
 						"type": "warning",
 					}
 				)
@@ -920,17 +925,18 @@ class Column:
 			if not self.date_format:
 				if self.df.fieldtype == "Time":
 					self.date_format = "%H:%M:%S"
-					format = "HH:mm:ss"
+					date_format = "HH:mm:ss"
 				else:
 					self.date_format = "%Y-%m-%d"
-					format = "yyyy-mm-dd"
+					date_format = "yyyy-mm-dd"
 
+				message = _(
+					"{0} format could not be determined from the values in this column. Defaulting to {1}."
+				)
 				self.warnings.append(
 					{
 						"col": self.column_number,
-						"message": _(
-							"{0} format could not be determined from the values in this column. Defaulting to {1}."
-						).format(self.df.fieldtype, format),
+						"message": message.format(self.df.fieldtype, date_format),
 						"type": "info",
 					}
 				)
@@ -940,6 +946,7 @@ class Column:
 				values = list(set([cstr(v) for v in self.column_values[1:] if v]))
 				invalid = list(set(values) - set(options))
 				if invalid:
+<<<<<<< HEAD
 					valid_values = ", ".join([frappe.bold(o) for o in options])
 					invalid_values = ", ".join([frappe.bold(i) for i in invalid])
 					self.warnings.append(
@@ -949,6 +956,15 @@ class Column:
 								"The following values are invalid: {0}. Values must be"
 								" one of {1}".format(invalid_values, valid_values)
 							),
+=======
+					valid_values = ", ".join(frappe.bold(o) for o in options)
+					invalid_values = ", ".join(frappe.bold(i) for i in invalid)
+					message = _("The following values are invalid: {0}. Values must be one of {1}")
+					self.warnings.append(
+						{
+							"col": self.column_number,
+							"message": message.format(invalid_values, valid_values),
+>>>>>>> 63a60c6457 (fix: translate import warnings (#17924))
 						}
 					)
 
