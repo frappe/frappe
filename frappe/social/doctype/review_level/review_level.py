2513767c00cd2d2f255336ee103f385743ed99ef
phot0n <ritwikpuri5678@gmail.com>
2022-09-11 20:31:01 +0530

    refactor: remove social module

--- frappe/social/doctype/review_level/review_level.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2019, Frappe Technologies and contributors
-# License: MIT. See LICENSE
-
-# import frappe
-from frappe.model.document import Document
-
-
-class ReviewLevel(Document):
-	pass
