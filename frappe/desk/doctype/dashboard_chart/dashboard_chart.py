413efe49859485eb1ab020c17152271811c881c8
Ankush Menat <ankush@frappe.io>
2022-10-05 12:48:32 +0530

    fix: let charts handle aggregation (#18293)
    Currently we are doing aggregation of remaining slices manually BUT this
    breaks basic principle of DRY:
    
    - dashboard chart decides how many groups to consider
    - frappe charts has different maxSlices config
    
    This results in mess like frappe computing "Other" and Frappe charts
    computing "rest" separately.
    
    Fix: Just pass all data to charts and let it figure out the "Rest".
    (cherry picked from commit d597acb5e209f67fd639d6231be66e2b7479b413)

--- frappe/desk/doctype/dashboard_chart/dashboard_chart.py
+++ frappe/desk/doctype/dashboard_chart/dashboard_chart.py
@@ -293,13 +293,6 @@ def get_group_by_chart_config(chart, filters):
 	)
 
 	if data:
-		if chart.number_of_groups and chart.number_of_groups < len(data):
-			other_count = 0
-			for i in range(chart.number_of_groups - 1, len(data)):
-				other_count += data[i]["count"]
-			data = data[0 : chart.number_of_groups - 1]
-			data.append({"name": "Other", "count": other_count})
-
 		chart_config = {
 			"labels": [item["name"] if item["name"] else "Not Specified" for item in data],
 			"datasets": [{"name": chart.name, "values": [item["count"] for item in data]}],
