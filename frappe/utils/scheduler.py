453a3144d410c2d5549e91171849efc06e20bace
Ankush Menat <ankush@frappe.io>
2022-10-10 13:09:34 +0530

    refactor!: scheduler without external libraries (#18331)
    (cherry picked from commit c3b547f897c70136f5d2be5ec1c35bf3e89a7959)
    
    # Conflicts:
    #	pyproject.toml

--- frappe/utils/scheduler.py
+++ frappe/utils/scheduler.py
@@ -14,30 +14,25 @@ from __future__ import print_function, unicode_literals
 import os
 import time
 
-# imports - third party imports
-import schedule
-
 # imports - module imports
 import frappe
 from frappe.core.doctype.user.user import STANDARD_USERS
 from frappe.installer import update_site_config
-from frappe.utils import get_datetime, get_sites, now_datetime
+from frappe.utils import cint, get_datetime, get_sites, now_datetime
 from frappe.utils.background_jobs import get_jobs
 
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
 def start_scheduler():
-	"""Run enqueue_events_for_all_sites every 2 minutes (default).
+	"""Run enqueue_events_for_all_sites based on scheduler tick.
 	Specify scheduler_interval in seconds in common_site_config.json"""
 
-	schedule.every(frappe.get_conf().scheduler_tick_interval or 60).seconds.do(
-		enqueue_events_for_all_sites
-	)
+	tick = cint(frappe.get_conf().scheduler_tick_interval) or 60
 
 	while True:
-		schedule.run_pending()
-		time.sleep(1)
+		time.sleep(tick)
+		enqueue_events_for_all_sites()
 
 
 def enqueue_events_for_all_sites():
