9ce8d6fd6d93e5a5b016c54979b59b4c6fcd0bee
Ankush Menat <ankush@frappe.io>
2022-10-13 15:55:40 +0530

    chore: dead code removal (#18410)
    * chore!: Drop dead code related to configs/moduelview
    
    * chore!: Drop dead boilerplate
    
    * chore: drop desktop.py boilerplate
    
    * chore!: Remove report dump
    
    (cherry picked from commit eebf7e1fff07cf4b11cbc8aa5d7835c76e457487)
    
    # Conflicts:
    #	frappe/utils/boilerplate.py

--- frappe/config/__init__.py
+++ frappe/config/__init__.py
@@ -1,11 +1,5 @@
 import frappe
 from frappe import _
-from frappe.desk.moduleview import (
-	config_exists,
-	get_data,
-	get_module_link_items_from_list,
-	get_onboard_items,
-)
 
 
 def get_modules_from_all_apps_for_user(user=None):
@@ -26,9 +20,6 @@ def get_modules_from_all_apps_for_user(user=None):
 		# Apply onboarding status
 		if module_name in empty_tables_by_module:
 			module["onboard_present"] = 1
-
-		# Set defaults links
-		module["links"] = get_onboard_items(module["app"], frappe.scrub(module_name))[:5]
 
 	return allowed_modules_list
 
--- frappe/tests/test_config.py
+++ frappe/tests/test_config.py
@@ -9,8 +9,6 @@ class TestConfig(FrappeTestCase):
 	def test_get_modules(self):
 		frappe_modules = frappe.get_all("Module Def", filters={"app_name": "frappe"}, pluck="name")
 		all_modules_data = get_modules_from_all_apps_for_user()
-		first_module_entry = all_modules_data[0]
 		all_modules = [x["module_name"] for x in all_modules_data]
-		self.assertIn("links", first_module_entry)
 		self.assertIsInstance(all_modules_data, list)
 		self.assertFalse([x for x in frappe_modules if x not in all_modules])
