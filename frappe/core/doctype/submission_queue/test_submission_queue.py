0eb2458278f3bb55219648ff7e537706503ad284
Aradhya <aradhyatripathi51@gmail.com>
2022-10-15 21:49:33 +0530

    test: fixed queueing test and added completion check

--- frappe/core/doctype/submission_queue/test_submission_queue.py
+++ frappe/core/doctype/submission_queue/test_submission_queue.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2022, Frappe Technologies and Contributors
 # See license.txt
 
+import time
+
 import frappe
 from frappe.tests.utils import FrappeTestCase
 from frappe.utils.background_jobs import get_queue
@@ -9,11 +11,18 @@ from frappe.utils.background_jobs import get_queue
 class TestSubmissionQueue(FrappeTestCase):
 	queue = get_queue(qtype="default")
 
-	def test_queue_creation(self):
+	def test_queue_operation(self):
 		from frappe.core.doctype.submission_queue.submission_queue import queue_submission
 
 		doc = frappe.get_doc({"doctype": "ToDo", "description": "Something"}).insert()
 		queue_submission(doc, "submit")
 		submission_queue = frappe.get_last_doc("Submission Queue")
+
+		# Test queueing / starting
 		job = self.queue.fetch_job(submission_queue.job_id)
-		self.assertEqual(job.get_status(refresh=True), "queued")
+		self.assertIn(job.get_status(refresh=True), ("queued", "started"))
+
+		time.sleep(2)
+
+		# Test completion
+		self.assertEqual(job.get_status(refresh=True), "finished")
