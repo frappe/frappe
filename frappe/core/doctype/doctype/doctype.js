b0c1e400ea165fc5b4d34ff931df0d6b265455cd
Shariq Ansari <30859809+shariquerik@users.noreply.github.com>
2022-10-17 16:36:01 +0530

    feat: configurable default views (#18409)
    Co-authored-by: hrwx <himanshuwarekar@yahoo.com>
    Co-authored-by: Raffael Meyer <14891507+barredterra@users.noreply.github.com>
    Co-authored-by: Ankush Menat <ankush@frappe.io>

--- frappe/core/doctype/doctype/doctype.js
+++ frappe/core/doctype/doctype/doctype.js
@@ -55,6 +55,7 @@ frappe.ui.form.on("DocType", {
 
 		if (frm.is_new()) {
 			frm.events.set_default_permission(frm);
+			frm.set_value("default_view", "List");
 		} else {
 			frm.toggle_enable("engine", 0);
 		}
@@ -66,12 +67,14 @@ frappe.ui.form.on("DocType", {
 
 		frm.cscript.autoname(frm);
 		frm.cscript.set_naming_rule_description(frm);
+		frm.trigger("setup_default_views");
 	},
 
 	istable: (frm) => {
 		if (frm.doc.istable && frm.is_new()) {
 			frm.set_value("autoname", "autoincrement");
 			frm.set_value("allow_rename", 0);
+			frm.set_value("default_view", null);
 		} else if (!frm.doc.istable && !frm.is_new()) {
 			frm.events.set_default_permission(frm);
 		}
@@ -81,6 +84,18 @@ frappe.ui.form.on("DocType", {
 		if (!(frm.doc.permissions && frm.doc.permissions.length)) {
 			frm.add_child("permissions", { role: "System Manager" });
 		}
+	},
+
+	is_tree: (frm) => {
+		frm.trigger("setup_default_views");
+	},
+
+	is_calendar_and_gantt: (frm) => {
+		frm.trigger("setup_default_views");
+	},
+
+	setup_default_views: (frm) => {
+		frappe.model.set_default_views_for_doctype(frm.doc.name, frm);
 	},
 });
 
@@ -170,6 +185,10 @@ frappe.ui.form.on("DocField", {
 
 	fieldtype: function (frm) {
 		frm.trigger("max_attachments");
+	},
+
+	fields_add: (frm) => {
+		frm.trigger("setup_default_views");
 	},
 });
 
