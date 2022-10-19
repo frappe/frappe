3700ebbf4b64c8a54bb6c122d4fe6e0805910ef6
PeterG <this-gavagai@users.noreply.github.com>
2022-10-19 04:46:03 +0000

    (chore) linter appeasement

--- frappe/integrations/doctype/google_calendar/google_calendar.py
+++ frappe/integrations/doctype/google_calendar/google_calendar.py
@@ -571,15 +571,15 @@ def google_calendar_to_repeat_on(start, end, recurrence=None):
 			get_datetime(start.get("date")) 
 			if start.get("date")
 			else parser.parse(start.get("dateTime"))
-				.astimezone(ZoneInfo(get_time_zone()))
-				.replace(tzinfo=None)
+			.astimezone(ZoneInfo(get_time_zone()))
+			.replace(tzinfo=None)
 		),
 		"ends_on": (
 			get_datetime(end.get("date"))
 			if end.get("date")
 			else parser.parse(end.get("dateTime"))
-				.astimezone(ZoneInfo(get_time_zone()))
-				.replace(tzinfo=None)
+			.astimezone(ZoneInfo(get_time_zone()))
+			.replace(tzinfo=None)
 		),
 		"all_day": 1 if start.get("date") else 0,
 		"repeat_this_event": 1 if recurrence else 0,
