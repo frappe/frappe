3a4040b0b1222bf6bf635ae59c0d2ebfa2446148
MOHAMMED NIYAS <76736615+niyazrazak@users.noreply.github.com>
2022-10-26 09:38:58 +0530

    feat: status indicator

--- frappe/desk/doctype/event/event_list.js
+++ frappe/desk/doctype/event/event_list.js
@@ -1,5 +1,14 @@
 frappe.listview_settings["Event"] = {
-	add_fields: ["starts_on", "ends_on"],
+	add_fields: ["starts_on", "ends_on", "status"],
+	get_indicator: function(doc) {
+		if (doc.status === "Closed") {
+			return [__("Closed"), "green", "status,=,Closed"];
+		}, else if (doc.status === "Completed") {
+			return [__("Completed"), "green", "status,=,Completed"];
+		} else if (doc.status === "Open") {
+			return [__("Open"), "red", "status,=,Open"];
+		}
+	},
 	onload: function () {
 		frappe.route_options = {
 			status: "Open",
