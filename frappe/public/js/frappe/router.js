ccbd6ffab371d01dbb1ef09b2634b801a2b322bd
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-10-27 16:30:28 +0530

    fix(formatting): use snake case for variable names

--- frappe/public/js/frappe/router.js
+++ frappe/public/js/frappe/router.js
@@ -38,13 +38,13 @@ $("body").on("click", "a", function (e) {
 		return false;
 	};
 
-	const targetElement = e.currentTarget;
-	const href = targetElement.getAttribute("href");
-	const isOfSameHost = targetElement.hostname === window.location.hostname;
+	const target_element = e.currentTarget;
+	const href = target_element.getAttribute("href");
+	const is_on_same_host = target_element.hostname === window.location.hostname;
 
 	// click handled, but not by href
 	if (
-		targetElement.getAttribute("onclick") || // has a handler
+		target_element.getAttribute("onclick") || // has a handler
 		e.ctrlKey ||
 		e.metaKey || // open in a new tab
 		href === "#" // hash is home
@@ -58,20 +58,20 @@ $("body").on("click", "a", function (e) {
 
 	if (href && href.startsWith("#")) {
 		// target startswith "#", this is a v1 style route, so remake it.
-		return override(targetElement.hash);
+		return override(target_element.hash);
 	}
 
-	if (isOfSameHost && frappe.router.is_app_route(targetElement.pathname)) {
+	if (is_on_same_host && frappe.router.is_app_route(target_element.pathname)) {
 		// target has "/app, this is a v2 style route.
 
-		if (targetElement.search) {
+		if (target_element.search) {
 			frappe.route_options = {};
-			let params = new URLSearchParams(targetElement.search);
+			let params = new URLSearchParams(target_element.search);
 			for (const [key, value] of params) {
 				frappe.route_options[key] = value;
 			}
 		}
-		return override(targetElement.pathname + targetElement.hash);
+		return override(target_element.pathname + target_element.hash);
 	}
 });
 
