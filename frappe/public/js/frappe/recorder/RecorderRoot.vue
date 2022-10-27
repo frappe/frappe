9c09c65de674f4e80d5f7358ef7d158ac62e2502
Sagar Vora <sagar@resilient.tech>
2022-10-27 14:42:36 +0530

    fix: use `async...await` when parsing route

--- frappe/public/js/frappe/recorder/RecorderRoot.vue
+++ frappe/public/js/frappe/recorder/RecorderRoot.vue
@@ -8,8 +8,8 @@
 export default {
 	name: "RecorderRoot",
 	watch: {
-		$route() {
-			frappe.router.current_route = frappe.router.parse();
+		async $route() {
+			frappe.router.current_route = await frappe.router.parse();
 			frappe.breadcrumbs.update();
 		}
 	}
