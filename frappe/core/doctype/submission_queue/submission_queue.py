1856d2eb3f42cab4d5d88f176d2b56349092e819
phot0n <ritwikpuri5678@gmail.com>
2022-10-30 19:11:13 +0530

    fix: realtime alert
    use session object's last updated time to figure out whether to send a realtime alert or a notification

--- frappe/core/doctype/submission_queue/submission_queue.py
+++ frappe/core/doctype/submission_queue/submission_queue.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2022, Frappe Technologies and contributors
 # For license information, please see license.txt
 
+from datetime import datetime
+
 from rq.exceptions import NoSuchJobError
 from rq.job import Job
 
@@ -9,7 +11,7 @@ from frappe import _
 from frappe.desk.doctype.notification_log.notification_log import enqueue_create_notification
 from frappe.model.document import Document
 from frappe.monitor import add_data_to_monitor
-from frappe.utils import now
+from frappe.utils import DATETIME_FORMAT, now, time_diff_in_seconds
 from frappe.utils.background_jobs import get_redis_conn
 
 
@@ -26,9 +28,10 @@ class SubmissionQueue(Document):
 	def queued_doc(self):
 		return getattr(self, "to_be_queued_doc", frappe.get_doc(self.ref_doctype, self.ref_docname))
 
-	def insert(self, to_be_queued_doc: Document, action: str):
+	def insert(self, to_be_queued_doc: Document, action: str, session_id: str):
 		self.to_be_queued_doc = to_be_queued_doc
 		self.action_for_queuing = action
+		self.enqueued_by_session_id = session_id
 		super().insert(ignore_permissions=True)
 
 	def lock(self):
@@ -42,6 +45,7 @@ class SubmissionQueue(Document):
 			"background_submission",
 			to_be_queued_doc=self.queued_doc,
 			action_for_queuing=self.action_for_queuing,
+			enqueued_by_session_id=self.enqueued_by_session_id,
 			timeout=600,
 		)
 		frappe.db.set_value(
@@ -51,7 +55,9 @@ class SubmissionQueue(Document):
 			update_modified=False,
 		)
 
-	def background_submission(self, to_be_queued_doc: Document, action_for_queuing: str):
+	def background_submission(
+		self, to_be_queued_doc: Document, action_for_queuing: str, enqueued_by_session_id: str
+	):
 		_action = action_for_queuing.lower()
 
 		if _action == "update":
@@ -67,9 +73,9 @@ class SubmissionQueue(Document):
 
 		values["ended_at"] = now()
 		frappe.db.set_value(self.doctype, self.name, values, update_modified=False)
-		self.notify(values["status"], action_for_queuing)
+		self.notify(values["status"], action_for_queuing, enqueued_by_session_id)
 
-	def notify(self, submission_status: str, action: str):
+	def notify(self, submission_status: str, action: str, session_id: str):
 		if submission_status == "Failed":
 			doctype = self.doctype
 			docname = self.name
@@ -79,37 +85,33 @@ class SubmissionQueue(Document):
 			docname = self.ref_docname
 			message = _("Submission of {0} {1} with action {2} completed successfully")
 
-		message = (
-			message.format(
-				frappe.bold(str(self.ref_doctype)), frappe.bold(self.ref_docname), frappe.bold(action)
-			)
-			+ f" view it <a href='/app/{self.ref_doctype.lower().replace(' ', '-')}/{self.ref_docname}'><b>here</b></a>"
+		message = message.format(
+			frappe.bold(str(self.ref_doctype)), frappe.bold(self.ref_docname), frappe.bold(action)
 		)
 
-		# TODO: this is messed up
-		if self.enqueued_by == frappe.session.user:
+		if time_diff_in_seconds(now(), get_user_last_request_time(session_id)) < 60:
 			frappe.publish_realtime(
 				"msgprint",
 				{
-					"message": message,
+					"message": message
+					+ f". View it <a href='/app/{doctype.lower().replace(' ', '-')}/{docname}'><b>here</b></a>",
 					"alert": True,
-					"indicator": "orange" if submission_status == "Failed" else "green",
+					"indicator": "red" if submission_status == "Failed" else "green",
 				},
+				user=self.enqueued_by,
 			)
 		else:
 			notification_doc = {
 				"type": "Alert",
 				"document_type": doctype,
 				"document_name": docname,
-				"subject": message.format(
-					frappe.bold(str(self.ref_doctype)), frappe.bold(self.ref_docname), frappe.bold(action)
-				),
+				"subject": message,
 			}
 
 			notify_to = frappe.db.get_value("User", self.enqueued_by, fieldname="email")
 			enqueue_create_notification([notify_to], notification_doc)
 
-	def unlock_reference_doc(self):
+	def _unlock_reference_doc(self):
 		try:
 			job = Job.fetch(self.job_id, connection=get_redis_conn())
 			status = job.get_status(refresh=True)
@@ -136,7 +138,7 @@ class SubmissionQueue(Document):
 		if self.status != "Queued":
 			return
 
-		self.unlock_reference_doc()
+		self._unlock_reference_doc()
 
 	@staticmethod
 	def clear_old_logs(days=30):
@@ -147,12 +149,21 @@ class SubmissionQueue(Document):
 		frappe.db.delete(table, filters=(table.modified < (Now() - Interval(days=days))))
 
 
+def get_user_last_request_time(session_id):
+	return (
+		frappe.cache()
+		.hget("session", session_id)
+		.get("data", {})
+		.get("last_updated", datetime.min.strftime(DATETIME_FORMAT))
+	)
+
+
 def queue_submission(doc: Document, action: str):
 	queue = frappe.new_doc("Submission Queue")
 	queue.state = "Queued"
 	queue.ref_doctype = doc.doctype
 	queue.ref_docname = doc.name
-	queue.insert(doc, action)
+	queue.insert(doc, action, frappe.session.sid)
 
 	frappe.msgprint(
 		_("Queued for Submission. You can track the progress over {0}.").format(
