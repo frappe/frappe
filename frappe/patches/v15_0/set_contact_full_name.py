14ffa90b2760ccc31782680a1d2cb7967629a8d7
barredterra <14891507+barredterra@users.noreply.github.com>
2022-11-01 17:49:27 +0100

    patch: autocommit and don't update modified

--- frappe/patches/v15_0/set_contact_full_name.py
+++ frappe/patches/v15_0/set_contact_full_name.py
@@ -4,9 +4,17 @@ from frappe.contacts.doctype.contact.contact import get_full_name
 
 def execute():
 	"""Set full name for all contacts"""
+	frappe.db.auto_commit_on_many_writes = 1
+
 	for name, first, middle, last, company in frappe.get_all(
 		"Contact",
 		fields=["name", "first_name", "middle_name", "last_name", "company_name"],
 		as_list=True,
 	):
-		frappe.db.set_value("Contact", name, "full_name", get_full_name(first, middle, last, company))
+		frappe.db.set_value(
+			"Contact",
+			name,
+			"full_name",
+			get_full_name(first, middle, last, company),
+			update_modified=False,
+		)
