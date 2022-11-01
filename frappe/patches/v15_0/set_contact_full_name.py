27c6c33d64acc01cfc0bafad733bfdd1b93ada1a
barredterra <14891507+barredterra@users.noreply.github.com>
2022-11-01 17:55:48 +0100

    patch: print progress while updating contacts

--- frappe/patches/v15_0/set_contact_full_name.py
+++ frappe/patches/v15_0/set_contact_full_name.py
@@ -1,16 +1,20 @@
 import frappe
 from frappe.contacts.doctype.contact.contact import get_full_name
+from frappe.utils import update_progress_bar
 
 
 def execute():
 	"""Set full name for all contacts"""
 	frappe.db.auto_commit_on_many_writes = 1
 
-	for name, first, middle, last, company in frappe.get_all(
+	contacts = frappe.get_all(
 		"Contact",
 		fields=["name", "first_name", "middle_name", "last_name", "company_name"],
 		as_list=True,
-	):
+	)
+	total = len(contacts)
+	for idx, (name, first, middle, last, company) in enumerate(contacts):
+		update_progress_bar("Setting full name for contacts", idx, total)
 		frappe.db.set_value(
 			"Contact",
 			name,
