ff11a8b83ff43e352bb9f776396764aea3b8b1ed
Ankush Menat <ankush@frappe.io>
2022-11-03 20:49:05 +0530

    build: py3.11 support (#18550)
    * chore: bump RestrictedPython
    
    * ci: use py3.11 on development version
    
    * fix: py311 compat changes
    
    * chore: bump to final versions
    
    (cherry picked from commit 0ed7600604e9574cc0e5841e38f3dd8333645e8d)
    
    # Conflicts:
    #	frappe/utils/background_jobs.py

--- frappe/utils/background_jobs.py
+++ frappe/utils/background_jobs.py
@@ -3,7 +3,11 @@ import socket
 import time
 from collections import defaultdict
 from functools import lru_cache
+<<<<<<< HEAD
 from typing import TYPE_CHECKING
+=======
+from typing import TYPE_CHECKING, Any, Union
+>>>>>>> 0ed7600604 (build: py3.11 support (#18550))
 from uuid import uuid4
 
 import redis
@@ -60,7 +64,11 @@ def enqueue(
 	*,
 	at_front=False,
 	**kwargs,
+<<<<<<< HEAD
 ) -> "Job":
+=======
+) -> Union["Job", Any]:
+>>>>>>> 0ed7600604 (build: py3.11 support (#18550))
 	"""
 	Enqueue method to be executed using a background worker
 
