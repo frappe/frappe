4ec69e9384813a2c0b6b4272d55fdc43892078e9
Aradhya <aradhyatripathi51@gmail.com>
2022-11-08 12:55:33 +0530

    refactor: using after commit in enqueuing & removed session time logic

--- frappe/core/doctype/submission_queue/submission_queue.py
+++ frappe/core/doctype/submission_queue/submission_queue.py
@@ -1,9 +1,7 @@
 # Copyright (c) 2022, Frappe Technologies and contributors
 # For license information, please see license.txt
 
-from datetime import datetime
-
-from rq.exceptions import NoSuchJobError
+from rq import get_current_job
 from rq.job import Job
 from rq.registry import FailedJobRegistry
 
@@ -12,7 +10,7 @@ from frappe import _
 from frappe.desk.doctype.notification_log.notification_log import enqueue_create_notification
 from frappe.model.document import Document
 from frappe.monitor import add_data_to_monitor
-from frappe.utils import DATETIME_FORMAT, now, time_diff_in_seconds
+from frappe.utils import now, time_diff_in_seconds
 from frappe.utils.background_jobs import get_queue, get_redis_conn
 from frappe.utils.data import cint
 
@@ -38,10 +36,9 @@ class SubmissionQueue(Document):
 		table = frappe.qb.DocType("Submission Queue")
 		frappe.db.delete(table, filters=(table.modified < (Now() - Interval(days=days))))
 
-	def insert(self, to_be_queued_doc: Document, action: str, session_id: str):
+	def insert(self, to_be_queued_doc: Document, action: str):
 		self.to_be_queued_doc = to_be_queued_doc
 		self.action_for_queuing = action
-		self.enqueued_by_session_id = session_id
 		super().insert(ignore_permissions=True)
 
 	def lock(self):
@@ -51,23 +48,24 @@ class SubmissionQueue(Document):
 		self.queued_doc.unlock()
 
 	def after_insert(self):
-		job = self.queue_action(
+		self.queue_action(
 			"background_submission",
 			to_be_queued_doc=self.queued_doc,
 			action_for_queuing=self.action_for_queuing,
-			enqueued_by_session_id=self.enqueued_by_session_id,
 			timeout=600,
+			enqueue_after_commit=True,
 		)
+
+	def background_submission(self, to_be_queued_doc: Document, action_for_queuing: str):
+		current_job = get_current_job()
+
+		# Set the job id for that submission doctype
 		frappe.db.set_value(
 			self.doctype,
 			self.name,
-			{"job_id": job.id},
+			{"job_id": current_job.id},
 			update_modified=False,
 		)
-
-	def background_submission(
-		self, to_be_queued_doc: Document, action_for_queuing: str, enqueued_by_session_id: str
-	):
 		_action = action_for_queuing.lower()
 		if _action == "update":
 			_action = "submit"
@@ -82,9 +80,9 @@ class SubmissionQueue(Document):
 
 		values["ended_at"] = now()
 		frappe.db.set_value(self.doctype, self.name, values, update_modified=False)
-		self.notify(values["status"], action_for_queuing, enqueued_by_session_id)
+		self.notify(values["status"], action_for_queuing)
 
-	def notify(self, submission_status: str, action: str, session_id: str):
+	def notify(self, submission_status: str, action: str):
 		if submission_status == "Failed":
 			doctype = self.doctype
 			docname = self.name
@@ -98,7 +96,7 @@ class SubmissionQueue(Document):
 			frappe.bold(str(self.ref_doctype)), frappe.bold(self.ref_docname), frappe.bold(action)
 		)
 
-		if cint(time_diff_in_seconds(now(), get_user_last_request_time(session_id))) <= 60:
+		if cint(time_diff_in_seconds(self.created_at, now())) <= 60:
 			frappe.publish_realtime(
 				"msgprint",
 				{
@@ -120,13 +118,9 @@ class SubmissionQueue(Document):
 			notify_to = frappe.db.get_value("User", self.enqueued_by, fieldname="email")
 			enqueue_create_notification([notify_to], notification_doc)
 
-		frappe.publish_realtime(f"reload_doc_{self.ref_doctype}_{self.ref_docname}")
-
 	def _unlock_reference_doc(self):
-		try:
-			job = Job.fetch(self.job_id, connection=get_redis_conn())
-			status = job.get_status(refresh=True)
-		except NoSuchJobError:
+		job_id = frappe.db.get_value(self.doctype, self.name, "job_id")
+		if not job_id:
 			# assuming the job failed here (?)
 			status = "failed"
 
@@ -147,8 +141,10 @@ class SubmissionQueue(Document):
 
 			# Defining job exception when unlocking document.
 			registry = FailedJobRegistry(queue=get_queue(qtype="default"))
-			for job_id in registry.get_job_ids():
-				if job_id == self.job_id:
+			# If job id is None then this job won't exist in the failed registry
+			for jid in registry.get_job_ids():
+				if jid == job_id:
+					job = Job.fetch(job_id, connection=get_redis_conn())
 					values = {"status": "Failed", "exception": job.exc_info}
 
 			frappe.db.set_value(self.doctype, self.name, values, update_modified=False)
@@ -166,21 +162,12 @@ class SubmissionQueue(Document):
 		self._unlock_reference_doc()
 
 
-def get_user_last_request_time(session_id):
-	return (
-		frappe.cache()
-		.hget("session", session_id)
-		.get("data", {})
-		.get("last_updated", datetime.min.strftime(DATETIME_FORMAT))
-	)
-
-
 def queue_submission(doc: Document, action: str):
 	queue = frappe.new_doc("Submission Queue")
 	queue.state = "Queued"
 	queue.ref_doctype = doc.doctype
 	queue.ref_docname = doc.name
-	queue.insert(doc, action, frappe.session.sid)
+	queue.insert(doc, action)
 
 	frappe.msgprint(
 		_("Queued for Submission. You can track the progress over {0}.").format(
