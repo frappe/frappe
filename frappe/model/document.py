86ef6c32680a16e9330a3db41a11c4aad465c05a
gavin <gavin18d@gmail.com>
2022-11-10 16:53:28 +0530

    perf(doc): skip order_by when name is set in load_from_db (#18837)
    Setting ORDER BY clause in the SQL nudges MariaDB to not use index
    (even) for primary keys.
    
    (cherry picked from commit 9ad053852157b1965ffa0c8f88666175315590f7)
    
    # Conflicts:
    #	frappe/model/document.py

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -146,7 +146,18 @@ class Document(BaseDocument):
 			self._fix_numeric_types()
 
 		else:
+<<<<<<< HEAD
 			d = frappe.db.get_value(self.doctype, self.name, "*", as_dict=1, for_update=self.flags.for_update)
+=======
+			get_value_kwargs = {"for_update": self.flags.for_update, "as_dict": True}
+			if not isinstance(self.name, (dict, list)):
+				get_value_kwargs["order_by"] = None
+
+			d = frappe.db.get_value(
+				doctype=self.doctype, filters=self.name, fieldname="*", **get_value_kwargs
+			)
+
+>>>>>>> 9ad0538521 (perf(doc): skip order_by when name is set in load_from_db (#18837))
 			if not d:
 				frappe.throw(
 					_("{0} {1} not found").format(_(self.doctype), self.name),
