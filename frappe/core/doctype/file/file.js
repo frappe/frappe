72f8d53b0809834bdf9c4e97b8593307350a97b2
Abdo Hamoud <abdo.host@gmail.com>
2022-11-15 16:59:08 +0200

    Update File Types Preview
    - Update preview types [PDF], [MP4], [Mp3]
    
    (cherry picked from commit e5ad087521843f0596e430136d1d2f143c659a4a)

--- frappe/core/doctype/file/file.js
+++ frappe/core/doctype/file/file.js
@@ -1,45 +1,72 @@
 frappe.ui.form.on("File", "refresh", function (frm) {
-	if (!frm.doc.is_folder) {
-		frm.add_custom_button(
-			__("Download"),
-			function () {
-				var file_url = frm.doc.file_url;
-				if (frm.doc.file_name) {
-					file_url = file_url.replace(/#/g, "%23");
-				}
-				window.open(file_url);
-			},
-			"fa fa-download"
-		);
-	}
+    if (!frm.doc.is_folder) {
+        frm.add_custom_button(
+            __("Download"),
+            function () {
+                var file_url = frm.doc.file_url;
+                if (frm.doc.file_name) {
+                    file_url = file_url.replace(/#/g, "%23");
+                }
+                window.open(file_url);
+            },
+            "fa fa-download"
+        );
+    }
 
-	frm.get_field("preview_html").$wrapper.html(`<div class="img_preview">
-		<img class="img-responsive" src="${frm.doc.file_url}" onerror="cur_frm.toggle_display('preview', false)" />
-	</div>`);
+    if (frappe.utils.is_image_file(frm.doc.file_url)) {
+        frm.get_field("preview_html").$wrapper.html(`<div class="img_preview">
+                                        <img class="img-responsive" src="${frm.doc.file_url}" onerror="cur_frm.toggle_display('preview', false)" />
+                                     </div>`);
+    } else if (frm.doc.file_name.split('.').pop() == 'pdf') {
+        cur_frm.toggle_display('preview', true);
+        frm.get_field("preview_html").$wrapper.html(`<div class="img_preview">
+                                          <object style="background:#323639;" width="100%">
+                                             <embed style="background:#323639;" width="100%" height="1190" src="${frm.doc.file_url}" type="application/pdf">
+                                          </object>
+                                      </div>`);
+    } else if (frm.doc.file_name.split('.').pop() == 'mp4') {
+        cur_frm.toggle_display('preview', true);
+        frm.get_field("preview_html").$wrapper.html(`<div class="img_preview">
+                                                <video width="480" height="320" controls>
+                                                    <source src="${frm.doc.file_url}" type="video/mp4">
+                                                    ${__("Your browser does not support the video element.")}
+                                                </video>
+                                     </div>`);
+    } else if (frm.doc.file_name.split('.').pop() == 'mp3') {
+        cur_frm.toggle_display('preview', true);
+        frm.get_field("preview_html").$wrapper.html(`<div class="img_preview">
+                                                <audio width="480" height="60" controls>
+                                                    <source src="${frm.doc.file_url}" type="audio/mpeg">
+                                                    ${__("Your browser does not support the audio element.")}
+                                                </audio >
+                                     </div>`);
+    } else {
+        cur_frm.toggle_display('preview', false);
+    }
 
-	var is_raster_image = /\.(gif|jpg|jpeg|tiff|png)$/i.test(frm.doc.file_url);
-	var is_optimizable = !frm.doc.is_folder && is_raster_image && frm.doc.file_size > 0;
+    var is_raster_image = /\.(gif|jpg|jpeg|tiff|png)$/i.test(frm.doc.file_url);
+    var is_optimizable = !frm.doc.is_folder && is_raster_image && frm.doc.file_size > 0;
 
-	if (is_optimizable) {
-		frm.add_custom_button(__("Optimize"), function () {
-			frappe.show_alert(__("Optimizing image..."));
-			frm.call("optimize_file").then(() => {
-				frappe.show_alert(__("Image optimized"));
-			});
-		});
-	}
+    if (is_optimizable) {
+        frm.add_custom_button(__("Optimize"), function () {
+            frappe.show_alert(__("Optimizing image..."));
+            frm.call("optimize_file").then(() => {
+                frappe.show_alert(__("Image optimized"));
+            });
+        });
+    }
 
-	if (frm.doc.file_name && frm.doc.file_name.split(".").splice(-1)[0] === "zip") {
-		frm.add_custom_button(__("Unzip"), function () {
-			frappe.call({
-				method: "frappe.core.api.file.unzip_file",
-				args: {
-					name: frm.doc.name,
-				},
-				callback: function () {
-					frappe.set_route("List", "File");
-				},
-			});
-		});
-	}
+    if (frm.doc.file_name && frm.doc.file_name.split(".").splice(-1)[0] === "zip") {
+        frm.add_custom_button(__("Unzip"), function () {
+            frappe.call({
+                method: "frappe.core.api.file.unzip_file",
+                args: {
+                    name: frm.doc.name,
+                },
+                callback: function () {
+                    frappe.set_route("List", "File");
+                },
+            });
+        });
+    }
 });
