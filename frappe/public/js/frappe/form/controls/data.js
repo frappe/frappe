90a59a345de4f096adc7dfb5e8bbabe81af9b4b0
Maharshi Patel <maharshi.patel988@gmail.com>
2022-11-16 12:55:25 +0530

    fix: z-index for barcode and awesomeplete
    (cherry picked from commit 88e331e23687c89b73736eb72c980407e9db70c0)

--- frappe/public/js/frappe/form/controls/data.js
+++ frappe/public/js/frappe/form/controls/data.js
@@ -77,7 +77,7 @@ frappe.ui.form.ControlData = class ControlData extends frappe.ui.form.ControlInp
 
 	setup_url_field() {
 		this.$wrapper.find(".control-input").append(
-			`<span class="link-btn">
+			`<span class="link-btn scan-icon">
 				<a class="btn-open no-decoration" title="${__("Open Link")}" target="_blank">
 					${frappe.utils.icon("link-url", "sm")}
 				</a>
