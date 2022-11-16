d8cc613dadccdee52104986eb894d20201e70c7b
Gavin D'souza <gavin18d@gmail.com>
2022-11-16 15:04:55 +0530

    fix(desk): maintain realtime & cached data consistency
    - Clear docinfo_update callbacks before setting one; ensure only
      one active callback at any given point.
    - Remove document from local cache if list_update sent if not edited

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -1282,6 +1282,10 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 		}
 		frappe.socketio.list_subscribe(this.doctype);
 		frappe.realtime.on("list_update", (data) => {
+			if (!frappe.get_doc(data?.doctype, data?.name)?.__unsaved) {
+				frappe.model.remove_from_locals(data.doctype, data.name);
+			}
+
 			if (this.filter_area.is_being_edited()) {
 				return;
 			}
