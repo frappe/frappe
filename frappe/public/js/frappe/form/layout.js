5c43bbf836a37d3de4f5800a1f7e69457b03d1b6
Maharshi Patel <maharshi.patel988@gmail.com>
2022-11-17 12:10:51 +0530

    feat(UI): Sticky Tabs Header

--- frappe/public/js/frappe/form/layout.js
+++ frappe/public/js/frappe/form/layout.js
@@ -479,10 +479,51 @@ frappe.ui.form.Layout = class Layout {
 	}
 
 	setup_events() {
+		let last_scroll = 0;
+		let is_tabs = true;
+		let tabs_list;
+		let tabs_content = this.tabs_content[0];
+		tabs_content.style.scrollMarginTop = "calc(var(--navbar-height) + 52px)";
+
+		window.addEventListener(
+			"scroll",
+			frappe.utils.throttle(() => {
+				let current_scroll = document.documentElement.scrollTop;
+				if (is_tabs && !tabs_list) {
+					if ($(".form-tabs-list").length) {
+						tabs_list = $(".form-tabs-list");
+					} else {
+						is_tabs = false;
+					}
+				}
+				if (current_scroll > 0 && last_scroll <= current_scroll) {
+					if (is_tabs) {
+						tabs_list.css("top", "calc(var(--navbar-height) - 1px)");
+					}
+				} else {
+					if (is_tabs) {
+						tabs_list.css(
+							"top",
+							"calc(var(--navbar-height) + var(--page-head-height) - 1px)"
+						);
+					}
+				}
+				last_scroll = current_scroll;
+			}),
+			500
+		);
+
 		this.tab_link_container.off("click").on("click", ".nav-link", (e) => {
 			e.preventDefault();
 			e.stopImmediatePropagation();
 			$(e.currentTarget).tab("show");
+			if (tabs_content.getBoundingClientRect().top < 100) {
+				tabs_content.scrollIntoView();
+				setTimeout(() => {
+					$(".page-head").css("top", "-15px");
+					$(".form-tabs-list").css("top", "calc(var(--navbar-height) - 1px)");
+				}, 3);
+			}
 		});
 	}
 
