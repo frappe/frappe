ed2862602193cad5478e48fac8a264a310492137
Ankush Menat <ankushmenat@gmail.com>
2022-11-25 12:02:51 +0530

    refactor: simpler deque strat selection
    Co-authored-by: Ritwik Puri <ritwikpuri5678@gmail.com>

--- frappe/commands/scheduler.py
+++ frappe/commands/scheduler.py
@@ -191,7 +191,7 @@ def start_scheduler():
 	"--strategy",
 	required=False,
 	type=click.Choice(["round_robbin", "random"]),
-	help="dequeuing strategy to use.",
+	help="Dequeuing strategy to use",
 )
 def start_worker(
 	queue, quiet=False, rq_username=None, rq_password=None, burst=False, strategy=None
