81cc0713104668ee30ca9c155cbb0cd83d941b9c
Sagar Vora <sagar@resilient.tech>
2022-11-25 06:29:04 +0000

    chore: remove unused whitelisting from API (#19000)
    (cherry picked from commit b1703117fccbb49a9949d480b905e9f4dbbdddf3)

--- frappe/desk/form/linked_with.py
+++ frappe/desk/form/linked_with.py
@@ -403,7 +403,6 @@ def get_exempted_doctypes():
 	return auto_cancel_exempt_doctypes
 
 
-@frappe.whitelist()
 def get_linked_docs(doctype: str, name: str, linkinfo: dict | None = None) -> dict[str, list]:
 	if isinstance(linkinfo, str):
 		# additional fields are added in linkinfo
