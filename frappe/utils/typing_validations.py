6678007a8da60fb0d26deda0e84bff0b0204f289
Gavin D'souza <gavin18d@gmail.com>
2022-11-28 18:02:00 +0530

    fix: Support missing Optional or mismatching annotation types
    Allow default types as acceptable values if not included in typing
    hints. A common example of this is:
    
    def test_password_strength(new_password: str, user_data: Sequence = None):
    
    where Optional[Sequence] is implicit since default value is None.

--- frappe/utils/typing_validations.py
+++ frappe/utils/typing_validations.py
@@ -1,5 +1,5 @@
-from inspect import isclass
-from typing import Callable, ForwardRef, Sequence
+from inspect import _empty, isclass, signature
+from typing import Callable, ForwardRef, Sequence, Union
 
 
 def qualified_name(obj) -> str:
@@ -13,9 +13,9 @@ def qualified_name(obj) -> str:
 	discovered_type = obj if isclass(obj) else type(obj)
 	module, qualname = discovered_type.__module__, discovered_type.__qualname__
 
-	if module == "types":
+	if module == "typing":
 		return obj
-	elif module in {"typing", "builtins"}:
+	elif module in {"types", "builtins"}:
 		return qualname
 	else:
 		return f"{module}.{qualname}"
@@ -34,6 +34,10 @@ def validate_argument_types(func: Callable, args: tuple, kwargs: dict):
 		prepared_args = dict(zip(arg_names, arg_values))
 		prepared_args.update(kwargs)
 
+		# check if type hints dont match the default values
+		func_signature = signature(func)
+		func_params = dict(func_signature.parameters)
+
 		# check if the argument types are correct
 		for current_arg, current_arg_type in annotations.items():
 			if current_arg not in prepared_args:
@@ -49,6 +53,15 @@ def validate_argument_types(func: Callable, args: tuple, kwargs: dict):
 				current_arg_type = tuple(
 					x for x in current_arg_type.__args__ if not isinstance(x, (ForwardRef, str))
 				)
+
+			param_def = func_params.get(current_arg)
+
+			if param_def.default is not _empty:
+				if isinstance(current_arg_type, tuple):
+					if param_def.default not in current_arg_type:
+						current_arg_type += (type(param_def.default),)
+				elif param_def.default != current_arg_type:
+					current_arg_type = Union[current_arg_type, type(param_def.default)]
 
 			if not isinstance(current_arg_value, current_arg_type):
 				raise TypeError(
