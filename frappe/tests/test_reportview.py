2c0827dda95f14c620b80dd20030bba557afe990
barredterra <14891507+barredterra@users.noreply.github.com>
2022-11-28 12:16:18 +0100

    fix: test reportview

--- frappe/tests/test_reportview.py
+++ frappe/tests/test_reportview.py
@@ -13,22 +13,22 @@ class TestReportview(FrappeTestCase):
 
 		frappe.local.form_dict = frappe._dict(
 			doctype="DocType",
-			title="Test Report",
 			file_format_type="CSV",
+			fields=("name", "module", "issingle"),
+			filters={"issingle": 1, "module": "Core"},
 		)
 
 		for delimiter in (",", ";", "\t", "|"):
+			frappe.local.form_dict.csv_delimiter = delimiter
 			for quoting in (QUOTE_ALL, QUOTE_MINIMAL, QUOTE_NONE, QUOTE_NONNUMERIC):
-				frappe.local.form_dict.update(
-					{
-						"csv_quoting": quoting,
-						"csv_delimiter": delimiter,
-					}
-				)
+				frappe.local.form_dict.csv_quoting = quoting
+
 				export_query()
 
 				self.assertTrue(frappe.response["filename"].endswith(".csv"))
 				self.assertEqual(frappe.response["type"], "binary")
 				with StringIO(frappe.response["filecontent"].decode("utf-8")) as result:
 					reader = DictReader(result, delimiter=delimiter, quoting=quoting)
-					reader.__next__()
+					for row in reader:
+						self.assertEqual(int(row["Is Single"]), 1)
+						self.assertEqual(row["Module"], "Core")
