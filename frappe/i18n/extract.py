803cb89d758cad3ec07ff3a84534167ec2bd4762
barredterra <14891507+barredterra@users.noreply.github.com>
2022-12-13 01:28:51 +0100

    refactor: less yielding

--- frappe/i18n/extract.py
+++ frappe/i18n/extract.py
@@ -25,39 +25,42 @@ def extract_doctype_json(
 	doctype = data.get("name")
 	yield None, FUNCNAME, (None, doctype), ["Name of a DocType"]
 
+	messages = []
 	for field in data.get("fields", []):
 		fieldtype = field.get("fieldtype")
 		if label := field.get("label"):
-			yield None, FUNCNAME, (doctype, label), [f"Label of a {fieldtype} field in DocType '{doctype}'"]
+			messages.append((label, f"Label of a {fieldtype} field in DocType '{doctype}'"))
 
 		if description := field.get("description"):
-			yield None, FUNCNAME, (doctype, description), [f"Description of a field in DocType '{doctype}'"]
+			messages.append((description, f"Description of a {fieldtype} field in DocType '{doctype}'"))
 
 		if message := field.get("options"):
 			if fieldtype == "Select":
 				select_options = [option for option in message.split("\n") if option and not option.isdigit()]
 				if select_options and "icon" in select_options[0]:
 					continue
-				for option in select_options:
-					yield None, FUNCNAME, (doctype, option), [
-						f"Option for a {fieldtype} field in DocType '{doctype}'"
-					]
+				messages.extend(
+					(option, f"Option for a {fieldtype} field in DocType '{doctype}'")
+					for option in select_options
+				)
 			elif fieldtype == "HTML":
-				yield None, FUNCNAME, (doctype, message), [
-					f"Content of an {fieldtype} field in DocType '{doctype}'"
-				]
-
-	for perm in data.get("permissions", []):
-		if role := perm.get("role"):
-			# pass None as context because translation of the role should not depend on the doctype
-			yield None, FUNCNAME, (None, role), ["Name of a role"]
+				messages.append((message, f"Content of an {fieldtype} field in DocType '{doctype}'"))
 
 	for link in data.get("links", []):
 		if group := link.get("group"):
-			yield None, FUNCNAME, (doctype, group), [f"Group in {doctype}'s connections"]
+			messages.append((group, f"Group in {doctype}'s connections"))
 
 		if link_doctype := link.get("link_doctype"):
-			yield None, FUNCNAME, (doctype, link_doctype), [f"Linked DocType in {doctype}'s connections"]
+			messages.append((link_doctype, f"Linked DocType in {doctype}'s connections"))
+
+	yield from ((None, FUNCNAME, (doctype, message), [comment]) for message, comment in messages)
+
+	# pass None as the context of a role name, because it gets used with multiple doctypes
+	yield from (
+		(None, FUNCNAME, (None, perm["role"]), ["Name of a role"])
+		for perm in data.get("permissions", [])
+		if "role" in perm
+	)
 
 
 def extract_python(
