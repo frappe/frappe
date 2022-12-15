f71183f6e254e14477daf9528bbbfe0c3d45f45d
Ankush Menat <ankush@frappe.io>
2022-12-15 19:07:42 +0530

    chore: typo

--- frappe/public/js/frappe/form/form.js
+++ frappe/public/js/frappe/form/form.js
@@ -168,7 +168,7 @@ frappe.ui.form.Form = class FrappeForm {
 			},
 			{
 				'shortcut': 'shift+alt+down',
-				'description': __('To duplcate current row')
+				'description': __('Duplicate current row')
 			}
 		];
 
