27bfcd79f1a2a3d30f920a5bda4a5fc923f4d06e
Smit Vora <smitvora203@gmail.com>
2022-12-16 10:09:45 +0000

    fix: allow zero page length to return complete list

--- frappe/frappeclient.py
+++ frappe/frappeclient.py
@@ -106,7 +106,7 @@ class FrappeClient:
 			headers=self.headers,
 		)
 
-	def get_list(self, doctype, fields='["name"]', filters=None, limit_start=0, limit_page_length=0):
+	def get_list(self, doctype, fields='["name"]', filters=None, limit_start=0, limit_page_length=None):
 		"""Returns list of records of a particular type"""
 		if not isinstance(fields, str):
 			fields = json.dumps(fields)
@@ -115,7 +115,7 @@ class FrappeClient:
 		}
 		if filters:
 			params["filters"] = json.dumps(filters)
-		if limit_page_length:
+		if limit_page_length is not None:
 			params["limit_start"] = limit_start
 			params["limit_page_length"] = limit_page_length
 		res = self.session.get(
