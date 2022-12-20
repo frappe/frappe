ff71779a59a9b1b63700aa2d0fea7aded7e276ed
Ankush Menat <ankush@frappe.io>
2022-12-20 13:16:56 +0530

    fix(postgres): psql with remote databases
    If you dont use unix socket psql command doesn't work.

--- frappe/commands/utils.py
+++ frappe/commands/utils.py
@@ -548,7 +548,13 @@ def _mariadb():
 
 def _psql():
 	psql = which("psql")
-	subprocess.run([psql, "-d", frappe.conf.db_name])
+
+	host = frappe.conf.db_host or "127.0.0.1"
+	port = frappe.conf.db_port or "5432"
+	env = os.environ.copy()
+	env["PGPASSWORD"] = frappe.conf.db_password
+	conn_string = f"postgresql://{frappe.conf.db_name}@{host}:{port}/{frappe.conf.db_name}"
+	subprocess.run([psql, conn_string], check=True, env=env)
 
 
 @click.command("jupyter")
