f8588d29365fbda629ab0eb00ea58c7e8002f969
Shariq Ansari <sharique.rik@gmail.com>
2022-12-22 17:46:14 +0530

    fix: support ctrl key for linux/windows
    (cherry picked from commit 8c9c9b9f3728bd35f1df09f47d7b8fd5dcbb14a0)
    
    # Conflicts:
    #	frappe/public/js/frappe/form/layout.js

--- frappe/public/js/frappe/form/layout.js
+++ frappe/public/js/frappe/form/layout.js
@@ -476,6 +476,22 @@ frappe.ui.form.Layout = class Layout {
 		});
 	}
 
+<<<<<<< HEAD
+=======
+	setup_tooltip_events() {
+		$(document).on("keydown", (e) => {
+			if (e.metaKey || e.ctrlKey) {
+				this.wrapper.addClass("show-tooltip");
+			}
+		});
+		$(document).on("keyup", (e) => {
+			if (!e.metaKey || e.ctrlKey) {
+				this.wrapper.removeClass("show-tooltip");
+			}
+		});
+	}
+
+>>>>>>> 8c9c9b9f37 (fix: support ctrl key for linux/windows)
 	handle_tab(doctype, fieldname, shift) {
 		let grid_row = null,
 			prev = null,
