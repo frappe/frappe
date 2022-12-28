49143922c5100e8cba8876ce90bf353fbd94d40a
marination <maricadsouza221197@gmail.com>
2022-12-28 23:00:00 +0530

    chore: Kanban and ToDo UI test helpers
    - `create_admin_kanban` and `create_todo` UI test helpers
    - `switch_to_user` Cypress command: logs out and logs in as specified user
    - Used `remove_role` in permissions test
    - Used `switch_to_user` command in test helper

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -371,6 +371,11 @@ Cypress.Commands.add("update_doc", (doctype, docname, args) => {
 		});
 });
 
+Cypress.Commands.add("switch_to_user", (user) => {
+	cy.call("logout");
+	cy.login(user);
+});
+
 Cypress.Commands.add("add_role", (user, role) => {
 	cy.window()
 		.its("frappe")
@@ -391,8 +396,7 @@ Cypress.Commands.add("remove_role", (user, role) => {
 
 const add_remove_role = (action, user, role, session_user) => {
 	if (session_user !== "Administrator") {
-		cy.call("logout");
-		cy.login("Administrator");
+		cy.switch_to_user("Administrator");
 	}
 
 	cy.call("frappe.tests.ui_test_helpers.add_remove_role", {
@@ -402,8 +406,7 @@ const add_remove_role = (action, user, role, session_user) => {
 	});
 
 	if (session_user !== "Administrator") {
-		cy.call("logout");
-		cy.login(session_user);
+		cy.switch_to_user(session_user);
 	}
 };
 
