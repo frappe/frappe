67c77dbd35530d402f52bdf34f983f4e5fa08e20
Tom Finke <tom.finke@webterra.de>
2023-01-03 10:22:28 +0000

    fix: Child Table in Dialog: sortable #19468
    (cherry picked from commit f7c4288aeebb40e8f9ccb54aba9accaa1ec95526)
    
    # Conflicts:
    #	frappe/public/js/frappe/form/grid.js

--- frappe/public/js/frappe/form/grid.js
+++ frappe/public/js/frappe/form/grid.js
@@ -330,7 +330,7 @@ export default class Grid {
 		this.toggle_checkboxes(this.display_status !== 'Read');
 
 		// sortable
-		if (this.frm && this.is_sortable() && !this.sortable_setup_done) {
+		if (this.is_sortable() && !this.sortable_setup_done) {
 			this.make_sortable($rows);
 			this.sortable_setup_done = true;
 		}
@@ -475,13 +475,24 @@ export default class Grid {
 				let idx = $(event.item).closest('.grid-row').attr('data-idx') - 1;
 				let doc = this.data[idx % this.grid_pagination.page_length];
 				this.renumber_based_on_dom();
+<<<<<<< HEAD
 				this.frm.script_manager.trigger(this.df.fieldname + "_move", this.df.options, doc.name);
 				this.refresh();
 				this.frm.dirty();
 			}
+=======
+				this.frm && this.frm.script_manager.trigger(
+					this.df.fieldname + "_move",
+					this.df.options,
+					doc.name
+				);
+				this.refresh();
+				this.frm && this.frm.dirty();
+			},
+>>>>>>> f7c4288aee (fix: Child Table in Dialog: sortable #19468)
 		});
 
-		$(this.frm.wrapper).trigger("grid-make-sortable", [this.frm]);
+		this.frm && $(this.frm.wrapper).trigger("grid-make-sortable", [this.frm]);
 	}
 
 	get_data() {
@@ -663,12 +674,22 @@ export default class Grid {
 
 		$rows.find(".grid-row").each((i, item) => {
 			let $item = $(item);
+<<<<<<< HEAD
 			let index = (this.grid_pagination.page_index - 1) * this.grid_pagination.page_length + i;
 			let d = locals[this.doctype][$item.attr('data-name')];
+=======
+			let index =
+				(this.grid_pagination.page_index - 1) * this.grid_pagination.page_length + i;
+			let d = this.grid_rows_by_docname[$item.attr("data-name")].doc;
+>>>>>>> f7c4288aee (fix: Child Table in Dialog: sortable #19468)
 			d.idx = index + 1;
 			$item.attr('data-idx', d.idx);
 
+<<<<<<< HEAD
 			this.frm.doc[this.df.fieldname][index] = (d);
+=======
+			if(this.frm) this.frm.doc[this.df.fieldname][index] = d;
+>>>>>>> f7c4288aee (fix: Child Table in Dialog: sortable #19468)
 			this.data[index] = d;
 			this.grid_rows[index] = (this.grid_rows_by_docname[d.name]);
 		});
