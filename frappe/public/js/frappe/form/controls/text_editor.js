3d7daacc4a8fb5696ec2c33816bf13cd2a92012c
RJPvT <48353029+RJPvT@users.noreply.github.com>
2023-01-09 11:02:15 +0100

    fix: ordered lists
    https://github.com/frappe/frappe/issues/11520

--- frappe/public/js/frappe/form/controls/text_editor.js
+++ frappe/public/js/frappe/form/controls/text_editor.js
@@ -232,7 +232,18 @@ frappe.ui.form.ControlTextEditor = frappe.ui.form.ControlCode.extend({
 		} catch(e) {
 			value = `<div class="ql-editor read-mode">${value}</div>`;
 		}
-
+		
+	    // quill keeps ol as a common container for both type of lists
+		// and uses css for appearances, this is not semantic
+		// so we convert ol to ul if it is unordered
+		value.find('ol li[data-list=bullet]:first-child').each((i, li) => {
+			let $li = $(li);
+			let $parent = $li.parent();
+			let $children = $parent.children();
+			let $ul = $('<ul>').append($children);
+			$parent.replaceWith($ul);
+		});
+		value = value.html();
 		return value;
 	},
 
