4acc20eb0ebdb72e6b799b8bb81b20bd4720233c
Shariq Ansari <sharique.rik@gmail.com>
2023-01-12 12:30:31 +0530

    chore: linter fix
    (cherry picked from commit 7e8257c82652beadd679a05c80b6e0db2bdeef01)

--- frappe/public/js/frappe/form/controls/text_editor.js
+++ frappe/public/js/frappe/form/controls/text_editor.js
@@ -309,7 +309,7 @@ frappe.ui.form.ControlTextEditor = class ControlTextEditor extends frappe.ui.for
 			value = `<div class="ql-editor read-mode">${value}</div>`;
 		}
 		
-	    // quill keeps ol as a common container for both type of lists
+		// quill keeps ol as a common container for both type of lists
 		// and uses css for appearances, this is not semantic
 		// so we convert ol to ul if it is unordered
 		let $value = $(value);
