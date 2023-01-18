e359457151ec828407b83fc5b7b366fe13e82669
Sabu Siyad <hello@ssiyad.com>
2023-01-18 20:30:08 +0530

    feat(i18n): engine

--- frappe/i18n/engine.py
+++ frappe/i18n/engine.py
@@ -1,45 +1,47 @@
-import os
 import gettext
+import os
 
 import frappe
 
 
 def get_translator(lang: str, localedir: str | None = "locale", context: bool | None = False):
-    t = gettext.translation("messages", localedir=localedir, languages=(lang,), fallback=True)
+	t = gettext.translation("messages", localedir=localedir, languages=(lang,), fallback=True)
 
-    if context:
-        return t.pgettext
+	if context:
+		return t.pgettext
 
-    return t.gettext
+	return t.gettext
 
 
 def f(msg: str, context: str = None, lang: str = "en"):
-    from frappe import as_unicode
-    from frappe.utils import is_html, strip_html_tags
+	from frappe import as_unicode
+	from frappe.utils import is_html, strip_html_tags
 
-    if not lang:
-        lang = "en"
+	if not lang:
+		lang = "en"
 
-    msg = as_unicode(msg).strip()
+	msg = as_unicode(msg).strip()
 
-    if is_html(msg):
-        msg = strip_html_tags(msg)
+	if is_html(msg):
+		msg = strip_html_tags(msg)
 
-    apps = frappe.get_all_apps()
+	apps = frappe.get_all_apps()
 
-    for app in apps:
-        app_path = frappe.get_pymodule_path(app)
-        locale_path = os.path.join(app_path, "locale")
+	for app in apps:
+		app_path = frappe.get_pymodule_path(app)
+		locale_path = os.path.join(app_path, "locale")
+		has_context = context is not None
 
-        t = get_translator(lang, localedir=locale_path, context=context is not None)
+		if has_context:
+			t = get_translator(lang, localedir=locale_path, context=has_context)
+			r = t(context, msg)
+			if r != msg:
+				return r
 
-        if context:
-            r = t(context, msg)
-            if r:
-                return r
+		t = get_translator(lang, localedir=locale_path, context=False)
+		r = t(msg)
 
-        r = t(msg)
-        if r:
-            return r
+		if r != msg:
+			return r
 
-    return msg
+	return msg
