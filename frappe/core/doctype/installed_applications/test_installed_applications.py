d6ac238be13b0c0f9e408851a8d77600120c92bd
Ankush Menat <ankush@frappe.io>
2023-01-18 17:53:04 +0530

    feat: let users modify hook resolution order
    Since hook resolution depends on the order in which apps were installed
    on site, it should be made configurable as escape hatch in case a
    different resolution order is desired.
    
    Keep in mind that changing order affects every hook, page, customization
    so you can't pick and choose priority for individual hooks as of now.
    
    Separate proposals are welcome for such configurabilty.
    
    (cherry picked from commit 1796cae6bf8294741ffa86e8d4cd7899948b9939)
    
    # Conflicts:
    #	frappe/core/doctype/installed_applications/installed_applications.js
    #	frappe/core/doctype/installed_applications/test_installed_applications.py

--- frappe/core/doctype/installed_applications/test_installed_applications.py
+++ frappe/core/doctype/installed_applications/test_installed_applications.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2020, Frappe Technologies and Contributors
+<<<<<<< HEAD
 # See license.txt
 from __future__ import unicode_literals
 
@@ -9,3 +10,20 @@ import unittest
 
 class TestInstalledApplications(unittest.TestCase):
 	pass
+=======
+# License: MIT. See LICENSE
+
+import frappe
+from frappe.core.doctype.installed_applications.installed_applications import (
+	InvalidAppOrder,
+	update_installed_apps_order,
+)
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestInstalledApplications(FrappeTestCase):
+	def test_order_change(self):
+		update_installed_apps_order(["frappe"])
+		self.assertRaises(InvalidAppOrder, update_installed_apps_order, [])
+		self.assertRaises(InvalidAppOrder, update_installed_apps_order, ["frappe", "deepmind"])
+>>>>>>> 1796cae6bf (feat: let users modify hook resolution order)
