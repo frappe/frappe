c6c2b4401ee4729d5c871ece7080dd598ce45363
phot0n <ritwikpuri5678@gmail.com>
2023-01-18 16:26:36 +0530

    chore: linter

--- frappe/public/js/frappe/form/controls/text_editor.js
+++ frappe/public/js/frappe/form/controls/text_editor.js
@@ -232,7 +232,7 @@ frappe.ui.form.ControlTextEditor = frappe.ui.form.ControlCode.extend({
 		} catch(e) {
 			value = `<div class="ql-editor read-mode">${value}</div>`;
 		}
-		
+
 		// quill keeps ol as a common container for both type of lists
 		// and uses css for appearances, this is not semantic
 		// so we convert ol to ul if it is unordered
