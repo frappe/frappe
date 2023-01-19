b483f27ae8e93d019bbfb63e3cd939712310a678
Ankush Menat <ankush@frappe.io>
2023-01-19 21:55:31 +0530

    fix: only generate fieldname for inconsequential fields (#19663)
    Creating columns that are altered in DB on save automatically is kinda
    bad and will leave unnecessary columns in DB if user makes a mistake or
    forgets to add a fieldname explicitly.
    
    The default generated names are meaningless like "float_asdx", there is
    no use case for this.
    
    [skip ci]

--- frappe/core/doctype/file/file.py
+++ frappe/core/doctype/file/file.py
@@ -330,7 +330,9 @@ class File(Document):
 
 	def set_file_name(self):
 		if not self.file_name and not self.file_url:
-			frappe.throw(_("Fields `file_name` or `file_url` must be set for File"), exc=frappe.MandatoryError)
+			frappe.throw(
+				_("Fields `file_name` or `file_url` must be set for File"), exc=frappe.MandatoryError
+			)
 		elif not self.file_name and self.file_url:
 			self.file_name = self.file_url.split("/")[-1]
 		else:
