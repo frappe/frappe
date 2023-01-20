f604796384cff0eb0695b5295e58228f8aea64f7
anandbaburajan <anandbaburajan@gmail.com>
2023-01-20 22:07:36 +0530

    fix: show the invalid naming series in special chars error msg

--- frappe/model/naming.py
+++ frappe/model/naming.py
@@ -59,8 +59,8 @@ class NamingSeries:
 		if not NAMING_SERIES_PATTERN.match(self.series):
 			frappe.throw(
 				_(
-					'Special Characters except "-", "#", ".", "/", "{" and "}" not allowed in naming series',
-				),
+					"Invalid naming series {}: special characters except '-', '#', '.', '/', '{{' and '}}' not allowed"
+				).format(frappe.bold(self.series)),
 				exc=InvalidNamingSeriesError,
 			)
 
