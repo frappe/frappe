d2681475e8e1884e4d8225ab083e72a0c0e017d6
Shariq Ansari <30859809+shariquerik@users.noreply.github.com>
2023-01-20 18:08:43 +0530

    chore: resolved conflict

--- frappe/public/js/frappe/form/grid.js
+++ frappe/public/js/frappe/form/grid.js
@@ -61,9 +61,9 @@ export default class Grid {
 
 	make() {
 		let template = `
-<<<<<<< HEAD
 			<div class="grid-field">
 				<label class="control-label">${__(this.df.label || "")}</label>
+				<span class="ml-1 help"></span>
 				<p class="text-muted small grid-description"></p>
 				<div class="grid-custom-buttons"></div>
 				<div class="form-grid-container">
@@ -79,24 +79,6 @@ export default class Grid {
 								>
 								${__("No Data")}
 							</div>
-=======
-			<label class="control-label">${__(this.df.label || "")}</label>
-			<span class="ml-1 help"></span>
-			<p class="text-muted small grid-description"></p>
-			<div class="grid-custom-buttons grid-field"></div>
-			<div class="form-grid-container">
-				<div class="form-grid">
-					<div class="grid-heading-row"></div>
-					<div class="grid-body">
-						<div class="rows"></div>
-						<div class="grid-empty text-center">
-							<img
-								src="/assets/frappe/images/ui-states/grid-empty-state.svg"
-								alt="Grid Empty State"
-								class="grid-empty-illustration"
-							>
-							${__("No Data")}
->>>>>>> e78c74cbc1 (feat: inline doc link for each field)
 						</div>
 					</div>
 				</div>
