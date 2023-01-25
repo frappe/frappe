27cbebec126f7735e4931edb4285483b668c9d33
Sagar Vora <sagar@resilient.tech>
2023-01-25 13:41:50 +0530

    chore: remove unsupported type hint

--- frappe/core/doctype/installed_applications/installed_applications.py
+++ frappe/core/doctype/installed_applications/installed_applications.py
@@ -43,7 +43,6 @@ def update_installed_apps_order(new_order):
 	if isinstance(new_order, str):
 		new_order = json.loads(new_order)
 
-	frappe.local.request_cache and frappe.local.request_cache.clear()
 	existing_order = frappe.get_installed_apps(_ensure_on_bench=True)
 
 	if set(existing_order) != set(new_order) or not isinstance(new_order, list):
@@ -72,7 +71,7 @@ def _create_version_log_for_change(old, new):
 
 
 @frappe.whitelist()
-def get_installed_app_order() -> list[str]:
+def get_installed_app_order():
 	frappe.only_for("System Manager")
 
 	return frappe.get_installed_apps(_ensure_on_bench=True)
