23ab84f6acaf3c492b4092519d42f4884d4d1bbf
Türker Tunalı <turker.tunali@gmail.com>
2023-01-30 07:08:35 +0300

    fix(i18n): Datepicker Turkish translations (#19777)
    * feat:Datepicker Turkish translations are added.
    
    * chore: format
    
    [skip ci]
    
    ---------
    
    (cherry picked from commit e18188ed3ed2a71997e756bf79999092762948e7)

--- frappe/public/js/frappe/form/controls/datepicker_i18n.js
+++ frappe/public/js/frappe/form/controls/datepicker_i18n.js
@@ -60,3 +60,44 @@ import "air-datepicker/dist/js/i18n/datepicker.zh.js";
 		firstDay: 1
 	};
 })(jQuery);
+
+(function ($) {
+	$.fn.datepicker.language["tr"] = {
+		days: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+		daysShort: ["Paz", "Pzt", "Sal", "Çar", "Per", "Cum", "Cmt"],
+		daysMin: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+		months: [
+			"Ocak",
+			"Şubat",
+			"Mart",
+			"Nisan",
+			"Mayıs",
+			"Haziran",
+			"Temmuz",
+			"Ağustos",
+			"Eylül",
+			"Ekim",
+			"Kasım",
+			"Aralık",
+		],
+		monthsShort: [
+			"Oca",
+			"Şub",
+			"Mar",
+			"Nis",
+			"May",
+			"Haz",
+			"Tem",
+			"Ağu",
+			"Eyl",
+			"Eki",
+			"Kas",
+			"Ara",
+		],
+		today: "Bugün",
+		clear: "Temizle",
+		dateFormat: "dd.mm.yyyy",
+		timeFormat: "hh:ii",
+		firstDay: 1,
+	};
+})(jQuery);
