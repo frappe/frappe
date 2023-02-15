bf1d0b6e835ddeadd007048f106c26a9125f9d8c
Ankush Menat <ankush@frappe.io>
2023-02-15 19:21:42 +0530

    fix: thread-unsafe shared class attribute on QB engine (#20052)
    `Engine.tables` when used with `gthreaded` workers causes race condition.
    
    E.g.
    
    Request 1: Generating query on User doctype and puts it in tables.
    Request 2: Generating query on Batch doctype and puts it in tables.
    
    Both if on same worker are modifying same class's attribute and hence
    generated query will have implicit join in it.
    
    (cherry picked from commit b1c94b317f9b371ac4811b37c3d2480165470bf9)

--- frappe/database/query.py
+++ frappe/database/query.py
@@ -183,7 +183,8 @@ OPERATOR_MAP: dict[str, Callable] = {
 
 
 class Engine:
-	tables: dict[str, str] = {}
+	def __init__(self):
+		self.tables: dict[str, str] = {}
 
 	@cached_property
 	def OPERATOR_MAP(self):
