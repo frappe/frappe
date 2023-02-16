ff077e9c393cbcaf45935e6982ebc4163179dc6a
Ejaaz Khan <67804911+iamejaaz@users.noreply.github.com>
2023-02-16 12:32:37 +0530

    fix: unable to upload image in responsive mode (#19963)
    Co-authored-by: Shariq Ansari <sharique.rik@gmail.com>
    (cherry picked from commit ce6e86b48427c62c482c0765f2837583affd8c16)
    
    # Conflicts:
    #	frappe/public/js/frappe/form/sidebar/user_image.js
    #	frappe/public/js/frappe/ui/page.js

--- frappe/public/js/frappe/form/sidebar/user_image.js
+++ frappe/public/js/frappe/form/sidebar/user_image.js
@@ -78,12 +78,36 @@ frappe.ui.form.setup_user_image_event = function(frm) {
 	}
 
 	// bind click on image_wrapper
+<<<<<<< HEAD
 	frm.sidebar.image_wrapper.on('click', '.sidebar-image-change, .sidebar-image-remove', function(e) {
 		let $target = $(e.currentTarget);
 		var field = frm.get_field(frm.meta.image_field);
 		if ($target.is('.sidebar-image-change')) {
 			if(!field.$input) {
 				field.make_input();
+=======
+	frm.sidebar.image_wrapper.on(
+		"click",
+		".sidebar-image-change, .sidebar-image-remove",
+		function (e) {
+			let $target = $(e.currentTarget);
+			var field = frm.get_field(frm.meta.image_field);
+			if ($target.is(".sidebar-image-change")) {
+				if (!field.$input) {
+					field.make_input();
+				}
+				field.$input.trigger("attach_doc_image");
+				// close sidebar
+				frm.page.close_sidebar();
+			} else {
+				/// on remove event for a sidebar image wrapper remove attach file.
+				frm.attachments.remove_attachment_by_filename(
+					frm.doc[frm.meta.image_field],
+					function () {
+						field.set_value("").then(() => frm.save());
+					}
+				);
+>>>>>>> ce6e86b484 (fix: unable to upload image in responsive mode (#19963))
 			}
 			field.$input.trigger('click');
 		} else {
