53dc11c592f8ba771f8175d97a1267e26570528a
Gavin D'souza <gavin18d@gmail.com>
2023-02-17 11:24:01 +0530

    fix(after_hook): Don't pass exception object to hook
    * it can fetch most relevant details via response object
    * Exceptions not supported by Frappe's WSGI (unsupported HTTP methods) may not be accessible to the after_request hooks - but the lack of active response may be an indicator / and peeking in the request
    
    (cherry picked from commit 5d0bd512e10bd3b81c241c0f19d851ec5f62aca1)

--- frappe/app.py
+++ frappe/app.py
@@ -48,7 +48,6 @@ class RequestContext:
 @Request.application
 def application(request: Request):
 	response = None
-	e = None
 
 	try:
 		rollback = True
@@ -92,7 +91,7 @@ def application(request: Request):
 			frappe.db.rollback()
 
 		for after_request_task in frappe.get_hooks("after_request"):
-			frappe.call(after_request_task, response=response, request=request, exception=e)
+			frappe.call(after_request_task, response=response, request=request)
 
 		log_request(request, response)
 		process_response(response)
