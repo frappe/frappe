3406b6d7527be04dc170d6fd0572ef786a5be144
gavin <gavin18d@gmail.com>
2023-02-22 22:14:15 +0530

    fix(db_query): selecting linked fields as alias (#20085)
    * fix(db_query): Don't track link_tables separately
    
    Treat all joined tables the same, expand the fieldnames whether Table or
    Link type with their respective table names
    
    * fix(db_query): Add link join conditions for all relevant fields
    
    * Revert "fix(db_query): Add link join conditions for all relevant fields"
    
    This reverts commit 79622ab4cea5aa73a24f4ba7afde8e83510a79fb.
    
    * Revert "fix(db_query): Don't track link_tables separately"
    
    This reverts commit b8364f781e52e7ffaa7faa8878ef409b023f2288.
    
    * fix: Check link field tables permissions in permlevel checks
    
    * test: Fix, add for test_permlevel_fields
    
    Previous assertion was wrong :')
    added extra to check if access is fine

--- frappe/model/db_query.py
+++ frappe/model/db_query.py
@@ -75,6 +75,10 @@ class DatabaseQuery:
 			self._doctype_meta = frappe.get_meta(self.doctype)
 		return self._doctype_meta
 
+	@property
+	def query_tables(self):
+		return self.tables + [d.table_name for d in self.link_tables]
+
 	def execute(
 		self,
 		fields=None,
@@ -473,9 +477,7 @@ class DatabaseQuery:
 					table_name = table_name[13:]
 				if not table_name[0] == "`":
 					table_name = f"`{table_name}`"
-				if table_name not in self.tables and table_name not in (
-					d.table_name for d in self.link_tables
-				):
+				if table_name not in self.query_tables:
 					self.append_table(table_name)
 
 	def append_table(self, table_name):
@@ -641,7 +643,7 @@ class DatabaseQuery:
 				table, column = column.split(".", 1)
 				ch_doctype = table.replace("`", "").replace("tab", "", 1)
 
-				if wrap_grave_quotes(table) in self.tables:
+				if wrap_grave_quotes(table) in self.query_tables:
 					permitted_child_table_fields = get_permitted_fields(
 						doctype=ch_doctype, parenttype=self.doctype
 					)
