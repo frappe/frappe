87805031915bcd77bd75375ade260b65dad3d647
Shariq Ansari <sharique.rik@gmail.com>
2023-02-28 10:49:17 +0530

    chore: code reformatting

--- frappe/desk/desktop.py
+++ frappe/desk/desktop.py
@@ -157,12 +157,12 @@ class Workspace:
 		return False
 
 	def build_workspace(self):
-		self.number_cards = {"items": self.get_number_cards()}
 		self.cards = {"items": self.get_links()}
 		self.charts = {"items": self.get_charts()}
 		self.shortcuts = {"items": self.get_shortcuts()}
 		self.onboardings = {"items": self.get_onboardings()}
 		self.quick_lists = {"items": self.get_quick_lists()}
+		self.number_cards = {"items": self.get_number_cards()}
 
 	def _doctype_contains_a_record(self, name):
 		exists = self.table_counts.get(name, False)
@@ -200,22 +200,6 @@ class Workspace:
 		item["label"] = _(item.label) if item.label else _(item.name)
 
 		return item
-
-	@handle_not_exist
-	def get_number_cards(self):
-		all_number_cards = []
-		if frappe.has_permission("Number Card", throw=False):
-			number_cards = self.doc.number_cards
-			for number_card in number_cards:
-				if frappe.has_permission("Number Card", doc=number_card.number_card_name):
-					# Translate label
-					number_card.label = (
-						_(number_card.label) if number_card.label else _(number_card.number_card_name)
-					)
-
-					all_number_cards.append(number_card)
-
-		return all_number_cards
 
 	@handle_not_exist
 	def get_links(self):
@@ -345,6 +329,21 @@ class Workspace:
 
 		return steps
 
+	@handle_not_exist
+	def get_number_cards(self):
+		all_number_cards = []
+		if frappe.has_permission("Number Card", throw=False):
+			number_cards = self.doc.number_cards
+			for number_card in number_cards:
+				if frappe.has_permission("Number Card", doc=number_card.number_card_name):
+					# Translate label
+					number_card.label = (
+						_(number_card.label) if number_card.label else _(number_card.number_card_name)
+					)
+					all_number_cards.append(number_card)
+
+		return all_number_cards
+
 
 @frappe.whitelist()
 @frappe.read_only()
@@ -362,12 +361,12 @@ def get_desktop_page(page):
 		workspace = Workspace(loads(page))
 		workspace.build_workspace()
 		return {
-			"number_cards": workspace.number_cards,
 			"charts": workspace.charts,
 			"shortcuts": workspace.shortcuts,
 			"cards": workspace.cards,
 			"onboardings": workspace.onboardings,
 			"quick_lists": workspace.quick_lists,
+			"number_cards": workspace.number_cards,
 		}
 	except DoesNotExistError:
 		frappe.log_error("Workspace Missing")
@@ -490,16 +489,16 @@ def save_new_widget(doc, page, blocks, new_widgets):
 	if loads(new_widgets):
 		widgets = _dict(loads(new_widgets))
 
-		if widgets.number_card:
-			doc.number_cards.extend(
-				new_widget(widgets.number_card, "Workspace Number Card", "number_cards")
-			)
 		if widgets.chart:
 			doc.charts.extend(new_widget(widgets.chart, "Workspace Chart", "charts"))
 		if widgets.shortcut:
 			doc.shortcuts.extend(new_widget(widgets.shortcut, "Workspace Shortcut", "shortcuts"))
 		if widgets.quick_list:
 			doc.quick_lists.extend(new_widget(widgets.quick_list, "Workspace Quick List", "quick_lists"))
+		if widgets.number_card:
+			doc.number_cards.extend(
+				new_widget(widgets.number_card, "Workspace Number Card", "number_cards")
+			)
 		if widgets.card:
 			doc.build_links_table_from_card(widgets.card)
 
@@ -529,7 +528,7 @@ def save_new_widget(doc, page, blocks, new_widgets):
 def clean_up(original_page, blocks):
 	page_widgets = {}
 
-	for wid in ["number_card", "shortcut", "card", "chart", "quick_list"]:
+	for wid in ["shortcut", "card", "chart", "quick_list", "number_card"]:
 		# get list of widget's name from blocks
 		page_widgets[wid] = [x["data"][wid + "_name"] for x in loads(blocks) if x["type"] == wid]
 
