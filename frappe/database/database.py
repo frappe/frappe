4ec874ac6ee2aae0d80e044e49972a3140185490
Ankush Menat <ankush@frappe.io>
2023-03-06 11:05:18 +0530

    style: format

--- frappe/database/database.py
+++ frappe/database/database.py
@@ -994,7 +994,7 @@ class Database:
 				if hasattr(obj, "on_rollback"):
 					obj.on_rollback()
 			frappe.local.rollback_observers = []
-		
+
 			frappe.local.realtime_log = []
 			frappe.flags.enqueue_after_commit = []
 
