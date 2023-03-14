e547f6e88aef2956acefdaa8752d35efce92ddf6
marination <maricadsouza221197@gmail.com>
2023-03-14 19:59:35 +0530

    feat: Settings checkbox must toggle share permission globally
    - Treat feature like a perm toggler. Essentially noone is allowed to explicity share anything
    - Implicit sharing via `ignore_share_permissions` is allowed. Devs can decide where sharing should happen under the hood
    - UI is made read only and not hidden. Users must see who doc is already shared with
    - Make sure perm APIs used by share feature return false if sharing is disabled
    - Rename checkbox to `Disable Document Sharing`

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -246,8 +246,7 @@ class User(Document):
 		)
 
 	def share_with_self(self):
-		sharing_disabled = frappe.get_system_settings("disable_sharing")
-		if sharing_disabled or (self.name in STANDARD_USERS):
+		if self.name in STANDARD_USERS:
 			return
 
 		frappe.share.add_docshare(
--- frappe/public/js/frappe/form/sidebar/share.js
+++ frappe/public/js/frappe/form/sidebar/share.js
@@ -5,17 +5,11 @@ frappe.ui.form.Share = class Share {
 	constructor(opts) {
 		$.extend(this, opts);
 		this.shares = this.parent.find(".shares");
-		this.sharing_disabled = cint(frappe.sys_defaults.disable_sharing);
 	}
 	refresh() {
 		this.render_sidebar();
 	}
 	render_sidebar() {
-		if (this.sharing_disabled) {
-			this.parent.hide();
-			return;
-		}
-
 		const shared = this.shared || this.frm.get_docinfo().shared;
 		const shared_users = shared.filter(Boolean).map((s) => s.user);
 
@@ -91,7 +85,7 @@ frappe.ui.form.Share = class Share {
 			})
 		).appendTo(d.body);
 
-		if (frappe.model.can_share(null, this.frm) && !this.sharing_disabled) {
+		if (frappe.model.can_share(null, this.frm)) {
 			this.make_user_input();
 			this.add_share_button();
 			this.set_edit_share_events();
