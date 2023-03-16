87993001f8928c4b725ef32733b2fed33851fd32
Shariq Ansari <sharique.rik@gmail.com>
2023-03-16 17:16:32 +0530

    fix: allow 5 column layout in doctype form
    (cherry picked from commit d7ab46f7f815ec70cb4359e487d4102bc3c41cec)
    
    # Conflicts:
    #	frappe/public/js/frappe/form/column.js

---
+++ frappe/public/js/frappe/form/column.js
@@ -0,0 +1,60 @@
+export default class Column {
+	constructor(section, df) {
+		if (!df) df = {};
+
+		this.df = df;
+		this.section = section;
+		this.section.columns.push(this);
+		this.make();
+		this.resize_all_columns();
+	}
+
+	make() {
+		this.wrapper = $(`
+			<div class="form-column" data-fieldname="${this.df.fieldname}">
+				<form>
+				</form>
+			</div>
+		`).appendTo(this.section.body);
+
+		this.form = this.wrapper.find("form").on("submit", () => false);
+
+		if (this.df.description) {
+			$(`
+				<p class="col-sm-12 form-column-description">
+					${__(this.df.description)}
+				</p>
+			`).prependTo(this.wrapper);
+		}
+
+		if (this.df.label) {
+			$(`
+				<label class="column-label">
+					${__(this.df.label)}
+				</label>
+			`).prependTo(this.wrapper);
+		}
+	}
+
+	resize_all_columns() {
+		// distribute all columns equally
+		let columns = this.section.wrapper.find(".form-column").length;
+		let colspan = cint(12 / columns);
+
+		if (columns == 5) {
+			colspan = 20;
+		}
+
+		this.section.wrapper
+			.find(".form-column")
+			.removeClass()
+			.addClass("form-column")
+			.addClass("col-sm-" + colspan);
+	}
+
+	add_field() {}
+
+	refresh() {
+		this.section.refresh();
+	}
+}
