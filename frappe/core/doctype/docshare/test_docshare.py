758873c9c7802d25fb6c01a7a5c714e06799d5e8
Marica <maricadsouza221197@gmail.com>
2023-03-29 14:12:19 +0530

    feat: Disable Sharing globally (#20318) [backport] (#20500)
    * feat: Disable Sharing globally (#20318)
    
    * feat: Disable Sharing globally
    
    - Checkbox in System Settings
    - If disabled, avoid share UI render
    - Share APIs return None (non-obstructing) if share APIs are invoked
    
    * feat: Settings checkbox must toggle share permission globally
    
    - Treat feature like a perm toggler. Essentially noone is allowed to explicity share anything
    - Implicit sharing via `ignore_share_permissions` is allowed. Devs can decide where sharing should happen under the hood
    - UI is made read only and not hidden. Users must see who doc is already shared with
    - Make sure perm APIs used by share feature return false if sharing is disabled
    - Rename checkbox to `Disable Document Sharing`
    
    * test: (server side) Impact of disabling sharing on APIs
    
    - Also, fix missed system setting rename in `assign_to`
    
    * fix: Inform assigner if assignee lacks perms and sharing is disabled
    
    - misc: readable conditions
    
    * fix: throw instead of msgprint
    
    * fix: Typo and appropriate message for `throw`
    
    ---------
    
    Co-authored-by: Ankush Menat <ankush@frappe.io>
    
    * test: fix test case to modified behaviour
    
    We throw instead of showing warning now
    
    * style: format
    
    [skip ci]
    
    ---------
    
    Co-authored-by: Ankush Menat <ankush@frappe.io>

--- frappe/core/doctype/docshare/test_docshare.py
+++ frappe/core/doctype/docshare/test_docshare.py
@@ -8,6 +8,7 @@ import unittest
 import frappe
 import frappe.share
 from frappe.automation.doctype.auto_repeat.test_auto_repeat import create_submittable_doctype
+from frappe.tests.utils import change_settings
 
 test_dependencies = ["User"]
 
@@ -128,3 +129,61 @@ class TestDocShare(unittest.TestCase):
 		)
 
 		frappe.share.remove(doctype, submittable_doc.name, self.user)
+
+	@change_settings("System Settings", {"disable_document_sharing": 1})
+	def test_share_disabled_add(self):
+		"Test if user loses share access on disabling share globally."
+		frappe.share.add("Event", self.event.name, self.user, share=1)  # Share as admin
+		frappe.set_user(self.user)
+
+		# User does not have share access although given to them
+		self.assertFalse(self.event.has_permission("share"))
+		self.assertRaises(
+			frappe.PermissionError, frappe.share.add, "Event", self.event.name, "test1@example.com"
+		)
+
+	@change_settings("System Settings", {"disable_document_sharing": 1})
+	def test_share_disabled_add_with_ignore_permissions(self):
+		frappe.share.add("Event", self.event.name, self.user, share=1)
+		frappe.set_user(self.user)
+
+		# User does not have share access although given to them
+		self.assertFalse(self.event.has_permission("share"))
+
+		# Test if behaviour is consistent for developer overrides
+		frappe.share.add(
+			"Event", self.event.name, "test1@example.com", flags={"ignore_share_permission": True}
+		)
+
+	@change_settings("System Settings", {"disable_document_sharing": 1})
+	def test_share_disabled_set_permission(self):
+		frappe.share.add("Event", self.event.name, self.user, share=1)
+		frappe.set_user(self.user)
+
+		# User does not have share access although given to them
+		self.assertFalse(self.event.has_permission("share"))
+		self.assertRaises(
+			frappe.PermissionError,
+			frappe.share.set_permission,
+			"Event",
+			self.event.name,
+			"test1@example.com",
+			"read",
+		)
+
+	@change_settings("System Settings", {"disable_document_sharing": 1})
+	def test_share_disabled_assign_to(self):
+		"""
+		Assigning a document to a user without access must not share the document,
+		if sharing disabled.
+		"""
+		from frappe.desk.form.assign_to import add
+
+		frappe.share.add("Event", self.event.name, self.user, share=1)
+		frappe.set_user(self.user)
+
+		self.assertRaises(
+			frappe.ValidationError,
+			add,
+			{"doctype": "Event", "name": self.event.name, "assign_to": ["test1@example.com"]},
+		)
