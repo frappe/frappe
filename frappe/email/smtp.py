9e104138d90fee9799f682e22d5bc6161f2901c4
Himanshu Shivhare <himanshushivhare047@gmail.com>
2023-03-30 12:39:52 +0530

    Update smtp.py
    Settings > Email Account path updated.

--- frappe/email/smtp.py
+++ frappe/email/smtp.py
@@ -70,7 +70,7 @@ class SMTPServer:
 		if not self.server:
 			frappe.msgprint(
 				_(
-					"Email Account not setup. Please create a new Email Account from Setup > Email > Email Account"
+					"Email Account not setup. Please create a new Email Account from Settings > Email Account"
 				),
 				raise_exception=frappe.OutgoingEmailError,
 			)
