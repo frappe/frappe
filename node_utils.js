77dd051d1f618d9666f4b0c41b84c134b5e4a336
David Arnold <david.arnold@iohk.io>
2023-04-01 14:17:49 -0500

    wip: allow to use unix socket to redis servers in js

--- node_utils.js
+++ node_utils.js
@@ -40,8 +40,20 @@ function get_conf() {
 
 function get_redis_subscriber(kind = "redis_socketio", options = {}) {
 	const conf = get_conf();
-	const host = conf[kind] || conf.redis_async_broker_port;
-	return redis.createClient({ url: host, ...options });
+	let redisSock = {
+	    redis_cache:
+	    	process.env.FRAPPE_REDIS_CACHE_SOCKET || conf[kind] || conf.redis_async_broker_port,
+	    redis_queue:
+	    	process.env.FRAPPE_REDIS_QUEUE_SOCKET || conf[kind] || conf.redis_async_broker_port,
+	    redis_socketio:
+	    	process.env.FRAPPE_REDIS_SOCKETIO_SOCKET || conf[kind] || conf.redis_async_broker_port,
+	};
+	const sockStr = redisSock[kind];
+	if socketStr.startsWith("/") {
+		return redis.createClient({ socket: { path: sockStr }, ...options });
+	} else {
+		return redis.createClient({ url: sockStr, ...options });
+	}
 }
 
 module.exports = {
