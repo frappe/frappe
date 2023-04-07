963acc47f1f2a37bd6cb7842c137f448287be2e5
David Arnold <david.arnold@iohk.io>
2023-04-06 20:47:20 -0500

    chore: give cli invocations of dbtools a dedicated interface
    This PR removes technical debt. It does not add any features in itself.
    
    By capturing the multiple ways in which python code interfaces with the
    database CLI tooling and consolidating the command construction in a
    single place / function, this PR increases confidence of for future
    refactorings and interface amendments
    
    While this commit does not make such amendments, a follow up PR may,
    for example, add full unix socket support to the db connections.

--- frappe/database/__init__.py
+++ frappe/database/__init__.py
@@ -3,6 +3,8 @@
 
 # Database Module
 # --------------------
+import os
+from shutil import which
 
 from frappe.database.database import savepoint
 
@@ -52,3 +54,75 @@ def get_db(host=None, user=None, password=None, port=None, dbname=None):
 		import frappe.database.mariadb.database
 
 		return frappe.database.mariadb.database.MariaDBDatabase(host, user, password, port, dbname)
+
+
+def get_command(
+	host=None, port=None, user=None, password=None, db_name=None, extra=None, dump=False
+):
+	import frappe
+
+	if frappe.conf.db_type == "postgres":
+		if dump:
+			bin, bin_name = which("pg_dump"), "pg_dump"
+		else:
+			bin, bin_name = which("psql"), "psql"
+
+		host = frappe.utils.esc(host, "$ ")
+		user = frappe.utils.esc(user, "$ ")
+		db_name = frappe.utils.esc(db_name, "$ ")
+
+		conn_string = str
+		if password:
+			password = frappe.utils.esc(password, "$ ")
+			conn_string = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+		else:
+			conn_string = f"postgresql://{user}@{host}:{port}/{db_name}"
+
+		command = [conn_string]
+
+		if extra:
+			command.extend(extra)
+
+	else:
+		if dump:
+			bin, bin_name = which("mysqldump"), "mysqldump"
+		else:
+			bin, bin_name = which("mysql"), "mysql"
+
+		host = frappe.utils.esc(host, "$ ")
+		user = frappe.utils.esc(user, "$ ")
+		db_name = frappe.utils.esc(db_name, "$ ")
+
+		command = [
+			f"--user={user}",
+			f"--host={host}",
+			f"--port={port}",
+		]
+
+		if password:
+			password = frappe.utils.esc(password, "$ ")
+			command.append(f"--password={password}")
+
+		if dump:
+			command.extend(
+				[
+					"--single-transaction",
+					"--quick",
+					"--lock-tables=false",
+				]
+			)
+		else:
+			command.extend(
+				[
+					"--pager='less -SFX'",
+					"--safe-updates",
+					"--no-auto-rehash",
+				]
+			)
+
+		command.append(db_name)
+
+		if extra:
+			command.extend(extra)
+
+	return bin, command, bin_name
