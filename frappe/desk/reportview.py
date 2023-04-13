2567c6655e61dfacdda2d1401396cbb55cce00fc
Shariq Ansari <sharique.rik@gmail.com>
2023-04-13 14:11:52 +0530

    fix: filter with capitalized operator was getting bypassed
    (cherry picked from commit 6b2b1618760dc39e4cb50ecfa48a4c25ef39564f)

--- frappe/desk/reportview.py
+++ frappe/desk/reportview.py
@@ -697,7 +697,7 @@ def get_filters_cond(
 			for f in filters:
 				if isinstance(f[1], str) and f[1][0] == "!":
 					flt.append([doctype, f[0], "!=", f[1][1:]])
-				elif isinstance(f[1], (list, tuple)) and f[1][0] in (
+				elif isinstance(f[1], (list, tuple)) and f[1][0].lower() in (
 					">",
 					"<",
 					">=",
