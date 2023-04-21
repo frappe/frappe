79b3c3af8d9b13789be24e860891c2b8f9e548f6
Shariq Ansari <30859809+shariquerik@users.noreply.github.com>
2023-04-21 14:43:32 +0530

    chore: moved import in order

--- frappe/www/login.py
+++ frappe/www/login.py
@@ -7,9 +7,9 @@ from frappe import _
 from frappe.auth import LoginManager
 from frappe.integrations.doctype.ldap_settings.ldap_settings import LDAPSettings
 from frappe.integrations.oauth2_logins import decoder_compat
-from frappe.utils.data import escape_html
 from frappe.rate_limiter import rate_limit
 from frappe.utils import cint, get_url
+from frappe.utils.data import escape_html
 from frappe.utils.html_utils import get_icon_html
 from frappe.utils.jinja import guess_is_path
 from frappe.utils.oauth import get_oauth2_authorize_url, get_oauth_keys, redirect_post_login
