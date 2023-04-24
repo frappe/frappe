32ad2303bd6f82d67c3ca8dcd10c6c758682d843
phot0n <ritwikpuri5678@gmail.com>
2023-04-24 15:45:33 +0530

    fix: add confirmation when updating counter and make document type mandatory
    (cherry picked from commit 71f45eba68e058d254c4e5c7cfe07b9b14352509)

--- frappe/core/doctype/document_naming_rule/document_naming_rule.js
+++ frappe/core/doctype/document_naming_rule/document_naming_rule.js
@@ -4,6 +4,24 @@
 frappe.ui.form.on("Document Naming Rule", {
 	refresh: function (frm) {
 		frm.trigger("document_type");
+		frm.last_counter_value = frm.doc.counter;
+		frm.skip_before_save = false;
+	},
+	before_save: function (frm) {
+		if (frm.is_new() || frm.skip_before_save || frm.last_counter_value === frm.doc.counter)
+			return;
+
+		frappe.validated = false;
+		frappe.warn(
+			__("Are you sure?"),
+			__("Updating counter may lead to document name conflicts if not done properly"),
+			() => {
+				frm.skip_before_save = true;
+				frm.save();
+			},
+			__("Proceed"),
+			false
+		);
 	},
 	document_type: (frm) => {
 		// update the select field options with fieldnames
