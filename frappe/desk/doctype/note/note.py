8e70e1cc8fcb0bf1bb36d3052d9b31d4d00b83e4
barredterra <14891507+barredterra@users.noreply.github.com>
2023-04-24 17:22:08 +0200

    fix: only System Manager can create public Notes

--- frappe/desk/doctype/note/note.py
+++ frappe/desk/doctype/note/note.py
@@ -1,22 +1,16 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: MIT. See LICENSE
 
-import re
-
 import frappe
 from frappe.model.document import Document
 
-NAME_PATTERN = re.compile("[%'\"#*?`]")
-
 
 class Note(Document):
-	def autoname(self):
-		# replace forbidden characters
-		self.name = NAME_PATTERN.sub("", self.title.strip())
+	def before_validate(self):
+		if not self.content:
+			self.content = "<span></span>"
 
-	def validate(self):
 		if self.notify_on_login and not self.expire_notification_on:
-
 			# expire this notification in a week (default)
 			self.expire_notification_on = frappe.utils.add_days(self.creation, 7)
 
@@ -24,30 +18,22 @@ class Note(Document):
 		self.print_heading = self.name
 		self.sub_heading = ""
 
+	def mark_seen_by(self, user: str) -> None:
+		if user in [d.user for d in self.seen_by]:
+			return
+
+		self.append("seen_by", {"user": user})
+
 
 @frappe.whitelist()
-def mark_as_seen(note):
-	note = frappe.get_doc("Note", note)
-	if frappe.session.user not in [d.user for d in note.seen_by]:
-		note.append("seen_by", {"user": frappe.session.user})
-		note.save(ignore_version=True, ignore_permissions=True)
+def mark_as_seen(note: str):
+	note: Note = frappe.get_doc("Note", note)
+	note.mark_seen_by(frappe.session.user)
+	note.save(ignore_permissions=True, ignore_version=True)
 
 
 def get_permission_query_conditions(user):
 	if not user:
 		user = frappe.session.user
 
-	if user == "Administrator":
-		return ""
-
-	return f"""(`tabNote`.public=1 or `tabNote`.owner={frappe.db.escape(user)})"""
-
-
-def has_permission(doc, ptype, user):
-	if doc.public == 1 or user == "Administrator":
-		return True
-
-	if user == doc.owner:
-		return True
-
-	return False
+	return f"(`tabNote`.public=1 or `tabNote`.owner={frappe.db.escape(user)})"
