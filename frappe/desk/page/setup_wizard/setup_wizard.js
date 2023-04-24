6edce521c72563775592c34d1ffbbe43667ce5d6
Ankush Menat <ankush@frappe.io>
2023-04-24 15:27:58 +0530

    feat: setup wizard progress telemetry
    (cherry picked from commit 6c3f29efe7d9321d46cc18958276635f445af64f)

--- frappe/desk/page/setup_wizard/setup_wizard.js
+++ frappe/desk/page/setup_wizard/setup_wizard.js
@@ -178,6 +178,7 @@ frappe.setup.SetupWizard = class SetupWizard extends frappe.ui.Slides {
 	}
 
 	action_on_complete() {
+		frappe.telemetry.log("completed", "setup");
 		if (!this.current_slide.set_values()) return;
 		this.update_values();
 		this.show_working_state();
@@ -325,6 +326,7 @@ frappe.setup.SetupWizardSlide = class SetupWizardSlide extends frappe.ui.Slide {
 	make() {
 		super.make();
 		this.set_init_values();
+		this.setup_telemetry_events();
 		this.reset_action_button_state();
 	}
 
@@ -339,6 +341,15 @@ frappe.setup.SetupWizardSlide = class SetupWizardSlide extends frappe.ui.Slide {
 				}
 			});
 		}
+	}
+
+	setup_telemetry_events() {
+		let me = this;
+		this.fields.filter(frappe.model.is_value_type).forEach((field) => {
+			me.get_input(field.fieldname).on("change", function () {
+				frappe.telemetry.log(`${field.fieldname}_set`, "setup");
+			});
+		});
 	}
 };
 
