cf53b1a58e9e88744b1781dac5624e534138c084
Ankush Menat <ankush@frappe.io>
2023-05-08 16:22:47 +0530

    chore: set_value docstring
    [skip ci]

--- frappe/database/database.py
+++ frappe/database/database.py
@@ -861,7 +861,7 @@ class Database:
 		**Warning:** this function will not call Document events and should be avoided in normal cases.
 
 		:param dt: DocType name.
-		:param dn: Document name.
+		:param dn: Document name for updating single record or filters for updating many records.
 		:param field: Property / field name or dictionary of values to be updated
 		:param value: Value to be updated.
 		:param modified: Use this as the `modified` timestamp.
