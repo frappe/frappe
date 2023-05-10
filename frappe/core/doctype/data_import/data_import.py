813308e048f51fdb93c813fbcc46cbb3f7dead6b
Ankush Menat <ankush@frappe.io>
2023-05-10 12:45:05 +0530

    refactor!: Use job_id for bg job deduplication (#20951)
    (cherry picked from commit ef6f4b59561fe53f29f96e3c559e49bcaee47506)
    
    # Conflicts:
    #	frappe/core/doctype/data_import/data_import.py

--- frappe/core/doctype/data_import/data_import.py
+++ frappe/core/doctype/data_import/data_import.py
@@ -9,7 +9,11 @@ from frappe.core.doctype.data_import.exporter import Exporter
 from frappe.core.doctype.data_import.importer import Importer
 from frappe.model.document import Document
 from frappe.modules.import_file import import_file_by_path
+<<<<<<< HEAD
 from frappe.utils.background_jobs import enqueue
+=======
+from frappe.utils.background_jobs import enqueue, is_job_enqueued
+>>>>>>> ef6f4b5956 (refactor!: Use job_id for bg job deduplication (#20951))
 from frappe.utils.csvutils import validate_google_sheets_url
 
 
@@ -65,15 +69,21 @@ class DataImport(Document):
 		if is_scheduler_inactive() and not frappe.flags.in_test:
 			frappe.throw(_("Scheduler is inactive. Cannot import data."), title=_("Scheduler Inactive"))
 
+<<<<<<< HEAD
 		enqueued_jobs = [d.get("job_name") for d in get_info()]
 
 		if self.name not in enqueued_jobs:
+=======
+		job_id = f"data_import::{self.name}"
+
+		if not is_job_enqueued(job_id):
+>>>>>>> ef6f4b5956 (refactor!: Use job_id for bg job deduplication (#20951))
 			enqueue(
 				start_import,
 				queue="default",
 				timeout=10000,
 				event="data_import",
-				job_name=self.name,
+				job_id=job_id,
 				data_import=self.name,
 				now=frappe.conf.developer_mode or frappe.flags.in_test,
 			)
