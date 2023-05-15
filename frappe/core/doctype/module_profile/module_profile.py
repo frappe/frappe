284b92c398bed61fd18989a3b61d0db91a079d02
Ankush Menat <ankush@frappe.io>
2023-05-15 16:40:27 +0530

    fix(UX): sort modules on user doctype
    [skip ci]

--- frappe/core/doctype/module_profile/module_profile.py
+++ frappe/core/doctype/module_profile/module_profile.py
@@ -8,4 +8,4 @@ class ModuleProfile(Document):
 	def onload(self):
 		from frappe.config import get_modules_from_all_apps
 
-		self.set_onload("all_modules", [m.get("module_name") for m in get_modules_from_all_apps()])
+		self.set_onload("all_modules", sorted(m.get("module_name") for m in get_modules_from_all_apps()))
