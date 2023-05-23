4953d5f22f1d6ca9c54c735dd4fd0df2eafc7d2f
phot0n <ritwikpuri5678@gmail.com>
2023-05-23 15:48:25 +0530

    refactor: simplify perm log controller js

--- frappe/core/doctype/permission_log/permission_log.js
+++ frappe/core/doctype/permission_log/permission_log.js
@@ -13,88 +13,63 @@ frappe.ui.form.on("Permission Log", {
 			<thead>
 				<tr>
 					<td>${__("Field")}</td>
-					<td>${__("Changed Value(s)")}</td>
+					<td>${__("From")}</td>
 					<td>${__("To")}</td>
 				</tr>
 			</thead>
-			<tbody></tbody>
+			<tbody class="main-body"></tbody>
 		</table>`);
 
 		Object.entries(changes["from"]).forEach(([key, value]) => {
 			if (Array.isArray(value || changes["to"][key])) {
-				let child_main = $(`<tr>
-					<td>${frappe.model.unscrub(key)}</td>
-					<td class="from"></td>
-					<td class="to"></td>
-				</tr>`);
-				let html = {
-					from: $(`<table class="table-bordered small">
-					<tbody></tbody>
-				</table>`),
-					to: $(`<table class="table-bordered small">
-					<tbody></tbody>
-				</table>`),
-				};
-
-				[value, changes["to"][key]].forEach((val, index) => {
-					let child_data = [];
-					let for_value = index > 0 ? "to" : "from";
-
-					val.forEach((k) => {
-						child_data.push([Object.keys(k), Object.values(k)]);
-					});
-					child_data.forEach((k) => {
-						if (k[0].length > 1) {
-							// for multiple child fields
-							let sub_child = $(`<tr>
-								<td class="field"></td>
-								<td class="value"></td>
-							</tr>`);
-
-							k[0].forEach((child, index) => {
-								sub_child
-									.find(".field")
-									.append(
-										$(
-											`<tr><td style="word-break: break-word">${frappe.model.unscrub(
-												child
-											)}</td></tr>`
-										)
-									);
-								sub_child
-									.find(".value")
-									.append(
-										$(
-											`<tr><td style="word-break: break-word">${k[1][index]}</td></tr>`
-										)
-									);
-							});
-							html[for_value].find("tbody").append(sub_child);
-						} else {
-							html[for_value].find("tbody").append(
-								$(`<tr>
-								<td style="word-break: break-word">${frappe.model.unscrub(k[0][0])}</td>
-								<td style="word-break: break-word">${k[1][0]}</td>
-							</tr>`)
-							);
-						}
-					});
-				});
-
-				child_main.find(".from").append(html["from"]);
-				child_main.find(".to").append(html["to"]);
-				changes_table.find("tbody").append(child_main);
+				changes_table
+					.find(".main-body")
+					.append(frm.events.get_child_changes(key, value, changes["to"][key]));
 			} else {
 				changes_table.find("tbody").append(
 					$(`<tr>
-				<td>${frappe.model.unscrub(key)}</td>
-				<td style="word-break: break-word">${changes["from"][key]}</td>
-				<td style="word-break: break-word">${changes["to"][key]}</td>
-			</tr>`)
+						<td>${frappe.model.unscrub(key)}</td>
+						<td style="word-break: break-word" class="diff-remove">${changes["from"][key]}</td>
+						<td style="word-break: break-word" class="diff-add">${changes["to"][key]}</td>
+					</tr>`)
 				);
 			}
 		});
 
 		wrapper.append(changes_table);
+	},
+
+	get_child_changes: function (field_key, from, to) {
+		let child_main = $(`<tr>
+			<td>${frappe.model.unscrub(field_key)}</td>
+			<td class="from"></td>
+			<td class="to"></td>
+		</tr>`);
+
+		[from, to].forEach((val, index) => {
+			if (!val) return;
+
+			let for_value = index > 0 ? "to" : "frfromom";
+			let child_table = $(`<table class="table-bordered small" style="margin-bottom: 5px">
+				<tbody></tbody>
+			</table>`);
+
+			val.forEach((child_row) => {
+				for (const [key, value] of Object.entries(child_row)) {
+					child_table.find("tbody").append(
+						$(
+							`<tr>
+							<td style="word-break: break-word">${frappe.model.unscrub(key)}</td>
+							<td style="word-break: break-word">${value}</td>
+						</tr>`
+						)
+					);
+				}
+			});
+
+			child_main.find(`.${for_value}`).append(child_table);
+		});
+
+		return child_main;
 	},
 });
