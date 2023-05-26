ac3fb737021110317e2118fca5d9705f5f256751
Shariq Ansari <sharique.rik@gmail.com>
2023-05-26 20:42:43 +0530

    fix: replaced add doctype button with select group button

---
+++ frappe/core/doctype/doctype/doctype_list.js
@@ -0,0 +1,25 @@
+frappe.listview_settings["DocType"] = {
+	onload: function (me) {
+		me.page.btn_primary.addClass("hidden");
+
+		let actions = [
+			{
+				label: __("Add DocType"),
+				description: __("Create a new DocType"),
+				action: () => frappe.new_doc("DocType"),
+			},
+			{
+				label: __("Add DocType (Form Builder)"),
+				description: __("Use the form builder to create a new DocType"),
+				action: () => frappe.set_route("form-builder", "new-doctype"),
+			},
+		];
+
+		frappe.utils.add_select_group_button(
+			me.page.btn_primary.parent(),
+			actions,
+			"btn-primary",
+			"add"
+		);
+	},
+};
