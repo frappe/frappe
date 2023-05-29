62565b51919b5493fb91b8bd7bb1b2a372d20394
Shariq Ansari <sharique.rik@gmail.com>
2023-05-29 15:57:58 +0530

    fix: made a util to create shadow element

--- frappe/desk/doctype/custom_html_block/custom_html_block.js
+++ frappe/desk/doctype/custom_html_block/custom_html_block.js
@@ -11,45 +11,5 @@ function render_custom_html_block(frm) {
 	let wrapper = frm.fields_dict["preview"].wrapper;
 	wrapper.classList.add("mb-3");
 
-	let random_id = "custom-block-" + frappe.utils.get_random(5).toLowerCase();
-
-	class CustomBlock extends HTMLElement {
-		constructor() {
-			super();
-
-			// html
-			let div = document.createElement("div");
-			div.innerHTML = frappe.dom.remove_script_and_style(frm.doc.html);
-
-			// link global desk css
-			let link = document.createElement("link");
-			link.rel = "stylesheet";
-			link.href = frappe.assets.bundled_asset("desk.bundle.css");
-
-			// css
-			let style = document.createElement("style");
-			style.textContent = frm.doc.style;
-
-			// javascript
-			let script = document.createElement("script");
-			script.textContent = `
-				(function() {
-					let cname = ${JSON.stringify(random_id)};
-					let root_element = document.querySelector(cname).shadowRoot;
-					${frm.doc.script}
-				})();
-			`;
-
-			this.attachShadow({ mode: "open" });
-			this.shadowRoot?.appendChild(link);
-			this.shadowRoot?.appendChild(div);
-			this.shadowRoot?.appendChild(style);
-			this.shadowRoot?.appendChild(script);
-		}
-	}
-
-	if (!customElements.get(random_id)) {
-		customElements.define(random_id, CustomBlock);
-	}
-	wrapper.innerHTML = `<${random_id}></${random_id}>`;
+	frappe.create_shadow_element(wrapper, frm.doc.html, frm.doc.style, frm.doc.script);
 }
