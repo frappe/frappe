b35e6ae32ba8f23e34ebbd78e374119488bcd66c
Shariq Ansari <sharique.rik@gmail.com>
2023-05-29 16:21:00 +0530

    refactor: rearranged code
    (cherry picked from commit d00e41d725ded5ec8bbb019676c73c91e28c8ba8)

--- frappe/desk/doctype/custom_html_block/custom_html_block.js
+++ frappe/desk/doctype/custom_html_block/custom_html_block.js
@@ -3,13 +3,9 @@
 
 frappe.ui.form.on("Custom HTML Block", {
 	refresh(frm) {
-		render_custom_html_block(frm);
+		let wrapper = frm.fields_dict["preview"].wrapper;
+		wrapper.classList.add("mb-3");
+
+		frappe.create_shadow_element(wrapper, frm.doc.html, frm.doc.style, frm.doc.script);
 	},
 });
-
-function render_custom_html_block(frm) {
-	let wrapper = frm.fields_dict["preview"].wrapper;
-	wrapper.classList.add("mb-3");
-
-	frappe.create_shadow_element(wrapper, frm.doc.html, frm.doc.style, frm.doc.script);
-}
