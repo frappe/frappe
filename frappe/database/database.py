691cbd6da74d6a17eec62eee42d35970935b0f0a
Ankush Menat <ankush@frappe.io>
2023-06-02 16:39:35 +0530

    chore: remove select from ddl prefix

--- frappe/database/database.py
+++ frappe/database/database.py
@@ -312,7 +312,7 @@ class Database:
 			frappe.log(f"<<<< query\n{_query}\n>>>>")
 
 		if unmogrified_query and is_query_type(
-			unmogrified_query, ("alter", "drop", "select", "create", "truncate", "rename")
+			unmogrified_query, ("alter", "drop", "create", "truncate", "rename")
 		):
 			_query = _query or str(mogrified_query)
 			self.logger.warning("DDL Query made to DB:\n" + _query)
