bf76f7df09b288c7717c6d2ca6b3b8640644cd6f
Ankush Menat <ankush@frappe.io>
2023-06-05 11:56:49 +0530

    fix: allow 0 as default precision

--- frappe/public/js/frappe/form/formatters.js
+++ frappe/public/js/frappe/form/formatters.js
@@ -103,8 +103,9 @@ frappe.form.formatters = {
 	},
 	Currency: function (value, docfield, options, doc) {
 		var currency = frappe.meta.get_field_currency(docfield, doc);
-		var precision =
-			docfield.precision || cint(frappe.boot.sysdefaults.currency_precision) || 2;
+		var precision = cint(
+			docfield.precision ?? frappe.boot.sysdefaults.currency_precision ?? 2
+		);
 
 		// If you change anything below, it's going to hurt a company in UAE, a bit.
 		if (precision > 2) {
