a429739d73a0b6d2cf610c4b95633c7230089389
David Arnold <david.arnold@iohk.io>
2023-06-09 10:55:54 -0500

    feat: prepare redis subsystem for unix domain socket connections

--- frappe/utils/connections.py
+++ frappe/utils/connections.py
@@ -6,11 +6,16 @@ from frappe import get_conf
 REDIS_KEYS = ("redis_cache", "redis_queue")
 
 
-def is_open(ip, port, timeout=10):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def is_open(scheme, hostname, port, path, timeout=10):
+	if scheme == "unix":
+		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+		conn = path
+	else:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		conn = (hostname, port)
 	s.settimeout(timeout)
 	try:
-		s.connect((ip, int(port)))
+		s.connect(conn)
 		s.shutdown(socket.SHUT_RDWR)
 		return True
 	except OSError:
@@ -24,17 +29,16 @@ def check_database():
 	db_type = config.get("db_type", "mariadb")
 	db_host = config.get("db_host", "localhost")
 	db_port = config.get("db_port", 3306 if db_type == "mariadb" else 5432)
-	return {db_type: is_open(db_host, db_port)}
+	return {db_type: is_open("other", db_host, db_port, None)}
 
 
 def check_redis(redis_services=None):
 	config = get_conf()
 	services = redis_services or REDIS_KEYS
 	status = {}
-	for conn in services:
-		redis_url = urlparse(config.get(conn)).netloc
-		redis_host, redis_port = redis_url.split(":")
-		status[conn] = is_open(redis_host, redis_port)
+	for srv in services:
+		url = urlparse(config[srv])
+		status[srv] = is_open(url.scheme, url.hostname, url.port, url.path)
 	return status
 
 
