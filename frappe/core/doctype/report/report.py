73d6a5b9c96073a504aedc9969d9fdee9a2493e1
Ankush Menat <ankush@frappe.io>
2023-06-15 15:56:51 +0530

    fix: reduce prepared report threshold

--- frappe/core/doctype/report/report.py
+++ frappe/core/doctype/report/report.py
@@ -122,7 +122,7 @@ class Report(Document):
 	def execute_script_report(self, filters):
 		# save the timestamp to automatically set to prepared
 		# TODO: Do this via signal handler
-		threshold = 30
+		threshold = 15
 		res = []
 
 		start_time = time.monotonic()
