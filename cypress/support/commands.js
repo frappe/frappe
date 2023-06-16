821f75c3ad43fab3e79cfed6c46b8d60add74bc6
Suraj Shetty <surajshetty3416@gmail.com>
2023-06-16 12:47:39 +0530

    fix: Try waiting for logout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -373,6 +373,7 @@ Cypress.Commands.add("update_doc", (doctype, docname, args) => {
 
 Cypress.Commands.add("switch_to_user", (user) => {
 	cy.call("logout");
+	cy.wait(200);
 	cy.login(user);
 	cy.reload();
 });
