14a21989c6767520d8e1b9d46cc9f58a45768563
David Arnold <david.arnold@iohk.io>
2023-07-03 14:22:51 -0500

    fix: gently log the port on which the realtime service listens
    without this, the service remains completely silent (e.g. in overmind) and it is hard to tell if it is even running

--- realtime/index.js
+++ realtime/index.js
@@ -1,9 +1,13 @@
 const { Server } = require("socket.io");
+const http = require("node:http");
 
 const { get_conf, get_redis_subscriber } = require("../node_utils");
 const conf = get_conf();
 
-let io = new Server(conf.socketio_port, {
+const server = http.createServer();
+const port = conf.socketio_port;
+
+let io = new Server(server, {
 	cors: {
 		// Should be fine since we are ensuring whether hostname and origin are same before adding setting listeners for s socket
 		origin: true,
@@ -52,3 +56,7 @@ subscriber.on("message", function (_channel, message) {
 
 subscriber.subscribe("events");
 // =======================
+
+server.listen(port, () => {
+	console.log("Realtime service listening on: ", port);
+});
