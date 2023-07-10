3d21b540035e6aae803b28f29402c641618cf450
Shariq Ansari <sharique.rik@gmail.com>
2023-07-10 14:02:14 +0530

    fix: removed unused action buttons

--- frappe/public/js/form_builder/form_builder.bundle.js
+++ frappe/public/js/form_builder/form_builder.bundle.js
@@ -27,16 +27,6 @@ class FormBuilder {
 	}
 
 	async setup_page_actions() {
-		// clear actions
-		this.page.clear_actions();
-		this.page.clear_menu();
-		this.page.clear_custom_actions();
-
-		// setup page actions
-		this.primary_btn = this.page.set_primary_action(__("Save"), () =>
-			this.store.save_changes()
-		);
-
 		this.preview_btn = this.page.add_button(__("Show Preview"), () => {
 			this.store.preview = !this.store.preview;
 
@@ -51,29 +41,6 @@ class FormBuilder {
 		this.reset_changes_btn = this.page.add_button(__("Reset Changes"), () => {
 			this.store.reset_changes();
 		});
-
-		this.go_to_doctype_list_btn = this.page.add_button(
-			__("Go to {0} List", [__(this.doctype)]),
-			() => {
-				window.open(`/app/${frappe.router.slug(this.doctype)}`);
-			}
-		);
-
-		this.customize_form_btn = this.page.add_menu_item(__("Switch to Customize"), () => {
-			frappe.set_route("form-builder", this.doctype, "customize");
-		});
-		this.doctype_form_btn = this.page.add_menu_item(__("Switch to DocType"), () => {
-			frappe.set_route("form-builder", this.doctype);
-		});
-
-		this.go_to_doctype_btn = this.page.add_menu_item(__("Go to DocType"), () =>
-			frappe.set_route("Form", "DocType", this.doctype)
-		);
-		this.go_to_customize_form_btn = this.page.add_menu_item(__("Go to Customize Form"), () =>
-			frappe.set_route("Form", "Customize Form", {
-				doc_type: this.doctype,
-			})
-		);
 	}
 
 	setup_app() {
@@ -112,59 +79,8 @@ class FormBuilder {
 				this.reset_changes_btn.hide();
 			}
 
-			// hide all buttons
-			this.go_to_doctype_list_btn.hide();
-			this.customize_form_btn.hide();
-			this.doctype_form_btn.hide();
-			this.go_to_doctype_btn.hide();
-			this.go_to_customize_form_btn.hide();
-
-			this.page.menu_btn_group.show();
-			let hide_menu = true;
-
-			// show customize form & Go to customize form btn
-			if (
-				this.store.doc &&
-				!this.store.doc.custom &&
-				!this.store.doc.issingle &&
-				!this.store.is_customize_form &&
-				!in_list(frappe.model.core_doctypes_list, this.doctype)
-			) {
-				this.customize_form_btn.show();
-				this.go_to_customize_form_btn.show();
-				hide_menu = false;
-			}
-
-			// show doctype form & Go to doctype form btn
-			if (
-				this.store.doc &&
-				!this.store.doc.custom &&
-				!this.store.doc.issingle &&
-				this.store.is_customize_form
-			) {
-				this.doctype_form_btn.show();
-				this.go_to_doctype_btn.show();
-				hide_menu = false;
-			}
-
-			// show Go to {0} List or Go to {0} button
-			if (this.store.doc && !this.store.doc.istable) {
-				let label = this.store.doc.issingle
-					? __("Go to {0}", [__(this.doctype)])
-					: __("Go to {0} List", [__(this.doctype)]);
-
-				this.go_to_doctype_list_btn.text(label).show();
-			}
-
-			if (hide_menu && window.matchMedia("(min-device-width: 992px)").matches) {
-				this.page.menu_btn_group.hide();
-			}
-
 			// toggle preview btn text
 			this.preview_btn.text(this.store.preview ? __("Hide Preview") : __("Show Preview"));
-
-			// toggle primary btn and show indicator based on read_only state
-			this.primary_btn.toggle(!this.store.read_only);
 			if (this.store.read_only) {
 				let message = this.store.preview ? __("Preview Mode") : __("Read Only");
 				this.page.set_indicator(message, "orange");
