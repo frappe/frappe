04f8d231a70854334c137922c224aeb1f4dfe01a
anandbaburajan <anandbaburajan@gmail.com>
2023-07-11 12:37:29 +0530

    fix: dont stop next unattached file from being attached if file_url of first file is invalid

--- frappe/core/doctype/file/utils.py
+++ frappe/core/doctype/file/utils.py
@@ -308,7 +308,7 @@ def attach_files_to_document(doc: "Document", event) -> None:
 		# we dont want the update to fail if file cannot be attached for some reason
 		value = doc.get(df.fieldname)
 		if not (value or "").startswith(("/files", "/private/files")):
-			return
+			continue
 
 		if frappe.db.exists(
 			"File",
