819f18acb47240a3bb25ca11139d051ca4b9f790
barredterra <14891507+barredterra@users.noreply.github.com>
2023-07-19 10:32:23 +0200

    refactor(Geolocation): image path via map defaults
    Move the default path for icons to map_defaults instead of hardcoding it
    in the control

--- frappe/public/js/frappe/form/controls/geolocation.js
+++ frappe/public/js/frappe/form/controls/geolocation.js
@@ -138,7 +138,7 @@ frappe.ui.form.ControlGeolocation = class ControlGeolocation extends frappe.ui.f
 			},
 		});
 
-		L.Icon.Default.imagePath = "/assets/frappe/images/leaflet/";
+		L.Icon.Default.imagePath = frappe.utils.map_defaults.image_path;
 	}
 
 	bind_leaflet_map() {
