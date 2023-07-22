db3738a89207bba18d2d264c2f366b5d9ed1382b
anandbaburajan <anandbaburajan@gmail.com>
2023-07-22 19:13:21 +0530

    chore: removed unnecessary non_standard_fieldname

--- frappe/tests/test_dashboard_connections.py
+++ frappe/tests/test_dashboard_connections.py
@@ -257,9 +257,6 @@ def create_dashboard_py_for_doctype_a_with_child_table_with_link_to_doctype_b():
 def get_data():
 	return {
 		"fieldname": "doctype_a_with_child_table_with_link_to_doctype_b",
-		"non_standard_fieldnames": {
-			"Doctype B With Child Table With Link To Doctype A": "doctype_a_with_child_table_with_link_to_doctype_b",
-		},
 		"internal_links": {
 			"Doctype B With Child Table With Link To Doctype A": ["child_table", "doctype_b_with_child_table_with_link_to_doctype_a"],
 		},
