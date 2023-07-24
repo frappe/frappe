4f4ea838370816d3979005526b6d9f5a3b665c09
Ankush Menat <ankush@frappe.io>
2023-07-24 14:22:07 +0530

    fix!: Drop unused link_doctype, link_name (#21788)
    (cherry picked from commit 1bfe585b71153e6d9acc69f8e45045899ef8bb71)

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -1350,8 +1350,6 @@ class Document(BaseDocument):
 		comment_type="Comment",
 		text=None,
 		comment_email=None,
-		link_doctype=None,
-		link_name=None,
 		comment_by=None,
 	):
 		"""Add a comment to this document.
@@ -1367,8 +1365,6 @@ class Document(BaseDocument):
 				"reference_doctype": self.doctype,
 				"reference_name": self.name,
 				"content": text or comment_type,
-				"link_doctype": link_doctype,
-				"link_name": link_name,
 			}
 		).insert(ignore_permissions=True)
 		return out
