ccfb6e29798e26cb5cddb6fe4f07a9280be8b679
Ankush Menat <ankush@frappe.io>
2023-07-27 17:28:08 +0530

    refactor: simpler doc mapping

--- frappe/desk/doctype/console_log/console_log.js
+++ frappe/desk/doctype/console_log/console_log.js
@@ -4,26 +4,10 @@
 frappe.ui.form.on("Console Log", {
 	refresh: function (frm) {
 		frm.add_custom_button(__("Reload in Console"), () => {
-			let system_console_doc = frappe.model.get_new_doc("System Console");
-			system_console_doc["console"] = frm.doc.script;
-			if (system_console_doc.doctype) {
-				const sleep = frappe.utils.sleep;
-
-				frappe.dom.freeze(__("Creating {0}", [system_console_doc.doctype]) + "...");
-				// to avoid abrupt UX
-				// wait for activity feedback
-				sleep(500).then(() => {
-					let res = frappe.model.with_doctype(system_console_doc.doctype, () => {
-						frappe.set_route(
-							"Form",
-							system_console_doc.doctype,
-							system_console_doc.name
-						);
-						frappe.dom.unfreeze();
-					});
-					res && res.fail(frappe.dom.unfreeze);
-				});
-			}
+			let system_console = frappe.model.get_new_doc("System Console");
+			system_console.console = frm.doc.script;
+			frappe.model.sync(system_console);
+			frappe.set_route("Form", system_console.doctype, system_console.name);
 		});
 	},
 });
