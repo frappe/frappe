2217e6701f71b1303eaf905680bfbfb01e41f7b5
barredterra <14891507+barredterra@users.noreply.github.com>
2023-07-27 18:29:22 +0200

    feat: letterhead param for attach_print

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -2064,6 +2064,7 @@ def attach_print(
 	lang=None,
 	print_letterhead=True,
 	password=None,
+	letterhead=None,
 ):
 	from frappe.translate import print_language
 	from frappe.utils import scrub_urls
@@ -2077,6 +2078,7 @@ def attach_print(
 		html=html,
 		doc=doc,
 		no_letterhead=not print_letterhead,
+		letterhead=letterhead,
 		password=password,
 	)
 
