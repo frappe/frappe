a8091bb4cec26f68cb91027e747ca9240486a4ec
jll-02 <63648645+jll-02@users.noreply.github.com>
2023-07-27 14:15:28 +0200

    fix: fallback to unscrubbed table fieldname if label not found (#21826)
    * fix: fallback to table fieldname if label not found
    
    * chore: remove whitespaces
    
    Co-authored-by: Ankush Menat <ankushmenat@gmail.com>
    
    * chore: removes whitespaces
    
    Co-authored-by: Ankush Menat <ankushmenat@gmail.com>
    
    ---------
    
    Co-authored-by: Ankush Menat <ankushmenat@gmail.com>

--- frappe/public/js/frappe/form/save.js
+++ frappe/public/js/frappe/form/save.js
@@ -156,7 +156,10 @@ frappe.ui.form.save = function (frm, action, callback, btn) {
 			if (error_fields.length) {
 				let meta = frappe.get_meta(doc.doctype);
 				if (meta.istable) {
-					const table_label = __(frappe.meta.docfield_map[doc.parenttype][doc.parentfield].label).bold();
+					const table_field = frappe.meta.docfield_map[doc.parenttype][doc.parentfield];
+					const table_label = __(
+						table_field.label || frappe.unscrub(table_field.fieldname)
+					).bold();
 					var message = __('Mandatory fields required in table {0}, Row {1}', [table_label, doc.idx]);
 				} else {
 					var message = __('Mandatory fields required in {0}', [__(doc.doctype)]);
