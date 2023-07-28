ff8a8ebcab73a1d2acc390f72109c38e8786091a
Ritvik Sardana <ritviksardana@Ritviks-MacBook-Air.local>
2023-07-28 14:43:07 +0530

    fix: renamed button name

--- frappe/desk/doctype/console_log/console_log.js
+++ frappe/desk/doctype/console_log/console_log.js
@@ -3,7 +3,7 @@
 
 frappe.ui.form.on("Console Log", {
 	refresh: function (frm) {
-		frm.add_custom_button(__("Reload in Console"), () => {
+		frm.add_custom_button(__("Re-Run in Console"), () => {
 			let system_console = frappe.model.get_new_doc("System Console");
 			system_console.console = frm.doc.script;
 			system_console.type = frm.doc.type;
