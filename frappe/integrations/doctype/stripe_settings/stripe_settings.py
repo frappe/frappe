0a70a6759f47d5f7f515de0a2663bfa2f33e019e
Aditya-skyai <aditya@skyaitechnologies.com>
2023-07-31 13:59:46 +0530

    stripe integration

--- frappe/integrations/doctype/stripe_settings/stripe_settings.py
+++ frappe/integrations/doctype/stripe_settings/stripe_settings.py
@@ -9,18 +9,18 @@ from six.moves.urllib.parse import urlencode
 import frappe
 from frappe import _
 from frappe.integrations.utils import (
-	create_payment_gateway,
-	create_request_log,
-	make_get_request,
-	make_post_request,
+    create_payment_gateway,
+    create_request_log,
+    make_get_request,
+    make_post_request,
 )
 from frappe.model.document import Document
 from frappe.utils import call_hook_method, cint, flt, get_url
 
 
 class StripeSettings(Document):
-	supported_currencies = [
-		"AED",
+    supported_currencies = [
+        "AED",
 		"ALL",
 		"ANG",
 		"ARS",
@@ -134,10 +134,10 @@ class StripeSettings(Document):
 		"XPF",
 		"YER",
 		"ZAR",
-	]
+    ]
 
-	currency_wise_minimum_charge_amount = {
-		"JPY": 50,
+    currency_wise_minimum_charge_amount = {
+        "JPY": 50,
 		"MXN": 10,
 		"DKK": 2.50,
 		"HKD": 4.00,
@@ -152,132 +152,130 @@ class StripeSettings(Document):
 		"GBP": 0.30,
 		"NZD": 0.50,
 		"SGD": 0.50,
-	}
+    }
 
-	def on_update(self):
-		create_payment_gateway(
-			"Stripe-" + self.gateway_name, settings="Stripe Settings", controller=self.gateway_name
-		)
-		call_hook_method("payment_gateway_enabled", gateway="Stripe-" + self.gateway_name)
-		if not self.flags.ignore_mandatory:
-			self.validate_stripe_credentails()
+    def on_update(self):
+        create_payment_gateway(
+            "Stripe-" + self.gateway_name,
+            settings="Stripe Settings",
+            controller=self.gateway_name,
+        )
+        call_hook_method("payment_gateway_enabled", gateway="Stripe-" + self.gateway_name)
+        if not self.flags.ignore_mandatory:
+            self.validate_stripe_credentials()
 
-	def validate_stripe_credentails(self):
-		if self.publishable_key and self.secret_key:
-			header = {
-				"Authorization": "Bearer {0}".format(
-					self.get_password(fieldname="secret_key", raise_exception=False)
-				)
-			}
-			try:
-				make_get_request(url="https://api.stripe.com/v1/charges", headers=header)
-			except Exception:
-				frappe.throw(_("Seems Publishable Key or Secret Key is wrong !!!"))
+    def validate_stripe_credentials(self):
+        if self.publishable_key and self.secret_key:
+            header = {
+                "Authorization": "Bearer {0}".format(
+                    self.get_password(fieldname="secret_key", raise_exception=False)
+                )
+            }
+            try:
+                make_get_request(url="https://api.stripe.com/v1/charges", headers=header)
+            except Exception:
+                frappe.throw(_("Seems Publishable Key or Secret Key is wrong !!!"))
 
-	def validate_transaction_currency(self, currency):
-		if currency not in self.supported_currencies:
-			frappe.throw(
-				_(
-					"Please select another payment method. Stripe does not support transactions in currency '{0}'"
-				).format(currency)
-			)
+    def validate_transaction_currency(self, currency):
+        if currency not in self.supported_currencies:
+            frappe.throw(
+                _(
+                    "Please select another payment method. Stripe does not support transactions in currency '{0}'"
+                ).format(currency)
+            )
 
-	def validate_minimum_transaction_amount(self, currency, amount):
-		if currency in self.currency_wise_minimum_charge_amount:
-			if flt(amount) < self.currency_wise_minimum_charge_amount.get(currency, 0.0):
-				frappe.throw(
-					_("For currency {0}, the minimum transaction amount should be {1}").format(
-						currency, self.currency_wise_minimum_charge_amount.get(currency, 0.0)
-					)
-				)
+    def validate_minimum_transaction_amount(self, currency, amount):
+        if currency in self.currency_wise_minimum_charge_amount:
+            if flt(amount) < self.currency_wise_minimum_charge_amount.get(currency, 0.0):
+                frappe.throw(
+                    _("For currency {0}, the minimum transaction amount should be {1}").format(
+                        currency, self.currency_wise_minimum_charge_amount.get(currency, 0.0)
+                    )
+                )
 
-	def get_payment_url(self, **kwargs):
-		return get_url("./integrations/stripe_checkout?{0}".format(urlencode(kwargs)))
+    def get_payment_url(self, **kwargs):
+        return get_url("./integrations/stripe_checkout?{0}".format(urlencode(kwargs)))
 
-	def create_request(self, data):
-		import stripe
+    def create_request(self, data):
+        import stripe
 
-		self.data = frappe._dict(data)
-		stripe.api_key = self.get_password(fieldname="secret_key", raise_exception=False)
-		stripe.default_http_client = stripe.http_client.RequestsClient()
+        self.data = frappe._dict(data)
+        stripe.api_key = self.get_password(fieldname="secret_key", raise_exception=False)
+        stripe.default_http_client = stripe.http_client.RequestsClient()
 
-		try:
-			self.integration_request = create_request_log(self.data, "Host", "Stripe")
-			return self.create_charge_on_stripe()
+        try:
+            self.integration_request = create_request_log(self.data, "Host", "Stripe")
+            return self.create_payment_intent_on_stripe()
 
-		except Exception:
-			frappe.log_error(frappe.get_traceback())
-			return {
-				"redirect_to": frappe.redirect_to_message(
-					_("Server Error"),
-					_(
-						"It seems that there is an issue with the server's stripe configuration. In case of failure, the amount will get refunded to your account."
-					),
-				),
-				"status": 401,
-			}
+        except Exception:
+            frappe.log_error(frappe.get_traceback())
+            return {
+                "redirect_to": frappe.redirect_to_message(
+                    _("Server Error"),
+                    _(
+                        "It seems that there is an issue with the server's stripe configuration. In case of failure, the amount will get refunded to your account."
+                    ),
+                ),
+                "status": 401,
+            }
 
-	def create_charge_on_stripe(self):
-		import stripe
+    def create_payment_intent_on_stripe(self):
+        import stripe
 
-		try:
-			charge = stripe.Charge.create(
-				amount=cint(flt(self.data.amount) * 100),
-				currency=self.data.currency,
-				source=self.data.stripe_token_id,
-				description=self.data.description,
-				receipt_email=self.data.payer_email,
-			)
+        try:
+            payment_intent = stripe.PaymentIntent.create(
+                amount=cint(flt(self.data.amount) * 100),
+                currency=self.data.currency,
+                description=self.data.description,
+                receipt_email=self.data.payer_email,
+            )
 
-			if charge.captured == True:
-				self.integration_request.db_set("status", "Completed", update_modified=False)
-				self.flags.status_changed_to = "Completed"
+            if payment_intent.status == "requires_payment_method":
+                self.integration_request.db_set("status", "Completed", update_modified=False)
+                self.flags.status_changed_to = "Completed"
 
-			else:
-				frappe.log_error(charge.failure_message, "Stripe Payment not completed")
+        except Exception:
+            frappe.log_error(frappe.get_traceback())
 
-		except Exception:
-			frappe.log_error(frappe.get_traceback())
+        return self.finalize_request()
 
-		return self.finalize_request()
+    def finalize_request(self):
+        redirect_to = self.data.get("redirect_to") or None
+        redirect_message = self.data.get("redirect_message") or None
+        status = self.integration_request.status
 
-	def finalize_request(self):
-		redirect_to = self.data.get("redirect_to") or None
-		redirect_message = self.data.get("redirect_message") or None
-		status = self.integration_request.status
+        if self.flags.status_changed_to == "Completed":
+            if self.data.reference_doctype and self.data.reference_docname:
+                custom_redirect_to = None
+                try:
+                    custom_redirect_to = frappe.get_doc(
+                        self.data.reference_doctype, self.data.reference_docname
+                    ).run_method("on_payment_authorized", self.flags.status_changed_to)
+                except Exception:
+                    frappe.log_error(frappe.get_traceback())
 
-		if self.flags.status_changed_to == "Completed":
-			if self.data.reference_doctype and self.data.reference_docname:
-				custom_redirect_to = None
-				try:
-					custom_redirect_to = frappe.get_doc(
-						self.data.reference_doctype, self.data.reference_docname
-					).run_method("on_payment_authorized", self.flags.status_changed_to)
-				except Exception:
-					frappe.log_error(frappe.get_traceback())
+                if custom_redirect_to:
+                    redirect_to = custom_redirect_to
 
-				if custom_redirect_to:
-					redirect_to = custom_redirect_to
+                redirect_url = "payment-success"
 
-				redirect_url = "payment-success"
+            if self.redirect_url:
+                redirect_url = self.redirect_url
+                redirect_to = None
+        else:
+            redirect_url = "payment-failed"
 
-			if self.redirect_url:
-				redirect_url = self.redirect_url
-				redirect_to = None
-		else:
-			redirect_url = "payment-failed"
+        if redirect_to:
+            redirect_url += "?" + urlencode({"redirect_to": redirect_to})
+        if redirect_message:
+            redirect_url += "&" + urlencode({"redirect_message": redirect_message})
 
-		if redirect_to:
-			redirect_url += "?" + urlencode({"redirect_to": redirect_to})
-		if redirect_message:
-			redirect_url += "&" + urlencode({"redirect_message": redirect_message})
-
-		return {"redirect_to": redirect_url, "status": status}
+        return {"redirect_to": redirect_url, "status": status}
 
 
 def get_gateway_controller(doctype, docname):
-	reference_doc = frappe.get_doc(doctype, docname)
-	gateway_controller = frappe.db.get_value(
-		"Payment Gateway", reference_doc.payment_gateway, "gateway_controller"
-	)
-	return gateway_controller
+    reference_doc = frappe.get_doc(doctype, docname)
+    gateway_controller = frappe.db.get_value(
+        "Payment Gateway", reference_doc.payment_gateway, "gateway_controller"
+    )
+    return gateway_controller
