f0c5de6c785385af6cd7132d1655348fb87ab2d9
barredterra <14891507+barredterra@users.noreply.github.com>
2023-08-05 17:39:08 +0200

    fix: validate type in share.get_users

--- frappe/share.py
+++ frappe/share.py
@@ -129,6 +129,12 @@ def set_docshare_permission(doctype, name, user, permission_to, value=1, everyon
 @frappe.whitelist()
 def get_users(doctype: str, name: str) -> list:
 	"""Get list of users with which this document is shared"""
+	if not isinstance(doctype, str):
+		raise TypeError("doctype must be of type str")
+
+	if not isinstance(name, str):
+		raise TypeError("name must be of type str")
+
 	doc = frappe.get_doc(doctype, name)
 	return _get_users(doc)
 
