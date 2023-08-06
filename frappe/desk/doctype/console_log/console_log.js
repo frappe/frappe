1a9e2d6dfe9b90fee04ff1256c940c17b01b67bd
Ritvik Sardana <ritviksardana@Ritviks-MacBook-Air.local>
2023-08-06 22:55:58 +0530

    fix: new solution for Re-run in console

--- frappe/desk/doctype/console_log/console_log.js
+++ frappe/desk/doctype/console_log/console_log.js
@@ -4,11 +4,9 @@
 frappe.ui.form.on("Console Log", {
 	refresh: function (frm) {
 		frm.add_custom_button(__("Re-Run in Console"), () => {
-			let system_console = frappe.model.get_new_doc("System Console");
-			system_console.console = frm.doc.script;
-			system_console.type = frm.doc.type;
-			frappe.model.sync(system_console);
-			frappe.set_route("Form", system_console.doctype, system_console.name);
+			window.localStorage.setItem("system_console_code", frm.doc.script);
+			window.localStorage.setItem("system_console_type", frm.doc.type);
+			window.location.href = "/app/system-console";
 		});
 	},
 });
