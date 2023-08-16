f45fde46458fbc11750027790b50199890964bc4
Deepesh Garg <deepeshgarg6@gmail.com>
2023-08-16 20:18:57 +0530

    chore: Provision to ignore prepared report via route options (#22077)
    (cherry picked from commit 112836dcd784043b0791a3c71a433957c79f5a0f)
    
    # Conflicts:
    #	frappe/desk/query_report.py

--- frappe/desk/query_report.py
+++ frappe/desk/query_report.py
@@ -214,6 +214,7 @@ def run(
 	if sbool(are_default_filters) and report.custom_filters:
 		filters = report.custom_filters
 
+<<<<<<< HEAD
 	if (
 		report.prepared_report
 		and not report.disable_prepared_report
@@ -221,6 +222,9 @@ def run(
 		and not custom_columns
 	):
 		dn = None
+=======
+	if report.prepared_report and not sbool(ignore_prepared_report) and not custom_columns:
+>>>>>>> 112836dcd7 (chore: Provision to ignore prepared report via route options (#22077))
 		if filters:
 			if isinstance(filters, str):
 				filters = json.loads(filters)
