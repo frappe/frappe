e1af3485d05b9eb8cef7c0c36a7f85d11426a8fc
Maharshi Patel <maharshi.patel988@gmail.com>
2023-08-17 19:28:25 +0530

    fix: add espresso icons in frappe.utils.icon
    this will need to clean this at the end.

--- frappe/public/js/frappe/utils/utils.js
+++ frappe/public/js/frappe/utils/utils.js
@@ -1208,14 +1208,18 @@ Object.assign(frappe.utils, {
 
 	icon(icon_name, size = "sm", icon_class = "", icon_style = "", svg_class = "") {
 		let size_class = "";
+		let is_espresso = icon_name.startsWith("es-");
 
+		icon_name = is_espresso ? `${"#" + icon_name}` : `${"#icon-" + icon_name}`;
 		if (typeof size == "object") {
 			icon_style += ` width: ${size.width}; height: ${size.height}`;
 		} else {
 			size_class = `icon-${size}`;
 		}
-		return `<svg class="icon ${svg_class} ${size_class}" style="${icon_style}">
-			<use class="${icon_class}" href="#icon-${icon_name}"></use>
+		return `<svg class="${
+			is_espresso ? "es-icon" : "icon"
+		} ${svg_class} ${size_class}" style="${icon_style}">
+			<use class="${icon_class}" href="${icon_name}"></use>
 		</svg>`;
 	},
 
