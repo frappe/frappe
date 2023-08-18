b4fd3817361bb291cfa9f858d81a524ef42e6c76
Gursheen Anand <gursheen@frappe.io>
2023-08-18 11:04:31 +0530

    feat: add html template for comparator
    (cherry picked from commit c3d0ab5f9a4ef4c961bf11e7574fb2175bc7d3a7)

---
+++ frappe/core/doctype/document_comparator/document_comparator.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2023, Frappe Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Document Comparator", {
+	refresh(frm) {
+		frm.disable_save();
+
+		frm.set_query("doctype_name", () => {
+			return {
+				filters: {
+					track_changes: 1,
+				},
+			};
+		});
+
+		frm.page.set_primary_action("Compare", () => {
+			frm.call({
+				doc: frm.doc,
+				method: "compare_document",
+				callback: function (r) {
+					let document_names = r.message[0];
+					let changed_fields = r.message[1];
+					let render_dict = {
+						documents: document_names,
+						changed: changed_fields.changed,
+						row_changed: changed_fields.row_changed,
+					};
+					$(frappe.render_template("document_comparator", render_dict)).appendTo(
+						frm.fields_dict.version_table.$wrapper.empty()
+					);
+				},
+			});
+		});
+	},
+});
