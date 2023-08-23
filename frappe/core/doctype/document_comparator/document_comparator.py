38dbb93e66ff5e7cbf25d0bafd8e722ad9b54dc0
Gursheen Anand <gursheen@frappe.io>
2023-08-23 12:27:34 +0530

    feat: add logic for addition and deletion grids
    (cherry picked from commit 98437f1c3091b52c83c612763cc9596dd8a4dc40)

--- frappe/core/doctype/document_comparator/document_comparator.py
+++ frappe/core/doctype/document_comparator/document_comparator.py
@@ -45,47 +45,58 @@ class DocumentComparator(Document):
 			pluck="name",
 			limit=5,
 		)
-		amended_docs = [frappe.get_doc(self.doctype_name, name) for name in amended_document_names]
-		self.docs_to_compare = len(amended_docs)
+		self.amended_docs = [frappe.get_doc(self.doctype_name, name) for name in amended_document_names]
+		self.docs_to_compare = len(self.amended_docs)
 
 		self.changed = {}
 		self.row_changed = {}
+		self.added = {}
+		self.removed = {}
 
 		for i in range(1, self.docs_to_compare):
-			diff = get_diff(amended_docs[i - 1], amended_docs[i], compare_cancelled=True)
+			diff = get_diff(self.amended_docs[i - 1], self.amended_docs[i], compare_cancelled=True)
 			self.get_diff_grid(i, diff)
+			self.get_rows_added_removed_grid(i, diff, "added", self.added)
+			self.get_rows_added_removed_grid(i, diff, "removed", self.removed)
 			self.get_rows_updated_grid(i, diff)
 
-		return amended_document_names, {"changed": self.changed, "row_changed": self.row_changed}
+		return amended_document_names, {
+			"changed": self.changed,
+			"row_changed": self.row_changed,
+			"added": self.added,
+			"removed": self.removed,
+		}
 
 	def get_diff_grid(self, i, diff):
 		for change in diff.changed:
 			fieldname = get_field_label(change[0], doctype=self.doctype_name)
 			value = change[-1]
-			if fieldname not in self.changed:
-				self.changed[fieldname] = [""] * self.docs_to_compare
+			value_list = [""] * self.docs_to_compare
+			self.changed.setdefault(fieldname, value_list)
 			self.changed[fieldname][i] = value or ""
 
 			if i == 1:
 				value = change[1]
 				self.changed[fieldname][i - 1] = value or ""
 
-	def get_rows_updated_grid(self, i, diff):
-		# set an empty dictionary for each table
-		# so it does not get overwritten for every change in same table
-		for table in diff.row_changed:
-			table_name = get_field_label(table[0], doctype=self.doctype_name)
-			self.row_changed[table_name] = {}
+	def get_rows_added_removed_grid(self, i, diff, key, changed_dict):
+		doc_name = self.amended_docs[i].name
+		changed_dict[doc_name] = {}
+		for change in diff[key]:
+			tablename = get_field_label(change[0], doctype=self.doctype_name)
+			value_dict = filter_fields_for_gridview(change[-1])
+			changed_dict[doc_name].setdefault(tablename, []).append(value_dict)
 
+	def get_rows_updated_grid(self, i, diff):
 		for change in diff.row_changed:
 			table_name = get_field_label(change[0], doctype=self.doctype_name)
 			index = change[1]
-			self.row_changed[table_name][index] = {}
+			self.row_changed.setdefault(table_name, {}).setdefault(index, {})
 			for field in change[-1]:
-				fieldname = get_field_label(field[0], is_child=True)
+				fieldname = get_field_label(field[0], doctype=self.doctype_name, child_field=change[0])
 				value = field[-1]
-				if fieldname not in self.row_changed[table_name][index]:
-					self.row_changed[table_name][index][fieldname] = [""] * self.docs_to_compare
+				value_list = [""] * self.docs_to_compare
+				self.row_changed[table_name][index].setdefault(fieldname, value_list)
 				self.row_changed[table_name][index][fieldname][i] = value or ""
 
 				if i == 1:
@@ -93,13 +104,26 @@ class DocumentComparator(Document):
 					self.row_changed[table_name][index][fieldname][i - 1] = value or ""
 
 
-def get_field_label(fieldname, doctype=None, is_child=False):
-	if is_child:
-		label = frappe.db.get_value("DocField", {"fieldname": fieldname}, "label")
-		return label
+def get_field_label(fieldname, doctype, child_field=None):
+	if child_field:
+		meta = frappe.get_meta(doctype)
+		for field in meta.fields:
+			if field.fieldname == child_field:
+				doctype = field.options
 
 	meta = frappe.get_meta(doctype)
 	label = meta.get_label(fieldname)
 	if label not in ["No Label", "None", ""]:
 		return label
 	return fieldname
+
+
+def filter_fields_for_gridview(row):
+	grid_row = {}
+	meta = frappe.get_meta(row.doctype)
+	for field in meta.fields:
+		if field.in_list_view == 1:
+			fieldlabel = get_field_label(field.fieldname, row.doctype)
+			grid_row[fieldlabel] = row[field.fieldname] or ""
+
+	return grid_row
