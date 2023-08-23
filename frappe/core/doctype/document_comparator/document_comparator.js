56d89112bf967de133928677527d73b658d8b1ce
Gursheen Anand <gursheen@frappe.io>
2023-08-23 12:29:19 +0530

    feat: add js for rendering html grids
    (cherry picked from commit 57e4e0bc30c502dc1d12cbc193f864ddf722708f)

--- frappe/core/doctype/document_comparator/document_comparator.js
+++ frappe/core/doctype/document_comparator/document_comparator.js
@@ -23,17 +23,45 @@ frappe.ui.form.on("Document Comparator", {
 				callback: function (r) {
 					let document_names = r.message[0];
 					let changed_fields = r.message[1];
-					let render_dict = {
-						documents: document_names,
-						changed: changed_fields.changed,
-						row_changed: changed_fields.row_changed,
-					};
-					$(frappe.render_template("document_comparator", render_dict)).appendTo(
-						frm.fields_dict.version_table.$wrapper.empty()
-					);
-					frm.set_df_property("version_table", "hidden", 0);
+					frm.events.render_changed_fields(frm, document_names, changed_fields);
+					frm.events.render_rows_added_or_removed(frm, changed_fields);
 				},
 			});
 		});
+	},
+
+	render_changed_fields(frm, document_names, changed_fields) {
+		let render_dict = {
+			documents: document_names,
+			changed: changed_fields.changed,
+			row_changed: changed_fields.row_changed,
+		};
+		$(frappe.render_template("document_comparator", render_dict)).appendTo(
+			frm.fields_dict.version_table.$wrapper.empty()
+		);
+		frm.set_df_property("version_table", "hidden", 0);
+	},
+
+	render_rows_added_or_removed(frm, changed_fields) {
+		let added_or_removed = {
+			rows_added: changed_fields.added,
+			rows_removed: changed_fields.removed,
+		};
+
+		let hide_section = 0;
+		let section_dict = {};
+
+		for (let key in added_or_removed) {
+			hide_section = 0;
+			section_dict = {
+				added_or_removed: added_or_removed[key],
+			};
+			$(
+				frappe.render_template("document_comparator_rows_added_removed", section_dict)
+			).appendTo(frm.fields_dict[key].$wrapper.empty());
+
+			if (!frm.fields_dict[key].disp_area.innerHTML.includes("<table")) hide_section = 1;
+			frm.set_df_property(key + "_section", "hidden", hide_section);
+		}
 	},
 });
