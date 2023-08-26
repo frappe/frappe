e89897fe337de1a9b56e61e6c1a8375d282cb381
Ankush Menat <ankush@frappe.io>
2023-08-26 12:32:47 +0530

    ci: install libmariadb-dev

--- .github/helper/install_dependencies.sh
+++ .github/helper/install_dependencies.sh
@@ -5,7 +5,7 @@ echo "Setting Up System Dependencies..."
 
 sudo apt update
 sudo apt remove mysql-server mysql-client
-sudo apt install libcups2-dev redis-server mariadb-client-10.6
+sudo apt install libcups2-dev redis-server mariadb-client-10.6 libmariadb-dev
 
 install_wkhtmltopdf() {
   wget -q https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
