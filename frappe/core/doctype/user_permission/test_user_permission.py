edd9ad4f6f7a77f98a8287bf19264c8c51e8557f
RitvikSardana <ritvik.sardana11@gmail.com>
2023-09-01 12:46:37 +0530

    test: give access of Parent DocType to Blogger role

--- frappe/core/doctype/user_permission/test_user_permission.py
+++ frappe/core/doctype/user_permission/test_user_permission.py
@@ -6,7 +6,7 @@ from frappe.core.doctype.user_permission.user_permission import (
 	add_user_permissions,
 	remove_applicable,
 )
-from frappe.permissions import has_user_permission
+from frappe.permissions import add_permission, has_user_permission
 from frappe.tests.utils import FrappeTestCase
 from frappe.website.doctype.blog_post.test_blog_post import make_test_blog
 
@@ -175,6 +175,8 @@ class TestUserPermission(FrappeTestCase):
 		self.assertTrue(has_user_permission(frappe.get_doc("Person", parent_record.name), user.name))
 		self.assertTrue(has_user_permission(frappe.get_doc("Person", child_record.name), user.name))
 
+		#  give access of Parent DocType to Blogger role
+		add_permission("Person", "Blogger")
 		frappe.set_user(user.name)
 		visible_names = frappe.get_list(
 			doctype="Person",
