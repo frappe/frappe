d1baf506fee21077166e531587d1edff2f441011
Gursheen Anand <gursheen@frappe.io>
2023-09-01 10:09:43 +0530

    feat: listview for vcs patch

---
+++ frappe/core/doctype/vcs_document_patch/vcs_document_patch_list.js
@@ -0,0 +1,23 @@
+frappe.listview_settings["VCS Document Patch"] = {
+	add_fields: ["is_standard"],
+	refresh: function (listview) {},
+	button: {
+		show: function (doc) {
+			return doc.document_type;
+		},
+		get_label: function () {
+			return __("Show", null, "Access");
+		},
+		get_description: function (doc) {
+			return __("Show {0}", [`${__(doc.document_type)}: ${doc.document_name}`]);
+		},
+		action: function (doc) {
+			const newdoc = frappe.model.get_new_doc(doc.document_type);
+			let doc_json = JSON.parse(doc.document_json);
+			for (let key in doc_json) {
+				newdoc[key] = doc_json[key];
+			}
+			frappe.set_route("Form", doc.document_type, doc.document_name);
+		},
+	},
+};
