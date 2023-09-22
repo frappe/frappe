de6ae798fd5b18e0c4b8218f776c86bdcf4caeab
riteshmoud <moudritesh@gmail.com>
2023-09-22 18:10:50 -0400

    fixed notification click event

--- frappe/public/js/frappe/ui/notifications/notifications.js
+++ frappe/public/js/frappe/ui/notifications/notifications.js
@@ -282,7 +282,6 @@ class NotificationsView extends BaseNotificationsView {
 		item_html.on("click", () => {
 			!notification_log.read && this.mark_as_read(notification_log.name, item_html);
 			this.notifications_icon.trigger("click");
-			this.dropdown.dropdown('hide')
 		});
 
 		return item_html;
