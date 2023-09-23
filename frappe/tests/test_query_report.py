7e0385df83d97b0700f23c12738fe0d7a461fbba
RitvikSardana <ritvik.sardana11@gmail.com>
2023-09-24 01:31:41 +0530

    test: added test for conflicting column names
    (cherry picked from commit 34cd9435566dcac6580c47d6b6bb8bc6ca1c4b9d)
    
    # Conflicts:
    #	frappe/tests/test_query_report.py

--- frappe/tests/test_query_report.py
+++ frappe/tests/test_query_report.py
@@ -1,14 +1,33 @@
 # Copyright (c) 2019, Frappe Technologies Pvt. Ltd. and Contributors
 # License: MIT. See LICENSE
 
+import json
+import os
+
 import frappe
 import frappe.utils
+<<<<<<< HEAD
 from frappe.desk.query_report import build_xlsx_data
+=======
+from frappe.core.doctype.doctype.test_doctype import new_doctype
+from frappe.desk.query_report import (
+	build_xlsx_data,
+	export_query,
+	generate_report_result,
+	get_data_for_custom_field,
+	get_data_for_custom_report,
+	run,
+)
+from frappe.tests.ui_test_helpers import create_doctype
+>>>>>>> 34cd943556 (test: added test for conflicting column names)
 from frappe.tests.utils import FrappeTestCase
 from frappe.utils.xlsxutils import make_xlsx
 
 
 class TestQueryReport(FrappeTestCase):
+	def tearDown(self):
+		frappe.db.rollback()
+
 	def test_xlsx_data_with_multiple_datatypes(self):
 		"""Test exporting report using rows with multiple datatypes (list, dict)"""
 
@@ -69,3 +88,165 @@ class TestQueryReport(FrappeTestCase):
 		for row in xlsx_data:
 			# column_b should be 'str' even with composite cell value
 			self.assertEqual(type(row[1]), str)
+<<<<<<< HEAD
+=======
+
+	def test_csv(self):
+		from csv import QUOTE_ALL, QUOTE_MINIMAL, QUOTE_NONE, QUOTE_NONNUMERIC, DictReader
+		from io import StringIO
+
+		REPORT_NAME = "Test CSV Report"
+		REF_DOCTYPE = "DocType"
+		REPORT_COLUMNS = ["name", "module", "issingle"]
+
+		if not frappe.db.exists("Report", REPORT_NAME):
+			report = frappe.new_doc("Report")
+			report.report_name = REPORT_NAME
+			report.ref_doctype = "User"
+			report.report_type = "Query Report"
+			report.query = frappe.qb.from_(REF_DOCTYPE).select(*REPORT_COLUMNS).limit(10).get_sql()
+			report.is_standard = "No"
+			report.save()
+
+		for delimiter in (",", ";", "\t", "|"):
+			for quoting in (QUOTE_ALL, QUOTE_MINIMAL, QUOTE_NONE, QUOTE_NONNUMERIC):
+				frappe.local.form_dict = frappe._dict(
+					{
+						"report_name": REPORT_NAME,
+						"file_format_type": "CSV",
+						"csv_quoting": quoting,
+						"csv_delimiter": delimiter,
+						"include_indentation": 0,
+						"visible_idx": [0, 1, 2],
+					}
+				)
+				export_query()
+
+				self.assertTrue(frappe.response["filename"].endswith(".csv"))
+				self.assertEqual(frappe.response["type"], "binary")
+				with StringIO(frappe.response["filecontent"].decode("utf-8")) as result:
+					reader = DictReader(result, delimiter=delimiter, quoting=quoting)
+					row = reader.__next__()
+					for column in REPORT_COLUMNS:
+						self.assertIn(column, row)
+
+		frappe.delete_doc("Report", REPORT_NAME, delete_permanently=True)
+
+	def test_report_for_duplicate_column_names(self):
+		"""Test report with duplicate column names"""
+
+		try:
+			fields = [
+				{"label": "First Name", "fieldname": "first_name", "fieldtype": "Data"},
+				{"label": "Last Name", "fieldname": "last_name", "fieldtype": "Data"},
+			]
+			docA = frappe.get_doc(
+				{
+					"doctype": "DocType",
+					"name": "Doc A",
+					"module": "Core",
+					"custom": 1,
+					"autoname": "field:first_name",
+					"fields": fields,
+					"permissions": [{"role": "System Manager"}],
+				}
+			).insert(ignore_if_duplicate=True)
+
+			docB = frappe.get_doc(
+				{
+					"doctype": "DocType",
+					"name": "Doc B",
+					"module": "Core",
+					"custom": 1,
+					"autoname": "field:last_name",
+					"fields": fields,
+					"permissions": [{"role": "System Manager"}],
+				}
+			).insert(ignore_if_duplicate=True)
+
+			for i in range(1, 3):
+				frappe.get_doc({"doctype": "Doc A", "first_name": f"John{i}", "last_name": "Doe"}).insert()
+				frappe.get_doc({"doctype": "Doc B", "last_name": f"Doe{i}", "first_name": "John"}).insert()
+
+			if not frappe.db.exists("Report", "Doc A Report"):
+				report = frappe.get_doc(
+					{
+						"doctype": "Report",
+						"ref_doctype": "Doc A",
+						"report_name": "Doc A Report",
+						"report_type": "Script Report",
+						"is_standard": "No",
+					}
+				).insert(ignore_permissions=True)
+			else:
+				report = frappe.get_doc("Report", "Doc A Report")
+
+			report.report_script = """
+result = [["Ritvik","Sardana", "Doe1"],["Shariq","Ansari", "Doe2"]]
+columns = [{
+			"label": "First Name",
+			"fieldname": "first_name",
+			"fieldtype": "Data",
+			"width": 180,
+		},
+		{
+			"label": "Last Name",
+			"fieldname": "last_name",
+			"fieldtype": "Data",
+			"width": 180,
+		},
+		{
+			"label": "Linked Field",
+			"fieldname": "linked_field",
+			"fieldtype": "Link",
+			"options": "Doc B",
+			"width": 180,
+		},
+	]
+
+data = columns, result
+				"""
+			report.save()
+			data = report.get_data()
+
+			custom_columns = [
+				{
+					"fieldname": "first_name-Doc_B",
+					"fieldtype": "Data",
+					"label": "First Name",
+					"insert_after_index": 1,
+					"link_field": {"fieldname": "linked_field", "names": {}},
+					"doctype": "Doc B",
+					"width": 100,
+					"id": "first_name-Doc_B",
+					"name": "First Name",
+					"editable": False,
+					"compareValue": None,
+				},
+			]
+
+			response = run(
+				"Doc A Report",
+				filters={"user": "Administrator", "doctype": "Doc A"},
+				custom_columns=custom_columns,
+			)
+
+			self.assertListEqual(
+				["first_name", "last_name", "first_name-Doc_B", "linked_field"],
+				[d["fieldname"] for d in response["columns"]],
+			)
+
+			self.assertDictEqual(
+				{
+					"first_name": "Ritvik",
+					"last_name": "Sardana",
+					"linked_field": "Doe1",
+					"first_name-Doc_B": "John",
+				},
+				response["result"][0],
+			)
+
+		except Exception as e:
+			raise e
+			frappe.db.rollback()
+>>>>>>> 34cd943556 (test: added test for conflicting column names)
