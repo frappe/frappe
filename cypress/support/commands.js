3334235e7a62c6e9324528be8abca47d0324c5b6
Suraj Shetty <surajshetty3416@gmail.com>
2023-09-25 09:16:59 +0530

    test: Load root page before logout to load CSRF token

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -53,6 +53,9 @@ Cypress.Commands.add("login", (email, password) => {
 });
 
 Cypress.Commands.add("call", (method, args) => {
+	if (method === "logout") {
+		cy.visit("/");
+	}
 	return cy
 		.window()
 		.its("frappe.csrf_token")
