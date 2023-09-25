de17ead024f86a92210cf96dc1e8cf9e5d3fce9d
Sagar Vora <sagar@resilient.tech>
2023-09-25 14:55:49 +0530

    fix: ensure that `this.data` is set in NumberCardWidget (#22527)
    (cherry picked from commit 4b1da7fe28b97889f35c553fcfb8581a63f12af2)
    
    # Conflicts:
    #	frappe/public/js/frappe/widgets/number_card_widget.js

--- frappe/public/js/frappe/widgets/number_card_widget.js
+++ frappe/public/js/frappe/widgets/number_card_widget.js
@@ -165,7 +165,7 @@ export default class NumberCardWidget extends Widget {
 		}
 
 		this.settings = this.get_settings(this.card_doc.type);
-		this.data = await this.get_data();
+		await this.get_data();
 
 		this.render_number();
 		this.render_stats();
@@ -177,8 +177,14 @@ export default class NumberCardWidget extends Widget {
 		</div>`);
 	}
 
+<<<<<<< HEAD
 	get_data() {
 		return frappe.xcall(this.settings.method, this.settings.args);
+=======
+	async get_data() {
+		this.data = await frappe.xcall(this.settings.method, this.settings.args);
+		return this.settings.get_number(this.data);
+>>>>>>> 4b1da7fe28 (fix: ensure that `this.data` is set in NumberCardWidget (#22527))
 	}
 
 	get_number_for_custom_card(res) {
