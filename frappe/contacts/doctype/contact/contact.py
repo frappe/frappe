833c661e0c5f561c123839375d0b310acb068b1e
Sagar Vora <sagar@resilient.tech>
2023-09-29 12:15:08 +0530

    chore: fix conflicts

--- frappe/contacts/doctype/contact/contact.py
+++ frappe/contacts/doctype/contact/contact.py
@@ -179,13 +179,9 @@ def invite_user(contact):
 @frappe.whitelist()
 def get_contact_details(contact):
 	contact = frappe.get_doc("Contact", contact)
-<<<<<<< HEAD
-	out = {
-=======
 	contact.check_permission()
 
 	return {
->>>>>>> 20178bd3eb (fix: improved validation for address and contact)
 		"contact_person": contact.get("name"),
 		"contact_display": " ".join(
 			filter(None, [contact.get("salutation"), contact.get("first_name"), contact.get("last_name")])
@@ -196,7 +192,6 @@ def get_contact_details(contact):
 		"contact_designation": contact.get("designation"),
 		"contact_department": contact.get("department"),
 	}
-	return out
 
 
 def update_contact(doc, method):
