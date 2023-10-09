3d5af35d903ca8658bfd9ee696ec03fdff1d7c18
Gursheen Anand <gursheen@frappe.io>
2023-10-09 21:09:37 +0530

    fix: route to proposed url

--- frappe/core/doctype/proposed_document/proposed_document_list.js
+++ frappe/core/doctype/proposed_document/proposed_document_list.js
@@ -5,17 +5,11 @@ frappe.listview_settings["Proposed Document"] = {
 	},
 
 	get_form_link: (doc) => {
-		if (doc.status == "Pending" && doc.is_new_doc) {
-			let dummy_doc = frappe.model.get_new_doc(doc.document_type);
-			let doc_json = JSON.parse(doc.document_json);
-			for (let key in doc_json) {
-				dummy_doc[key] = doc_json[key];
-			}
-			dummy_doc.name = "new-" + frappe.scrub(doc.document_type) + "-" + (doc._idx + 1);
-			dummy_doc.proposed_doc = doc.name;
-			return "/app/" + frappe.router.slug(doc.document_type) + "/" + dummy_doc.name;
-		} else if (doc.status == "Approved") {
+		if (doc.status == "Approved") {
 			return "/app/" + frappe.router.slug(doc.document_type) + "/" + doc.document_name;
+		} else {
+			let form_name = "proposed-" + frappe.scrub(doc.document_type) + "-" + doc.name;
+			return "/app/" + frappe.router.slug(doc.document_type) + "/" + form_name;
 		}
 	},
 };
