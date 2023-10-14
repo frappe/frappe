8aeb02572f823699668a00dd93f4edabdc85e619
badihi <badihi.2006@gmail.com>
2023-10-14 23:30:06 +0330

    Importing fa lang for datepicker

--- frappe/public/js/frappe/form/controls/datepicker_i18n.js
+++ frappe/public/js/frappe/form/controls/datepicker_i18n.js
@@ -2,6 +2,7 @@ import "air-datepicker/dist/js/i18n/datepicker.cs.js";
 import "air-datepicker/dist/js/i18n/datepicker.da.js";
 import "air-datepicker/dist/js/i18n/datepicker.de.js";
 import "air-datepicker/dist/js/i18n/datepicker.en.js";
+import "air-datepicker/dist/js/i18n/datepicker.fa.js";
 import "air-datepicker/dist/js/i18n/datepicker.es.js";
 import "air-datepicker/dist/js/i18n/datepicker.fi.js";
 import "air-datepicker/dist/js/i18n/datepicker.fr.js";
