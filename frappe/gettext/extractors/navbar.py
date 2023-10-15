3efe08084412bc86b61213272ae9dd76dba28989
barredterra <14891507+barredterra@users.noreply.github.com>
2023-10-15 21:02:50 +0200

    fix: navbar extractor

--- frappe/gettext/extractors/navbar.py
+++ frappe/gettext/extractors/navbar.py
@@ -11,27 +11,27 @@ def extract(fileobj, *args, **kwargs):
 	    python file containing two global variables `standard_navbar_items` and
 	    `standard_help_items` which are lists of dicts.
 	"""
-	# import variables standard_navbar_items and standard_help_items from the
-	# given python file
 	module = get_module(fileobj.name)
-	standard_navbar_items = getattr(module, "standard_navbar_items")
-	standard_help_items = getattr(module, "standard_help_items")
 
-	for nav_item in standard_navbar_items:
-		if label := nav_item.get("item_label"):
-			item_type = nav_item.get("item_type")
-			yield None, "_", label, [
-				"Label of a standard navbar item",
-				f"Type: {item_type}",
-			]
+	if hasattr(module, "standard_navbar_items"):
+		standard_navbar_items = getattr(module, "standard_navbar_items")
+		for nav_item in standard_navbar_items:
+			if label := nav_item.get("item_label"):
+				item_type = nav_item.get("item_type")
+				yield None, "_", label, [
+					"Label of a standard navbar item",
+					f"Type: {item_type}",
+				]
 
-	for help_item in standard_help_items:
-		if label := help_item.get("item_label"):
-			item_type = nav_item.get("item_type")
-			yield None, "_", label, [
-				"Label of a standard help item",
-				f"Type: {item_type}",
-			]
+	if hasattr(module, "standard_help_items"):
+		standard_help_items = getattr(module, "standard_help_items")
+		for help_item in standard_help_items:
+			if label := help_item.get("item_label"):
+				item_type = nav_item.get("item_type")
+				yield None, "_", label, [
+					"Label of a standard help item",
+					f"Type: {item_type}",
+				]
 
 
 def get_module(path):
