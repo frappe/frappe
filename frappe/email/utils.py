ca5afa85ebe742588bf84cc631cc28184ec08d31
IgorA100 <igora100@gmail.com>
2023-10-17 16:34:41 +0300

    Update utils.py Fix: helper method for proper decoding a sequence
    Introduced new helpder method for decoding "subject" from emails, see #20808 and #20716

--- frappe/email/utils.py
+++ frappe/email/utils.py
@@ -3,6 +3,7 @@
 
 import imaplib
 import poplib
+import chardet
 
 from frappe.utils import cint
 
@@ -16,3 +17,21 @@ def get_port(doc):
 			doc.incoming_port = poplib.POP3_SSL_PORT if doc.use_ssl else poplib.POP3_PORT
 
 	return cint(doc.incoming_port)
+
+def decode_sequence(encoded_sequence) -> str:
+	"""
+	Decodes a encoded_sequence consisting of a tuple (string, charset_encoding). The function concatenates all chunks and returns the resulting decoded string.
+	Args:
+		encoded_sequence ((string, charset_encoding)): A list of tuples where each tuple contains a chunk of the string and its encoding.
+	Returns:
+		str: The decoded and concatenated sequence string.
+	"""
+	from frappe import safe_decode
+	decoded_string = ""
+	for chunk, encoding in encoded_sequence:
+		if not isinstance(chunk, str):
+			detected_encoding = encoding if encoding is not None and encoding != 'unknown-8bit' else chardet.detect(chunk)["encoding"]
+			decoded_string += chunk.decode(detected_encoding, 'ignore')
+		else:
+			decoded_string += chunk
+	return decoded_string
