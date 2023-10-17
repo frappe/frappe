77831b4404613847e406d3cdc48128a4a32e14db
Sambasiva Suda <ssuda777@gmail.com>
2023-10-18 01:28:59 +0530

    chore: adding test case
    (cherry picked from commit 6193f23fd60c88855d515ade1f63818a668c897d)

--- frappe/core/doctype/scheduled_job_type/test_scheduled_job_type.py
+++ frappe/core/doctype/scheduled_job_type/test_scheduled_job_type.py
@@ -4,6 +4,7 @@ import frappe
 from frappe.core.doctype.scheduled_job_type.scheduled_job_type import sync_jobs
 from frappe.tests.utils import FrappeTestCase
 from frappe.utils import get_datetime
+from frappe.utils.data import add_to_date
 
 
 class TestScheduledJobType(FrappeTestCase):
@@ -65,9 +66,16 @@ class TestScheduledJobType(FrappeTestCase):
 		self.assertFalse(job.is_event_due(get_datetime("2019-01-31 23:59:59")))
 
 	def test_cron_job(self):
+		# Daily but offset by 45 minutes
+		job = frappe.get_doc("Scheduled Job Type", dict(method="frappe.core.doctype.log_settings.log_settings.run_log_clean_up"))
+		self.assertEqual(
+			job.next_execution,
+			add_to_date(None, days=1).replace(hour=0, minute=45, second=0, microsecond=0)
+		)
 		# runs every 15 mins
 		job = frappe.get_doc("Scheduled Job Type", dict(method="frappe.oauth.delete_oauth2_data"))
 		job.db_set("last_execution", "2019-01-01 00:00:00")
+		self.assertEqual(job.next_execution, get_datetime("2019-01-01 00:15:00"))
 		self.assertTrue(job.is_event_due(get_datetime("2019-01-01 00:15:01")))
 		self.assertFalse(job.is_event_due(get_datetime("2019-01-01 00:05:06")))
 		self.assertFalse(job.is_event_due(get_datetime("2019-01-01 00:14:59")))
