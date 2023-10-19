4c75b734d84dd7ae78137686a25830fa5907f35b
Bernd Oliver Sünderhauf <46800703+bosue@users.noreply.github.com>
2023-10-19 20:01:36 +0200

    fix(I18n): Use normalize to make fuzzy_match recognize diacritics, umlauts etc.

--- frappe/public/js/frappe/ui/toolbar/fuzzy_match.js
+++ frappe/public/js/frappe/ui/toolbar/fuzzy_match.js
@@ -81,8 +81,18 @@ function fuzzy_match_recursive(
 	// Loop through pattern and str looking for a match.
 	let first_match = true;
 	while (pattern_cur_index < pattern.length && str_curr_index < str.length) {
+		// Normalize and compare individual characters
+		const normalizedPatternChar = pattern[pattern_cur_index]
+			.normalize("NFD")
+			.replace(/[\u0300-\u036f]/g, "")
+			.toLowerCase();
+		const normalizedStrChar = str[str_curr_index]
+			.normalize("NFD")
+			.replace(/[\u0300-\u036f]/g, "")
+			.toLowerCase();
+
 		// Match found.
-		if (pattern[pattern_cur_index].toLowerCase() === str[str_curr_index].toLowerCase()) {
+		if (normalizedPatternChar === normalizedStrChar) {
 			if (next_match >= max_matches) {
 				return [false, out_score, matches];
 			}
