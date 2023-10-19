ad121d11401a2218add346dec374b15616be8af8
Bernd Oliver Sünderhauf <46800703+bosue@users.noreply.github.com>
2023-10-19 17:23:45 +0200

    refactor: Convert frappe.tags.utils to enhanced fuzzy_search.

--- frappe/public/js/frappe/ui/toolbar/tag_utils.js
+++ frappe/public/js/frappe/ui/toolbar/tag_utils.js
@@ -13,15 +13,14 @@ frappe.tags.utils = {
 			return [];
 		}
 
-		for (let i in frappe.tags.tags) {
-			let tag = frappe.tags.tags[i];
-			let level = frappe.search.utils.fuzzy_search(txt, tag);
-			if (level) {
+		frappe.tags.tags.forEach((tag) => {
+			const search_result = frappe.search.utils.fuzzy_search(txt, tag, true);
+			if (search_result.score) {
 				out.push({
 					type: "Tag",
-					label: __("#{0}", [frappe.search.utils.bolden_match_part(__(tag), txt)]),
+					label: __("#{0}", [search_result.marked_string]),
 					value: __("#{0}", [__(tag)]),
-					index: 1 + level,
+					index: 1 + search_result.score,
 					match: tag,
 					onclick() {
 						// Use Global Search Dialog for tag search too.
@@ -29,8 +28,7 @@ frappe.tags.utils = {
 					},
 				});
 			}
-		}
-
+		});
 		return out;
 	},
 
