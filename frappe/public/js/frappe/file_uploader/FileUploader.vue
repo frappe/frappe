experiments001/frappe-sqlite

fix: attach file from library
Previously, the attach from library only shared file_url, problem is we
need more information such as is_private.

fix: send name instead of file_url on file uploader and use that in
upload_file handler to get more information about the file then attach
that info to newly created file.

frappe/public/js/frappe/file_uploader/FileUploader.vue
@@ -390,7 +390,7 @@ function upload_via_file_browser() {
 	}
 	close_dialog.value = true;
 	return upload_file({
-		file_url: selected_file.file_url
+		name: selected_file.value,
 	});
 }
 function upload_via_web_link() {
@@ -517,6 +517,9 @@ function upload_file(file, i) {
 
 		if (file.file_name) {
 			form_data.append('file_name', file.file_name);
+		}
+		if (file.name) {
+			form_data.append('name', file.name);
 		}
 
 		if (props.doctype && props.docname) {

frappe/frappe

frappe/public/js/frappe/file_uploader/FileUploader.vue
@@ -414,7 +414,7 @@ export default {
 			}
 			this.close_dialog = true;
 			return this.upload_file({
-				file_url: selected_file.file_url
+				library_file_name: selected_file.value,
 			});
 		},
 		upload_via_web_link() {
@@ -528,6 +528,10 @@ export default {
 
 				if (file.file_name) {
 					form_data.append('file_name', file.file_name);
+				}
+
+				if (file.library_file_name) {
+					form_data.append('library_file_name', file.library_file_name);
 				}
 
 				if (this.doctype && this.docname) {