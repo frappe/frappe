15d74867a5d59b74e450df00e37a01bb26871dfd
Sambasiva Suda <ssuda777@gmail.com>
2023-10-24 22:40:47 +0530

    fix: search_link fails when txt contains parentheses

--- frappe/model/db_query.py
+++ frappe/model/db_query.py
@@ -54,7 +54,7 @@ STRICT_UNION_PATTERN = re.compile(r".*\s(union).*\s")
 ORDER_GROUP_PATTERN = re.compile(r".*[^a-z0-9-_ ,`'\"\.\(\)].*")
 FN_PARAMS_PATTERN = re.compile(r".*?\((.*)\).*")
 SPECIAL_FIELD_CHARS = frozenset(("(", "`", ".", "'", '"', "*"))
-
+QUOTE_STR_PATTERN = re.compile(r"'.*?'[ ,]*")
 
 class DatabaseQuery:
 	def __init__(self, doctype, user=None):
@@ -648,7 +648,7 @@ class DatabaseQuery:
 			else:
 				# field: 'count(`tabPhoto`.name) as total_count'
 				# column: 'tabPhoto.name'
-				column = field.split("(")[-1].split(")", 1)[0]
+				column = QUOTE_STR_PATTERN.sub("", field).split("(")[-1].split(")", 1)[0]
 				column = strip_alias(column).replace("`", "")
 
 			if column == "*" and not in_function("*", field):
