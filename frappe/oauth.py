45de7cfa739561398c572e8bf06702caf9156acb
nrhdasa <40687270+nrhdasa@users.noreply.github.com>
2023-10-26 21:17:08 +0530

    correct token_expiration_local calculation
    token_expiration_local is having the timezone offset as "+ 05:53 " whereas actually "Asia/Kolkata" timezone is "+ 05:30"

--- frappe/oauth.py
+++ frappe/oauth.py
@@ -248,9 +248,8 @@ class OAuthWebRequestValidator(RequestValidator):
 	def validate_bearer_token(self, token, scopes, request):
 		# Remember to check expiration and scope membership
 		otoken = frappe.get_doc("OAuth Bearer Token", token)
-		token_expiration_local = otoken.expiration_time.replace(
-			tzinfo=pytz.timezone(get_system_timezone())
-		)
+		tzinfo = pytz.timezone(get_system_timezone())
+		token_expiration_local = tzinfo.localize(otoken.expiration_time)
 		token_expiration_utc = token_expiration_local.astimezone(pytz.utc)
 		is_token_valid = (
 			frappe.utils.datetime.datetime.utcnow().replace(tzinfo=pytz.utc) < token_expiration_utc
