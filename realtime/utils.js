be85c246ff1ff95dee8d0b3da25e0af84c0481b5
Faris Ansari <netchamp.faris@gmail.com>
2023-10-26 15:27:27 +0530

    fix(socketio): send correct url for auth
    in case, an app uses a different frontend stack on a different development server,
    socketio should use the correct webserver port when authenticating

--- realtime/utils.js
+++ realtime/utils.js
@@ -1,8 +1,19 @@
+const { get_conf } = require("../node_utils");
+const conf = get_conf();
+
 function get_url(socket, path) {
 	if (!path) {
 		path = "";
 	}
-	return socket.request.headers.origin + path;
+	let url = socket.request.headers.origin;
+	if (conf.developer_mode) {
+		let [protocal, host, port] = url.split(':');
+		if (port != conf.webserver_port) {
+			port = conf.webserver_port;
+		}
+		url = `${protocal}:${host}:${port}`;
+	}
+	return url + path;
 }
 
 module.exports = {
