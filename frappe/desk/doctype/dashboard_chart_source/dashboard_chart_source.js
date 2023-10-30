f087ee5a945af93db86ec4a6af9687b59074af73
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-10-30 12:52:36 +0000

    fix: linters

--- frappe/desk/doctype/dashboard_chart_source/dashboard_chart_source.js
+++ frappe/desk/doctype/dashboard_chart_source/dashboard_chart_source.js
@@ -3,13 +3,17 @@
 
 frappe.ui.form.on("Dashboard Chart Source", {
 	refresh: function (frm) {
-		if (!frm.is_new()){
-			frm.add_custom_button(__('Dashboard Chart'), function () {
-				let dashboard_chart = frappe.model.get_new_doc("Dashboard Chart");
-				dashboard_chart.chart_type = "Custom"
-				dashboard_chart.source = frm.doc.name
-				frappe.set_route('Form', "Dashboard Chart", dashboard_chart.name);
-			}, __("Create"));
+		if (!frm.is_new()) {
+			frm.add_custom_button(
+				__("Dashboard Chart"),
+				function () {
+					let dashboard_chart = frappe.model.get_new_doc("Dashboard Chart");
+					dashboard_chart.chart_type = "Custom";
+					dashboard_chart.source = frm.doc.name;
+					frappe.set_route("Form", "Dashboard Chart", dashboard_chart.name);
+				},
+				__("Create")
+			);
 		}
-	}
+	},
 });
