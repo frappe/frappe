a8a87166b1fffa9b49fb3340ec0eb1743384efef
Thomas Fojan <thomas@tmf.one>
2023-11-04 13:55:15 +0100

    fix: switch google drive picker to gis (prettier)

--- frappe/public/js/integrations/google_drive_picker.js
+++ frappe/public/js/integrations/google_drive_picker.js
@@ -17,7 +17,7 @@ export default class GoogleDrivePicker {
 	loadPicker() {
 		$.when(ajax_gapi(), ajax_gis()).done(this.libsLoaded.bind(this));
 		// load the google identity service library
-		function ajax_gis(){
+		function ajax_gis() {
 			return $.ajax({
 				method: "GET",
 				url: "https://accounts.google.com/gsi/client",
@@ -26,7 +26,7 @@ export default class GoogleDrivePicker {
 			});
 		}
 
-		function ajax_gapi(){
+		function ajax_gapi() {
 			// load the google API library
 			$.ajax({
 				method: "GET",
@@ -38,35 +38,35 @@ export default class GoogleDrivePicker {
 	}
 	libsLoaded() {
 		this.tokenClient = google.accounts.oauth2.initTokenClient({
-		  client_id: this.clientId,
-		  scope: this.scope,
-		  callback: async (response) => {
-						if (response.error !== undefined) {
-							frappe.throw(response);
-						}
-						this.accessToken = response.access_token;
-						frappe.boot.user.google_drive_token = response.access_token;
-						await this.createPicker();
-					},
+			client_id: this.clientId,
+			scope: this.scope,
+			callback: async (response) => {
+				if (response.error !== undefined) {
+					frappe.throw(response);
+				}
+				this.accessToken = response.access_token;
+				frappe.boot.user.google_drive_token = response.access_token;
+				await this.createPicker();
+			},
 		});
 		this.gisInited = true;
-		gapi.load('client:picker', {callback: this.initializePicker.bind(this)});
+		gapi.load("client:picker", { callback: this.initializePicker.bind(this) });
 		if (frappe.boot.user.google_drive_token === null) {
 			// Prompt the user to select a Google Account and ask for consent to share their data
 			// when establishing a new session.
-			this.tokenClient.requestAccessToken({prompt: 'consent'});
+			this.tokenClient.requestAccessToken({ prompt: "consent" });
 		} else {
 			// Skip display of account chooser and consent dialog for an existing session.
-			this.tokenClient.requestAccessToken({prompt: ''});
+			this.tokenClient.requestAccessToken({ prompt: "" });
 		}
 	}
 	async initializePicker() {
-		gapi.client.load('https://www.googleapis.com/discovery/v1/apis/drive/v3/rest');
+		gapi.client.load("https://www.googleapis.com/discovery/v1/apis/drive/v3/rest");
 		this.pickerInited = true;
 	}
 	createPicker() {
 		this.view = new google.picker.View(google.picker.ViewId.DOCS);
-		this.view.setMimeTypes('image/png,image/jpeg,image/jpg');
+		this.view.setMimeTypes("image/png,image/jpeg,image/jpg");
 		this.picker = new google.picker.PickerBuilder()
 			.enableFeature(google.picker.Feature.NAV_HIDDEN)
 			.enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
