97e9e5e0e42757bb3682c0380fe5090ba7e48df7
David Arnold <dgx.arnold@gmail.com>
2023-11-09 13:56:16 +0100

    fix(db): add missing bootstrap public interface

--- frappe/database/__init__.py
+++ frappe/database/__init__.py
@@ -23,6 +23,19 @@ def setup_database(force, source_sql=None, verbose=None, no_mariadb_socket=False
 		)
 
 
+def bootstrap_database(db_name, verbose=None, source_sql=None):
+	import frappe
+
+	if frappe.conf.db_type == "postgres":
+		import frappe.database.postgres.setup_db
+
+		return frappe.database.postgres.setup_db.bootstrap_database(db_name, verbose, source_sql)
+	else:
+		import frappe.database.mariadb.setup_db
+
+		return frappe.database.mariadb.setup_db.bootstrap_database(db_name, verbose, source_sql)
+
+
 def drop_user_and_database(db_name, root_login=None, root_password=None):
 	import frappe
 
