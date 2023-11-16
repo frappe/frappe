8a8be3673e197d12bf2b130c973f400a70b45df1
Shariq Ansari <sharique.rik@gmail.com>
2023-11-16 14:02:30 +0530

    text: fixed failing Customize Form UI Test

--- cypress/integration/customize_form.js
+++ cypress/integration/customize_form.js
@@ -4,6 +4,7 @@ context("Customize Form", () => {
 		cy.visit("/app/customize-form");
 	});
 	it("Changing to naming rule should update autoname", () => {
+		cy.findByRole("tab", { name: "Details" }).click();
 		cy.fill_field("doc_type", "ToDo", "Link").blur();
 		cy.click_form_section("Naming");
 		const naming_rule_default_autoname_map = {
@@ -15,6 +16,7 @@ context("Customize Form", () => {
 			"By script": "",
 		};
 		Cypress._.forOwn(naming_rule_default_autoname_map, (value, naming_rule) => {
+			cy.findByRole("tab", { name: "Details" }).click();
 			cy.fill_field("naming_rule", naming_rule, "Select");
 			cy.get_field("autoname", "Data").should("have.value", value);
 		});
