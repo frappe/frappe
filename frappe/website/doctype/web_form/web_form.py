f095c9e8ef6bcef426e500907e36424f349831e6
Nrtyck <37080373+Nrtyck@users.noreply.github.com>
2023-11-18 19:05:21 +0800

    fix:The title of the Link control does not appear correctly in the website form
    The title of the Link control does not appear correctly in the website form

--- frappe/website/doctype/web_form/web_form.py
+++ frappe/website/doctype/web_form/web_form.py
@@ -664,6 +664,11 @@ def get_link_options(web_form_name, doctype, allow_read_on_all_link_options=Fals
 		show_title_field_in_link = (
 			frappe.db.get_value("DocType", doctype, "show_title_field_in_link", cache=1) == 1
 		)
+		if not show_title_field_in_link:
+			value = frappe.db.get_value("Property Setter", fieldname="value", filters={"property":"show_title_field_in_link","doc_type":doctype}, cache=1)
+			if value and int(value) == 1 :
+				show_title_field_in_link=True
+		
 		if title_field and show_title_field_in_link:
 			fields.append(f"{title_field} as label")
 
