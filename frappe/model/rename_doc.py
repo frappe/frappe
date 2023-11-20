668721a15d752d0a5cb3fbc88d93b8545bd130b6
ruthra kumar <ruthra@erpnext.com>
2023-11-20 10:11:42 +0530

    fix: dont rename link fields in Virtual doctypes
    (cherry picked from commit 94b2e509b9944b7ac26e5ff92f25846f355a8761)

--- frappe/model/rename_doc.py
+++ frappe/model/rename_doc.py
@@ -453,11 +453,12 @@ def get_link_fields(doctype: str) -> list[dict]:
 		cf = frappe.qb.DocType("Custom Field")
 		ps = frappe.qb.DocType("Property Setter")
 
-		st_issingle = frappe.qb.from_(dt).select(dt.issingle).where(dt.name == df.parent).as_("issingle")
 		standard_fields = (
 			frappe.qb.from_(df)
-			.select(df.parent, df.fieldname, st_issingle)
-			.where((df.options == doctype) & (df.fieldtype == "Link"))
+			.inner_join(dt)
+			.on(df.parent == dt.name)
+			.select(df.parent, df.fieldname, dt.issingle.as_("issingle"))
+			.where((df.options == doctype) & (df.fieldtype == "Link") & (dt.is_virtual == 0))
 			.run(as_dict=True)
 		)
 
