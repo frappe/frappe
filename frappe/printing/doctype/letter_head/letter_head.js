5321e5d23bd842d8f6dcc9ffd9f8037aa72fb3ad
marination <maricadsouza221197@gmail.com>
2023-11-20 17:26:15 +0100

    feat: Include letterhead scripts in print (html)
    - misc: also validate letterhead scripts

--- frappe/printing/doctype/letter_head/letter_head.js
+++ frappe/printing/doctype/letter_head/letter_head.js
@@ -5,4 +5,20 @@ frappe.ui.form.on("Letter Head", {
 	refresh: function (frm) {
 		frm.flag_public_attachments = true;
 	},
+
+	validate: (frm) => {
+		["header_script", "footer_script"].forEach((field) => {
+			if (!frm.doc[field]) return;
+
+			try {
+				eval(frm.doc[field]);
+			} catch (e) {
+				frappe.throw({
+					title: __("Error in Header/Footer Script"),
+					indicator: "orange",
+					message: '<pre class="small"><code>' + e.stack + "</code></pre>",
+				});
+			}
+		});
+	},
 });
