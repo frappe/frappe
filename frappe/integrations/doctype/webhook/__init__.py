973fde23437de8937dc2e8d8e9fbf69ac1260e31
Sagar Vora <sagar@resilient.tech>
2023-11-24 18:29:02 +0530

    fix: avoid caching webhooks twice

--- frappe/integrations/doctype/webhook/__init__.py
+++ frappe/integrations/doctype/webhook/__init__.py
@@ -4,48 +4,50 @@
 import frappe
 
 
+def get_all_webhooks():
+	# query webhooks
+	webhooks_list = frappe.get_all(
+		"Webhook",
+		fields=["name", "condition", "webhook_docevent", "webhook_doctype"],
+		filters={"enabled": True},
+	)
+
+	# make webhooks map
+	webhooks = {}
+	for w in webhooks_list:
+		webhooks.setdefault(w.webhook_doctype, []).append(w)
+
+	return webhooks
+
+
 def run_webhooks(doc, method):
 	"""Run webhooks for this method"""
+
+	frappe_flags = frappe.local.flags
+
 	if (
-		frappe.flags.in_import
-		or frappe.flags.in_patch
-		or frappe.flags.in_install
-		or frappe.flags.in_migrate
+		frappe_flags.in_import
+		or frappe_flags.in_patch
+		or frappe_flags.in_install
+		or frappe_flags.in_migrate
 	):
 		return
 
-	if frappe.flags.webhooks_executed is None:
-		frappe.flags.webhooks_executed = {}
+	if frappe_flags.webhooks_executed is None:
+		frappe_flags.webhooks_executed = {}
 
-	# TODO: remove this hazardous unnecessary cache in flags
-	if frappe.flags.webhooks is None:
-		# load webhooks from cache
-		webhooks = frappe.cache.get_value("webhooks")
-		if webhooks is None:
-			# query webhooks
-			webhooks_list = frappe.get_all(
-				"Webhook",
-				fields=["name", "condition", "webhook_docevent", "webhook_doctype"],
-				filters={"enabled": True},
-			)
-
-			# make webhooks map for cache
-			webhooks = {}
-			for w in webhooks_list:
-				webhooks.setdefault(w.webhook_doctype, []).append(w)
-			frappe.cache.set_value("webhooks", webhooks)
-
-		frappe.flags.webhooks = webhooks
+	# load all webhooks from cache / DB
+	webhooks = frappe.cache.get_value("webhooks", get_all_webhooks)
 
 	# get webhooks for this doctype
-	webhooks_for_doc = frappe.flags.webhooks.get(doc.doctype, None)
+	webhooks_for_doc = webhooks.get(doc.doctype, None)
 
 	if not webhooks_for_doc:
 		# no webhooks, quit
 		return
 
 	def _webhook_request(webhook):
-		if webhook.name not in frappe.flags.webhooks_executed.get(doc.name, []):
+		if webhook.name not in frappe_flags.webhooks_executed.get(doc.name, []):
 			frappe.enqueue(
 				"frappe.integrations.doctype.webhook.webhook.enqueue_webhook",
 				enqueue_after_commit=True,
@@ -56,7 +58,7 @@ def run_webhooks(doc, method):
 			# keep list of webhooks executed for this doc in this request
 			# so that we don't run the same webhook for the same document multiple times
 			# in one request
-			frappe.flags.webhooks_executed.setdefault(doc.name, []).append(webhook.name)
+			frappe_flags.webhooks_executed.setdefault(doc.name, []).append(webhook.name)
 
 	event_list = ["on_update", "after_insert", "on_submit", "on_cancel", "on_trash"]
 
