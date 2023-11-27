a8b462daa3bca8b77fb14b4df166646e9d8e7cd7
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2023-11-27 20:12:14 +0100

    fix: commit after each write
    Co-authored-by: Ankush Menat <ankushmenat@gmail.com>
    (cherry picked from commit 380a67777a2ef5d05e2fb5944bea3970d0146fed)

--- frappe/patches/v15_0/validate_newsletter_recipients.py
+++ frappe/patches/v15_0/validate_newsletter_recipients.py
@@ -6,3 +6,4 @@ def execute():
 	for name, email in frappe.get_all("Email Group Member", fields=["name", "email"], as_list=True):
 		if not validate_email_address(email, throw=False):
 			frappe.db.set_value("Email Group Member", name, "unsubscribed", 1)
+			frappe.db.commit()
