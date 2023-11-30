82d4c04235b4953646c9bd3cca6b9d8030a5dc1e
Ankush Menat <ankush@frappe.io>
2023-11-30 17:44:51 +0530

    fix: tweak sentry config
    - disable session tracking - This causes sentry to phone home on every load, we don't need this (yet)
    - disable automatic integrtaions (like redis)
    - disable all default integration and cherry-pick what's required
    
    (cherry picked from commit 0e8b4de96fc133ef1c3d226ab8bb94173e9c2eb7)

--- frappe/public/js/sentry.bundle.js
+++ frappe/public/js/sentry.bundle.js
@@ -3,4 +3,9 @@ import * as Sentry from "@sentry/browser";
 Sentry.init({
 	dsn: frappe.boot.sentry_dsn,
 	release: frappe?.boot?.versions?.frappe,
+	autoSessionTracking: false,
+	initialScope: {
+		// don't use frappe.session.user, it's set much later and will fail because of async loading
+		user: { id: frappe.boot.user.name ?? "Unidentified" },
+	},
 });
