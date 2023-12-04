e5fb567873e093bcf63dac1d12b49624d0233f9e
Ankush Menat <ankush@frappe.io>
2023-12-04 18:21:02 +0530

    style: format

--- frappe/desk/page/setup_wizard/setup_wizard.py
+++ frappe/desk/page/setup_wizard/setup_wizard.py
@@ -4,12 +4,12 @@
 import json
 
 import frappe
+from frappe import _
 from frappe.geo.country_info import get_country_info
 from frappe.permissions import AUTOMATIC_ROLES
 from frappe.translate import get_messages_for_boot, send_translations, set_default_language
 from frappe.utils import cint, now, strip
 from frappe.utils.password import update_password
-from frappe import _
 
 from . import install_fixtures
 
