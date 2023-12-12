468f3c6738cb8e2a55f1ab668ab4ff453a1519c3
barredterra <14891507+barredterra@users.noreply.github.com>
2023-12-12 20:06:45 +0100

    feat(Web Template): switch from pure HTML to Jinja highlighting

--- frappe/website/doctype/web_template/web_template.py
+++ frappe/website/doctype/web_template/web_template.py
@@ -27,6 +27,7 @@ class WebTemplate(Document):
 		template: DF.Code | None
 		type: DF.Literal["Component", "Section", "Navbar", "Footer"]
 	# end: auto-generated types
+
 	def validate(self):
 		if self.standard and not frappe.conf.developer_mode and not frappe.flags.in_patch:
 			frappe.throw(_("Enable developer mode to create a standard Web Template"))
