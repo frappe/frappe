8b73517cbde021d957ae3d4f494f161b7efcc579
Corentin Flr <10946971+cogk@users.noreply.github.com>
2023-12-21 18:50:25 +0100

    fix(translations): Add context to Image Cropper text (#23908)
    (cherry picked from commit 1d23888475ff668500a0f7817a1310ae540e396e)
    
    # Conflicts:
    #	frappe/public/js/frappe/file_uploader/ImageCropper.vue

--- frappe/public/js/frappe/file_uploader/ImageCropper.vue
+++ frappe/public/js/frappe/file_uploader/ImageCropper.vue
@@ -40,6 +40,7 @@
 
 <script>
 import Cropper from "cropperjs";
+<<<<<<< HEAD
 export default {
 	name: "ImageCropper",
 	props: ["file", "fixed_aspect_ratio"],
@@ -51,6 +52,89 @@ export default {
 			image: null,
 			aspect_ratio,
 		};
+=======
+
+// props
+const props = defineProps({
+	file: Object,
+	fixed_aspect_ratio: Number,
+});
+
+// emits
+let emit = defineEmits(["toggle_image_cropper"]);
+
+// variables
+let aspect_ratio = ref(props.fixed_aspect_ratio != null ? props.fixed_aspect_ratio : NaN);
+let src = ref(null);
+let cropper = ref(null);
+let image = ref(null);
+let image_ref = ref(null); // Template ref
+
+// methods
+function crop_image() {
+	props.file.crop_box_data = cropper.value.getData();
+	const canvas = cropper.value.getCroppedCanvas();
+	const file_type = props.file.file_obj.type;
+	canvas.toBlob((blob) => {
+		var cropped_file_obj = new File([blob], props.file.name, {
+			type: blob.type,
+		});
+		props.file.file_obj = cropped_file_obj;
+		emit("toggle_image_cropper");
+	}, file_type);
+}
+
+// mounted
+onMounted(() => {
+	if (window.FileReader) {
+		let fr = new FileReader();
+		fr.onload = () => (src.value = fr.result);
+		fr.readAsDataURL(props.file.cropper_file);
+	}
+	let crop_box = props.file.crop_box_data;
+	image.value = image_ref.value;
+	image.value.onload = () => {
+		cropper.value = new Cropper(image.value, {
+			zoomable: false,
+			scalable: false,
+			viewMode: 1,
+			data: crop_box,
+			aspectRatio: aspect_ratio.value,
+		});
+		window.cropper = cropper.value;
+	};
+});
+
+//  computed
+let aspect_ratio_buttons = computed(() => {
+	return [
+		{
+			label: __("1:1", null, "Image Cropper"),
+			value: 1,
+		},
+		{
+			label: __("4:3", null, "Image Cropper"),
+			value: 4 / 3,
+		},
+		{
+			label: __("16:9", null, "Image Cropper"),
+			value: 16 / 9,
+		},
+		{
+			label: __("Free", null, "Image Cropper"),
+			value: NaN,
+		},
+	];
+});
+
+// watcher
+watch(
+	aspect_ratio,
+	(value) => {
+		if (cropper.value) {
+			cropper.value.setAspectRatio(value);
+		}
+>>>>>>> 1d23888475 (fix(translations): Add context to Image Cropper text (#23908))
 	},
 	watch: {
 		aspect_ratio(value) {
