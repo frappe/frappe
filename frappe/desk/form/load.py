afc9fcd75b6ef8b64197968136c8743b3d96ed1d
David Arnold <dgx.arnold@gmail.com>
2023-12-25 13:38:51 +0100

    chore: remove unused communication type
    This was removed and migrated already in v12:
    
    ```
    frappe/patches/v12_0/setup_comments_from_communications.py:	frappe.db.delete("Communication", {"communication_type": "Comment"})
    ```
    ... comming from https://github.com/frappe/frappe/commit/41d90fa6d1fda781f6c96493faa616a63c5e0eb3
    which effectively reverted https://github.com/frappe/frappe/commit/465318878e15b96f4dbeb02c410db19d15fee817

--- frappe/desk/form/load.py
+++ frappe/desk/form/load.py
@@ -278,7 +278,7 @@ def get_communication_data(
 	if not fields:
 		fields = """
 			C.name, C.communication_type, C.communication_medium,
-			C.comment_type, C.communication_date, C.content,
+			C.communication_date, C.content,
 			C.sender, C.sender_full_name, C.cc, C.bcc,
 			C.creation AS creation, C.subject, C.delivery_status,
 			C._liked_by, C.reference_doctype, C.reference_name,
