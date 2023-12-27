275d992731993cbefe4e189f79fe6a31c849aa9b
Ankush Menat <ankush@frappe.io>
2023-12-27 16:45:42 +0530

    test: use doctype that supports prompt naming (#23980)

--- cypress/integration/navigation.js
+++ cypress/integration/navigation.js
@@ -3,14 +3,18 @@ context("Navigation", () => {
 		cy.login();
 	});
 	it("Navigate to route with hash in document name", () => {
-		cy.insert_doc("ToDo", {
-			__newname: "ABC#123",
-			description: "Test this",
-			ignore_duplicate: true,
-		});
-		cy.visit("/app/todo/ABC#123");
-		cy.title().should("eq", "Test this - ABC#123");
-		cy.get_field("description", "Text Editor").contains("Test this");
+		cy.insert_doc(
+			"Client Script",
+			{
+				__newname: "ABC#123",
+				dt: "User",
+				script: "console.log('ran')",
+				enabled: 0,
+			},
+			true
+		);
+		cy.visit(`/app/client-script/${encodeURIComponent("ABC#123")}`);
+		cy.title().should("eq", "ABC#123");
 		cy.go("back");
 		cy.title().should("eq", "Website");
 	});
