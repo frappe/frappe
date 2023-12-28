85bc2a3099fd7af392ce588e8a12dde9ca309406
Ankush Menat <ankush@frappe.io>
2023-12-28 10:44:19 +0530

    fix: Avoid showing exc when traceback is disabled

--- frappe/utils/response.py
+++ frappe/utils/response.py
@@ -57,11 +57,6 @@ def report_error(status_code):
 	response = build_response("json")
 	response.status_code = status_code
 
-	if not allow_traceback:
-		resp_data = response.get_json()
-		resp_data.pop("exc", None)
-		response.data = json.dumps(resp_data).encode("utf-8")
-
 	return response
 
 
@@ -174,8 +169,9 @@ def _make_logs_v1():
 	from frappe.utils.error import guess_exception_source
 
 	response = frappe.local.response
+	allow_traceback = frappe.get_system_settings("allow_error_traceback") if frappe.db else False
 
-	if frappe.error_log:
+	if frappe.error_log and allow_traceback:
 		if source := guess_exception_source(frappe.local.error_log and frappe.local.error_log[0]["exc"]):
 			response["_exc_source"] = source
 		response["exc"] = json.dumps([frappe.utils.cstr(d["exc"]) for d in frappe.local.error_log])
