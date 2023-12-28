1ae79badf761eaab007556e32f71b2463d83a3de
Ankush Menat <ankush@frappe.io>
2023-12-28 18:04:44 +0530

    fix: only set change listener if available

--- frappe/desk/page/setup_wizard/setup_wizard.js
+++ frappe/desk/page/setup_wizard/setup_wizard.js
@@ -350,7 +350,7 @@ frappe.setup.SetupWizardSlide = class SetupWizardSlide extends frappe.ui.Slide {
 		let me = this;
 		this.fields.filter(frappe.model.is_value_type).forEach((field) => {
 			field.fieldname &&
-				me.get_input(field.fieldname)?.on("change", function () {
+				me.get_input(field.fieldname)?.on?.("change", function () {
 					frappe.telemetry.capture(`${field.fieldname}_set`, "setup");
 					if (
 						field.fieldname == "enable_telemetry" &&
