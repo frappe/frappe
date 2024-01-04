edac879d7e2b6061a2f79d423d585c7347327841
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2024-01-04 13:43:12 +0100

    refactor!: remove unused parameter from rebuild_tree, add type hints (#24123)
    * refactor!: remove unused parameter from rebuild_tree, add type hints
    
    * fix: remove parent_field parameter from backend calls

--- frappe/utils/nestedset.py
+++ frappe/utils/nestedset.py
@@ -168,12 +168,8 @@ def update_move_node(doc: Document, parent_field: str):
 
 
 @frappe.whitelist()
-def rebuild_tree(doctype, parent_field=None):
-	"""Call rebuild_node for all root nodes.
-
-	The `parent_field` parameter is ignored and will be removed in v16+ (kept for backward compatibility).
-	"""
-
+def rebuild_tree(doctype: str) -> None:
+	"""Call rebuild_node for all root nodes."""
 	# Check for perm if called from client-side
 	if frappe.request and frappe.local.form_dict.cmd == "rebuild_tree":
 		frappe.only_for("System Manager")
@@ -330,7 +326,7 @@ class NestedSet(Document):
 		)
 
 		if merge:
-			rebuild_tree(self.doctype, parent_field)
+			rebuild_tree(self.doctype)
 
 	def validate_one_root(self):
 		if not self.get(self.nsm_parent_field):
