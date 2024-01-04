c1bff1e3ad04220f3f0637bd49f6733ef3cb4bc2
Ankush Menat <ankush@frappe.io>
2024-01-04 15:43:10 +0530

    refactor: respect multi-tenancy
    redis set/get don't consider site
    
    (cherry picked from commit 71908f1e00459c937af8a2dacc799980c050cf00)
    
    # Conflicts:
    #	frappe/email/doctype/email_account/email_account.py

--- frappe/email/doctype/email_account/email_account.py
+++ frappe/email/doctype/email_account/email_account.py
@@ -433,10 +433,17 @@ class EmailAccount(Document):
 			self.set_failed_attempts_count(self.get_failed_attempts_count() + 1)
 
 	def set_failed_attempts_count(self, value):
+<<<<<<< HEAD
 		frappe.cache().set(f"{self.name}:email-account-failed-attempts", value)
 
 	def get_failed_attempts_count(self):
 		return cint(frappe.cache().get(f"{self.name}:email-account-failed-attempts"))
+=======
+		frappe.cache.set_value(f"{self.name}:email-account-failed-attempts", value)
+
+	def get_failed_attempts_count(self):
+		return cint(frappe.cache.get_value(f"{self.name}:email-account-failed-attempts"))
+>>>>>>> 71908f1e00 (refactor: respect multi-tenancy)
 
 	def receive(self):
 		"""Called by scheduler to receive emails from this EMail account using POP3/IMAP."""
