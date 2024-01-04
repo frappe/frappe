c2bf59a80f5d6d73141c179a7e9e871b8b10b84b
Ankush Menat <ankush@frappe.io>
2024-01-04 15:53:59 +0530

    fix: correct arg type
    assign_to expects a list of strings of users not a single string.
    
    (cherry picked from commit cc2bb854841a1e13d43face6e32d97fab4d4f9bf)
    
    # Conflicts:
    #	frappe/tests/test_email.py

--- frappe/email/doctype/email_account/email_account.py
+++ frappe/email/doctype/email_account/email_account.py
@@ -232,6 +232,8 @@ class EmailAccount(Document):
 				error_message = _(
 					"Authentication failed while receiving emails from Email Account: {0}."
 				).format(self.name)
+
+				error_message = _("Email Account Disabled.") + " " + error_message
 				error_message += "<br>" + _("Message from server: {0}").format(cstr(e))
 				self.handle_incoming_connect_error(description=error_message)
 				return None
@@ -412,7 +414,7 @@ class EmailAccount(Document):
 		return SMTPServer(**config)
 
 	def handle_incoming_connect_error(self, description):
-		if self.get_failed_attempts_count() > 2:
+		if self.get_failed_attempts_count() > 5:
 			# This is done in background to avoid committing here.
 			frappe.enqueue(self._disable_broken_incoming_account, description=description)
 		else:
@@ -425,7 +427,7 @@ class EmailAccount(Document):
 			try:
 				assign_to.add(
 					{
-						"assign_to": user,
+						"assign_to": [user],
 						"doctype": self.doctype,
 						"name": self.name,
 						"description": description,
