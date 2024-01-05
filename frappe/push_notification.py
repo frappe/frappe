51f5f44e316d015e2f1ca25d5fb2d6cd0f6418e1
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-01-05 08:58:16 +0530

    chore: remove commit and move apis to POST

--- frappe/push_notification.py
+++ frappe/push_notification.py
@@ -206,7 +206,6 @@ class PushNotification:
 			notification_settings.api_key = response["credentials"]["api_key"]
 			notification_settings.api_secret = response["credentials"]["api_secret"]
 			notification_settings.save(ignore_permissions=True)
-			frappe.db.commit()
 			return notification_settings.api_key, notification_settings.api_secret
 
 	def _send_post_request(self, method: str, params: dict, use_authentication: bool = True):
@@ -269,13 +268,13 @@ def auth_webhook():
 
 
 # Subscribe and Unsubscribe API
-@frappe.whitelist(methods=["GET"])
+@frappe.whitelist(methods=["POST"])
 def subscribe(fcm_token: str):
 	success, message = PushNotification().add_token(frappe.session.user, fcm_token)
 	return {"success": success, "message": message}
 
 
-@frappe.whitelist(methods=["GET"])
+@frappe.whitelist(methods=["POST"])
 def unsubscribe(fcm_token: str):
 	success, message = PushNotification().remove_token(frappe.session.user, fcm_token)
 	return {"success": success, "message": message}
