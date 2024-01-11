c3d417aa30bc53d946c1f02e087f2c2ee72bc1ed
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-01-11 14:42:38 +0530

    fix(setup wizard): allow setting user password for an existing user
    (cherry picked from commit 49bbd0d1d756d637bb34ed559e576a5c3cf1cde0)

--- frappe/desk/page/setup_wizard/setup_wizard.py
+++ frappe/desk/page/setup_wizard/setup_wizard.py
@@ -110,7 +110,7 @@ def update_global_settings(args):
 	frappe.clear_cache()
 
 	update_system_settings(args)
-	update_user_name(args)
+	create_or_update_user(args)
 	set_timezone(args)
 
 
@@ -193,63 +193,39 @@ def update_system_settings(args):
 		frappe.db.set_default("session_recording_start", now())
 
 
-def update_user_name(args):
+def create_or_update_user(args):  # nosemgrep
+	email = args.get("email")
 	first_name, last_name = args.get("full_name", ""), ""
 	if " " in first_name:
 		first_name, last_name = first_name.split(" ", 1)
 
-	if args.get("email"):
-		if frappe.db.exists("User", args.get("email")):
-			# running again
-			return
-
-		args["name"] = args.get("email")
-
+	if user := frappe.db.get_value("User", email, ["first_name", "last_name"], as_dict=True):
+		if user.first_name != first_name or user.last_name != last_name:
+			(
+				frappe.qb.update("User")
+				.set("first_name", first_name)
+				.set("last_name", last_name)
+				.set("full_name", args.get("full_name"))
+			).run()
+	else:
 		_mute_emails, frappe.flags.mute_emails = frappe.flags.mute_emails, True
-		doc = frappe.get_doc(
+
+		user = frappe.new_doc("User")
+		user.update(
 			{
-				"doctype": "User",
-				"email": args.get("email"),
+				"email": email,
 				"first_name": first_name,
 				"last_name": last_name,
 			}
 		)
+		user.append_roles(*_get_default_roles())
+		user.flags.no_welcome_mail = True
+		user.insert()
 
-		doc.append_roles(*_get_default_roles())
-		doc.flags.no_welcome_mail = True
-		doc.insert()
 		frappe.flags.mute_emails = _mute_emails
-		update_password(args.get("email"), args.get("password"))
 
-	elif first_name:
-		args.update({"name": frappe.session.user, "first_name": first_name, "last_name": last_name})
-
-		frappe.db.sql(
-			"""update `tabUser` SET first_name=%(first_name)s,
-			last_name=%(last_name)s WHERE name=%(name)s""",
-			args,
-		)
-
-	if args.get("attach_user"):
-		attach_user = args.get("attach_user").split(",")
-		if len(attach_user) == 3:
-			filename, filetype, content = attach_user
-			_file = frappe.get_doc(
-				{
-					"doctype": "File",
-					"file_name": filename,
-					"attached_to_doctype": "User",
-					"attached_to_name": args.get("name"),
-					"content": content,
-					"decode": True,
-				}
-			)
-			_file.save()
-			fileurl = _file.file_url
-			frappe.db.set_value("User", args.get("name"), "user_image", fileurl)
-
-	if args.get("name"):
-		add_all_roles_to(args.get("name"))
+	if args.get("password"):
+		update_password(email, args.get("password"))
 
 
 def set_timezone(args):
