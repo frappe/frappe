9c91f79170d0eb703be42660f51e8f8f9bd9a878
Sagar Vora <sagar@resilient.tech>
2024-01-12 21:07:31 +0530

    fix: sanitize html instead of escaping when creating/updating workspace (#24284)

--- frappe/public/js/frappe/views/workspace/workspace.js
+++ frappe/public/js/frappe/views/workspace/workspace.js
@@ -646,7 +646,7 @@ frappe.views.Workspace = class Workspace {
 			],
 			primary_action_label: __("Update"),
 			primary_action: (values) => {
-				values.title = frappe.utils.escape_html(values.title);
+				values.title = strip_html(values.title);
 				let is_title_changed = values.title != old_item.title;
 				let is_section_changed = Boolean(values.is_public) != Boolean(old_item.public);
 				if (
@@ -1216,7 +1216,7 @@ frappe.views.Workspace = class Workspace {
 			],
 			primary_action_label: __("Create"),
 			primary_action: (values) => {
-				values.title = frappe.utils.escape_html(values.title);
+				values.title = strip_html(values.title);
 				if (!this.validate_page(values)) return;
 				d.hide();
 				this.initialize_editorjs_undo();
