dcb12c89eda207f39d2a4f242dc9c27e9a4ed2d2
Govind Jangid <govindjangidsj@gmail.com>
2024-01-28 22:19:18 +0530

    feat: use_first_day_of_period

--- frappe/email/doctype/auto_email_report/auto_email_report.py
+++ frappe/email/doctype/auto_email_report/auto_email_report.py
@@ -3,6 +3,7 @@
 
 import calendar
 from datetime import timedelta
+import datetime
 
 import frappe
 from frappe import _
@@ -20,6 +21,11 @@ from frappe.utils import (
 	now_datetime,
 	today,
 	validate_email_address,
+	get_first_day_of_week,
+	get_first_day,
+	get_quarter_start,
+	get_year_start,
+	getdate,
 )
 from frappe.utils.csvutils import to_csv
 from frappe.utils.xlsxutils import make_xlsx
@@ -166,22 +172,50 @@ class AutoEmailReport(Document):
 
 	def get_file_name(self):
 		return "{}.{}".format(self.report.replace(" ", "-").replace("/", "-"), self.format.lower())
-
+	
 	def prepare_dynamic_filters(self):
 		self.filters = frappe.parse_json(self.filters)
 
 		to_date = today()
-		from_date_value = {
-			"Daily": ("days", -1),
-			"Weekly": ("weeks", -1),
-			"Monthly": ("months", -1),
-			"Quarterly": ("months", -3),
-			"Half Yearly": ("months", -6),
-			"Yearly": ("years", -1),
-		}[self.dynamic_date_period]
+		
+		if self.use_first_day_of_period:
+			from_date_value = {
+				"Daily": ("days", -1),
+				"Weekly": ("weeks", 0),
+				"Monthly": ("months", 0),
+				"Quarterly": ("months", 0),
+				"Half Yearly": ("months", -6),
+				"Yearly": ("years", 0),
+			}[self.dynamic_date_period]
 
-		from_date = add_to_date(to_date, **{from_date_value[0]: from_date_value[1]})
+			from_date = add_to_date(to_date, **{from_date_value[0]: from_date_value[1]})
+			
+			if self.dynamic_date_period == "Weekly":
+				from_date = get_first_day_of_week(from_date)
+			elif self.dynamic_date_period == "Monthly":
+				from_date = get_first_day(from_date)
+			elif self.dynamic_date_period == "Quarterly":
+				from_date = get_quarter_start(from_date)
+			elif self.dynamic_date_period == "Half Yearly":
+				from_date = get_first_day_of_half_year(from_date)
+			elif self.dynamic_date_period == "Yearly":
+				from_date = get_year_start(from_date)
 
+			self.set_date_filters(from_date, to_date)
+		else:
+			from_date_value = {
+				"Daily": ("days", -1),
+				"Weekly": ("weeks", -1),
+				"Monthly": ("months", -1),
+				"Quarterly": ("months", -3),
+				"Half Yearly": ("months", -6),
+				"Yearly": ("years", -1),
+			}[self.dynamic_date_period]
+
+			from_date = add_to_date(to_date, **{from_date_value[0]: from_date_value[1]})
+			self.set_date_filters(from_date, to_date)
+		
+	def set_date_filters(self, from_date, to_date):
 		self.filters[self.from_date_field] = from_date
 		self.filters[self.to_date_field] = to_date
 
@@ -295,3 +329,22 @@ def update_field_types(columns):
 			col.fieldtype = "Data"
 			col.options = ""
 	return columns
+
+DATE_FORMAT = "%Y-%m-%d"
+def get_first_day_of_half_year(as_str=False):
+    """
+    Returns the first day of the current half-year based on the current date.
+    """
+    today_date = getdate(today())
+
+    # Determine the half-year (1st or 2nd half)
+    half_year = 1 if today_date.month <= 6 else 2
+
+    # Calculate the first day of the half-year
+    year = today_date.year if half_year == 1 else today_date.year + 1
+    month = 1 if half_year == 1 else 7
+    day = 1
+
+    result_date = datetime.date(year, month, day)
+
+    return result_date if not as_str else result_date.strftime(DATE_FORMAT)
