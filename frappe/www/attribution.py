82ddd385ba8d25aa37e7f6ca3258411c0425a47d
barredterra <14891507+barredterra@users.noreply.github.com>
2024-01-29 17:11:43 +0100

    refactor: async load info in frontend

--- frappe/www/attribution.py
+++ frappe/www/attribution.py
@@ -1,83 +1,67 @@
 import json
 import re
 import tomllib
-
-import requests
+from pathlib import Path
 
 import frappe
-from frappe.utils.caching import redis_cache
 
 
 def get_context(context):
 	if frappe.session.user == "Guest":
 		frappe.throw("You need to be logged in to access this page.", frappe.PermissionError)
 
-	packages_by_app = {}
+	apps = []
 	for app in frappe.get_installed_apps():
-		packages_by_app[app] = get_app_deps(app)
+		app_info = get_app_info(app)
+		if any([app_info.get("authors"), app_info.get("dependencies"), app_info.get("description")]):
+			apps.append(app_info)
 
-	context.packages_by_app = packages_by_app
+	context.apps = apps
 
 
-@redis_cache(ttl=60 * 60 * 24 * 7)
-def get_app_deps(app: str):
-	dependencies = []
-
+def get_app_info(app: str):
 	app_info = get_pyproject_info(app)
+	result = {
+		"name": app,
+		"description": app_info.get("description", ""),
+		"authors": ", ".join([a.get("name", "") for a in app_info.get("authors", [])]),
+		"dependencies": [],
+	}
+
 	for requirement in app_info.get("dependencies", []):
-		name, version = parse_pip_requirement(requirement)
-		info = get_py_registry_info(name)
-		info["name"] = name
-		info["version"] = version
-		dependencies.append(info)
+		name = parse_pip_requirement(requirement)
+		result["dependencies"].append({"name": name, "type": "Python"})
 
-	for name, version in get_js_deps(app).items():
-		info = get_js_registry_info(name)
-		info["name"] = name
-		info["version"] = version
-		dependencies.append(info)
+	result["dependencies"].extend(get_js_deps(app))
 
-	return dependencies
+	return result
 
 
-def get_js_deps(app: str):
-	package_json = frappe.get_app_path(app, "..", "package.json")
+def get_js_deps(app: str) -> list[dict]:
+	package_json = Path(frappe.get_app_path(app, "..", "package.json"))
+	if not package_json.exists():
+		return {}
+
 	with open(package_json) as f:
 		package = json.load(f)
 
-	return package.get("dependencies", {})
+	packages = package.get("dependencies", {}).keys()
+	return [{"name": name, "type": "JavaScript"} for name in packages]
 
 
-def get_js_registry_info(package):
-	registry_url = f"https://registry.npmjs.org/{package}"
-	registry_info = requests.get(registry_url).json()
-	return {
-		"license": registry_info.get("license"),
-		"author": registry_info.get("author", {}).get("name"),
-		"homepage": registry_info.get("homepage"),
-	}
+def get_pyproject_info(app: str) -> dict:
+	pyproject_toml = Path(frappe.get_app_path(app, "..", "pyproject.toml"))
+	if not pyproject_toml.exists():
+		return {}
 
-
-def get_pyproject_info(app: str) -> list[str]:
-	pyproject_toml = frappe.get_app_path(app, "..", "pyproject.toml")
 	with open(pyproject_toml, "rb") as f:
 		pyproject = tomllib.load(f)
 
 	return pyproject.get("project", {})
 
 
-def get_py_registry_info(package):
-	registry_url = f"https://pypi.org/pypi/{package}/json"
-	registry_info = requests.get(registry_url).json().get("info", {})
-	return {
-		"license": registry_info.get("license"),
-		"author": registry_info.get("author"),
-		"homepage": registry_info.get("home_page"),
-	}
-
-
-def parse_pip_requirement(requirement: str) -> tuple[str, str]:
+def parse_pip_requirement(requirement: str) -> str:
 	"""Parse pip requirement string to package name and version"""
 	match = re.match(r"^([A-Za-z0-9_\-\[\]]+)(.*)$", requirement)
 
-	return (match[1], match[2]) if match else (requirement, "")
+	return match[1] if match else requirement
