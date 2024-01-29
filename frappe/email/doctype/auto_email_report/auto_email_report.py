1fcb10506d9dc200e4c83993d2cc8fa143f8d057
Govind Jangid <govindjangidsj@gmail.com>
2024-01-29 12:17:58 +0530

    feat: use_first_day_of_period
    (cherry picked from commit 0c6c0c5f50ed894082b8cb37d9cee919f9fa2511)

--- frappe/email/doctype/auto_email_report/auto_email_report.py
+++ frappe/email/doctype/auto_email_report/auto_email_report.py
@@ -4,6 +4,7 @@
 import calendar
 from datetime import timedelta
 from email.utils import formataddr
+import datetime
 
 import frappe
 from frappe import _
@@ -21,6 +22,11 @@ from frappe.utils import (
 	now_datetime,
 	today,
 	validate_email_address,
+	get_first_day_of_week,
+	get_first_day,
+	get_quarter_start,
+	get_year_start,
+	getdate,
 )
 from frappe.utils.csvutils import to_csv
 from frappe.utils.xlsxutils import make_xlsx
@@ -36,13 +42,9 @@ class AutoEmailReport(Document):
 		from frappe.types import DF
 
 		data_modified_till: DF.Int
-		day_of_week: DF.Literal[
-			"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
-		]
+		day_of_week: DF.Literal["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 		description: DF.TextEditor | None
-		dynamic_date_period: DF.Literal[
-			"", "Daily", "Weekly", "Monthly", "Quarterly", "Half Yearly", "Yearly"
-		]
+		dynamic_date_period: DF.Literal["", "Daily", "Weekly", "Monthly", "Quarterly", "Half Yearly", "Yearly"]
 		email_to: DF.SmallText
 		enabled: DF.Check
 		filter_meta: DF.Text | None
@@ -57,8 +59,10 @@ class AutoEmailReport(Document):
 		send_if_data: DF.Check
 		sender: DF.Link | None
 		to_date_field: DF.Literal
+		use_first_day_of_period: DF.Check
 		user: DF.Link
 	# end: auto-generated types
+
 	def autoname(self):
 		self.name = _(self.report)
 		if frappe.db.exists("Auto Email Report", self.name):
@@ -207,17 +211,45 @@ class AutoEmailReport(Document):
 		self.filters = frappe.parse_json(self.filters)
 
 		to_date = today()
-		from_date_value = {
-			"Daily": ("days", -1),
-			"Weekly": ("weeks", -1),
-			"Monthly": ("months", -1),
-			"Quarterly": ("months", -3),
-			"Half Yearly": ("months", -6),
-			"Yearly": ("years", -1),
-		}[self.dynamic_date_period]
+		
+		if self.use_first_day_of_period:
+			from_date_value = {
+				"Daily": ("days", -1),
+				"Weekly": ("weeks", 0),
+				"Monthly": ("months", 0),
+				"Quarterly": ("months", 0),
+				"Half Yearly": ("months", 0),
+				"Yearly": ("years", 0),
+			}[self.dynamic_date_period]
 
-		from_date = add_to_date(to_date, **{from_date_value[0]: from_date_value[1]})
+			from_date = add_to_date(to_date, **{from_date_value[0]: from_date_value[1]})
+			
+			if self.dynamic_date_period == "Weekly":
+				from_date = get_first_day_of_week(from_date)
+			elif self.dynamic_date_period == "Monthly":
+				from_date = get_first_day(from_date)
+			elif self.dynamic_date_period == "Quarterly":
+				from_date = get_quarter_start(from_date)
+			elif self.dynamic_date_period == "Half Yearly":
+				from_date = get_half_year_start(from_date)
+			elif self.dynamic_date_period == "Yearly":
+				from_date = get_year_start(from_date)
 
+			self.set_date_filters(from_date, to_date)
+		else:
+			from_date_value = {
+				"Daily": ("days", -1),
+				"Weekly": ("weeks", -1),
+				"Monthly": ("months", -1),
+				"Quarterly": ("months", -3),
+				"Half Yearly": ("months", -6),
+				"Yearly": ("years", -1),
+			}[self.dynamic_date_period]
+
+			from_date = add_to_date(to_date, **{from_date_value[0]: from_date_value[1]})
+			self.set_date_filters(from_date, to_date)
+		
+	def set_date_filters(self, from_date, to_date):
 		self.filters[self.from_date_field] = from_date
 		self.filters[self.to_date_field] = to_date
 
@@ -332,3 +364,20 @@ def update_field_types(columns):
 			col.fieldtype = "Data"
 			col.options = ""
 	return columns
+
+DATE_FORMAT = "%Y-%m-%d"
+def get_half_year_start(as_str=False):
+    """
+    Returns the first day of the current half-year based on the current date.
+    """
+    today_date = getdate(today())
+
+    half_year = 1 if today_date.month <= 6 else 2
+
+    year = today_date.year if half_year == 1 else today_date.year + 1
+    month = 1 if half_year == 1 else 7
+    day = 1
+
+    result_date = datetime.date(year, month, day)
+
+    return result_date if not as_str else result_date.strftime(DATE_FORMAT)
