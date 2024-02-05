57b2c36e543a27bc5fefb4128208ecd0c4977527
Ankush Menat <ankush@frappe.io>
2024-02-05 19:41:46 +0530

    fix(UX): improve server script message

--- frappe/core/doctype/server_script/server_script.js
+++ frappe/core/doctype/server_script/server_script.js
@@ -28,9 +28,13 @@ frappe.ui.form.on("Server Script", {
 	check_safe_exec(frm) {
 		frappe.xcall("frappe.core.doctype.server_script.server_script.enabled").then((enabled) => {
 			if (enabled === false) {
+				let docs_link =
+					"https://frappeframework.com/docs/user/en/desk/scripting/server-script";
+				let docs = `<a href=${docs_link}>${__("Official Documentation")}</a>`;
+
 				frm.dashboard.clear_comment();
 				let msg = __("Server Scripts feature is not available on this site.") + " ";
-				msg += __("Please contact your system administrator to enable this feature.");
+				msg += __("To enable server scripts, read the {0}.", [docs]);
 				frm.dashboard.add_comment(msg, "yellow", true);
 			}
 		});
