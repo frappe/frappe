83b99f1066c80d037fcf96102ce42341dcdb0168
14987 <nirajgautam196@gmail.com>
2024-02-06 11:29:35 +0530

    fix(patch): Use bulk insert child records

--- frappe/patches/v15_0/migrate_role_profile_to_table_multi_select.py
+++ frappe/patches/v15_0/migrate_role_profile_to_table_multi_select.py
@@ -1,13 +1,27 @@
 import frappe
+from frappe.model.document import bulk_insert
 
 
 def execute():
-	user = frappe.qb.DocType("User")
-	role_profiles = frappe.qb.from_(user).select(user.name, user.role_profile_name).run(as_dict=True)
-	for user in role_profiles:
-		if not user.role_profile_name:
-			continue
-		user_role_profile = [{"role_profile": user.role_profile_name}]
-		user = frappe.get_doc("User", user.name)
-		user.update({"role_profiles": user_role_profile})
-		user.save()
+	users = frappe.get_list(
+		"User", filters={"role_profile_name": ["is", "set"]}, fields=["name", "role_profile_name"]
+	)
+	user_profiles = get_records_to_insert(users)
+	bulk_insert("User Role Profile", user_profiles)
+
+
+def get_records_to_insert(users):
+	user_profiles = []
+	for user in users:
+		profiles = frappe.get_doc(
+			{
+				"doctype": "User Role Profile",
+				"role_profile": user.role_profile_name,
+				"parent": user.name,
+				"parenttype": "User",
+				"parentfield": "role_profiles",
+			}
+		)
+		profiles.set_new_name()
+		user_profiles.append(profiles)
+	return user_profiles
