6f8df344953f366c8b9f02ce0f3529dd753ea9ca
RonUlitsky <106537439+RonUlitsky@users.noreply.github.com>
2024-02-13 14:10:39 -0500

    passed options as its own argument

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -1029,8 +1029,8 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 				formatTooltipY: (d) =>
 					frappe.format(d, {
 						fieldtype: options.fieldtype,
-						options: options.options,
-					}),
+					}, 
+						options.options),
 			};
 		}
 		options.axisOptions = {
