a1dcfc677d55698b7276d8675f62b666f34dade0
Devin Slauenwhite <devin.slauenwhite@gmail.com>
2024-02-14 09:54:52 -0500

    fix: replace $.extend with spread operator
    Co-authored-by: Raffael Meyer <14891507+barredterra@users.noreply.github.com>
    (cherry picked from commit c2eff72f022ef20567ca8895d3dbe53a52418357)

--- frappe/public/js/frappe/form/grid.js
+++ frappe/public/js/frappe/form/grid.js
@@ -833,9 +833,7 @@ export default class Grid {
 					acc[d.fieldname] = d.default;
 					return acc;
 				}, {});
-				this.df.data.push(
-					$.extend({ idx: this.df.data.length + 1, __islocal: true }, defaults)
-				);
+				this.df.data.push({ idx: this.df.data.length + 1, __islocal: true, ...defaults});
 				this.refresh();
 			}
 
