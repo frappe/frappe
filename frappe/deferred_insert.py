a4fa05705df68259d1f7b99f76885ab66d75839a
Rehan Ansari <110723484+rehanansari26@users.noreply.github.com>
2024-02-18 13:13:03 +0530

    fix: Batch in save_to_db()

--- frappe/deferred_insert.py
+++ frappe/deferred_insert.py
@@ -26,21 +26,26 @@ def deferred_insert(doctype: str, records: list[Union[dict, "Document"]] | str):
 
 
 def save_to_db():
-	queue_keys = frappe.cache.get_keys(queue_prefix)
-	for key in queue_keys:
-		record_count = 0
-		queue_key = get_key_name(key)
-		doctype = get_doctype_name(key)
-		while frappe.cache.llen(queue_key) > 0 and record_count <= 500:
-			records = frappe.cache.lpop(queue_key)
-			records = json.loads(records.decode("utf-8"))
-			if isinstance(records, dict):
-				record_count += 1
-				insert_record(records, doctype)
-				continue
-			for record in records:
-				record_count += 1
-				insert_record(record, doctype)
+    queue_keys = frappe.cache.get_keys(queue_prefix)
+    batch_size = 500
+
+    for key in queue_keys:
+        queue_key = get_key_name(key)
+        doctype = get_doctype_name(key)
+
+        while True:
+            records_batch = [frappe.cache.lpop(queue_key) for _ in range(batch_size)]
+            records_batch = [json.loads(record.decode("utf-8")) for record in records_batch if record]
+
+            if not records_batch:
+                break
+
+            for records in records_batch:
+                if isinstance(records, dict):
+                    insert_record(records, doctype)
+                else:
+                    for record in records:
+                        insert_record(record, doctype)
 
 
 def insert_record(record: Union[dict, "Document"], doctype: str):
