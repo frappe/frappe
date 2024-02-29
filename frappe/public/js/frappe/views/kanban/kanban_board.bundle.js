e634c0f34e5a269622ffe43980e36491a29ee75c
andres santos <andresrodriguezsantos44@gmail.com>
2024-02-29 15:23:06 +0000

    logs deleted

--- frappe/public/js/frappe/views/kanban/kanban_board.bundle.js
+++ frappe/public/js/frappe/views/kanban/kanban_board.bundle.js
@@ -866,7 +866,6 @@ frappe.provide("frappe.views");
 		function getLoanCarIcons(){
 			let html = "";
 			if(!card.doc.is_loan_car || card.doc.is_loan_car === "No" || card.doc.is_loan_car === "Car returned"){
-				console.log("-----> card.doc.loan_car: ",card.doc.loan_car);
 				html = `<svg xmlns="http://www.w3.org/2000/svg" class="is_loan_car_no" height="1em" viewBox="0 0 512 512"><title>${card.doc.loan_car ?? ''}</title> <style>svg{fill:#d1d1d1}</style><path d="M192 176c0-79.5 64.5-144 144-144s144 64.5 144 144s-64.5 144-144 144c-11.1 0-22-1.3-32.4-3.6c-5.4-1.2-11 .4-14.9 4.3L257.4 352H208c-8.8 0-16 7.2-16 16v48H144c-8.8 0-16 7.2-16 16v48H32V398.6L194.8 235.8c4.2-4.2 5.7-10.5 3.9-16.2c-4.4-13.8-6.7-28.4-6.7-43.7zM336 0C238.8 0 160 78.8 160 176c0 15.1 1.9 29.8 5.5 43.9L4.7 380.7c-3 3-4.7 7.1-4.7 11.3V496c0 8.8 7.2 16 16 16H144c8.8 0 16-7.2 16-16V448h48c8.8 0 16-7.2 16-16V384h40c4.2 0 8.3-1.7 11.3-4.7l30-30c10 1.8 20.2 2.7 30.7 2.7c97.2 0 176-78.8 176-176S433.2 0 336 0zm32 168a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg>`;
 			}
 			else if(card.doc.is_loan_car === "Yes"){
