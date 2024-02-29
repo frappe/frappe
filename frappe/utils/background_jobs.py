f7bff5893554ca9c714c9d2533b2b575ef510e81
Ankush Menat <ankush@frappe.io>
2024-02-29 20:08:48 +0530

    fix: use name for RQ worker instead of PID (#25175)
    Also avoid complex naming schemes.

--- frappe/utils/background_jobs.py
+++ frappe/utils/background_jobs.py
@@ -291,7 +291,6 @@ def start_worker(
 		if queue:
 			queue = [q.strip() for q in queue.split(",")]
 		queues = get_queue_list(queue, build_queue_name=True)
-		queue_name = queue and generate_qname(queue)
 
 	if os.environ.get("CI"):
 		setup_loghandlers("ERROR")
@@ -302,7 +301,7 @@ def start_worker(
 	if quiet:
 		logging_level = "WARNING"
 
-	worker = Worker(queues, name=get_worker_name(queue_name), connection=redis_connection)
+	worker = Worker(queues, connection=redis_connection)
 	worker.work(
 		logging_level=logging_level,
 		burst=burst,
