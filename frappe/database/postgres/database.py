99171530b9ae9020ce70002f6e304ea9ee788150
Ankush Menat <ankush@frappe.io>
2024-03-03 17:17:12 +0530

    fix: rollback invalid customize form changes (#25198)
    * fix: rollback invalid customize form changes
    
    * fix: commit before doing DDL
    
    (cherry picked from commit b60c64cacc19b637c203f8460bfe3a12ab5d44af)
    (cherry picked from commit d6bff86205ca40f79c583c5c49c55670a8cc7dca)

--- frappe/database/postgres/database.py
+++ frappe/database/postgres/database.py
@@ -319,7 +319,6 @@ class PostgresDatabase(PostgresExceptionUtil, Database):
 			db_table = PostgresTable(doctype, meta)
 			db_table.validate()
 
-			self.commit()
 			db_table.sync()
 			self.begin()
 
