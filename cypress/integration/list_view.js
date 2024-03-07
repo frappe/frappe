7136a46a32f0d947d1b782eaad9743e98e20d2dd
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2024-03-07 19:38:35 +0530

    fix: tests again

--- cypress/integration/list_view.js
+++ cypress/integration/list_view.js
@@ -13,7 +13,7 @@ context("List View", () => {
 	it("Keep checkbox checked after Refresh", { scrollBehavior: false }, () => {
 		cy.go_to_list("ToDo");
 		cy.clear_filters();
-		cy.get(".select-like > .list-check-all").click();
+		cy.get(".list-header-subject > .list-subject > .list-check-all").click();
 		cy.get("button[data-original-title='Reload List']").click();
 		cy.get(".list-row-container .list-row-checkbox:checked").should("be.visible");
 	});
@@ -33,7 +33,7 @@ context("List View", () => {
 		];
 		cy.go_to_list("ToDo");
 		cy.clear_filters();
-		cy.get(".list-header-subject > .list-subject > .list-check-all").click();
+		cy.get(".select-like > .list-check-all").click();
 		cy.findByRole("button", { name: "Actions" }).click();
 		cy.get(".dropdown-menu li:visible .dropdown-item")
 			.should("have.length", 10)
