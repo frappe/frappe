939ea489028173a98f0b6fa22288d8a7fa31ec30
barredterra <14891507+barredterra@users.noreply.github.com>
2024-03-10 19:14:50 +0100

    refactor: get_config

--- frappe/desk/doctype/dashboard_chart_source/dashboard_chart_source.py
+++ frappe/desk/doctype/dashboard_chart_source/dashboard_chart_source.py
@@ -15,9 +15,8 @@ FOLDER_NAME = "dashboard_chart_source"
 
 @frappe.whitelist()
 def get_config(name: str) -> str:
-	module, name = frappe.get_value("Dashboard Chart Source", name, ["module", "name"])
-	file = get_folder_path(module, name) / f"{scrub(name)}.js"
-	return file.read_text()
+	doc: "DashboardChartSource" = frappe.get_doc("Dashboard Chart Source", name)
+	return doc.read_config()
 
 
 class DashboardChartSource(Document):
@@ -44,12 +43,19 @@ class DashboardChartSource(Document):
 		if not frappe.conf.developer_mode and not frappe.flags.in_migrate:
 			frappe.throw(_("Deletion of this document is only permitted in developer mode."))
 
-		frappe.db.after_commit.add(self.delete_folder_with_contents)
+		frappe.db.after_commit.add(self.delete_folder)
 
-	def delete_folder_with_contents(self):
+	def read_config(self) -> str:
+		"""Return the config JS file for this dashboard chart source."""
+		config_path = self.get_folder_path() / f"{scrub(self.name)}.js"
+		return config_path.read_text() if config_path.exists() else ""
+
+	def delete_folder(self):
+		"""Delete the folder for this dashboard chart source."""
 		path = self.get_folder_path()
 		if path.exists():
 			shutil.rmtree(path, ignore_errors=True)
 
 	def get_folder_path(self) -> Path:
+		"""Return the path of the folder for this dashboard chart source."""
 		return Path(get_module_path(self.module)) / FOLDER_NAME / frappe.scrub(self.name)
