50f4f9a07a012b84a959c4f99175f3756c74ae25
Ankush Menat <ankush@frappe.io>
2024-03-11 21:41:29 +0530

    ci: merge install script

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -2,6 +2,19 @@
 set -e
 cd ~ || exit
 
+
+echo "Setting Up System Dependencies..."
+
+sudo apt update
+sudo apt remove mysql-server mysql-client
+sudo apt install libcups2-dev redis-server mariadb-client-10.6
+
+install_wkhtmltopdf() {
+  wget -q https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
+  sudo apt install ./wkhtmltox_0.12.6-1.focal_amd64.deb
+}
+install_wkhtmltopdf &
+
 echo "Setting Up Bench..."
 
 pip install frappe-bench
