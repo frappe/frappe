e65c4d2778e4308304bf42d763e3b01f94fc3e97
Ankush Menat <ankush@frappe.io>
2024-03-11 21:42:10 +0530

    ci: use apt-get
    apt doesn't have stable CLI

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -3,17 +3,15 @@ set -e
 cd ~ || exit
 
 
-echo "Setting Up System Dependencies..."
-
-sudo apt update
-sudo apt remove mysql-server mysql-client
-sudo apt install libcups2-dev redis-server mariadb-client-10.6
-
-install_wkhtmltopdf() {
+install_apt_dependencies() {
+  echo "Setting Up System Dependencies..."
+  sudo apt-get update -qq
+  sudo apt-get remove -qq mysql-server mysql-client
+  sudo apt-get install -qq libcups2-dev redis-server mariadb-client-10.6
   wget -q https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
-  sudo apt install ./wkhtmltox_0.12.6-1.focal_amd64.deb
+  sudo dpkg -i install ./wkhtmltox_0.12.6-1.focal_amd64.deb
 }
-install_wkhtmltopdf &
+install_apt_dependencies &
 
 echo "Setting Up Bench..."
 
