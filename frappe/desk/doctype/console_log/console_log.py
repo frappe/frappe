89e75e372b8ac4679e1d126b2a573d984d544c00
Ankush Menat <ankush@frappe.io>
2024-03-12 20:36:07 +0530

    fix!: console logs can't be deleted
    (cherry picked from commit 54a2b0827813074383a431d7a10ac6ab070636e6)
    
    # Conflicts:
    #	frappe/desk/doctype/console_log/console_log.json
    #	frappe/desk/doctype/console_log/console_log.py

--- frappe/desk/doctype/console_log/console_log.py
+++ frappe/desk/doctype/console_log/console_log.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2020, Frappe Technologies and contributors
 # License: MIT. See LICENSE
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ConsoleLog(Document):
+<<<<<<< HEAD
 	pass
+=======
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		committed: DF.Check
+		script: DF.Code | None
+		type: DF.Data | None
+	# end: auto-generated types
+
+	def after_delete(self):
+		# because on_trash can be bypassed
+		frappe.throw(frappe._("Console Logs can not be deleted"))
+>>>>>>> 54a2b08278 (fix!: console logs can't be deleted)
