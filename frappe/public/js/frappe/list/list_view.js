28a320c86e1a918db15a4441ef52c04a2017a2c1
Ankush Menat <ankush@frappe.io>
2024-03-12 15:39:14 +0530

    fix: dont show tooltip if already limited

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -623,7 +623,7 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 		let $count = this.get_count_element();
 		this.get_count_str().then((count) => {
 			$count.html(`<span>${count}</span>`);
-			if (this.count_upper_bound) {
+			if (this.count_upper_bound && this.count_upper_bound == this.total_count) {
 				$count.attr(
 					"title",
 					__(
