d47b133bc5e590b7749b8430bd1875103966c253
Corentin Flr <10946971+cogk@users.noreply.github.com>
2024-03-15 13:52:38 +0100

    test(format): Test Duration Python formatter

--- frappe/tests/test_formatter.py
+++ frappe/tests/test_formatter.py
@@ -17,3 +17,15 @@ class TestFormatter(FrappeTestCase):
 		self.assertEqual(format(100000, df, doc, format="#,###.##"), "$ 100,000.00")
 
 		frappe.db.set_default("currency", None)
+
+	def test_duration_formatting(self):
+		self.assertEqual(format(1, "Duration"), "1s")
+		self.assertEqual(format(60, "Duration"), "1m")
+		self.assertEqual(format(3600, "Duration"), "1h")
+		self.assertEqual(format(24 * 3600, "Duration"), "1d")
+
+		try:
+			frappe.local.lang = "fr"
+			self.assertEqual(format(24 * 3600, "Duration"), "1j")
+		finally:
+			frappe.local.lang = "en"
