37ed86eb554b6cddf296d6c0f30b9e5a5dd9cb47
Fisher Yu <12823863+szufisher@users.noreply.github.com>
2024-03-15 22:29:34 +0800

    Update Tabs.vue

--- frappe/public/js/form_builder/components/Tabs.vue
+++ frappe/public/js/form_builder/components/Tabs.vue
@@ -131,7 +131,7 @@ function delete_tab(tab, with_children) {
 					@dragover="drag_over(element)"
 				>
 					<EditableInput
-						:text="element.df.label"
+						:text="__(element.df.label)"
 						:placeholder="__('Tab Label')"
 						v-model="element.df.label"
 					/>
