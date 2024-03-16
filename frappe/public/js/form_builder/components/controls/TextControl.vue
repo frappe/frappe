b498b994d033ac5c9d3af3bd31a58f582922fb87
Fisher Yu <12823863+szufisher@users.noreply.github.com>
2024-03-16 10:14:08 +0800

    Update TextControl.vue

--- frappe/public/js/form_builder/components/controls/TextControl.vue
+++ frappe/public/js/form_builder/components/controls/TextControl.vue
@@ -42,6 +42,6 @@ let height = computed(() => {
 		/>
 
 		<!-- description -->
-		<div v-if="df.description" class="mt-2 description" v-html="__(df.description)"></div>
+		<div v-if="df.description" class="mt-2 description" v-html="df.description"></div>
 	</div>
 </template>
