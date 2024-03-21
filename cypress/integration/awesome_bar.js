605b5e9c97c807a6554e838cce67d1ca8b561a25
Ankush Menat <ankush@frappe.io>
2024-03-21 16:52:40 +0530

    chore: drop not-so-useful test

--- cypress/integration/awesome_bar.js
+++ cypress/integration/awesome_bar.js
@@ -20,27 +20,6 @@ context("Awesome Bar", () => {
 		cy.location("pathname").should("eq", "/app/todo");
 	});
 
-	it("find text in doctype list", () => {
-		cy.get("@awesome_bar").type("test in todo");
-		cy.wait(100);
-		cy.get("@awesome_bar").type("{enter}");
-		cy.get(".title-text").should("contain", "To Do");
-		cy.wait(200); // Wait a bit longer before checking the filter.
-		cy.get('[data-original-title="ID"] > input').should("have.value", "%test%");
-
-		// filter preserved, now finds something else
-		cy.visit("/app/todo");
-		cy.get(".title-text").should("contain", "To Do");
-		cy.wait(200); // Wait a bit longer before checking the filter.
-		cy.get('[data-original-title="ID"] > input').as("filter");
-		cy.get("@filter").should("have.value", "%test%");
-		cy.get("@awesome_bar").type("anothertest in todo");
-		cy.wait(200); // Wait a bit longer before hitting enter.
-		cy.get("@awesome_bar").type("{enter}");
-		cy.wait(200); // Wait a bit longer before checking the filter.
-		cy.get("@filter").should("have.value", "%anothertest%");
-	});
-
 	it("navigates to new form", () => {
 		cy.get("@awesome_bar").type("new blog post");
 		cy.wait(100);
