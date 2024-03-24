241f3e9b1fa6e0ca299f27168cf64586df3ce47b
14987 <nirajgautam196@gmail.com>
2024-03-24 14:35:57 +0530

    feat: Display snapshot

--- frappe/core/doctype/document_snapshot/document_snapshot.js
+++ frappe/core/doctype/document_snapshot/document_snapshot.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2024, Frappe Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Document Snapshot", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Document Snapshot", {
+	refresh(frm) {
+		$(
+			frappe.render_template("document_view", {
+				doc: frm.doc,
+				data: JSON.parse(frm.doc.data),
+			})
+		).appendTo(frm.fields_dict.table_html.$wrapper.empty());
+	},
+});
