6a2d2b604d9425c20085bb2c9c132c6a0e302599
Ankush Menat <ankush@frappe.io>
2024-03-25 11:04:52 +0530

    fix: check title field

--- frappe/desk/search.py
+++ frappe/desk/search.py
@@ -179,7 +179,7 @@ def search_widget(
 			formatted_fields = [f"`tab{meta.name}`.`{f.strip()}`" for f in fields]
 
 			# Insert title field query after name
-			if meta.show_title_field_in_link:
+			if meta.show_title_field_in_link and meta.title_field:
 				formatted_fields.insert(1, f"`tab{meta.name}`.{meta.title_field} as `label`")
 
 			# In order_by, `idx` gets second priority, because it stores link count
