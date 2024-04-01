59813dba864f0fee34f640e591b05f8bd7ab4fef
Akhil Narang <me@akhilnarang.dev>
2024-04-01 13:34:08 +0530

    feat: allow skipping msgprint
    If we're handling the exception on our own, we don't need to show the user anything
    Example: https://github.com/frappe/frappe/blob/version-15/frappe/desk/doctype/event/event.py#L398
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    (cherry picked from commit ca83ad580c954a0cb0bfe40488fc37794248b287)

--- frappe/utils/data.py
+++ frappe/utils/data.py
@@ -78,7 +78,9 @@ def is_invalid_date_string(date_string: str) -> bool:
 
 
 def getdate(
-	string_date: Optional["DateTimeLikeObject"] = None, parse_day_first: bool = False
+	string_date: Optional["DateTimeLikeObject"] = None,
+	parse_day_first: bool = False,
+	skip_msgprint: bool = False,
 ) -> datetime.date | None:
 	"""
 	Converts string date (yyyy-mm-dd) to datetime.date object.
@@ -100,6 +102,7 @@ def getdate(
 		frappe.throw(
 			frappe._("{} is not a valid date string.").format(frappe.bold(string_date)),
 			title=frappe._("Invalid Date"),
+			skip_msgprint=skip_msgprint,
 		)
 
 
