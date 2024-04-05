99952880cc4aa8b9519a33f74517b52dc5ea0b8f
Ankush Menat <ankush@frappe.io>
2024-04-05 18:43:55 +0530

    fix!: Don't let users with write access to UP bypass UP
    IDK why we truly need this, except maybe debugging sometimes. This just
    causes confusion and people keep reporting this as security issue.

--- frappe/permissions.py
+++ frappe/permissions.py
@@ -332,11 +332,6 @@ def has_user_permission(doc, user=None, debug=False):
 		debug and _debug_log("User is not affected by any user permissions")
 		return True
 
-	# user can create own role permissions, so nothing applies
-	if get_role_permissions("User Permission", user=user).get("write"):
-		debug and _debug_log("User permission bypassed because user can modify user permissions.")
-		return True
-
 	# don't apply strict user permissions for single doctypes since they contain empty link fields
 	apply_strict_user_permissions = (
 		False if doc.meta.issingle else frappe.get_system_settings("apply_strict_user_permissions")
