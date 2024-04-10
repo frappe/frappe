0846230ce431a2f10a60c38e4f25273ce5d23f19
andres santos <andresrodriguezsantos44@gmail.com>
2024-04-10 14:46:44 +0000

    capitalize party name in forms

--- frappe/desk/form/save.py
+++ frappe/desk/form/save.py
@@ -14,96 +14,116 @@ from frappe.utils.telemetry import capture_doc
 
 @frappe.whitelist()
 def savedocs(doc, action):
-	createStatusChangedComment(json.loads(doc))
-	"""save / submit / update doclist"""
-	doc = frappe.get_doc(json.loads(doc))
-	capture_doc(doc, action)
-	if doc.get("__islocal") and doc.name.startswith("new-" + doc.doctype.lower().replace(" ", "-")):
-		# required to relink missing attachments if they exist.
-		doc.__temporary_name = doc.name
-	set_local_name(doc)
+    createStatusChangedComment(json.loads(doc))
+    """save / submit / update doclist"""
+    doc = frappe.get_doc(json.loads(doc))
+    if doc.get("party_name"):
+        doc.party_name = capitalize_first_letter(doc.get("party_name"))
 
-	# action
-	doc.docstatus = {
-		"Save": DocStatus.draft(),
-		"Submit": DocStatus.submitted(),
-		"Update": DocStatus.submitted(),
-		"Cancel": DocStatus.cancelled(),
-	}[action]
-	if doc.docstatus.is_submitted():
-		if action == "Submit" and doc.meta.queue_in_background and not is_scheduler_inactive():
-			queue_submission(doc, action)
-			return
-		doc.submit()
-	else:
-		doc.save()
+    capture_doc(doc, action)
+    if doc.get("__islocal") and doc.name.startswith(
+        "new-" + doc.doctype.lower().replace(" ", "-")
+    ):
+        # required to relink missing attachments if they exist.
+        doc.__temporary_name = doc.name
+    set_local_name(doc)
 
-	# update recent documents
-	run_onload(doc)
-	send_updated_docs(doc)
+    # action
+    doc.docstatus = {
+        "Save": DocStatus.draft(),
+        "Submit": DocStatus.submitted(),
+        "Update": DocStatus.submitted(),
+        "Cancel": DocStatus.cancelled(),
+    }[action]
+    if doc.docstatus.is_submitted():
+        if action == "Submit" and doc.meta.queue_in_background and not is_scheduler_inactive():
+            queue_submission(doc, action)
+            return
+        doc.submit()
+    else:
+        doc.save()
 
-	add_data_to_monitor(doctype=doc.doctype, action=action)
-	frappe.msgprint(frappe._("Saved"), indicator="green", alert=True)
+    # update recent documents
+    run_onload(doc)
+    send_updated_docs(doc)
+
+    add_data_to_monitor(doctype=doc.doctype, action=action)
+    frappe.msgprint(frappe._("Saved"), indicator="green", alert=True)
+
+
+def capitalize_first_letter(text):
+    if not text:
+        return text
+    return " ".join(word.capitalize() for word in text.split())
+
 
 @frappe.whitelist()
 def createStatusChangedComment(doc):
-	if(doc['doctype'] == "Project"):
-		project_name = doc['name']
-		current_project = frappe.db.sql("select status from tabProject where name = '"+project_name+"'", as_dict=True)
-		if(len(current_project)> 0):
-			current_status = current_project[0]['status']
-			new_status = doc['status']
-			user = doc['modified_by']
-			if(current_status != new_status):
-				comment = frappe.new_doc("Comment")
-				comment.update(
-				{
-					"comment_type": "Comment",
-					"reference_doctype": "Project",
-					"reference_name": project_name,
-					"comment_email": "",
-					"comment_by": "",
-					"content": "<div class=\"ql-editor read-mode\"><p>Project updated. From: " + current_status + " To: " + new_status + ". Modified by: " + user + "</p></div>"
-				})
-				comment.insert(ignore_permissions=True)
-  
-  
+    if doc["doctype"] == "Project":
+        project_name = doc["name"]
+        current_project = frappe.db.sql(
+            "select status from tabProject where name = '" + project_name + "'", as_dict=True
+        )
+        if len(current_project) > 0:
+            current_status = current_project[0]["status"]
+            new_status = doc["status"]
+            user = doc["modified_by"]
+            if current_status != new_status:
+                comment = frappe.new_doc("Comment")
+                comment.update(
+                    {
+                        "comment_type": "Comment",
+                        "reference_doctype": "Project",
+                        "reference_name": project_name,
+                        "comment_email": "",
+                        "comment_by": "",
+                        "content": '<div class="ql-editor read-mode"><p>Project updated. From: '
+                        + current_status
+                        + " To: "
+                        + new_status
+                        + ". Modified by: "
+                        + user
+                        + "</p></div>",
+                    }
+                )
+                comment.insert(ignore_permissions=True)
+
+
 @frappe.whitelist()
 def cancel(doctype=None, name=None, workflow_state_fieldname=None, workflow_state=None):
-	"""cancel a doclist"""
-	doc = frappe.get_doc(doctype, name)
-	capture_doc(doc, "Cancel")
+    """cancel a doclist"""
+    doc = frappe.get_doc(doctype, name)
+    capture_doc(doc, "Cancel")
 
-	if workflow_state_fieldname and workflow_state:
-		doc.set(workflow_state_fieldname, workflow_state)
-	doc.cancel()
-	send_updated_docs(doc)
-	frappe.msgprint(frappe._("Cancelled"), indicator="red", alert=True)
+    if workflow_state_fieldname and workflow_state:
+        doc.set(workflow_state_fieldname, workflow_state)
+    doc.cancel()
+    send_updated_docs(doc)
+    frappe.msgprint(frappe._("Cancelled"), indicator="red", alert=True)
 
 
 def send_updated_docs(doc):
-	from .load import get_docinfo
+    from .load import get_docinfo
 
-	get_docinfo(doc)
+    get_docinfo(doc)
 
-	d = doc.as_dict()
-	if hasattr(doc, "localname"):
-		d["localname"] = doc.localname
+    d = doc.as_dict()
+    if hasattr(doc, "localname"):
+        d["localname"] = doc.localname
 
-	frappe.response.docs.append(d)
+    frappe.response.docs.append(d)
 
 
 def set_local_name(doc):
-    
 
-	def _set_local_name(d):
-		if doc.get("__islocal") or d.get("__islocal"):
-			d.localname = d.name
-			d.name = None
+    def _set_local_name(d):
+        if doc.get("__islocal") or d.get("__islocal"):
+            d.localname = d.name
+            d.name = None
 
-	_set_local_name(doc)
-	for child in doc.get_all_children():
-		_set_local_name(child)
+    _set_local_name(doc)
+    for child in doc.get_all_children():
+        _set_local_name(child)
 
-	if doc.get("__newname"):
-		doc.name = doc.get("__newname")
+    if doc.get("__newname"):
+        doc.name = doc.get("__newname")
