ca1011a2959b19d77fbccf73f7cc002e0c96d0d5
Ankush Menat <ankush@frappe.io>
2024-04-19 15:45:35 +0530

    chore: rename child doctypes
    (cherry picked from commit a94534a938614ec515a8c9a75c9482d425eb0ecd)

--- frappe/desk/doctype/system_health_report_workers/system_health_report_workers.py
+++ frappe/desk/doctype/system_health_report_workers/system_health_report_workers.py
@@ -5,7 +5,7 @@
 from frappe.model.document import Document
 
 
-class SystemHealthWorkers(Document):
+class SystemHealthReportWorkers(Document):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
