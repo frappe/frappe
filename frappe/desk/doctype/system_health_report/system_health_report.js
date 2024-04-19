3c88613fc366e9fbd31af7e4eace4ad266423609
Ankush Menat <ankush@frappe.io>
2024-04-19 15:21:40 +0530

    feat: highlight bad indicators
    (cherry picked from commit 4f406d7c3b4be50746cbb90837bd71036bf25970)

--- frappe/desk/doctype/system_health_report/system_health_report.js
+++ frappe/desk/doctype/system_health_report/system_health_report.js
@@ -30,5 +30,51 @@ frappe.ui.form.on("System Health Report", {
 		});
 		frappe.realtime.emit("ping");
 		frm.disable_save();
+		frm.trigger("setup_highlight");
+	},
+
+	setup_highlight(frm) {
+		/// field => is bad?
+		const conditions = {
+			scheduler_status: (val) => val.toLowerCase() != "active",
+			background_jobs_check: (val) => val.toLowerCase() != "finished",
+			total_background_workers: (val) => val == 0,
+			binary_logging: (val) => val.toLowerCase() != "on",
+			socketio_ping_check: (val) => val != "Pass",
+			socketio_transport_mode: (val) => val != "websocket",
+			onsite_backups: (val) => val == 0,
+			failed_logins: (val) => val > frm.doc.total_users,
+			total_errors: (val) => val > 100,
+			// 5% excluding very small numbers
+			unhandled_emails: (val) =>
+				val > 3 && frm.doc.handled_emails > 3 && val / frm.doc.handled_emails > 0.05,
+			failed_emails: (val) =>
+				val > 3 &&
+				frm.doc.total_outgoing_emails > 3 &&
+				val / frm.doc.total_outgoing_emails > 0.05,
+			pending_emails: (val) =>
+				val > 3 &&
+				frm.doc.total_outgoing_emails > 3 &&
+				val / frm.doc.total_outgoing_emails > 0.1,
+		};
+
+		const style = document.createElement("style");
+		style.innerText = ".health-check-failed { border: 1px solid var(--error-border); }";
+		document.head.appendChild(style);
+
+		const update_fields = () => {
+			Object.entries(conditions).forEach(([field, condition]) => {
+				try {
+					let is_bad = condition(frm.doc[field]);
+					let df = frm.fields_dict[field];
+					$(df.disp_area).toggleClass("health-check-failed", is_bad);
+				} catch (e) {
+					console.log("Failed to evaluated", e);
+				}
+			});
+		};
+
+		update_fields();
+		const interval = setInterval(update_fields, 1000);
 	},
 });
