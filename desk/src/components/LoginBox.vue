73fa2795c1212acaea4d01cd2e89501cc305e9f6
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-04-22 00:15:23 +0530

    refactor: extract Login Box component & fix error message visibility

---
+++ desk/src/components/LoginBox.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="relative h-full">
+		<div class="relative z-10 mx-auto py-8 sm:w-max sm:py-32">
+			<div class="flex">
+				<div class="mx-auto flex flex-col items-center gap-4 space-x-2">
+					<AppLogo class="inline-block h-8 w-8" />
+					<span class="text-xl font-semibold leading-5 tracking-wide text-gray-900">
+						{{ title }}
+					</span>
+				</div>
+			</div>
+
+			<!-- Login Form -->
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import AppLogo from "@/components/icons/AppLogo.vue"
+
+const props = defineProps({
+	title: {
+		type: String,
+		default: "Login",
+	},
+})
+</script>
--- desk/src/pages/Login.vue
+++ desk/src/pages/Login.vue
@@ -1,106 +1,106 @@
 <template>
-	<div class="relative h-full">
-		<div class="relative z-10 mx-auto py-8 sm:w-max sm:py-32">
-			<div class="flex">
-				<div class="mx-auto flex items-center space-x-2">
-					<AppLogo class="inline-block h-7 w-7" />
-					<span class="select-none text-xl font-semibold tracking-tight text-gray-900">
-						Frappe
-					</span>
+	<LoginBox :title="title">
+		<!-- Login -->
+		<div
+			v-if="!resetPasswordEmailSent"
+			class="mx-auto w-full bg-white px-4 py-8 sm:mt-6 sm:w-96 sm:rounded-lg sm:px-8 sm:shadow-xl"
+		>
+			<form class="flex flex-col" @submit.prevent="loginOrResetPassword">
+				<FormControl
+					label="Email"
+					placeholder="johndoe@mail.com"
+					v-model="email"
+					name="email"
+					autocomplete="email"
+					:type="email !== 'Administrator' ? 'email' : 'text'"
+					required
+				/>
+				<FormControl
+					class="mt-4"
+					v-if="!forgotPassword"
+					label="Password"
+					type="password"
+					placeholder="•••••"
+					v-model="password"
+					name="password"
+					autocomplete="current-password"
+					required
+				/>
+				<div class="mt-2 text-left text-sm text-gray-700">
+					<router-link v-if="forgotPassword" to="/login"> I remember my password </router-link>
+					<router-link
+						v-else
+						:to="{
+							name: 'Login',
+							query: { forgot: 1 },
+						}"
+					>
+						Forgot Password?
+					</router-link>
 				</div>
-			</div>
 
-			<!-- Login Box -->
-			<div
-				v-if="!resetPasswordEmailSent"
-				class="mx-auto w-full bg-white px-4 py-8 sm:mt-6 sm:w-96 sm:rounded-lg sm:px-8 sm:shadow-xl"
-			>
-				<div class="mb-6 text-center" v-if="title">
-					<span class="text-center text-lg font-medium leading-5 tracking-tight text-gray-900">
-						{{ title }}
-					</span>
-				</div>
-
-				<form class="flex flex-col" @submit.prevent="loginOrResetPassword">
-					<FormControl
-						label="Email"
-						placeholder="johndoe@mail.com"
-						v-model="email"
-						name="email"
-						autocomplete="email"
-						:type="email !== 'Administrator' ? 'email' : 'text'"
-						required
-					/>
-					<FormControl
-						class="mt-4"
-						v-if="!forgotPassword"
-						label="Password"
-						type="password"
-						placeholder="•••••"
-						v-model="password"
-						name="password"
-						autocomplete="current-password"
-						required
-					/>
-					<div class="mt-2 text-sm">
-						<router-link v-if="forgotPassword" to="/login"> I remember my password </router-link>
-						<router-link
-							v-else
-							:to="{
-								name: 'Login',
-								query: { forgot: 1 },
-							}"
-						>
-							Forgot Password?
-						</router-link>
-					</div>
-
-					<ErrorMessage :message="session.login.error" class="mt-4" />
-					<Button class="mt-4" variant="solid" :loading="session.login.loading">
-						{{ !forgotPassword ? "Log in with email" : "Reset Password" }}
+				<div class="mt-5 flex w-full flex-col gap-2">
+					<ErrorMessage :message="session.login.error || resetPassword.error" />
+					<Button variant="solid" :loading="session.login.loading">
+						{{ !forgotPassword ? "Login" : "Reset Password" }}
 					</Button>
-				</form>
+				</div>
+			</form>
+		</div>
+
+		<!-- Reset Password Message -->
+		<div
+			v-else
+			class="mx-auto w-full bg-white px-4 py-8 sm:mt-6 sm:w-96 sm:rounded-lg sm:px-8 sm:shadow-xl"
+		>
+			<div class="mb-6 text-center" v-if="title">
+				<span class="text-center text-lg font-medium leading-5 tracking-tight text-gray-900">
+					{{ title }}
+				</span>
 			</div>
 
-			<!-- Reset Password Message -->
-			<div
-				v-else
-				class="mx-auto w-full bg-white px-4 py-8 sm:mt-6 sm:w-96 sm:rounded-lg sm:px-8 sm:shadow-xl"
-			>
-				<div class="mb-6 text-center" v-if="title">
-					<span class="text-center text-lg font-medium leading-5 tracking-tight text-gray-900">
-						{{ title }}
-					</span>
-				</div>
-
-				<div class="text-p-base text-gray-700">
-					<p>
-						We have sent an email to <span class="font-semibold">{{ email }}</span
-						>. Please click on the link received to reset your password.
-					</p>
-				</div>
+			<div class="text-p-base text-gray-700">
+				<p>
+					We have sent an email to <span class="font-semibold">{{ email }}</span
+					>. Please click on the link received to reset your password.
+				</p>
 			</div>
 		</div>
-	</div>
+	</LoginBox>
 </template>
 
 <script setup>
-import { ref, computed, inject } from "vue"
+import { ref, computed, inject, watch } from "vue"
 import { useRoute } from "vue-router"
 
-import AppLogo from "@/components/icons/AppLogo.vue"
 import { createResource } from "frappe-ui"
+import LoginBox from "@/components/LoginBox.vue"
 
 const email = ref(null)
 const password = ref(null)
-const errorMessage = ref(null)
 const resetPasswordEmailSent = ref(false)
 
 const route = useRoute()
 const session = inject("$session")
 
 const forgotPassword = computed(() => route.query.forgot)
-const title = computed(() => (forgotPassword.value ? "Reset Password" : "Log in to your account"))
+const title = computed(() => (forgotPassword.value ? "Reset Password" : "Login to Frappe"))
+
+watch(
+	() => forgotPassword.value,
+	(_val) => {
+		session.login.error = null
+		resetPassword.error = null
+		password.value = null
+	}
+)
+
+const resetPassword = createResource({
+	url: "frappe.core.doctype.user.user.reset_password",
+	onSuccess: (_data) => {
+		resetPasswordEmailSent.value = true
+	},
+})
 
 const loginOrResetPassword = async () => {
 	if (!forgotPassword.value) {
@@ -109,15 +109,7 @@ const loginOrResetPassword = async () => {
 			password: password.value,
 		})
 	} else {
-		resetPassword.reload()
+		resetPassword.submit({ user: email.value })
 	}
 }
-
-const resetPassword = createResource({
-	url: "frappe.core.doctype.user.user.reset_password",
-	data: { user: email.value },
-	onSuccess: (_data) => {
-		resetPasswordEmailSent.value = true
-	},
-})
 </script>
