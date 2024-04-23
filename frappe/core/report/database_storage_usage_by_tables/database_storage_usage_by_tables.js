3f0c2330fcd8883b866ddbd9cfc149d368c3cd2c
Niraj Gautam <nirajgautam196@gmail.com>
2024-04-23 17:19:11 +0530

    feat: Allow user to optimize tables (#26109)
    * feat: Allow user to optimize tables
    
    * fix: Update butto label
    
    * fix: Filter out single & virtual doctypes
    
    * fix: Create long job for optimization
    
    * fix: check perm
    
    ---------
    
    Co-authored-by: Ankush Menat <ankush@frappe.io>

--- frappe/core/report/database_storage_usage_by_tables/database_storage_usage_by_tables.js
+++ frappe/core/report/database_storage_usage_by_tables/database_storage_usage_by_tables.js
@@ -3,4 +3,49 @@
 
 frappe.query_reports["Database Storage Usage By Tables"] = {
 	filters: [],
+	onload: function (report) {
+		report.page.add_inner_button(
+			__("Optimize"),
+			function () {
+				let d = new frappe.ui.Dialog({
+					title: "Optimize Doctype",
+					fields: [
+						{
+							label: "Select a DocType",
+							fieldname: "doctype_name",
+							fieldtype: "Link",
+							options: "DocType",
+							get_query: function () {
+								return {
+									filters: { issingle: ["=", false], is_virtual: ["=", false] },
+								};
+							},
+						},
+					],
+					size: "small",
+					primary_action_label: "Optimize",
+					primary_action(values) {
+						frappe.call({
+							method: "frappe.core.report.database_storage_usage_by_tables.database_storage_usage_by_tables.optimize_doctype",
+							args: {
+								doctype_name: values.doctype_name,
+							},
+							callback: function (r) {
+								if (!r.exec) {
+									frappe.show_alert(
+										__(
+											`${values.doctype_name} has been added to queue for optimization`
+										)
+									);
+								}
+							},
+						});
+						d.hide();
+					},
+				});
+				d.show();
+			},
+			__("Actions")
+		);
+	},
 };
