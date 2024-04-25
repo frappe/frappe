af611d4bcbff60413e61160cfc16c8d49e2d833c
Akhil Narang <me@akhilnarang.dev>
2024-04-25 17:26:12 +0530

    fix(report_utils): ensure that delimiter and separator can't be empty
    This results in them setting as `undefined`, which ends up as a string
    in python, and the delimiter ends up as `'u'`, and the separator as
    `'undefined'`.
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- frappe/public/js/frappe/views/reports/report_utils.js
+++ frappe/public/js/frappe/views/reports/report_utils.js
@@ -264,8 +264,18 @@ frappe.report_utils = {
 
 		dialog.fields_dict["file_format"].df.onchange = () => update_csv_preview(dialog);
 		dialog.fields_dict["csv_quoting"].df.onchange = () => update_csv_preview(dialog);
-		dialog.fields_dict["csv_delimiter"].df.onchange = () => update_csv_preview(dialog);
-		dialog.fields_dict["csv_decimal_sep"].df.onchange = () => update_csv_preview(dialog);
+		dialog.fields_dict["csv_delimiter"].df.onchange = () => {
+			if (!dialog.get_value("csv_delimiter")) {
+				dialog.set_value("csv_delimiter", ",");
+			}
+			update_csv_preview(dialog);
+		};
+		dialog.fields_dict["csv_decimal_sep"].df.onchange = () => {
+			if (!dialog.get_value("csv_decimal_sep")) {
+				dialog.set_value("csv_decimal_sep", ".");
+			}
+			update_csv_preview(dialog);
+		};
 
 		return dialog;
 	},
