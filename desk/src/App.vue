0c643c401d581a199cdb765463511631cd2d7723
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-04-28 19:36:41 +0530

    feat: add Espresso & Timeless icons

--- desk/src/App.vue
+++ desk/src/App.vue
@@ -1,5 +1,12 @@
 <template>
 	<div class="bg-white antialiased">
 		<router-view />
+		<IconsTimeless />
+		<IconsEspresso />
 	</div>
 </template>
+
+<script setup>
+import IconsTimeless from "@/components/icons/IconsTimeless.vue"
+import IconsEspresso from "@/components/icons/IconsEspresso.vue"
+</script>
---
+++ desk/src/components/Icon.vue
@@ -0,0 +1,14 @@
+<template>
+	<svg :aria-label="name" class="fill-transparent stroke-current">
+		<use :href="`#${name}`" />
+	</svg>
+</template>
+
+<script setup>
+const props = defineProps({
+	name: {
+		type: String,
+		required: true,
+	},
+})
+</script>
