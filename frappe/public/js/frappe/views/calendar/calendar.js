edf313765d9a11f373f324ab562484ddd4774289
Ankush Menat <ankush@frappe.io>
2024-04-29 18:46:25 +0530

    chore: conflicts

--- frappe/public/js/frappe/views/calendar/calendar.js
+++ frappe/public/js/frappe/views/calendar/calendar.js
@@ -250,17 +250,13 @@ frappe.views.Calendar = class Calendar {
 	setup_options(defaults) {
 		var me = this;
 		defaults.meridiem = "false";
-		let lang = frappe.boot.lang;
+		let lang = frappe.boot.user.language || "en";
 		if (lang == "ar") {
 			// arabic doesn't work with fullcalendar - doesn't show anything.
 			lang = "en";
 		}
 		this.cal_options = {
-<<<<<<< HEAD
-			locale: frappe.boot.user.language || "en",
-=======
 			locale: lang,
->>>>>>> e4ed1b98e4 (fix(ar): render fullcalendar in english (#26207))
 			header: {
 				left: "prev, title, next",
 				right: "today, month, agendaWeek, agendaDay",
