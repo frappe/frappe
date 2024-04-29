c961023717f6762a3a3b09442864c52f13bee191
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-04-29 13:41:49 +0530

    fix(style): responsiveness in module grid
    - truncate module label
    
    - fix font colors

--- desk/src/components/Desktop.vue
+++ desk/src/components/Desktop.vue
@@ -1,19 +1,20 @@
 <template>
-	<div class="my-5 w-full max-w-5xl">
-		<div class="grid grid-cols-[repeat(5,_60px)] justify-between gap-y-13">
+	<div class="my-12 w-full max-w-5xl">
+		<div
+			class="grid grid-cols-3 justify-between gap-y-13 md:grid-cols-4 lg:grid-cols-[repeat(6,_60px)]"
+		>
 			<template v-for="item in desktopModules.data?.pages">
 				<!-- Module Link -->
-				<router-link to="/" class="mt-5 flex flex-col items-center justify-center gap-2">
-					<div class="rounded-lg border border-gray-400 p-3 hover:scale-110">
-						<Icon :name="item.icon" class="h-8 w-8 text-gray-500" />
+				<router-link to="/" class="mt-5 flex flex-col items-center justify-center gap-3">
+					<div class="rounded-lg border border-gray-500 p-3 hover:scale-110">
+						<Icon :name="item.icon" class="h-8 w-8 text-gray-600" />
 					</div>
-					<span class="text-base text-gray-600">{{ item.label }}</span>
+					<span class="truncate text-base text-gray-800">{{ item.label }}</span>
 				</router-link>
 			</template>
 		</div>
 	</div>
 </template>
-
 <script setup>
 import { desktopModules } from "@/data/desktop"
 import Icon from "@/components/Icon.vue"
