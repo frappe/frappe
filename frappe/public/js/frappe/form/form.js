4e497f69b0cbe4c84f3f173b1707ada2f97cafd5
Ankush Menat <ankush@frappe.io>
2024-04-30 11:38:58 +0530

    fix(UX): warning when printing unsaved documents (#26229)
    (cherry picked from commit 99e4f559401acb4d7a6afdfa6a4821dc3151e846)

--- frappe/public/js/frappe/form/form.js
+++ frappe/public/js/frappe/form/form.js
@@ -1234,6 +1234,15 @@ frappe.ui.form.Form = class FrappeForm {
 	// ACTIONS
 
 	print_doc() {
+		if (this.is_dirty()) {
+			frappe.toast({
+				message: __(
+					"This document has unsaved changes which might not appear in final PDF. <br> Consider saving the document before printing."
+				),
+				indicator: "yellow",
+			});
+		}
+
 		frappe.route_options = {
 			frm: this,
 		};
