1d7e7de80f6cef38e99244bb2e97c15b389ecb66
Ankush Menat <ankush@frappe.io>
2024-05-01 19:06:06 +0530

    Update navbar_settings.js

--- frappe/core/doctype/navbar_settings/navbar_settings.js
+++ frappe/core/doctype/navbar_settings/navbar_settings.js
@@ -2,12 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Navbar Settings", {
-<<<<<<< HEAD
-	// refresh: function(frm) {
-	// }
-=======
 	after_save: function (frm) {
 		frappe.ui.toolbar.clear_cache();
 	},
->>>>>>> 8eb8c64fbd (fix(Navbar Settings): reload page after save (#26274))
 });
