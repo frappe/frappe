b8a2bdedf148da788a7679332328fc1525ebd338
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2024-05-01 15:20:04 +0200

    fix(Navbar Settings): reload page after save (#26274)
    * fix(Navbar Settings): reload page after save
    
    * test: file uploader flake
    
    ---------
    
    Co-authored-by: Ankush Menat <ankush@frappe.io>
    (cherry picked from commit 8eb8c64fbd20f8bb3e583a6c1208b63ca382ddea)
    
    # Conflicts:
    #	frappe/core/doctype/navbar_settings/navbar_settings.js

--- frappe/core/doctype/navbar_settings/navbar_settings.js
+++ frappe/core/doctype/navbar_settings/navbar_settings.js
@@ -2,6 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Navbar Settings", {
+<<<<<<< HEAD
 	// refresh: function(frm) {
 	// }
+=======
+	after_save: function (frm) {
+		frappe.ui.toolbar.clear_cache();
+	},
+>>>>>>> 8eb8c64fbd (fix(Navbar Settings): reload page after save (#26274))
 });
