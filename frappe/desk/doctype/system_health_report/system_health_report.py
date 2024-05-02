bee2641a880015eeacb097e063e82487e90f9cae
Ankush Menat <ankush@frappe.io>
2024-05-02 12:49:30 +0530

    feat: show oldest unscheduled job in health report
    (cherry picked from commit b2ef2cd506c9645e637928b1d353f8f4f01f1c66)
    
    # Conflicts:
    #	frappe/desk/doctype/system_health_report/system_health_report.py

--- frappe/desk/doctype/system_health_report/system_health_report.py
+++ frappe/desk/doctype/system_health_report/system_health_report.py
@@ -25,11 +25,12 @@ from collections import defaultdict
 from collections.abc import Callable
 
 import frappe
+from frappe.core.doctype.scheduled_job_type.scheduled_job_type import ScheduledJobType
 from frappe.model.document import Document
 from frappe.utils.background_jobs import get_queue, get_queue_list
 from frappe.utils.caching import redis_cache
 from frappe.utils.data import add_to_date
-from frappe.utils.scheduler import get_scheduler_status
+from frappe.utils.scheduler import get_scheduler_status, get_scheduler_tick
 
 
 def health_check(step: str):
@@ -50,6 +51,68 @@ def health_check(step: str):
 
 
 class SystemHealthReport(Document):
+<<<<<<< HEAD
+=======
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.desk.doctype.system_health_report_errors.system_health_report_errors import (
+			SystemHealthReportErrors,
+		)
+		from frappe.desk.doctype.system_health_report_failing_jobs.system_health_report_failing_jobs import (
+			SystemHealthReportFailingJobs,
+		)
+		from frappe.desk.doctype.system_health_report_queue.system_health_report_queue import (
+			SystemHealthReportQueue,
+		)
+		from frappe.desk.doctype.system_health_report_tables.system_health_report_tables import (
+			SystemHealthReportTables,
+		)
+		from frappe.desk.doctype.system_health_report_workers.system_health_report_workers import (
+			SystemHealthReportWorkers,
+		)
+		from frappe.types import DF
+
+		active_sessions: DF.Int
+		background_jobs_check: DF.Data | None
+		background_workers: DF.Table[SystemHealthReportWorkers]
+		backups_size: DF.Float
+		binary_logging: DF.Data | None
+		bufferpool_size: DF.Data | None
+		cache_keys: DF.Int
+		cache_memory_usage: DF.Data | None
+		database: DF.Data | None
+		database_version: DF.Data | None
+		db_storage_usage: DF.Float
+		failed_emails: DF.Int
+		failed_logins: DF.Int
+		failing_scheduled_jobs: DF.Table[SystemHealthReportFailingJobs]
+		handled_emails: DF.Int
+		last_10_active_users: DF.Code | None
+		new_users: DF.Int
+		oldest_unscheduled_job: DF.Link | None
+		onsite_backups: DF.Int
+		pending_emails: DF.Int
+		private_files_size: DF.Float
+		public_files_size: DF.Float
+		queue_status: DF.Table[SystemHealthReportQueue]
+		scheduler_status: DF.Data | None
+		socketio_ping_check: DF.Literal["Fail", "Pass"]
+		socketio_transport_mode: DF.Literal["Polling", "Websocket"]
+		test_job_id: DF.Data | None
+		top_db_tables: DF.Table[SystemHealthReportTables]
+		top_errors: DF.Table[SystemHealthReportErrors]
+		total_background_workers: DF.Int
+		total_errors: DF.Int
+		total_outgoing_emails: DF.Int
+		total_users: DF.Int
+		unhandled_emails: DF.Int
+	# end: auto-generated types
+
+>>>>>>> b2ef2cd506 (feat: show oldest unscheduled job in health report)
 	def db_insert(self, *args, **kwargs):
 		raise NotImplementedError
 
@@ -129,6 +192,18 @@ class SystemHealthReport(Document):
 
 		for job in failing_jobs:
 			self.append("failing_scheduled_jobs", job)
+
+		threshold = add_to_date(None, seconds=-30 * get_scheduler_tick(), as_datetime=True)
+		for job_type in frappe.get_all(
+			"Scheduled Job Type",
+			filters={"stopped": 0, "last_execution": ("<", threshold)},
+			fields="*",
+			order_by="last_execution asc",
+		):
+			job_type: ScheduledJobType = frappe.get_doc(doctype="Scheduled Job Type", **job_type)
+			if job_type.is_event_due():
+				self.oldest_unscheduled_job = job_type.name
+				break
 
 	@health_check("Emails")
 	def fetch_email_stats(self):
