9943dde4ebccb7e4a3119d8ce162720eb281a776
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-05-03 01:36:40 +0530

    feat: collapsible sidebar sections for different link types (doctypes/reports/pages)

--- desk/src/components/ModuleSidebar.vue
+++ desk/src/components/ModuleSidebar.vue
@@ -2,23 +2,33 @@
 	<div
 		class="relative flex h-full max-h-screen min-h-screen w-64 flex-col overflow-auto bg-gray-50 p-4"
 	>
-		<div class="flex items-center gap-2">
+		<router-link class="mb-2 flex items-center gap-2" :to="{ name: 'Home' }">
 			<AppLogo class="h-6 w-6" />
 			<span class="text-xl font-semibold text-gray-800">Frappe</span>
-		</div>
+		</router-link>
 
-		<div class="mt-1">
-			<div v-for="(items, linkType) in sidebarLinks.data" :key="linkType">
-				<div v-if="items.length" class="mt-7">
-					<div class="text-base font-semibold text-gray-600">{{ linkType }}</div>
-					<div class="mt-2 flex flex-col gap-1 pl-2">
-						<div
-							v-for="item in items"
-							:key="item.link_to"
-							class="cursor-pointer truncate rounded-md px-2 py-1 text-base text-gray-800 hover:bg-gray-100"
-						>
-							{{ item.label }}
-						</div>
+		<div v-for="group in sidebarLinks.data" :key="group.linkType">
+			<!-- Link Group - DocTypes/Reports/Pages -->
+			<div v-if="group.items.length" class="mt-5">
+				<div class="flex items-center gap-1">
+					<FeatherIcon
+						@click="group.opened = !group.opened"
+						:name="group.opened ? 'chevron-down' : 'chevron-right'"
+						class="h-4 w-4 font-semibold text-gray-600"
+					/>
+					<div class="flex items-center gap-1 text-base font-semibold text-gray-600">
+						{{ group.linkType }}
+					</div>
+				</div>
+
+				<!-- Link Group Items -->
+				<div v-if="group.opened" class="mt-2 flex flex-col pl-5">
+					<div
+						v-for="item in group.items"
+						:key="item.link_to"
+						class="cursor-pointer truncate border-l-2 px-2 py-1.5 text-base text-gray-800 hover:border-gray-900 hover:text-gray-900"
+					>
+						{{ item.label }}
 					</div>
 				</div>
 			</div>
@@ -29,7 +39,7 @@
 <script setup>
 import { watch } from "vue"
 import { useRoute } from "vue-router"
-import { createResource } from "frappe-ui"
+import { createResource, FeatherIcon } from "frappe-ui"
 
 import AppLogo from "@/components/icons/AppLogo.vue"
 
@@ -37,6 +47,13 @@ const route = useRoute()
 
 const sidebarLinks = createResource({
 	url: "frappe.api.desk.get_links_for_workspace",
+	transform: (data) => {
+		return Object.entries(data).map(([linkType, items]) => ({
+			linkType,
+			items,
+			opened: true,
+		}))
+	},
 })
 
 watch(
