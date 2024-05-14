6e934d7dfb38f52deff951126d20bc56819a5fec
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-05-14 23:15:27 +0530

    fix(style): Desktop height
    Co-authored-by: Gursheen Anand <gursheen@frappe.io>

--- desk/src/components/Desktop.vue
+++ desk/src/components/Desktop.vue
@@ -1,23 +1,25 @@
 <template>
-	<div class="my-12 w-full max-w-5xl">
-		<div
-			class="grid grid-cols-3 justify-between gap-y-13 md:grid-cols-4 lg:grid-cols-[repeat(6,_60px)]"
-		>
-			<template v-for="item in desktopItems.data">
-				<!-- Module Link -->
-				<router-link
-					:to="{ name: 'Module', params: { module: item.module_slug } }"
-					class="mt-5 flex flex-col items-center justify-center gap-3"
-				>
-					<div
-						class="rounded-lg p-3 shadow-md hover:scale-110"
-						:style="`background-color: ${item.color}`"
+	<div class="flex h-lvh w-full flex-col items-center">
+		<div class="w-full max-w-5xl py-12">
+			<div
+				class="grid grid-cols-3 justify-between gap-y-13 md:grid-cols-4 lg:grid-cols-[repeat(6,_60px)]"
+			>
+				<template v-for="item in desktopItems.data">
+					<!-- Module Link -->
+					<router-link
+						:to="{ name: 'Module', params: { module: item.module_slug } }"
+						class="mt-5 flex flex-col items-center justify-center gap-3"
 					>
-						<Icon :name="item.icon" class="h-8 w-8 text-white" />
-					</div>
-					<span class="truncate text-base text-gray-800">{{ item.label }}</span>
-				</router-link>
-			</template>
+						<div
+							class="rounded-lg p-3 shadow-xl hover:scale-110"
+							:style="`background-color: ${item.color}`"
+						>
+							<Icon :name="item.icon" class="h-8 w-8 text-white" />
+						</div>
+						<span class="truncate text-base text-gray-800">{{ item.label }}</span>
+					</router-link>
+				</template>
+			</div>
 		</div>
 	</div>
 </template>
