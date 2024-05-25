cc2c4536934145426922d9278226925eb25ee717
Gursheen Anand <gursheen@frappe.io>
2024-05-25 12:30:09 +0530

    refactor: cleanup event emitting for filters

--- desk/src/components/ListControls.vue
+++ desk/src/components/ListControls.vue
@@ -1,32 +1,20 @@
 <template>
-    <div class="flex justify-end gap-2 items-center">
-        <slot name="customControls"><div></div></slot>
-        <div class="flex gap-2">
-            <div v-if="config_updated"
-                class="flex items-center gap-2 border-r pr-2"
-            >
-                <Button :label="'Cancel'" @click="cancelChanges"/>
-                <Button :label="'Save Changes'" @click="saveChanges"/>
-            </div>
-            <template v-if="options.showFilters">
-                <ListFilter v-model="config.filters" :allFilterableFields="config.fields" @update="(f) => emit('filterChange', f)"/>
-            </template>
-            <template v-if="options.showColumnSettings">
-                <ListColumnSettings v-model="config.columns" :allColumns="config.fields"/>
-            </template>
-        </div>
-    </div>
+    <template v-if="options.showFilters">
+        <ListFilters v-model="config.filters" :allFilterableFields="config.fields" />
+    </template>
+    <template v-if="options.showColumnSettings && config.columns">
+        <ListColumnSettings v-model="config.columns" :allColumns="config.fields" />
+    </template>
 </template>
 
 <script setup>
-import { ref, watch, computed, defineEmits, defineModel  } from 'vue';
+import { defineModel } from 'vue';
 import ListColumnSettings from '@/components/ListColumnSettings.vue';
-import ListFilter from '@/components/ListFilter.vue';
-
-const emit = defineEmits(['updateConfigSettings', 'filterChange']);
+import ListFilters from '@/components/ListFilters.vue';
 
 const config = defineModel();
-const oldConfig = ref({});
+
+const emit = defineEmits(['updateFilter']);
 
 const props = defineProps({
     options: {
@@ -34,23 +22,4 @@ const props = defineProps({
         default: {}
     }
 });
-
-watch(
-    config,
-    (config) => {
-        if (!config) return;
-        oldConfig.value = JSON.parse(JSON.stringify(config));
-    },
-    { immediate: true }
-);
-
-const config_updated = computed(() => JSON.stringify(oldConfig.value) != JSON.stringify(config.value));
-
-const saveChanges = () => {
-    emit('updateConfigSettings');
-}
-
-const cancelChanges = () => {
-    config.value = JSON.parse(JSON.stringify(oldConfig.value));
-}
 </script>
--- desk/src/pages/List.vue
+++ desk/src/pages/List.vue
@@ -1,39 +1,36 @@
 <template>
-    <div v-if="list.data">
-        <ListView v-model="list_config" :options="{
-        showTooltip: false,
-        resizeColumn: true,
-        showColumnSettings: true,
-        showFilters: true,
-        emptyState: {
-            title: 'No records found',
-            description: 'Create a new record to get started',
-            button: {
-                label: `New ${route.params.doctype}`,
-                onClick: () => {
-                    //add route to create new record
-                }
-            },
-        },
-    }" @update="handleUpdateConfig" @filterChange="handleFilterChange">
-            <template #customControls>
-                <div class="flex items-center gap-2">
-                    <Dropdown :placement="'right'" :options="viewsDropdownOptions">
-                        <template #default="{ open }">
-                            <Button :label="config_settings.data?.label">
-                                <template #prefix>
-                                    <FeatherIcon :name="config_settings.data?.icon || 'list'" class="h-3.5" />
-                                </template>
-                                <template #suffix>
-                                    <FeatherIcon :name="open ? 'chevron-up' : 'chevron-down'"
-                                        class="h-3.5 text-gray-600" />
-                                </template>
-                            </Button>
-                        </template>
-                    </Dropdown>
+    <div class="flex flex-col gap-5 m-5">
+        <div class="flex justify-between items-center">
+            <div>
+            </div>
+            <div class="flex gap-4" v-if="config_settings.data">
+                <Dropdown :placement="'right'" :options="viewsDropdownOptions">
+                    <template #default="{ open }">
+                        <Button :label="config_settings.data.label">
+                            <template #prefix>
+                                <FeatherIcon :name="config_settings.data.icon || 'list'" class="h-3.5 text-gray-600" />
+                            </template>
+                            <template #suffix>
+                                <FeatherIcon :name="open ? 'chevron-up' : 'chevron-down'" class="h-3.5 text-gray-600" />
+                            </template>
+                        </Button>
+                    </template>
+                </Dropdown>
+                <div v-if="configUpdated" class="flex items-center gap-2 border-r pr-2">
+                    <Button :label="'Cancel'" @click="cancelChanges" />
+                    <template v-if="isDefaultConfig">
+                        <Button :label="'Create View'" />
+                    </template>
+                    <template v-else>
+                        <Button :label="'Save'" @click="saveChangesToView" />
+                    </template>
                 </div>
-            </template>
-        </ListView>
+                <ListControls v-model="listConfig" :options="listControlOptions" @updateFilter="handleFilterChange"/>
+            </div>
+        </div>
+        <div v-if="list.data">
+            <ListView :rows="list.data" rowKey="name" :columns="listConfig.columns" :options="listOptions" />
+        </div>
     </div>
 </template>
 
@@ -42,10 +39,96 @@ import { config_name, config_settings, isDefaultConfig } from '@/stores/view';
 import { createResource, FeatherIcon, Dropdown, call } from 'frappe-ui';
 import { ref, watch, computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
-import ListView from '@/components/ListView.vue';
+
+import { ListView } from 'frappe-ui';
+import ListControls from '@/components/ListControls.vue';
 
 const route = useRoute();
 const router = useRouter();
+
+const viewsDropdownOptions = computed(() => {
+    let options = [
+        {
+            group: 'Default Views',
+            items: [
+                {
+                    label: 'List',
+                    icon: 'list'
+                },
+                {
+                    label: 'Report',
+                    icon: 'table'
+                },
+                {
+                    label: 'Kanban',
+                    icon: 'grid'
+                },
+                {
+                    label: 'Dashboard',
+                    icon: 'pie-chart'
+                }
+            ]
+        }
+    ];
+    let saved_views = [];
+    if (route.params.doctype) {
+        call('frappe.desk.doctype.view_config.view_config.get_views_for_doctype', {
+            doctype: route.params.doctype
+        }).then((res) => {
+            res.map((v) => {
+                v.name != config_name.value && saved_views.push({
+                    label: v.label,
+                    icon: v.icon,
+                    onClick: () => {
+                        router.push({ query: { config: v.name } });
+                    }
+                });
+            });
+
+            saved_views.length && options.push({
+                group: 'Saved Views',
+                items: saved_views,
+            });
+        });
+    }
+    return options;
+});
+
+const listControlOptions = {
+    showColumnSettings: true,
+    showFilters: true,
+};
+
+const listOptions = {
+    showTooltip: false,
+    selectable: true,
+    resizeColumn: true,
+    rowHeight: 40,
+};
+
+const listConfig = ref({});
+
+const oldConfig = ref({});
+
+const configUpdated = computed(() => JSON.stringify(oldConfig.value) != JSON.stringify(listConfig.value));
+
+const loadConfig = async (query_config) => {
+    if (!query_config) {
+        isDefaultConfig.value = true;
+        config_name.value = route.params.doctype;
+    }
+    else {
+        isDefaultConfig.value = false;
+        config_name.value = query_config;
+    }
+    await config_settings.fetch();
+    listConfig.value = {
+        columns: config_settings.data.columns,
+        fields: config_settings.data.doctype_fields,
+        filters: getParsedFilters(),
+    };
+    oldConfig.value = JSON.parse(JSON.stringify(listConfig.value));
+};
 
 const list = createResource({
     url: `frappe.client.get_list`,
@@ -62,29 +145,38 @@ const list = createResource({
     }
 });
 
-const list_config = ref({});
-
-const loadList = async (config) => {
+watch(() => route.query.config, async (query_config) => {
+    await loadConfig(query_config);
     await list.fetch();
-    list_config.value = {
-        rows: list.data,
-        rowKey: "name",
-        columns: config.columns,
-        fields: config.doctype_fields,
-        filters: getParsedFilters(),
-    };
+}, { immediate: true });
+
+const updateConfigResource = createResource({
+    url: 'frappe.desk.doctype.view_config.view_config.update_config',
+    method: 'POST',
+});
+
+const saveChangesToView = () => {
+    updateConfigResource.submit({ config_name: config_name.value, new_config: listConfig.value });
+    loadList(config);
 }
 
+const cancelChanges = () => {
+    listConfig.value = JSON.parse(JSON.stringify(oldConfig.value));
+}
+
+// Handle filters from URL query params
+
 const filters = computed(() => {
+    let filters = {};
     if (route.query) {
-        let filters = {};
         for (let key in route.query) {
             if (key == 'config')
                 continue;
             filters[key] = JSON.parse(route.query[key]);
         }
-        return filters;
     }
+    if (filters.length)
+        return filters;
     // show config filters if no query params
     return config_settings.data.filters;
 });
@@ -101,81 +193,9 @@ const getParsedFilters = () => {
     return parsedFilters;
 }
 
-watch(() => route.query.config, async (query_config) => {
-    if (!query_config) {
-        isDefaultConfig.value = true;
-        config_name.value = route.params.doctype;
-    }
-    else {
-        isDefaultConfig.value = false;
-        config_name.value = query_config;
-    }
-    await config_settings.fetch();
-    loadList(config_settings.data);
-}, { immediate: true });
-
-const updateConfigResource = createResource({
-    url: 'frappe.desk.doctype.view_config.view_config.update_config',
-    method: 'POST',
-});
-
-const handleUpdateConfig = async (config) => {
-    updateConfigResource.submit({ config_name: config_name.value, new_config: config });
-    loadList(config);
-};
-
-// TODO: add default view routes
-const defaultViews = [
-    {
-        label: 'Report',
-        icon: 'table'
-    },
-    {
-        label: 'Kanban',
-        icon: 'grid'
-    },
-    {
-        label: 'Dashboard',
-        icon: 'pie-chart'
-    }
-];
-
-const viewsDropdownOptions = computed(() => {
-    let _views = [
-        {
-            group: 'Default Views',
-            items: defaultViews,
-        },
-    ]
-
-    let saved_views = [];
-    if (config_settings.data?.document_type) {
-        call('frappe.desk.doctype.view_config.view_config.get_views_for_doctype', {
-            doctype: config_settings.data.document_type
-        }).then((res) => {
-            res.map((v) => {
-                v.name != config_name.value && saved_views.push({
-                    label: v.label,
-                    icon: v.icon,
-                    onClick: () => {
-                        router.push({ query: { config: v.name } });
-                    }
-                });
-            });
-
-            saved_views.length && _views.push({
-                group: 'Saved Views',
-                items: saved_views,
-            });
-        });
-    }
-    return _views;
-});
-
 const handleFilterChange = async (filter) => {
     router.replace({ query: { ...route.query, [filter.field]: JSON.stringify([filter.operator, filter.value]) } });
     await list.fetch();
-    list_config.value.rows = list.data;
 };
 
 </script>
