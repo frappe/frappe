616152519f9acfb3b0e66f2fb0673f814f7d4238
Ankush Menat <ankush@frappe.io>
2024-05-27 20:00:35 +0530

    fix: Don't enable submit on quick entry with workflow

--- frappe/public/js/frappe/form/quick_entry.js
+++ frappe/public/js/frappe/form/quick_entry.js
@@ -191,7 +191,10 @@ frappe.ui.form.QuickEntryForm = class QuickEntryForm {
 					doc: me.dialog.doc,
 				},
 				callback: function (r) {
-					if (frappe.model.is_submittable(me.doctype)) {
+					if (
+						frappe.model.is_submittable(me.doctype) &&
+						!frappe.model.has_workflow(me.doctype)
+					) {
 						frappe.run_serially([
 							() => (me.dialog.working = true),
 							() => {
