3ff5a7fb14f702f93135aa4f5c755a307b3417b5
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-06-05 01:56:39 +0530

    feat: Icon Picker component

---
+++ desk/src/components/FormControls/IconPicker.vue
@@ -0,0 +1,75 @@
+<template>
+	<div class="flex flex-col">
+		<div v-if="label" class="mb-1.5 text-xs text-gray-600">{{ label }}</div>
+		<Popover transition="default">
+			<template #target="{ togglePopover, isOpen }">
+				<Button @click="togglePopover()" class="flex w-28 items-center !justify-start gap-1">
+					<template #prefix v-if="modelValue">
+						<Icon :name="modelValue" class="h-4 w-4 text-gray-700" />
+					</template>
+					{{ modelValue }}
+				</Button>
+			</template>
+
+			<template #body>
+				<div
+					class="mt-2 flex max-h-72 max-w-64 flex-col overflow-y-auto rounded bg-white pb-3 shadow hide-scrollbar"
+				>
+					<div class="sticky top-0 z-10 bg-white p-3">
+						<FormControl
+							type="text"
+							placeholder="Search for icons..."
+							v-model="searchText"
+							:debounce="200"
+							autocomplete="off"
+						/>
+					</div>
+					<div class="grid grid-cols-8 items-center px-3">
+						<button
+							v-for="icon in icons"
+							:key="icon"
+							@click="modelValue = icon"
+							:title="icon"
+							class="flex h-8 w-8 items-center justify-center rounded-md p-1.5 text-2xl hover:bg-gray-100 focus:outline-none"
+						>
+							<Icon :name="icon" class="h-5 w-5" />
+						</button>
+					</div>
+				</div>
+			</template>
+		</Popover>
+	</div>
+</template>
+
+<script setup>
+import { computed, ref } from "vue"
+import { Popover } from "frappe-ui"
+import Icon from "@/components/Icon.vue"
+import FormControl from "frappe-ui/src/components/FormControl.vue"
+
+const props = defineProps({
+	label: {
+		type: String,
+		required: false,
+	},
+})
+
+const modelValue = defineModel("modelValue", {
+	type: String,
+	default: "",
+})
+const searchText = ref("")
+
+const icons = computed(() => {
+	const iconList = []
+
+	const iconNodeList = searchText.value
+		? document.querySelectorAll(`#icons-timeless > symbol[id*="${searchText.value}"]`)
+		: document.querySelectorAll("#icons-timeless > symbol[id]")
+
+	iconNodeList.forEach((icon) => {
+		iconList.push(icon.id.replace("icon-", ""))
+	})
+	return iconList
+})
+</script>
