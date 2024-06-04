3d7381da5dada744e7fc185f1709e46df6597c9e
cwooldridge <cwooldridge@lopezfoods.com>
2024-06-04 00:33:58 -0500

    fix: Red was defined twice. (#26658)

--- frappe/desk/doctype/kanban_board_column/kanban_board_column.py
+++ frappe/desk/doctype/kanban_board_column/kanban_board_column.py
@@ -15,7 +15,7 @@ class KanbanBoardColumn(Document):
 
 		column_name: DF.Data | None
 		indicator: DF.Literal[
-			"Blue", "Cyan", "Gray", "Green", "Light Blue", "Orange", "Pink", "Purple", "Red", "Red", "Yellow"
+			"Blue", "Cyan", "Gray", "Green", "Light Blue", "Orange", "Pink", "Purple", "Red", "Yellow"
 		]
 		order: DF.Code | None
 		parent: DF.Data
