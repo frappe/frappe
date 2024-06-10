f5f1afa076b19e4ba424b00b3a72e4285af691ef
Nihantra Patel <nihantra@frappe.io>
2024-06-10 16:57:26 +0530

    fix: redirect report builder from workspace
    (cherry picked from commit 0de30c897d932b7956501d73aa9d0a420566ecba)
    
    # Conflicts:
    #	frappe/public/js/frappe/utils/utils.js

--- frappe/public/js/frappe/utils/utils.js
+++ frappe/public/js/frappe/utils/utils.js
@@ -1312,8 +1312,13 @@ Object.assign(frappe.utils, {
 					route = "/report/" + item.name;
 =======
 				} else if (!item.is_query_report && item.report_ref_doctype) {
+<<<<<<< HEAD
 					route = frappe.router.slug(item.report_ref_doctype) + "/view/report/" + item.name;
 >>>>>>> b4c4dbb418 (fix: redirect report builder from workspace)
+=======
+					route = 
+							frappe.router.slug(item.report_ref_doctype) + "/view/report/" + item.name;
+>>>>>>> 0de30c897d (fix: redirect report builder from workspace)
 				} else {
 					route = "/report/" + item.name;
 				}
