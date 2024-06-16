b72362dcc58ced2a42c37682bbd9f7c5091d654f
barredterra <14891507+barredterra@users.noreply.github.com>
2024-06-16 17:49:16 +0200

    feat: download Contact as vCard
    (cherry picked from commit b53203e14c7cc57ba2914d99b0257931c75b2761)

--- frappe/contacts/doctype/contact/contact.js
+++ frappe/contacts/doctype/contact/contact.js
@@ -93,6 +93,17 @@ frappe.ui.form.on("Contact", {
 				);
 			}
 		}
+
+		if (!frm.is_dirty()) {
+			frm.page.add_menu_item(__("Download vCard"), function () {
+				window.open(
+					`/api/method/frappe.contacts.doctype.contact.contact.download_vcard?contact=${encodeURIComponent(
+						frm.doc.name
+					)}`,
+					"_blank"
+				);
+			});
+		}
 	},
 	validate: function (frm) {
 		// clear linked customer / supplier / sales partner on saving...
