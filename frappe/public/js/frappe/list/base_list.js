a54b68e2da759ffa7af4ef3cd240b6047b3dc2f5
mahsem <137205921+mahsem@users.noreply.github.com>
2024-06-23 15:54:40 +0200

    fix: translation in base_list.js
    fix: translation in base_list.js

--- frappe/public/js/frappe/list/base_list.js
+++ frappe/public/js/frappe/list/base_list.js
@@ -206,7 +206,7 @@ frappe.views.BaseList = class BaseList {
 			__("Kanban View") __("Calendar View") __("Image View") __("Inbox View"),
 			__("Tree View") __("Map View") */
 			this.views_menu = this.page.add_custom_button_group(
-				__("{0} View", [this.view_name]),
+				__("{0} View", [__(this.view_name)]),
 				icon_map[this.view_name] || "list"
 			);
 			this.views_list = new frappe.views.ListViewSelect({
