47d79b168dd1980dc481b6b9117a2acb464e196f
barredterra <14891507+barredterra@users.noreply.github.com>
2024-06-27 18:38:05 +0200

    refactor: extract common methods for parsing df and links

--- frappe/gettext/extractors/custom_field.py
+++ frappe/gettext/extractors/custom_field.py
@@ -1,6 +1,6 @@
 import json
 
-from .utils import EXCLUDE_SELECT_OPTIONS
+from .utils import extract_messages_from_docfield
 
 
 def extract(fileobj, *args, **kwargs):
@@ -18,46 +18,7 @@ def extract(fileobj, *args, **kwargs):
 	messages = []
 
 	for custom_field in data:
-		print(custom_field)
-		fieldtype = custom_field.get("fieldtype")
-		fieldname = custom_field.get("fieldname")
-		label = custom_field.get("label")
 		doctype = custom_field.get("dt")
-
-		if label:
-			messages.append((label, f"Label of the '{fieldname}' ({fieldtype}) field in DocType '{doctype}'"))
-			_label = label
-		else:
-			_label = fieldname
-
-		if description := custom_field.get("description"):
-			messages.append(
-				(
-					description,
-					f"Description of the '{_label}' ({fieldtype}) field in DocType '{doctype}'",
-				)
-			)
-
-		if message := custom_field.get("options"):
-			if fieldtype == "Select":
-				if fieldname in EXCLUDE_SELECT_OPTIONS:
-					continue
-
-				select_options = [option for option in message.split("\n") if option and not option.isdigit()]
-
-				if select_options and "icon" in select_options[0]:
-					continue
-
-				messages.extend(
-					(
-						option,
-						f"Option for the '{_label}' ({fieldtype}) field in DocType '{doctype}'",
-					)
-					for option in select_options
-				)
-			elif fieldtype == "HTML":
-				messages.append(
-					(message, f"Content of the '{_label}' ({fieldtype}) field in DocType '{doctype}'")
-				)
+		messages.extend(extract_messages_from_docfield(doctype, custom_field))
 
 	yield from ((None, "_", message, [comment]) for message, comment in messages)
--- frappe/gettext/extractors/customization.py
+++ frappe/gettext/extractors/customization.py
@@ -1,6 +1,6 @@
 import json
 
-from .utils import EXCLUDE_SELECT_OPTIONS
+from .utils import EXCLUDE_SELECT_OPTIONS, extract_messages_from_docfield, extract_messages_from_links
 
 
 def extract(fileobj, *args, **kwargs):
@@ -19,48 +19,9 @@ def extract(fileobj, *args, **kwargs):
 	messages = []
 
 	for field in data.get("custom_fields", []):
-		fieldtype = field.get("fieldtype")
-		fieldname = field.get("fieldname")
-		label = field.get("label")
+		messages.extend(extract_messages_from_docfield(doctype, field))
 
-		if label:
-			messages.append((label, f"Label of the '{fieldname}' ({fieldtype}) field in DocType '{doctype}'"))
-			_label = label
-		else:
-			_label = fieldname
-
-		if description := field.get("description"):
-			messages.append(
-				(
-					description,
-					f"Description of the '{_label}' ({fieldtype}) field in DocType '{doctype}'",
-				)
-			)
-
-		if message := field.get("options"):
-			if fieldtype == "Select":
-				if fieldname in EXCLUDE_SELECT_OPTIONS:
-					continue
-
-				select_options = [option for option in message.split("\n") if option and not option.isdigit()]
-
-				if select_options and "icon" in select_options[0]:
-					continue
-
-				messages.extend(
-					(
-						option,
-						f"Option for the '{_label}' ({fieldtype}) field in DocType '{doctype}'",
-					)
-					for option in select_options
-				)
-			elif fieldtype == "HTML":
-				messages.append(
-					(
-						message,
-						f"Content of the '{_label}' ({fieldtype}) field in DocType '{doctype}'",
-					)
-				)
+	messages.extend(extract_messages_from_links(doctype, data.get("links", [])))
 
 	for property_setter in data.get("property_setters", []):
 		if property_setter.get("doctype_or_field") != "DocField":
@@ -100,12 +61,4 @@ def extract(fileobj, *args, **kwargs):
 				)
 			)
 
-	for link in data.get("links", []):
-		group = link.get("group")
-		if not group:
-			continue
-
-		messages.append((group, f"Connections link group in DocType '{doctype}'"))
-
-	# By using "pgettext" as the function name we can supply the doctype as context
 	yield from ((None, "_", message, [comment]) for message, comment in messages)
