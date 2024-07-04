c9f05e71bf09ce81c9ad774b9af35aed495522f3
Akhil Narang <me@akhilnarang.dev>
2024-07-04 17:28:48 +0530

    fix: Revert "fix: refresh fields after changing form to read-only (#26439)"
    This reverts commit cc8c0f917cefb3eebc2b4a2c7ebba2a8a768763a.
    
    This also reverts the attemped fix: #26983
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    (cherry picked from commit 216015f49ebac5d590c953d1aa1002d2a73cfc19)

--- frappe/public/js/frappe/form/form.js
+++ frappe/public/js/frappe/form/form.js
@@ -664,7 +664,7 @@ frappe.ui.form.Form = class FrappeForm {
 	}
 
 	refresh_fields() {
-		this?.layout?.refresh(this.doc);
+		this.layout.refresh(this.doc);
 		this.layout.primary_button = this.$wrapper.find(".btn-primary");
 
 		// cleanup activities after refresh
@@ -1760,7 +1760,6 @@ frappe.ui.form.Form = class FrappeForm {
 				email: p.email,
 			};
 		});
-		this.refresh_fields();
 	}
 
 	trigger(event, doctype, docname) {
