111a24efc1f6c926064f4cddd6c92be1f5f29dcd
barredterra <14891507+barredterra@users.noreply.github.com>
2024-07-10 17:12:02 +0200

    Revert "fix(UX): Nudge to disable user instead of deleting"
    This reverts commit 081be53e17b9e55a55b5c502e4976008a020f297.

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -17,7 +17,6 @@ from frappe.desk.doctype.notification_settings.notification_settings import (
 	toggle_notifications,
 )
 from frappe.desk.notifications import clear_notifications
-from frappe.model.delete_doc import check_if_doc_is_linked
 from frappe.model.document import Document
 from frappe.query_builder import DocType
 from frappe.rate_limiter import rate_limit
@@ -535,12 +534,6 @@ class User(Document):
 
 		# Delete EPS data
 		frappe.db.delete("Energy Point Log", {"user": self.name})
-
-		# Ask user to disable instead if document is still linked
-		try:
-			check_if_doc_is_linked(self)
-		except frappe.LinkExistsError:
-			frappe.throw(_("You can disable the user instead of deleting it."), frappe.LinkExistsError)
 
 	def before_rename(self, old_name, new_name, merge=False):
 		# if merging, delete the old user notification settings
