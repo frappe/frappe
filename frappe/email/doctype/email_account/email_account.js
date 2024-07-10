928428c6a7b0f5cf0ba6d85320a789b474fff977
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2024-07-10 12:36:51 +0530

    feat(Email): manually pull email from given email account

--- frappe/email/doctype/email_account/email_account.js
+++ frappe/email/doctype/email_account/email_account.js
@@ -155,11 +155,14 @@ frappe.ui.form.on("Email Account", {
 			delete locals["User"][frappe.route_flags.linked_user];
 		}
 
-		if (frappe.boot.developer_mode && !frm.is_dirty() && frm.doc.enable_incoming) {
+		if (!frm.is_dirty() && frm.doc.enable_incoming) {
 			frm.add_custom_button(__("Pull Emails"), () => {
+				frappe.show_alert({ message: __("Pulling Emails..."), indicator: "blue" });
 				frm.call({
 					method: "pull_emails",
 					args: { email_account: frm.doc.name },
+				}).then(() => {
+					frappe.show_alert({ message: __("Emails Pulled"), indicator: "green" });
 				});
 			});
 		}
