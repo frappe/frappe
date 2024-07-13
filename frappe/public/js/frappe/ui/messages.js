5d0b9982faeb08e789b4983dbe1d8a63c28991a7
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2024-07-13 10:43:29 +0530

    fix: precommit check

--- frappe/public/js/frappe/ui/messages.js
+++ frappe/public/js/frappe/ui/messages.js
@@ -240,7 +240,9 @@ frappe.msgprint = function (msg, title, is_minimizable) {
 
 	if (data.secondary_action) {
 		frappe.msg_dialog.set_secondary_action(data.secondary_action.action);
-		frappe.msg_dialog.set_secondary_action_label(__(data.secondary_action.label) || __("Close"));
+		frappe.msg_dialog.set_secondary_action_label(
+			__(data.secondary_action.label) || __("Close")
+		);
 	}
 
 	if (data.message == null) {
