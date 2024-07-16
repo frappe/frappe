f508338a5ffa8626d22dce6c4aa67e6b19824f70
Arun <me@arunmathaisk.in>
2024-07-16 12:28:28 +0530

    chore: remove commented code

--- frappe/integrations/frappe_providers/__init__.py
+++ frappe/integrations/frappe_providers/__init__.py
@@ -3,25 +3,6 @@
 
 from frappe.integrations.frappe_providers.frappecloud import frappecloud_migrator
 
-# def is_working_press(remote_site_name: str):
-# 	try:
-# 		print(f"Checking if {remote_site_name} is a working press")
-# 		request_url = f"http://{remote_site_name}/api/method/press.api.script"
-# 		request = requests.get(request_url)
-# 		if request.status_code / 100 != 2:
-# 			click.secho(
-# 			"Does not seem to be a working press",
-# 			fg="red",
-# 			)
-# 			return False
-# 		return True
-# 	except:
-# 		click.secho(
-# 			"Does not seem to be a valid domain",
-# 			fg="yellow",
-# 		)
-# 		return False
-
 
 def migrate_to():
 	return frappecloud_migrator()
