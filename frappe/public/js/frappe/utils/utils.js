23720f109a3065936bf019d767eb91d0ec704157
El-Shafei H. <el.shafei.developer@gmail.com>
2024-07-20 12:11:12 +0300

    fix: add translation function (__) to route label text
    This commit updates the get_route_label function to include the translation function (__) for route parts that require translation. 
    
    Changes made:
    - Added __

--- frappe/public/js/frappe/utils/utils.js
+++ frappe/public/js/frappe/utils/utils.js
@@ -918,16 +918,16 @@ Object.assign(frappe.utils, {
 		let route = route_str.split("/");
 
 		if (route[2] === "Report" || route[0] === "query-report") {
-			return __("{0} Report", [route[3] || route[1]]);
+			return __("{0} Report", [__(route[3]) || __(route[1])]);
 		}
 		if (route[0] === "List") {
-			return __("{0} List", [route[1]]);
+			return __("{0} List", [__(route[1])]);
 		}
 		if (route[0] === "modules") {
-			return __("{0} Modules", [route[1]]);
+			return __("{0} Modules", [__(route[1])]);
 		}
 		if (route[0] === "dashboard") {
-			return __("{0} Dashboard", [route[1]]);
+			return __("{0} Dashboard", [__(route[1])]);
 		}
 		return __(frappe.utils.to_title_case(route[0], true));
 	},
