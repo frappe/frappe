7ff7ad32ad6c348dd0dde0aaff550c18e6a0ae29
ALB.Leach <alexleach@users.noreply.github.com>
2024-07-22 12:17:18 +0100

    feat: Implement OAuth Backend App Flow for Email Accounts (#27167)
    * feat: Implement OAuth Backend App Flow for Email Accounts
    
    * chore: Reformat to satisfy linter
    
    * chore: format
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    
    ---------
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    Co-authored-by: Akhil Narang <me@akhilnarang.dev>
    (cherry picked from commit dde466be3d8e78d0ca3ea4de1f8da4899a60b4d3)
    
    # Conflicts:
    #	frappe/email/doctype/email_account/email_account.json

--- frappe/email/doctype/email_account/email_account.js
+++ frappe/email/doctype/email_account/email_account.js
@@ -166,7 +166,11 @@ frappe.ui.form.on("Email Account", {
 	},
 
 	show_oauth_authorization_message(frm) {
-		if (frm.doc.auth_method === "OAuth" && frm.doc.connected_app) {
+		if (
+			frm.doc.auth_method === "OAuth" &&
+			frm.doc.connected_app &&
+			!frm.doc.backend_app_flow
+		) {
 			frappe.call({
 				method: "frappe.integrations.doctype.connected_app.connected_app.has_token",
 				args: {
--- frappe/email/doctype/email_account/email_account.py
+++ frappe/email/doctype/email_account/email_account.py
@@ -68,6 +68,7 @@ class EmailAccount(Document):
 		auth_method: DF.Literal["Basic", "OAuth"]
 		auto_reply_message: DF.TextEditor | None
 		awaiting_password: DF.Check
+		backend_app_flow: DF.Check
 		brand_logo: DF.AttachImage | None
 		connected_app: DF.Link | None
 		connected_user: DF.Link | None
@@ -732,7 +733,12 @@ class EmailAccount(Document):
 	def get_oauth_token(self):
 		if self.auth_method == "OAuth":
 			connected_app = frappe.get_doc("Connected App", self.connected_app)
-			return connected_app.get_active_token(self.connected_user)
+			if self.backend_app_flow:
+				token = connected_app.get_backend_app_token()
+			else:
+				token = connected_app.get_active_token(self.connected_user)
+
+			return token
 
 
 @frappe.whitelist()
@@ -831,8 +837,10 @@ def pull(now=False):
 	)
 
 	for email_account in email_accounts:
-		if email_account.auth_method == "OAuth" and not has_token(
-			email_account.connected_app, email_account.connected_user
+		if (
+			email_account.auth_method == "OAuth"
+			and not email_account.backend_app_flow
+			and not has_token(email_account.connected_app, email_account.connected_user)
 		):
 			# don't try to pull from accounts which dont have access token (for Oauth)
 			continue
