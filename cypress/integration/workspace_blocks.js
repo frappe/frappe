d545f30b22d62e9aa267680140b927458c58b988
Rushabh Mehta <rmehta@gmail.com>
2024-07-29 07:58:10 +0530

    fix(tests): [Workspace] Fix tests for actions in footer

--- cypress/integration/workspace_blocks.js
+++ cypress/integration/workspace_blocks.js
@@ -18,7 +18,7 @@ context("Workspace Blocks", () => {
 
 		cy.visit("/app/website");
 		cy.get(".codex-editor__redactor .ce-block");
-		cy.get('.workspace-footer button[data-label="New"]:visible').click();
+		cy.get(".btn-new-workspace").click();
 		cy.fill_field("title", "Test Block Page", "Data");
 		cy.get_open_dialog().find(".modal-header").click();
 		cy.get_open_dialog().find(".btn-primary").click();
@@ -71,7 +71,7 @@ context("Workspace Blocks", () => {
 		}).as("get_doctype");
 
 		cy.get(".codex-editor__redactor .ce-block");
-		cy.get(".standard-actions .btn-secondary[data-label=Edit]").click();
+		cy.get(".standard-actions .btn[data-label=Edit]").click();
 
 		// test quick list creation
 		cy.get(".ce-block").first().click({ force: true }).type("{enter}");
@@ -159,7 +159,7 @@ context("Workspace Blocks", () => {
 		]);
 
 		cy.get(".codex-editor__redactor .ce-block");
-		cy.get(".workspace-footer .btn[data-label=Edit]").click();
+		cy.get(".btn-edit-workspace").click();
 
 		cy.get(".ce-block").first().click({ force: true }).type("{enter}");
 		cy.get(".block-list-container .block-list-item").contains("Number Card").click();
