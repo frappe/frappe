def47446c3bfd55772d6b227b00f5580b48f4e24
Bevan Tony Medrano <medranobevantony@gmail.com>
2024-08-01 20:54:10 +0800

    feat:(notification)add user group as recipient column

--- frappe/email/doctype/notification/notification.py
+++ frappe/email/doctype/notification/notification.py
@@ -271,6 +271,12 @@ def get_context(context):
 			if recipient.condition:
 				if not frappe.safe_eval(recipient.condition, None, context):
 					continue
+
+			if recipient.user_group:
+				user_group = frappe.get_list("User Group Member", {'parent':recipient.user_group}, 'user')
+				for member in user_group:
+					recipients.append(member.user)
+
 			if recipient.receiver_by_document_field:
 				fields = recipient.receiver_by_document_field.split(",")
 				# fields from child table
