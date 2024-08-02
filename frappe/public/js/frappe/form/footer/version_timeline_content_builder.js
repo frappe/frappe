84fd056439a132ccee07c93af69809a172db2d49
Osama Shaikh <31347530+0samashaikh@users.noreply.github.com>
2024-08-02 16:18:36 +0530

    fix: Allow track changes for Roles and Allowed Modules child tables in User DocType (#26841)
    * fix: Allow track changes for Roles and Allowed Modules child tables in User DocType
    
    * fix: lint
    
    * feat: config in docfield to show field in timeline
    
    * fix: enable 'Show on Timeline' only when hidden is checked

--- frappe/public/js/frappe/form/footer/version_timeline_content_builder.js
+++ frappe/public/js/frappe/form/footer/version_timeline_content_builder.js
@@ -83,13 +83,17 @@ function get_version_timeline_content(version_doc, frm) {
 				}
 			} else {
 				const df = frappe.meta.get_docfield(frm.doctype, p[0], frm.docname);
-				if (df && !df.hidden) {
+				if (df && (!df.hidden || df.show_on_timeline)) {
 					const field_display_status = frappe.perm.get_field_display_status(
 						df,
 						null,
 						frm.perm
 					);
-					if (field_display_status === "Read" || field_display_status === "Write") {
+					if (
+						field_display_status === "Read" ||
+						field_display_status === "Write" ||
+						(df.hidden && df.show_on_timeline)
+					) {
 						parts.push(
 							__("{0} from {1} to {2}", [
 								__(df.label, null, df.parent),
@@ -142,14 +146,18 @@ function get_version_timeline_content(version_doc, frm) {
 						frm.docname
 					);
 
-				if (df && !df.hidden) {
+				if (df && (!df.hidden || df.show_on_timeline)) {
 					var field_display_status = frappe.perm.get_field_display_status(
 						df,
 						null,
 						frm.perm
 					);
 
-					if (field_display_status === "Read" || field_display_status === "Write") {
+					if (
+						field_display_status === "Read" ||
+						field_display_status === "Write" ||
+						(df.hidden && df.show_on_timeline)
+					) {
 						parts.push(
 							__("{0} from {1} to {2} in row #{3}", [
 								frappe.meta.get_label(frm.fields_dict[row[0]].grid.doctype, p[0]),
@@ -197,14 +205,19 @@ function get_version_timeline_content(version_doc, frm) {
 		if (data[key] && data[key].length) {
 			let parts = (data[key] || []).map(function (p) {
 				var df = frappe.meta.get_docfield(frm.doctype, p[0], frm.docname);
-				if (df && !df.hidden) {
+
+				if (df && (!df.hidden || df.show_on_timeline)) {
 					var field_display_status = frappe.perm.get_field_display_status(
 						df,
 						null,
 						frm.perm
 					);
 
-					if (field_display_status === "Read" || field_display_status === "Write") {
+					if (
+						field_display_status === "Read" ||
+						field_display_status === "Write" ||
+						(df.hidden && df.show_on_timeline)
+					) {
 						return __(frappe.meta.get_label(frm.doctype, p[0]));
 					}
 				}
