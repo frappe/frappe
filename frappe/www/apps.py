8b54e62f7c4b8c1382d13ee819d2816c534d99e3
Shariq Ansari <sharique.rik@gmail.com>
2024-08-02 20:10:57 +0530

    feat: Apps page
    (cherry picked from commit 69bb43ab138678f7c42f2f50dbb6e3e31cdd2625)

---
+++ frappe/www/apps.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+import frappe
+from frappe import _
+
+
+def get_context():
+	_apps = frappe.get_installed_apps()
+	app_shortcuts = [
+		{
+			"name": "frappe",
+			"icon_url": "/assets/frappe/images/frappe-framework-logo.svg",
+			"title": _("Admin"),
+			"route": "/app",
+		}
+	]
+	for app in _apps:
+		app_icon_url = frappe.get_hooks("app_icon_url", app_name=app)
+		app_icon_route = frappe.get_hooks("app_icon_route", app_name=app)
+		if app_icon_url and app_icon_route:
+			app_title = frappe.get_hooks("app_title", app_name=app)
+			icon_title = frappe.get_hooks("app_icon_title", app_name=app)
+			app_shortcuts.append(
+				{
+					"name": app,
+					"icon_url": app_icon_url[0],
+					"title": _(icon_title[0] if icon_title else app_title[0] if app_title else app),
+					"route": app_icon_route[0],
+				}
+			)
+	return {"apps": app_shortcuts}
