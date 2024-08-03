baf1ac26719068bc6d41f5106a5aa33412d5d4b4
s-aga-r <sagarsharma.s312@gmail.com>
2024-08-03 13:21:14 +0530

    chore: `conflicts`

--- frappe/desk/doctype/notification_log/notification_log.py
+++ frappe/desk/doctype/notification_log/notification_log.py
@@ -29,7 +29,6 @@ class NotificationLog(Document):
 		read: DF.Check
 		subject: DF.Text | None
 		type: DF.Literal["", "Mention", "Energy Point", "Assignment", "Share", "Alert"]
-	# end: auto-generated types
 
 	# end: auto-generated types
 	def after_insert(self):
