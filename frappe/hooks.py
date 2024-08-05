15a2c325466b8084d6058a627fce12fd64cda9e6
Shariq Ansari <sharique.rik@gmail.com>
2024-08-05 16:08:49 +0530

    fix: redirect to apps if multiple apps installed or redirect to apps portal
    (cherry picked from commit 67eb28d83633697c048f94eec9dfc2d126dd6ace)

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -487,7 +487,7 @@ standard_navbar_items = [
 		"is_standard": 1,
 	},
 	{
-		"item_label": "App Launcher",
+		"item_label": "Apps",
 		"item_type": "Route",
 		"route": "/apps",
 		"is_standard": 1,
