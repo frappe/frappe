519bdec3e9b3e4f5a60886aa35ef3a8aff449608
Rushabh Mehta <rmehta@gmail.com>
2024-08-07 09:21:57 +0530

    fix(test): complete setup before running UI tests

--- frappe/core/doctype/navbar_settings/navbar_settings.py
+++ frappe/core/doctype/navbar_settings/navbar_settings.py
@@ -65,22 +65,20 @@ def sync_standard_items():
 def sync_table(key, hook):
 	navbar_settings = NavbarSettings("Navbar Settings")
 	existing_items = {d.item_label: d for d in navbar_settings.get(key)}
-	new_items = {}
+	all_items = {}
 
 	# add new items
 	count = 0  # matain count because list may come from seperate apps
 	for item in frappe.get_hooks(hook):
 		if item.get("item_label") not in existing_items:
 			navbar_settings.append(key, item, count)
-		new_items[item.get("item_label")] = True
+		all_items[item.get("item_label")] = True
 		count += 1
 
 	# remove unused items
-	def fn(item):
-		if item.is_standard and (item.item_label not in new_items):
-			return False
-		else:
-			return True
+	items = navbar_settings.get(key)
+	for item in navbar_settings.get(key):
+		if item.is_standard and (item.item_label not in all_items):
+			items.remove(item)
 
-	navbar_settings.set(key, filter(lambda item: fn, navbar_settings.get(key)))
 	navbar_settings.save()
