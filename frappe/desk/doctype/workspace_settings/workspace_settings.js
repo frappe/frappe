d4e7551481665124c3b5a7c087362af92fd18b70
Rushabh Mehta <rmehta@gmail.com>
2024-08-07 20:41:32 +0530

    fix(minor): add column break in workspace settings modal

--- frappe/desk/doctype/workspace_settings/workspace_settings.js
+++ frappe/desk/doctype/workspace_settings/workspace_settings.js
@@ -8,14 +8,23 @@ frappe.ui.form.on("Workspace Settings", {
 		let workspace_visibilty = JSON.parse(frm.doc.workspace_visibility_json || "{}");
 
 		// build fields from workspaces
+		let cnt = 0,
+			column_added = false;
 		for (let w of frappe.boot.allowed_workspaces) {
 			if (w.public) {
+				cnt++;
 				frm.docfields.push({
 					fieldtype: "Check",
 					fieldname: w.name,
 					label: w.title,
 					initial_value: workspace_visibilty[w.name] !== 0, // not set is also visible
 				});
+			}
+
+			if (cnt >= frappe.boot.allowed_workspaces.length / 2 && !column_added) {
+				// add column break to split into 2 columns
+				frm.docfields.push({ fieldtype: "Column Break" });
+				column_added = true;
 			}
 		}
 
