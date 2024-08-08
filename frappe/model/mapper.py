19ded8361b527821c76c96922a04b08a51cfcac1
Ninad1306 <ninad_1063@yahoo.com>
2024-08-08 17:54:09 +0530

    fix: before mapping the doc run before_mapping hook

--- frappe/model/mapper.py
+++ frappe/model/mapper.py
@@ -103,6 +103,8 @@ def get_mapped_doc(
 		if not source_doc.has_permission("read"):
 			source_doc.raise_no_permission_to("read")
 
+	ret_doc.run_method("before_mapping", source_doc, table_maps)
+
 	map_doc(source_doc, target_doc, table_maps[source_doc.doctype])
 
 	row_exists_for_parentfield = {}
