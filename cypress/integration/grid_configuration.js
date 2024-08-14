a464040179040ee900528a75c518f5b67438bc51
Rushabh Mehta <rmehta@gmail.com>
2024-08-14 20:09:02 +0530

    fix(test): grid_configuration.js

--- cypress/integration/grid_configuration.js
+++ cypress/integration/grid_configuration.js
@@ -1,11 +1,10 @@
 context("Grid Configuration", () => {
 	beforeEach(() => {
 		cy.login();
-		cy.visit("/app/doctype/User");
+		cy.visit("/app/website-settings");
 	});
 	it("Set user wise grid settings", () => {
-		cy.findByRole("tab", { name: "Fields" }).click();
-		cy.scrollTo("top");
+		cy.findByRole("tab", { name: "Navbar" }).click();
 		cy.get('.form-section[data-fieldname="fields_section"]').click();
 		cy.wait(100);
 		cy.get('.frappe-control[data-fieldname="fields"]').as("table");
@@ -14,13 +13,13 @@ context("Grid Configuration", () => {
 		cy.get('.frappe-control[data-fieldname="fields_html"]').as("modal");
 		cy.get("@modal").find(".add-new-fields").click();
 		cy.wait(100);
-		cy.get('[type="checkbox"][data-unit="read_only"]').check();
+		cy.get('[type="checkbox"][data-unit="right"]').check();
 		cy.findByRole("button", { name: "Add" }).click();
 		cy.wait(100);
-		cy.get('[data-fieldname="options"]').invoke("attr", "value", "1");
-		cy.get('.form-control.column-width[data-fieldname="options"]').trigger("change");
+		cy.get('[data-fieldname="parent_label"]').invoke("attr", "value", "1");
+		cy.get('.form-control.column-width[data-fieldname="parent_label"]').trigger("change");
 		cy.findByRole("button", { name: "Update" }).click();
 		cy.wait(200);
-		cy.get('[title="Read Only"').should("be.visible");
+		cy.get('[title="Align Right"').should("be.visible");
 	});
 });
