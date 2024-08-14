bdef8d7187185e85108a936d085bf2cf088a1c5f
Rushabh Mehta <rmehta@gmail.com>
2024-08-14 21:36:14 +0530

    fix(test): grid_configuration.js

--- cypress/integration/grid_configuration.js
+++ cypress/integration/grid_configuration.js
@@ -5,17 +5,16 @@ context("Grid Configuration", () => {
 	});
 	it("Set user wise grid settings", () => {
 		cy.findByRole("tab", { name: "Navbar" }).click();
-		cy.get('.form-section[data-fieldname="fields_section"]').click();
 		cy.wait(100);
-		cy.get('.frappe-control[data-fieldname="fields"]').as("table");
+		cy.get('.frappe-control[data-fieldname="top_bar_items"]').as("table");
 		cy.get("@table").find(".icon-sm").click();
 		cy.wait(100);
 		cy.get('.frappe-control[data-fieldname="fields_html"]').as("modal");
 		cy.get("@modal").find(".add-new-fields").click();
 		cy.wait(100);
 		cy.get('[type="checkbox"][data-unit="right"]').check();
-		cy.findByRole("button", { name: "Add" }).click();
 		cy.wait(100);
+		cy.findByRole("button", { name: "Add" }).wait(100).click();
 		cy.get('[data-fieldname="parent_label"]').invoke("attr", "value", "1");
 		cy.get('.form-control.column-width[data-fieldname="parent_label"]').trigger("change");
 		cy.findByRole("button", { name: "Update" }).click();
