985abcd60887e74e602f6a6f48fb00a69a36763d
Corentin Forler <corentin@dokos.io>
2024-08-20 14:38:32 +0200

    refactor(File): Move validations to before_insert
    (cherry picked from commit 49ec84206635033f4d5bd7dd611173f2f61ce0c5)

--- frappe/core/doctype/file/file.py
+++ frappe/core/doctype/file/file.py
@@ -59,6 +59,7 @@ class File(Document):
 
 	def before_insert(self):
 		self.set_folder_name()
+		self.set_is_private()
 		self.set_file_name()
 		self.validate_attachment_limit()
 		self.set_file_type()
@@ -73,12 +74,11 @@ class File(Document):
 			self.flags.new_file = True
 			frappe.local.rollback_observers.append(self)
 
+		self.validate_duplicate_entry()  # Hash is generated in save_file
+
 	def after_insert(self):
 		if not self.is_folder:
 			self.create_attachment_record()
-		self.set_is_private()
-		self.set_file_name()
-		self.validate_duplicate_entry()
 
 	def validate(self):
 		if self.is_folder:
