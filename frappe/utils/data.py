7529c6601ed4a98c522aff792ef7b55c63a0de45
vishnu <vishnuviswambara2002@gmail.com>
2024-08-21 18:07:03 +0000

    chore: removed extra functiion
    (cherry picked from commit ace2122e92e7c19339b055e849578d765cd4080e)
    
    # Conflicts:
    #	frappe/utils/data.py

--- frappe/utils/data.py
+++ frappe/utils/data.py
@@ -731,6 +731,7 @@ def get_weekday(datetime: datetime.datetime | None = None) -> str:
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 def get_timespan_date_range(timespan: str) -> tuple[datetime.datetime, datetime.datetime]:
 	today = nowdate()
 	date_range_map = {
@@ -817,6 +818,8 @@ def get_months() -> dict[int, str]:
 	}
 
 
+=======
+>>>>>>> ace2122e92 (chore: removed extra functiion)
 def get_month(datetime: DateTimeLikeObject | None = None) -> str:
 	"""Return the month name (e.g. 'January') for the given datetime like object (datetime.date, datetime.datetime, string).
 
