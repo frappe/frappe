115f2337dea9c3507b33611b945d408e080e24c0
Akhil Narang <me@akhilnarang.dev>
2024-08-21 18:18:52 +0530

    refactor: change logic slightly, reformat JSON
    Invert the check: `disable_edit` -> `allow_edit`
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    (cherry picked from commit 2c3916f8988fd1212ca2013639fbe0cd2808f724)
    
    # Conflicts:
    #	frappe/desk/doctype/list_view_settings/list_view_settings.json

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -1835,11 +1835,12 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 			return frappe.perm.has_perm(doctype, 0, "submit");
 		};
 
-		const is_bulk_editable = (doctype) => {
-			if (this.list_view_settings && this.list_view_settings.disable_edit != undefined) {
-				return !this.list_view_settings.disable_edit;
+		const is_bulk_edit_allowed = (doctype) => {
+			// Check settings if there is a workflow defined, otherwise directly allow
+			if (frappe.model.has_workflow(doctype)) {
+				return !!this.list_view_settings?.allow_edit;
 			}
-			return !frappe.model.has_workflow(doctype);
+			return true;
 		};
 
 		// utility
@@ -2041,7 +2042,7 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 		};
 
 		// bulk edit
-		if (has_editable_fields(doctype) && is_bulk_editable(doctype)) {
+		if (has_editable_fields(doctype) && is_bulk_edit_allowed(doctype)) {
 			actions_menu_items.push(bulk_edit());
 		}
 
