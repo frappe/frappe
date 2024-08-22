fbb183cc5f2cc07a69157a7ae407547437d101be
Akhil Narang <me@akhilnarang.dev>
2024-08-22 15:25:16 +0530

    fix(query_report): sentry FRAPPE-6P0
    TypeError: Cannot read properties of undefined (reading 'ref_doctype')
      at <anonymous>(../../../../../apps/frappe/frappe/public/js/frappe/views/reports/query_report.js:417:58)
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    (cherry picked from commit 7547937521d76414c0beb6212ed79aa104e82a15)

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -397,7 +397,7 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 			.then((doc) => {
 				this.report_doc = doc;
 			})
-			.then(() => frappe.model.with_doctype(this.report_doc.ref_doctype));
+			.then(() => frappe.model.with_doctype(this.report_doc?.ref_doctype));
 	}
 
 	get_report_settings() {
