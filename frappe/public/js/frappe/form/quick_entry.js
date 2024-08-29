0fe8a287c22badc406e5babc0cc543c07f5b06bb
Le Hoang Long <le.hoang.long@outlook.com>
2024-08-29 19:56:13 +0700

    Add bugfix

--- frappe/public/js/frappe/form/quick_entry.js
+++ frappe/public/js/frappe/form/quick_entry.js
@@ -34,6 +34,7 @@ frappe.ui.form.QuickEntryForm = class QuickEntryForm extends frappe.ui.Dialog {
 		this.doc = doc;
 		this.force = force ? force : false;
 		this.dialog = this; // for backward compatibility
+		this.fetch_dict = {};
 	}
 
 	setup() {
@@ -58,6 +59,43 @@ frappe.ui.form.QuickEntryForm = class QuickEntryForm extends frappe.ui.Dialog {
 				}
 			});
 		});
+	}
+
+	add_custom_button(label, fn, group) {
+		// temp! old parameter used to be icon
+		if (group && group.indexOf("fa fa-") !== -1) group = null;
+
+		let btn = this.page.add_inner_button(label, fn, group);
+
+		if (btn) {
+			// Add actions as menu item in Mobile View
+			let menu_item_label = group ? `${group} > ${label}` : label;
+			let menu_item = this.page.add_menu_item(menu_item_label, fn, false);
+			menu_item.parent().addClass("hidden-xl");
+
+			this.custom_buttons[label] = btn;
+		}
+		return btn;
+	}
+
+	// UTILITIES
+	add_fetch(link_field, source_field, target_field, target_doctype) {
+		/*
+		Example fetch dict to get sender_email from email_id field in sender:
+			{
+				"Notification": {
+					"sender": {
+						"sender_email": "email_id"
+					}
+				}
+			}
+		*/
+
+		if (!target_doctype) target_doctype = "*";
+
+		// Target field kept as key because source field could be non-unique
+		this.fetch_dict.setDefault(target_doctype, {}).setDefault(link_field, {})[target_field] =
+			source_field;
 	}
 
 	set_meta_and_mandatory_fields() {
