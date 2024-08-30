f2e8f055583dc329f62ffb41382773739a820159
Shariq Ansari <sharique.rik@gmail.com>
2024-08-30 16:23:24 +0530

    fix: redirect to /app if no apps are installed

--- frappe/auth.py
+++ frappe/auth.py
@@ -184,7 +184,7 @@ class LoginManager:
 			frappe.local.cookie_manager.set_cookie("system_user", "yes")
 			if not resume:
 				frappe.local.response["message"] = "Logged In"
-				frappe.local.response["home_page"] = get_default_path()
+				frappe.local.response["home_page"] = get_default_path() or "/app"
 
 		if not resume:
 			frappe.response["full_name"] = self.full_name
