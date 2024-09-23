dc21fbcb563a7f61a01b4667bfd0f8cf22c15ce6
Rushabh Mehta <rmehta@gmail.com>
2024-09-23 20:50:43 +0530

    fix(minor): set default app as frappe in workspace if no app or module defined

--- frappe/desk/doctype/workspace/workspace.py
+++ frappe/desk/doctype/workspace/workspace.py
@@ -267,7 +267,7 @@ def new_page(new_page):
 
 	doc = frappe.new_doc("Workspace")
 	doc.title = page.get("title")
-	doc.icon = page.get("icon") or "dashboard"
+	doc.icon = page.get("icon") or "grid"
 	doc.indicator_color = page.get("indicator_color")
 	doc.content = page.get("content")
 	doc.parent_page = page.get("parent_page")
