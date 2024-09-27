e9ec270da3fcceea16f373b04fa53c6410a0289d
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2024-09-27 13:03:56 +0530

    fix: default to frappe logo when more than 2 app logos are present
    (cherry picked from commit 759f8306b6e274a7a4e3dceed6c507365796c6d6)

--- frappe/www/login.py
+++ frappe/www/login.py
@@ -8,6 +8,7 @@ import frappe
 import frappe.utils
 from frappe import _
 from frappe.auth import LoginManager
+from frappe.core.doctype.navbar_settings.navbar_settings import get_app_logo
 from frappe.rate_limiter import rate_limit
 from frappe.utils import cint, get_url
 from frappe.utils.data import escape_html
@@ -41,7 +42,7 @@ def get_context(context):
 	context["provider_logins"] = []
 	context["disable_signup"] = cint(frappe.get_website_settings("disable_signup"))
 	context["disable_user_pass_login"] = cint(frappe.get_system_settings("disable_user_pass_login"))
-	context["logo"] = frappe.get_website_settings("app_logo") or frappe.get_hooks("app_logo_url")[-1]
+	context["logo"] = get_app_logo()
 	context["app_name"] = (
 		frappe.get_website_settings("app_name") or frappe.get_system_settings("app_name") or _("Frappe")
 	)
