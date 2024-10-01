cb83427dd00b5b4ccd4205dad139a3c1fcb3175a
aartimanjare <aartimanjare@gmail.com>
2024-10-01 14:40:23 +0530

    fix: leaderboard error resolved

--- frappe/desk/leaderboard.py
+++ frappe/desk/leaderboard.py
@@ -15,36 +15,38 @@ def get_leaderboards():
 
 @frappe.whitelist()
 def get_energy_point_leaderboard(date_range, company=None, field=None, limit=None):
-	users = frappe.get_list(
-		"User",
-		filters={
-			"name": ["not in", ["Administrator", "Guest"]],
-			"enabled": 1,
-			"user_type": ["!=", "Website User"],
-		},
-		pluck="name",
-	)
+    users = frappe.get_list(
+        "User",
+        filters={
+            "name": ["not in", ["Administrator", "Guest"]],
+            "enabled": 1,
+            "user_type": ["!=", "Website User"],
+        },
+        pluck="name",
+    )
 
-	filters = [["type", "!=", "Review"], ["user", "in", users]]
-	if date_range:
-		date_range = frappe.parse_json(date_range)
-		filters.append(["creation", "between", [date_range[0], date_range[1]]])
-	energy_point_users = frappe.get_all(
-		"Energy Point Log",
-		fields=["user as name", "sum(points) as value"],
-		filters=filters,
-		group_by="user",
-		order_by="value desc",
-	)
+    filters = [["type", "!=", "Review"], ["user", "in", users]]
+    if date_range:
+        date_range = frappe.parse_json(date_range)
+        filters.append(["creation", "between", [date_range[0], date_range[1]]])
 
-	energy_point_users_list = list(map(lambda x: x["name"], energy_point_users))
-	for user in users:
-		if user not in energy_point_users_list:
-			energy_point_users.append({"name": user, "value": 0})
+    # Add the user field to the group_by clause
+    energy_point_users = frappe.get_all(
+        "Energy Point Log",
+        fields=["user as name", "sum(points) as value"],
+        filters=filters,
+        group_by="user, name",  # Include 'name' in group_by
+        order_by="value desc",
+    )
 
-	for user in energy_point_users:
-		user_id = user["name"]
-		user["name"] = get_fullname(user["name"])
-		user["formatted_name"] = f'<a href="/app/user-profile/{user_id}">{get_fullname(user_id)}</a>'
+    energy_point_users_list = list(map(lambda x: x["name"], energy_point_users))
+    for user in users:
+        if user not in energy_point_users_list:
+            energy_point_users.append({"name": user, "value": 0})
 
-	return energy_point_users
+    for user in energy_point_users:
+        user_id = user["name"]
+        user["name"] = get_fullname(user["name"])
+        user["formatted_name"] = f'<a href="/app/user-profile/{user_id}">{get_fullname(user_id)}</a>'
+
+    return energy_point_users
