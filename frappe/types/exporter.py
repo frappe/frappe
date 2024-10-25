ce01f8ada87e158c43ca842538cfcfc25bda8be8
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2024-10-25 03:13:28 +0200

    fix: controller path for exporting types (#28251)

--- frappe/types/exporter.py
+++ frappe/types/exporter.py
@@ -17,6 +17,7 @@ from keyword import iskeyword
 from pathlib import Path
 
 import frappe
+from frappe import scrub
 from frappe.types import DF
 
 field_template = "{field}: {type}"
@@ -59,7 +60,12 @@ class TypeExporter:
 
 		self.imports = {"from frappe.types import DF"}
 		self.indent = "\t"
-		self.controller_path = Path(inspect.getfile(get_controller(self.doctype)))
+		self.controller_path = (
+			Path(frappe.get_module_path(doc.module))
+			/ "doctype"
+			/ scrub(self.doctype)
+			/ f"{scrub(self.doctype)}.py"
+		)
 
 	def export_types(self):
 		self._guess_indentation()
