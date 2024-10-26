experiments001/frappe-sqlite

refactor: improve maintainability with a simple dispatcher (#27975)
* refactor: improve maintainability with a simple dispatcher

* refactor: improve maintainability with a init dispatcher on Document

* refactor: improve maintainability with an init dispatcher on meta

frappe/model/meta.py
@@ -19,6 +19,7 @@ import json
 import os
 import typing
 from datetime import datetime
+from functools import singledispatchmethod
 
 import click
 
@@ -57,10 +58,20 @@ DEFAULT_FIELD_LABELS = {
 }
 
 
-def get_meta(doctype, cached=True) -> "_Meta":
-	cached = cached and isinstance(doctype, str)
-	if cached and (meta := frappe.cache.hget("doctype_meta", doctype)):
-		return meta
+def get_meta(doctype: str | Document, cached=True) -> "_Meta":
+	"""Get metadata for a doctype.
+
+	Args:
+	    doctype: The doctype as a string or Document object.
+	    cached: Whether to use cached metadata (default: True).
+
+	Returns:
+	    Meta object for the given doctype.
+	"""
+	doctype_name = getattr(doctype, "doctype", doctype)
+	if cached and not isinstance(doctype, Document):
+		if meta := frappe.cache.hget("doctype_meta", doctype_name):
+			return meta
 
 	meta = Meta(doctype)
 	frappe.cache.hset("doctype_meta", meta.name, meta)
@@ -112,12 +123,18 @@ class Meta(Document):
 		frappe._dict(fieldname="owner", fieldtype="Data"),
 	)
 
-	def __init__(self, doctype):
-		if isinstance(doctype, Document):
-			super().__init__(doctype.as_dict())
-		else:
-			super().__init__("DocType", doctype)
+	@singledispatchmethod
+	def __init__(self, arg):
+		raise TypeError(f"Unsupported argument type: {type(arg)}")
 
+	@__init__.register(str)
+	def _(self, doctype):
+		super().__init__("DocType", doctype)
+		self.process()
+
+	@__init__.register(Document)
+	def _(self, doc):
+		super().__init__(doc.as_dict())
 		self.process()
 
 	def load_from_db(self):

frappe/frappe

frappe/model/meta.py
@@ -17,6 +17,7 @@ Example:
 import json
 import os
 from datetime import datetime
+from functools import singledispatchmethod
 
 import click
 
@@ -55,10 +56,20 @@ DEFAULT_FIELD_LABELS = {
 }
 
 
-def get_meta(doctype, cached=True) -> "Meta":
-	cached = cached and isinstance(doctype, str)
-	if cached and (meta := frappe.cache.hget("doctype_meta", doctype)):
-		return meta
+def get_meta(doctype: str | Document, cached=True) -> "Meta":
+	"""Get metadata for a doctype.
+
+	Args:
+	    doctype: The doctype as a string or Document object.
+	    cached: Whether to use cached metadata (default: True).
+
+	Returns:
+	    Meta object for the given doctype.
+	"""
+	doctype_name = getattr(doctype, "doctype", doctype)
+	if cached and not isinstance(doctype, Document):
+		if meta := frappe.cache.hget("doctype_meta", doctype_name):
+			return meta
 
 	meta = Meta(doctype)
 	frappe.cache.hset("doctype_meta", meta.name, meta)
@@ -110,12 +121,18 @@ class Meta(Document):
 		frappe._dict(fieldname="owner", fieldtype="Data"),
 	)
 
-	def __init__(self, doctype):
-		if isinstance(doctype, Document):
-			super().__init__(doctype.as_dict())
-		else:
-			super().__init__("DocType", doctype)
+	@singledispatchmethod
+	def __init__(self, arg):
+		raise TypeError(f"Unsupported argument type: {type(arg)}")
 
+	@__init__.register(str)
+	def _(self, doctype):
+		super().__init__("DocType", doctype)
+		self.process()
+
+	@__init__.register(Document)
+	def _(self, doc):
+		super().__init__(doc.as_dict())
 		self.process()
 
 	def load_from_db(self):