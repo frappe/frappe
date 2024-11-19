21a421885e9eec96bc4064689f0cb3ec064c6b69
Abdeali Chharchhoda <abdealiking786@gmail.com>
2024-11-19 17:24:00 +0530

    fix: Remove typo from `get_doc` docstring of `FrappeClient`

--- frappe/frappeclient.py
+++ frappe/frappeclient.py
@@ -215,7 +215,7 @@ class FrappeClient:
 		:param doctype: DocType of the document to be returned
 		:param name: (optional) `name` of the document to be returned
 		:param filters: (optional) Filter by this dict if name is not set
-		:param fields: (optional) Fields to be returned, will return everythign if not set"""
+		:param fields: (optional) Fields to be returned, will return everything if not set"""
 		params = {}
 		if filters:
 			params["filters"] = json.dumps(filters)
