e48b4b92922547e410ebfcb2dc6426d7c25af917
David <dgx.arnold@gmail.com>
2024-12-04 16:55:37 +0100

    fix: type narrowing

--- frappe/types/docref.py
+++ frappe/types/docref.py
@@ -16,14 +16,17 @@ class DocRef:
 
 	@override
 	def __hash__(self: Union[type, "DocRef"]) -> int:
-		if isinstance(self, DocRef):
-			if self.name:
-				return hash(self.doctype + self.name)
-			else:
-				raise TypeError(
-					f"Only named documents can be hashed; maybe the document ({self.doctype}) is unsaved."
-				)
-		raise TypeError("Only document instances can be hashed.")
+		if isinstance(self, type):
+			raise TypeError("Only document instances can be hashed.")
+		try:
+			name = self.name
+		except AttributeError:
+			raise TypeError("Partially instantiated document instances can't be hashed.")
+		if name:
+			return hash(self.doctype + name)
+		raise TypeError(
+			f"Only named documents can be hashed; maybe the document ({self.doctype}) is unsaved."
+		)
 
 	@override
 	def __str__(self) -> str:
