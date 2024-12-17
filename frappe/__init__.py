13a28f77f2301f392dae88a5fc904053e26da5ba
Ankush Menat <ankush@frappe.io>
2024-12-17 10:53:46 +0530

    perf: Avoid patching QB in every request
    (cherry picked from commit 5bf50b6bc298d7df997850b1f56aa516eb4b2095)
    
    # Conflicts:
    #	frappe/__init__.py

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -88,7 +88,11 @@ local = Local()
 cache = None
 STANDARD_USERS = ("Guest", "Administrator")
 
+<<<<<<< HEAD
 _qb_patched = {}
+=======
+_one_time_setup: dict[str, bool] = {}
+>>>>>>> 5bf50b6bc2 (perf: Avoid patching QB in every request)
 _dev_server = int(sbool(os.environ.get("DEV_SERVER", False)))
 _tune_gc = bool(sbool(os.environ.get("FRAPPE_TUNE_GC", True)))
 
@@ -249,10 +253,11 @@ def init(site: str, sites_path: str = ".", new_site: bool = False, force=False) 
 	local.qb.get_query = get_query
 	setup_redis_cache_connection()
 
-	if not _qb_patched.get(local.conf.db_type):
+	if not _one_time_setup.get(local.conf.db_type):
 		patch_query_execute()
 		patch_query_aggregation()
 		_register_fault_handler()
+		_one_time_setup[local.conf.db_type] = True
 
 	setup_module_map(include_all_apps=not (frappe.request or frappe.job or frappe.flags.in_migrate))
 
