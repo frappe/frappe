522396c190c5d47ba4292ca319816e7012373796
barredterra <14891507+barredterra@users.noreply.github.com>
2025-01-27 16:25:43 +0100

    feat(Data Import): delete Data Import Log on trash

--- frappe/core/doctype/data_import/data_import.py
+++ frappe/core/doctype/data_import/data_import.py
@@ -130,6 +130,9 @@ class DataImport(Document):
 	def get_importer(self):
 		return Importer(self.reference_doctype, data_import=self, use_sniffer=self.use_csv_sniffer)
 
+	def on_trash(self):
+		frappe.db.delete("Data Import Log", {"data_import": self.name})
+
 
 @frappe.whitelist()
 def get_preview_from_template(data_import, import_file=None, google_sheets_url=None):
