83e4aaaf99634bc6c633d2482536cfd980b21dc2
shubham <shubham@8848digital.com>
2025-02-05 18:33:33 +0530

    feat: datepicker_swedish

--- frappe/public/js/frappe/form/controls/datepicker_i18n.js
+++ frappe/public/js/frappe/form/controls/datepicker_i18n.js
@@ -177,3 +177,44 @@ import "air-datepicker/dist/js/i18n/datepicker.zh.js";
 		firstDay: 1,
 	};
 })(jQuery);
+
+(function ($) {
+	$.fn.datepicker.language["sv"] = {
+		days: ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"],
+		daysShort: ["Sön", "Mån", "Tis", "Ons", "Tor", "Fre", "Lör"],
+		daysMin: ["Sö", "Må", "Ti", "On", "To", "Fr", "Lö"],
+		months: [
+			"Januari",
+			"Februari",
+			"Mars",
+			"April",
+			"Maj",
+			"Juni",
+			"Juli",
+			"Augusti",
+			"September",
+			"Oktober",
+			"November",
+			"December",
+		],
+		monthsShort: [
+			"Jan",
+			"Feb",
+			"Mar",
+			"Apr",
+			"Maj",
+			"Jun",
+			"Jul",
+			"Aug",
+			"Sep",
+			"Okt",
+			"Nov",
+			"Dec",
+		],
+		today: "Idag",
+		clear: "Återställ",
+		dateFormat: "yyyy-mm-dd",
+		timeFormat: "hh:ii",
+		firstDay: 1,
+	};
+})(jQuery);
