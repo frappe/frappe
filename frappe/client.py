28905bea4e98d4661f2b55299037195a4ff5be7d
Ankush Menat <ankush@frappe.io>
2025-02-06 12:46:39 +0530

    fix: only check parent doctype for child doctypes
    (cherry picked from commit 063f2bbc79316df297d1131edd48fcca4e9714af)

--- frappe/client.py
+++ frappe/client.py
@@ -424,7 +424,9 @@ def validate_link(doctype: str, docname: str, fields=None):
 		frappe.throw(_("Document Name must be a string"))
 
 	if doctype != "DocType":
-		parent_doctype = frappe.db.get_value(doctype, docname, "parenttype")  # needed for links to child rows
+		parent_doctype = None
+		if frappe.get_meta(doctype).istable:  # needed for links to child rows
+			parent_doctype = frappe.db.get_value(doctype, docname, "parenttype")
 		if not (
 			frappe.has_permission(doctype, "select", parent_doctype=parent_doctype)
 			or frappe.has_permission(doctype, "read", parent_doctype=parent_doctype)
