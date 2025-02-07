35e1aa5bee3d8d99cafe18c268172e1760e791d0
Akhil Narang <me@akhilnarang.dev>
2025-02-07 11:43:33 +0530

    Revert "fix: don't allow attaching a file to a child table in a new document"
    This reverts commit 2fdc94840ffe8c5bbab59c24a44105801f57afe6.

--- frappe/public/js/frappe/form/controls/attach.js
+++ frappe/public/js/frappe/form/controls/attach.js
@@ -32,12 +32,6 @@ frappe.ui.form.ControlAttach = class ControlAttach extends frappe.ui.form.Contro
 
 		frappe.utils.bind_actions_with_object(this.$value, this);
 		this.toggle_reload_button();
-
-		// Don't allow attaching to child tables for new documents
-		if (this.doc.__islocal && this.doc.parent) {
-			this.$input.prop("disabled", true);
-			this.$input.attr("title", __("Save to enable file upload"));
-		}
 	}
 	clear_attachment() {
 		let me = this;
