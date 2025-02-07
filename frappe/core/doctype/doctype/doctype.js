0f7f3f02669accc16ef4301780f76cc7d0db49e0
kakde-sandeep <sandeepk5997@gmail.com>
2025-02-07 17:55:02 +0530

    refactor: extracting trim_table to index.js from doctype.js & customize_form.js

--- frappe/core/doctype/doctype/doctype.js
+++ frappe/core/doctype/doctype/doctype.js
@@ -145,39 +145,6 @@ frappe.ui.form.on("DocType", {
 		}
 	},
 
-	async trim_table(frm) {
-		let dropped_columns = await frappe.xcall(
-			"frappe.custom.doctype.customize_form.customize_form.get_orphaned_columns",
-			{ doctype: frm.doc.name }
-		);
-
-		if (!dropped_columns?.length) {
-			frappe.toast(__("This doctype has no orphan fields to trim"));
-			return;
-		}
-		let msg = __(
-			"Warning: DATA LOSS IMMINENT! Proceeding will permanently delete following database columns from doctype {0}:",
-			[frm.doc.name.bold()]
-		);
-		msg += "<ol>" + dropped_columns.map((col) => `<li>${col}</li>`).join("") + "</ol>";
-		msg += __("This action is irreversible. Do you wish to continue?");
-
-		frappe.confirm(msg, () => {
-			return frm.call({
-				doc: frm.doc,
-				method: "trim_table",
-				callback: function (r) {
-					if (!r.exc) {
-						frappe.show_alert({
-							message: __("Table Trimmed"),
-							indicator: "green",
-						});
-						frm.refresh();
-					}
-				},
-			});
-		});
-	},
 });
 
 frappe.ui.form.on("DocField", {
