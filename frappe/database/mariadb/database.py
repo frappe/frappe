1c0194d0de8b55365a90caf1d3837ef93298d47e
Akhil Narang <me@akhilnarang.dev>
2025-02-12 18:58:36 +0530

    fix: use `frappe.conf.db_name` instead of `frappe.db.cur_db_name`
    For some reason, the latter isn't defined here.
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- frappe/database/mariadb/database.py
+++ frappe/database/mariadb/database.py
@@ -440,7 +440,7 @@ class MariaDBDatabase(MariaDBConnectionUtil, MariaDBExceptionUtil, Database):
 			tables = (
 				frappe.qb.from_(information_schema.tables)
 				.select(information_schema.tables.table_name)
-				.where(information_schema.tables.table_schema == frappe.db.cur_db_name)
+				.where(information_schema.tables.table_schema == frappe.conf.db_name)
 				.run(pluck=True)
 			)
 			frappe.cache().set_value("db_tables", tables)
