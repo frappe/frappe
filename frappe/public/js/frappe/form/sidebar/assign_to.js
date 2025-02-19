38e2472f5f97f6e9de27361abd69aa16e5215333
Akhil Narang <me@akhilnarang.dev>
2025-02-19 16:40:09 +0530

    refactor(assign_to): use a `Text Editor` field for description
    The `ToDo` doctype uses this, so it makes more sense here.
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    (cherry picked from commit c22f652473b4fb1a18920b35df2be4e8cdb6b2dc)

--- frappe/public/js/frappe/form/sidebar/assign_to.js
+++ frappe/public/js/frappe/form/sidebar/assign_to.js
@@ -208,7 +208,7 @@ frappe.ui.form.AssignToDialog = class AssignToDialog {
 			},
 			{
 				label: __("Comment"),
-				fieldtype: "Small Text",
+				fieldtype: "Text Editor",
 				fieldname: "description",
 			},
 		];
