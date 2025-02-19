66348fac55ed56d05ce8f3cb32924cef7a821f4f
Leonard Goertz <leonard.goertz@innolizer.de>
2025-02-19 16:19:20 +0100

    fix: remove autoname condition for title delete_doc dialog

--- frappe/public/js/frappe/model/model.js
+++ frappe/public/js/frappe/model/model.js
@@ -724,7 +724,7 @@ $.extend(frappe.model, {
 	delete_doc: function (doctype, docname, callback) {
 		let title = docname;
 		const title_field = frappe.get_meta(doctype).title_field;
-		if (frappe.get_meta(doctype).autoname == "hash" && title_field) {
+		if (title_field) {
 			const value = frappe.model.get_value(doctype, docname, title_field);
 			if (value) {
 				title = `${value} (${docname})`;
