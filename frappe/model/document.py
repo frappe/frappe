bdaa8be60959dbd0284b29c797aa4076ad057d78
Ankush Menat <ankush@frappe.io>
2025-02-19 16:39:03 +0530

    fix: clear cache after db transaction ends
    This reduces probability of stale cache, but it's still very easily
    possible because of repeatable read (!)

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -1307,6 +1307,8 @@ class Document(BaseDocument, DocRef):
 
 	def clear_cache(self):
 		frappe.clear_document_cache(self.doctype, self.name)
+		frappe.db.after_commit.add(lambda: frappe.clear_document_cache(self.doctype, self.name))
+		frappe.db.after_rollback.add(lambda: frappe.clear_document_cache(self.doctype, self.name))
 
 	def reset_seen(self):
 		"""Clear _seen property and set current user as seen"""
