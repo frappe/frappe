ffc01df5d61626abe460d97d8e1a95907208d407
Corentin Forler <10946971+cogk@users.noreply.github.com>
2025-02-20 17:44:53 +0100

    fix(EmailAccount): Fix always_bcc condition

--- frappe/email/doctype/email_queue/email_queue.py
+++ frappe/email/doctype/email_queue/email_queue.py
@@ -690,8 +690,8 @@ class QueueBuilder:
 
 	def prepare_email_content(self):
 		email_account = self.get_outgoing_email_account()
-		if isinstance(self._bcc, list) and email_account.always_bcc is not None:
-			self._bcc.append(str(email_account.always_bcc))
+		if isinstance(self._bcc, list) and email_account.always_bcc:
+			self._bcc.append(email_account.always_bcc)
 		mail = get_email(
 			recipients=self.final_recipients(),
 			sender=self.sender,
