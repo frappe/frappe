de0f6bb1aef58e2ed8089d4706a1f5aa1cdcbc79
Sagar Vora <sagar@resilient.tech>
2025-02-24 15:37:12 +0530

    fix: conflict

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -1069,28 +1069,6 @@ class Document(BaseDocument):
 		return self._cancel()
 
 	@frappe.whitelist()
-<<<<<<< HEAD
-=======
-	def discard(self):
-		"""Discard the draft document. Sets `docstatus` = 2 with db_set."""
-		self._action = "discard"
-
-		self.check_if_locked()
-		self.set_user_and_timestamp()
-		self.check_if_latest()
-
-		if not self.docstatus.is_draft():
-			raise frappe.ValidationError(_("Only draft documents can be discarded"), self.docstatus)
-
-		self.check_permission("write")
-
-		self.run_method("before_discard")
-		self.db_set("docstatus", DocStatus.CANCELLED)
-		delattr(self, "_action")
-		self.run_method("on_discard")
-
-	@frappe.whitelist()
->>>>>>> fcf657ba80 (perf: improved `DocStatus` API and other minor improvements)
 	def rename(self, name: str, merge=False, force=False, validate_rename=True):
 		"""Rename the document to `name`. This transforms the current object."""
 		return self._rename(name=name, merge=merge, force=force, validate_rename=validate_rename)
