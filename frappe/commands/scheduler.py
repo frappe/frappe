24f96f758ff0d63f44fd7bb1ccece68b2ed829bb
Ankush Menat <ankush@frappe.io>
2025-02-26 15:00:38 +0530

    fix: check for running jobs before migrating (#31438)
    (cherry picked from commit 8baeb5151d822276cf9660b83da092b8b56bc6bf)
    
    # Conflicts:
    #	frappe/commands/scheduler.py

--- frappe/commands/scheduler.py
+++ frappe/commands/scheduler.py
@@ -231,15 +231,33 @@ def start_worker_pool(queue, quiet=False, num_workers=2, burst=False):
 @click.command("ready-for-migration")
 @click.option("--site", help="site name")
 @pass_context
+<<<<<<< HEAD
 def ready_for_migration(context, site=None):
+=======
+def ready_for_migration(context: CliCtxObj, site=None):
+	import time
+
+>>>>>>> 8baeb5151d (fix: check for running jobs before migrating (#31438))
 	from frappe.utils.doctor import any_job_pending
 
 	if not site:
 		site = get_site(context)
 
 	try:
+<<<<<<< HEAD
 		frappe.init(site=site)
 		pending_jobs = any_job_pending(site=site)
+=======
+		frappe.init(site)
+		pending_jobs = False
+
+		# HACK: Check at least 3 times, 1 second apart.
+		# Rare edge case: Scheduler hasn't seen 'maintenance_mode=1` yet
+		#                    and takes more than 3 second to schedule.
+		for _ in range(3):
+			pending_jobs |= any_job_pending(site=site)
+			time.sleep(1)
+>>>>>>> 8baeb5151d (fix: check for running jobs before migrating (#31438))
 
 		if pending_jobs:
 			print(f"NOT READY for migration: site {site} has pending background jobs")
