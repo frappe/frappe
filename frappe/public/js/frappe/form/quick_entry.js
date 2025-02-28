5aacad4bb0ccde04d0ce9ee862490d2820525956
Gavin D'souza <gavin18d@gmail.com>
2025-02-28 14:49:06 +0100

    fix(quickentry): Show submit option only if User can
    ref: https://github.com/frappe/frappe/pull/26579/files#r1975437243
    (cherry picked from commit 5930011849080805db3ab69f2790a9576a5c1407)
    
    # Conflicts:
    #	frappe/public/js/frappe/form/quick_entry.js

--- frappe/public/js/frappe/form/quick_entry.js
+++ frappe/public/js/frappe/form/quick_entry.js
@@ -184,7 +184,14 @@ frappe.ui.form.QuickEntryForm = class QuickEntryForm {
 					doc: me.dialog.doc,
 				},
 				callback: function (r) {
+<<<<<<< HEAD
 					if (frappe.model.is_submittable(me.doctype)) {
+=======
+					if (
+						frappe.model.can_submit(me.doctype) &&
+						!frappe.model.has_workflow(me.doctype)
+					) {
+>>>>>>> 5930011849 (fix(quickentry): Show submit option only if User can)
 						frappe.run_serially([
 							() => (me.dialog.working = true),
 							() => {
