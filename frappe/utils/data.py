e440b2ef1be0c0dfe00a8f1b25ca51f433d1d522
Akhil Narang <me@akhilnarang.dev>
2025-03-04 15:12:57 +0530

    fix(get_url): allow disabling host header override
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    (cherry picked from commit 627b8e04f500c762eb4160c9faa481e681184787)
    
    # Conflicts:
    #	frappe/utils/data.py

--- frappe/utils/data.py
+++ frappe/utils/data.py
@@ -1590,8 +1590,17 @@ def filter_strip_join(some_list: list[str], sep: str) -> list[str]:
 	return (cstr(sep)).join(cstr(a).strip() for a in filter(None, some_list))
 
 
+<<<<<<< HEAD
 def get_url(uri: str | None = None, full_address: bool = False) -> str:
 	"""get app url from request"""
+=======
+def get_url(
+	uri: str | None = None,
+	full_address: bool = False,
+	allow_header_override: bool = True,
+) -> str:
+	"""Get app url from request."""
+>>>>>>> 627b8e04f5 (fix(get_url): allow disabling host header override)
 	host_name = frappe.local.conf.host_name or frappe.local.conf.hostname
 
 	if uri and (uri.startswith("http://") or uri.startswith("https://")):
@@ -1600,7 +1609,7 @@ def get_url(uri: str | None = None, full_address: bool = False) -> str:
 	if not host_name:
 		request_host_name = get_host_name_from_request()
 
-		if request_host_name:
+		if request_host_name and allow_header_override:
 			host_name = request_host_name
 
 		elif frappe.local.site:
