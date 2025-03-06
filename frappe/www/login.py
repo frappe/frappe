ea388b3b955cab8a323ae12802c03c8248bc5f92
Bread Genie <msuhailbh07@gmail.com>
2025-03-06 11:45:43 +0530

    refactor: use a better fc logo
    also show login with fc button only for users coming from new signup flow (for now)

--- frappe/www/login.py
+++ frappe/www/login.py
@@ -113,7 +113,9 @@ def get_context(context):
 
 	context["login_with_email_link"] = frappe.get_system_settings("login_with_email_link")
 	context["login_with_frappe_cloud_url"] = (
-		f"{get_site_login_url()}?site={frappe.local.site}" if on_frappecloud() else None
+		f"{get_site_login_url()}?site={frappe.local.site}"
+		if on_frappecloud() and frappe.conf.get("fc_communication_secret")
+		else None
 	)
 
 	return context
