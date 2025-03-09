3167f0c92b25cc96ab045ee68173db1e7e536a97
sokumon <sohamkulkarns9@gmail.com>
2025-03-09 23:54:21 +0530

    fix: change guest message

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -11,6 +11,7 @@ be used to build database driven apps.
 Read the documentation: https://frappeframework.com/docs
 """
 
+import copy
 import functools
 import importlib
 import inspect
@@ -865,7 +866,7 @@ def is_whitelisted(method):
 
 	is_guest = session["user"] == "Guest"
 	if method not in whitelisted or (is_guest and method not in guest_methods):
-		summary = _("You are not permitted to access this resource.")
+		summary = _("Your session has expired. Please login again")
 		detail = _("Function {0} is not whitelisted.").format(bold(f"{method.__module__}.{method.__name__}"))
 		msg = f"<details><summary>{summary}</summary>{detail}</details>"
 		throw(msg, PermissionError, title=_("Method Not Allowed"))
@@ -1672,22 +1673,14 @@ def get_file_json(path):
 		return json.load(f)
 
 
-def read_file(path, raise_not_found=False, as_base64=False):
-	"""Open a file and return its content as Unicode or Base64 string."""
+def read_file(path, raise_not_found=False):
+	"""Open a file and return its content as Unicode."""
 	if isinstance(path, str):
 		path = path.encode("utf-8")
 
 	if os.path.exists(path):
-		if as_base64:
-			import base64
-
-			with open(path, "rb") as f:
-				content = f.read()
-				return base64.b64encode(content).decode("utf-8")
-		else:
-			with open(path) as f:
-				content = f.read()
-				return as_unicode(content)
+		with open(path) as f:
+			return as_unicode(f.read())
 	elif raise_not_found:
 		raise OSError(f"{path} Not Found")
 	else:
@@ -2083,6 +2076,52 @@ def format(*args, **kwargs):
 	return frappe.utils.formatters.format_value(*args, **kwargs)
 
 
+def get_print(
+	doctype=None,
+	name=None,
+	print_format=None,
+	style=None,
+	as_pdf=False,
+	doc=None,
+	output=None,
+	no_letterhead=0,
+	password=None,
+	pdf_options=None,
+	letterhead=None,
+):
+	"""Get Print Format for given document.
+
+	:param doctype: DocType of document.
+	:param name: Name of document.
+	:param print_format: Print Format name. Default 'Standard',
+	:param style: Print Format style.
+	:param as_pdf: Return as PDF. Default False.
+	:param password: Password to encrypt the pdf with. Default None"""
+	from frappe.utils.pdf import get_pdf
+	from frappe.website.serve import get_response_without_exception_handling
+
+	original_form_dict = copy.deepcopy(local.form_dict)
+	try:
+		local.form_dict.doctype = doctype
+		local.form_dict.name = name
+		local.form_dict.format = print_format
+		local.form_dict.style = style
+		local.form_dict.doc = doc
+		local.form_dict.no_letterhead = no_letterhead
+		local.form_dict.letterhead = letterhead
+
+		pdf_options = pdf_options or {}
+		if password:
+			pdf_options["password"] = password
+
+		response = get_response_without_exception_handling("printview", 200)
+		html = str(response.data, "utf-8")
+	finally:
+		local.form_dict = original_form_dict
+
+	return get_pdf(html, options=pdf_options, output=output) if as_pdf else html
+
+
 def attach_print(
 	doctype,
 	name,
@@ -2337,6 +2376,5 @@ from frappe.config import get_common_site_config, get_site_config
 from frappe.core.doctype.system_settings.system_settings import get_system_settings
 from frappe.utils import parse_json
 from frappe.utils.error import log_error
-from frappe.utils.print_utils import get_print
 
 frappe._optimizations.optimize_all()
