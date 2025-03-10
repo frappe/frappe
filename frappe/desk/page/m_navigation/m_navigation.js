71b0ae0f602916c7fb90b43ed21bed7e94927ba9
sokumon <sohamkulkarns9@gmail.com>
2025-03-10 15:52:04 +0530

    fix: only show page if it is mobile

--- frappe/desk/page/m_navigation/m_navigation.js
+++ frappe/desk/page/m_navigation/m_navigation.js
@@ -1,10 +1,12 @@
 frappe.pages["m-navigation"].on_page_load = function (wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: "Home",
-		single_column: true,
-	});
-	let sidebar = new frappe.ui.Sidebar();
-	var element = $(".sidebar-items").detach();
-	$(page.wrapper).append(element);
+	if (frappe.is_mobile()) {
+		var page = frappe.ui.make_app_page({
+			parent: wrapper,
+			title: "Home",
+			single_column: true,
+		});
+		let sidebar = new frappe.ui.Sidebar();
+		var element = $(".sidebar-items").detach();
+		$(page.wrapper).append(element);
+	}
 };
