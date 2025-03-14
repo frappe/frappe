b29792c764d89b803e5173945fba12967872b600
Sagar Vora <sagar@resilient.tech>
2025-03-15 01:02:21 +0530

    chore: remove commented code

--- frappe/database/database.py
+++ frappe/database/database.py
@@ -37,8 +37,6 @@ from frappe.utils import CallbackManager, cint, get_datetime, get_table_name, ge
 from frappe.utils import cast as cast_fieldtype
 
 if TYPE_CHECKING:
-	# from mariadb.connections import Connection as MariadbConnection
-	# from mariadb.cursors import Cursor as MariadbCursor
 	from MySQLdb.connections import Connection as MySQLdbConnection
 	from MySQLdb.cursors import Cursor as MySQLdbCursor
 	from psycopg2 import connection as PostgresConnection
