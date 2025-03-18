00ffda9919ba3d0f5e11e3ac06309e612587f36c
sokumon <sohamkulkarns9@gmail.com>
2025-03-18 15:21:24 +0530

    fix: remove mobile bottombar

--- frappe/public/js/frappe/desk.js
+++ frappe/public/js/frappe/desk.js
@@ -89,11 +89,7 @@ frappe.Application = class Application {
 	}
 
 	make_sidebar() {
-		if (frappe.is_mobile()) {
-			this.sidebar = new frappe.ui.MobileSidebar();
-		} else {
-			this.sidebar = new frappe.ui.Sidebar();
-		}
+		this.sidebar = new frappe.ui.Sidebar();
 	}
 
 	setup_theme() {
