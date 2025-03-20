e76f52c79689fb394f38a872df45f3fc5dda2607
Ankush Menat <ankush@frappe.io>
2025-03-20 18:08:45 +0530

    fix: log deduplication as error
    in most cases we don't exepct it to happen.

--- frappe/utils/background_jobs.py
+++ frappe/utils/background_jobs.py
@@ -94,7 +94,7 @@ def enqueue(
 			frappe.throw(_("`job_id` paramater is required for deduplication."))
 		job = get_job(job_id)
 		if job and job.get_status() in (JobStatus.QUEUED, JobStatus.STARTED):
-			frappe.logger().debug(f"Not queueing job {job.id} because it is in queue already")
+			frappe.logger().error(f"Not queueing job {job.id} because it is in queue already")
 			return
 		elif job:
 			# delete job to avoid argument issues related to job args
