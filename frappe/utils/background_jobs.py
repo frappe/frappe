08339cacc555df4f7ce972bfbf787e77b35c206f
Ankush Menat <ankush@frappe.io>
2025-03-21 12:11:37 +0530

    fix: drop proctitle from dependency
    - While it improves our own processes, it makes gunicorn process titles worse
    - It also crashes on mac because of some unknown reason related to fork-safety.
    
    Best to not use this.

--- frappe/utils/background_jobs.py
+++ frappe/utils/background_jobs.py
@@ -12,7 +12,6 @@ from typing import Any, NoReturn
 from uuid import uuid4
 
 import redis
-import setproctitle
 from redis.exceptions import BusyLoadingError, ConnectionError
 from rq import Callback, Queue, Worker
 from rq.defaults import DEFAULT_WORKER_TTL
@@ -236,9 +235,6 @@ def execute_job(site, method, event, job_name, kwargs, user=None, is_async=True,
 		method = frappe.get_attr(method)
 	else:
 		method_name = f"{method.__module__}.{method.__qualname__}"
-
-	actual_func_name = kwargs.get("job_type") if "run_scheduled_job" in method_name else method_name
-	setproctitle.setproctitle(f"rq: Started running {actual_func_name} at {time.time()}")
 
 	frappe.local.job = frappe._dict(
 		site=site,
