8f987ee99f1016af4be2d8e134260bd110ba55ed
Nikhil Kothari <nik.kothari22@live.com>
2025-03-23 15:53:03 +0530

    chore: linting

--- realtime/middlewares/authenticate.js
+++ realtime/middlewares/authenticate.js
@@ -32,7 +32,7 @@ function authenticate_with_frappe(socket, next) {
 	}
 
 	let auth_req = request.get(get_url(socket, "/api/method/frappe.realtime.get_user_info"));
-	if (authorization_header){
+	if (authorization_header) {
 		auth_req = auth_req.set("Authorization", authorization_header);
 	} else if (cookies.sid) {
 		auth_req = auth_req.query({ sid: cookies.sid });
