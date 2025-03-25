b53c6f0b8db784c4a341dd79e3b4f8c60e7d0be4
Akhil Narang <me@akhilnarang.dev>
2025-03-25 13:52:33 +0530

    fix: only run patch for MariaDB (#31891)
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    (cherry picked from commit d1870b745e3921ce94e55eed05531b1bc6fa5a8a)

--- frappe/patches/v14_0/fix_user_settings_collation.py
+++ frappe/patches/v14_0/fix_user_settings_collation.py
@@ -2,4 +2,7 @@ import frappe
 
 
 def execute():
-	frappe.db.sql("ALTER TABLE __UserSettings CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
+	if frappe.db.db_type == "mariadb":
+		frappe.db.sql(
+			"ALTER TABLE __UserSettings CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+		)
