38be3de672c9a43f43a4e46e85685529243fbed6
0xD0M1M0 <76812428+0xD0M1M0@users.noreply.github.com>
2025-03-27 13:10:25 +0100

    fix: linter

--- frappe/email/doctype/email_queue/email_queue.py
+++ frappe/email/doctype/email_queue/email_queue.py
@@ -695,7 +695,7 @@ class QueueBuilder:
 	def prepare_email_content(self):
 		email_account = self.get_outgoing_email_account()
 		if email_account.always_bcc:
-			self._bcc = self.bcc + [email_account.always_bcc]
+			self._bcc = [*self.bcc, email_account.always_bcc]
 		mail = get_email(
 			recipients=self.final_recipients(),
 			sender=self.sender,
