16d20643737614c1fb70b4016e097c186ee23c15
Ankush Menat <ankush@frappe.io>
2025-03-31 13:15:51 +0530

    Update tag_link.py

--- frappe/desk/doctype/tag_link/tag_link.py
+++ frappe/desk/doctype/tag_link/tag_link.py
@@ -6,23 +6,6 @@ from frappe.model.document import Document
 
 
 class TagLink(Document):
-<<<<<<< HEAD
-=======
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
-
-	from typing import TYPE_CHECKING
-
-	if TYPE_CHECKING:
-		from frappe.types import DF
-
-		document_name: DF.DynamicLink | None
-		document_type: DF.Link | None
-		tag: DF.Link | None
-		title: DF.Data | None
-
-	# end: auto-generated types
->>>>>>> f27f304dcf (perf: index tag links (#31953))
 	pass
 
 
