dba395b516fff5c04b4676c0e6207af3fcd58dfb
Jan Lukas Liesen <=>
2025-04-11 08:41:02 +0200

    feat: :sparkles: addes x-priority option to email header and the frappe.sendmail function
    creating the option to give the email an importance.
    1 = Highest, 3 = Normal, 5 = Lowest
    commonly used to flag the importance of emails

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -514,6 +514,7 @@ def sendmail(
 	print_letterhead=False,
 	with_container=False,
 	email_read_tracker_url=None,
+	x_priority: Literal[1, 3, 5] = 3,
 ) -> Optional["EmailQueue"]:
 	"""Send email using user's default **Email Account** or global default **Email Account**.
 
@@ -541,6 +542,7 @@ def sendmail(
 	:param args: Arguments for rendering the template
 	:param header: Append header in email
 	:param with_container: Wraps email inside a styled container
+	:param x_priority: 1 = HIGHEST, 3 = NORMAL, 5 = LOWEST
 	"""
 
 	if recipients is None:
@@ -596,6 +598,7 @@ def sendmail(
 		print_letterhead=print_letterhead,
 		with_container=with_container,
 		email_read_tracker_url=email_read_tracker_url,
+		x_priority=x_priority,
 	)
 
 	# build email queue and send the email if send_now is True.
