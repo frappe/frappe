6b65693bc0e802e44f7f749e908d4e1dd9d1ce70
Niraj Gautam <nirajgautam196@gmail.com>
2025-04-16 18:35:02 +0530

    refactor: Add option to mark comments as spam or ham

--- frappe/core/doctype/comment/comment.js
+++ frappe/core/doctype/comment/comment.js
@@ -2,6 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Comment", {
-	// refresh: function(frm) {
-	// }
+	refresh: function (frm) {
+		if (frm.is_new() || frm.doc.comment_type != "Comment") {
+			return;
+		}
+		const is_spam =
+			["Spam", "Discard"].includes(frm.doc.spam_type) && frm.doc.spam_type != "Pending";
+		frm.add_custom_button(__(`Mark as ${is_spam ? "Ham" : "Spam"}`), function () {
+			frm.call("mark_as_spam_or_ham", { is_spam: !is_spam })
+				.then((r) => {
+					if (r.message) {
+						frm.reload_doc();
+					}
+				})
+				.catch((e) => {
+					frappe.msgprint(__("Unable to mark comment as spam/ham"));
+					console.error(e);
+				});
+		});
+	},
 });
