ad06690f32a9a1ffb3b036b3c3075b17d23c48a8
lc <iliuchao2326@163.com>
2025-04-16 10:27:42 +0800

    fix Remove menu item in Mobile View

--- frappe/public/js/frappe/ui/page.js
+++ frappe/public/js/frappe/ui/page.js
@@ -713,6 +713,11 @@ frappe.ui.Page = class Page {
 		} else {
 			this.inner_toolbar.find(`button[data-label="${encodeURIComponent(label)}"]`).remove();
 		}
+
+        // Remove menu item in Mobile View
+        group = __(group);
+        const encodeMobileLabel = encodeURIComponent(`${group} > ${label}`);
+        $('li.user-action').has(`span.menu-item-label[data-label="${encodeMobileLabel}"]`).remove();
 	}
 
 	change_inner_button_type(label, group, type) {
