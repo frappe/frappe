c743fb54e59c5738832e4518b42f7abba2dc1e9d
Bread Genie <msuhailbh07@gmail.com>
2025-04-19 17:27:11 +0530

    fix: selection of timezone after changing the language
    It was working without changing because on country selection the timezone was auto-filled.

--- frappe/desk/page/setup_wizard/setup_wizard.js
+++ frappe/desk/page/setup_wizard/setup_wizard.js
@@ -427,7 +427,7 @@ frappe.setup.slides_settings = [
 			};
 
 			if (frappe.setup.data.regional_data) {
-				this.setup_fields(slide);
+				setup_fields(slide);
 			} else {
 				frappe.setup.utils.load_regional_data(slide, setup_fields);
 			}
@@ -525,7 +525,7 @@ frappe.setup.utils = {
 				if (r.message) {
 					frappe.wizard.values.currency = r.message.currency;
 					frappe.wizard.values.country = r.message.country;
-					frappe.wizard.values.timezone = r.message.timezone;
+					frappe.wizard.values.timezone = r.message.time_zone;
 					frappe.wizard.values.language = r.message.language;
 
 					frappe.db.get_value(
