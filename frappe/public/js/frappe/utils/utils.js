3e4823f97890757ffc2e622ea76cc0520674db86
Andres rodriguez santos <52182239+andresrodriguezsantos44@users.noreply.github.com>
2025-04-21 15:38:22 -0500

    feat: validate payment id not null

--- frappe/public/js/frappe/utils/utils.js
+++ frappe/public/js/frappe/utils/utils.js
@@ -1756,4 +1756,12 @@ Object.assign(frappe.utils, {
 			__("Generate Tracking URL")
 		);
 	},
+	randomString(length) {
+		const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		let result = '';
+		for (let i = 0; i < length; i++) {
+			result += chars.charAt(Math.floor(Math.random() * chars.length));
+		}
+		return result;
+	}
 });
