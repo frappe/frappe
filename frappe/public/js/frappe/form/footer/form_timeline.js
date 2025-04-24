430bdef05a8b7da62c09d50dcc741b480caf285a
barredterra <14891507+barredterra@users.noreply.github.com>
2025-04-24 13:00:44 +0200

    fix: refresh timeline only

--- frappe/public/js/frappe/form/footer/form_timeline.js
+++ frappe/public/js/frappe/form/footer/form_timeline.js
@@ -752,7 +752,13 @@ class FormTimeline extends BaseTimeline {
 				})
 				.then(() => {
 					frappe.utils.play_sound("click");
-					this.frm.reload_doc();
+
+					// update the comment info that is stored in the frontend, then refresh the timeline
+					const comment = this.frm
+						.get_docinfo()
+						.comments.find((comment) => comment.name === comment_name);
+					comment.published = publish;
+					this.refresh();
 				});
 		});
 	}
