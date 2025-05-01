da70bbddf1bf5e06583dedcd14ea0576f1c5a4fd
UmakanthKaspa <kaspaumakanth1999@gmail.com>
2025-05-01 04:13:42 +0000

    feat: include print format name in Access Log during print

--- frappe/www/printview.py
+++ frappe/www/printview.py
@@ -90,8 +90,15 @@ def get_context(context) -> PrintContext:
 			settings=settings,
 		)
 
+	# Include selected print format name in access log
+	print_format_name = getattr(print_format, "name", "Standard")
+
 	make_access_log(
-		doctype=frappe.form_dict.doctype, document=frappe.form_dict.name, file_type="PDF", method="Print"
+		doctype=frappe.form_dict.doctype,
+		document=frappe.form_dict.name,
+		file_type="PDF",
+		method="Print",
+		page=f"Print Format: {print_format_name}",
 	)
 
 	return {
