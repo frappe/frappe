bca1c8409dfec311dccc535329b03660a0cf983e
sokumon <sohamkulkarns9@gmail.com>
2025-05-13 14:36:55 +0530

    fix: delete newsletter related files

--- frappe/website/doctype/utm_medium/utm_medium.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Frappe Technologies and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("UTM Medium", {
-// 	refresh(frm) {
-
-// 	},
-// });
