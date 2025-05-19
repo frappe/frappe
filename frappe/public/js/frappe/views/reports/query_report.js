ad72051a5f92c128cc0208cc8b199a35728dbf14
Ankush Menat <ankush@frappe.io>
2025-05-19 12:11:29 +0530

    fix: bump report length limit

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -1027,7 +1027,7 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 		let data = this.data;
 		let columns = this.columns.filter((col) => !col.hidden);
 
-		if (data.length > 100000) {
+		if (data.length > 200000) {
 			let msg = __(
 				"This report contains {0} rows and is too big to display in browser, you can {1} this report instead.",
 				[cstr(format_number(data.length, null, 0)).bold(), __("export").bold()]
