6deb51dfd6a722ef60c2ab3ce43d82875bb3164a
UmakanthKaspa <kaspaumakanth1999@gmail.com>
2025-05-21 12:10:08 +0000

    feat: add 'show full number' option to number card
    (cherry picked from commit f1fdb5003426af9a05865ac35cdeb060a498c063)
    
    # Conflicts:
    #	frappe/desk/doctype/number_card/number_card.json
    #	frappe/public/js/frappe/widgets/number_card_widget.js

--- frappe/public/js/frappe/widgets/number_card_widget.js
+++ frappe/public/js/frappe/widgets/number_card_widget.js
@@ -223,9 +223,22 @@ export default class NumberCardWidget extends Widget {
 
 	set_formatted_number(df, doc) {
 		const default_country = frappe.sys_defaults.country;
+<<<<<<< HEAD
 		const shortened_number = frappe.utils.shorten_number(this.number, default_country, 5);
 		let number_parts = shortened_number.split(" ");
 		const symbol = number_parts[1] || "";
+=======
+
+		let number_parts;
+
+		// Use full number if the checkbox is enabled
+		if (this.card_doc.show_full_number) {
+			number_parts = [this.number.toString(), ""];
+		} else {
+			const shortened_number = frappe.utils.shorten_number(this.number, default_country, 5);
+			number_parts = shortened_number.split(" ");
+		}
+>>>>>>> f1fdb50034 (feat: add 'show full number' option to number card)
 
 		// done to add multicurrency support in number card
 		if (this.card_doc.currency) {
