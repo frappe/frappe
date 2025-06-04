e2d41b1bf8ec3e1914900d9827fd07eb15ffb9e6
Ankush Menat <ankush@frappe.io>
2025-06-04 11:11:36 +0530

    fix: Avoid patching uninstalled app

--- frappe/patches/v16_0/enable_setup_complete.py
+++ frappe/patches/v16_0/enable_setup_complete.py
@@ -6,7 +6,11 @@ def execute():
 	frappe.reload_doc("core", "doctype", "installed_applications")
 
 	is_setup_complete = frappe.db.get_single_value("System Settings", "setup_complete")
+	installed_apps = frappe.get_installed_apps(_ensure_on_bench=True)
 	for app_name in frappe.get_all("Installed Application", pluck="app_name"):
+		if app_name not in installed_apps:
+			continue
+
 		has_setup_wizard = 0
 		if app_name == "frappe":
 			has_setup_wizard = 1
