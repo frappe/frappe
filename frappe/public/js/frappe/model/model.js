f406d782b918f052c97b827c220c41078f2ebf35
barredterra <14891507+barredterra@users.noreply.github.com>
2025-06-18 14:18:34 +0200

    fix(DocType): offer calendar view as default

--- frappe/public/js/frappe/model/model.js
+++ frappe/public/js/frappe/model/model.js
@@ -858,7 +858,7 @@ $.extend(frappe.model, {
 			let meta = frappe.get_meta(doctype);
 			let default_views = ["List", "Report", "Dashboard", "Kanban"];
 
-			if (meta.is_calendar_and_gantt && frappe.views.calendar[doctype]) {
+			if (meta.is_calendar_and_gantt) {
 				let views = ["Calendar", "Gantt"];
 				default_views.push(...views);
 			}
