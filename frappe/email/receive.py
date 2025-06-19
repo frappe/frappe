9207b8e3b1f30b33350bee381d1913ed82bb8db6
RitvikSardana <65544983+RitvikSardana@users.noreply.github.com>
2025-06-19 18:13:32 +0530

    fix: add flag for initial email sync (#33006)
    * fix: add flag for initial email sync
    
    * fix: rever from_uid calc
    
    (cherry picked from commit 4fbdaf3a9b424f22fe14e7f94a4c6edd0dbf140a)

--- frappe/email/receive.py
+++ frappe/email/receive.py
@@ -218,6 +218,9 @@ class EmailServer:
 		uidnext = int(self.parse_imap_response("UIDNEXT", message[0]) or "1")
 		frappe.db.set_value("Email Account", self.settings.email_account, "uidnext", uidnext)
 
+		if uid_validity is None:
+			frappe.flags.initial_sync = True
+
 		if not uid_validity or uid_validity != current_uid_validity:
 			# uidvalidity changed & all email uids are reindexed by server
 			Communication = frappe.qb.DocType("Communication")
