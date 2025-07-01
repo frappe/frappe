1394de10ee17f933930369251b24d7932ef4a3d8
sokumon <sohamkulkarns9@gmail.com>
2025-07-01 21:15:27 +0530

    fix: resolve conflicts

--- frappe/tests/test_permissions.py
+++ frappe/tests/test_permissions.py
@@ -624,17 +624,17 @@ class TestPermissions(IntegrationTestCase):
 
 		with self.set_user("testperm@example.com"):
 			# Website Manager able to read
-			getdoc("Blog Post", doc.name)
+			getdoc("Test Blog Post", doc.name)
 			doclist = [d.name for d in frappe.response.docs]
 			self.assertTrue(doc.name in doclist)
 
 			# Website Manager should not be able to delete
-			self.assertRaises(frappe.PermissionError, frappe.delete_doc, "Blog Post", doc.name)
+			self.assertRaises(frappe.PermissionError, frappe.delete_doc, "Test Blog Post", doc.name)
 
 		with self.set_user("test2@example.com"):
-			frappe.delete_doc("Blog Post", "-test-blog-post-title-new-1")
+			frappe.delete_doc("Test Blog Post", "_Test Blog Post Title New 1")
 
-		update("Blog Post", "Website Manager", 0, "delete", 1, 1)
+		update("Test Blog Post", "Website Manager", 0, "delete", 1, 1)
 
 	def test_clear_user_permissions(self):
 		current_user = frappe.session.user
