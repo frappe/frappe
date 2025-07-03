6a163561dd5e6a67dd96dadc17f677aa4966cce9
Akhil Narang <me@akhilnarang.dev>
2025-07-03 17:26:55 +0530

    feat(api/v2): set name if passed when creating document
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    (cherry picked from commit ef5dc763e988795078475629c5144851447b7788)
    
    # Conflicts:
    #	frappe/api/v2.py

--- frappe/api/v2.py
+++ frappe/api/v2.py
@@ -89,12 +89,22 @@ def create_doc(doctype: str):
 	data = frappe.form_dict
 	data.pop("doctype", None)
 <<<<<<< HEAD
+<<<<<<< HEAD
 	if (name := data.get("name")) and isinstance(name, str):
 		frappe.flags.api_name_set = True
 	return frappe.new_doc(doctype, **data).insert()
 =======
 	return frappe.new_doc(doctype, **data).insert().as_dict()
 >>>>>>> ad948c9b98 (Revert "Merge pull request #32327 from akhilnarang/allow-passing-name-in-api")
+=======
+
+	doc = frappe.new_doc(doctype, **data)
+
+	if (name := data.get("name")) and isinstance(name, str | int):
+		doc.flags.name_set = True
+
+	return doc.insert().as_dict()
+>>>>>>> ef5dc763e9 (feat(api/v2): set name if passed when creating document)
 
 
 def copy_doc(doctype: str, name: str, ignore_no_copy: bool = True):
