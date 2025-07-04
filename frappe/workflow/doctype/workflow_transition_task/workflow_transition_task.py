dfe0905bc5c347979361d7e45c2ac835a109c5dd
Ayush Chaudhari <ayush@frappe.io>
2025-07-04 16:49:17 +0530

    feat: add workflow task doctypes

---
+++ frappe/workflow/doctype/workflow_transition_task/workflow_transition_task.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2025, Frappe Technologies and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class WorkflowTransitionTask(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		enabled: DF.Check
+		execute_asynchronously: DF.Check
+		task: DF.Autocomplete | None
+	# end: auto-generated types
+
+	pass
