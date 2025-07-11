d7587bc50d11afcacae767f4a9bbcc02895dfb3f
Tuanbd MBWD <antivenomff@gmail.com>
2025-07-11 19:15:29 +0700

    fix preview file

--- frappe/core/doctype/file/file.js
+++ frappe/core/doctype/file/file.js
@@ -1,39 +1,30 @@
 frappe.ui.form.on("File", {
 	refresh: function (frm) {
+		// View File button
 		if (frm.doc.file_url && frm.doc.file_url.startsWith("s3://")) {
-			// Nếu file được lưu trên S3 (dùng API trung gian để stream)
-			frm.add_custom_button(__("View File"), () => {
-				frappe.call({
-					method: "frappe.utils.s3_file_handler.get_temp_s3_link",
-					args: {
-						file_name: frm.doc.name,
-						expiration: 600
-					},
-					callback: function (r) {
-						if (r && r.message) {
-							window.open(r.message);  // mở presigned URL
-						} else {
-							frappe.msgprint(__("Không thể tạo link xem file"));
-						}
-					}
-				});
+			frm.add_custom_button(__("View File"), async () => {
+				const url = await getRealFileUrl(frm.doc);
+				if (url) {
+					window.open(url);
+				} else {
+					frappe.msgprint(__("Không thể tạo link xem file"));
+				}
 			});
 		} else if (frm.doc.file_url) {
-			// Trường hợp file local hoặc không phải s3://
 			frm.add_custom_button(__("View File"), () => {
-				if (!frappe.utils.is_url(frm.doc.file_url)) {
-					window.open(window.location.origin + frm.doc.file_url);
-				} else {
-					window.open(frm.doc.file_url);
-				}
+				const url = frappe.utils.is_url(frm.doc.file_url)
+					? frm.doc.file_url
+					: window.location.origin + frm.doc.file_url;
+				window.open(url);
 			});
 		}
 
+		// Download button
 		if (!frm.doc.is_folder) {
-			// add download button
 			frm.add_custom_button(__("Download"), () => frm.trigger("download"), "fa fa-download");
 		}
 
+		// Public file warning
 		if (!frm.doc.is_private) {
 			frm.dashboard.set_headline(
 				__("This file is public. It can be accessed without authentication."),
@@ -42,58 +33,49 @@ frappe.ui.form.on("File", {
 		}
 
 		frm.toggle_display("preview", false);
-
-		// preview different file types
 		frm.trigger("preview_file");
 
 		let is_raster_image = /\.(gif|jpg|jpeg|tiff|png)$/i.test(frm.doc.file_url);
 		let is_optimizable = !frm.doc.is_folder && is_raster_image && frm.doc.file_size > 0;
 
-		// add optimize button
+		// Optimize image
 		is_optimizable && frm.add_custom_button(__("Optimize"), () => frm.trigger("optimize"));
 
-		// add unzip button
+		// Unzip if zip
 		if (frm.doc.file_name && frm.doc.file_name.split(".").splice(-1)[0] === "zip") {
 			frm.add_custom_button(__("Unzip"), () => frm.trigger("unzip"));
 		}
 	},
 
-	preview_file: function (frm) {
+	preview_file: async function (frm) {
+		let file_url = await getRealFileUrl(frm.doc);
+		let file_extension = (frm.doc.file_type || "").toLowerCase();
 		let $preview = "";
-		let file_extension = frm.doc.file_type.toLowerCase();
 
-		if (frappe.utils.is_image_file(frm.doc.file_url)) {
+		if (frappe.utils.is_image_file(file_url)) {
 			$preview = $(`<div class="img_preview">
-				<img
-					class="img-responsive"
-					src="${frappe.utils.escape_html(frm.doc.file_url)}"
-					onerror="${frm.toggle_display("preview", false)}"
-				/>
+				<img class="img-responsive" src="${frappe.utils.escape_html(file_url)}" />
 			</div>`);
-		} else if (frappe.utils.is_video_file(frm.doc.file_url)) {
+		} else if (frappe.utils.is_video_file(file_url)) {
 			$preview = $(`<div class="img_preview">
 				<video width="480" height="320" controls>
-					<source src="${frappe.utils.escape_html(frm.doc.file_url)}">
+					<source src="${frappe.utils.escape_html(file_url)}">
 					${__("Your browser does not support the video element.")}
 				</video>
 			</div>`);
 		} else if (file_extension === "pdf") {
 			$preview = $(`<div class="img_preview">
 				<object style="background:#323639;" width="100%">
-					<embed
-						style="background:#323639;"
-						width="100%"
-						height="1190"
-						src="${frappe.utils.escape_html(frm.doc.file_url)}" type="application/pdf"
-					>
+					<embed style="background:#323639;" width="100%" height="1190"
+						src="${frappe.utils.escape_html(file_url)}" type="application/pdf">
 				</object>
 			</div>`);
 		} else if (file_extension === "mp3") {
 			$preview = $(`<div class="img_preview">
 				<audio width="480" height="60" controls>
-					<source src="${frappe.utils.escape_html(frm.doc.file_url)}" type="audio/mpeg">
+					<source src="${frappe.utils.escape_html(file_url)}" type="audio/mpeg">
 					${__("Your browser does not support the audio element.")}
-				</audio >
+				</audio>
 			</div>`);
 		}
 
@@ -130,3 +112,22 @@ frappe.ui.form.on("File", {
 		});
 	},
 });
+
+
+async function getRealFileUrl(file_doc) {
+	if (file_doc.file_url.startsWith("s3://")) {
+		// Lấy presigned URL từ backend
+		const res = await frappe.call({
+			method: "frappe.utils.s3_file_handler.get_temp_s3_link",
+			args: {
+				file_name: file_doc.name,
+				expiration: 600,
+			},
+		});
+		return res.message || "";
+	} else if (!frappe.utils.is_url(file_doc.file_url)) {
+		return window.location.origin + file_doc.file_url;
+	} else {
+		return file_doc.file_url;
+	}
+}
