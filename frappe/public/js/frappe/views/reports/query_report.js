ffabc756694f2f85aa1e78e2230798692a7b57b3
Diptanil Saha <diptanil@frappe.io>
2025-07-16 12:33:50 +0530

    fix: export query report hidden columns (#33350)
    (cherry picked from commit 725659972ea38692506ab3e0dc8233cce2c41137)
    
    # Conflicts:
    #	frappe/public/js/frappe/views/reports/query_report.js

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -1570,6 +1570,32 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 			});
 		}
 
+<<<<<<< HEAD
+=======
+		if (this.report_settings.export_hidden_cols) {
+			const hidden_fields = [];
+			this.columns.forEach((column) => {
+				if (column.hidden) {
+					hidden_fields.push(column.label);
+				}
+			});
+			if (hidden_fields.length) {
+				extra_fields.push(
+					{
+						fieldname: "column_break_1",
+						fieldtype: "Column Break",
+					},
+					{
+						label: __("Include hidden columns"),
+						fieldname: "include_hidden_columns",
+						description: __("Hidden columns include: {0}", [hidden_fields.join(", ")]),
+						fieldtype: "Check",
+					}
+				);
+			}
+		}
+
+>>>>>>> 725659972e (fix: export query report hidden columns (#33350))
 		this.export_dialog = frappe.report_utils.get_export_dialog(
 			__(this.report_name),
 			extra_fields,
