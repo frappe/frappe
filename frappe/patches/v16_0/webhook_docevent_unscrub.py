595d588db1865c9ae4f1eb9029c69cb2b4533178
Ayush Chaudhari <ayush@frappe.io>
2025-07-18 16:43:37 +0530

    Revert "fix: patch for change in webhook fields"
    This reverts commit 2e9c51c43ca2b3698991fbe75cc4032368ab174c.

--- frappe/patches/v16_0/webhook_docevent_unscrub.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-
-def execute():
-	webhooks = frappe.get_all("Webhook", ["name", "webhook_docevent"])
-	for webhook in webhooks:
-		frappe.db.set_value(
-			"Webhook", webhook.name, "webhook_docevent", frappe.unscrub(webhook.webhook_docevent)
-		)
