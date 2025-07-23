6c59d2c2589f231eacac29f88c6d1b6e3a8f0828
mahsem <137205921+mahsem@users.noreply.github.com>
2025-07-23 14:44:33 +0200

    fix: web_form_context_title_translation (#33173)
    (cherry picked from commit 3efc476da92cc08f4c45a5a39b9812fb80bc3686)

--- frappe/website/doctype/web_form/web_form.py
+++ frappe/website/doctype/web_form/web_form.py
@@ -393,7 +393,7 @@ def get_context(context):
 			context.parents = frappe.safe_eval(self.breadcrumbs, {"_": _})
 
 		if self.show_list and frappe.form_dict.is_new:
-			context.title = _("New {0}").format(context.title)
+			context.title = _("New {0}").format(_(context.title))
 
 		context.has_header = (frappe.form_dict.name or frappe.form_dict.is_new) and (
 			frappe.session.user != "Guest" or not self.login_required
