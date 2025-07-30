8c895633878ce725b312758178d62246e4fc6ed1
Florian HENRY <florian.henry@open-concept.pro>
2025-07-30 11:32:11 +0200

    feat(recorder): remove strip_dirs in Recorder python cProcfile

--- frappe/recorder.py
+++ frappe/recorder.py
@@ -250,7 +250,7 @@ class Recorder:
 		if self.config.profile or self.profiler:
 			self.profiler.disable()
 			profiler_output = io.StringIO()
-			pstats.Stats(self.profiler, stream=profiler_output).strip_dirs().sort_stats(
+			pstats.Stats(self.profiler, stream=profiler_output).sort_stats(
 				"cumulative"
 			).print_stats()
 			profile = profiler_output.getvalue()
