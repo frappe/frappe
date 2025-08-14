954f11827f1ea162e8fdff0843ebec5b1bdfc505
Maxim Sysoev <maks4a@gmail.com>
2025-08-14 21:43:02 +0300

    feat: respect linl title in Preview Popup

--- frappe/desk/link_preview.py
+++ frappe/desk/link_preview.py
@@ -1,13 +1,16 @@
 import frappe
 from frappe.model import no_value_fields, table_fields
 from frappe.utils.caching import http_cache
+from frappe.www.printview import set_link_titles
 
 
 @frappe.whitelist()
 @http_cache(max_age=60 * 10)
 def get_preview_data(doctype, docname):
 	preview_fields = []
-	meta = frappe.get_meta(doctype)
+	doc = frappe.get_doc(doctype, docname)
+	set_link_titles(doc)
+	meta = doc.meta
 	if not meta.show_preview_popup:
 		return
 
@@ -30,7 +33,8 @@ def get_preview_data(doctype, docname):
 	preview_fields.append(image_field)
 	preview_fields.append("name")
 
-	preview_data = frappe.get_list(doctype, filters={"name": docname}, fields=preview_fields, limit=1)
+	preview_data = frappe.get_list(
+		doctype, filters={"name": docname}, fields=preview_fields, limit=1)
 
 	if not preview_data:
 		return
@@ -46,8 +50,9 @@ def get_preview_data(doctype, docname):
 	for key, val in preview_data.items():
 		if val and meta.has_field(key) and key not in [image_field, title_field, "name"]:
 			formatted_preview_data[meta.get_field(key).label] = frappe.format(
-				val,
-				meta.get_field(key).fieldtype,
+				value=val,
+				doc=doc,
+				df=meta.get_field(key),
 				translated=True,
 			)
 
