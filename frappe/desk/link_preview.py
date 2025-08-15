d2ddf5768bf7acf464cb1cc5f3dab4f116df7a81
Maxim Sysoev <maks4a@gmail.com>
2025-08-15 08:39:31 +0300

    Format code using pre-commit

--- frappe/desk/link_preview.py
+++ frappe/desk/link_preview.py
@@ -33,8 +33,7 @@ def get_preview_data(doctype, docname):
 	preview_fields.append(image_field)
 	preview_fields.append("name")
 
-	preview_data = frappe.get_list(
-		doctype, filters={"name": docname}, fields=preview_fields, limit=1)
+	preview_data = frappe.get_list(doctype, filters={"name": docname}, fields=preview_fields, limit=1)
 
 	if not preview_data:
 		return
