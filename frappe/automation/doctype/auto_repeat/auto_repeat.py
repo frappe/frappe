9c612f8bfa8639e01a254840bbed06d6d159435d
Ravibharathi <131471282+ravibharathi656@users.noreply.github.com>
2025-08-21 16:53:43 +0530

    fix(auto repeat): validate date (#33724)
    * fix(auto repeat): validate date
    
    * fix(auto repeat): use if instead of elif
    
    (cherry picked from commit 50fa79f74f202e406dc04b7f4e701a6649a0cd5d)
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- frappe/automation/doctype/auto_repeat/auto_repeat.py
+++ frappe/automation/doctype/auto_repeat/auto_repeat.py
@@ -87,9 +87,8 @@ class AutoRepeat(Document):
 
 	def before_insert(self):
 		if not frappe.flags.in_test:
-			start_date = self.start_date
-			today_date = today()
-			if start_date <= today_date:
+			today_date = getdate()
+			if getdate(self.start_date) < today_date:
 				self.start_date = today_date
 
 	def after_save(self):
@@ -134,16 +133,18 @@ class AutoRepeat(Document):
 			return
 
 		if self.end_date:
-			self.validate_from_to_dates("start_date", "end_date")
-		if self.end_date == today():
-			frappe.throw(_("End Date cannot be today."))
+			end_date = getdate(self.end_date)
 
-		if self.end_date == self.start_date:
-			frappe.throw(
-				_("{0} should not be same as {1}").format(
-					frappe.bold(_("End Date")), frappe.bold(_("Start Date"))
+			self.validate_from_to_dates("start_date", "end_date")
+
+			if end_date == getdate():
+				frappe.throw(_("End Date cannot be today."))
+			if end_date == getdate(self.start_date):
+				frappe.throw(
+					_("{0} should not be same as {1}").format(
+						frappe.bold(_("End Date")), frappe.bold(_("Start Date"))
+					)
 				)
-			)
 
 	def validate_email_id(self):
 		if self.notify_by_email:
