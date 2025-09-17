57e9400250311114176d1fe977047deef74a5967
Akhil Narang <me@akhilnarang.dev>
2025-09-18 01:24:17 +0530

    fix: call `format()` on the result of `_()` (#34016)
    Co-authored-by: barredterra <14891507+barredterra@users.noreply.github.com>
    (cherry picked from commit 38fbe65d093917c3bab028c9fe865786c2090ec2)

--- frappe/desk/query_report.py
+++ frappe/desk/query_report.py
@@ -350,9 +350,8 @@ def export_query():
 		)
 		frappe.msgprint(
 			_(
-				"Your report is being generated in the background. "
-				"You will receive an email on {0} with a download link once it is ready.".format(user_email)
-			)
+				"Your report is being generated in the background. You will receive an email on {0} with a download link once it is ready."
+			).format(user_email)
 		)
 		return
 
