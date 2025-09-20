ccfe79aed671f9181cab81d6a6a0f77b798e0d20
KerollesFathy <kerolles.f@outlook.com>
2025-09-20 10:20:14 +0000

    fix: update app logo retrieval to use Website Settings instead of Navbar Settings for ERPNext

--- frappe/apps.py
+++ frappe/apps.py
@@ -38,8 +38,8 @@ def get_apps():
 				if has_permission_path and not frappe.get_attr(has_permission_path)():
 					continue
 
-				if app == "erpnext" and frappe.db.get_single_value("Navbar Settings", "app_logo"):
-					app_detail["logo"] = frappe.db.get_single_value("Navbar Settings", "app_logo")
+				if app == "erpnext" and frappe.db.get_single_value("Website Settings", "app_logo"):
+					app_detail["logo"] = frappe.db.get_single_value("Website Settings", "app_logo")
 
 				app_list.append(
 					{
