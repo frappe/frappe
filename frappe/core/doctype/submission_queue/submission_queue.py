48234a936c87f1175b27e63189cd84cade838456
Dhinesh <dhinesh@aerele.in>
2025-09-23 12:45:53 +0530

    fix: check Submission Queue already exists for the record

--- frappe/core/doctype/submission_queue/submission_queue.py
+++ frappe/core/doctype/submission_queue/submission_queue.py
@@ -164,6 +164,18 @@ class SubmissionQueue(Document):
 
 
 def queue_submission(doc: Document, action: str, alert: bool = True):
+	already_in_queue = frappe.db.get_value(
+		"Submission Queue", {"ref_doctype": doc.doctype, "ref_docname": doc.name, "status": "Queued"}
+	)
+	if already_in_queue:
+		frappe.msgprint(
+			_("Queued for Submission. You can track the progress over {0}.").format(
+				f"<a href='/app/submission-queue/{already_in_queue}'><b>here</b></a>"
+			),
+			indicator="green",
+			alert=True,
+		)
+		return
 	queue = frappe.new_doc("Submission Queue")
 	queue.ref_doctype = doc.doctype
 	queue.ref_docname = doc.name
