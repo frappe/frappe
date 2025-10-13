825b3ec9984dcd9fdc4cded402b872ad9a00dab3
UmakanthKaspa <kaspaumakanth1999@gmail.com>
2025-10-13 17:29:53 +0000

    fix: enable fetching additional field types in both forms and quick entry dialogs
    (cherry picked from commit 691d79493d3014e836500ba5e154267987e07780)

--- frappe/public/js/frappe/form/controls/link.js
+++ frappe/public/js/frappe/form/controls/link.js
@@ -808,6 +808,13 @@ frappe.ui.form.ControlLink = class ControlLink extends frappe.ui.form.ControlDat
 					"Select",
 					"Duration",
 					"Time",
+					"Percent",
+					"Phone",
+					"Barcode",
+					"Autocomplete",
+					"Icon",
+					"Color",
+					"Rating",
 				].includes(df.fieldtype) ||
 				df.read_only == 1 ||
 				df.is_virtual == 1;
