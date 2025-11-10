4c22dfd55934a1409603363e8186377de9f1036a
barredterra <14891507+barredterra@users.noreply.github.com>
2025-11-10 12:00:18 +0100

    fix: more informative warning message

--- frappe/gettext/translate.py
+++ frappe/gettext/translate.py
@@ -180,13 +180,25 @@ def _get_ignored_strings(app: str) -> set[tuple[str, str | None]]:
 		try:
 			catalog = get_catalog(ignore_app)
 		except ModuleNotFoundError:
-			secho(f"App '{ignore_app}' is not installed. Skipping", err=True, fg="yellow")
+			secho(
+				f"App '{ignore_app}' specified in '{app}/hooks.py' 'ignore_translatable_strings_from' hook is not installed. Skipping",
+				err=True,
+				fg="yellow",
+			)
 			continue
 		except ImportError:
-			secho(f"App '{ignore_app}' hooks.py could not be imported. Skipping", err=True, fg="yellow")
+			secho(
+				f"App '{ignore_app}' specified in '{app}/hooks.py' 'ignore_translatable_strings_from' hook could not be imported. Skipping",
+				err=True,
+				fg="yellow",
+			)
 			continue
 		except AttributeError:
-			secho(f"Site not initialized. Cannot load app '{ignore_app}'. Skipping", err=True, fg="yellow")
+			secho(
+				f"Site not initialized. Cannot load app '{ignore_app}' specified in '{app}/hooks.py' 'ignore_translatable_strings_from' hook. Skipping",
+				err=True,
+				fg="yellow",
+			)
 			continue
 
 		for message in catalog:
