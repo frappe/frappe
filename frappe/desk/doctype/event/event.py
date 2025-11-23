197aafd9b4b12906f4015173f0c27bf771d5ce82
AarDG10 <aarol.dsouza@gmail.com>
2025-11-23 12:12:03 +0530

    fix(postgres): fix order_by in get_all for sync_communication

--- frappe/desk/doctype/event/event.py
+++ frappe/desk/doctype/event/event.py
@@ -139,6 +139,7 @@ class Event(Document):
 				],
 				pluck="name",
 				distinct=True,
+				order_by=None,
 			):
 				for comm in communications:
 					communication = frappe.get_doc("Communication", comm)
