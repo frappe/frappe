93120e1b224679dce09131404a2d4a97080315ce
Karm Soni <karmdsoni8159@gmail.com>
2025-11-28 13:15:52 +0530

    fix: return empty string instead of undefined for URL and IBAN options

--- frappe/public/js/frappe/form/formatters.js
+++ frappe/public/js/frappe/form/formatters.js
@@ -35,11 +35,11 @@ frappe.form.formatters = {
 	},
 	Data: function (value, df) {
 		if (df && df.options == "URL") {
-			if (!value) return;
+			if (!value) return "";
 			return `<a href="${value}" title="Open Link" target="_blank">${value}</a>`;
 		}
 		if (df && df.options == "IBAN") {
-			if (!value) return;
+			if (!value) return "";
 			return frappe.utils.get_formatted_iban(value);
 		}
 		value = value == null ? "" : value;
