392d8d4510ddde23ef2c60afe0254ee383b27a70
Akhil Narang <me@akhilnarang.dev>
2025-12-12 15:48:45 +0530

    fix(attachments): escape file_url
    If a file name has `&amp;` it gets converted to `&` implicitly.
    So we need to escape it, leading to `&amp;amp;` which would become `&amp;`
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- frappe/public/js/frappe/form/sidebar/attachments.js
+++ frappe/public/js/frappe/form/sidebar/attachments.js
@@ -123,7 +123,9 @@ frappe.ui.form.Attachments = class Attachments {
 		var me = this;
 
 		let file_label = `
-			<a href="${file_url}" target="_blank" title="${frappe.utils.escape_html(file_name)}"
+			<a href="${frappe.utils.escape_html(file_url)}" target="_blank" title="${frappe.utils.escape_html(
+			file_name
+		)}"
 				class="ellipsis attachment-file-label"
 			>
 				<span>${frappe.utils.xss_sanitise(file_name)}</span>
