65bcd8fa0e3c4fe7322bda4f8beb2e55fcc126a1
Abhishek Kumar <abhishekism0010@gmail.com>
2025-12-16 16:15:56 +0530

    Refactor communication date copy logic for DB compatibility
    Updated the execute function to handle PostgreSQL and MySQL/MariaDB syntax for copying communication dates.

--- frappe/core/doctype/communication_link/patches/copy_communication_date_to_link.py
+++ frappe/core/doctype/communication_link/patches/copy_communication_date_to_link.py
@@ -1,32 +1,65 @@
 import frappe
 
 
-# copy communication_date from Communication to Communication Link
-def execute():
-	batch_size = 10_000
+  # copy communication_date from Communication to Communication Link
+  def execute():
+      batch_size = 10_000
 
-	while True:
-		frappe.db.sql(
-			"""
-			update `tabCommunication Link` cl
-			inner join `tabCommunication` c on cl.parent = c.name
-			set cl.communication_date = c.communication_date
-			where cl.communication_date is null
-			and c.communication_date is not null
-			limit %s
-			""",
-			(batch_size,),
-		)
+      # Check if using PostgreSQL
+      is_postgres = frappe.db.db_type == "postgres"
 
-		frappe.db.commit()
+      while True:
+          if is_postgres:
+              # PostgreSQL syntax
+              frappe.db.sql(
+                  """
+                  update "tabCommunication Link" cl
+                  set communication_date = c.communication_date
+                  from "tabCommunication" c
+                  where cl.parent = c.name
+                  and cl.communication_date is null
+                  and c.communication_date is not null
+                  limit %s
+                  """,
+                  (batch_size,),
+              )
+          else:
+              # MySQL/MariaDB syntax
+              frappe.db.sql(
+                  """
+                  update `tabCommunication Link` cl
+                  inner join `tabCommunication` c on cl.parent = c.name
+                  set cl.communication_date = c.communication_date
+                  where cl.communication_date is null
+                  and c.communication_date is not null
+                  limit %s
+                  """,
+                  (batch_size,),
+              )
 
-		if not frappe.db.sql(
-			"""
-			select 1 from `tabCommunication Link` cl
-			inner join `tabCommunication` c on cl.parent = c.name
-			where cl.communication_date is null
-			and c.communication_date is not null
-			limit 1
-			"""
-		):
-			break
+          frappe.db.commit()
+
+          # Check if more rows need updating
+          if is_postgres:
+              check = frappe.db.sql(
+                  """
+                  select 1 from "tabCommunication Link" cl
+                  inner join "tabCommunication" c on cl.parent = c.name
+                  where cl.communication_date is null
+                  and c.communication_date is not null
+                  limit 1
+                  """
+              )
+          else:
+              check = frappe.db.sql(
+                  """
+                  select 1 from `tabCommunication Link` cl
+                  inner join `tabCommunication` c on cl.parent = c.name
+                  where cl.communication_date is null
+                  and c.communication_date is not null
+                  limit 1
+                  """
+              )
+
+          if not check:
+              break
