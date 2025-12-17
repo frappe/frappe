1abc5e3b793d6acdf77add45dff87455b3adb574
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-12-17 17:35:48 +0530

    fix: don't auto enable create log for `All` and `Cron`
    (cherry picked from commit a9a4c7981911662805e1d68c578aa30092466566)
    
    # Conflicts:
    #	frappe/core/doctype/server_script/server_script.py

--- frappe/core/doctype/server_script/server_script.py
+++ frappe/core/doctype/server_script/server_script.py
@@ -120,6 +120,7 @@ class ServerScript(Document):
 				script_name=self.name, frequency=self.event_frequency, cron_format=cron_format
 			)
 
+<<<<<<< HEAD
 	def clear_scheduled_events(self):
 		"""Deletes existing scheduled jobs by Server Script if self.event_frequency or self.cron_format has changed"""
 		if (
@@ -128,6 +129,44 @@ class ServerScript(Document):
 		) or (self.has_value_changed("script_type") and self.script_type != "Scheduler Event"):
 			for scheduled_job in self.scheduled_jobs:
 				frappe.delete_doc("Scheduled Job Type", scheduled_job.name, delete_permanently=1)
+=======
+		def get_scheduled_job() -> "ScheduledJobType":
+			if scheduled_script := frappe.db.get_value("Scheduled Job Type", {"server_script": self.name}):
+				return frappe.get_doc("Scheduled Job Type", scheduled_script)
+			else:
+				should_create_log = self.event_frequency not in ("All", "Cron")
+				return frappe.get_doc(
+					{
+						"doctype": "Scheduled Job Type",
+						"server_script": self.name,
+						"create_log": should_create_log,
+					}
+				)
+
+		previous_script_type = self.get_value_before_save("script_type")
+		if previous_script_type != self.script_type and previous_script_type == "Scheduler Event":
+			get_scheduled_job().update({"stopped": 1}).save()
+			return
+
+		if self.script_type != "Scheduler Event" or not (
+			self.has_value_changed("event_frequency")
+			or self.has_value_changed("cron_format")
+			or self.has_value_changed("disabled")
+			or self.has_value_changed("script_type")
+		):
+			return
+
+		get_scheduled_job().update(
+			{
+				"method": frappe.scrub(f"{self.name}-{self.event_frequency}"),
+				"frequency": self.event_frequency,
+				"cron_format": self.cron_format if self.event_frequency == "Cron" else "",
+				"stopped": self.disabled,
+			}
+		).save()
+
+		frappe.msgprint(_("Scheduled execution for script {0} has updated").format(self.name), alert=True)
+>>>>>>> a9a4c79819 (fix: don't auto enable create log for `All` and `Cron`)
 
 	def check_if_compilable_in_restricted_context(self):
 		"""Check compilation errors and send them back as warnings."""
