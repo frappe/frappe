a2537a6886364bec01db4b7eab1f6611266d0d3e
hichamdz38 <dachirhicham@gmail.com>
2025-12-18 10:03:42 +0000

    fix: support postgres database for the communication link patch

--- frappe/core/doctype/communication_link/patches/copy_communication_date_to_link.py
+++ frappe/core/doctype/communication_link/patches/copy_communication_date_to_link.py
@@ -1,32 +1,38 @@
+from pypika.terms import Term
+
 import frappe
+
+
+class Subquery(Term):
+	def __init__(self, query):
+		self.query = query
+
+	def get_sql(self, **kwargs):
+		return f"({self.query.get_sql(**kwargs)})"
 
 
 # copy communication_date from Communication to Communication Link
 def execute():
+	cl = frappe.qb.DocType("Communication Link")
+	c = frappe.qb.DocType("Communication")
 	batch_size = 10_000
 
 	while True:
-		frappe.db.sql(
-			"""
-			update `tabCommunication Link` cl
-			inner join `tabCommunication` c on cl.parent = c.name
-			set cl.communication_date = c.communication_date
-			where cl.communication_date is null
-			and c.communication_date is not null
-			limit %s
-			""",
-			(batch_size,),
-		)
+		ids = (
+			frappe.qb.from_(cl)
+			.join(c)
+			.on(cl.parent == c.name)
+			.select(cl.name)
+			.where(cl.communication_date.isnull())
+			.where(c.communication_date.isnotnull())
+			.limit(batch_size)
+		).run(pluck=True)
+
+		if not ids:
+			break
+
+		subquery = frappe.qb.from_(c).select(c.communication_date).where(c.name == cl.parent)
+
+		(frappe.qb.update(cl).set(cl.communication_date, Subquery(subquery)).where(cl.name.isin(ids))).run()
 
 		frappe.db.commit()
-
-		if not frappe.db.sql(
-			"""
-			select 1 from `tabCommunication Link` cl
-			inner join `tabCommunication` c on cl.parent = c.name
-			where cl.communication_date is null
-			and c.communication_date is not null
-			limit 1
-			"""
-		):
-			break
