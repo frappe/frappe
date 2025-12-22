c0fca6682f1058b57cf32aec20d8d3c0fe2c07f1
Abhishek Kumar <abhishekism0010@gmail.com>
2025-12-23 01:16:19 +0530

    fix: fixed whitespace issue for github actions

--- frappe/core/doctype/communication_link/patches/copy_communication_date_to_link.py
+++ frappe/core/doctype/communication_link/patches/copy_communication_date_to_link.py
@@ -1,12 +1,14 @@
 import frappe
 from frappe.query_builder import DocType
 
+
 # copy communication_date from Communication to Communication Link
 def execute():
 	batch_size = 10_000
-	
+
 	CommunicationLink = DocType("Communication Link")
 	Communication = DocType("Communication")
+
 	while True:
 		# Fetch records that need updating with their communication_date
 		records = (
@@ -18,10 +20,10 @@ def execute():
 			.where(Communication.communication_date.isnotnull())
 			.limit(batch_size)
 		).run(as_dict=True)
-		
+
 		if not records:
 			break
-			
+
 		# Update records in batch
 		for record in records:
 			(
@@ -29,5 +31,5 @@ def execute():
 				.set(CommunicationLink.communication_date, record.communication_date)
 				.where(CommunicationLink.name == record.name)
 			).run()
-		
+
 		frappe.db.commit()
