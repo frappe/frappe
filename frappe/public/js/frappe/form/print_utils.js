dd967c3682f9a0154f218eb77c8e40f11d080eca
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-12-30 11:24:47 +0530

    revert: remove index column option from print settings and template

--- frappe/public/js/frappe/form/print_utils.js
+++ frappe/public/js/frappe/form/print_utils.js
@@ -65,13 +65,6 @@ frappe.ui.get_print_settings = function (
 	if (pick_columns) {
 		columns.push(
 			{
-				label: __("Include Index Column"),
-				fieldtype: "Check",
-				fieldname: "include_index_column",
-				default: 1,
-				depends_on: "eval: !doc.print_format",
-			},
-			{
 				label: __("Pick Columns"),
 				fieldtype: "Check",
 				fieldname: "pick_columns",
@@ -106,7 +99,6 @@ frappe.ui.get_print_settings = function (
 			}
 
 			if (settings.print_format) {
-				settings.include_index_column = 0;
 				settings.pick_columns = 0;
 				settings.columns = [];
 			}
