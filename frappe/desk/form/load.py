frappe/frappe

commit dbfcbe499372ecdd594538a0593102e9ff2f3b07

fix: Fetch user info for owner, modified_by and versions (#35557)
(cherry picked from commit e7598ec3a8107995f41dfd4fe07dac55f3f0e72c)

frappe/desk/form/load.py
@@ -138,7 +138,7 @@ def get_docinfo(doc=None, doctype=None, name=None):
 		}
 	)
 
-	update_user_info(docinfo)
+	update_user_info(docinfo, doc)
 
 	frappe.response["docinfo"] = docinfo
 
@@ -480,8 +480,12 @@ def send_link_titles(link_titles):
 	frappe.local.response["_link_titles"].update(link_titles)
 
 
-def update_user_info(docinfo):
+def update_user_info(docinfo, doc=None):
 	users = set()
+
+	if doc:
+		users.add(doc.owner)
+		users.add(doc.modified_by)
 
 	users.update(d.sender for d in docinfo.communications)
 	users.update(d.user for d in docinfo.shared)
@@ -493,6 +497,7 @@ def update_user_info(docinfo):
 	users.update(d.owner for d in docinfo.attachment_logs)
 	users.update(d.owner for d in docinfo.assignment_logs)
 	users.update(d.owner for d in docinfo.comments)
+	users.update(d.owner for d in docinfo.versions)
 
 	frappe.utils.add_user_info(users, docinfo.user_info)
 

commit 6f22fae1c6bd4c2a4ccea5e89925df9709e01296

fix(desk): guard owner/modified_by access in update_user_info
Fixes a regression introduced by #35557

This updates the code to safely check for the presence of those
fields using `getattr` before adding them to the uservlist. This
preserves the intended upstream behavior while avoiding
runtime errors for virtual/single/transient documents.

frappe/desk/form/load.py
@@ -481,8 +481,9 @@ def update_user_info(docinfo, doc=None):
 	users = set()
 
 	if doc:
-		users.add(doc.owner)
-		users.add(doc.modified_by)
+		for field in ("owner", "modified_by"):
+			if getattr(doc, field, None):
+				users.add(getattr(doc, field))
 
 	users.update(d.sender for d in docinfo.communications)
 	users.update(d.user for d in docinfo.shared)