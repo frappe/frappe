95a3b003378a4164f4ecab50fef280600aada0d4
ismxilxrif <38789073+ismxilxrif@users.noreply.github.com>
2026-01-01 15:03:15 +0800

    fix(desk): guard owner/modified_by access in update_user_info (#35581)
    * fix(desk): guard owner/modified_by access in update_user_info
    
    Fixes a regression introduced by #35557
    
    This updates the code to safely check for the presence of those
    fields using `getattr` before adding them to the uservlist. This
    preserves the intended upstream behavior while avoiding
    runtime errors for virtual/single/transient documents.
    
    * chore: cache getattr instead of calling it twice
    
    * refactor: less code
    
    ---------
    
    Co-authored-by: Ankush Menat <ankush@frappe.io>
    (cherry picked from commit 2f50f3174f66ca9b229ce71efd2da0c4f27cf5f5)
    
    # Conflicts:
    #	frappe/desk/doctype/system_health_report/test_system_health_report.py

--- frappe/desk/doctype/system_health_report/test_system_health_report.py
+++ frappe/desk/doctype/system_health_report/test_system_health_report.py
@@ -2,9 +2,14 @@
 # See license.txt
 
 import frappe
+<<<<<<< HEAD
 from frappe.tests.utils import FrappeTestCase
+=======
+from frappe.desk.form.load import getdoc
+from frappe.tests import IntegrationTestCase
+>>>>>>> 2f50f3174f (fix(desk): guard owner/modified_by access in update_user_info (#35581))
 
 
 class TestSystemHealthReport(FrappeTestCase):
 	def test_it_works(self):
-		frappe.get_doc("System Health Report")
+		getdoc("System Health Report", "System Health Report")
