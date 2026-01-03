829a49ba6ffc446fb2d8b6c911c427ea9e10d0f2
Matt Howard <github.severity519@passmail.net>
2026-01-03 01:45:05 -0500

    fix(postgres): make communication link patch compatible

--- frappe/core/doctype/communication_link/patches/copy_communication_date_to_link.py
+++ frappe/core/doctype/communication_link/patches/copy_communication_date_to_link.py
@@ -3,11 +3,20 @@ import frappe
 
 # copy communication_date from Communication to Communication Link
 def execute():
-	frappe.db.sql(
-		"""
-        update `tabCommunication Link` cl
-        inner join `tabCommunication` c on cl.parent = c.name
-        set cl.communication_date = c.communication_date
-        where c.communication_date is not null
-    """
+	frappe.db.multisql(
+		{
+			"mariadb": """
+				UPDATE `tabCommunication Link` cl
+				INNER JOIN `tabCommunication` c ON cl.parent = c.name
+				SET cl.communication_date = c.communication_date
+				WHERE c.communication_date IS NOT NULL
+			""",
+			"postgres": """
+				UPDATE "tabCommunication Link" cl
+				SET communication_date = c.communication_date
+				FROM "tabCommunication" c
+				WHERE cl.parent = c.name
+				  AND c.communication_date IS NOT NULL
+			""",
+		}
 	)
