18901f77e84a24689942375ca52462c70c7dcc0b
Matt Howard <github.severity519@passmail.net>
2026-01-03 01:45:05 -0500

    fix(postgres): make communication link patch compatible
    Keep the new hotfix batching logic, but make it Postgres-safe.

--- frappe/core/doctype/communication_link/patches/copy_communication_date_to_link.py
+++ frappe/core/doctype/communication_link/patches/copy_communication_date_to_link.py
@@ -6,27 +6,65 @@ def execute():
 	batch_size = 10_000
 
 	while True:
-		frappe.db.sql(
-			"""
-			update `tabCommunication Link` cl
-			inner join `tabCommunication` c on cl.parent = c.name
-			set cl.communication_date = c.communication_date
-			where cl.communication_date is null
-			and c.communication_date is not null
-			limit %s
-			""",
-			(batch_size,),
-		)
+		if frappe.db.db_type == "postgres":
+			# Postgres doesn't support UPDATE..JOIN..LIMIT.
+			# Use UPDATE..FROM and limit via ctid selection.
+			frappe.db.sql(
+				"""
+				UPDATE "tabCommunication Link" cl
+				SET communication_date = c.communication_date
+				FROM "tabCommunication" c
+				WHERE cl.parent = c.name
+				  AND cl.communication_date IS NULL
+				  AND c.communication_date IS NOT NULL
+				  AND cl.ctid IN (
+						SELECT cl2.ctid
+						FROM "tabCommunication Link" cl2
+						JOIN "tabCommunication" c2 ON cl2.parent = c2.name
+						WHERE cl2.communication_date IS NULL
+						  AND c2.communication_date IS NOT NULL
+						LIMIT %s
+				  )
+				""",
+				(batch_size,),
+			)
 
-		frappe.db.commit()
+			frappe.db.commit()
 
-		if not frappe.db.sql(
-			"""
-			select 1 from `tabCommunication Link` cl
-			inner join `tabCommunication` c on cl.parent = c.name
-			where cl.communication_date is null
-			and c.communication_date is not null
-			limit 1
-			"""
-		):
-			break
+			if not frappe.db.sql(
+				"""
+				SELECT 1
+				FROM "tabCommunication Link" cl
+				JOIN "tabCommunication" c ON cl.parent = c.name
+				WHERE cl.communication_date IS NULL
+				  AND c.communication_date IS NOT NULL
+				LIMIT 1
+				"""
+			):
+				break
+
+		else:
+			frappe.db.sql(
+				"""
+				update `tabCommunication Link` cl
+				inner join `tabCommunication` c on cl.parent = c.name
+				set cl.communication_date = c.communication_date
+				where cl.communication_date is null
+				  and c.communication_date is not null
+				limit %s
+				""",
+				(batch_size,),
+			)
+
+			frappe.db.commit()
+
+			if not frappe.db.sql(
+				"""
+				select 1 from `tabCommunication Link` cl
+				inner join `tabCommunication` c on cl.parent = c.name
+				where cl.communication_date is null
+				  and c.communication_date is not null
+				limit 1
+				"""
+			):
+				break
