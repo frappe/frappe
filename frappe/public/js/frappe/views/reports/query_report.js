a99f133a6d71b1238422aa335cca93f9ed000fa9
Abdeali Chharchhoda <abdealiking786@gmail.com>
2026-01-11 18:44:28 +0530

    fix: set filter value only if availabele in report

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -1469,15 +1469,15 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 			}, {});
 	}
 
-	get_filter(fieldname, warn = false) {
+	get_filter(fieldname, warn = true) {
 		const field = (this.filters || []).find((f) => f.df.fieldname === fieldname);
-		if (warn && !field) {
+		if (!field && warn) {
 			console.warn(`[Query Report] Invalid filter: ${fieldname}`);
 		}
 		return field;
 	}
 
-	get_filter_value(fieldname, warn = false) {
+	get_filter_value(fieldname, warn = true) {
 		const field = this.get_filter(fieldname, warn);
 		return field ? field.get_value() : null;
 	}
@@ -1498,7 +1498,11 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 				this._no_refresh = false;
 			}
 
-			this.get_filter(fieldname).set_value(value);
+			const filter = this.get_filter(fieldname);
+
+			if (filter) {
+				filter.set_value(value);
+			}
 		});
 	}
 
