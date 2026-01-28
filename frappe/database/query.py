ce74e13427b9f21da66c66c383cb3dabd02940e9
p-dhan <p.g.sso.f@gmail.com>
2026-01-28 22:47:31 +0000

    fix

--- frappe/database/query.py
+++ frappe/database/query.py
@@ -919,9 +919,7 @@ class Engine:
 							self.query = child_field_handler.apply_join(self.query, engine=self)
 							return child_field_handler.field
 
-				permission_type = self.get_permission_type(target_doctype, parent_doctype_for_perm)
-				if permission_type != "select":
-					self._check_field_permission(target_doctype, target_fieldname, parent_doctype_for_perm)
+				self._check_field_permission(target_doctype, target_fieldname, parent_doctype_for_perm)
 				# Convert string field name to pypika Field object for the specified/current doctype
 				return frappe.qb.DocType(target_doctype)[target_fieldname]
 
