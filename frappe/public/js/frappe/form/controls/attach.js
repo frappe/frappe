7bd7c7f1802ba4f29410a83c5290a59c5cf54d45
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2026-02-05 13:07:18 +0530

    chore: pre-commit fix formatting

--- frappe/public/js/frappe/form/controls/attach.js
+++ frappe/public/js/frappe/form/controls/attach.js
@@ -66,7 +66,12 @@ frappe.ui.form.ControlAttach = class ControlAttach extends frappe.ui.form.Contro
 		this.set_upload_options();
 		this.upload_options.restrictions.allowed_file_types = ["image/*"];
 		// file types like .heic/.tiff are not supported for preview directly in the browser, so we block the user from uploading them
-		this.upload_options.restrictions.blocked_file_types = ["image/heic", "image/heif", "image/tiff", "image/tif"];
+		this.upload_options.restrictions.blocked_file_types = [
+			"image/heic",
+			"image/heif",
+			"image/tiff",
+			"image/tif",
+		];
 		this.file_uploader = new frappe.ui.FileUploader(this.upload_options);
 	}
 	set_upload_options() {
--- frappe/public/js/frappe/form/controls/attach_image.js
+++ frappe/public/js/frappe/form/controls/attach_image.js
@@ -22,6 +22,11 @@ frappe.ui.form.ControlAttachImage = class ControlAttachImage extends frappe.ui.f
 		super.set_upload_options();
 		this.upload_options.restrictions.allowed_file_types = ["image/*"];
 		// file types like .heic/.tiff are not supported for preview directly in the browser, so we block the user from uploading them
-		this.upload_options.restrictions.blocked_file_types = ["image/heic", "image/heif", "image/tiff", "image/tif"];
+		this.upload_options.restrictions.blocked_file_types = [
+			"image/heic",
+			"image/heif",
+			"image/tiff",
+			"image/tif",
+		];
 	}
 };
