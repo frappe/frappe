e0f2895ad79b53e88bf95d04c04796408d3fcb6a
Ankush Menat <ankush@frappe.io>
2026-02-06 13:13:25 +0530

    fix: Log timeout and deadlocks
    closes https://github.com/frappe/frappe/issues/34254

--- frappe/database/database.py
+++ frappe/database/database.py
@@ -275,9 +275,11 @@ class Database:
 				frappe.log(f"Syntax error in query:\n{query} {values or ''}")
 
 			elif self.is_deadlocked(e):
+				self.db_type == "mariadb" and frappe.log_error("Query deadlocked", defer_insert=True)
 				raise frappe.QueryDeadlockError(e) from e
 
 			elif self.is_timedout(e):
+				self.db_type == "mariadb" and frappe.log_error("Query timed out", defer_insert=True)
 				raise frappe.QueryTimeoutError(e) from e
 
 			elif self.is_read_only_mode_error(e):
