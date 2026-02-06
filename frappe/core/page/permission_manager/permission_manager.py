d60819a7dfb0ba1e4645230f385ee49311c65916
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2026-02-06 11:13:13 +0530

    fix(permission_manager): enhance permission validation for Submit and Amend ptypes

--- frappe/core/page/permission_manager/permission_manager.py
+++ frappe/core/page/permission_manager/permission_manager.py
@@ -141,6 +141,23 @@ def update(doctype: str, role: str, permlevel: int, ptype: str, value=None, if_o
 	if ptype == "report" and value == "1" and if_owner == "1":
 		frappe.throw(_("Cannot set 'Report' permission if 'Only If Creator' permission is set"))
 
+	# Submit -> Write permission must be set
+	# Amend -> Write and Create permission must be set
+	if ptype in ["submit", "amend"] and value == "1":
+		has_write, has_create = frappe.db.get_value("Custom DocPerm", {"role": role}, ["write", "create"], order_by="modified desc")
+		if ptype == "submit" and not has_write:
+			frappe.throw(_(f"Cannot set {ptype.capitalize()} permission without setting Write permission"))
+		if ptype == "amend" and not (has_create and has_write):
+			frappe.throw(_(f"Cannot set {ptype.capitalize()} permission without setting Create or Write permission"))
+
+	if ptype in ["write", "create"] and value == "0":
+		# cannot remove write and create permission without removing submit and amend permissions
+		has_submit, has_amend = frappe.db.get_value("Custom DocPerm", {"role": role}, ["submit", "amend"], order_by="modified desc")
+		if ptype == "write" and has_submit:
+			frappe.throw(_(f"Cannot remove {ptype.capitalize()} permission without removing Submit permission"))
+		if (ptype == "create" or ptype == "write") and has_amend:
+			frappe.throw(_(f"Cannot remove {ptype.capitalize()} permission without removing Amend permission"))
+
 	out = update_permission_property(doctype, role, permlevel, ptype, value, if_owner=if_owner)
 
 	if ptype == "if_owner" and value == "1":
