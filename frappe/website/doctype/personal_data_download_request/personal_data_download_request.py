48419cba3a0dcb2aac6f13cf90ceac061001e672
Shrihari Mahabal <166826779+ShrihariMahabal@users.noreply.github.com>
2026-02-07 01:51:34 +0530

    fix: update file size check flag name
    Co-authored-by: Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>

--- frappe/website/doctype/personal_data_download_request/personal_data_download_request.py
+++ frappe/website/doctype/personal_data_download_request/personal_data_download_request.py
@@ -48,7 +48,7 @@ class PersonalDataDownloadRequest(Document):
 				"is_private": 1,
 			}
 		)
-		f.flags.skip_max_file_size_check = True
+		f.flags.skip_file_size_check = True
 		f.save(ignore_permissions=True)
 
 		file_link = (
