433e8645fd211fa72fe80c0cf1a0369abc4b7476
Kaushal Shriwas <kaushalshriwas118@gmail.com>
2026-02-12 12:53:37 +0530

    refactor: use bootstrap text-break instead of inline style
    (cherry picked from commit cabe78f8dd365d14bc46563c1c7b1546869d74c9)

--- frappe/public/js/frappe/form/link_selector.js
+++ frappe/public/js/frappe/form/link_selector.js
@@ -97,9 +97,9 @@ frappe.ui.form.LinkSelector = class LinkSelector {
 						var row = $(
 							repl(
 								'<div class="row link-select-row">\
-						<div class="col-xs-4" style="overflow-wrap: break-word;">\
+						<div class="col-xs-4 text-break">\
 							<b><a href="#">%(name)s</a></b></div>\
-						<div class="col-xs-8" style="overflow-wrap: break-word;">\
+						<div class="col-xs-8 text-break">\
 							<span class="text-muted">%(values)s</span></div>\
 						</div>',
 								{
