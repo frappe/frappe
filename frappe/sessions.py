7199043f5799a885cb4115d8ce1ce72d35d0beee
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2026-02-15 13:02:18 +0530

    fix: Resolve conflicts

--- frappe/sessions.py
+++ frappe/sessions.py
@@ -411,9 +411,7 @@ class Session:
 
 		# database persistence is secondary, don't update it too often
 		updated_in_db = False
-		if (
-			force or (time_diff is None) or (time_diff > threshold) or self._update_in_cache
-		) and not frappe.flags.read_only:
+		if (force or (time_diff is None) or (time_diff > threshold)) and not frappe.flags.read_only:
 			self.data.data.last_updated = now
 			self.data.data.lang = str(frappe.lang)
 			# update sessions table
