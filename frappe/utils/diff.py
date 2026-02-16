d804c411cd3565002e34b47aaeab580663b70eaa
AarDG10 <aarol.dsouza@gmail.com>
2026-02-16 19:52:48 +0530

    fix(diff): add type hints to whitelisted methods

--- frappe/utils/diff.py
+++ frappe/utils/diff.py
@@ -44,7 +44,7 @@ def _get_value_from_version(version_name: int | str, fieldname: str):
 
 @frappe.whitelist()
 @frappe.validate_and_sanitize_search_inputs
-def version_query(doctype, txt, searchfield, start, page_len, filters):
+def version_query(doctype: str, txt: str, searchfield: str, start: int, page_len: int, filters: dict):
 	version_filters = {
 		"docname": filters["docname"],
 		"ref_doctype": filters["ref_doctype"],
