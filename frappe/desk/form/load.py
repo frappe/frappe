e1031ac0e2aa869970dbfc934ccfe3a858f933d2
AarDG10 <aarol.dsouza@gmail.com>
2026-02-18 19:22:01 +0530

    fix(load): add type hints to whitelisted methods

--- frappe/desk/form/load.py
+++ frappe/desk/form/load.py
@@ -21,7 +21,7 @@ if typing.TYPE_CHECKING:
 
 
 @frappe.whitelist()
-def getdoc(doctype, name):
+def getdoc(doctype: str, name: str | int):
 	"""
 	Loads a doclist for a given document. This method is called directly from the client.
 	Requires "doctype", "name" as form variables.
@@ -60,7 +60,7 @@ def getdoc(doctype, name):
 
 
 @frappe.whitelist()
-def getdoctype(doctype, with_parent=False):
+def getdoctype(doctype: str, with_parent: int | bool = False):
 	"""load doctype"""
 
 	docs = []
@@ -90,7 +90,7 @@ def get_meta_bundle(doctype):
 
 
 @frappe.whitelist()
-def get_docinfo(doc=None, doctype=None, name=None):
+def get_docinfo(doc: Document | None = None, doctype: str | None = None, name: str | int | None = None):
 	from frappe.share import _get_users as get_docshares
 
 	if not doc:
@@ -200,7 +200,7 @@ def get_versions(doc: "Document") -> list[dict]:
 
 
 @frappe.whitelist()
-def get_communications(doctype, name, start=0, limit=20):
+def get_communications(doctype: str, name: str | int, start: str | int = 0, limit: str | int = 20):
 	from frappe.utils import cint
 
 	frappe.get_lazy_doc(doctype, name).check_permission()
@@ -500,7 +500,7 @@ def update_user_info(docinfo, doc=None):
 
 
 @frappe.whitelist()
-def get_user_info_for_viewers(users):
+def get_user_info_for_viewers(users: str):
 	user_info = {}
 	for user in json.loads(users):
 		frappe.utils.add_user_info(user, user_info)
