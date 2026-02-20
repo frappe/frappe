fdfaff49c4dc82154012dca467d95876a7a6ebab
Sayantan Ghosh <stravoone@gmail.com>
2026-02-20 10:53:49 +0530

    fix: enable/disable buttons in roles editor based on state (#37212)
    fixes #31662
    
    fixed select_all logic where disabled checkboxes did not trigger clicks and stayed in the opposite checked state.
    
    although now it is not possible to trigger select_all if RolesEditor is disabled as the buttons to make that function call are also diabled, but this logic acts as a failsafe.
    
    (cherry picked from commit 66877405e19e97f3fd864544525874e51b52c650)
    
    # Conflicts:
    #	frappe/core/doctype/user/user.js

--- frappe/core/doctype/user/user.js
+++ frappe/core/doctype/user/user.js
@@ -435,14 +435,24 @@ frappe.ui.form.on("User Role Profile", {
 			frm.call("populate_role_profile_roles").then(() => {
 				frm.roles_editor.show();
 			});
+<<<<<<< HEAD
 			$(".deselect-all, .select-all").prop("disabled", true);
+=======
+>>>>>>> 66877405e1 (fix: enable/disable buttons in roles editor based on state (#37212))
 		}
 	},
 	role_profiles_remove: function (frm) {
 		if (frm.doc.role_profiles.length == 0) {
+<<<<<<< HEAD
 			frm.roles_editor.disable = 0;
 			frm.roles_editor.show();
 			$(".deselect-all, .select-all").prop("disabled", false);
+=======
+			if (frm.roles_editor) {
+				frm.roles_editor.disable = 0;
+				frm.roles_editor.show();
+			}
+>>>>>>> 66877405e1 (fix: enable/disable buttons in roles editor based on state (#37212))
 		}
 	},
 });
