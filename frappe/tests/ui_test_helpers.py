31a7772427414eb6e592f249ca6eb71b614deb13
Aarol D'Souza <98270103+AarDG10@users.noreply.github.com>
2026-02-20 17:41:16 +0530

    Merge pull request #37318 from AarDG10/fix-types
    fix(tests): add type hints to whitelisted test methods
    
    (cherry picked from commit cbe479412c4c9ecfe624d3276761b0421b9e4ad9)
    
    # Conflicts:
    #	frappe/tests/ui_test_helpers.py

--- frappe/tests/ui_test_helpers.py
+++ frappe/tests/ui_test_helpers.py
@@ -632,6 +632,7 @@ def add_remove_role(action, user, role):
 
 @whitelist_for_tests
 def publish_realtime(
+<<<<<<< HEAD
 	event=None,
 	message=None,
 	room=None,
@@ -639,6 +640,15 @@ def publish_realtime(
 	doctype=None,
 	docname=None,
 	task_id=None,
+=======
+	event: str | None = None,
+	message: str | dict | None = None,
+	room: str | None = None,
+	user: str | None = None,
+	doctype: str | None = None,
+	docname: str | None = None,
+	task_id: str | None = None,
+>>>>>>> cbe479412c (Merge pull request #37318 from AarDG10/fix-types)
 ):
 	frappe.publish_realtime(
 		event=event,
