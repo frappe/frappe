d37f0584649221b138a2d7edf4b2532903cede57
AarDG10 <aarol.dsouza@gmail.com>
2026-02-24 17:45:57 +0530

    test: adapt tests to new behavior

--- frappe/core/doctype/version/test_version.py
+++ frappe/core/doctype/version/test_version.py
@@ -74,65 +74,73 @@ class TestHTMLDiff(UnitTestCase):
 class TestVersion(IntegrationTestCase):
 	def test_onload_generates_html_diffs_for_multiline(self):
 		"""Test onload generates HTML diffs for multiline changes."""
-		version = frappe.get_doc(
-			doctype="Version",
-			ref_doctype="ToDo",
-			docname="test-doc",
-			data=frappe.as_json({"changed": [["description", "line1\nline2", "line1\nmodified"]]}),
-		)
-
-		version.onload()
-
-		html_diffs = version.get_onload().get("html_diffs")
-		self.assertIsNotNone(html_diffs)
-		self.assertIn("description", html_diffs)
-		self.assertIn("<table", html_diffs["description"])
+		todo = frappe.get_doc(doctype="ToDo", description="original line").insert()
+		try:
+			version = frappe.get_doc(
+				doctype="Version",
+				ref_doctype="ToDo",
+				docname=todo.name,
+				data=frappe.as_json({"changed": [["description", "line1\nline2", "line1\nmodified"]]}),
+			)
+			version.onload()
+			html_diffs = version.get_onload().get("html_diffs")
+			self.assertIsNotNone(html_diffs)
+			self.assertIn("description", html_diffs)
+			self.assertIn("<table", html_diffs["description"])
+		finally:
+			todo.delete()
 
 	def test_onload_generates_html_diffs_for_long_text(self):
 		"""Test onload generates HTML diffs for text > 80 characters."""
-		version = frappe.get_doc(
-			doctype="Version",
-			ref_doctype="ToDo",
-			docname="test-doc",
-			data=frappe.as_json({"changed": [["notes", "x" * 81, "y" * 81]]}),
-		)
-
-		version.onload()
-
-		html_diffs = version.get_onload().get("html_diffs")
-		self.assertIsNotNone(html_diffs)
-		self.assertIn("notes", html_diffs)
+		todo = frappe.get_doc(doctype="ToDo", description="original line").insert()
+		try:
+			version = frappe.get_doc(
+				doctype="Version",
+				ref_doctype="ToDo",
+				docname=todo.name,
+				data=frappe.as_json({"changed": [["description", "x" * 81, "y" * 81]]}),
+			)
+			version.onload()
+			html_diffs = version.get_onload().get("html_diffs")
+			self.assertIsNotNone(html_diffs)
+			self.assertIn("description", html_diffs)
+		finally:
+			todo.delete()
 
 	def test_onload_no_html_diffs_for_simple_changes(self):
 		"""Test onload doesn't generate HTML diffs for simple short changes."""
-		version = frappe.get_doc(
-			doctype="Version",
-			ref_doctype="ToDo",
-			docname="test-doc",
-			data=frappe.as_json({"changed": [["status", "Open", "Closed"]]}),
-		)
-
-		version.onload()
-
-		html_diffs = version.get_onload().get("html_diffs")
-		self.assertIsNone(html_diffs)
+		todo = frappe.get_doc(doctype="ToDo", description="original line").insert()
+		try:
+			version = frappe.get_doc(
+				doctype="Version",
+				ref_doctype="ToDo",
+				docname=todo.name,
+				data=frappe.as_json({"changed": [["status", "Open", "Closed"]]}),
+			)
+			version.onload()
+			html_diffs = version.get_onload().get("html_diffs")
+			self.assertIsNone(html_diffs)
+		finally:
+			todo.delete()
 
 	def test_onload_handles_empty_data(self):
 		"""Test onload handles empty or missing data gracefully."""
-		version = frappe.get_doc(
-			doctype="Version",
-			ref_doctype="ToDo",
-			docname="test-doc",
-			data=None,
-		)
-
-		# Should not raise an error
-		version.onload()
-		self.assertIsNone(version.get_onload().get("html_diffs"))
-
-		version.data = frappe.as_json({"changed": []})
-		version.onload()
-		self.assertIsNone(version.get_onload().get("html_diffs"))
+		todo = frappe.get_doc(doctype="ToDo", description="original line").insert()
+		try:
+			version = frappe.get_doc(
+				doctype="Version",
+				ref_doctype="ToDo",
+				docname=todo.name,
+				data=None,
+			)
+			# Should not raise an error
+			version.onload()
+			self.assertIsNone(version.get_onload().get("html_diffs"))
+			version.data = frappe.as_json({"changed": []})
+			version.onload()
+			self.assertIsNone(version.get_onload().get("html_diffs"))
+		finally:
+			todo.delete()
 
 	def test_get_diff(self):
 		frappe.set_user("Administrator")
