ba9ac9ba3c84e84e0e7e12f3e7dcad0b0b476415
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2026-02-25 14:29:23 +0530

    fix: make percent precision dynamic

--- frappe/public/js/frappe/form/controls/float.js
+++ frappe/public/js/frappe/form/controls/float.js
@@ -29,4 +29,16 @@ frappe.ui.form.ControlFloat = class ControlFloat extends frappe.ui.form.ControlI
 	}
 };
 
-frappe.ui.form.ControlPercent = frappe.ui.form.ControlFloat;
+frappe.ui.form.ControlPercent = class ControlPercent extends frappe.ui.form.ControlFloat {
+	format_for_input(value) {
+		if (value === null || value === undefined || isNaN(Number(value))) {
+			return "";
+		}
+		const precision = value.toString().split(".")[1]?.length || 0;
+		return format_number(
+			value,
+			this.get_number_format(),
+			Math.min(this.get_precision(), precision)
+		);
+	}
+};
