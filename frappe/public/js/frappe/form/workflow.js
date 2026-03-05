704f314f3b0644cc84177020e18e0de9768fa582
pipech <poranut@spacecode.co.th>
2026-03-05 11:03:07 +0700

    Update frappe/public/js/frappe/form/workflow.js
    Co-authored-by: Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>

--- frappe/public/js/frappe/form/workflow.js
+++ frappe/public/js/frappe/form/workflow.js
@@ -114,7 +114,14 @@ frappe.ui.form.States = class FormStates {
 									);
 								} else {
 									me.handle_workflow_action(d);
-								}
+						if (frappe.workflow?.workflows?.[me.frm.doctype]?.enable_action_confirmation) {
+							frappe.confirm(
+								__("Are you sure you want to {0}?", [d.action]),
+								() => me.handle_workflow_action(d)
+							);
+						} else {
+							me.handle_workflow_action(d);
+						}
 							});
 					});
 				}
