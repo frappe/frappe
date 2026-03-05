70bb88087b1c5f6232b0253db4527a5cc2a16a22
pipech <poranut@spacecode.co.th>
2026-03-05 11:15:18 +0700

    style(workflow): fix lint

--- frappe/public/js/frappe/form/workflow.js
+++ frappe/public/js/frappe/form/workflow.js
@@ -102,10 +102,12 @@ frappe.ui.form.States = class FormStates {
 				if (frappe.user_roles.includes(d.allowed) && has_approval_access(d)) {
 					added = true;
 					me.frm.page.add_action_item(__(d.action), function () {
-						if (frappe.workflow?.workflows?.[me.frm.doctype]?.enable_action_confirmation) {
-							frappe.confirm(
-								__("Are you sure you want to {0}?", [d.action]),
-								() => me.handle_workflow_action(d)
+						if (
+							frappe.workflow?.workflows?.[me.frm.doctype]
+								?.enable_action_confirmation
+						) {
+							frappe.confirm(__("Are you sure you want to {0}?", [d.action]), () =>
+								me.handle_workflow_action(d)
 							);
 						} else {
 							me.handle_workflow_action(d);
