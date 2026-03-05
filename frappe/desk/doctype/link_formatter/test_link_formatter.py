f50b01eb345bce3a77924c43518b7401f952a2d6
Thomas Antony <thomasantony12042001@gmail.com>
2026-03-05 19:01:23 +0530

    chore: Integration Test Case add

--- frappe/desk/doctype/link_formatter/test_link_formatter.py
+++ frappe/desk/doctype/link_formatter/test_link_formatter.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2026, Frappe Technologies and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests import IntegrationTestCase
 
 # On IntegrationTestCase, the doctype test records and all
@@ -17,4 +17,16 @@ class IntegrationTestLinkFormatter(IntegrationTestCase):
 	Use this class for testing interactions between multiple components.
 	"""
 
-	pass
+
+def test_link_formatter_data_created(self):
+	"""Ensure migration created the required link formatter configuration"""
+
+	link_formatter = frappe.get_doc("Link Formatter", "Link Formatter")
+	self.assertTrue(
+		any(
+			row.doctype_name == "User"
+			and row.link_fieldname == "user"
+			and row.display_fieldname == "user_full_name"
+			for row in link_formatter.link_field_display
+		)
+	)
