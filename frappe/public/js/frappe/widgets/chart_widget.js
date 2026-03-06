0df063e08fc78cab133982022f955f8dd55e071f
Shllokkk <shllokosan23@gmail.com>
2026-03-07 01:07:11 +0530

    feat: show error state in dashboard chart widget instead of throwing errors

--- frappe/public/js/frappe/widgets/chart_widget.js
+++ frappe/public/js/frappe/widgets/chart_widget.js
@@ -60,6 +60,11 @@ export default class ChartWidget extends Widget {
 		);
 		this.empty.hide().appendTo(this.body);
 
+		this.error_state = $(
+			`<div class="chart-loading-state text-extra-muted" style="height: ${this.height}px;"></div>`
+		);
+		this.error_state.hide().appendTo(this.body);
+
 		this.chart_wrapper = $(`<div></div>`);
 		this.chart_wrapper.appendTo(this.body);
 
@@ -583,14 +588,23 @@ export default class ChartWidget extends Widget {
 			}
 		};
 
-		if (!this.data || !this.data.labels || !Object.keys(this.data).length) {
+		if (this.data && this.data.error_message) {
+			this.chart_wrapper.hide();
+			this.loading.hide();
+			this.$summary && this.$summary.hide();
+			this.empty.hide();
+			this.error_state.text(this.data.error_message);
+			this.error_state.show();
+		} else if (!this.data || !this.data.labels || !Object.keys(this.data).length) {
 			this.chart_wrapper.hide();
 			this.loading.hide();
 			this.$summary && this.$summary.hide();
 			this.empty.show();
+			this.error_state.hide();
 		} else {
 			this.loading.hide();
 			this.empty.hide();
+			this.error_state.hide();
 			this.chart_wrapper.show();
 			this.chart_doc.document_type = await this.get_source_doctype();
 
