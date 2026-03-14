c805c9fc9d12493db647cf9a69f154a7d88c4945
barredterra <14891507+barredterra@users.noreply.github.com>
2026-03-14 19:09:25 +0100

    fix: add symbol only if present and don't translate it
    shorten_number already returns translated symbol

--- frappe/public/js/frappe/widgets/number_card_widget.js
+++ frappe/public/js/frappe/widgets/number_card_widget.js
@@ -252,15 +252,14 @@ export default class NumberCardWidget extends Widget {
 		if (this.card_doc.currency) {
 			this.formatted_number =
 				format_currency(parseFloat(number_parts[0]), this.card_doc.currency) +
-				" " +
-				symbol;
+				(symbol ? " " + symbol : "");
 			return;
 		}
 
 		number_parts[0] = window.convert_old_to_new_number_format(number_parts[0]);
 		const formatted_number = frappe.format(number_parts[0], df, null, doc);
 		this.formatted_number =
-			($(formatted_number).text() || formatted_number) + " " + __(symbol);
+			($(formatted_number).text() || formatted_number) + (symbol ? " " + symbol : "");
 	}
 
 	_generate_common_doc(rows) {
