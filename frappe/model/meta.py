eaf8b0f41b53ba40e4dc848b593036ad67cd1002
gajjug004 <gajjug004@gmail.com>
2026-03-17 15:39:46 +0530

    fix: use parent doctype for DocShare check on child table field permissions
    Co-Authored-By: Akhil Narang <me@akhilnarang.dev>

--- frappe/model/meta.py
+++ frappe/model/meta.py
@@ -617,7 +617,8 @@ class Meta(Document):
 		)
 
 		if 0 not in permlevel_access and permission_type in ("read", "select"):
-			if frappe.share.get_shared(self.name, user, rights=[permission_type], limit=1):
+			check_doctype = parenttype if self.istable and parenttype else self.name
+			if frappe.share.get_shared(check_doctype, user, rights=[permission_type], limit=1):
 				permlevel_access.add(0)
 
 		permitted_fieldnames.extend(
