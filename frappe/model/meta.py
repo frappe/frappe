69df5a4ccc1831abfaf2b4cc85b5965fea7ad777
gajjug004 <gajjug004@gmail.com>
2026-03-17 18:39:26 +0530

    fix: check read right instead of permission_type

--- frappe/model/meta.py
+++ frappe/model/meta.py
@@ -618,7 +618,7 @@ class Meta(Document):
 
 		if 0 not in permlevel_access and permission_type in ("read", "select"):
 			check_doctype = parenttype if self.istable and parenttype else self.name
-			if frappe.share.get_shared(check_doctype, user, rights=[permission_type], limit=1):
+			if frappe.share.get_shared(check_doctype, user, rights=["read"], limit=1):
 				permlevel_access.add(0)
 
 		permitted_fieldnames.extend(
