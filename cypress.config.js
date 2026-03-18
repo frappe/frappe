8cd9daabbdbf444b31fb4b675c933d75622b5b49
sokumon <sohamkulkarns9@gmail.com>
2026-03-18 16:07:47 +0530

    fix: run ui tests parallely
    (cherry picked from commit 4c68fae18cb09a0ba4ac0238ef7bde61741c7b0b)
    
    # Conflicts:
    #	cypress.config.js
    #	yarn.lock

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require("cypress");
+<<<<<<< HEAD
+=======
+const fs = require("fs");
+const path = require("path");
+const cypressSplit = require("cypress-split");
+>>>>>>> 4c68fae18c (fix: run ui tests parallely)
 
 module.exports = defineConfig({
 	projectId: "92odwv",
@@ -18,6 +24,23 @@ module.exports = defineConfig({
 		// We've imported your old cypress plugins here.
 		// You may want to clean this up later by importing these.
 		setupNodeEvents(on, config) {
+<<<<<<< HEAD
+=======
+			// Delete videos for specs without failing or retried tests
+			// https://docs.cypress.io/guides/guides/screenshots-and-videos#Delete-videos-for-specs-without-failing-or-retried-tests
+			cypressSplit(on, config);
+			on("after:spec", (spec, results) => {
+				if (results && results.video) {
+					const failures = results.tests.some((test) =>
+						test.attempts.some((attempt) => attempt.state === "failed")
+					);
+					if (!failures) {
+						fs.unlinkSync(results.video);
+					}
+				}
+			});
+
+>>>>>>> 4c68fae18c (fix: run ui tests parallely)
 			return require("./cypress/plugins/index.js")(on, config);
 		},
 		testIsolation: false,
