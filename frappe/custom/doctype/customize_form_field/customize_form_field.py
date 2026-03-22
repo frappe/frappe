63c26d9c3cf76e9fb9b3b4803e28ca717a77223f
UmakanthKaspa <kaspaumakanth1999@gmail.com>
2026-03-22 10:48:03 +0530

    fix: add set_only_once property to custom field and customize form
    (cherry picked from commit 22a8d8dd664e94c6980902af4f1f381959eaf582)

--- frappe/custom/doctype/customize_form_field/customize_form_field.py
+++ frappe/custom/doctype/customize_form_field/customize_form_field.py
@@ -13,10 +13,10 @@ class CustomizeFormField(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		alignment: DF.Literal["", "Left", "Center", "Right"]
 		allow_bulk_edit: DF.Check
 		allow_in_quick_entry: DF.Check
 		allow_on_submit: DF.Check
-		alignment: DF.Literal["", "Left", "Center", "Right"]
 		bold: DF.Check
 		button_color: DF.Literal["", "Default", "Primary", "Info", "Success", "Warning", "Danger"]
 		collapsible: DF.Check
@@ -109,6 +109,7 @@ class CustomizeFormField(Document):
 		remember_last_selected_value: DF.Check
 		report_hide: DF.Check
 		reqd: DF.Check
+		set_only_once: DF.Check
 		show_dashboard: DF.Check
 		sort_options: DF.Check
 		translatable: DF.Check
