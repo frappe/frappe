93846eb36519e5a82f8adde2d0eaa153d968c637
Ejaaz Khan <67804911+iamejaaz@users.noreply.github.com>
2026-03-24 10:16:33 +0530

    Merge pull request #38183 from Shllokkk/print-format-fix
    fix: remove links from filters in report print pdf
    (cherry picked from commit 11768a600e0f3ea1fa94c1548fc908e17637dd4e)

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -1735,7 +1735,7 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 				if (docfield.fieldtype === "Check") {
 					display_value = this.boolean_labels[cint(value)];
 				} else {
-					display_value = frappe.format(value, docfield);
+					display_value = frappe.format(value, docfield, { for_print: true });
 				}
 
 				return `<div class="filter-row">
