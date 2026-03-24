32bd3430240a04f0c02a0a5faa60bdf935a6b010
Rucha Mahabal <ruchamahabal2@gmail.com>
2026-03-24 18:19:17 +0530

    chore: fix conflicts

--- frappe/model/meta.py
+++ frappe/model/meta.py
@@ -583,16 +583,9 @@ class Meta(Document):
 	def get_high_permlevel_fields(self):
 		"""Build list of fields with high perm level and all the higher perm levels defined."""
 		if not hasattr(self, "high_permlevel_fields"):
-			self.high_permlevel_fields = [df for df in self.fields if df.permlevel > 0]
+			self.high_permlevel_fields = [df for df in self.fields if (df.permlevel or 0) > 0]
 		return self.high_permlevel_fields
 
-<<<<<<< HEAD
-=======
-	@cached_property
-	def high_permlevel_fields(self):
-		return [df for df in self.fields if (df.permlevel or 0) > 0]
-
->>>>>>> 7e6c9def71 (fix: `get_list` calls fail sometimes due to permlevel TypeError (#38214))
 	def get_permitted_fieldnames(
 		self,
 		parenttype=None,
