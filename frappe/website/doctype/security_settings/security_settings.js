a69bdfd57fa3320c1de55b960f47309f7ad968f8
Sabu Siyad <hello@ssiyad.com>
2026-04-12 12:56:22 +0530

    feat(security-settings): banner/alert on security.txt with link to RFC

--- frappe/website/doctype/security_settings/security_settings.js
+++ frappe/website/doctype/security_settings/security_settings.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2026, Frappe Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Security Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Security Settings", {
+	refresh(frm) {
+		$(frm.fields_dict.securitytxt_section.wrapper)
+			.find(".section-body")
+			.prepend(
+				`<div class="alert alert-warning border d-flex justify-content-between align-items-center" style="flex: 0 0 100%; max-width: 100%; border-color: var(--border-color);">
+					<span>${__("Security.txt will be served only under HTTPS.")}</span>
+					<a href="https://tools.ietf.org/html/rfc9116#section-6.7" target="_blank" class="btn btn-xs btn-secondary">${__(
+						"Learn more"
+					)}</a>
+				</div>`
+			);
+	},
+});
