f79b76f3e5ad6e9bd7d829be30fcc7b830ea5ca0
Rucha Mahabal <ruchamahabal2@gmail.com>
2026-04-13 11:06:41 +0530

    revert: "feat: add option to skip running `after_build` hooks"
    This reverts commit 6e9d2c6a2333d487fcf4d1908c366b496a8d80b1.
    Removing the flag for now as other hooks (like after/before migrate) don't have a skip option either

--- frappe/commands/utils.py
+++ frappe/commands/utils.py
@@ -48,7 +48,6 @@ if typing.TYPE_CHECKING:
 	envvar="USING_CACHED",
 	help="Skips build and uses cached build artifacts (cache is set by Bench). Ignored if developer_mode enabled.",
 )
-@click.option("--skip-after-build", is_flag=True, help="Skip running after build hooks")
 def build(
 	app=None,
 	apps=None,
@@ -58,7 +57,6 @@ def build(
 	force=False,
 	save_metafiles=False,
 	using_cached=False,
-	skip_after_build=False,
 ):
 	"Compile JS and CSS source files"
 	from frappe.build import bundle, download_frappe_assets
@@ -110,8 +108,7 @@ def build(
 			print("Compiling translations for", app)
 			compile_translations(app, force=force)
 
-		if not skip_after_build:
-			run_after_build_hook(apps)
+		run_after_build_hook(apps)
 
 
 def run_after_build_hook(apps):
