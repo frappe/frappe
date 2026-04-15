795db2b34faea0c7c17780b1a0f947566d982f9f
Sabu Siyad <hello@ssiyad.com>
2026-04-15 15:25:54 +0530

    chore: fix conflicts

--- frappe/app.py
+++ frappe/app.py
@@ -125,18 +125,12 @@ def application(request: Request):
 		elif request.path.startswith("/private/files/"):
 			response = frappe.utils.response.download_private_file(request.path)
 
-<<<<<<< HEAD
-=======
 		elif request.path == "/.well-known/security.txt" and request.method == "GET":
 			if request.scheme != "https":
 				raise NotFound
 			security_settings = frappe.get_doc("Security Settings")
 			response = Response(security_settings.security_txt, content_type="text/plain")
 
-		elif request.path.startswith("/.well-known/") and request.method == "GET":
-			response = handle_wellknown(request.path)
-
->>>>>>> ec9a60172f (feat: `security.txt` (#38530))
 		elif request.method in ("GET", "HEAD", "POST"):
 			response = get_response()
 
