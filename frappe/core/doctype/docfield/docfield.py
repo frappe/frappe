d5da7038ab3f5361c29b96fe7524ab4f85b26407
Sumit Jain <jainmit23@gmail.com>
2026-04-24 13:34:42 +0530

    refactor: Rename 'include_in_import_template' to 'in_import_template'
    (cherry picked from commit 78b81fc7cf34a2bae8a105495c26643a099e7ffb)

--- frappe/core/doctype/docfield/docfield.py
+++ frappe/core/doctype/docfield/docfield.py
@@ -83,10 +83,10 @@ class DocField(Document):
 		ignore_xss_filter: DF.Check
 		in_filter: DF.Check
 		in_global_search: DF.Check
+		in_import_template: DF.Check
 		in_list_view: DF.Check
 		in_preview: DF.Check
 		in_standard_filter: DF.Check
-		include_in_import_template: DF.Check
 		is_virtual: DF.Check
 		label: DF.Data | None
 		length: DF.Int
