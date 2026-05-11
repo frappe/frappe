74c16c3d4571c72535818147213cbce39956086b
sokumon <sohamkulkarns9@gmail.com>
2026-05-11 19:10:20 +0530

    fix: delete only orphan workspaces
    (cherry picked from commit 06abc317ad1afede0773201eefda894796274e3d)

--- frappe/patches/v16_0/auto_generate_desktop_icon_and_sidebar.py
+++ frappe/patches/v16_0/auto_generate_desktop_icon_and_sidebar.py
@@ -4,5 +4,5 @@ from frappe.utils.install import auto_generate_icons_and_sidebar
 
 def execute():
 	"""Auto Create desktop icons and workspace sidebars."""
-	remove_orphan_entities()
+	remove_orphan_entities("Workspace")
 	auto_generate_icons_and_sidebar()
