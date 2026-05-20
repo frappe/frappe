ad5898cd136d7df0573a94dca291bf7bb02cf4eb
gajjug004 <gajjug004@gmail.com>
2026-05-20 18:36:41 +0530

    refactor: replace list comprehension with set comprehension
    (cherry picked from commit 524d4895f87ee3ccc0280c1911513240dfd81c53)

--- frappe/desk/doctype/number_card/number_card.py
+++ frappe/desk/doctype/number_card/number_card.py
@@ -222,7 +222,7 @@ def get_cards_for_user(doctype, txt, searchfield, start, page_len, filters):
 		validate_filters=True,
 	)
 
-	allowed_modules = [module.get("module_name") for module in get_modules_from_all_apps_for_user()]
+	allowed_modules = {module.get("module_name") for module in get_modules_from_all_apps_for_user()}
 
 	return (
 		condition_query.select(numberCard.name, numberCard.label, numberCard.document_type)
