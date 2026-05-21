64cec91c7278eec4694ee0cf9b03c4d69a06c085
AarDG10 <aarol.dsouza@gmail.com>
2026-05-21 11:34:20 +0530

    feat: add whitelist for guest allowed uploads
    Introducing a whitelist for guest uploads, this gives more control to Sys. Man. on which doctypes guests are allowed to upload to.
    
    (cherry picked from commit 1883475dee210cb1260bde2d77c074afb4b40309)
    
    # Conflicts:
    #	frappe/core/doctype/system_settings/system_settings.json
    #	frappe/core/doctype/system_settings/system_settings.py

--- frappe/core/doctype/system_settings/system_settings.py
+++ frappe/core/doctype/system_settings/system_settings.py
@@ -24,7 +24,11 @@ class SystemSettings(Document):
 		allow_login_after_fail: DF.Int
 		allow_login_using_mobile_number: DF.Check
 		allow_login_using_user_name: DF.Check
+<<<<<<< HEAD
 		allow_older_web_view_links: DF.Check
+=======
+		allowed_doctypes_for_guest_uploads: DF.SmallText | None
+>>>>>>> 1883475dee (feat: add whitelist for guest allowed uploads)
 		allowed_file_extensions: DF.SmallText | None
 		app_name: DF.Data | None
 		apply_strict_user_permissions: DF.Check
