cd3cb089de20286a9aabd70c9a0d0e59ef20accb
Ankush Menat <ankush@frappe.io>
2026-05-21 11:57:32 +0530

    perf: lazy-load leaflet bundle
    Leaflet is only used by Geolocation fields and the Map view, so loading
    it eagerly in libs/desk bundles bloats every page. Move it into its own
    bundle and require it on demand, mirroring the scanner lib pattern.

--- frappe/public/js/libs.bundle.js
+++ frappe/public/js/libs.bundle.js
@@ -1,9 +1,5 @@
 import "./jquery-bootstrap";
 import "./lib/moment";
-import "../js/lib/leaflet/leaflet.js";
-import "../js/lib/leaflet_easy_button/easy-button.js";
-import "../js/lib/leaflet_draw/leaflet.draw.js";
-import "../js/lib/leaflet_control_locate/L.Control.Locate.js";
 import Sortable from "sortablejs";
 import { gemoji } from "gemoji";
 
