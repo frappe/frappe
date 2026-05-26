f7f030feb56dccec3bf780ea55de780bfcda385e
gajjug004 <gajjug004@gmail.com>
2026-05-26 19:37:12 +0530

    fix: keep selection banner count fresh

--- frappe/public/js/frappe/form/grid.js
+++ frappe/public/js/frappe/form/grid.js
@@ -218,20 +218,24 @@ export default class Grid {
 				this.last_checked_docname = docname;
 			}
 			this.refresh_remove_rows_button();
-			this.update_selection_banner(this.get_selected().length);
+			this.update_selection_banner();
 		});
 	}
 
-	update_selection_banner(count) {
+	update_selection_banner() {
+		const num_selected_rows = this.get_selected_children().length;
+
 		let $container = this.wrapper.find(".form-grid-container");
 		let $toast = this.wrapper.find("> .grid-selection-toast");
-		if (count > 0) {
+		if (num_selected_rows > 0) {
 			if (!$toast.length) {
 				$toast = $(
 					`<div class="grid-selection-toast"><span class="grid-selection-toast__message"></span></div>`
 				).insertAfter($container);
 			}
-			$toast.find(".grid-selection-toast__message").text(__("{0} rows selected", [count]));
+			$toast
+				.find(".grid-selection-toast__message")
+				.text(__("{0} row(s) selected", [num_selected_rows]));
 			$toast.show();
 		} else if ($toast.length) {
 			$toast.hide();
@@ -479,6 +483,7 @@ export default class Grid {
 		this.refresh_remove_rows_button();
 
 		this.wrapper.trigger("change");
+		this.update_selection_banner();
 	}
 
 	render_result_rows($rows) {
