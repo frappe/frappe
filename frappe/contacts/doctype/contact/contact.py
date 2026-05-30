ea20f3abef8a161652fe1c613a523e290214a64b
diptanilsaha <diptanil@frappe.io>
2026-05-30 17:32:02 +0530

    fix(contact): key_error while popping link_doctype and link_name in contact_query

--- frappe/contacts/doctype/contact/contact.py
+++ frappe/contacts/doctype/contact/contact.py
@@ -352,8 +352,8 @@ def contact_query(
 	if not frappe.get_meta(doctype).get_field(searchfield) and searchfield not in frappe.db.DEFAULT_COLUMNS:
 		return []
 
-	link_doctype = filters.pop("link_doctype")
-	link_name = filters.pop("link_name")
+	link_doctype = filters.pop("link_doctype", None)
+	link_name = filters.pop("link_name", None)
 
 	return frappe.db.sql(
 		f"""select
