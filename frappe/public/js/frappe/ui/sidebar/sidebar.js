2127bb2584c8930bf3bd8d3a86673c94bbe24c38
Ejaaz Khan <67804911+iamejaaz@users.noreply.github.com>
2026-06-02 12:57:55 +0530

    Merge pull request #39687 from iamejaaz/fix/notification-panel-icons-and-text
    fix(notifications): use lucide icons and fix empty state capitalisation
    
    (cherry picked from commit 4b9933232f8fda6a34cc2474b44d79c5ae7c1d5d)

--- frappe/public/js/frappe/ui/sidebar/sidebar.js
+++ frappe/public/js/frappe/ui/sidebar/sidebar.js
@@ -445,14 +445,6 @@ frappe.ui.Sidebar = class Sidebar {
 						frappe.ui.toolbar.toggle_full_width();
 					},
 				},
-				{
-					name: "Notifications Settings",
-					label: __("Notifications Settings"),
-					icon: "bell",
-					onClick: function () {
-						frappe.set_route("Form", "Notification Settings", frappe.session.user);
-					},
-				},
 				{ is_divider: true },
 				{
 					name: "logout",
