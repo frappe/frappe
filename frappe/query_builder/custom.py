80a48b7711547c7def0df967b8a77129bef79029
Daniel Radl <daniel.radl@amr.at>
2026-06-03 09:20:01 +0000

    feat: support YEAR query function
    (cherry picked from commit 3e8e719afb0826c2d8a6888dfaf44d7d5bbbcf9c)
    
    # Conflicts:
    #	frappe/query_builder/custom.py

--- frappe/query_builder/custom.py
+++ frappe/query_builder/custom.py
@@ -134,4 +134,17 @@ class Quarter(Function):
 
 class Month(Function):
 	def __init__(self, field, alias=None):
+<<<<<<< HEAD
 		super().__init__("MONTH", field, alias=alias)
+=======
+		self._postgres = _is_postgres()
+		if self._postgres:
+			super().__init__("date_part", "month", field, alias=alias)
+		else:
+			super().__init__("MONTH", field, alias=alias)
+
+
+class Year(Function):
+	def __init__(self, field, alias=None):
+		super().__init__("YEAR", field, alias=alias)
+>>>>>>> 3e8e719afb (feat: support YEAR query function)
