d4026ffaaadcf7a0e946aabcc8eca4a8284b2d64
ruthra kumar <ruthra@erpnext.com>
2026-06-04 17:04:24 +0530

    refactor: separate file for each doctype
    (cherry picked from commit b16b5abda8029c9b6f27ae948ee00d30bb2eab34)

--- frappe/core/doctype/duckdb_sync_log/duckdb_sync_log.py
+++ frappe/core/doctype/duckdb_sync_log/duckdb_sync_log.py
@@ -3,6 +3,8 @@
 
 import frappe
 from frappe import qb
+from frappe.database.duckdb.database import get_duckdb
+from frappe.database.duckdb.schema import DuckDBTable
 from frappe.model.document import Document
 
 
@@ -19,12 +21,16 @@ class DuckDBSyncLog(Document):
 		amended_from: DF.Link | None
 		db_tables: DF.Table[DuckDBSyncLogItem]
 		doc_type: DF.Link | None
+		filename: DF.Data | None
 	# end: auto-generated types
 
 	_DOCTYPE_NAME = "DuckDB Sync Log"
 
 	def before_save(self):
-		self.append("db_tables", {"table": f"tab{self.doc_type}"})
+		filename = self.doc_type.lower().replace(" ", "_")
+		self.filename = f"{frappe.conf.db_name}_{filename}.duckdb"
+
+		self.append("db_tables", {"table": self.doc_type})
 		df = qb.DocType("DocField")
 		if (
 			res := qb.from_(df)
@@ -33,4 +39,28 @@ class DuckDBSyncLog(Document):
 			.run()
 		):
 			for tb in res:
-				self.append("db_tables", {"table": f"tab{tb[0]}"})
+				self.append("db_tables", {"table": tb[0]})
+
+	def on_submit(self):
+		self.sync()
+
+	def get_duckdb_conn(self):
+		return get_duckdb(False, frappe.conf.db_name, self.filename)
+
+	def sync(self):
+		duck_conn = self.get_duckdb_conn()
+		# create non-existent tables
+		existing = set([x[0] for x in duck_conn.sql("show tables").fetchall()])
+
+		for x in self.db_tables:
+			ddbt = DuckDBTable(x.table)
+			if ddbt.table_name not in existing:
+				ddbt.sync(duck_conn)
+		duck_conn.close()
+		# frappe.enqueue(
+		# 	method="frappe.database.duckdb.database.sync_data",
+		# 	timeout="300",
+		# 	is_async=True,
+		# 	enqueue_after_commit=True,
+		# )
+		# frappe.toast("Data sync started")
