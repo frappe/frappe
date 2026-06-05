95fe9b691b13016940770ed8e2085993290e37cf
Ejaaz Khan <ejaaz@frappe.io>
2026-06-05 12:42:19 +0530

    fix(search): show full pill button on all desktop pages, use unicode shortcut

--- frappe/public/js/frappe/ui/toolbar/awesome_bar.js
+++ frappe/public/js/frappe/ui/toolbar/awesome_bar.js
@@ -467,19 +467,13 @@ frappe.search.AwesomeBar = class AwesomeBar {
 	setup_correct_button(wrapper) {
 		let small_button = $(wrapper).find("#small-search-button");
 		let full_button = $(wrapper).find("#full-search-button");
-		let route = frappe.get_route();
 		if (frappe.is_mobile()) {
 			small_button.removeClass("hidden");
 			full_button.addClass("hidden");
 			return;
 		}
-		if (route[0] == "Workspaces" || frappe.is_mobile()) {
-			small_button.addClass("hidden");
-			full_button.removeClass("hidden");
-		} else {
-			full_button.addClass("hidden");
-			small_button.removeClass("hidden");
-		}
+		small_button.addClass("hidden");
+		full_button.removeClass("hidden");
 	}
 	setup_page_change_event() {
 		const me = this;
