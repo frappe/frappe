5c1e1eee2ac4f0cc0952ce9da21a71d7cd840f4c
Ankush Menat <ankush@frappe.io>
2026-06-05 17:03:46 +0530

    fix: block QB writes in render
    (cherry picked from commit 4656f6827054f7517f55874cc264e6a3b3a0f673)

--- frappe/tests/test_safe_exec.py
+++ frappe/tests/test_safe_exec.py
@@ -2,7 +2,7 @@ import types
 
 import frappe
 from frappe.tests import IntegrationTestCase
-from frappe.utils.jinja import get_jenv
+from frappe.utils.jinja import get_jenv, render_template
 from frappe.utils.safe_exec import ServerScriptNotEnabled, get_safe_globals, safe_exec
 
 
@@ -74,6 +74,16 @@ class TestSafeExec(IntegrationTestCase):
 
 	def test_safe_query_builder(self):
 		self.assertRaises(frappe.PermissionError, safe_exec, """frappe.qb.from_("User").delete().run()""")
+
+	def test_safe_query_builder_in_render(self):
+		self.assertRaises(
+			frappe.PermissionError,
+			render_template,
+			""" {{ frappe.qb.from_("User").delete().run() }} """,
+		)
+
+		# Allowed read query
+		frappe.render_template(""" {{ frappe.qb.from_("User").select("*").run() }} """)
 
 	def test_call(self):
 		# call non whitelisted method
