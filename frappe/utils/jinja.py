74aac0197ac638c58136ad9aeb2108040325f0bb
AarDG10 <aarol.dsouza@gmail.com>
2026-06-05 12:11:10 +0530

    ci: test w/o exec. globals
    (cherry picked from commit 0c7c8eb1e5a9c1823fa0a91ac64f99ba68f102b1)

--- frappe/utils/jinja.py
+++ frappe/utils/jinja.py
@@ -41,10 +41,9 @@ def get_jenv(restrict_globals=None):
 	else:
 		jenv.globals.update(get_safe_globals())
 
-	if not restrict_globals:
-		methods, filters = get_jinja_hooks()
-		jenv.globals.update(methods or {})
-		jenv.filters.update(filters or {})
+	methods, filters = get_jinja_hooks()
+	jenv.globals.update(methods or {})
+	jenv.filters.update(filters or {})
 
 	setattr(frappe.local, local_key, jenv)
 >>>>>>> b85bc79439 (fix: decide globals in get_jenv)
