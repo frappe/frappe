2b276b2e4cd741a794f33a7d856f2bd46ded9c4e
ruthra kumar <ruthra@erpnext.com>
2026-06-06 13:25:57 +0530

    refactor: rename duckdb sync log to duckdb sync

--- frappe/core/doctype/duckdb_sync/duckdb_sync.js
+++ frappe/core/doctype/duckdb_sync/duckdb_sync.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2026, Frappe Technologies and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("DuckDB Sync Log", {
+frappe.ui.form.on("DuckDB Sync", {
 	sync_data: function (frm) {
 		frm.call({
 			doc: frm.doc,
