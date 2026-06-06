6969027022bcba98e4622acf387aab9bc105537a
ruthra kumar <ruthra@erpnext.com>
2026-06-06 13:25:57 +0530

    refactor: rename duckdb sync log to duckdb sync
    (cherry picked from commit 2b276b2e4cd741a794f33a7d856f2bd46ded9c4e)

--- frappe/core/doctype/duckdb_sync/test_duckdb_sync.py
+++ frappe/core/doctype/duckdb_sync/test_duckdb_sync.py
@@ -11,9 +11,9 @@ EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 
 
-class IntegrationTestDuckDBSyncLog(IntegrationTestCase):
+class IntegrationTestDuckDBSync(IntegrationTestCase):
 	"""
-	Integration tests for DuckDBSyncLog.
+	Integration tests for DuckDBSync.
 	Use this class for testing interactions between multiple components.
 	"""
 
