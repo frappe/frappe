0b2cae4727a7cf46e39dfc5da27caf9d9376b9d4
sokumon <sohamkulkarns9@gmail.com>
2026-06-08 02:43:48 +0530

    fix: always auto generate the sidebar for module

--- frappe/desk/doctype/workspace_sidebar/workspace_sidebar.py
+++ frappe/desk/doctype/workspace_sidebar/workspace_sidebar.py
@@ -241,10 +241,7 @@ def auto_generate_sidebar_from_module():
 	"""Auto generate sidebar from module"""
 	sidebars = []
 	for module in frappe.get_all("Module Def", pluck="name"):
-		if not (
-			frappe.db.exists("Workspace Sidebar", {"module": module, "for_user": None})
-			or frappe.db.exists("Workspace Sidebar", {"name": module, "for_user": None})
-		):
+		if not (frappe.db.exists("Workspace Sidebar", {"name": module, "for_user": None})):
 			module_info = get_module_info(module)
 			sidebar_items = create_sidebar_items(module_info)
 			sidebar = frappe.new_doc("Workspace Sidebar")
