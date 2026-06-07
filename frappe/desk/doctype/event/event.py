2876419137c05d1da8a80da6ca783f8bc70a7b81
gajjug004 <gajjug004@gmail.com>
2026-06-07 08:57:14 +0530

    fix: show calendar events for users in a different timezone than the system

--- frappe/desk/doctype/event/event.py
+++ frappe/desk/doctype/event/event.py
@@ -339,12 +339,14 @@ def send_event_digest():
 @frappe.whitelist()
 @http_cache(max_age=5 * 60, stale_while_revalidate=60 * 60)
 def get_events(
-	start: date,
-	end: date,
+	start: str | date,
+	end: str | date,
 	user: str | None = None,
 	for_reminder: bool = False,
 	filters: str | list | dict[str, Any] | None = None,
 ) -> list[frappe._dict]:
+	start, end = getdate(start), getdate(end)
+
 	caller = frappe.session.user
 	target_user = user or caller
 
