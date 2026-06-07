14be12ef15dbca03b07d8f5c49b19cafadbacb62
ishtiyak <ishtiyaq.itmajlis@gmail.com>
2026-06-07 23:17:21 +0530

    fix: preserve list view sort order during export
    (cherry picked from commit 47c69f0163a65af2e2f720381e7d39607e749000)

--- frappe/core/doctype/data_import/data_import.py
+++ frappe/core/doctype/data_import/data_import.py
@@ -13,6 +13,7 @@ from frappe.core.doctype.data_import.exporter import Exporter
 from frappe.core.doctype.data_import.importer import Importer
 from frappe.model import CORE_DOCTYPES
 from frappe.model.document import Document
+from frappe.model.utils.user_settings import get_user_settings
 from frappe.modules.import_file import import_file_by_path
 from frappe.utils import cint
 from frappe.utils.background_jobs import enqueue, get_redis_conn, is_job_enqueued
@@ -224,6 +225,11 @@ def download_template(doctype, export_fields=None, export_records=None, export_f
 	export_filters = frappe.parse_json(export_filters)
 	export_data = export_records != "blank_template"
 
+	list_settings = frappe.parse_json(get_user_settings(doctype)).get("List", {})
+	sort_by = list_settings.get("sort_by")
+	sort_order = list_settings.get("sort_order")
+	order_by = f"{sort_by} {sort_order}" if sort_by and sort_order else None
+
 	e = Exporter(
 		doctype,
 		export_fields=export_fields,
@@ -231,6 +237,7 @@ def download_template(doctype, export_fields=None, export_records=None, export_f
 		export_filters=export_filters,
 		file_type=file_type,
 		export_page_length=5 if export_records == "5_records" else None,
+		order_by=order_by,
 	)
 	e.build_response()
 
