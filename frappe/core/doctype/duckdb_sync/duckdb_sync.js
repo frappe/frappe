efcf19ae2a1c0cb391a3f07c5198eb08f98a407f
ruthra kumar <ruthra@erpnext.com>
2026-06-08 18:47:33 +0530

    refactor: show sync button only if anything is pending
    (cherry picked from commit 14e08beb1a55825145bcae0fcdfde3153e665521)

--- frappe/core/doctype/duckdb_sync/duckdb_sync.js
+++ frappe/core/doctype/duckdb_sync/duckdb_sync.js
@@ -13,13 +13,23 @@ frappe.ui.form.on("DuckDB Sync", {
 			};
 		});
 		if (frm.doc.docstatus == 1) {
-			frm.add_custom_button(__("Sync Data"), function () {
-				frappe.call({
-					method: "frappe.core.doctype.duckdb_sync.duckdb_sync.start_data_sync",
-					args: {
-						docname: frm.doc.name,
-					},
-				});
+			frappe.call({
+				method: "frappe.core.doctype.duckdb_sync.duckdb_sync.is_data_sync_pending",
+				args: {
+					docname: frm.doc.name,
+				},
+				callback: function (r) {
+					if (r.message) {
+						frm.add_custom_button(__("Sync Data"), function () {
+							frappe.call({
+								method: "frappe.core.doctype.duckdb_sync.duckdb_sync.start_data_sync",
+								args: {
+									docname: frm.doc.name,
+								},
+							});
+						});
+					}
+				},
 			});
 		}
 	},
