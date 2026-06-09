5404c68a0f87ba9f8e5dbe94fb17bbd839ae171e
Shrihari Mahabal <shriharimahabal08@gmail.com>
2026-06-09 18:41:33 +0530

    fix(Link): handle numeric docnames in awesomplete selectcomplete handler
    (cherry picked from commit 7bec69fab66c728903d471726ebe70a7e638311f)

--- frappe/public/js/frappe/form/controls/link.js
+++ frappe/public/js/frappe/form/controls/link.js
@@ -459,7 +459,7 @@ frappe.ui.form.ControlLink = class ControlLink extends frappe.ui.form.ControlDat
 
 		this.$input.on("awesomplete-selectcomplete", function (e) {
 			let o = e.originalEvent;
-			if (o.text.value.indexOf("__link_option") !== -1) {
+			if (cstr(o.text.value).indexOf("__link_option") !== -1) {
 				me.$input.val("");
 			}
 		});
