e60636de923350cb79883a6d9d0ada3e206aa34f
Shrihari Mahabal <shriharimahabal08@gmail.com>
2026-06-13 12:50:18 +0530

    fix: link_filters customization should take precedence over programmatic get_query
    (cherry picked from commit 2f30725701565bf916818d29cff2be76373ba02e)

--- frappe/public/js/frappe/form/controls/link.js
+++ frappe/public/js/frappe/form/controls/link.js
@@ -854,7 +854,7 @@ frappe.ui.form.ControlLink = class ControlLink extends frappe.ui.form.ControlDat
 		// take filters from the link field and add to the query
 		const query_filters = this.get_query?.()?.filters || {};
 		if (query_filters) {
-			filters = { ...filters, ...query_filters };
+			filters = { ...query_filters, ...filters };
 		}
 
 		this.get_query = function () {
