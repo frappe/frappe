06b1c47e22fc6b77be2139d005c42c2e84c9f004
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2026-06-17 00:48:52 +0200

    feat: Gravatar deletion prompt (#40018)
    (cherry picked from commit 94b2e94770ed41ebaf0e197bb7cbdbc99a11b77f)
    
    # Conflicts:
    #	frappe/patches.txt
    #	frappe/sessions.py

--- frappe/sessions.py
+++ frappe/sessions.py
@@ -130,6 +130,9 @@ def get():
 	"""get session boot info"""
 	from frappe.boot import get_bootinfo, get_unseen_notes
 	from frappe.utils.change_log import get_change_log
+	from frappe.utils.legacy_gravatar_cleanup import (
+		should_show_gravatar_deletion_prompt,
+	)
 
 	bootinfo = None
 	if not getattr(frappe.conf, "disable_session_cache", None):
@@ -182,6 +185,11 @@ def get():
 	bootinfo["user"]["impersonated_by"] = frappe.session.data.get("impersonated_by")
 	bootinfo["navbar_settings"] = frappe.get_cached_doc("Navbar Settings")
 	bootinfo.has_app_updates = has_app_update_notifications()
+<<<<<<< HEAD
+=======
+	bootinfo.show_external_link_warning = frappe.get_system_settings("show_external_link_warning")
+	bootinfo.show_gravatar_deletion_prompt = should_show_gravatar_deletion_prompt()
+>>>>>>> 94b2e94770 (feat: Gravatar deletion prompt (#40018))
 
 	return bootinfo
 
