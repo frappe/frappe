887e52413aba6e3917b4e825705a6316cdd61d62
Ejaaz Khan <ejaaz@frappe.io>
2026-06-17 15:29:41 +0530

    fix: guard printpreview against non-beta print formats

--- frappe/www/printpreview.py
+++ frappe/www/printpreview.py
@@ -3,11 +3,16 @@ no_cache = 1
 
 def get_context(context):
 	import frappe
-	from frappe.utils.print_format_generator import get_html
 
 	print_format = frappe.form_dict.print_format
 	docname = frappe.form_dict.name
 	letterhead = frappe.form_dict.get("letterhead")
 
 	pf = frappe.get_doc("Print Format", print_format)
-	context.body = get_html(pf.doc_type, docname, print_format, letterhead)
+
+	if pf.get("print_format_builder_beta"):
+		from frappe.utils.print_format_generator import get_html
+
+		context.body = get_html(pf.doc_type, docname, print_format, letterhead)
+	else:
+		context.body = pf.get_html(docname, letterhead)
