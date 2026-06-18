d1f917f303f421a3a7dbe88bde5ac3b8089b8bb6
ruthra kumar <ruthra@erpnext.com>
2026-06-18 11:16:13 +0530

    refactor: rename both single doctype and child table
    (cherry picked from commit d93b4a33e67c831cc01876440b780aee6c9f848f)

--- frappe/core/doctype/synced_report_settings/test_synced_report_settings.py
+++ frappe/core/doctype/synced_report_settings/test_synced_report_settings.py
@@ -11,9 +11,9 @@ EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 
 
-class IntegrationTestSyncedReportDoctype(IntegrationTestCase):
+class IntegrationTestSyncedReportSettings(IntegrationTestCase):
 	"""
-	Integration tests for SyncedReportDoctype.
+	Integration tests for SyncedReportSettings.
 	Use this class for testing interactions between multiple components.
 	"""
 
