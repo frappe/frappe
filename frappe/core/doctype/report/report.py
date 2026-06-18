89cc7f9f41763f32c1a094176d275372c64f78f1
ruthra kumar <ruthra@erpnext.com>
2026-06-18 11:38:50 +0530

    refactor: move doctype dependency to reports

--- frappe/core/doctype/report/report.py
+++ frappe/core/doctype/report/report.py
@@ -27,6 +27,7 @@ class Report(Document):
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
+		from frappe.core.doctype.doctype_to_sync.doctype_to_sync import DoctypeToSync
 		from frappe.core.doctype.has_role.has_role import HasRole
 		from frappe.core.doctype.report_column.report_column import ReportColumn
 		from frappe.core.doctype.report_filter.report_filter import ReportFilter
@@ -39,6 +40,8 @@ class Report(Document):
 		default_print_format: DF.Link | None
 		disable_prepared_report_automation: DF.Check
 		disabled: DF.Check
+		doctype_to_sync: DF.Table[DoctypeToSync]
+		documentation_url: DF.Data | None
 		filters: DF.Table[ReportFilter]
 		generate_csv: DF.Check
 		is_standard: DF.Literal["No", "Yes"]
@@ -53,6 +56,7 @@ class Report(Document):
 		report_script: DF.Code | None
 		report_type: DF.Literal["Report Builder", "Query Report", "Script Report", "Custom Report"]
 		roles: DF.Table[HasRole]
+		synced_report: DF.Check
 		timeout: DF.Int
 	# end: auto-generated types
 
