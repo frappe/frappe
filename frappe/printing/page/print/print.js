1eae8468646092dc8573b871e93c70f3efb30385
KerollesFathy <kerollesfhabib@gmail.com>
2026-06-18 13:41:58 +0000

    refactor: skip clearing letterhead selector when no default exists
    (cherry picked from commit c2ff1dd7d593f5a7f46d5a536029c85cac68dcda)

--- frappe/printing/page/print/print.js
+++ frappe/printing/page/print/print.js
@@ -390,7 +390,9 @@ frappe.ui.form.PrintView = class {
 					{ disabled: 0, is_default: 1, letter_head_for: "DocType" },
 					"name"
 				)
-				.then(({ message }) => this.letterhead_selector.val(message?.name || ""));
+				.then(({ message }) => {
+					if (message?.name) this.letterhead_selector.val(message.name);
+				});
 
 		if (!this.frm.doc.letter_head) return get_default();
 
