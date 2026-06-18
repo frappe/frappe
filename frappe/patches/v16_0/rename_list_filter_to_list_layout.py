ef81fcee92e035338811163d3bbc3d370b0986bb
Sumit Jain <jainmit23@gmail.com>
2026-06-18 23:28:44 +0530

    fix: update List Filter migration to use add_column from schema module

--- frappe/patches/v16_0/rename_list_filter_to_list_layout.py
+++ frappe/patches/v16_0/rename_list_filter_to_list_layout.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.database.schema import add_column
 from frappe.model.rename_doc import rename_doc
 from frappe.patches.v16_0.rename_list_layout_filter_name_to_layout_name import (
 	migrate_filter_name_to_layout_name,
@@ -11,7 +12,7 @@ def execute():
 		return
 
 	if frappe.db.table_exists("List Filter") and not frappe.db.has_column("List Filter", "route_signature"):
-		frappe.db.add_column("List Filter", "route_signature", "Data")
+		add_column("List Filter", "route_signature", "Data")
 
 	rename_doc("DocType", "List Filter", "List Layout", force=True)
 	migrate_filter_name_to_layout_name()
