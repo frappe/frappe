94bd640c494fb0ec3506fb45edb9ae052ec0fb05
Nishka Gosalia <58264710+nishkagosalia@users.noreply.github.com>
2026-06-19 17:53:47 +0530

    fix: resolving conflicts

--- frappe/public/js/frappe/form/formatters.js
+++ frappe/public/js/frappe/form/formatters.js
@@ -100,7 +100,7 @@ frappe.form.formatters = {
 			docfield.precision ||
 			cint(frappe.boot.sysdefaults && frappe.boot.sysdefaults.float_precision) ||
 			2;
-		return frappe.form.formatters._right(format_number(value, null, Math.min(precision, valuePrecision))) + "%", options);
+		return frappe.form.formatters._right(format_number(value, null, Math.min(precision, valuePrecision)) + "%", options);
 	},
 	Rating: function (value, docfield) {
 		let rating_html = "";
