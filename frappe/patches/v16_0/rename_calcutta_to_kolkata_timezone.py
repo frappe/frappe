7a3e64405fa91ef48e9c80b224bf243c7a625351
Rushabh Mehta <rmehta@gmail.com>
2026-06-21 20:48:18 +0530

    fix: use set_single_value

--- frappe/patches/v16_0/rename_calcutta_to_kolkata_timezone.py
+++ frappe/patches/v16_0/rename_calcutta_to_kolkata_timezone.py
@@ -5,4 +5,4 @@ def execute():
 	frappe.db.set_value("User", {"time_zone": "Asia/Calcutta"}, "time_zone", "Asia/Kolkata")
 
 	if frappe.db.get_single_value("System Settings", "time_zone") == "Asia/Calcutta":
-		frappe.db.set_value("System Settings", "System Settings", "time_zone", "Asia/Kolkata")
+		frappe.db.set_single_value("System Settings", "System Settings", "time_zone", "Asia/Kolkata")
