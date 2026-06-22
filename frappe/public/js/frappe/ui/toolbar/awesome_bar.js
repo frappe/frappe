bb343844154540cd8c0003564feb7900b74d04c4
Ejaaz Khan <ejaaz@frappe.io>
2026-06-22 12:12:22 +0530

    fix: remove empty state from awesomebar, show bare input when no history

--- frappe/public/js/frappe/ui/toolbar/awesome_bar.js
+++ frappe/public/js/frappe/ui/toolbar/awesome_bar.js
@@ -36,22 +36,7 @@ frappe.search.AwesomeBar = class AwesomeBar {
 			/>
 			<div class="modal-divider"></div>
 		</div>
-		<div class="awesomebar-empty-state hidden">
-			<div class="text-center">
-				<div class="awesomebar-empty-icon">
-					${frappe.utils.icon("search", "md")}
-				</div>
-				<div class="awesomebar-empty-state-text">${__("Search or run a command")}</div>
-				<div class="awesomebar-empty-state-tip text-muted">
-					${__("Type to search")}
-					&nbsp;·&nbsp;<span class="awesomebar-shortcut-key">${__("new ...")}</span> ${__("to create")}
-					&nbsp;·&nbsp;<span class="awesomebar-shortcut-key">${__("... in ...")}</span> ${__("to scope")}
-					&nbsp;·&nbsp;<span class="awesomebar-shortcut-key">${
-						frappe.utils.is_mac() ? "⌘G" : "Ctrl+G"
-					}</span> ${__("global")}
-				</div>
-			</div>
-		</div>`;
+		`;
 
 		let search_modal_footer = `<div class="awesomebar-modal-footer flex justify-between w-100">
 			<div class="help-navigation">
@@ -205,12 +190,6 @@ frappe.search.AwesomeBar = class AwesomeBar {
 				awesomplete.options_with_desc = me.create_options_with_descriptions(options);
 				Awesomplete.prototype._itemCursor = 0;
 				awesomplete.list = options;
-				const $empty_state = search_modal.find(".awesomebar-empty-state");
-				if (!txt && !options.length) {
-					$empty_state.removeClass("hidden");
-				} else {
-					$empty_state.addClass("hidden");
-				}
 			}, 50)
 		);
 
