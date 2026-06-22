4fa299aa0dd2434bc23fd570c821c756aa1d322e
nishkagosalia <nishka.gosalia@gmail.com>
2026-06-22 14:58:52 +0530

    feat: Moving naming series dialog from erpnext to framework

--- frappe/public/js/desk.bundle.js
+++ frappe/public/js/desk.bundle.js
@@ -50,6 +50,7 @@ import "./frappe/ui/capture.js";
 import "./frappe/ui/embedded_list.js";
 import "./frappe/ui/app_icon.js";
 import "./frappe/ui/theme_switcher.js";
+import "./frappe/ui/naming_series.js";
 
 import "./frappe/model/model.js";
 import "./frappe/db.js";
