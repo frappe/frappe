d54681d65cb9786c954116de504bd8fce77d065b
KerollesFathy <kerollesfhabib@gmail.com>
2026-06-29 14:05:12 +0000

    fix: use funnel filter icons in FilterArea component

--- frappe/public/js/frappe/list/base_list.js
+++ frappe/public/js/frappe/list/base_list.js
@@ -1398,7 +1398,7 @@ class FilterArea {
 			<div class="btn-group">
 				<button class="btn btn-default btn-sm filter-button">
 					<span class="filter-icon button-icon">
-						${frappe.utils.icon("es-line-filter")}
+						${frappe.utils.icon("funnel")}
 					</span>
 					<span class="button-label hidden-xs">
 					${__("Filter")}
@@ -1406,7 +1406,7 @@ class FilterArea {
 				</button>
 				<button class="btn btn-default btn-sm filter-x-button" title="${__("Clear all filters")}">
 					<span class="filter-icon button-icon">
-						${frappe.utils.icon("es-small-close")}
+						${frappe.utils.icon("funnel-x")}
 					</span>
 				</button>
 			</div>
