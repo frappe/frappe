b20fc9c12d4b8e72f69583770d6f6d8052d9b7ca
Sydney <sydney223as@gmail.com>
2026-06-30 14:50:42 +0530

    fix: ui bug in border

--- ui/src/components/Composer/EmailComposer/RecipientFields.vue
+++ ui/src/components/Composer/EmailComposer/RecipientFields.vue
@@ -23,7 +23,7 @@
 		<Row v-if="showBcc || model.bcc.length" label="BCC">
 			<RecipientSelect v-model="model.bcc" class="flex-1" :search="search" />
 		</Row>
-		<div class="border-b bg-surface-gray-1 pt-2.5"></div>
+		<div class="border-b bg-surface-gray-1 mt-2.5"></div>
 	</div>
 </template>
 
