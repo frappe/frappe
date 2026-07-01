0340e145193f2e57d7e3d6eff31f87d13fc98045
Ankush Menat <ankush@frappe.io>
2026-07-01 19:16:50 +0530

    fix: Skip snapshot violation while dismissing changelog

--- frappe/utils/change_log.py
+++ frappe/utils/change_log.py
@@ -88,13 +88,14 @@ def get_change_log_for_app(app, from_version, to_version):
 
 @frappe.whitelist()
 def update_last_known_versions():
-	frappe.db.set_value(
-		"User",
-		frappe.session.user,
-		"last_known_versions",
-		json.dumps(get_versions()),
-		update_modified=False,
-	)
+	with suppress(frappe.QueryDeadlockError):
+		frappe.db.set_value(
+			"User",
+			frappe.session.user,
+			"last_known_versions",
+			json.dumps(get_versions()),
+			update_modified=False,
+		)
 
 
 @frappe.whitelist()
