adb8d838fef70814642eea842d29761374763ee2
AarDG10 <aarol.dsouza@gmail.com>
2026-07-02 23:18:28 +0530

    fix(document_follow): ignore if document cannot be followed
    Instead of throwing, ignore , if document cannot be followed. Particularly being done due to breakage observed in workflow_action because when an user has auto-follow on creation and when a workflow action doc is created and they don't have read perms. then it throws an error.

--- frappe/desk/form/document_follow.py
+++ frappe/desk/form/document_follow.py
@@ -65,7 +65,7 @@ def follow_document(doctype: str, doc_name: str, user: str) -> Document | bool:
 		frappe.throw(_("You can only follow documents for yourself."), frappe.PermissionError)
 
 	if not frappe.has_permission(doctype, "read", doc=doc_name, user=user):
-		frappe.throw(_("You do not have permission to access this document."), frappe.PermissionError)
+		return False
 
 	if not frappe.db.get_value("User", user, "document_follow_notify", ignore=True, cache=True):
 		frappe.toast(_("Document follow is not enabled for this user."))
