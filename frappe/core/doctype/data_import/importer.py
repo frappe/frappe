754e099a2fec33510ae9432a3c941c92321c1aba
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-07-05 04:11:43 +0530

    fix: make data import file logging best-effort

--- frappe/core/doctype/data_import/importer.py
+++ frappe/core/doctype/data_import/importer.py
@@ -281,10 +281,13 @@ class Importer:
 
 					log_index += 1
 
-					frappe.logger("data_import").error(
-						f"Data Import {self.data_import.name}: row(s) {row_indexes} failed to import",
-						exc_info=True,
-					)
+					try:
+						frappe.logger("data_import").error(
+							f"Data Import {self.data_import.name}: row(s) {row_indexes} failed to import",
+							exc_info=True,
+						)
+					except Exception:
+						pass
 
 		# Logs are db inserted directly so will have to be fetched again
 		import_log = (
@@ -312,9 +315,12 @@ class Importer:
 			status = "Error"
 		elif len(failures) > 0 and len(successes) > 0:
 			status = "Partial Success"
-			frappe.logger("data_import").warning(
-				f"Data Import {self.data_import.name}: {len(failures)} of {attempted_payload_count} rows failed"
-			)
+			try:
+				frappe.logger("data_import").warning(
+					f"Data Import {self.data_import.name}: {len(failures)} of {attempted_payload_count} rows failed"
+				)
+			except Exception:
+				pass
 		else:
 			status = "Pending"
 
