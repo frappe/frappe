216255fce095b8dd663246334f4735ed2d84461e
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-07-11 21:17:56 +0530

    feat: add Automation Action controller

---
+++ frappe/automation/doctype/automation_action/automation_action.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+# License: MIT. See LICENSE
+
+from frappe.model.document import Document
+
+
+class AutomationAction(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		action_type: DF.Data
+		params: DF.JSON | None
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		step_condition: DF.Code | None
+	# end: auto-generated types
+
+	pass
