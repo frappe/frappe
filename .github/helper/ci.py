7fe54762c34634bd1f38fa28fac46e8cac94bceb
Tanmoy Sarkar <ts741127@gmail.com>
2026-07-14 20:17:06 +0530

    ci(sqlite): add sqlite to the server test matrix

--- .github/helper/ci.py
+++ .github/helper/ci.py
@@ -9,9 +9,10 @@ configuration.
 """
 
 import json
-import sys
 import os
+import sys
 from pathlib import Path
+
 from coverage import Coverage
 
 STANDARD_INCLUSIONS = ["*.py"]
@@ -54,9 +55,7 @@ FRAPPE_EXCLUSIONS = [
 	"*frappe/setup.py",
 	"*/doctype/*/*_dashboard.py",
 	"*/patches/*",
-	"*/frappe/database/postgres/*",
 	"*/.github/helper/ci.py",
-	"*/frappe/database/sqlite/*",
 	*TESTED_VIA_CLI,
 ]
 
@@ -87,6 +86,11 @@ class CodeCoverage:
 
 			if self.app == "frappe":
 				omit.extend(FRAPPE_EXCLUSIONS)
+				db = os.environ.get("DB")
+				if db != "postgres":
+					omit.append("*/frappe/database/postgres/*")
+				if db != "sqlite":
+					omit.append("*/frappe/database/sqlite/*")
 
 			self.coverage = Coverage(source=[source_path], omit=omit, include=STANDARD_INCLUSIONS)
 
