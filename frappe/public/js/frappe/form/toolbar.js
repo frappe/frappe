f39763d28d84a90b014166c1ec0db50dc7c94af6
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-07-19 14:17:15 +0530

    feat(form): add print to form menu on mobile
    (cherry picked from commit 8cda524c3a7f9865a02f249696fabd4536636573)

--- frappe/public/js/frappe/form/toolbar.js
+++ frappe/public/js/frappe/form/toolbar.js
@@ -360,7 +360,7 @@ frappe.ui.form.Toolbar = class Toolbar {
 	}
 
 	make_menu_items() {
-		// Print
+		this.add_print();
 		this.add_discard();
 		this.add_open_sidebar();
 		this.add_email();
@@ -461,6 +461,30 @@ frappe.ui.form.Toolbar = class Toolbar {
 				},
 				true
 			);
+		}
+	}
+
+	add_print() {
+		const print_settings = frappe.model.get_doc(":Print Settings", "Print Settings");
+		const allow_print_for_draft = cint(print_settings.allow_print_for_draft);
+		const allow_print_for_cancelled = cint(print_settings.allow_print_for_cancelled);
+
+		if (
+			!frappe.model.is_submittable(this.frm.doc.doctype) ||
+			this.frm.doc.docstatus == 1 ||
+			(allow_print_for_cancelled && this.frm.doc.docstatus == 2) ||
+			(allow_print_for_draft && this.frm.doc.docstatus == 0)
+		) {
+			if (frappe.model.can_print(null, this.frm) && !this.frm.meta.issingle) {
+				let menu_item = this.page.add_menu_item(
+					__("Print"),
+					() => {
+						this.frm.print_doc();
+					},
+					true
+				);
+				menu_item.parent().addClass("hidden-xl");
+			}
 		}
 	}
 
