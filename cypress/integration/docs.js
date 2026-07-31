1e06f7b2f0fa8bc64a0d82736019e0d3b3285e5e
barredterra <14891507+barredterra@users.noreply.github.com>
2026-07-31 20:32:07 +0200

    test: fix frontend cases

--- cypress/integration/docs.js
+++ cypress/integration/docs.js
@@ -7,14 +7,13 @@ context("Documentation Browser", () => {
 		cy.visit("/desk/docs");
 		cy.get(".docs-tree .docs-tree-node").should("have.length.at.least", 1);
 		cy.get(".docs-reading-pane").should("not.have.class", "hide");
-		cy.get(".docs-reading-pane h1").should("exist");
+		cy.get(".docs-reading-pane").should("contain", "Frappe Framework");
 	});
 
 	it("navigates nested documentation routes", () => {
-		cy.visit("/desk/docs/authoring");
-		cy.get('.docs-tree-node.active[data-path="authoring"]').should("exist");
+		cy.visit("/desk/docs/framework/docs/authoring");
+		cy.get('.docs-tree-node.active[data-path="framework/docs/authoring"]').should("exist");
 		cy.get(".docs-reading-pane").contains("Authoring Guide");
-		cy.get(".navbar-breadcrumbs li").should("have.length", 2);
 		cy.get(".navbar-breadcrumbs li").first().should("contain", "Documentation");
 		cy.get(".navbar-breadcrumbs li").last().should("contain", "Authoring Guide");
 	});
@@ -22,6 +21,7 @@ context("Documentation Browser", () => {
 	it("shows not-found state for missing pages", () => {
 		cy.visit("/desk/docs/does-not-exist-page");
 		cy.get(".docs-state").should("not.have.class", "hide");
-		cy.contains("could not find");
+		cy.get(".docs-reading-pane").should("have.class", "hide");
+		cy.get(".docs-state-content").should("contain", "could not find");
 	});
 });
