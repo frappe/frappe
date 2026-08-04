b4a86bd6f7d76b793495b98ada7e55e24686f4d5
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-08-04 19:50:20 +0530

    chore: resolve backport conflict in file_view.js

--- frappe/public/js/frappe/views/file/file_view.js
+++ frappe/public/js/frappe/views/file/file_view.js
@@ -52,13 +52,8 @@ frappe.views.FileView = class FileView extends frappe.views.ListView {
 
 		frappe.breadcrumbs.add({
 			type: "Custom",
-<<<<<<< HEAD
-			label: __("Home"),
-			route: "/app/List/File/Home",
-=======
 			label: at_home_folder ? this.page_title : __("Home"),
-			route: "/desk/file",
->>>>>>> 81465ecf60 (fix(file manager): resolve nested folder routes correctly)
+			route: "/app/file",
 		});
 	}
 
@@ -353,14 +348,10 @@ frappe.views.FileView = class FileView extends frappe.views.ListView {
 	}
 
 	get_route_url(file) {
-<<<<<<< HEAD
-		return file.is_folder ? "/app/List/File/" + file.name : this.get_form_link(file);
-=======
 		if (!file.is_folder) return this.get_form_link(file);
 
 		const folder_path = file.name.split("/").map(encodeURIComponent).join("/");
-		return "/desk/file/view/" + folder_path;
->>>>>>> 81465ecf60 (fix(file manager): resolve nested folder routes correctly)
+		return "/app/file/view/" + folder_path;
 	}
 
 	get_creation_date(file) {
