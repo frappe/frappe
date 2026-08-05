dc6b6b20aef8de2ef2e444317234678aaddda1ad
AarDG10 <aarol.dsouza@gmail.com>
2026-08-05 16:37:45 +0530

    fix(inbox): add perm. check in create_email_flag_queue
    (cherry picked from commit c9b0b54616c58af303aa8d0d2d4e0a602beeb6e9)
    
    # Conflicts:
    #	frappe/email/inbox.py

--- frappe/email/inbox.py
+++ frappe/email/inbox.py
@@ -41,8 +41,12 @@ def get_email_accounts(user=None):
 def create_email_flag_queue(names, action):
 	"""create email flag queue to mark email either as read or unread"""
 
+<<<<<<< HEAD
 	def mark_as_seen_unseen(name, action):
 		doc = frappe.get_doc("Communication", name)
+=======
+	def mark_as_seen_unseen(doc, action):
+>>>>>>> c9b0b54616 (fix(inbox): add perm. check in create_email_flag_queue)
 		if action == "Read":
 			doc.add_seen()
 		else:
@@ -53,10 +57,16 @@ def create_email_flag_queue(names, action):
 	if not all([names, action]):
 		return
 
+<<<<<<< HEAD
 	for name in json.loads(names or []):
 		uid, seen_status, email_account = frappe.db.get_value(
 			"Communication", name, ["uid", "seen", "email_account"]
 		)
+=======
+	for name in frappe.parse_json(names):
+		doc = frappe.get_lazy_doc("Communication", name, check_permission="read")
+		uid, seen_status, email_account = doc.uid, doc.seen, doc.email_account
+>>>>>>> c9b0b54616 (fix(inbox): add perm. check in create_email_flag_queue)
 		if not uid:
 			uid = -1
 		if not seen_status:
@@ -95,8 +105,8 @@ def create_email_flag_queue(names, action):
 					}
 				)
 				flag_queue.save(ignore_permissions=True)
-				frappe.db.set_value("Communication", name, "seen", seen, update_modified=False)
-				mark_as_seen_unseen(name, action)
+				doc.db_set("seen", seen, update_modified=False)
+				mark_as_seen_unseen(doc, action)
 
 
 @frappe.whitelist()
