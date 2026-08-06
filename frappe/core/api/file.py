e072d2ce2249326cc08b960a7fdf230a6a66be23
AarDG10 <aarol.dsouza@gmail.com>
2026-08-06 17:32:28 +0530

    feat(system settings): add a config. to enforce a lim.
    Decoupling this from Max File Size, instead uses it's own config. similar to Max File Size via System Settings.
    
    (cherry picked from commit b20e1b4b590a526ade989f9b3991f043667f6075)
    
    # Conflicts:
    #	frappe/core/api/file.py

--- frappe/core/api/file.py
+++ frappe/core/api/file.py
@@ -90,6 +90,21 @@ def get_max_file_size() -> int:
 	)
 
 
+<<<<<<< HEAD
+=======
+def get_max_extract_size() -> int:
+	return (
+		cint(frappe.get_system_settings("max_zip_extract_size")) * 1024 * 1024
+		or cint(frappe.conf.get("max_zip_extract_size"))
+		or 25 * 1024 * 1024
+	)
+
+
+def get_file_chunk_size() -> int:
+	return cint(frappe.conf.get("file_chunk_size")) or 25 * 1024 * 1024
+
+
+>>>>>>> b20e1b4b59 (feat(system settings): add a config. to enforce a lim.)
 @frappe.whitelist()
 def create_new_folder(file_name: str, folder: str) -> File:
 	"""create new folder under current parent folder"""
