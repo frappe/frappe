4c4ce8f9af20a8b131c6345e2ca3af39afdcb609
Ejaaz Khan <67804911+iamejaaz@users.noreply.github.com>
2026-08-11 20:51:08 +0530

    Merge pull request #34553 from karm1000/feat/#48564
    feat: add user/doctype filtering to `User Doctype Permissions` report
    (cherry picked from commit 03431a99089960c61f00bedda86354b7cca01ea5)

--- frappe/core/report/user_doctype_permissions/user_doctype_permissions.js
+++ frappe/core/report/user_doctype_permissions/user_doctype_permissions.js
@@ -8,7 +8,25 @@ frappe.query_reports["User Doctype Permissions"] = {
 			label: __("User"),
 			fieldtype: "Link",
 			options: "User",
-			reqd: 1,
+		},
+		{
+			fieldname: "doctype",
+			label: __("Doctype"),
+			fieldtype: "Link",
+			options: "DocType",
 		},
 	],
+	onload(report) {
+		const refresh = report.refresh.bind(report);
+		report.refresh = function () {
+			const user = report.get_filter_value("user");
+			const doctype = report.get_filter_value("doctype");
+			if (!user && !doctype) {
+				report.toggle_message(true, __("Please set at least one filter: User or DocType"));
+				report.toggle_report(false);
+				return;
+			}
+			return refresh();
+		};
+	},
 };
