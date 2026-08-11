05469b88095f6c33c5c9d21f2db0a91549e10c7c
AarDG10 <aarol.dsouza@gmail.com>
2026-08-11 12:41:45 +0530

    chore: resolve conflicts

--- frappe/email/inbox.py
+++ frappe/email/inbox.py
@@ -41,12 +41,7 @@ def get_email_accounts(user=None):
 def create_email_flag_queue(names, action):
 	"""create email flag queue to mark email either as read or unread"""
 
-<<<<<<< HEAD
-	def mark_as_seen_unseen(name, action):
-		doc = frappe.get_doc("Communication", name)
-=======
 	def mark_as_seen_unseen(doc, action):
->>>>>>> c9b0b54616 (fix(inbox): add perm. check in create_email_flag_queue)
 		if action == "Read":
 			doc.add_seen()
 		else:
@@ -57,16 +52,10 @@ def create_email_flag_queue(names, action):
 	if not all([names, action]):
 		return
 
-<<<<<<< HEAD
-	for name in json.loads(names or []):
-		uid, seen_status, email_account = frappe.db.get_value(
-			"Communication", name, ["uid", "seen", "email_account"]
-		)
-=======
 	for name in frappe.parse_json(names):
-		doc = frappe.get_lazy_doc("Communication", name, check_permission="read")
+		doc = frappe.get_doc("Communication", name)
+		doc.check_permission("read")
 		uid, seen_status, email_account = doc.uid, doc.seen, doc.email_account
->>>>>>> c9b0b54616 (fix(inbox): add perm. check in create_email_flag_queue)
 		if not uid:
 			uid = -1
 		if not seen_status:
