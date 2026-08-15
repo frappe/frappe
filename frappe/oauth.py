ccafd6de3de95d878f5c738aaba730fab627cef7
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2026-08-15 22:37:40 +0200

    fix(oauth2): authenticate UserInfo bearer tokens (#41545)
    (cherry picked from commit ee58f845d70ee0556a20c9cb1315512c9698310f)
    
    # Conflicts:
    #	frappe/oauth.py

--- frappe/oauth.py
+++ frappe/oauth.py
@@ -226,13 +226,26 @@ class OAuthWebRequestValidator(RequestValidator):
 
 	def validate_bearer_token(self, token, scopes, request):
 		# Remember to check expiration and scope membership
+<<<<<<< HEAD
 		otoken = frappe.get_doc("OAuth Bearer Token", token)
 		is_token_valid = (now_datetime() < otoken.expiration_time) and otoken.status != "Revoked"
 		client_scopes = frappe.db.get_value("OAuth Client", otoken.client, "scopes").split(
+=======
+		otoken = frappe.get_doc("OAuth Bearer Token", {"access_token": get_oauth_token_hash(token)})
+		is_token_valid = (
+			now_datetime() < otoken.expiration_time
+			and otoken.status != "Revoked"
+			and frappe.db.exists("User", {"name": otoken.user, "enabled": 1})
+		)
+		client_scopes = frappe.get_cached_value("OAuth Client", otoken.client, "scopes").split(
+>>>>>>> ee58f845d7 (fix(oauth2): authenticate UserInfo bearer tokens (#41545))
 			get_url_delimiter()
 		)
 		are_scopes_valid = all(scope in client_scopes for scope in scopes)
-		return is_token_valid and are_scopes_valid
+		if is_token_valid and are_scopes_valid:
+			request.user = otoken.user
+			return True
+		return False
 
 	# Token refresh request
 
@@ -349,7 +362,7 @@ class OAuthWebRequestValidator(RequestValidator):
 		return self.finalize_id_token(id_token, token, token_handler, request)
 
 	def get_userinfo_claims(self, request):
-		user = frappe.get_doc("User", frappe.session.user)
+		user = frappe.get_doc("User", request.user)
 		return get_userinfo(user)
 
 	def validate_id_token(self, token, scopes, request):
