13ad103e92ce4124e11f47f5bc0b3d36eac5880b
Balamurali M <balamurali@frappe.io>
2026-08-19 11:26:51 +0530

    Revert "fix: Always start with clean `frappe.local` in request/jobs (#40088)"
    This reverts commit 72c57c97eb71124dad6f7734b6fc68dcbcf1e98d.

--- frappe/app.py
+++ frappe/app.py
@@ -170,12 +170,13 @@ def run_after_request_hooks(request, response):
 
 
 def init_request(request):
-	site = _site or request.headers.get("X-Frappe-Site-Name") or get_site_name(request.host)
-	frappe.init(site, sites_path=_sites_path, force=True, is_request=True)
-
 	frappe.local.request = request
+	frappe.local.request.after_response = CallbackManager()
+
 	frappe.local.is_ajax = frappe.get_request_header("X-Requested-With") == "XMLHttpRequest"
-	request.after_response = CallbackManager()
+
+	site = _site or request.headers.get("X-Frappe-Site-Name") or get_site_name(request.host)
+	frappe.init(site, sites_path=_sites_path, force=True)
 
 	if not (frappe.local.conf and frappe.local.conf.db_name):
 		# site does not exist
