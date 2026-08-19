f8142fe76eaae7d35bf9ae43e2228b280ce6b415
Nihantra Patel <nihantra@frappe.io>
2026-08-19 19:26:34 +0530

    fix: cypress enter key trigger did not set keyCode properly
    The test used cy.trigger(keydown, { keyCode: 13, which: 13 }) to press Enter. Those properties don't reliably land on a real KeyboardEvent through trigger(), so frappe.ui.keys never saw a proper Enter key and the dialog stayed open in CI.
    
    Use cy.type("{enter}") instead, the same way Enter is pressed elsewhere in this test suite.

--- cypress/integration/list_view.js
+++ cypress/integration/list_view.js
@@ -240,7 +240,7 @@ context("List View", () => {
 			cy.get("body").type("{ctrl}{shift}d");
 			cy.get_open_dialog().should("contain", "Permanently delete");
 
-			cy.get("body").trigger("keydown", { key: "Enter", keyCode: 13, which: 13 });
+			cy.get("body").type("{enter}");
 			cy.get(".modal:visible").should("not.exist");
 
 			cy.call("frappe.client.get_list", {
