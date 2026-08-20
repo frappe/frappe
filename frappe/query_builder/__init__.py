eff2e9128f272d69a5fdb6b15f1552bb3e618f99
Anubhav <42858179+eagledot@users.noreply.github.com>
2026-08-20 11:18:58 +0530

    fix: reverting (pypika) monkey-patched classes

--- frappe/query_builder/__init__.py
+++ frappe/query_builder/__init__.py
@@ -4,12 +4,8 @@ from pypika import Field
 from pypika.utils import ignore_copy
 
 from frappe.query_builder.terms import (
-	BasicCriterionPatched,
-	ComplexCriterionPatched,
-	FieldPatched,
 	ParameterizedFunction,
-	ParameterizedValueWrapper,
-	format_quotes_patched,
+	ParameterizedValueWrapper
 )
 from frappe.query_builder.utils import (
 	Column,
@@ -24,23 +20,9 @@ pypika.terms.Function.get_sql = ParameterizedFunction.get_sql
 pypika.terms.Function = ParameterizedFunction
 
 # * Overrides the field() method and replaces it with the a `PseudoColumn` 'field' for consistency
-pypika.queries.Selectable.__getattr__ = ignore_copy(lambda table, x: FieldPatched(x, table=table))
-pypika.queries.Selectable.__getitem__ = ignore_copy(lambda table, x: FieldPatched(x, table=table))
+pypika.queries.Selectable.__getattr__ = ignore_copy(lambda table, x: Field(x, table=table))
+pypika.queries.Selectable.__getitem__ = ignore_copy(lambda table, x: Field(x, table=table))
 pypika.queries.Selectable.field = pypika.terms.PseudoColumn("field")
-
-# ===========================================================
-# Overrides anywhere `format_quotes` may already have been looked up. (as part of utils!)
-pypika.utils.format_quotes = format_quotes_patched
-pypika.terms.format_quotes = format_quotes_patched
-pypika.queries.format_quotes = format_quotes_patched
-pypika.dialects.format_quotes = format_quotes_patched
-
-pypika.terms.Field = FieldPatched
-pypika.terms.ComplexCriterion = ComplexCriterionPatched
-pypika.terms.BasicCriterion = BasicCriterionPatched
-pypika.queries.Field = FieldPatched
-pypika.Field = FieldPatched
-# =============================================================
 
 # run monkey patches
 patch_all()
