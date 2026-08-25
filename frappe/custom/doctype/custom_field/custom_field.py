d0a92caa2ecdc5ce36e1377d98d478c038ed2509
Ejaaz Khan <ejaaz@frappe.io>
2026-08-25 14:40:23 +0530

    fix(custom-field): name the field in fieldtype-change error

--- frappe/custom/doctype/custom_field/custom_field.py
+++ frappe/custom/doctype/custom_field/custom_field.py
@@ -201,7 +201,9 @@ class CustomField(Document):
 			and not CustomizeForm.allow_fieldtype_change(old_fieldtype, self.fieldtype)
 		):
 			frappe.throw(
-				_("Fieldtype cannot be changed from {0} to {1}").format(old_fieldtype, self.fieldtype)
+				_("Fieldtype of {0} in {1} cannot be changed from {2} to {3}").format(
+					frappe.bold(self.fieldname), self.dt, old_fieldtype, self.fieldtype
+				)
 			)
 
 		if not self.fieldname:
