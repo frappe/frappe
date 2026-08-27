8f0556ec951f214fbe07f63a0367ff1dd2732475
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-08-27 16:22:06 +0530

    test(ui): cover escaping of default link text in get_form_link
    (cherry picked from commit 2c06f3d2319d030fc48b5d13311e8494585a262a)

--- cypress/integration/utils.js
+++ cypress/integration/utils.js
@@ -69,6 +69,21 @@ context("Utils", () => {
 		});
 	});
 
+	it("should escape the docname when it is used as the link text", () => {
+		run_util("get_form_link", "ToDo", "<img src=x onerror=alert(1)>", true).then((link) => {
+			expect(link).to.equal(
+				'<a href="/desk/todo/%3Cimg%20src%3Dx%20onerror%3Dalert(1)%3E">' +
+					"&lt;img src&#x3D;x onerror&#x3D;alert(1)&gt;</a>"
+			);
+		});
+	});
+
+	it("should not escape display text passed by the caller", () => {
+		run_util("get_form_link", "ToDo", "TODO-0001", true, "<b>Open item</b>").then((link) => {
+			expect(link).to.equal('<a href="/desk/todo/TODO-0001"><b>Open item</b></a>');
+		});
+	});
+
 	it("should parse days, hours, minutes and seconds", () => {
 		run_util("seconds_to_duration", 60 * 60 * 24 + 60 * 60 + 60 + 1).then((duration) => {
 			expect(duration).to.deep.equal({
