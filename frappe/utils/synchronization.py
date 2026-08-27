dd30dfc1c108a469ed768262abbfbdce8836bed0
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-08-27 16:56:47 +0530

    fix: don't advise deleting the lock file on lock timeout

--- frappe/utils/synchronization.py
+++ frappe/utils/synchronization.py
@@ -45,5 +45,5 @@ def filelock(lock_name: str, *, timeout=30, is_global=False):
 		raise LockTimeoutError(
 			_("Failed to aquire lock: {}. Lock may be held by another process.").format(lock_name)
 			+ "<br>"
-			+ _("You can manually remove the lock if you think it's safe: {}").format(lock_path)
+			+ _("Wait for that process to finish. Removing the lock file won't help: {}").format(lock_path)
 		) from e
