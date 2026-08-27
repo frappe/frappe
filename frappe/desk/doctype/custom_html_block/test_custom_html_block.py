f748cf57d57ce17f01a92cb3d3571ae623e3a334
Ejaaz Khan <ejaaz@frappe.io>
2026-08-27 13:08:25 +0530

    fix(custom-html-block): allow null filters in link query
    (cherry picked from commit f607fbe3fc06243ff4e90121e32377dd5c86d9c3)
    
    # Conflicts:
    #	frappe/desk/doctype/custom_html_block/test_custom_html_block.py

--- frappe/desk/doctype/custom_html_block/test_custom_html_block.py
+++ frappe/desk/doctype/custom_html_block/test_custom_html_block.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2023, Frappe Technologies and Contributors
 # See license.txt
 
-# import frappe
+from frappe.desk.doctype.custom_html_block.custom_html_block import get_custom_blocks_for_user
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestCustomHTMLBlock(FrappeTestCase):
-	pass
+	def test_query_allows_null_filters(self):
+		result = get_custom_blocks_for_user("Custom HTML Block", "", "name", 0, 20, None)
+		self.assertIsInstance(result, (list, tuple))
