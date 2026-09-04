013d0c6bad06e393cf68c883996b4ecb26161640
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-09-04 22:01:06 +0530

    refactor(ui): use es-button for capture preview remove control

--- frappe/public/js/frappe/ui/capture.js
+++ frappe/public/js/frappe/ui/capture.js
@@ -193,9 +193,16 @@ frappe.ui.Capture = class {
 		this.images.forEach((image, idx) => {
 			images += `
 				<div class="mt-1 p-1 rounded col-md-3 col-sm-4 col-xs-4" data-idx="${idx}">
-					<span class="capture-remove-btn" data-idx="${idx}" title="${__("Remove")}">
+					<button
+						class="es-button capture-remove-btn"
+						data-size="xs"
+						data-variant="outline"
+						data-icon-button="true"
+						data-idx="${idx}"
+						title="${__("Remove")}"
+					>
 						${frappe.utils.icon("x", "sm")}
-					</span>
+					</button>
 					<img class="rounded img-fluid" src="${image}" data-idx="${idx}">
 				</div>
 			`;
