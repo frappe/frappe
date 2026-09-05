cc6b2d7386f2a607150ab67e2b532db001df9bab
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-09-05 19:50:33 +0530

    fix(ui): make capture preview remove control readable over photos

--- frappe/public/js/frappe/ui/capture.js
+++ frappe/public/js/frappe/ui/capture.js
@@ -193,9 +193,14 @@ frappe.ui.Capture = class {
 		this.images.forEach((image, idx) => {
 			images += `
 				<div class="mt-1 p-1 rounded col-md-3 col-sm-4 col-xs-4" data-idx="${idx}">
-					<span class="capture-remove-btn" data-idx="${idx}">
-						${frappe.utils.icon("close", "lg")}
-					</span>
+					<button
+						type="button"
+						class="btn btn-default icon-btn capture-remove-btn"
+						data-idx="${idx}"
+						title="${__("Remove")}"
+					>
+						${frappe.utils.icon("x", "sm")}
+					</button>
 					<img class="rounded img-fluid" src="${image}" data-idx="${idx}">
 				</div>
 			`;
