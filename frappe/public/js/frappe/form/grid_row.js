7e135e165b551f7e35136f37b3ceec9205d7f862
Arya Dharmadhikari <aryadharmadhikari10@gmail.com>
2026-09-09 18:40:42 +0530

    fix(grid): use the modal backdrop colour for the child table row form

--- frappe/public/js/frappe/form/grid_row.js
+++ frappe/public/js/frappe/form/grid_row.js
@@ -1441,7 +1441,7 @@ export default class GridRow {
 			.find(".grid-delete-row")
 			.toggle(!(this.grid.df && this.grid.df.cannot_delete_rows));
 
-		frappe.dom.freeze("", "dark grid-form");
+		frappe.dom.freeze("", "grid-form");
 		if (cur_frm) cur_frm.cur_grid = this;
 		this.wrapper.addClass("grid-row-open");
 		if (
