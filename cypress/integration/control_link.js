8379b80c3759214cf907cd058a40dc62ade06d55
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-09-10 20:05:43 +0530

    test(link): cover a cross doctype filter sharing a link filter fieldname

--- cypress/integration/control_link.js
+++ cypress/integration/control_link.js
@@ -372,6 +372,7 @@ context("Control Link", () => {
 						filters: [
 							["ToDo", "status", "=", "Open"],
 							["ToDo", "priority", "=", "High"],
+							["Communication", "status", "=", "Open"],
 							["description", "like", "%test todo%"],
 						],
 					}),
@@ -386,6 +387,7 @@ context("Control Link", () => {
 
 			expect(filters).to.deep.eq([
 				["ToDo", "priority", "=", "High"],
+				["Communication", "status", "=", "Open"],
 				["description", "like", "%test todo%"],
 				["status", "=", "Closed"],
 			]);
