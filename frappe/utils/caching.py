b874e3df9f34a3ef412d1abd8e4a29d41899ffb2
anubhav <anubhav@frappe.io>
2026-09-10 15:46:53 +0530

    refactor: site_cache leveraging frappe.client_cache

--- frappe/utils/caching.py
+++ frappe/utils/caching.py
@@ -77,97 +77,61 @@ def request_cache(func: Callable) -> Callable:
 	return wrapper
 
 
-def site_cache(ttl: int | None = None, maxsize: int | None = None) -> Callable:
-	"""
-	Decorator to cache method calls across requests.
-
-	The cache is stored in `frappe.utils.caching._SITE_CACHE`.
-
-	The cache persists on the parent process.
-
-	It offers a light-weight cache for the current process without the additional
-	overhead of serializing / deserializing Python objects.
-
-	Note: This cache isn't shared among workers. If you need to share data across
-	workers, use redis (frappe.cache API) instead.
-
-	---
-	Usage:
-	```
-	        from frappe.utils.caching import site_cache
-
-	        @site_cache
-	        def calculate_pi():
-	            import math, time
-
-	            precision = get_precision("Math Constant", "Pi") # depends on site data
-	            return round(math.pi, precision)
-
-	        calculate_pi(10) # will calculate value
-	        calculate_pi(10) # will return value from cache
-	        calculate_pi.clear_cache() # clear this function's cache for all sites
-	        calculate_pi(10) # will calculate value
-	```
+def site_cache(ttl: int | None = 3600, maxsize: int = 16) -> Callable:
+	"""Decorator to cache method calls and its return values in Client_cache.
+		# TODO: implement some policy to evict. Just set `maxsize` to be double or higher count for desired functions for now.
+		# NOTE: `shared = False` is used as cache is supposed to be site-specific.
+	args:
+	        ttl: time to expiry in seconds, defaults to 1 hour
+	        maxsize:int   to limit the number of count, a func can be called with different arguments.
 	"""
 
-	def time_cache_wrapper(func: Callable | None = None) -> Callable:
+	def wrapper(func: Callable | None = None) -> Callable:
+		# Final key would be conditioned on function_name + args + site/database name.
+		func_key = f"{func.__module__}.{func.__qualname__}"
+
 		def clear_cache():
-			"""Clear cache for this function for all sites if not specified."""
-			_SITE_CACHE[func].clear()
+			if (
+				frappe.client_cache
+			):  # In case called during boot-straping itself, frappe.client_cache won't be available.
+				frappe.client_cache.delete_keys(func_key, shared=False)
+				func.cached_values_counter = 0
 
 		func.clear_cache = clear_cache
-
-		if ttl is not None and not callable(ttl):
-			func.ttl = ttl
-			func.expiration = time.monotonic() + func.ttl
-
-		if maxsize is not None and not callable(maxsize):
-			assert maxsize > 0, "site_cache maxsize must be positive to allow caching"
-			func.maxsize = maxsize
+		func.ttl = ttl if not callable(ttl) else 3600
+		func.maxsize = maxsize
+		func.cached_values_counter = 0
 
 		@wraps(func)
-		def site_cache_wrapper(*args, **kwargs):
-			site = getattr(frappe.local, "site", None)
-			if not site:
+		def redis_cache_wrapper(*args, **kwargs):
+			# NOTE: reason for both checks is to support `site_cache` functionality too, but by default it would create
+			# a `boot-strapping` problem, as internally `redis_wrapper` depends on `local.conf`. We also wait for `frappe.client_cache` to be available.
+			if not (frappe.client_cache) or not (hasattr(frappe.local, "conf")):
 				return func(*args, **kwargs)
 
-			arguments_key = (site, __generate_request_cache_key(args, kwargs))
+			func_call_key = f"{func_key}::{hash(__generate_request_cache_key(args, kwargs))}"
+			cached_val = frappe.client_cache.get_value(func_call_key, shared=False)
+			if cached_val is not None:
+				return cached_val
 
-			if hasattr(func, "ttl") and time.monotonic() >= func.expiration:
-				func.clear_cache()
-				func.expiration = time.monotonic() + func.ttl
+			val = func(*args, **kwargs)
+			if (func.cached_values_counter + 1) > func.maxsize:
+				return val
 
-			# NOTE: Important things to consider from thread safety POV:
-			#   1. Other thread can issue clear_cache and delete entire dictionary.
-			#   2. Other thread can pop the exact elemement we are reading if maxsize is hit.
+			if val is None:
+				return None
 
-			# NOTE: Keep a local reference to dictionary of interest so it doesn't get swapped
-			function_cache = _SITE_CACHE[func]
+			ttl = getattr(func, "ttl", 3600)
+			frappe.client_cache.set_value(func_call_key, val, shared=False, ttl=ttl)
+			# NOTE: since `client_cache` is technically a dictionary, so assumed we would always be successful set_value (given no error occured), we increment the following counter)
+			func.cached_values_counter += 1
+			return val
 
-			try:
-				return function_cache[arguments_key]
-
-			# not handling TypeError here, expecting arguments_key to be hashable
-			except (KeyError, RuntimeError):
-				# NOTE: This is just a cache miss or dictionary was modified while reading it
-				pass
-
-			if hasattr(func, "maxsize") and len(function_cache) >= func.maxsize:
-				# Note: This implements FIFO eviction policy
-				with suppress(RuntimeError):
-					function_cache.pop(next(iter(function_cache)), None)
-
-			result = func(*args, **kwargs)
-			function_cache[arguments_key] = result
-
-			return result
-
-		return site_cache_wrapper
+		return redis_cache_wrapper
 
 	if callable(ttl):
-		return time_cache_wrapper(ttl)
-
-	return time_cache_wrapper
+		return wrapper(ttl)
+	return wrapper
 
 
 def redis_cache(ttl: int | None = 3600, user: str | bool | None = None, shared: bool = False) -> Callable:
