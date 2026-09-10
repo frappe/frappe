eb8c60c9ddad8f3bf568607de50c48cf4b509b06
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-09-10 20:05:38 +0530

    fix(link): compare doctype when a link filter overrides a query filter

--- frappe/public/js/frappe/form/controls/link.js
+++ frappe/public/js/frappe/form/controls/link.js
@@ -848,12 +848,14 @@ frappe.ui.form.ControlLink = class ControlLink extends frappe.ui.form.ControlDat
 			const link_filters = this.apply_link_field_filters();
 
 			if (Array.isArray(args.filters)) {
+				const doctype = this.get_options();
 				const fieldnames = Object.keys(link_filters);
 				args.filters = args.filters
-					.filter(
-						(filter) =>
-							!fieldnames.includes(filter.length >= 4 ? filter[1] : filter[0])
-					)
+					.filter((filter) => {
+						const [filter_doctype, fieldname] =
+							filter.length >= 4 ? filter : [doctype, filter[0]];
+						return filter_doctype !== doctype || !fieldnames.includes(fieldname);
+					})
 					.concat(
 						fieldnames.map((fieldname) => [fieldname, ...link_filters[fieldname]])
 					);
