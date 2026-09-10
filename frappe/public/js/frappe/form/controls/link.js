6d558b7ad02275e9bab8bb70e8a1d5202982ccae
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-09-10 19:52:46 +0530

    fix(link): keep list format filters when merging link filters
    (cherry picked from commit f326c164944ebf84b22ff38dee12a1e6f7001ecd)

--- frappe/public/js/frappe/form/controls/link.js
+++ frappe/public/js/frappe/form/controls/link.js
@@ -845,7 +845,21 @@ frappe.ui.form.ControlLink = class ControlLink extends frappe.ui.form.ControlDat
 		}
 
 		if (this.df.link_filters && !!this.df.link_filters.length) {
-			args.filters = { ...(args.filters || {}), ...this.apply_link_field_filters() };
+			const link_filters = this.apply_link_field_filters();
+
+			if (Array.isArray(args.filters)) {
+				const fieldnames = Object.keys(link_filters);
+				args.filters = args.filters
+					.filter(
+						(filter) =>
+							!fieldnames.includes(filter.length >= 4 ? filter[1] : filter[0])
+					)
+					.concat(
+						fieldnames.map((fieldname) => [fieldname, ...link_filters[fieldname]])
+					);
+			} else {
+				args.filters = { ...(args.filters || {}), ...link_filters };
+			}
 		}
 	}
 
