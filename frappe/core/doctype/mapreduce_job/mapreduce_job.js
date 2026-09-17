fa5deaf9f0729d5bcf4bad0fe64925c7ba6d6020
ruthra kumar <ruthra@erpnext.com>
2026-09-17 14:31:51 +0530

    refactor: only get progress on submitted mapreduce

--- frappe/core/doctype/mapreduce_job/mapreduce_job.js
+++ frappe/core/doctype/mapreduce_job/mapreduce_job.js
@@ -28,33 +28,35 @@ frappe.ui.form.on("MapReduce Job", {
 			__("Actions")
 		);
 
-		frm.call({
-			method: "frappe.core.doctype.mapreduce_job.mapreduce_job.get_progress",
-			args: {
-				job: frm.doc.name,
-			},
-		}).then((r) => {
-			if (!r.exc) {
-				frm.dashboard.add_progress("Task completion", r.message.progress, "");
-				if (r.message.status == "Queued") {
-					add_control_button(
-						frm,
-						"frappe.core.doctype.mapreduce_job.mapreduce_job.pause_execution",
-						"Pause",
-						frm.doc.name,
-						"Tasks paused"
-					);
-				} else if (r.message.status == "Paused") {
-					add_control_button(
-						frm,
-						"frappe.core.doctype.mapreduce_job.mapreduce_job.start_execution",
-						"Resume",
-						frm.doc.name,
-						"Tasks queued"
-					);
+		if (frm.doc.docstatus == 1) {
+			frm.call({
+				method: "frappe.core.doctype.mapreduce_job.mapreduce_job.get_progress",
+				args: {
+					job: frm.doc.name,
+				},
+			}).then((r) => {
+				if (!r.exc) {
+					frm.dashboard.add_progress("Task completion", r.message.progress, "");
+					if (r.message.status == "Queued") {
+						add_control_button(
+							frm,
+							"frappe.core.doctype.mapreduce_job.mapreduce_job.pause_execution",
+							"Pause",
+							frm.doc.name,
+							"Tasks paused"
+						);
+					} else if (r.message.status == "Paused") {
+						add_control_button(
+							frm,
+							"frappe.core.doctype.mapreduce_job.mapreduce_job.start_execution",
+							"Resume",
+							frm.doc.name,
+							"Tasks queued"
+						);
+					}
 				}
-			}
-		});
+			});
+		}
 	},
 });
 function add_control_button(frm, method, title, job_id, msg) {
