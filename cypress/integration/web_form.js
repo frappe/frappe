acc7d7ed25adc43dad70ed8f93abb729405d3281
Shivam Ghosh <shivam@buildwithhussain.com>
2026-09-17 11:53:20 +0530

    fix(Web Form): skip field defaults for saved documents
    (cherry picked from commit 42b2b942ab6eb9f6244ca30a81f634abe36004f6)

--- cypress/integration/web_form.js
+++ cypress/integration/web_form.js
@@ -221,6 +221,9 @@ context("Web Form", () => {
 
 	it("Retain Saved Value Over Field Default", () => {
 		cy.visit("/note");
+		set_web_form_field_default("expire_notification_on", "2030-01-01 00:00:00");
+
+		cy.visit("/note");
 		cy.url().should("include", "/note/list");
 		cy.get(".web-list-table tbody tr:last").click();
 
@@ -232,6 +235,16 @@ context("Web Form", () => {
 		cy.get(".success-page .edit-button").click();
 
 		cy.get('input[data-fieldname="public"]').should("be.checked");
+
+		cy.call("frappe.client.get_value", {
+			doctype: "Note",
+			filters: { title: "Note 1 Edited" },
+			fieldname: "expire_notification_on",
+		})
+			.its("message.expire_notification_on")
+			.should("be.null");
+
+		set_web_form_field_default("expire_notification_on", "");
 	});
 
 	it("Allow Multiple Response", () => {
@@ -303,3 +316,10 @@ context("Web Form", () => {
 		});
 	});
 });
+
+function set_web_form_field_default(fieldname, value) {
+	cy.get_doc("Web Form", "note").then(({ data }) => {
+		const field = data.web_form_fields.find((df) => df.fieldname === fieldname);
+		cy.set_value("Web Form Field", field.name, { default: value });
+	});
+}
