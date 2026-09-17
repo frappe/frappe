1c07887ef2a22ad22afa6e5aaa6fd31f9e71e853
Ejaaz Khan <ejaaz@frappe.io>
2026-09-17 15:53:29 +0530

    fix(pfb): close the linked field picker after a choice

--- frappe/public/js/print_format_builder/components/inspector/SelectRow.vue
+++ frappe/public/js/print_format_builder/components/inspector/SelectRow.vue
@@ -4,7 +4,7 @@
 		<Autocomplete
 			class="pfb-insp-autocomplete"
 			:options="options"
-			:model-value="modelValue || null"
+			:model-value="modelValue || ''"
 			:placeholder="placeholder"
 			@select="(o) => $emit('update:modelValue', o.value)"
 		/>
