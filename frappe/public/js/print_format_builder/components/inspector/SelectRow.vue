143bc549ff0ada6bad445edc6904407aee5d3a13
Ejaaz Khan <ejaaz@frappe.io>
2026-09-17 15:48:46 +0530

    feat(pfb): autocomplete for the linked field pickers

--- frappe/public/js/print_format_builder/components/inspector/SelectRow.vue
+++ frappe/public/js/print_format_builder/components/inspector/SelectRow.vue
@@ -1,18 +1,19 @@
 <template>
 	<div class="pfb-insp-row">
 		<span class="pfb-insp-label">{{ label }}</span>
-		<select
-			class="pfb-insp-select"
-			:value="modelValue || ''"
-			@change="$emit('update:modelValue', $event.target.value)"
-		>
-			<option value="">{{ placeholder }}</option>
-			<option v-for="o in options" :key="o.value" :value="o.value">{{ o.label }}</option>
-		</select>
+		<Autocomplete
+			class="pfb-insp-autocomplete"
+			:options="options"
+			:model-value="modelValue || null"
+			:placeholder="placeholder"
+			@select="(o) => $emit('update:modelValue', o.value)"
+		/>
 	</div>
 </template>
 
 <script setup>
+import Autocomplete from "../../../vue-components/Autocomplete.vue";
+
 defineProps(["label", "modelValue", "options", "placeholder"]);
 defineEmits(["update:modelValue"]);
 </script>
