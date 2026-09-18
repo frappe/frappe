e14c82c0b06212f68fdff1ccec558c067d615b78
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-09-18 08:58:43 +0530

    fix(report): match prepared report owner regardless of report name
    (cherry picked from commit 7082b6b2d3d467b123c2f19569afa23217e060ad)

--- frappe/desk/query_report.py
+++ frappe/desk/query_report.py
@@ -298,9 +298,7 @@ def _run(
 					filters = json.loads(filters)
 
 				dn = filters.pop("prepared_report_name", None)
-				if dn and not frappe.db.exists(
-					"Prepared Report", {"name": dn, "owner": user, "report_name": report_name}
-				):
+				if dn and not frappe.db.exists("Prepared Report", {"name": dn, "owner": user}):
 					frappe.has_permission("Prepared Report", "read", dn, throw=True)
 			else:
 				dn = ""
