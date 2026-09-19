096ba7f49aebf300b1cf1790128482f9c2a8e8b6
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-09-19 18:36:20 +0530

    test(email_account): positional outgoing lookup after a cached default
    (cherry picked from commit 707200e302a99f9513c5c0331b216ed70cc4f2ba)

--- frappe/email/test_smtp.py
+++ frappe/email/test_smtp.py
@@ -77,6 +77,7 @@ class TestSMTP(FrappeTestCase):
 			EmailAccount.find_outgoing(match_by_email="support@example.com").email_id, "support@example.com"
 		)
 		self.assertEqual(EmailAccount.find_outgoing(match_by_doctype="ToDo").email_id, "support@example.com")
+		self.assertEqual(EmailAccount.find_outgoing("support@example.com").email_id, "support@example.com")
 
 
 def create_email_account(email_id, password, enable_outgoing, default_outgoing=0, append_to=None):
