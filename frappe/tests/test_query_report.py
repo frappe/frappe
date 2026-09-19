726362f59dc2e4ccad48f83bf3930ff32821470b
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-09-19 23:39:57 +0530

    test(report): owner of a deleted custom report's prepared report gets permission error

--- frappe/tests/test_query_report.py
+++ frappe/tests/test_query_report.py
@@ -222,6 +222,10 @@ class TestQueryReport(IntegrationTestCase):
 		with self.set_user(reader.name), self.assertRaises(frappe.PermissionError):
 			run(report.name, filters)
 
+		custom_report.delete()
+		with self.set_user(owner.name), self.assertRaises(frappe.PermissionError):
+			run(report.name, filters)
+
 	def test_xlsx_data_with_multiple_datatypes(self):
 		"""Test exporting report using rows with multiple datatypes (list, dict)"""
 
