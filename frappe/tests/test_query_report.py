142df33b932951c61652521b39ac43ca403ab544
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-09-20 12:37:33 +0530

    test(report): run the chained custom column test as a regular user

--- frappe/tests/test_query_report.py
+++ frappe/tests/test_query_report.py
@@ -476,18 +476,34 @@ data = columns, result
 	def test_custom_column_linked_to_another_custom_column(self):
 		"""Test custom column that looks up its value through another custom column"""
 
-		frappe.set_user("Administrator")
+		user = frappe.get_doc(
+			{
+				"doctype": "User",
+				"email": "test_custom_column_chain@example.com",
+				"first_name": "Rhea",
+				"last_name": "Menon",
+				"send_welcome_email": 0,
+				"roles": [{"role": "System Manager"}],
+			}
+		).insert()
+
+		self.addCleanup(frappe.set_user, frappe.session.user)
+		frappe.set_user(user.name)
 		todo = frappe.get_doc({"doctype": "ToDo", "description": "Follow up on renewal"}).insert()
 
 		custom_columns = [
 			{"fieldname": "owner", "doctype": "ToDo", "link_field": {"fieldname": "todo", "names": []}},
-			{"fieldname": "email", "doctype": "User", "link_field": {"fieldname": "owner", "names": []}},
+			{
+				"fieldname": "full_name",
+				"doctype": "User",
+				"link_field": {"fieldname": "owner", "names": []},
+			},
 		]
 
 		result = add_custom_column_data(custom_columns, [{"todo": todo.name}])
 
 		self.assertDictEqual(
-			{"todo": todo.name, "owner": "Administrator", "email": "admin@example.com"},
+			{"todo": todo.name, "owner": user.name, "full_name": "Rhea Menon"},
 			result[0],
 		)
 
