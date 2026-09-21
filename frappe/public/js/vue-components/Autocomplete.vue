e230f04ea7c4a02ed7a32c9e5755ef1addfc24a1
Ejaaz Khan <ejaaz@frappe.io>
2026-09-21 23:38:19 +0530

    fix(pfb): the shared autocomplete uses the same search icon size

--- frappe/public/js/vue-components/Autocomplete.vue
+++ frappe/public/js/vue-components/Autocomplete.vue
@@ -31,7 +31,7 @@
 		<div class="frappe-autocomplete-input-wrap" :class="{ focused }">
 			<span
 				class="frappe-autocomplete-icon"
-				v-html="frappe.utils.icon('search', 'xs')"
+				v-html="frappe.utils.icon('search', 'sm')"
 			></span>
 			<input
 				ref="input_el"
@@ -158,7 +158,7 @@ defineExpose({ focus: () => input_el.value?.focus() });
 .frappe-autocomplete-icon {
 	display: flex;
 	align-items: center;
-	color: var(--gray-400);
+	color: var(--ink-gray-4);
 	flex-shrink: 0;
 }
 
