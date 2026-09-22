7372ea49c762324226a8adb004fe0ae7133b47aa
Bowrna <mailbowrna@gmail.com>
2026-09-22 14:24:35 +0530

    fix(controls): Set type as button explicitely to avoid trigger on enter

--- frappe/public/js/frappe/form/controls/button.js
+++ frappe/public/js/frappe/form/controls/button.js
@@ -22,7 +22,7 @@ frappe.ui.form.ControlButton = class ControlButton extends frappe.ui.form.Contro
 
 		const btn_size = this.df.btn_size ? `btn-${this.df.btn_size}` : "btn-xs";
 		this.$input = $(
-			`<button
+			`<button type="button"
 				class="btn ${frappe.utils.escape_html(btn_size)} ${frappe.utils.escape_html(btn_type)}"
 				title="${this.df.title || frappe.utils.escape_html(this.df.label)}"
 			>`
