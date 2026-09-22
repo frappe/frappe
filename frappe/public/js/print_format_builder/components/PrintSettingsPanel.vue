a4bf40ef3a09ec7dc8d305afe1a04880f3ca2c9a
Ejaaz Khan <ejaaz@frappe.io>
2026-09-22 13:03:09 +0530

    fix(pfb): say what typst cannot render

--- frappe/public/js/print_format_builder/components/PrintSettingsPanel.vue
+++ frappe/public/js/print_format_builder/components/PrintSettingsPanel.vue
@@ -176,7 +176,7 @@ let hint_icon = ref(null);
 let renderer_hint = computed(() => {
 	if (typst_blockers.value.length) {
 		const items = typst_blockers.value.map((b) => "• " + frappe.utils.escape_html(b));
-		return [__("Typst needs these fixed:"), ...items].join("<br>");
+		return [__("Typst cannot render:"), ...items].join("<br>");
 	}
 	if (has_typst_block.value) {
 		return __("Chromium unavailable: this format uses a Typst block.");
