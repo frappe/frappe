eba8e9f38a163ef30f46929e69f1b1b0c428f4c6
AarDG10 <aarol.dsouza@gmail.com>
2026-09-23 12:06:40 +0530

    fix(login): stop password reset flow from bypassing 2FA
    (cherry picked from commit 5339359ce4858099ac962e40842a6160112c1656)

--- frappe/templates/includes/login/login.js
+++ frappe/templates/includes/login/login.js
@@ -277,7 +277,7 @@ login.login_handlers = (function () {
 				document.body.innerHTML = `{% include "templates/includes/splash_screen.html" %}`;
 				window.location.href = frappe.utils.sanitise_redirect(frappe.utils.get_url_arg("redirect-to")) || data.home_page;
 			} else if (data.message == 'Password Reset') {
-				window.location.href = frappe.utils.sanitise_redirect(data.redirect_to);
+				login.set_status({{ _("Reset link emailed") | tojson }}, 'green');
 			} else if (data.message == "No App") {
 				login.set_status({{ _("Success") | tojson }}, 'green');
 				if (localStorage) {
