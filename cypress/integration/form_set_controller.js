2a24491254dbd8a1f0987e69517341b057370f91
Mihir Kandoi <kandoimihir@gmail.com>
2026-09-23 13:20:41 +0530

    test(form): cover a subclass bound before set_controller binds

--- cypress/integration/form_set_controller.js
+++ cypress/integration/form_set_controller.js
@@ -1,5 +1,6 @@
 import form_controller_doctype from "../fixtures/form_controller_doctype";
 const doctype_name = form_controller_doctype.name;
+const subclass_doctype_name = "Form Controller Subclass Test";
 
 const controller_script = `
 frappe.ui.form.set_controller(
@@ -16,22 +17,43 @@ frappe.ui.form.set_controller(
 );
 `;
 
+const subclass_script = `
+class Base extends frappe.ui.form.Controller {
+	title() {
+		this.frm.set_value("mirror", "Set by the registered class");
+	}
+}
+
+class Subclass extends Base {
+	title(doc) {
+		this.frm.set_value("mirror", doc.title);
+	}
+}
+
+frappe.ui.form.set_controller("${subclass_doctype_name}", Base);
+extend_cscript(cur_frm.cscript, new Subclass({ frm: cur_frm }));
+`;
+
+function insert_client_script(dt, script) {
+	return cy.insert_doc(
+		"Client Script",
+		{ __newname: dt, dt: dt, view: "Form", script: script, enabled: 1 },
+		true
+	);
+}
+
 context("Form Set Controller", () => {
 	before(() => {
 		cy.login();
 		cy.visit("/desk/website");
 		cy.insert_doc("DocType", form_controller_doctype, true);
-		return cy.insert_doc(
-			"Client Script",
-			{
-				__newname: doctype_name,
-				dt: doctype_name,
-				view: "Form",
-				script: controller_script,
-				enabled: 1,
-			},
+		cy.insert_doc(
+			"DocType",
+			{ ...form_controller_doctype, name: subclass_doctype_name },
 			true
 		);
+		insert_client_script(doctype_name, controller_script);
+		return insert_client_script(subclass_doctype_name, subclass_script);
 	});
 
 	it("binds the controller class while the form loads", () => {
@@ -41,6 +63,12 @@ context("Form Set Controller", () => {
 			"Set by controller setup"
 		);
 
+		cy.fill_field("title", "Mirrored").blur();
+		cy.get_field("mirror").should("have.value", "Mirrored");
+	});
+
+	it("keeps a subclass that a form script already bound", () => {
+		cy.new_form(subclass_doctype_name);
 		cy.fill_field("title", "Mirrored").blur();
 		cy.get_field("mirror").should("have.value", "Mirrored");
 	});
