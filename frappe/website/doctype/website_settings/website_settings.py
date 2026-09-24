f5f3177cb724457d905f2637f46c71c9c44348d3
Arya Dharmadhikari <aryadharmadhikari10@gmail.com>
2026-09-24 21:25:32 +0530

    fix(website): remove navbar search setting
    (cherry picked from commit f5b77b17b14e1d16a4a2ad17d513236885c47aef)
    
    # Conflicts:
    #	frappe/website/doctype/website_settings/website_settings.json
    #	frappe/website/doctype/website_settings/website_settings.py

--- frappe/website/doctype/website_settings/website_settings.py
+++ frappe/website/doctype/website_settings/website_settings.py
@@ -50,7 +50,11 @@ class WebsiteSettings(Document):
 		home_page: DF.Data | None
 		indexing_authorization_code: DF.Data | None
 		indexing_refresh_token: DF.Data | None
+<<<<<<< HEAD
 		navbar_search: DF.Check
+=======
+		max_signups_per_minute: DF.Int
+>>>>>>> f5b77b1 (fix(website): remove navbar search setting)
 		navbar_template: DF.Link | None
 		navbar_template_values: DF.Code | None
 		robots_txt: DF.Code | None
@@ -200,7 +204,6 @@ def get_website_settings(context=None):
 		"title_prefix",
 		"navbar_template",
 		"footer_template",
-		"navbar_search",
 		"enable_view_tracking",
 		"footer_logo",
 		"call_to_action",
