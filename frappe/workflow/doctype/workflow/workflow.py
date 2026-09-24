1901e5dd495d755ea9af01991ead702d3ef61ae1
AarDG10 <aarol.dsouza@gmail.com>
2026-09-24 11:28:49 +0530

    fix(workflow): replace get_all by get_list instead

--- frappe/workflow/doctype/workflow/workflow.py
+++ frappe/workflow/doctype/workflow/workflow.py
@@ -146,8 +146,12 @@ def get_workflow_state_count(doctype: str, workflow_state_field: str, states: st
 	frappe.has_permission(doctype=doctype, ptype="read", throw=True)
 	states = frappe.parse_json(states)
 
-	if workflow_state_field in frappe.get_meta(doctype).get_valid_columns():
-		result = frappe.get_all(
+	is_workflow_field = frappe.db.exists(
+		"Workflow",
+		{"document_type": doctype, "workflow_state_field": workflow_state_field},
+	)
+	if is_workflow_field and workflow_state_field in frappe.get_meta(doctype).get_valid_columns():
+		result = frappe.get_list(
 			doctype,
 			fields=[workflow_state_field, {"COUNT": "*", "as": "count"}],
 			filters={workflow_state_field: ["not in", states]},
