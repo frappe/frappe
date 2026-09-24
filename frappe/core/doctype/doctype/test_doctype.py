fff40be1a368ba442d4c14675f730cd068b8b2ed
Shllokkk <shllokosan23@gmail.com>
2026-09-24 10:58:19 +0530

    test: no-value fieldtypes reset column-only properties on save

--- frappe/core/doctype/doctype/test_doctype.py
+++ frappe/core/doctype/doctype/test_doctype.py
@@ -386,6 +386,23 @@ class TestDocType(IntegrationTestCase):
 
 		self.assertRaises(CannotIndexedError, doc.insert)
 
+	def test_no_value_field_resets_column_properties(self):
+		doc = new_doctype("Test No Value Field Reset")
+		field_1 = doc.append("fields", {})
+		field_1.fieldname = "some_fieldname_1"
+		field_1.fieldtype = "HTML"
+		field_1.unique = 1
+		field_1.search_index = 1
+		field_1.not_nullable = 1
+
+		doc.insert()  # would raise NonUniqueError before the reset
+
+		self.assertFalse(field_1.unique)
+		self.assertFalse(field_1.search_index)
+		self.assertFalse(field_1.not_nullable)
+
+		doc.delete()
+
 	def test_cancel_link_doctype(self):
 		import json
 
