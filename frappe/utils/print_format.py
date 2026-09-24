63ddb243117dbfa11accd9d91462f7d25daf7de1
Ejaaz Khan <ejaaz@frappe.io>
2026-09-25 01:16:26 +0530

    fix(print): tell the list view a bulk print failed before aborting on a permission error

--- frappe/utils/print_format.py
+++ frappe/utils/print_format.py
@@ -272,6 +272,8 @@ def _download_multi_pdf(
 			try:
 				pdf_writer = print_into_writer(doctype, ss)
 			except frappe.PermissionError:
+				if task_id:
+					frappe.publish_realtime(task_id=task_id, message={"message": "Failed"})
 				raise
 			except Exception:
 				frappe.log_error(
@@ -307,6 +309,8 @@ def _download_multi_pdf(
 				try:
 					pdf_writer = print_into_writer(doctype_name, doc_name)
 				except frappe.PermissionError:
+					if task_id:
+						frappe.publish_realtime(task_id=task_id, message="Failed")
 					raise
 				except Exception:
 					if task_id:
