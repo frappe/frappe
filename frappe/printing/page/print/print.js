9b7a37079928c1266ae81969b141884deb135c6e
Ejaaz Khan <ejaaz@frappe.io>
2026-09-25 00:23:38 +0530

    fix(print): download the PDF in the language the print view shows

--- frappe/printing/page/print/print.js
+++ frappe/printing/page/print/print.js
@@ -752,6 +752,9 @@ frappe.ui.form.PrintView = class {
 			if (this.additional_settings && Object.keys(this.additional_settings).length) {
 				params.append("settings", JSON.stringify(this.additional_settings));
 			}
+			if (this.lang_code) {
+				params.append("_lang", this.lang_code);
+			}
 			let w = window.open(
 				`/api/method/frappe.utils.print_format_generator.download_pdf?${params}`
 			);
